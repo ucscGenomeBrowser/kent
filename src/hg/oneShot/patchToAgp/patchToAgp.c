41a8c42b864f7f5c61be91624d3f81e4059ae2d6
kent <kent>
2001-03-01 18:59:59 +0000

    Should work now.

--- src/hg/oneShot/patchToAgp/patchToAgp.c
+++ src/hg/oneShot/patchToAgp/patchToAgp.c
@@ -1,5 +1,7 @@
 /* patchToAgp - Convert Elia's patch file that describe NT clone position to AGP file. */
 #include "common.h"
+#include "hash.h"
+#include "linefile.h"
 
 void usage()
 /* Explain usage and exit. */
@@ -7,19 +9,67 @@ void usage()
 errAbort(
   "patchToAgp - Convert Elia's patch file that describe NT clone position to AGP file\n"
   "usage:\n"
-  "   patchToAgp XXX\n");
+  "   patchToAgp ~gs/ffa/sequence.inf in.patch out.agp\n");
 }
 
-void patchToAgp(char *XXX)
+struct hash *readVersionInfo(char *fileName)
+/* Read sequence.inf file to figure out version number of
+ * each clone.  Save it as a hash keyed by accession with
+ * values accession.version. */
+{
+char acc[128];
+char *row[1];
+struct lineFile *lf = lineFileOpen(fileName, TRUE);
+struct hash *hash = newHash(0);
+
+while (lineFileRow(lf, row))
+    {
+    strcpy(acc, row[0]);
+    chopSuffix(acc);
+    hashAdd(hash, acc, cloneString(row[0]));
+    }
+lineFileClose(&lf);
+return hash;
+}
+
+char *fixVersion(char *accVer, struct hash *versionHash)
+/* See if accVer ends in .0.  If so try to fix it via
+ * hash lookup. */
+{
+static char fixed[128];
+char cloneName[128];
+char *s;
+
+strcpy(cloneName, accVer);
+chopSuffix(cloneName);
+return hashMustFindVal(versionHash, cloneName);
+}
+
+void patchToAgp(char *infName, char *inName, char *outName)
 /* patchToAgp - Convert Elia's patch file that describe NT clone position to AGP file. */
 {
+char *row[9];
+struct lineFile *lf = lineFileOpen(inName, TRUE);
+FILE *f = mustOpen(outName, "w");
+int ix = 0;
+struct hash *versionHash = readVersionInfo(infName);
+
+while (lineFileRow(lf, row))
+    {
+    fprintf(f, "%s\t%s\t%s\t%d\t%s\t%s\t%s\t%s\t%c\n",
+	row[0],
+	row[2], row[3], ++ix,
+	row[5], fixVersion(row[6], versionHash),
+	row[7], row[8], 
+	row[4][0]);
+    }
 }
 
 int main(int argc, char *argv[])
 /* Process command line. */
 {
-if (argc != 2)
+if (argc != 4)
     usage();
-patchToAgp(argv[1]);
+patchToAgp(argv[1], argv[2], argv[3]);
 return 0;
 }
