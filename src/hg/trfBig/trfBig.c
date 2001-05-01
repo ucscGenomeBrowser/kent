e079e30bbf2864f2e1cad4c60edafd23abf8a19a
kent <kent>
2001-05-01 19:40:05 +0000

    Added support for .fa files.

--- src/hg/trfBig/trfBig.c
+++ src/hg/trfBig/trfBig.c
@@ -6,6 +6,10 @@
 #include "portable.h"
 #include "cheapcgi.h"
 
+/* Variables that can be set from command line. */
+char *trfExe = "trf";	/* trf executable name. */
+boolean doBed = FALSE;	/* Output .bed file as well as masked file. */
+
 void usage()
 /* Explain usage and exit. */
 {
@@ -18,7 +22,8 @@ errAbort(
   "or .nib format.\n"
   "\n"
   "options:\n"
-  "   -bed creates a bed file in current dir\n");
+  "   -bed creates a bed file in current dir\n"
+  "   -trf=trfExe explicitly specifies trf executable name\n");
 }
 
 void writeSomeDatToBed(char *inName, FILE *out, char *chromName, int chromOffset, 
@@ -64,9 +69,46 @@ void removeWild(char *pat)
 {
 char dir[256], fn[128], ext[64];
 char wild[256];
+struct slName *list, *el;
+
 splitPath(pat, dir, fn, ext);
 sprintf(wild, "%s%s", fn, ext);
-uglyf("dir '%s' fn '%s' ext '%s' pat '%s'\n", dir, fn, ext, pat);
+
+list = listDir(dir, wild);
+for (el = list; el != NULL; el = el->next)
+    {
+    remove(el->name);
+    uglyf("Removed %s\n", el->name);
+    }
+slFreeList(&list);
+}
+
+void makeTrfRootName(char trfRootName[512], char *faFile)
+/* Make root name of files trf produces from faFile. */
+{
+sprintf(trfRootName, "%s.2.7.7.80.10.50.500", faFile);
+}
+
+void trfSysCall(char *faFile)
+/* Invoke trf program on file. */
+{
+char command[512];
+sprintf(command, "%s %s 2 7 7 80 10 50 500 -m %s", 
+	trfExe, faFile, doBed ? "-d" : "");
+system(command);
+}
+
+void outputWithBreaks(FILE *out, char *s, int size, int lineSize)
+/* Print s of given size to file, adding line feeds every now and then. */
+{
+int i, oneSize;
+for (i=0; i<size; i += oneSize)
+    {
+    oneSize = size - i;
+    if (oneSize > lineSize) oneSize = lineSize;
+    mustWrite(out, s+i, oneSize);
+    fputc('\n', out);
+    }
 }
 
 void trfBig(char *input, char *output)
@@ -76,12 +118,11 @@ int maxSize = 5000000;
 int overlapSize = 10000;
 int start, end, s, e;
 int halfOverlapSize = overlapSize/2;
-struct dnaSeq *seqList = NULL, *seq;
 char *tempFile, trfRootName[512], trfTemp[512], bedFileName[512];
-char command[512];
 char dir[256], chrom[128], ext[64];
-boolean doBed = cgiBoolean("bed");
 FILE *bedFile = NULL;
+struct tempName tn;
+struct dnaSeq  *maskedSeq = NULL;
 
 if (doBed)
     {
@@ -89,16 +130,15 @@ if (doBed)
     sprintf(bedFileName, "%s%s.bed", dir, chrom);
     bedFile = mustOpen(bedFileName, "w");
     }
+makeTempName(&tn, "trf", ".fa");
+tempFile = tn.forCgi;
 if (endsWith(input, ".nib") && endsWith(output, ".nib"))
     {
     int nibSize;
     FILE *in;
     struct nibStream *ns = nibStreamOpen(output);
-    struct tempName tn;
+    struct dnaSeq *seq;
 
-    
-    makeTempName(&tn, "trf", ".fa");
-    tempFile = tn.forCgi;
     nibOpenVerify(input, &in, &nibSize);
     for (start = 0; start < nibSize; start = end)
         {
@@ -107,12 +147,10 @@ if (endsWith(input, ".nib") && endsWith(output, ".nib"))
 	seq = nibLdPart(input, in, nibSize, start, end - start);
 	faWrite(tempFile, seq->name, seq->dna, seq->size);
 	freeDnaSeq(&seq);
-	sprintf(command, "trf %s 2 7 7 80 10 50 500 -m %s", 
-		tempFile, doBed ? "-d" : "");
-	system(command);
-	sprintf(trfRootName, "%s.2.7.7.80.10.50.500", tempFile);
+	trfSysCall(tempFile);
+	makeTrfRootName(trfRootName, tempFile);
 	sprintf(trfTemp, "%s.mask", trfRootName);
-	seq = faReadDna(trfTemp);
+	maskedSeq = faReadDna(trfTemp);
 	s = (start == 0 ? 0 : halfOverlapSize);
 	if (end == nibSize)
 	    e = end - start;
@@ -121,7 +159,8 @@ if (endsWith(input, ".nib") && endsWith(output, ".nib"))
 	    e = end - halfOverlapSize - start;
 	    end -= overlapSize;
 	    }
-	nibStreamMany(ns, seq->dna + s, e-s);
+	nibStreamMany(ns, maskedSeq->dna + s, e-s);
+	freeDnaSeq(&maskedSeq);
 	if (doBed)
 	    {
 	    sprintf(trfTemp, "%s.dat", trfRootName);
@@ -129,19 +168,59 @@ if (endsWith(input, ".nib") && endsWith(output, ".nib"))
 	    }
 	}
     nibStreamClose(&ns);
-    sprintf(trfTemp, "%s*", tempFile);
-    removeWild(trfTemp);
+    }
+else if (!endsWith(input, ".nib") && !endsWith(output, ".nib"))
+    {
+    struct lineFile *lf = lineFileOpen(input, TRUE);
+    struct dnaSeq seq;
+    FILE *f = mustOpen(output, "w");
+    FILE *out = mustOpen(output, "w");
+    while (faSpeedReadNext(lf, &seq.dna, &seq.size, &seq.name))
+        {
+	fprintf(out, ">%s\n", seq.name);
+	for (start = 0; start < seq.size; start = end)
+	    {
+	    end = start + maxSize;
+	    if (end > seq.size) end = seq.size;
+	    faWrite(tempFile, seq.name, seq.dna+start, end - start);
+	    trfSysCall(tempFile);
+	    makeTrfRootName(trfRootName, tempFile);
+	    sprintf(trfTemp, "%s.mask", trfRootName);
+	    maskedSeq = faReadDna(trfTemp);
+	    s = (start == 0 ? 0 : halfOverlapSize);
+	    if (end == seq.size)
+		e = end - start;
+	    else
+		{
+		e = end - halfOverlapSize - start;
+		end -= overlapSize;
+		}
+	    outputWithBreaks(out, maskedSeq->dna+s, e-s, 50);
+	    freeDnaSeq(&maskedSeq);
+	    if (doBed)
+		{
+		sprintf(trfTemp, "%s.dat", trfRootName);
+		writeSomeDatToBed(trfTemp, bedFile, seq.name, start, s, e);
+		}
+	    }
+	}
+    lineFileClose(&lf);
+    carefulClose(&out);
     }
 else
     {
-    errAbort("Sorry, currently can only handle nib files.");
+    errAbort("Sorry, both input and output must be in same format.");
     }
+sprintf(trfTemp, "%s*", tempFile);
+removeWild(trfTemp);
 }
 
 int main(int argc, char *argv[])
 /* Process command line. */
 {
 cgiSpoof(&argc, argv);
+doBed = cgiBoolean("bed");
+trfExe = cgiUsualString("trf", trfExe);
 if (argc != 3)
     usage();
 trfBig(argv[1], argv[2]);
