6df104b42cde29478fae7991c66131efb8a74dc3
kent <kent>
2001-05-14 10:53:18 +0000

    Made fileExists() recognize stdin and stdout.

--- src/lib/portimpl.c
+++ src/lib/portimpl.c
@@ -71,6 +71,11 @@ boolean fileExists(char *fileName)
  * portable faster way some day). */
 {
 int fd;
+/* To make piping easier stdin and stdout always exist. */
+if (sameString(fileName, "stdin")) return TRUE;
+if (sameString(fileName, "stdout")) return TRUE;
+
+/* Otherwise open file and close it to find out... */
 if ((fd = open(fileName, O_RDONLY)) < 0)
     return FALSE;
 close(fd);
