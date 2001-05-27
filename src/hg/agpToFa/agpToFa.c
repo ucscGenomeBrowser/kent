75aaf0c59e49ff48d7991086c92cd10bcceb516b
kent <kent>
2001-05-27 02:47:26 +0000

    Basically works.

--- src/hg/agpToFa/agpToFa.c
+++ src/hg/agpToFa/agpToFa.c
@@ -3,6 +3,9 @@
 #include "linefile.h"
 #include "hash.h"
 #include "cheapcgi.h"
+#include "fa.h"
+#include "agpFrag.h"
+#include "agpGap.h"
 
 void usage()
 /* Explain usage and exit. */
@@ -10,22 +13,78 @@ void usage()
 errAbort(
   "agpToFa - Convert a .agp file to a .fa file\n"
   "usage:\n"
-  "   agpToFa XXX\n"
-  "options:\n"
-  "   -xxx=XXX\n");
+  "   agpToFa in.agp agpSeq out.fa seqDir\n"
+  "Where agpSeq matches a sequence name in in.agp\n"
+  "and seqDirs are where program looks for sequence\n"
+  );
 }
 
-void agpToFa(char *XXX)
+void agpToFa(char *agpFile, char *agpSeq, char *faOut, char *seqDir)
 /* agpToFa - Convert a .agp file to a .fa file. */
 {
+struct lineFile *lf = lineFileOpen(agpFile, TRUE);
+char *line, *words[16];
+int lineSize, wordCount;
+int lastPos = 0;
+struct agpFrag *agpList = NULL, *agp;
+DNA *dna;
+
+while (lineFileNext(lf, &line, &lineSize))
+    {
+    if (line[0] == '#' || line[0] == '\n')
+        continue;
+    wordCount = chopLine(line, words);
+    if (wordCount < 5)
+        errAbort("Bad line %d of %s\n", lf->lineIx, lf->fileName);
+    if (words[4][0] == 'N')
+        errAbort("Sorry, can't handle gap lines yet (line %d of %s)\n", lf->lineIx, lf->fileName);
+    lineFileExpectWords(lf, 9, wordCount);
+    if (sameWord(words[0], agpSeq))
+	{
+	agp = agpFragLoad(words);
+	if (agp->chromStart != lastPos)
+	    errAbort("Start doesn't match previous end line %d of %s\n", lf->lineIx, lf->fileName);
+	if (agp->chromEnd - agp->chromStart != agp->fragEnd - agp->fragStart)
+	    errAbort("Sizes don't match in %s and %s line %d of %s\n",
+	    	agp->chrom, agp->frag, lf->lineIx, lf->fileName);
+	slAddHead(&agpList, agp);
+	lastPos = agp->chromEnd;
+	}
+    }
+slReverse(&agpList);
+if (lastPos == 0)
+    errAbort("%s not found in %s\n", agpSeq, agpFile);
+dna = needLargeMem(lastPos+1);
+memset(dna, 'n', lastPos);
+dna[lastPos] = 0;
+for (agp = agpList; agp != NULL; agp = agp->next)
+    {
+    char clone[128];
+    char path[512];
+    struct dnaSeq *seq;
+    int size;
+    strcpy(clone, agp->frag);
+    chopSuffix(clone);
+    sprintf(path, "%s/%s.fa", seqDir, clone);
+    seq = faReadAllDna(path);
+    if (slCount(seq) != 1)
+        errAbort("Can only handle exactly one clone in %s.", path);
+    size = agp->fragEnd - agp->fragStart;
+    if (agp->strand[0] == '-')
+	reverseComplement(seq->dna + agp->fragStart, size);
+    memcpy(dna + agp->chromStart, seq->dna + agp->fragStart, size);
+    freeDnaSeq(&seq);
+    }
+printf("Writing %d bases to %s\n", lastPos, faOut);
+faWrite(faOut, agpSeq, dna, lastPos);
 }
 
 int main(int argc, char *argv[])
 /* Process command line. */
 {
 cgiSpoof(&argc, argv);
-if (argc != 2)
+if (argc != 5)
     usage();
-agpToFa(argv[1]);
+agpToFa(argv[1], argv[2], argv[3], argv[4]);
 return 0;
 }
--- src/hg/oneShot/musAliAt/musAliAt.c
+++ src/hg/oneShot/musAliAt/musAliAt.c
@@ -3,6 +3,10 @@
 #include "linefile.h"
 #include "hash.h"
 #include "cheapcgi.h"
+#include "jksql.h"
+#include "hdb.h"
+#include "psl.h"
+#include "fa.h"
 
 void usage()
 /* Explain usage and exit. */
@@ -10,22 +14,45 @@ void usage()
 errAbort(
   "musAliAt - Produce .fa files where mouse alignments hit on chr22\n"
   "usage:\n"
-  "   musAliAt XXX\n"
-  "options:\n"
-  "   -xxx=XXX\n");
+  "   musAliAt database chromosome human.fa mouse.fa\n"
+  );
 }
 
-void musAliAt(char *XXX)
+void musAliAt(char *database, char *chrom, char *humanFa, char *mouseFa)
 /* musAliAt - Produce .fa files where mouse alignments hit on chr22. */
 {
+char query[256], **row;
+struct sqlResult *sr;
+struct sqlConnection *conn;
+struct dnaSeq *musSeq, *homoSeq;
+struct psl *psl;
+struct hash *musHash = newHash(10);
+FILE *musOut = mustOpen(mouseFa, "w");
+
+hSetDb(database);
+conn = hAllocConn();
+sprintf(query, "select * from blatMouse where tName = '%s'", chrom);
+sr = sqlGetResult(conn, query);
+while ((row = sqlNextRow(sr)) != NULL)
+    {
+    psl = pslLoad(row);
+    if ((musSeq = hashFindVal(musHash, psl->qName)) == NULL)
+        {
+	musSeq = hExtSeq(psl->qName);
+	hashAdd(musHash, psl->qName, NULL);
+	faWriteNext(musOut, musSeq->name, musSeq->dna, musSeq->size);
+	freeDnaSeq(&musSeq);
+	}
+    pslFree(&psl);
+    }
 }
 
 int main(int argc, char *argv[])
 /* Process command line. */
 {
 cgiSpoof(&argc, argv);
-if (argc != 2)
+if (argc != 5)
     usage();
-musAliAt(argv[1]);
+musAliAt(argv[1], argv[2], argv[3], argv[4]);
 return 0;
 }
