38856825369c87f96464975862ff6ad68d98dc57
kent <kent>
2001-05-27 02:41:01 +0000

    Tinkering.

--- src/blatTheory/blatTheory.c
+++ src/blatTheory/blatTheory.c
@@ -52,19 +52,20 @@ for (i = n; i<= t; ++i)
 return pTotal;
 }
 
-double blatSpecificity(int k, int n, double genomeSize, double querySize)
+double blatSpecificity(int k, int n, double genomeSize, double querySize, int alphabetSize)
 {
-double p = power(0.05, k);
+double a = 1.0/alphabetSize;
+double p = power(a, k);
 double f1 = p*querySize*genomeSize/k;
 double total = f1;
 int i;
 
 for (i=1; i<n; ++i)
-    total *= 300*p;
+    total *= 100.0/k*p;
 return total;
 }
 
-double exonerateSensitivity(double m, int w, int k)
+double exonerateSensitivity(double m, double w, int k)
 /* Probability that exonerate finds a hit. */
 {
 int t = w/k;
@@ -73,10 +74,11 @@ double pTotal = 1.0 - power((1.0 - p), t);
 return pTotal;
 }
 
-double exonerateSpecificity(int k, double genomeSize, double querySize)
+double exonerateSpecificity(int k, double genomeSize, double querySize, double alphabetSize)
 /* Return number of false positives. */
 {
-double p = power(0.25, k) + k*power(0.25, k-1)*(1.0-0.25);
+double a = 1.0/alphabetSize;
+double p = power(a, k) + k*power(a, k-1)*(1.0-a);
 return p*genomeSize*querySize/k;
 }
 
@@ -98,7 +100,7 @@ printf("exonerate sensitivity %f (m=%f, w=%d, k=%d)\n",
 }
 
 
-void blatTheory()
+void oldBlatTheory()
 /* blatTheory - Calculate some theoretical blat statistics. */
 {
 int k;
@@ -114,14 +116,86 @@ printf("  size    hit%%     false pos   hit%%     false pos\n");
 for (w=10; w<=150; w += 10)
     {
     blatSens = blatSensitivity(aaConservation, w/3, 4, blatNum);
-    blatSpec = blatSpecificity(4, blatNum, 6.0e9, 600/3);
+    blatSpec = blatSpecificity(4, blatNum, 6.0e9, 600/3, 20);
     exonSens = exonerateSensitivity(dnaConservation, w, exonerateWin);
-    exonSpec = exonerateSpecificity(exonerateWin, 6.0e9, 600);
+    exonSpec = exonerateSpecificity(exonerateWin, 6.0e9, 600, 4);
     printf("%3d       %4.2f    %5.1f     %4.2f    %5.1f\n",
     	w, 100*exonSens, exonSpec, 100*blatSens, blatSpec);
     }
 }
 
+double p1(double m, int k, double h)
+/* Probability of single tile perfect match. */
+{
+double t = (double)h/(double)k;
+double p = pow(m, k);
+return 1  - pow((1-p), t);
+}
+
+double f1(double q, double g, double k, double a)
+/* Number of single tile false positives. */
+{
+return q*(g/k)*pow(a,-k);
+}
+
+
+
+void blatTheory2()
+/* blatTheory - Calculate some theoretical blat statistics. */
+{
+int k,m,n;
+
+ for (m=85; m<99; m += 1)
+    {
+//    printf("%d%% ", m);
+    for (n=2; n<=3; ++n)
+	for (k=3; k<=6; ++k)
+//	    printf("%1.3f ", blatSensitivity(m*0.01, 100.0/3, k, n));
+	printf("%1.0f ", blatSpecificity(k, n, 3e9, 500.0/3, 20));
+    printf("\n");
+    }
+}
+
+void blatTheory3()
+/* blatTheory - Calculate some theoretical blat statistics. */
+{
+int w,k,m,n;
+int s = 20, e = 150;
+
+printf("5\t");
+/* Probability of single tile perfect match. */
+for (w=s; w<=e; w += 10)
+    printf("%1.3f ", p1(0.89, 5, w/3));
+printf("\n");
+printf("2x4\t");
+for (w=s; w<=e; w += 10)
+    printf("%1.3f ", blatSensitivity(0.89, w/3, 4, 2));
+printf("\n");
+printf("12-1\t");
+for (w=s; w<=e; w += 10)
+    printf("%1.3f ", exonerateSensitivity(0.86, w, 12));
+printf("\n");
+printf("8-1\t");
+for (w=s; w<=e; w += 10)
+    printf("%1.3f ", exonerateSensitivity(0.89, w/3, 8));
+printf("\n");
+}
+
+void blatTheory()
+/* blatTheory - Calculate some theoretical blat statistics. */
+{
+int k;
+
+for (k=5; k<12; ++k)
+    printf("%4d ", k);
+printf("\n");
+for (k=5; k<12; ++k)
+    printf("%1.3f ", blatSensitivity(0.86, 100.0, k, 2));
+printf("\n");
+for (k=5; k<12; ++k)
+    printf("%4.0f ", blatSpecificity(k, 2, 6.0e9, 600, 4));
+printf("\n");
+}
 
 int main(int argc, char *argv[])
 /* Process command line. */
