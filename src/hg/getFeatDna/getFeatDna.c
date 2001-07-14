5ea57409071361ca2204555ce7df41998a7bf6be
kent <kent>
2001-07-14 20:09:05 +0000

    Added several command line options.

--- src/hg/getFeatDna/getFeatDna.c
+++ src/hg/getFeatDna/getFeatDna.c
@@ -3,23 +3,23 @@
 #include "linefile.h"
 #include "hash.h"
 #include "cheapcgi.h"
+#include "genePred.h"
 #include "dnaseq.h"
 #include "fa.h"
 #include "dystring.h"
 #include "jksql.h"
 #include "hdb.h"
 #include "psl.h"
+#include "nib.h"
 
 /* Variables set explicitly or optionally from command line. */
-char *database = NULL;		/* Which database to use. */
-char *table = NULL;		/* Which feature table to use. */
-char *outName = NULL;		/* Output file name. */
-char *chrom = NULL;		/* Chromosome to restrict to. */
-int start = 0;			/* Start of range to select from. */
-int end = BIGNUM;               /* End of range. */
+char *database = NULL;		/* Which database? */
+int chromStart = 0;		/* Start of range to select from. */
+int chromEnd = BIGNUM;          /* End of range. */
 char *where = NULL;		/* Extra selection info. */
 boolean breakUp = FALSE;	/* Break up things? */
-int merge = 0;			/* Merge close blocks? */
+int merge = -1;			/* Merge close blocks? */
+char *outputType = "fasta";	/* Type of output. */
 
 void usage()
 /* Explain usage and exit. */
@@ -28,12 +28,30 @@ errAbort(
   "getFeatDna - Get dna for a type of feature\n"
   "usage:\n"
   "   getFeatDna database table chrom output.fa\n"
+  "where:\n"
+  "   database is the name of a browser database, like 'hg6'\n"
+  "   table is the name of a table in this database, like 'exoFish'\n"
+  "   chrom is a chromosome name, like 'chr8' or 'chr7_random'.  'all'\n"
+  "       can be used here to denote all chromosomes\n"
+  "   output.fa is where to put the output in Fasta format\n"
   "options:\n"
-  "   -start=NNN   restrict to features that start after NNN\n"
-  "   -end=NNN     restrict to features that end after NNN\n"
+  "   -chromStart=NNN   restrict to features that start after NNN\n"
+  "   -chromStart=NNN     restrict to features that end after NNN\n"
   "   '-where=some sql pattern'  restrict to features matching some sql pattern\n"
-  "   -breakUp     break up alignments into pieces\n"
+  "   -breakUp=how break up alignments into pieces.  How is one of\n"
+  "                exons - parts that align\n"
+  "                introns - parts between alignments\n"
+  "                cdsExons - coding exons (only for gene predictions)\n"
   "   -merge=N     merge blocks separated by N or less\n"
+  "   -output=format format of output.  One of:\n"
+  "                fasta - fasta sequence (default)\n"
+  "                pos - position list\n"
+  "   -addStart=NNN How much to add to feature start coordinate\n"
+  "   -addEnd=NNN   How much to add to feature end coordinate\n"
+  "   -addTo=how    How to add to feature coordinates.  One of:\n"
+  "                   whole - the whole thing (default)\n"
+  "                   start - addStart/padEnd relative to feature start\n"
+  "                   end   - addStart/padEnd relative to feature end\n"
   );
 }
 
@@ -54,7 +72,7 @@ else
     return FALSE;
 }
 
-void findChromStartEndFields(struct sqlConnection *conn, char *table, 
+void hFindChromStartEndFields(char *table, 
 	char retChrom[32], char retStart[32], char retEnd[32])
 /* Given a table return the fields for selecting chromosome, start, and end. */
 {
@@ -62,6 +80,7 @@ char query[256];
 struct sqlResult *sr;
 char **row;
 struct hash *hash = newHash(5);
+struct sqlConnection *conn = hAllocConn();
 
 /* Read table description into hash. */
 sprintf(query, "describe %s", table);
@@ -87,71 +106,152 @@ else if (fitFields(hash, "genoName", "genoStart", "genoEnd", retChrom, retStart,
 else
     errAbort("Couldn't find chrom/start/end fields in table %s", table);
 freeHash(&hash);
+hFreeConn(&conn);
 }
 
-void getFeatDna()
-/* getFeatDna - Get dna for a type of feature. */
+static int blockIx = 0;	/* Index of block written. */
+
+char *makeFaStartLine(char *chrom, char *table, int start, int size)
+/* Create fa start line. */
 {
-/* Get chromosome in lower case case.  Set bits that are part of
- * feature of interest to upper case, and then scan for blocks
- * of lower upper case and output them. */
+char faName[128];
+static char faStartLine[512];
+
+++blockIx;
+if (startsWith("chr", table))
+    sprintf(faName, "%s_%d", table, blockIx);
+else
+    sprintf(faName, "%s_%s_%d", chrom, table, blockIx);
+sprintf(faStartLine, "%s %s %s %d %d", 
+	faName, database, chrom, start, start+size);
+return faStartLine;
+}
+
+void writeOut(FILE *f, char *chrom, int start, int size, char strand, DNA *dna, char *faHeader)
+/* Write output to file */
+{
+if (sameWord(outputType, "pos"))
+    fprintf(f, "%s\t%d\t%d\t%c\n", chrom, start, start+size, strand);
+else if (sameWord(outputType, "fasta")
+    faWriteNext(f, faHeader, dna, size);
+else
+    errAbort("Unknown output type %s\n", outputType);
+}
+
+void outputDna(FILE *f, char *chrom, char *table, int start, int size, char *dna, 
+	char *nibFileName, FILE *nibFile, int nibSize, char strand)
+/* Output DNA either directly from nib or by upper-casing DNA. */
+{
+if (merge >= 0)
+    toUpperN(dna + start, size);
+else
+    {
+    struct dnaSeq *seq = nibLdPart(nibFileName, nibFile, nibSize, start, size);
+    if (strand == '-')
+        reverseComplement(seq->dna, size);
+    writeOut(f, chrom, start, size, strand, seq->dna, makeFaStartLine(chrom, table, start, size));
+    freeDnaSeq(&seq);
+    }
+}
+
+void chromFeatDna(char *table, char *chrom, FILE *f)
+/* chromFeatDna - Get dna for a type of feature on one chromosome. */
+{
+/* Get chromosome in lower case case.  If merging set bits that 
+ * are part of feature of interest to upper case, and then scan 
+ * for blocks of lower upper case and output them. If not merging
+ * then just output dna for features directly. */
 struct dyString *query = newDyString(512);
 struct sqlConnection *conn = hAllocConn();
 struct sqlResult *sr;
 char **row;
 char chromField[32], startField[32], endField[32];
-struct dnaSeq *seq;
-DNA *dna;
+struct dnaSeq *seq = NULL;
+DNA *dna = NULL;
 int s,e,sz,i,size;
-int blockIx = 0;
-char faName[64], faStartLine[256];
 boolean lastIsUpper = FALSE, isUpper = FALSE;
-FILE *f = mustOpen(outName, "w");
 int last;
+char nibFileName[512];
+FILE *nibFile = NULL;
+int nibSize;
 
-findChromStartEndFields(conn, table, chromField, startField, endField);
+hFindChromStartEndFields(table, chromField, startField, endField);
 
-seq = hLoadChrom(chrom);
-dna = seq->dna;
-size = seq->size;
+if (merge >= 0)
+    {
+    seq = hLoadChrom(chrom);
+    dna = seq->dna;
+    size = seq->size;
+    }
+else
+    {
+    hNibForChrom(chrom, nibFileName);
+    nibOpenVerify(nibFileName, &nibFile, &nibSize);
+    }
 
 if (breakUp)
     {
-    if (!sameString(startField, "tStart") )
-        errAbort("Can only use breakUp parameter with psl formatted tables");
-    dyStringPrintf(query, "select * from %s where tStart >= %d and tEnd < %d",
-    	table, start, end);
-    dyStringPrintf(query, " and %s = '%s'", chromField, chrom);
-    if (where != NULL)
-	dyStringPrintf(query, " and %s", where);
-    sr = sqlGetResult(conn, query->string);
-    while ((row = sqlNextRow(sr)) != NULL)
+    if (sameString(startField, "tStart"))
+	{
+	dyStringPrintf(query, "select * from %s where tStart >= %d and tEnd < %d",
+	    table, chromStart, chromEnd);
+	dyStringPrintf(query, " and %s = '%s'", chromField, chrom);
+	if (where != NULL)
+	    dyStringPrintf(query, " and %s", where);
+	sr = sqlGetResult(conn, query->string);
+	while ((row = sqlNextRow(sr)) != NULL)
+	    {
+	    struct psl *psl = pslLoad(row);
+	    if (psl->strand[1] == '-')	/* Minus strand on target */
+		{
+		int tSize = psl->tSize;
+		for (i=0; i<psl->blockCount; ++i)
+		     {
+		     sz = psl->blockSizes[i];
+		     s = tSize - (psl->tStarts[i] + sz);
+		     outputDna(f, chrom, table, s, sz, dna, nibFileName, nibFile, nibSize, '-');
+		     }
+		}
+	    else
+		{
+		for (i=0; i<psl->blockCount; ++i)
+		     outputDna(f, chrom, table, psl->tStarts[i], psl->blockSizes[i], 
+			    dna, nibFileName, nibFile, nibSize, '+');
+		}
+	    pslFree(&psl);
+	    }
+	}
+    else if (sameString(startField, "txStart"))
         {
-	struct psl *psl = pslLoad(row);
-	int i;
-	if (psl->strand[1] == '-')	/* Minus strand on target */
+	dyStringPrintf(query, "select * from %s where txStart >= %d and txEnd < %d",
+	    table, chromStart, chromEnd);
+	dyStringPrintf(query, " and %s = '%s'", chromField, chrom);
+	if (where != NULL)
+	    dyStringPrintf(query, " and %s", where);
+	sr = sqlGetResult(conn, query->string);
+	while ((row = sqlNextRow(sr)) != NULL)
 	    {
-	    int tSize = psl->tSize;
-	    for (i=0; i<psl->blockCount; ++i)
+	    struct genePred *gp = genePredLoad(row);
+	    for (i=0; i<gp->exonCount; ++i)
 		 {
-		 sz = psl->blockSizes[i];
-		 s = tSize - (psl->tStarts[i] + sz);
-		 toUpperN(seq->dna + s, sz);
+		 s = gp->exonStarts[i];
+		 sz = gp->exonEnds[i] - s;
+		 outputDna(f, chrom, table, 
+		    s, sz, dna, nibFileName, nibFile, nibSize, gp->strand[0]);
 		 }
+	    genePredFree(&gp);
 	    }
-	else
-	    {
-	    for (i=0; i<psl->blockCount; ++i)
-		 toUpperN(seq->dna + psl->tStarts[i], psl->blockSizes[i]);
-	    }
-	pslFree(&psl);
+	}
+    else
+        {
+        errAbort("Can only use breakUp parameter with psl or genePred formatted tables");
 	}
     }
 else
     {
     dyStringPrintf(query, "select %s,%s from %s where %s >= %d and %s < %d", 
 	    startField, endField, table,
-	    startField, start, endField, end);
+	    startField, chromStart, endField, chromEnd);
     dyStringPrintf(query, " and %s = '%s'", chromField, chrom);
     if (where != NULL)
 	dyStringPrintf(query, " and %s", where);
@@ -161,16 +261,16 @@ else
 	s = sqlUnsigned(row[0]);
 	e = sqlUnsigned(row[1]);
 	sz = e - s;
-	if (sz < 0 || e >= size)
+	if (seq != NULL && (sz < 0 || e >= size))
 	    errAbort("Coordinates out of range %d %d (%s size is %d)", s, e, chrom, size);
-	toUpperN(seq->dna + s, sz);
+	outputDna(f, chrom, table, s, sz, dna, nibFileName, nibFile, nibSize, '+');
 	}
     }
 
 /* Merge nearby upper case blocks. */
-e = -merge-10;		/* Don't merge initial block. */
-if (merge != 0)
+if (merge > 0)
     {
+    e = -merge-10;		/* Don't merge initial block. */
     for (i=0; i<=size; ++i)
 	{
 	isUpper = isupper(dna[i]);
@@ -190,55 +290,75 @@ if (merge != 0)
 	}
     }
 
-for (i=0; i<=size; ++i)
+if (merge >= 0)
     {
-    isUpper = isupper(dna[i]);
-    if (isUpper && !lastIsUpper)
-        s = i;
-    else if (!isUpper && lastIsUpper)
-        {
-	e = i;
-	++blockIx;
-	if (startsWith("chr", table))
-	    sprintf(faName, "%s_%d", table, blockIx);
-	else
-	    sprintf(faName, "%s_%s_%d", chrom, table, blockIx);
-	sprintf(faStartLine, "%s %s %s %d %d", 
-		faName, database, chrom, s, e);
-	faWriteNext(f, faStartLine, dna+s, e-s);
+    for (i=0; i<=size; ++i)
+	{
+	isUpper = isupper(dna[i]);
+	if (isUpper && !lastIsUpper)
+	    s = i;
+	else if (!isUpper && lastIsUpper)
+	    {
+	    e = i;
+	    sz = e - s;
+	    writeOut(f, chrom, s, sz, '+', dna+s, makeFaStartLine(chrom, table, s, sz));
+	    }
+	lastIsUpper = isUpper;
 	}
-    lastIsUpper = isUpper;
     }
+
 hFreeConn(&conn);
-if (!sameString(outName, "stdout"))
+freeDnaSeq(&seq);
+carefulClose(&nibFile);
+}
+
+void getFeatDna(char *table, char *chrom, char *outName)
+/* getFeatDna - Get dna for a type of feature an all relevant chromosomes. */
+{
+char chrTableBuf[256];
+struct slName *chromList = NULL, *chromEl;
+boolean chromSpecificTable = !hTableExists(table);
+char *chrTable = (chromSpecificTable ? chrTableBuf : table);
+FILE *f = mustOpen(outName, "w");
+boolean toStdout = (sameString(outName, "stdout"));
+
+if (sameWord(chrom, "all"))
+    chromList = hAllChromNames();
+else
+    chromList = newSlName(chrom);
+
+for (chromEl = chromList; chromEl != NULL; chromEl = chromEl->next)
+    {
+    chrom = chromEl->name;
+    if (chromSpecificTable)
+        {
+	sprintf(chrTable, "%s_%s", chrom, table);
+	if (!hTableExists(table))
+	    errAbort("table %s (and %s) don't exist in %s", table, 
+	         chrTable, database);
+	}
+    if (!toStdout)
+        printf("Processing %s %s\n", chrTable, chrom);
+    chromFeatDna(chrTable, chrom, f);
+    }
+carefulClose(&f);
+if (!toStdout)
     printf("%d features extracted to %s\n", blockIx, outName);
 }
 
 int main(int argc, char *argv[])
 /* Process command line. */
 {
-char chrTable[256];
-
 cgiSpoof(&argc, argv);
 if (argc != 5)
     usage();
-database = argv[1];
-table = argv[2];
-chrom = argv[3];
-outName = argv[4];
-start = cgiUsualInt("start", start);
-end = cgiUsualInt("end", end);
+chromStart = cgiUsualInt("chromStart", chromStart);
+chromEnd = cgiUsualInt("chromEnd", chromEnd);
 where = cgiOptionalString("where");
 breakUp = cgiBoolean("breakUp");
 merge = cgiUsualInt("merge", merge);
+database = argv[1];
 hSetDb(database);
-if (!hTableExists(table))
-    {
-    sprintf(chrTable, "%s_%s", chrom, table);
-    if (!hTableExists(table))
-        errAbort("table %s (and %s) don't exist in %s", table, chrTable, database);
-    table = chrTable;
-    }
-getFeatDna();
+getFeatDna(argv[2], argv[3], argv[4]);
 return 0;
 }
