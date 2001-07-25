f8cadd2f29b6705e849c08bab05454f139a81aa0
kent <kent>
2001-07-25 20:55:30 +0000

    Seems to work.

--- src/utils/stringify/stringify.c
+++ src/utils/stringify/stringify.c
@@ -1,8 +1,6 @@
 /* stringify - Convert file to C strings. */
 #include "common.h"
 #include "linefile.h"
-#include "hash.h"
-#include "cheapcgi.h"
 
 void usage()
 /* Explain usage and exit. */
@@ -10,23 +8,39 @@ void usage()
 errAbort(
   "stringify - Convert file to C strings\n"
   "usage:\n"
-  "   stringify XXX\n"
-  "options:\n"
-  "   -xxx=XXX\n"
+  "   stringify in.txt\n"
+  "A stringified version of in.txt  will be printed to standard output.\n"
   );
 }
 
-void stringify(char *XXX)
+void stringify(char *fileName, FILE *f)
 /* stringify - Convert file to C strings. */
 {
+struct lineFile *lf = lineFileOpen(fileName, TRUE);
+char *line, c;
+
+while (lineFileNext(lf, &line, NULL))
+    {
+    fputc('"', f);
+    while ((c = *line++) != 0)
+        {
+        if (c == '"' || c == '\\')
+            fputc('\\', f);
+        fputc(c, f);
+        }
+    fputc('\\', f);
+    fputc('n', f);
+    fputc('"', f);
+    fputc('\n', f);
+    }
 }
 
 int main(int argc, char *argv[])
 /* Process command line. */
 {
-cgiSpoof(&argc, argv);
 if (argc != 2)
     usage();
-stringify(argv[1]);
+stringify(argv[1], stdout);
 return 0;
 }
+
