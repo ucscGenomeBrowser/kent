41362c12dc689e00bee1669f4173f6b7862eb0a2
kent <kent>
2001-08-14 17:29:22 +0000

    Added count to types query when they are operating on a chromosome.

--- src/hg/das/das.c
+++ src/hg/das/das.c
@@ -1,4 +1,4 @@
-/* das - Das Server. */
+/* das - Distributed Annotation System server. */
 #include "common.h"
 #include "linefile.h"
 #include "hash.h"
@@ -8,6 +8,7 @@
 #include "jksql.h"
 #include "hdb.h"
 #include "chromInfo.h"
+#include <regex.h>
 
 char *database = "hg7";
 char *version = "7.00";
@@ -41,7 +42,7 @@ if (err != 200)
     {
     exit(-1);
     }
-puts("<?xml version=\"1.0\" standalone=\"no\"?>\n");
+puts("<?xml version=\"1.0\" standalone=\"no\"?>");
 }
 
 void normalHeader()
@@ -71,9 +72,10 @@ struct segment
     char *seq;		/* Name of sequence, often a chromosome. */
     int start;		/* Zero based start. */
     int end;		/* Base after end. */
+    boolean wholeThing;	/* TRUE if user requested whole sequence. */
     };
 
-struct segment *segmentNew(char *seq, int start, int end)
+struct segment *segmentNew(char *seq, int start, int end, boolean wholeThing)
 /* Make a new segment. */
 {
 struct segment *segment;
@@ -81,10 +83,11 @@ AllocVar(segment);
 segment->seq = cloneString(seq);
 segment->start = start;
 segment->end = end;
+segment->wholeThing = wholeThing;
 return segment;
 }
 
-struct segment *dasSegmentList()
+struct segment *dasSegmentList(boolean mustExist)
 /* Get a DAS segment list from either segment or ref parameters. 
  * Call this before you write out header so it can return errors
  * properly if parameters are malformed. */
@@ -93,21 +96,25 @@ struct slName *segList, *seg;
 struct segment *segmentList = NULL, *segment;
 char *seq;
 int start=0, end=0;
+boolean wholeThing;
 
 segList = cgiStringList("segment");
 if (segList != NULL)
     {
+    /* Handle new format (post 0.995 spec) lists. */
     for (seg = segList; seg != NULL; seg = seg->next)
 	{
 	char *parts[3];
 	int partCount;
 
+	wholeThing = FALSE;
 	partCount = chopString(seg->name, ":,", parts, ArraySize(parts));
 	seq = parts[0];
 	if (partCount == 1)
 	    {
 	    start = 0;
 	    end = hChromSize(seq);
+	    wholeThing = TRUE;
 	    }
 	else if (partCount == 3)
 	    {
@@ -122,27 +129,48 @@ if (segList != NULL)
 	    {
 	    earlyError(402);
 	    }
-	segment = segmentNew(seq, start, end);
+	segment = segmentNew(seq, start, end, wholeThing);
 	slAddHead(&segmentList, segment);
 	}
     slReverse(&segmentList);
     }
 else
     {
+    /* Handle old format (pre 0.995 spec) lists. */
     seq = cgiOptionalString("ref");
+    wholeThing = TRUE;
     if (seq == NULL)
-	earlyError(402);
+	{
+	if (mustExist)
+	    earlyError(402);
+	else
+	    {
+	    return NULL;
+	    }
+	}
     if (cgiVarExists("start"))
+	{
         start = cgiInt("start")-1;
+	wholeThing = FALSE;
+	}
     else
         start = 0;
     if (cgiVarExists("stop"))
+	{
         end = cgiInt("stop");
+	wholeThing = FALSE;
+	}
     else
         end = hChromSize(seq);
     if (start > end)
 	earlyError(405);
-    segmentList = segmentNew(seq, start, end);
+    segmentList = segmentNew(seq, start, end, wholeThing);
+    }
+/* Check all segments are chromosomes. */
+for (segment = segmentList; segment != NULL; segment = segment->next)
+    {
+    if (!startsWith("chr", segment->seq))
+        earlyError(403);
     }
 return segmentList;
 }
@@ -152,7 +180,7 @@ void doDsn()
 {
 normalHeader();
 printf(
-" <!DOCTYPE DASDSN SYSTEM \"dasdsn.dtd\">\n"
+" <!DOCTYPE DASDSN SYSTEM \"http://www.biodas.org/dtd/dasdsn.dtd\">\n"
 " <DASDSN>\n");
 printf(
 "   <DSN>\n"
@@ -160,17 +188,6 @@ printf(
 "     <MAPMASTER>http://genome-test.cse.ucsc.edu:80/cgi-bin/das/%s</MAPMASTER>\n"
 "     <DESCRIPTION>Reference April 2001 Human Genome at UCSC</DESCRIPTION>\n"
 "   </DSN>\n", database, version, database);
-printf(
-"   <DSN>\n"
-"     <SOURCE id=\"intronEst\" version=\"%s\">UCSC Spliced ESTs</SOURCE>\n"
-"     <MAPMASTER>http://genome-test.cse.ucsc.edu:80/cgi-bin/das/%s</MAPMASTER>\n"
-"     <DESCRIPTION>UCSC Spliced ESTs on April 2001</DESCRIPTION>\n"
-"   </DSN>\n", version, database);
-printf("   <DSN>\n"
-"     <SOURCE id=\"intronEst\" version=\"%s\">UCSC Spliced ESTs</SOURCE>\n"
-"     <MAPMASTER>http://servlet.sanger.ac.uk:8080/das/ensembl110</MAPMASTER>\n"
-"     <DESCRIPTION>UCSC Spliced ESTs on ensembl110</DESCRIPTION>\n"
-"   </DSN>\n", version);
 printf(" </DASDSN>\n");
 }
 
@@ -210,33 +227,35 @@ for (i=0; i<psl->blockCount; ++i)
 void doFeatures()
 /* features - DAS Annotation Feature Server. */
 {
-struct segment *segmentList = dasSegmentList(), *segment;
+struct segment *segmentList = dasSegmentList(TRUE), *segment;
+struct slName *typeList = cgiStringList("type"), *typeEl;
 char *type = cgiOptionalString("type");
 char *category = cgiOptionalString("category");
 char *parts[3];
 struct sqlConnection *conn;
 struct sqlResult *sr;
 char **row;
-struct dyString *table = newDyString(0);
-struct dyString *query = newDyString(0);
 int start, end;
 char *seq;
 
 /* Write out DAS features header. */
 normalHeader();
 printf(
-"<!DOCTYPE DASGFF SYSTEM \"dasgff.dtd\">\n"
+"<!DOCTYPE DASGFF SYSTEM \"http://www.biodas.org/dtd/dasgff.dtd\">\n"
 "<DASGFF>\n"
 "<GFF version=\"1.0\" href=\"%s\">\n", currentUrl());
 
 for (segment = segmentList; segment != NULL; segment = segment->next)
     {
+    struct dyString *table = newDyString(0);
+    struct dyString *query = newDyString(0);
+
     /* Print segment header. */
     seq = segment->seq;
     start = segment->start;
     end = segment->end;
     printf(
-    "<SEGMENT id=\"splicedEsts\" start=\"%d\" stop=\"%d\" version=\"%s\" label=\"%s\">\n",
+    "<SEGMENT id=\"seq\" start=\"%d\" stop=\"%d\" version=\"%s\" label=\"%s\">\n",
 	   start+1, end, version, seq);
 
     /* Query database and output features. */
@@ -252,6 +271,9 @@ for (segment = segmentList; segment != NULL; segment = segment->next)
 	pslFree(&psl);
 	}
     printf("</SEGMENT>\n");
+    sqlFreeResult(&sr);
+    freeDyString(&table);
+    freeDyString(&query);
     }
 
 /* Write out DAS footer. */
@@ -268,7 +290,7 @@ struct chromInfo *ci;
 
 normalHeader();
 conn = hAllocConn();
-printf("<!DOCTYPE DASEP SYSTEM \"dasep.dtd\">\n");
+printf("<!DOCTYPE DASEP SYSTEM \"http://www.biodas.org/dtd/dasep.dtd\">\n");
 printf("<DASEP>\n");
 printf("<ENTRY_POINTS href=\"%s\" version=\"7.00\">\n",
 	currentUrl());
@@ -287,14 +309,14 @@ printf("</DASEP>\n");
 void doDna()
 /* Handle request for DNA. */
 {
-struct segment *segmentList = dasSegmentList(), *segment;
+struct segment *segmentList = dasSegmentList(TRUE), *segment;
 int size = 0;
 struct dnaSeq *seq;
 int i, oneSize, lineSize = 50;
 
 /* Write header. */
 normalHeader();
-printf("<!DOCTYPE DASDNA SYSTEM \"dasdna.dtd\">\n");
+printf("<!DOCTYPE DASDNA SYSTEM \"http://www.biodas.org/dtd/dasdna.dtd\">\n");
 printf("<DASDNA>\n");
 
 /* Write each sequence. */
@@ -324,14 +346,44 @@ for (segment = segmentList; segment != NULL; segment = segment->next)
 printf("</DASDNA>\n");
 }
 
+int countFeatures(char *table, struct segment *segmentList)
+/* Count all the features in a given segment. */
+{
+struct segment *segment;
+int acc = 0;
+struct sqlConnection *conn = hAllocConn();
+char chrTable[256];
+char query[512];
+for (segment = segmentList; segment != NULL; segment = segment->next)
+    {
+    if (segment->wholeThing)
+        {
+	sprintf(chrTable, "%s_%s", segment->seq, table);
+	if (sqlTableExists(conn, chrTable))
+	    {
+	    sprintf(query, "select count(*) from %s", chrTable);
+	    acc += sqlQuickNum(conn, query);
+	    }
+	}
+    }
+hFreeConn(&conn);
+return acc;
+}
+
 void doTypes()
 /* Handle a types request. */
 {
+struct segment *segmentList = dasSegmentList(FALSE);
+int count = countFeatures("intronEst", segmentList);
 normalHeader();
+printf("<!DOCTYPE DASTYPES SYSTEM \"http://www.biodas.org/dtd/dastypes.dtd\">\n");
 printf("<DASTYPES>\n");
 printf("<GFF version=\"1.2\" summary=\"yes\" href=\"%s\">\n", currentUrl());
 printf("<SEGMENT version=\"%s\">\n", version);
-printf(" <TYPE id=\"intronEst\" category=\"transcription\" method=\"BLAT\" />\n");
+printf("<TYPE id=\"intronEst\" category=\"transcription\" method=\"BLAT\" >");
+if (count > 0)
+    printf("%d", count);
+printf("</TYPE>\n");
 printf("</SEGMENT>\n");
 printf("</GFF>\n");
 printf("</DASTYPES>\n");
@@ -340,6 +392,8 @@ printf("</DASTYPES>\n");
 void dispatch(char *dataSource, char *command)
 /* Dispatch a dase command. */
 {
+if (sameString(dataSource, "ensembl110"))
+    dataSource = "hg7";
 if (sameString(dataSource, "dsn"))
     doDsn();
 else if (sameString(database, dataSource))
