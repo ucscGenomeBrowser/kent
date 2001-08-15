7c696518c8deef76f555c8712698bbf339092637
kent <kent>
2001-08-15 18:14:15 +0000

    Tinkering on pet freen as usual.  Made it reformat browserTable into trackTable.

--- src/hg/oneShot/freen/freen.c
+++ src/hg/oneShot/freen/freen.c
@@ -1,6 +1,7 @@
 /* freen - My Pet Freen. */
 #include "common.h"
 #include "jksql.h"
+#include "browserTable.h"
 
 
 void usage()
@@ -12,56 +13,37 @@ errAbort(
   "   freen hgN\n");
 }
 
-char *contamClones[] =
-   {
-   "AC007777",
-   "AC008027",
-   "AC008248",
-   "AC008972",
-   "AC009624",
-   "AC011090",
-   "AC011335",
-   "AC016087",
-   "AC019017",
-   "AC019351",
-   "AC021555",
-   "AC021569",
-   "AC021574",
-   "AC023079",
-   "AC023934",
-   "AC024373",
-   "AC024562",
-   "AC025002",
-   "AC025003",
-   "AC025326",
-   "AC025598",
-   "AC026940",
-   "AC027158",
-   "AC035148",
-   "AC064799",
-   "AC069076",
-   "AC069192",
-   };
-
-
-void contam(char *database, char **badClones, int badCloneCount)
-/* Look at clones and see if in database. */
+void scanBrowserTable(char *db)
+/* Scan browser table and print selected fields. */
 {
-struct sqlConnection *conn = sqlConnect(database);
-int i;
-char buf[256];
-char query[256];
-char *res;
+struct sqlConnection *conn = sqlConnect(db);
+struct sqlResult *sr = sqlGetResult(conn, "select * from browserTable");
+char **row;
+struct browserTable *bt;
 
-for (i=0; i<badCloneCount; ++i)
+while ((row = sqlNextRow(sr)) != NULL)
     {
-    char *clone = badClones[i];
-    sprintf(query, "select name from clonePos where name like '%s%%'",
-    	clone);
-    res = sqlQuickQuery(conn, query, buf, sizeof(buf));
-    if (res != NULL)
-        printf("%s\n", res);
+    bt = browserTableLoad(row);
+    if (bt->shortLabel != NULL && bt->shortLabel[0] != 0 && bt->isPlaced)
+        {
+	printf("{\n");
+	printf(" NULL,\n");
+	printf(" \"%s\",	/* mapName */\n", bt->mapName);
+	printf(" \"%s\",	/* tableName */\n", bt->tableName);
+	printf(" \"%s\",	/* shortLabel */\n", bt->shortLabel);
+	printf(" \"%s\",	/* longLabel */\n", bt->longLabel);
+	printf(" %d,	/* visibility */\n", bt->visibility);
+	printf(" %d,%d,%d,	/* color */\n", bt->colorR, bt->colorG, bt->colorB);
+	printf(" %d,%d,%d,	/* altColor */\n", bt->altColorR, bt->altColorG, bt->altColorB);
+	printf(" %d,	/* useScore */\n", bt->useScore);
+	printf(" %d,	/* isSplit */\n", bt->isSplit);
+	printf(" %d,	/* private */\n", bt->private);
+	printf(" TRUE,	/* hardCoded */\n");
+	printf("};\n");
+	}
+    browserTableFree(&bt);
     }
+sqlFreeResult(&sr);
 sqlDisconnect(&conn);
 }
 
@@ -70,6 +52,6 @@ int main(int argc, char *argv[])
 {
 if (argc != 2 )
     usage();
-contam(argv[1], contamClones, ArraySize(contamClones));
+scanBrowserTable(argv[1]);
 return 0;
 }
