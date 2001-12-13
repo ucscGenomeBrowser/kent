c265c9097082a6a1b90c647363aa571311a50643
kent <kent>
2001-12-13 23:37:52 +0000

    Minor fix.

--- src/blat/blat.c
+++ src/blat/blat.c
@@ -15,7 +15,7 @@
 #include "trans3.h"
 #include "repMask.h"
 
-int version = 7;	/* Blat version number. */
+int version = 8;	/* Blat version number. */
 
 enum constants {
 	qWarnSize = 5000000,	/* Warn if more than this many bases in one query. */
