ef9a63ef45781db65c3dda5daf320a4e3a96502b
kent <kent>
2002-01-30 17:45:46 +0000

    Added keepName option.

--- src/utils/faRc/faRc.c
+++ src/utils/faRc/faRc.c
@@ -13,6 +13,8 @@ errAbort(
   "usage:\n"
   "   faRc in.fa out.fa\n"
   "In.fa and out.fa may be the same file.\n"
+  "options:\n"
+  "   -keepName - keep name identical (don't prepend RC)\n"
   );
 }
 
@@ -22,13 +24,14 @@ void faRc(char *in, char *out)
 struct dnaSeq *seqList = NULL, *seq;
 FILE *f;
 char buf[512];
+char *prefix = (cgiBoolean("keepName") ? "" : "RC_");
 
 seqList = faReadAllDna(in);
 f = mustOpen(out, "w");
 for (seq = seqList; seq != NULL; seq = seq->next)
     {
     reverseComplement(seq->dna, seq->size);
-    sprintf(buf, "RC_%s", seq->name);
+    sprintf(buf, "%s%s", prefix, seq->name);
     faWriteNext(f, buf,  seq->dna, seq->size);
     }
 }
