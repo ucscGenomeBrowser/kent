bf9dd48af1ad9ee547c567709834d904bd1447f4
kent <kent>
2002-03-10 17:56:33 +0000

    Seems to work as planned.

--- src/parasol/paraTestJob/paraTestJob.c
+++ src/parasol/paraTestJob/paraTestJob.c
@@ -10,15 +10,56 @@ void usage()
 errAbort(
   "paraTestJob - A good test job to run on Parasol.  Can be configured to take a long time or crash\n"
   "usage:\n"
-  "   paraTestJob XXX\n"
+  "   paraTestJob count\n"
+  "Run a relatively time consuming algorithm count times.  This\n"
+  "algorithm takes about 1/10 per second each time\n"
   "options:\n"
-  "   -xxx=XXX\n"
+  "   -crash - Try to write to NULL when done.\n"
+  "   -err   - Return -1 error code when done\n"
+  "   -sleep=n Sleep for N seconds\n"
   );
 }
 
-void paraTestJob(char *XXX)
+double cumErr = 0.0;
+
+void noteResults(double a, double b)
+/* Don't really do anything. */
+{
+double diff = a - b;
+if (diff < 0) diff = -diff;
+cumErr += diff;
+}
+
+void compute()
+/* Do a relatively time consuming calculation.
+ * This should last about a second. */
+{
+int i;
+double res;
+
+for (i=0; i<1060000; ++i)
+    {
+    res = sqrt(i);
+    noteResults(i, res*res);
+    }
+}
+
+void paraTestJob(char *countString)
 /* paraTestJob - A good test job to run on Parasol.  Can be configured to take a long time or crash. */
 {
+int i, count = atoi(countString);
+for (i=0; i<count; ++i)
+    compute();
+printf("Cumulative error %f\n", cumErr);
+if (optionExists("sleep"))
+    sleep(optionInt("sleep", 1));
+if (optionExists("err"))
+    errAbort("Aborting with error");
+if (optionExists("crash"))
+    {
+    char *s = NULL;
+    s[0] = 0;
+    }
 }
 
 int main(int argc, char *argv[])
