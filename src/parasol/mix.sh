64d9c67e558dce563b7e1f2c4913eaa879ddd338
kent <kent>
2002-03-11 20:31:46 +0000

    Cleaning up after mistaken import.  Adding README.

--- src/parasol/mix.sh
+++
@@ -1,8 +0,0 @@
-if (! -e mix) then
-    mkdir mix
-endif
-foreach k (0 1 2 3 4 5 6 7 8 9)
-    parasol -out=mix/echo$k add job /bin/echo $k
-    parasol -out=mix/$k add job faSize /scratch/hg/mrna.127/mrna.fa
-    parasol add job sleep 10
-end
