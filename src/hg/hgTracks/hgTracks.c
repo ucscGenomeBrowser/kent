91f1c516825eb5d50439d06ec508e353c11ce33c
sugnet <sugnet>
2002-04-03 20:24:30 +0000

    Added methods for displaying altGraphX track.

--- src/hg/hgTracks/hgTracks.c
+++ src/hg/hgTracks/hgTracks.c
@@ -56,6 +56,7 @@
 #include "geneGraph.h"
 #include "sample.h"
 #include "uPennClones.h"
+#include "altGraphX.h"
 
 #define ROGIC_CODE 1	/* Please take these out.  It's *everyone's* code now. -jk */
 #define FUREY_CODE 1
@@ -754,6 +755,20 @@ void makeSeaShades(struct memGfx *mg)
 {
 mgMakeColorGradient(mg, &lightSeaColor, &darkSeaColor, maxShade+1, shadesOfSea);
 }
+
+void makeRedGreenShades(struct memGfx *mg) 
+/* Allocate the  shades of Red, Green and Blue */
+{
+static struct rgbColor black = {0, 0, 0};
+static struct rgbColor red = {255, 0, 0};
+static struct rgbColor green = {0, 255, 0};
+static struct rgbColor blue = {0, 0, 255};
+mgMakeColorGradient(mg, &black, &blue, EXPR_DATA_SHADES, shadesOfBlue);
+mgMakeColorGradient(mg, &black, &red, EXPR_DATA_SHADES, shadesOfRed);
+mgMakeColorGradient(mg, &black, &green, EXPR_DATA_SHADES, shadesOfGreen);
+exprBedColorsMade = TRUE;
+}
+
 
 int grayInRange(int val, int minVal, int maxVal)
 /* Return gray shade corresponding to a number from minVal - maxVal */
@@ -5283,6 +5298,250 @@ tg->mapsSelf = TRUE;
 tg->mapItem = altGraphMapItem;
 }
 
+void altGraphXMapItem(struct trackGroup *tg, void *item, char *itemName, int start, int end, 
+		    int x, int y, int width, int height)
+/* create a link for each altGraphX that centers it on browser with 
+   known genes, human mrnas, and intron est tracks open */
+{
+struct altGraphX *ag = item;
+char buff[32];
+snprintf(buff, sizeof(buff), "%d", ag->id);
+mapBoxHc(start, end, x, y, width, height, tg->mapName, buff, "altGraphX Details");
+}
+
+boolean altGraphXEdgeSeen(struct altGraphX *ag, int *seen, int *seenCount, int mrnaIx)
+/* is the mrnaIx already in seen? */
+{
+int i=0;
+boolean result = FALSE;
+for(i=0; i<*seenCount; i++)
+    {
+    if(ag->mrnaTissues[seen[i]] == ag->mrnaTissues[mrnaIx] ||
+       ag->mrnaLibs[seen[i]] == ag->mrnaLibs[mrnaIx])
+	{
+	result = TRUE;
+	break;
+	}
+    }
+if(!result)
+    {
+    seen[*seenCount++] = mrnaIx;
+    }
+return result;
+}
+
+int altGraphConfidenceForEdge(struct altGraphX *ag, int eIx)
+/* count how many unique libraries or tissues contain a given edge */
+{
+struct evidence *ev = slElementFromIx(ag->evidence, eIx);
+int *seen = NULL;
+int seenCount = 0,i;
+int conf = 0;
+AllocArray(seen, ag->edgeCount);
+for(i=0; i<ag->edgeCount; i++)
+    seen[i] = -1;
+for(i=0; i<ev->evCount; i++)
+    if(!altGraphXEdgeSeen(ag, seen, &seenCount, ev->mrnaIds[i]))
+	conf++;
+freez(&seen);
+return conf;
+}
+
+boolean altGraphXInEdges(struct ggEdge *edges, int v1, int v2)
+/* Return TRUE if a v1-v2 edge is in the list FALSE otherwise. */
+{
+struct ggEdge *e = NULL;
+for(e = edges; e != NULL; e = e->next)
+    {
+    if(e->vertex1 == v1 && e->vertex2 == v2)
+	return TRUE;
+    }
+return FALSE;
+}
+
+Color altGraphXColorForEdge(struct memGfx *mg, struct altGraphX *ag, int eIx)
+/* Return the color of an edge given by confidence */
+{
+int confidence = altGraphConfidenceForEdge(ag, eIx);
+Color c = shadesOfGray[maxShade/4];
+struct geneGraph *gg = NULL;
+struct ggEdge *edges = NULL;
+
+if(ag->edgeTypes[eIx] == ggCassette)
+    {
+    if(!exprBedColorsMade)
+	makeRedGreenShades(mg);
+    if(confidence == 1) c = shadesOfRed[(maxRGBShade - 6 > 0) ? maxRGBShade - 6 : 0];
+    else if(confidence == 2) c = shadesOfRed[(maxRGBShade - 4 > 0) ? maxRGBShade - 4: 0];
+    else if(confidence >= 3) c = shadesOfRed[(maxRGBShade - 4 > 0) ? maxRGBShade - 1: 0];
+    }
+else
+    {
+    if(confidence == 1) c = shadesOfGray[maxShade/3];
+    else if(confidence == 2) c = shadesOfGray[2*maxShade/3];
+    else if(confidence >= 3) c = shadesOfGray[maxShade];
+    }
+return c;
+}
+
+static void altGraphXDraw(struct trackGroup *tg, int seqStart, int seqEnd,         
+			 struct memGfx *mg, int xOff, int yOff, int width, 
+			 MgFont *font, Color color, enum trackVisibility vis)
+/* Draws the blocks for an alt-spliced gene and the connections */
+{
+int baseWidth = seqEnd - seqStart;
+int y = yOff;
+int heightPer = tg->heightPer;
+int lineHeight = tg->lineHeight;
+int x1,x2;
+boolean isFull = (vis == tvFull);
+double scale = width/(double)baseWidth;
+int i;
+double y1, y2;
+int midLineOff = heightPer/2;
+struct altGraphX *ag=NULL, *agList = NULL;
+int s =0, e=0;
+agList = tg->items;
+for(ag = agList; ag != NULL; ag = ag->next)
+    {	   
+    x1 = round((double)((int)ag->tStart-winStart)*scale) + xOff;
+    x2 = round((double)((int)ag->tEnd-winStart)*scale) + xOff;
+    if(tg->mapsSelf && tg->mapItem)
+	{
+	tg->mapItem(tg, ag, "notUsed", ag->tStart, ag->tEnd, xOff, y, width, heightPer);
+	}
+    if(!isFull && (x2-x1 > 0))
+	{
+	mgDrawBox(mg, x1, yOff+tg->heightPer/2, x2-x1, 1, MG_BLACK);
+	}
+    for(i= ag->edgeCount -1; i >= 0; i--)   // for(i=0; i< ag->edgeCount; i++)
+	{
+	char buff[16];
+	int textWidth;
+	int sx1 = 0;
+	int sx2 = 0;
+	int sw = 0;
+	s = ag->vPositions[ag->edgeStarts[i]];
+	e = ag->vPositions[ag->edgeEnds[i]];
+	sx1 = roundingScale(s-winStart, width, baseWidth)+xOff;
+	sx2 = roundingScale(e-winStart, width, baseWidth)+xOff;
+	sw = sx2 - sx1;
+	snprintf(buff, sizeof(buff), "%d-%d", ag->edgeStarts[i], ag->edgeEnds[i]);        /* draw exons as boxes */
+	if( (ag->vTypes[ag->edgeStarts[i]] == ggHardStart || ag->vTypes[ag->edgeStarts[i]] == ggSoftStart)  
+	    && (ag->vTypes[ag->edgeEnds[i]] == ggHardEnd || ag->vTypes[ag->edgeEnds[i]] == ggSoftEnd)) 
+	    {
+	    Color color2 = altGraphXColorForEdge(mg, ag, i);
+	    if(isFull)
+		{
+		drawScaledBox(mg, s, e, scale, xOff, y+(heightPer/2), heightPer/2, color2);
+		textWidth = mgFontStringWidth(font, buff);
+		if (textWidth <= sw + 2 && hgDebug )
+		    mgTextCentered(mg, sx2-textWidth-2, y+(heightPer/2), textWidth+2, heightPer/2, MG_WHITE, font, buff);
+		}
+	    else
+		drawScaledBox(mg, s, e, scale, xOff, y, heightPer, color2);
+
+	    }
+	if(isFull)
+	    {
+	    /* draw introns as arcs */
+
+	    if( (ag->vTypes[ag->edgeStarts[i]] == ggHardEnd || ag->vTypes[ag->edgeStarts[i]] == ggSoftEnd) 
+		&& (ag->vTypes[ag->edgeEnds[i]] == ggHardStart || ag->vTypes[ag->edgeEnds[i]] == ggSoftStart))
+		{
+		Color color2 = altGraphXColorForEdge(mg, ag, i);
+		int x1, x2;
+		int midX;   
+		int midY = y + heightPer/2;
+		s = ag->vPositions[ag->edgeStarts[i]];
+		e = ag->vPositions[ag->edgeEnds[i]];
+		x1 = round((double)((int) s - winStart)*scale) + xOff;
+		x2 = round((double)((int) e - winStart)*scale) + xOff;
+		midX = (x1+x2)/2;
+		mgDrawLine(mg, x1, midY, midX, y, color2);
+		mgDrawLine(mg, midX, y, x2, midY, color2);
+		textWidth = mgFontStringWidth(font, buff);
+		if (textWidth <= sw && hgDebug )
+		    mgTextCentered(mg, sx1, y+(heightPer/2), sw, heightPer/2, MG_BLACK, font, buff);
+		}
+	    }
+	}
+    if(isFull)
+	y += lineHeight;
+    }
+}
+
+
+void altGraphXLoadItems(struct trackGroup *tg)
+/* load the altGraphX data to a trackGroup */
+{
+struct sqlConnection *conn = hAllocConn();
+int rowOffSet;
+char **row;
+struct altGraphX *ag=NULL, *agList=NULL;
+struct sqlResult *sr = hRangeQuery(conn, tg->mapName, chromName,
+				   winStart, winEnd, NULL, &rowOffSet);
+while((row = sqlNextRow(sr)) != NULL)
+    {
+    ag = altGraphXLoad(row + rowOffSet);
+    slAddHead(&agList, ag);
+    }
+slReverse(&agList);
+sqlFreeResult(&sr);
+tg->items = agList;
+}
+
+void altGraphXFreeItems(struct trackGroup *tg)
+/* free up tha altGraphX items in tg->items */
+{
+altGraphXFreeList((struct altGraphX**)(&tg->items));
+}
+
+static int altGraphXFixedTotalHeight(struct trackGroup *tg, enum trackVisibility vis)
+/* set track height to 2 * font size if full , 1 * if dense
+*/
+{
+switch (vis)
+    {
+    case tvFull:
+	tg->lineHeight  = 2 * mgFontLineHeight(tl.font)+1;
+	tg->heightPer = tg->lineHeight -1;
+	tg->height = slCount(tg->items) * tg->lineHeight;
+	break;
+    case tvDense:
+	tg->lineHeight  = mgFontLineHeight(tl.font)+1;
+	tg->heightPer = tg->lineHeight -1;
+	tg->height = tg->lineHeight;
+	break;
+    }
+return tg->height;
+}
+
+char *altGraphXItemName(struct trackGroup *tg, void *item)
+/* returns the number of alternative splice paths as a string name */
+{
+char buff[32];
+struct altGraphX *ag = item;
+int count =0, i=0;
+for(i=0; i<ag->edgeCount; i++)
+    if(ag->edgeTypes[i] == ggCassette)
+	count++;
+snprintf(buff, sizeof(buff), "%d", count );
+return (cloneString(buff));
+}
+
+void altGraphXMethods(struct trackGroup *tg)
+/* setup special methods for altGraphX track */
+{
+tg->drawItems = altGraphXDraw;
+tg->loadItems = altGraphXLoadItems;
+tg->freeItems = altGraphXFreeItems;
+tg->totalHeight = altGraphXFixedTotalHeight;
+tg->itemName = altGraphXItemName;
+tg->mapsSelf = TRUE;
+tg->mapItem = altGraphXMapItem;
+}
+
 
 void mapBoxHcTwoItems(int start, int end, int x, int y, int width, int height, 
 	char *group, char *item1, char *item2, char *statusLine)
@@ -5800,18 +6059,6 @@ else
 return lfsList;
 }
 
-void makeRedGreenShades(struct memGfx *mg) 
-/* Allocate the  shades of Red, Green and Blue */
-{
-static struct rgbColor black = {0, 0, 0};
-static struct rgbColor red = {255, 0, 0};
-static struct rgbColor green = {0, 255, 0};
-static struct rgbColor blue = {0, 0, 255};
-mgMakeColorGradient(mg, &black, &blue, EXPR_DATA_SHADES, shadesOfBlue);
-mgMakeColorGradient(mg, &black, &red, EXPR_DATA_SHADES, shadesOfRed);
-mgMakeColorGradient(mg, &black, &green, EXPR_DATA_SHADES, shadesOfGreen);
-exprBedColorsMade = TRUE;
-}
 
 Color cghNci60Color(struct trackGroup *tg, void *item, struct memGfx *mg ) 
 {
@@ -7340,6 +7587,7 @@ registerTrackHandler("affy", affyMethods);
 registerTrackHandler("affyRatio", affyRatioMethods);
 registerTrackHandler("ancientR", ancientRMethods );
 registerTrackHandler("altGraph", altGraphMethods );
+registerTrackHandler("altGraphX", altGraphXMethods );
 /* MGC related */
 registerTrackHandler("mgcNcbiPicks", estMethods);
 registerTrackHandler("mgcNcbiSplicedPicks", intronEstMethods);
