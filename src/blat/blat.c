a23f668af439391d0d27a3904bd731dc345b45da
kent <kent>
2002-04-05 17:13:14 +0000

    Bumped version number with -q=prot -t=dnax mem leak fix.

--- src/blat/blat.c
+++ src/blat/blat.c
@@ -16,7 +16,7 @@
 #include "trans3.h"
 #include "repMask.h"
 
-int version = 8;	/* Blat version number. */
+int version = 9;	/* Blat version number. */
 
 enum constants {
 	qWarnSize = 5000000,	/* Warn if more than this many bases in one query. */
