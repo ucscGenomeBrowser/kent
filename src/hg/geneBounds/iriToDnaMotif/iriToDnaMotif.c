b397c4b7beba0c346c469019ede833072adcfc63
kent <kent>
2002-04-09 18:29:51 +0000

    Seems to work.

--- src/hg/geneBounds/iriToDnaMotif/iriToDnaMotif.c
+++ src/hg/geneBounds/iriToDnaMotif/iriToDnaMotif.c
@@ -3,6 +3,9 @@
 #include "linefile.h"
 #include "hash.h"
 #include "options.h"
+#include "jksql.h"
+#include "improbRunInfo.h"
+#include "dnaMotif.h"
 
 void usage()
 /* Explain usage and exit. */
@@ -10,23 +13,38 @@ void usage()
 errAbort(
   "iriToDnaMotif - Convert improbRunInfo to dnaMotif\n"
   "usage:\n"
-  "   iriToDnaMotif XXX\n"
+  "   iriToDnaMotif improbRunInfo.txt dnaMotif.txt\n"
   "options:\n"
   "   -xxx=XXX\n"
   );
 }
 
-void iriToDnaMotif(char *XXX)
+void iriToDnaMotif(char *inName, char *outName)
 /* iriToDnaMotif - Convert improbRunInfo to dnaMotif. */
 {
+FILE *f = mustOpen(outName, "w");
+static struct dnaMotif motif;
+struct improbRunInfo *iriList = improbRunInfoLoadAll(inName);
+struct improbRunInfo *iri;
+
+for (iri = iriList; iri != NULL; iri = iri->next)
+    {
+    motif.name = iri->name;
+    motif.columnCount = iri->columnCount;
+    motif.aProb = iri->aProb;
+    motif.cProb = iri->cProb;
+    motif.gProb = iri->gProb;
+    motif.tProb = iri->tProb;
+    dnaMotifTabOut(&motif, f);
+    }
 }
 
 int main(int argc, char *argv[])
 /* Process command line. */
 {
 optionHash(&argc, argv);
-if (argc != 2)
+if (argc != 3)
     usage();
-iriToDnaMotif(argv[1]);
+iriToDnaMotif(argv[1], argv[2]);
 return 0;
 }
