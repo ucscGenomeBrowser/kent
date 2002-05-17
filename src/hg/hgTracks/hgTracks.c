4826699b6103d4645938301ff716a1ae16ac3ba9
matt <matt>
2002-05-17 23:10:38 +0000

    Bumped up the version number to v_13

--- src/hg/hgTracks/hgTracks.c
+++ src/hg/hgTracks/hgTracks.c
@@ -8960,6 +8960,6 @@ htmlSetBackground("../images/floret.jpg");
 if (cgiVarExists("hgt.reset"))
     resetVars();
 
-cartHtmlShell("UCSC Genome Browser v11", doMiddle, hUserCookie(), excludeVars, NULL);
+cartHtmlShell("UCSC Genome Browser v13", doMiddle, hUserCookie(), excludeVars, NULL);
 return 0;
 }
