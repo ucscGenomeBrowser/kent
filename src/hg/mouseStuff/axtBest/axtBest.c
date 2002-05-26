b0c96510a72b988cc259ba48dbf0472b9c838187
kent <kent>
2002-05-26 20:11:27 +0000

    Seems to work again.

--- src/hg/mouseStuff/axtBest/axtBest.c
+++ src/hg/mouseStuff/axtBest/axtBest.c
@@ -1,6 +1,7 @@
 /* axtBest - Remove second best alignments. */
 #include "common.h"
 #include "linefile.h"
+#include "memalloc.h"
 #include "hash.h"
 #include "options.h"
 #include "dnautil.h"
@@ -87,6 +88,37 @@ for (i=0; i<symCount; ++i)
 return score;
 }
 
+int countNonDash(char *a, int size)
+/* Count number of non-dash characters. */
+{
+int count = 0;
+int i;
+for (i=0; i<size; ++i)
+    if (a[i] != '-') 
+        ++count;
+return count;
+}
+
+boolean axtCheck(struct axt *axt, struct lineFile *lf)
+/* Return FALSE if there's a problem with axt. */
+{
+int tSize = countNonDash(axt->tSym, axt->symCount);
+int qSize = countNonDash(axt->qSym, axt->symCount);
+if (tSize != axt->tEnd - axt->tStart)
+    {
+    warn("%d non-dashes, but %d bases to cover at line %d of %s", 
+    	tSize, axt->tEnd - axt->tStart, lf->lineIx, lf->fileName);
+    return FALSE;
+    }
+if (qSize != axt->qEnd - axt->qStart)
+    {
+    warn("%d non-dashes, but %d bases to cover at line %d of %s", 
+    	tSize, axt->qEnd - axt->qStart, lf->lineIx, lf->fileName);
+    return FALSE;
+    }
+return TRUE;
+}
+
 struct axt *readAllAxt(char *fileName, struct axtScoreScheme *ss, int threshold,
 	char *chromName)
 /* Read all axt's in a file. */
@@ -95,6 +127,11 @@ struct lineFile *lf = lineFileOpen(fileName, TRUE);
 struct axt *list = NULL, *axt;
 while ((axt = axtRead(lf)) != NULL)
     {
+    if (!axtCheck(axt, lf))
+	{
+	axtFree(&axt);
+	continue;
+	}
     if (sameString(chromName, axt->tName))
 	{
 	if (axt->tStrand != '+')
@@ -116,19 +153,47 @@ slReverse(&list);
 return list;
 }
 
-struct bestKeeper
+struct bestKeep
 /* Structure to help find best scoring alignment for a base. */
     {
-    int score;		/* Best score. */
-    struct axt *axt;	/* Best scoring axt. */
+    int tStart, tEnd;	/* Target range covered */
+    int tSize;		/* Size of range. */
+    int *scores;	/* Array of scores. */
+    struct axt **axts;   /* Array of axts. */
     };
 
-void markBest(struct axt *axt, struct bestKeeper *bk, 
-	int chromSize, struct axtScoreScheme *ss, int winSize, boolean soft)
+void bestKeepInitScores(struct bestKeep *bk)
+/* Initialize scores to all bad. */
+{
+int tSize = bk->tSize, *scores = bk->scores, i;
+for (i=0; i<tSize; ++i)
+    scores[i] = -1024*1024;
+}
+
+struct bestKeep *bestKeepNew(int tStart, int tEnd)
+/* Allocate new bestKeep structure */
+{
+struct bestKeep *bk;
+int size;
+AllocVar(bk);
+size = tEnd - tStart;
+assert(size >= 0);
+bk->tStart = tStart;
+bk->tEnd = tEnd;
+bk->tSize = size;
+size += 1;
+AllocArray(bk->scores, size);
+AllocArray(bk->axts, size);
+return bk;
+}
+
+void markBest(struct axt *axt, struct bestKeep *bk, 
+	int chromStart, int rangeSize, 
+	struct axtScoreScheme *ss, int winSize, boolean soft)
 /* Figure out alignment score within big window and mark best ones in bk. */
 {
-struct bestKeeper *b = bk + axt->tStart;
 int i, s, e, symCount = axt->symCount;
+int bIx = axt->tStart - bk->tStart;
 char *qSym = axt->qSym, *tSym = axt->tSym;
 char q,t;
 int score = 0;
@@ -171,6 +236,7 @@ for (i=0; i<symCount; ++i)
 assert(tScore == tProjScores + tSize);
 tScore = tProjScores;
 
+
 /* Figure out average score in window around each
  * base in target, and if it's better than the existing
  * best score in that region mark it. */
@@ -187,12 +253,12 @@ if (soft)
 	e = i + winHalf;
 	if (e >= tSize) e = tSize-1;
 	score = tScore[e] - tScore[s];
-	if (score > b->score)
-	   {
-	   b->score = score;
-	   b->axt = axt;
-	   }
-	b += 1;
+	if (score > bk->scores[bIx])
+	    {
+	    bk->scores[bIx] = score;
+	    bk->axts[bIx] = axt;
+	    }
+	++bIx;
 	}
     }
 else
@@ -204,12 +270,12 @@ else
 	score = tScore[tSize-1];
 	for (i=0; i<tSize; ++i)
 	    {
-	    if (score > b->score)
+	    if (score > bk->scores[bIx])
 	        {
-		b->score = score;
-		b->axt = axt;
+		bk->scores[bIx] = score;
+		bk->axts[bIx] = axt;
 		}
-	    b += 1;
+	    ++bIx;
 	    }
 	}
     else
@@ -218,36 +284,36 @@ else
 	score = tScore[halfWin-1];
 	for (i=0; i<halfWin; ++i)
 	    {
-	    if (score > b->score)
+	    if (score > bk->scores[bIx])
 	        {
-		b->score = score;
-		b->axt = axt;
+		bk->scores[bIx] = score;
+		bk->axts[bIx] = axt;
 		}
-	    b += 1;
+	    ++bIx;
 	    }
 	for (i=halfWin; i<lastHalfWin; ++i)
 	    {
 	    score = tScore[i+winHalf] - tScore[i-halfWin];
-	    if (score > b->score)
+	    if (score > bk->scores[bIx])
 	        {
-		b->score = score;
-		b->axt = axt;
+		bk->scores[bIx] = score;
+		bk->axts[bIx] = axt;
 		}
-	    b += 1;
+	    ++bIx;
 	    }
 	score = tScore[tSize-1] - tScore[lastHalfWin];
 	for (i=lastHalfWin; i<tSize; ++i)
 	    {
-	    if (score > b->score)
+	    if (score > bk->scores[bIx])
 	        {
-		b->score = score;
-		b->axt = axt;
+		bk->scores[bIx] = score;
+		bk->axts[bIx] = axt;
 		}
-	    b += 1;
+	    ++bIx;
 	    }
 	}
+    assert(bIx == axt->tEnd - bk->tStart);
     }
-assert(b == bk + axt->tEnd);
 freez(&tScore);
 }
 
@@ -264,17 +330,6 @@ if (skipTrailingDash)
     while (*a == '-')
        ++a;
 return a;
-}
-
-int countNonDash(char *a, int size)
-/* Count number of non-dash characters. */
-{
-int count = 0;
-int i;
-for (i=0; i<size; ++i)
-    if (a[i] != '-') 
-        ++count;
-return count;
 }
 
 void writeAxtTrackStats(struct axt *axt, FILE *f)
@@ -319,24 +374,25 @@ else
     }
 }
 
-void outputBestRanges(struct bestKeeper *bk, int chromSize, 
-	struct axtScoreScheme *ss, FILE *f)
+void outputBestRanges(struct bestKeep *bk, int chromStart, 
+	int rangeSize, struct axtScoreScheme *ss, FILE *f)
 /* Find runs inside of bk that refer to same alignment and
  * then output. */
 {
 int s = 0, i;
 struct axt *axt, *lastAxt = NULL;
+struct axt **bkAxt = bk->axts;
 
 /* This loop depends on having a sentinal NULL bk->axt at 
  * end of array. */
-for (i=0; i<=chromSize; ++i)
+for (i=0; i<=rangeSize; ++i)
     {
-    axt = bk[i].axt;
+    axt = bkAxt[i];
     if (axt != lastAxt)
         {
 	if (lastAxt != NULL)
 	    {
-	    axtSubsetOnT(lastAxt, s, i, ss, f);
+	    axtSubsetOnT(lastAxt, s+chromStart, i+chromStart, ss, f);
 	    }
 	s = i;
 	lastAxt = axt;
@@ -344,25 +400,20 @@ for (i=0; i<=chromSize; ++i)
     }
 }
 
-int maxChromSize(struct axt *axtList)
-/* Return largest tEnd. */
+void chromRange(struct axt *axtList, int *retStart, int *retEnd)
+/* Return range in chromosome covered. */
 {
-int maxSize = 0;
+int maxSize = 0, minSize = BIGNUM;
 struct axt *axt;
 for (axt = axtList; axt != NULL; axt = axt->next)
     {
     if (axt->tEnd > maxSize)
         maxSize = axt->tEnd;
+    if (axt->tStart < minSize)
+        minSize = axt->tStart;
     }
-return maxSize;
-}
-
-void clearBkScores(struct bestKeeper *bk, int size)
-/* Set scores in bk to large negative number. */
-{
-int i;
-for (i=0; i<size; ++i)
-    bk->score = -1024*1024;
+*retStart = minSize;
+*retEnd = maxSize;
 }
 
 void axtBest(char *inName, char *chromName, char *outName)
@@ -372,33 +423,35 @@ FILE *f = mustOpen(outName, "w");
 char *matrixName = optionVal("matrix", NULL);
 struct axtScoreScheme *ss = NULL;
 struct axt *axtList = NULL, *axt, *nextAxt, *goodList = NULL;
-struct bestKeeper *bk = NULL;
+struct bestKeep *bk = NULL;
 int i;
-int chromSize;
+int chromStart, chromEnd;
+int rangeSize;
 
 if (matrixName == NULL)
     ss = axtScoreSchemeDefault();
 else
     ss = axtScoreSchemeRead(matrixName);
 axtList = readAllAxt(inName, ss, minScore, chromName);
-chromSize = maxChromSize(axtList);
 printf("Read %d elements from %s\n", slCount(axtList), inName);
-AllocArray(bk, chromSize+1);	/* Include extra sentinal value. */
-printf("Allocated %d elements in array\n", chromSize);
+chromRange(axtList, &chromStart, &chromEnd);
+rangeSize = chromEnd - chromStart;
+bk = bestKeepNew(chromStart, chromEnd);
+printf("Allocated %d elements in array\n", rangeSize);
+
 
 /* Find local best in big soft window and get rid of
  * alignments that aren't best anywhere, using axt->score
  * field for scratch space. */
-clearBkScores(bk, chromSize);
 for (axt = axtList; axt != NULL; axt = axt->next)
     {
-    markBest(axt, bk, chromSize, ss, winSize, TRUE);
+    markBest(axt, bk, chromStart, rangeSize, ss, winSize, TRUE);
     }
 for (axt = axtList; axt != NULL; axt = axt->next)
     axt->score = 0;
-for (i=0; i<chromSize; ++i)
+for (i=0; i<bk->tSize; ++i)
     {
-    if ((axt = bk[i].axt) != NULL)
+    if ((axt = bk->axts[i]) != NULL)
         axt->score += 1;
     }
 for (axt = axtList; axt != NULL; axt = nextAxt)
@@ -416,11 +469,11 @@ printf("%d elements in after soft filter.\n", slCount(goodList));
 
 /* Find local best alignments in big hard window and
  * output them. */
-memset(bk, 0, chromSize * sizeof(*bk));
-clearBkScores(bk, chromSize);
+memset(bk->axts, 0, rangeSize * sizeof(bk->axts[0]));
+bestKeepInitScores(bk);
 for (axt = goodList; axt != NULL; axt = axt->next)
-    markBest(axt, bk, chromSize, ss, winSize, FALSE);
-outputBestRanges(bk, chromSize, ss, f);
+    markBest(axt, bk, chromStart, rangeSize, ss, winSize, FALSE);
+outputBestRanges(bk, chromStart, rangeSize, ss, f);
 uglyf("Output %d alignments including %d trimmed from overlaps\n", writeCount, subsetCount);
 uglyf("Bases in %d, bases out %d (%4.2f%%)\n", baseInCount, baseOutCount,
 	100.0 * baseOutCount / baseInCount);
@@ -437,6 +490,7 @@ winSize = optionInt("winSize", winSize);
 minScore = optionInt("minScore", minScore);
 minOutSize = optionInt("minOutSize", minOutSize);
 initBaseVal();
+setMaxAlloc(2000000000U);
 axtBest(argv[1], argv[2], argv[3]);
 return 0;
 }
