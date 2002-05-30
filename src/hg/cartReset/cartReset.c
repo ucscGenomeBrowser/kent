9a67996bc948691d78f53f3c8e009cc702184581
matt <matt>
2002-05-30 17:42:43 +0000

    Fixed RT bug 264 to make the reset page more user friendly.

--- src/hg/cartReset/cartReset.c
+++ src/hg/cartReset/cartReset.c
@@ -11,12 +11,15 @@ void doMiddle()
 /* cartReset - Reset cart. */
 {
 cartResetInDb("hguid");
-printf("Your settings are now reset to defaults.");
+printf("Your settings are now reset to defaults.<BR>");
+printf("You will be automatically redirected to the gateway page in 2 seconds,\n"
+" or you can <BR> <A href=\"/cgi-bin/hgGateway\">click here to continue</A>.\n");
 }
 
 int main(int argc, char *argv[])
 /* Process command line. */
 {
-htmShell("Reset Cart", doMiddle, NULL);
+char *headText = "<META HTTP-EQUIV=\"REFRESH\" CONTENT=\"2;URL=/cgi-bin/hgGateway\">";
+htmShellWithHead("Reset Cart", headText, doMiddle, NULL);
 return 0;
 }
