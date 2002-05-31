fbd967a904e506ecd0753bfdf82e450d118cdb0b
kent <kent>
2002-05-31 13:55:06 +0000

    New function again.

--- src/hg/oneShot/freen/freen.c
+++ src/hg/oneShot/freen/freen.c
@@ -1,68 +1,34 @@
 /* freen - My Pet Freen. */
-#include <sys/wait.h>
 #include "common.h"
 #include "linefile.h"
-#include "jksql.h"
-#include "genePred.h"
-#include "xAli.h"
+#include "psl.h"
 
 void usage()
 /* Print usage and exit. */
 {
-errAbort("usage: freen in out");
+errAbort("usage: freen in.psl out.psl");
 }
 
-int statusToRetVal(int status)
-/* Convert wait() return status to return value. */
-{
-if (WIFEXITED(status))
-    return WEXITSTATUS(status);
-else 
-    return -666;
-}
-
-void genePredOffset(struct genePred *gp, int offset)
-/* Add offset to gene prediction. */
-{
-int i;
-gp->txStart += offset;
-gp->txEnd += offset;
-gp->cdsStart += offset;
-gp->cdsEnd += offset;
-for (i=0; i<gp->exonCount; ++i)
-    {
-    gp->exonStarts[i] += offset;
-    gp->exonEnds[i] += offset;
-    }
-}
-
-void oldFreen(char *input, char *output)
-{
-FILE *f = mustOpen(output, "w");
-struct lineFile *lf = NULL;
-char *row[50];
-struct genePred *gp;
-
-lf = lineFileOpen(input, TRUE);
-while (lineFileChop(lf, row))
-    {
-    gp = genePredLoad(row);
-    genePredOffset(gp, 100000);
-    genePredTabOut(gp, f);
-    }
-}
-
-void freen(char *status)
+void freen(char *in, char *out)
 /* Print status code. */
 {
-int stat = atoi(status);
-printf("%d\n", statusToRetVal(stat));
+struct psl *psl, *pslList = pslLoadAll(in);
+FILE *f = mustOpen(out, "w");
+char buf[256];
+
+uglyf("Loaded %s\n", in);
+for (psl = pslList; psl != NULL; psl = psl->next)
+    {
+    sprintf(buf, "mm2.%s", psl->qName);
+    psl->qName = buf;
+    pslTabOut(psl, f);
+    }
 }
 
 int main(int argc, char *argv[])
 /* Process command line. */
 {
-if (argc != 2)
+if (argc != 3)
    usage();
-freen(argv[1]);
+freen(argv[1], argv[2]);
 }
