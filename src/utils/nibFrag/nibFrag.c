cd5d7c7ee0d459d4ac434a49d004fcbd2fb3dcad
baertsch <baertsch>
2002-06-04 20:16:56 +0000

    add option to force upper case

--- src/utils/nibFrag/nibFrag.c
+++ src/utils/nibFrag/nibFrag.c
@@ -13,10 +13,11 @@ errAbort(
   "   nibFrag [options] file.nib start end strand out.fa\n"
   "options:\n"
   "   -masked - use lower case characters for masked-out bases\n"
+  "   -upper - use uppper case characters for all bases\n"
   );
 }
 
-void nibFrag(int options, char *nibFile, int start, int end, char strand, char *faFile)
+void nibFrag(int options, char *nibFile, int start, int end, char strand, char *faFile, int optUpper)
 /* nibFrag - Extract part of a nib file as .fa. */
 {
 struct dnaSeq *seq;
@@ -31,6 +32,8 @@ if (start >= end)
 seq = nibLoadPartMasked(options, nibFile, start, end-start);
 if (strand == '-')
     reverseComplement(seq->dna, seq->size);
+if (optUpper == 1)
+    touppers(seq->dna);
 faWrite(faFile, seq->name, seq->dna, seq->size);
 }
 
@@ -38,9 +41,12 @@ int main(int argc, char *argv[])
 /* Process command line. */
 {
 int options = 0;
+int optUpper = 0;
 optionHash(&argc, argv);
 if (optionExists("masked"))
     options = NIB_MASK_MIXED;
+if (optionExists("upper"))
+    optUpper = 1;
 
 if (argc != 6)
     {
@@ -50,6 +56,6 @@ if (!isdigit(argv[2][0]) || !isdigit(argv[3][0]))
     {
     usage();
     }
-nibFrag(options, argv[1], atoi(argv[2]), atoi(argv[3]), argv[4][0], argv[5]);
+nibFrag(options, argv[1], atoi(argv[2]), atoi(argv[3]), argv[4][0], argv[5], optUpper);
 return 0;
 }
