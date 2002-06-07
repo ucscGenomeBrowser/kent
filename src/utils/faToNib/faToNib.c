a0320cd5966495432fb8520465d3f1e242e3ac23
kent <kent>
2002-06-07 00:06:03 +0000

    Renamed 'masked' to 'softMask' and made usage statement parse in English.   Added 'hardMask' option.

--- src/utils/faToNib/faToNib.c
+++ src/utils/faToNib/faToNib.c
@@ -12,13 +12,35 @@ errAbort(
   "usage:\n"
   "   faToNib [options] in.fa out.nib\n"
   "options:\n"
-  "   -masked - use lower-case characters masked-out based\n");
+  "   -softMask - create nib that soft-masks lower case sequence\n"
+  "               Note gfServer/gfClient don't know about this yet\n"
+  "   -hardMask - create nib that hard-masks lower case sequence\n");
+}
+
+void maskLower(struct dnaSeq *seq)
+/* Convert upper case to lower case and lower case to 'n' */
+{
+DNA *dna = seq->dna, base;
+int i, size = seq->size;
+for (i=0; i<size; ++i)
+    {
+    base = dna[size];
+    if (isupper(base))
+	base = tolower(base);
+    else
+        base = 'n';
+    dna[size] = base;
+    }
 }
 
 void faToNib(int options, char *in, char *out)
 /* faToNib - Convert from .fa to .nib format. */
 {
 struct dnaSeq *seq = faReadAllMixed(in);
+if (optionExists("hardMask"))
+    {
+    maskLower(seq);
+    }
 nibWriteMasked(options, seq, out);
 }
 
@@ -27,7 +49,7 @@ int main(int argc, char *argv[])
 {
 int options = 0;
 optionHash(&argc, argv);
-if (optionExists("masked"))
+if (optionExists("softMask"))
     options = NIB_MASK_MIXED;
 if (argc != 3)
     usage();
