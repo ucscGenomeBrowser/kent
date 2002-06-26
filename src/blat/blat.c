7cf710c35af26bcb2642faf7050963db9506a28a
kent <kent>
2002-06-26 05:39:07 +0000

    Bumped version number.

--- src/blat/blat.c
+++ src/blat/blat.c
@@ -16,7 +16,7 @@
 #include "trans3.h"
 #include "repMask.h"
 
-int version = 11;	/* Blat version number. */
+int version = 12;	/* Blat version number. */
 
 enum constants {
 	qWarnSize = 5000000, /* Warn if more than this many bases in one query. */
