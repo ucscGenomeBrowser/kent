bba9be7d75e57efed7ecf3310bdf1893af03f741
tcbruen <tcbruen>
2002-08-15 22:27:56 +0000

    zoo stuff

---
+++ src/hg/zoo/zooInc/misc.h
@@ -0,0 +1,9 @@
+/* Copyright 2002 Trevor Bruen */
+#include <stdlib.h>
+#include <stdio.h>
+
+/* Allocates space checking for safety */
+void *safe_alloc(size_t );
+
+/* Create new copy of string */
+char *strClone(char *s2);
