328c859c594b1d1734b1c6b29d17a97dff950f6b
tcbruen <tcbruen>
2002-08-16 21:14:26 +0000

    blah

--- src/hg/zoo/zooLib/misc.c
+++ src/hg/zoo/zooLib/misc.c
@@ -1,5 +1,6 @@
 /* Copyright 2002 Trevor Bruen */
 #include "misc.h"
+#include <stdio.h>
 
 /* Allocates space checking safety - should be improved */
 void *safe_alloc(size_t size)
@@ -22,4 +23,18 @@ s1=malloc((strlen(s2)+1) * sizeof(char));
 memcpy(s1,s2,strlen(s2));
 s1[strlen(s2)+1] = 0;
 return s1;
+}
+
+/* Open safely */
+FILE *safe_open(char *name, char *mode)
+{
+FILE *handle;
+
+if((handle = fopen(name,mode)) == NULL)
+    {
+    fprintf(stderr,"Error: Couldn't open %s\n",name);
+    exit(1);
+    }
+
+return handle;
 }
