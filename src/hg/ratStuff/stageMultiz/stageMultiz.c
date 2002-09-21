5a0e26907e1b092771d38df1786d6f341ca1ca99
kent <kent>
2002-09-21 07:39:41 +0000

    Seems to work!

--- src/hg/ratStuff/stageMultiz/stageMultiz.c
+++ src/hg/ratStuff/stageMultiz/stageMultiz.c
@@ -5,11 +5,14 @@
 #include "options.h"
 #include "binRange.h"
 #include "axt.h"
-#include "maf.h"
 #include "portable.h"
+#include "errabort.h"
+#include "dnautil.h"
+#include "maf.h"
 
 int winSize = 1010000;
 int overlapSize = 10000;
+boolean noDieMissing = FALSE;
 
 void usage()
 /* Explain usage and exit. */
@@ -17,14 +20,20 @@ void usage()
 errAbort(
   "stageMultiz - Stage input directory for Webb's multiple aligner\n"
   "usage:\n"
-  "   stageMultiz humanChr.axt mouseRatAxtDir outputDir\n"
-  "This will create outputDir, and a subdirectory of outputDir\n"
-  "for each window of the human.   The mouseRatAxtDir needs to\n"
-  "have a chrN.axt.ix for each chrN.axt.  Use axtIndex to generate\n"
-  "these.\n"
+  "   stageMultiz h.sizes m.sizes r.sizes hm.axt mrAxtDir outputDir\n"
+  "where:\n"
+  "   h.sizes - file with sizes of human sequences\n"
+  "   m.sizes - file with sizes of mouse sequences\n"
+  "   r.sizes - file with sizes of rat sequences\n"
+  "   hm.axt - human/mouse alignments for one human chromosome\n"
+  "   mrAxtDir - directory full of mouse/rat alignments with indexes\n"
+  "              built by axtIndex\n"
+  "   outputDir - this will be created, as will subdirectories, one\n"
+  "               for each window on the human chromosome\n"
   "options:\n"
   "   -winSize=N Human bases in each directory.  Default %d\n"
   "   -overlapSize=N Amount to overlap each window. Default %d\n",
+  "   -noDieMissing Don't die over missing mouse/rat alignments\n",
   winSize, overlapSize
   );
 }
@@ -44,21 +53,176 @@ lineFileClose(&lf);
 return bk;
 }
 
-void outputSlice(FILE *f, struct axt *axt, int tStart, int tEnd)
-/* Output a portion of to file as maf. */
+void tempMafAliFromAxt(struct axt *axt, int tSize, int qSize,
+	struct mafAli *temp)
+/* Make a maf out of axt,  parasiting on the memory in axt.
+ * Do *not* mafFree this temp.  The memory it has in pointers
+ * is still owned by the axt.  Furthermore the next call to
+ * this function will invalidate the previous temp value.
+ * It's sort of a kludge, but quick to run and easy to implement. */
 {
-if (tStart < axt->tStart)
-    tStart = axt->tStart;
-if (tEnd > axt->tEnd)
-    tEnd = axt->tEnd;
-axtSubsetOnT(axt, tStart, tEnd, axtScoreSchemeDefault(), f);
+static struct mafComp qComp, tComp;
+ZeroVar(temp);
+ZeroVar(&qComp);
+ZeroVar(&tComp);
+temp->score = axt->score;
+temp->textSize = axt->symCount;
+qComp.src = axt->qName;
+qComp.srcSize = qSize;
+qComp.strand = axt->qStrand;
+qComp.start = axt->qStart;
+qComp.size = axt->qEnd - axt->qStart;
+qComp.text = axt->qSym;
+slAddHead(&temp->components, &qComp);
+tComp.src = axt->tName;
+tComp.srcSize = tSize;
+tComp.strand = axt->tStrand;
+tComp.start = axt->tStart;
+tComp.size = axt->tEnd - axt->tStart;
+tComp.text = axt->tSym;
+slAddHead(&temp->components, &tComp);
 }
 
-void stageMultiz(char *humanAxtFile, char *ratMouseDir, char *outputDir)
+void outputAxtAsMaf(FILE *f, struct axt *axt, 
+	struct hash *tSizeHash, struct hash *qSizeHash)
+/* Write out an axt in maf format. */
+{
+struct mafAli temp;
+tempMafAliFromAxt(axt, hashIntVal(tSizeHash, axt->tName),
+	hashIntVal(qSizeHash, axt->qName), &temp);
+mafWrite(f, &temp);
+}
+
+struct hash *loadSizeHash(char *fileName)
+/* Load up file with lines of name<space>size into hash. */
+{
+struct lineFile *lf = lineFileOpen(fileName, TRUE);
+char *row[2];
+struct hash *hash = hashNew(0);
+
+while (lineFileRow(lf, row))
+    hashAddInt(hash, row[0], lineFileNeedNum(lf, row, 1));
+return hash;
+}
+
+struct mouseChromCache
+/* Hold cashe for one mouse chromosome. */
+    {
+    struct mouseChromCache *next;
+    char *name;	/* Chromosome name. */
+    int size;	/* Mouse chromosome size. */
+    struct lineFile *lf; /* Open lineFile containing alignments */
+    struct binKeeper *bk; /* Interval index containing positions in lf. */
+    };
+
+struct mouseChromCache *newMouseChromCache(char *chrom, int chromSize, 
+	char *ratMouseDir)
+/* Create a new chromCache. */
+{
+struct mouseChromCache *mcc;
+char fileName[512];
+struct lineFile *lf;
+char *row[3];
+int start,end;
+long long *pPos;
+
+/* Open up file with actual alignments.  Warn and return NULL
+ * if it doesn't exist. */
+sprintf(fileName, "%s/%s.axt", ratMouseDir, chrom);
+lf = lineFileMayOpen(fileName, TRUE);
+
+/* Allocate structure and store basic info in it. */
+AllocVar(mcc);
+mcc->name = cloneString(chrom);
+mcc->size = chromSize;
+mcc->lf = lf;
+if (lf == NULL)
+    {
+    warn("%s doesn't exist", fileName);
+    if (!noDieMissing)
+        noWarnAbort(); 
+    return mcc;
+    }
+
+/* Read index file into bk. */
+sprintf(fileName, "%s/%s.axt.ix", ratMouseDir, chrom);
+mcc->bk = binKeeperNew(0, chromSize);
+lf = lineFileOpen(fileName, TRUE);
+printf("Reading %s\n", fileName);
+while (lineFileRow(lf, row))
+    {
+    start = lineFileNeedNum(lf, row, 0);
+    end = lineFileNeedNum(lf, row, 1) + start;
+    AllocVar(pPos);
+    *pPos = atoll(row[2]);
+    binKeeperAdd(mcc->bk, start, end, pPos);
+    }
+lineFileClose(&lf);
+
+/* Return initialized object. */
+return mcc;
+}
+
+void writeMousePartsAsMaf(FILE *f, struct hash *mouseHash, 
+	char *ratMouseDir, char *mouseChrom,
+	int mouseStart, int mouseEnd, int mouseChromSize, 
+	struct hash *rSizeHash, struct hash *dupeHash)
+/* Write out mouse/rat alignments that intersect given region of mouse.
+ * This gets a little involved because we need to do random access on
+ * the mouse/rat alignment files, which are too big to fit into memory.
+ * On disk we have a mouse/rat alignment file for each mouse chromosome,
+ * and an index of it.  When we first access a mouse chromosome we load
+ * the index for that chromosome into memory, and open the alignment file.
+ * We then do a seek and read to load a particular alignment. */
+{
+struct mouseChromCache *mcc = NULL;
+struct binElement *list = NULL, *el;
+char aliName[512];
+
+/* Get cache for this mouse chromosome */
+mcc = hashFindVal(mouseHash, mouseChrom);
+if (mcc == NULL)
+    {
+    mcc = newMouseChromCache(mouseChrom, mouseChromSize, ratMouseDir);
+    hashAdd(mouseHash, mouseChrom, mcc);
+    }
+if (mcc->lf == NULL)
+    return;
+
+/* Get list of positions and process one axt into a maf for each */
+list = binKeeperFindSorted(mcc->bk, mouseStart, mouseEnd);
+for (el = list; el != NULL; el = el->next)
+    {
+    struct axt *axt;
+    struct mafAli temp;
+    long long *pPos, pos;
+    pPos = el->val;
+    pos = *pPos;
+    sprintf(aliName, "%s.%lld", mouseChrom, pos);
+    if (!hashLookup(dupeHash, aliName))
+	{
+	hashAdd(dupeHash, aliName, NULL);
+	lineFileSeek(mcc->lf, pos, SEEK_SET);
+	axt = axtRead(mcc->lf);
+	tempMafAliFromAxt(axt, mouseChromSize, hashIntVal(rSizeHash, axt->qName),
+	    &temp);
+	mafWrite(f, &temp);
+	axtFree(&axt);
+	}
+    }
+slFreeList(&list);
+}
+
+void stageMultiz(char *hSizeFile, char *mSizeFile, char *rSizeFile,
+	char *humanAxtFile, char *ratMouseDir, char *outputDir)
 /* stageMultiz - Stage input directory for Webb's multiple aligner. */
 {
 struct binKeeper *humanBk = loadAxtsIntoRange(humanAxtFile);
 struct hash *mouseHash = newHash(0);
+struct hash *hSizeHash = loadSizeHash(hSizeFile);
+struct hash *mSizeHash = loadSizeHash(mSizeFile);
+struct hash *rSizeHash = loadSizeHash(rSizeFile);
+struct hash *dupeHash = newHash(16);
 int hStart;
 char humanChromName[256];
 
@@ -77,18 +241,37 @@ for (hStart = 0; hStart<maxChromSize - winSize; hStart += winSize - overlapSize)
 	{
 	/* Make output directory and create output file names. */
 	sprintf(dirName, "%s/%s.%d", outputDir, humanChromName, hStart);
-	uglyf("Making %s\n", dirName);
+	printf("Making %s\n", dirName);
 	makeDir(dirName);
-	sprintf(hmName, "%s/hm.axt", dirName);
-	sprintf(mrName, "%s/mr.axt", dirName);
+	sprintf(hmName, "%s/hm.maf", dirName);
+	sprintf(mrName, "%s/mr.maf", dirName);
 
 	/* Write human/mouse file. */
 	f = mustOpen(hmName, "w");
+	mafWriteStart(f, "blastz");
 	for (humanEl = humanList; humanEl != NULL; humanEl = humanEl->next)
 	    {
 	    struct axt *axt = humanEl->val;
-	    outputSlice(f, axt, hStart, hEnd);
+	    outputAxtAsMaf(f, axt, hSizeHash, mSizeHash);
 	    }
+	mafWriteEnd(f);
+	carefulClose(&f);
+
+	/* Write mouse/rat file. */
+	f = mustOpen(mrName, "w");
+	mafWriteStart(f, "blastz");
+	for (humanEl = humanList; humanEl != NULL; humanEl = humanEl->next)
+	    {
+	    struct axt *axt = humanEl->val;
+	    int start = axt->qStart, end = axt->qEnd;
+	    int mouseChromSize = hashIntVal(mSizeHash, axt->qName);
+	    struct binElement *list, *el;
+	    if (axt->qStrand == '-')
+	        reverseIntRange(&start, &end, mouseChromSize);
+	    writeMousePartsAsMaf(f, mouseHash, ratMouseDir, axt->qName, 
+	    	start, end, mouseChromSize, rSizeHash, dupeHash);
+	    }
+	mafWriteEnd(f);
 	carefulClose(&f);
 	slFreeList(&humanList);
 	}
@@ -98,11 +281,13 @@ for (hStart = 0; hStart<maxChromSize - winSize; hStart += winSize - overlapSize)
 int main(int argc, char *argv[])
 /* Process command line. */
 {
+dnaUtilOpen();
 optionHash(&argc, argv);
-if (argc != 4)
+if (argc != 7)
     usage();
 winSize = optionInt("winSize", winSize);
 overlapSize = optionInt("overlapSize", overlapSize);
-stageMultiz(argv[1], argv[2], argv[3]);
+noDieMissing = optionExists("noDieMissing");
+stageMultiz(argv[1], argv[2], argv[3], argv[4], argv[5], argv[6]);
 return 0;
 }
