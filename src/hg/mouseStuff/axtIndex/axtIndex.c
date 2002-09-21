519a1ebf8a1b765b41ce25706f2377e3f4aa801f
kent <kent>
2002-09-21 04:10:09 +0000

    Seems to work.

--- src/hg/mouseStuff/axtIndex/axtIndex.c
+++ src/hg/mouseStuff/axtIndex/axtIndex.c
@@ -1,32 +1,46 @@
-/* axtIndex - Create summary file for axt. */
+/* axtIndex - index axt file by range. */
 #include "common.h"
 #include "linefile.h"
 #include "hash.h"
 #include "options.h"
+#include "axt.h"
 
 void usage()
 /* Explain usage and exit. */
 {
 errAbort(
-  "axtIndex - Create summary file for axt\n"
+  "axtIndex - build index of axt file\n"
   "usage:\n"
-  "   axtIndex XXX\n"
+  "   axtIndex in.axt out.axt.ix\n"
   "options:\n"
   "   -xxx=XXX\n"
   );
 }
 
-void axtIndex(char *XXX)
+void axtIndex(char *in, char *out)
 /* axtIndex - Create summary file for axt. */
 {
+struct lineFile *lf = lineFileOpen(in, TRUE);
+FILE *f = mustOpen(out, "w");
+struct axt *axt;
+
+for (;;)
+    {
+    off_t pos = lineFileTell(lf);
+    axt = axtRead(lf);
+    if (axt == NULL)
+        break;
+    fprintf(f, "%d %d %lld\n", axt->tStart, axt->tEnd - axt->tStart, pos); 
+    axtFree(&axt);
+    }
 }
 
 int main(int argc, char *argv[])
 /* Process command line. */
 {
 optionHash(&argc, argv);
-if (argc != 2)
+if (argc != 3)
     usage();
-axtIndex(argv[1]);
+axtIndex(argv[1], argv[2]);
 return 0;
 }
