410b7d4b10525b0b309803f0b542005d7d2f0718
kent <kent>
2002-09-24 16:31:03 +0000

    Seems to work.

--- src/hg/mouseStuff/axtInvert/axtInvert.c
+++ src/hg/mouseStuff/axtInvert/axtInvert.c
@@ -3,6 +3,8 @@
 #include "linefile.h"
 #include "hash.h"
 #include "options.h"
+#include "dnautil.h"
+#include "axt.h"
 
 void usage()
 /* Explain usage and exit. */
@@ -10,23 +12,78 @@ void usage()
 errAbort(
   "axtInvert - Swap source and query in an axt file\n"
   "usage:\n"
-  "   axtInvert XXX\n"
+  "   axtInvert source.axt target.sizes query.sizes dest.axt\n"
   "options:\n"
   "   -xxx=XXX\n"
   );
 }
 
-void axtInvert(char *XXX)
+struct hash *loadIntHash(char *fileName)
+/* Load up file with lines of name<space>size into hash. */
+{
+struct lineFile *lf = lineFileOpen(fileName, TRUE);
+char *row[2];
+struct hash *hash = hashNew(0);
+
+while (lineFileRow(lf, row))
+    hashAddInt(hash, row[0], lineFileNeedNum(lf, row, 1));
+return hash;
+}
+
+
+void axtInvert(struct axt *axt, int tSize, int qSize)
+/* Flip target and query on one axt. */
+{
+struct axt old = *axt;
+
+/* Copy non-strand dependent stuff */
+axt->qName = old.tName;
+axt->tName = old.qName;
+axt->qSym = old.tSym;
+axt->tSym = old.qSym;
+axt->qStart = old.tStart;
+axt->qEnd = old.tEnd;
+axt->tStart = old.qStart;
+axt->tEnd = old.qEnd;
+
+/* Copy strand dependent stuff. */
+assert(axt->tStrand != '-');
+
+if (axt->qStrand == '-')
+    {
+    /* axt's are really set up so that the target is on the
+     * + strand and the query is on the minus strand.
+     * Therefore we need to reverse complement both 
+     * strands while swapping to preserve this. */
+    reverseIntRange(&axt->tStart, &axt->tEnd, qSize);
+    reverseIntRange(&axt->qStart, &axt->qEnd, tSize);
+    reverseComplement(axt->qSym, axt->symCount);
+    reverseComplement(axt->tSym, axt->symCount);
+    }
+}
+
+void axtInvertFile(char *source, char *targetSizes, char *querySizes, char *dest)
 /* axtInvert - Swap source and query in an axt file. */
 {
+struct hash *tHash = loadIntHash(targetSizes);
+struct hash *qHash = loadIntHash(querySizes);
+struct lineFile *lf = lineFileOpen(source, TRUE);
+FILE *f = mustOpen(dest, "w");
+struct axt *axt;
+
+while ((axt = axtRead(lf)) != NULL)
+    {
+    axtInvert(axt, hashIntVal(tHash, axt->tName), hashIntVal(qHash, axt->qName));
+    axtWrite(axt, f);
+    }
 }
 
 int main(int argc, char *argv[])
 /* Process command line. */
 {
 optionHash(&argc, argv);
-if (argc != 2)
+if (argc != 5)
     usage();
-axtInvert(argv[1]);
+axtInvertFile(argv[1], argv[2], argv[3], argv[4]);
 return 0;
 }
