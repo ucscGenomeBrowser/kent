11aa960a73783f7b93ff423ac12ddd347b3a1237
kent <kent>
2002-09-24 16:39:20 +0000

    Seems to work.

--- src/hg/mouseStuff/axtSplitByTarget/axtSplitByTarget.c
+++ src/hg/mouseStuff/axtSplitByTarget/axtSplitByTarget.c
@@ -3,6 +3,8 @@
 #include "linefile.h"
 #include "hash.h"
 #include "options.h"
+#include "portable.h"
+#include "axt.h"
 
 void usage()
 /* Explain usage and exit. */
@@ -10,23 +12,38 @@ void usage()
 errAbort(
   "axtSplitByTarget - Split a single axt file into one file per target\n"
   "usage:\n"
-  "   axtSplitByTarget XXX\n"
-  "options:\n"
-  "   -xxx=XXX\n"
+  "   axtSplitByTarget in.axt outDir\n"
   );
 }
 
-void axtSplitByTarget(char *XXX)
+void axtSplitByTarget(char *inName, char *outDir)
 /* axtSplitByTarget - Split a single axt file into one file per target. */
 {
+char outName[512];
+struct hash *outHash = newHash(8);  /* FILE valued hash */
+struct lineFile *lf = lineFileOpen(inName, TRUE);
+struct axt *axt;
+
+makeDir(outDir);
+while ((axt = axtRead(lf)) != NULL)
+    {
+    FILE *f = hashFindVal(outHash, axt->tName);
+    if (f == NULL)
+        {
+	sprintf(outName, "%s/%s.axt", outDir, axt->tName);
+	f = mustOpen(outName, "w");
+	hashAdd(outHash, axt->tName, f);
+	}
+    axtWrite(axt, f);
+    }
 }
 
 int main(int argc, char *argv[])
 /* Process command line. */
 {
 optionHash(&argc, argv);
-if (argc != 2)
+if (argc != 3)
     usage();
-axtSplitByTarget(argv[1]);
+axtSplitByTarget(argv[1], argv[2]);
 return 0;
 }
