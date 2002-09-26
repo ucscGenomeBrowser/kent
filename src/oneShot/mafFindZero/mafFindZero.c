7d7f8b8b05a46c0e9a059042fc1995567b5ecfe7
kent <kent>
2002-09-26 04:04:23 +0000

    Some noodling that's not coming to anything.

--- src/oneShot/mafFindZero/mafFindZero.c
+++ src/oneShot/mafFindZero/mafFindZero.c
@@ -3,6 +3,7 @@
 #include "linefile.h"
 #include "hash.h"
 #include "options.h"
+#include "maf.h"
 
 void usage()
 /* Explain usage and exit. */
@@ -10,23 +11,34 @@ void usage()
 errAbort(
   "mafFindZero - Find small mafs\n"
   "usage:\n"
-  "   mafFindZero XXX\n"
+  "   mafFindZero mafFile(s)\n"
   "options:\n"
   "   -xxx=XXX\n"
   );
 }
 
-void mafFindZero(char *XXX)
+void mafFindZero(char *file)
 /* mafFindZero - Find small mafs. */
 {
+struct mafFile *mf = mafOpen(file);
+struct mafAli *maf;
+
+while ((maf = mafNext(mf)) != NULL)
+    {
+    if (maf->textSize <= 0)
+        printf("%s %d\n", mf->lf->fileName, mf->lf->lineIx-3);
+    mafAliFree(&maf);
+    }
 }
 
 int main(int argc, char *argv[])
 /* Process command line. */
 {
+int i;
 optionHash(&argc, argv);
-if (argc != 2)
+if (argc < 2)
     usage();
-mafFindZero(argv[1]);
+for (i=1; i<argc; ++i)
+    mafFindZero(argv[i]);
 return 0;
 }
