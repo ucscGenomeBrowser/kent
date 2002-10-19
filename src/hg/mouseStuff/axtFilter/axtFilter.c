c2cf45c44f3cdb0c565134de748540b836d65729
kent <kent>
2002-10-19 21:00:19 +0000

    Seems to work.

--- src/hg/mouseStuff/axtFilter/axtFilter.c
+++ src/hg/mouseStuff/axtFilter/axtFilter.c
@@ -3,30 +3,95 @@
 #include "linefile.h"
 #include "hash.h"
 #include "options.h"
+#include "axt.h"
 
 void usage()
 /* Explain usage and exit. */
 {
 errAbort(
-  "axtFilter - Filter axt files\n"
+  "axtFilter - Filter axt files.  Output goes to standard out.\n"
   "usage:\n"
-  "   axtFilter XXX\n"
+  "   axtFilter file(s)\n"
   "options:\n"
-  "   -xxx=XXX\n"
+  "   -q=chr1,chr2 - restrict query side sequence to those named\n"
+  "   -notQ=chr1,chr2 - restrict query side sequence to those not named\n"
+  "   -t=chr1,chr2 - restrict target side sequence to those named\n"
+  "   -notT=chr1,chr2 - restrict target side sequence to those not named\n"
+  "   -minScore=N - restrict to those scoring at least N\n"
+  "   -maxScore=N - restrict to those scoring less than N\n"
   );
 }
 
-void axtFilter(char *XXX)
+struct hash *hashCommaString(char *s)
+/* Make hash out of comma separated string. */
+{
+char *e;
+struct hash *hash = newHash(8);
+while (s != NULL && s[0] != 0)
+    {
+    e = strchr(s, ',');
+    if (e != NULL)
+        *e = 0;
+    hashAdd(hash, s, NULL);
+    if (e != NULL)
+	e += 1;
+    s = e;
+    }
+return hash;
+}
+
+struct hash *hashCommaOption(char *opt)
+/* Make hash out of optional value. */
+{
+char *s = optionVal(opt, NULL);
+if (s == NULL)
+    return NULL;
+return hashCommaString(s);
+}
+
+void axtFilter(int inCount, char *inNames[])
 /* axtFilter - Filter axt files. */
 {
+struct hash *qHash = hashCommaOption("q");
+struct hash *tHash = hashCommaOption("t");
+struct hash *notQHash = hashCommaOption("notQ");
+struct hash *notTHash = hashCommaOption("notT");
+int minScore = optionInt("minScore", -BIGNUM);
+int maxScore = optionInt("maxScore", BIGNUM);
+int i;
+
+for (i=0; i<inCount; ++i)
+    {
+    char *fileName = inNames[i];
+    struct lineFile *lf = lineFileOpen(fileName, TRUE);
+    struct axt *axt;
+    while ((axt = axtRead(lf)) != NULL)
+        {
+	boolean writeIt = TRUE;
+	if (qHash != NULL && !hashLookup(qHash, axt->qName))
+	    writeIt = FALSE;
+	if (notQHash != NULL && hashLookup(notQHash, axt->qName))
+	    writeIt = FALSE;
+	if (tHash != NULL && !hashLookup(tHash, axt->tName))
+	    writeIt = FALSE;
+	if (notTHash != NULL && hashLookup(notTHash, axt->tName))
+	    writeIt = FALSE;
+	if (axt->score < minScore || axt->score >= maxScore)
+	    writeIt = FALSE;
+	if (writeIt)
+	    axtWrite(axt, stdout);
+	axtFree(&axt);
+	}
+    lineFileClose(&lf);
+    }
 }
 
 int main(int argc, char *argv[])
 /* Process command line. */
 {
 optionHash(&argc, argv);
-if (argc != 2)
+if (argc < 2)
     usage();
-axtFilter(argv[1]);
+axtFilter(argc-1, argv+1);
 return 0;
 }
