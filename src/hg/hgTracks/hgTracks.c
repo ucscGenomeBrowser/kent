3215f1c89747dc5603a2f7990f9305db00bda2f0
matt <matt>
2002-10-30 23:54:07 +0000

    Changed refgene track handling

--- src/hg/hgTracks/hgTracks.c
+++ src/hg/hgTracks/hgTracks.c
@@ -2831,7 +2831,7 @@ char *refGeneName(struct trackGroup *tg, void *item)
 struct linkedFeatures *lf = item;
 if (lf->extra != NULL) 
     {
-    sprintf(cat,"%s %s",lf->name, (char *)lf->extra);
+    sprintf(cat,"%s",(char *)lf->extra);
     return cat;
     }
 else return lf->name;
