b6812eda3667cf57648f776bb972569594a05179
kent <kent>
2002-11-01 21:20:04 +0000

    Seems to at least mostly work traversing tree and outputting highest chain.

--- src/oneShot/newStitch3/newStitch3.c
+++ src/oneShot/newStitch3/newStitch3.c
@@ -1,4 +1,5 @@
 /* newStitch3 - Another stitching experiment - with kd-trees.. */
+
 #include "common.h"
 #include "linefile.h"
 #include "hash.h"
@@ -30,7 +31,10 @@ struct asBlock
     int tStart, tEnd;		/* Range covered in target. */
     int score;			/* Alignment score for this block. */
     void *ali;			/* Alignment in some format. */
-    bool hit;			/* This hit? (I wish this were somewhere else) */
+    struct asBranch *branch;	/* Node in tree. Used internally. */
+    struct asBranch *bestPred;	/* Best predicessor - used internally. */
+    int totalScore;		/* Total score of chain up to here. */
+    bool hit;			/* This hit? Used by system internally. */
     };
 
 struct asChain
@@ -38,19 +42,19 @@ struct asChain
     {
     struct asChain *next;	/* Next in list. */
     struct asBlock *blockList;	/* List of blocks. */
+    int score;			/* Total score for chain. */
     };
 
 struct asBranch
-/* A kd-tree.  That is a binary tree which partitions the children
+/* A kd-tree. That is a binary tree which partitions the children
  * into higher and lower one dimension at a time.  We're just doing
- * one in two dimensions, so it alternates between x and y dimensions,
- * or, for alignments between q and t. */
+ * one in two dimensions, so it alternates between q and t dimensions. */
     {
-    struct asBranch *low;       /* Pointer to children with lower coordinates. */
-    struct asBranch *high;      /* Pointer to children with higher coordinates. */
-    struct asBlock *block;	/* Block pointer for leaves on tree. */
-    int cutCoord;		/* Coordinate (in some dimension) to cut on */
-    int maxScore;		/* Max score of any leaf below us. */
+    struct asBranch *lo;      /* Pointer to children with lower coordinates. */
+    struct asBranch *hi;      /* Pointer to children with higher coordinates. */
+    struct asBlock *block;    /* Block pointer for leaves on tree. */
+    int cutCoord;	      /* Coordinate (in some dimension) to cut on */
+    int maxScore;	      /* Max score of any leaf below us. */
     };
 
 struct asTree
@@ -58,7 +62,40 @@ struct asTree
     {
     struct asBranch *root;	/* Pointer to root of kd-tree. */
     struct lm *lm;		/* Branches etc. allocated from here. */
+    int maxDepth;		/* Maximum depth of tree. */
     };
+
+void indent(FILE *f, int count)
+/* Print given number of spaces. */
+{
+while (--count >= 0)
+    fprintf(f, "  ");
+}
+
+void rBranchDump(struct asBranch *branch, int depth, FILE *f)
+/* Dump branches. */
+{
+int i;
+if (branch->block)
+    {
+    indent(f, depth+1);
+    fprintf(f, "[%d] %d (q), %d (t)\n", branch->maxScore, branch->block->qStart, branch->block->tStart);
+    }
+else
+    {
+    rBranchDump(branch->lo, depth+1, f);
+    indent(f, depth+1);
+    fprintf(f, "%c [%d] %d\n", ((depth&1) == 0 ? 'q' : 't'), branch->maxScore, branch->cutCoord);
+    rBranchDump(branch->hi, depth+1, f);
+    }
+}
+
+void asTreeDump(struct asTree *tree, FILE *f)
+/* Dump out tree to file. */
+{
+fprintf(f, "treeDump: maxDepth=%d\n", tree->maxDepth);
+rBranchDump(tree->root, 0, f);
+}
 
 int asBlockCmpQ(const void *va, const void *vb)
 /* Compare to sort based on target start. */
@@ -77,25 +114,24 @@ return a->tStart - b->tStart;
 }
 
 int medianVal(struct dlList *list, int medianIx, int dim)
-/* Return value of median block in list on given dimension */
+/* Return value of median block in list on given dimension 
+ * Mark blocks up to median as hit. */
 {
 struct dlNode *node = list->head;
 struct asBlock *block = NULL;
 int i;
 
-/* Figure out median value in this dimension */
 for (i=0; i<medianIx; ++i)
     {
-    node = node->next;
     block = node->val;
-    block->hit = TRUE;
+    block->hit = TRUE;  
+    node = node->next;
     }
 return (dim == 0 ? block->qStart : block->tStart);
 }
 
-int splitList(struct dlList *oldList, struct dlList *newList, int cutCoord)
-/* Peel off members of oldList that are greater than cutCoord
- * in given dimension onto new list.  Return count on new list. */
+int splitList(struct dlList *oldList, struct dlList *newList)
+/* Peel off members of oldList that are not hit onto new list. */
 {
 struct dlNode *node, *next;
 struct asBlock *block;
@@ -105,7 +141,7 @@ for (node = oldList->head; !dlEnd(node); node = next)
     {
     next = node->next;
     block = node->val;
-    if (block->hit)
+    if (!block->hit)
 	{
 	dlRemove(node);
 	dlAddTail(newList, node);
@@ -126,18 +162,39 @@ for (node = list->head; !dlEnd(node); node = node->next)
     }
 }
 
-static struct lm *lm;
-/* Local memory pool for branches. */
+static struct lm *lm; /* Local memory pool for branches. */
+static int depth;     /* Current recursion depth. */
+static int maxDepth;  /* Maximum recursion depth. */
+
+
+void dumpList(char *name, struct dlList *list)
+/* Dump list of blocks. */
+{
+struct dlNode *node;
+uglyf("%s\n", name);
+for (node = list->head; !dlEnd(node); node = node->next)
+    {
+    struct asBlock *block = node->val;
+    uglyf("  %d,%d  %d,%d\n", block->qStart,block->qEnd, block->tStart,block->tEnd);
+    }
+}
 
 struct asBranch *kdBuild(int nodeCount, struct dlList *lists[2], int dim)
 /* Build up kd-tree recursively. */
 {
 struct asBranch *branch;
 
+if (++depth > maxDepth)
+    maxDepth = depth;
+// uglyf("kdBuild depth %d, nodeCount %d\n", depth, nodeCount);
+// dumpList(" qList", lists[0]);
+// dumpList(" tList", lists[1]);
 lmAllocVar(lm, branch);
 if (nodeCount <= 1)
     {
-    branch->block = lists[0]->head->val;
+    struct asBlock *block = lists[0]->head->val;
+    branch->block = block;
+    block->branch = branch;
     }
 else
     {
@@ -149,15 +206,16 @@ else
     /* Subdivide lists along median.  */
     newLists[0] = &newQ;
     newLists[1] = &newT;
-    clearHits(lists[dim]);
-    cutCoord = medianVal(lists[dim], nodeCount/2, dim);
-    newCount = splitList(lists[0], newLists[0], cutCoord);
-    splitList(lists[1], newLists[1], cutCoord);
+    clearHits(lists[0]);
+    branch->cutCoord = medianVal(lists[dim], nodeCount/2, dim);
+    newCount = splitList(lists[0], newLists[0]);
+    splitList(lists[1], newLists[1]);
 
     /* Recurse on each side. */
-    branch->low = kdBuild(nodeCount - newCount, lists, nextDim);
-    branch->high = kdBuild(newCount, newLists, nextDim);
+    branch->lo = kdBuild(nodeCount - newCount, lists, nextDim);
+    branch->hi = kdBuild(newCount, newLists, nextDim);
     }
+--depth;
 return branch;
 }
 
@@ -174,7 +232,7 @@ int i;
 
 /* Build lists sorted in each dimension. This
  * will help us quickly find medians while constructing
- * the kd-tree. Block list is sorted initially on t.*/
+ * the kd-tree. */
 dlListInit(&qList);
 dlListInit(&tList);
 AllocArray(qNodes, nodeCount);
@@ -185,14 +243,16 @@ for (i=0 , block=blockList; block != NULL; block = block->next, ++i)
     dlAddTail(&qList, &qNodes[i]);
     dlAddTail(&tList, &tNodes[i]);
     }
-dlSort(&qList, asBlockCmpQ);
+dlSort(&qList, asBlockCmpQ); /* tList sorted since blockList is sorted. */
 lists[0] = &qList;
 lists[1] = &tList;
 
 /* Allocate master data structure and call recursive builder. */
 AllocVar(tree);
+maxDepth = depth = 0;
 tree->lm = lm = lmInit(0);
 tree->root = kdBuild(nodeCount, lists, 0);
+tree->maxDepth = maxDepth;
 lm = NULL;
 
 /* Clean up and go home. */
@@ -212,6 +272,133 @@ if (tree != NULL)
     }
 }
 
+int defaultGapPenalty(int qSize, int tSize)
+/* A logarithmic gap penalty scaled to axt defaults. */
+{
+int total = qSize + tSize;
+if (total <= 0)
+    return 0;
+return 400 * pow(total, 1.0/3.3);
+}
+
+int gap(struct asBlock *a, struct asBlock *b)
+/* Return gap score from a to b */
+{
+int dq = b->qStart - a->qEnd;
+int dt = b->tStart - a->tEnd;
+if (dq < 0) dq = 0;
+if (dt < 0) dt = 0;
+return defaultGapPenalty(dq, dt);
+}
+
+
+struct predScore
+/* Predecessor and score we get merging with it. */
+    {
+    struct asBranch *pred;	/* Predecessor. */
+    int score;			/* Score of us plus predecessor. */
+    };
+
+struct predScore bestPredecessor(
+	struct asBlock *curBlock,   /* Block who's predecessor we seek */
+	int dim,		    /* Dimension level of tree splits on. */
+	struct asBranch *branch,    /* Subtree to explore */
+	struct predScore bestSoFar) /* Best predecessor so far. */
+{
+struct asBlock *leaf;
+
+/* If best score in this branch of tree wouldn't be enough
+ * (even without gaps) don't bother exploring it. */
+if (branch->maxScore + curBlock->score < bestSoFar.score)
+    return bestSoFar;
+
+/* If it's a leaf branch, then calculate score to connect
+ * with it. */
+else if ((leaf = branch->block) != NULL)
+    {
+    if (leaf->qStart < curBlock->qStart && leaf->tStart < curBlock->tStart)
+	{
+	int score = leaf->totalScore + curBlock->score - gap(leaf, curBlock);
+	if (score > bestSoFar.score)
+	   {
+	   bestSoFar.score = score;
+	   bestSoFar.pred = branch;
+	   }
+	}
+    return bestSoFar;
+    }
+
+/* Otherwise explore sub-trees that could harbor predecessors. */
+else
+    {
+    int newDim = 1-dim;
+    int dimCoord = (dim == 0 ? curBlock->qStart : curBlock->tStart);
+    
+    /* Explore hi branch first as it is more likely to have high
+     * scores.  However only explore it if it can have things starting
+     * before us. */
+    if (dimCoord > branch->cutCoord)
+         bestSoFar = bestPredecessor(curBlock, newDim, branch->hi, bestSoFar);
+    bestSoFar = bestPredecessor(curBlock, newDim, branch->lo, bestSoFar);
+    return bestSoFar;
+    }
+}
+
+void updateScoresOnWay(struct asBranch *branch, int dim, struct asBlock *block)
+/* Traverse kd-tree to find block.  Update all maxScores on the way
+ * to reflect block->totalScore. */
+{
+int newDim = 1-dim;
+int dimCoord = (dim == 0 ? block->qStart : block->tStart);
+if (branch->maxScore < block->totalScore) branch->maxScore = block->totalScore;
+if (branch->block == NULL)
+    {
+    if (dimCoord <= branch->cutCoord)
+	updateScoresOnWay(branch->lo, newDim, block);
+    if (dimCoord >= branch->cutCoord)
+	updateScoresOnWay(branch->hi, newDim, block);
+    }
+}
+
+
+struct asChain *asChainFind(struct asTree *tree, struct asBlock *blockList)
+/* Find highest scoring chain in tree. */
+{
+static struct predScore noBest;
+struct asBlock *block;
+struct asBlock *bestBlock = NULL;
+int bestScore = 0;
+struct asChain *chain;
+
+for (block = blockList; block != NULL; block = block->next)
+    {
+    struct predScore best = bestPredecessor(block, 0, tree->root, noBest);
+    if (best.score > block->totalScore)
+        {
+	block->totalScore = best.score;
+	block->bestPred = best.pred;
+	}
+    updateScoresOnWay(tree->root, 0, block);
+    if (bestScore < block->totalScore)
+        {
+	bestScore = block->totalScore;
+	bestBlock = block;
+	}
+    }
+AllocVar(chain);
+chain->score = bestScore;
+for (block = bestBlock; ; )
+    {
+    struct asBranch *pred;
+    slAddHead(&chain->blockList, block);
+    pred = block->bestPred;
+    if (pred == NULL)
+        break;
+    block = pred->block;
+    }
+return chain;
+}
+
 struct seqPair
 /* Pair of sequences. */
     {
@@ -219,6 +406,7 @@ struct seqPair
     char *name;	/* Allocated in hash */
     struct axt *axtList; /* List of alignments. */
     };
+
 
 void chainPair(struct seqPair *sp, FILE *f)
 /* Make chains for all alignments in sp. */
@@ -228,6 +416,7 @@ struct asNode *n, *nList = NULL;
 struct asTree *tree;
 struct axt *axt, *first, *last;
 struct asBlock *blockList = NULL, *block;
+struct asChain *chain;
 
 /* Make block list */
 for (axt = sp->axtList; axt != NULL; axt = axt->next)
@@ -237,7 +426,7 @@ for (axt = sp->axtList; axt != NULL; axt = axt->next)
     block->qEnd = axt->qEnd;
     block->tStart = axt->tStart;
     block->tEnd = axt->tEnd;
-    block->score = axt->score;
+    block->score = block->totalScore = axt->score;
     block->ali = axt;
     slAddHead(&blockList, block);
     }
@@ -246,11 +435,29 @@ slSort(&blockList, asBlockCmpT);
 /* Make up tree and time it for debugging. */
 startTime = clock1000();
 tree = asTreeMake(blockList);
+// asTreeDump(tree, uglyOut);
 dt = clock1000() - startTime;
-uglyf("Made tree in %f seconds\n", dt*0.001);
+uglyf("Made tree in %5.3f seconds\n", dt*0.001);
 
+/* Get longest chain from tree and print it out. */
+chain = asChainFind(tree, blockList);
+if (chain != NULL)
+    {
+    struct asBlock *first = chain->blockList;
+    struct asBlock *last = slLastEl(first);
+    fprintf(f, "%s Chain %d, score %d, %d %d, %d %d:\n", 
+	sp->name, slCount(chain->blockList), chain->score,
+	first->qStart, last->qEnd, first->tStart, last->tEnd);
+    for (block = chain->blockList; block != NULL; block = block->next)
+        {
+	struct axt *axt = block->ali;
+	fprintf(f, " %s%c%s %d %d, %d %d, %d\n",
+		axt->qName, axt->qStrand, axt->tName, 
+		axt->qStart, axt->qEnd, axt->tStart, axt->tEnd,
+		axt->score);
+	}
+    }
 asTreeFree(&tree);
-
 slFreeList(&blockList);
 }
 
