2dec6e5b14daddf3cc1aa5b2a6677b9fb60957e2
kent <kent>
2002-11-09 19:18:37 +0000

    Testing faSpeedReadMixed.

--- src/hg/oneShot/freen/freen.c
+++ src/hg/oneShot/freen/freen.c
@@ -3,6 +3,7 @@
 #include "linefile.h"
 #include "obscure.h"
 #include "dlist.h"
+#include "fa.h"
 
 void usage()
 /* Print usage and exit. */
@@ -10,43 +11,13 @@ void usage()
 errAbort("usage: freen count");
 }
 
-struct number
-/* A list of numbers. */
-    {
-    struct numList *next;
-    int number;
-    };
-
-int numberCmp(const void *va, const void *vb)
-/* Compare to sort based on query. */
-{
-const struct number *a = *((struct number **)va);
-const struct number *b = *((struct number **)vb);
-return a->number - b->number;
-}
-
-
 void freen(char *s)
 /* Print status code. */
 {
-int count = atoi(s);
-int i;
-struct number *num;
-struct dlList *list = newDlList();
-struct dlNode *node;
-
-for (i=0; i<count; ++i)
-    {
-    AllocVar(num);
-    num->number = rand();
-    dlAddValTail(list, num);
-    }
-dlSort(list, numberCmp);
-for (node = list->head; !dlEnd(node); node = node->next)
-    {
-    num = node->val;
-    printf("%d\n", num->number);
-    }
+struct lineFile *lf = lineFileOpen(s, TRUE);
+static struct dnaSeq seq;
+faMixedSpeedReadNext(lf, &seq.dna, &seq.size, &seq.name);
+printf("%s has %d bases in first seq\n", s, seq.size);
 }
 
 
