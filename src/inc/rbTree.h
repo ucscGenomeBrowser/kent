ed27c5ebf6a187c186f18ec6fa9a22b14d262285
kent <kent>
2002-11-11 22:43:25 +0000

    Added in rbTree scavenged from net. Very nice implementation.

---
+++ src/inc/rbTree.h
@@ -0,0 +1,66 @@
+/* rbTree - rbTreeRed-rbTreeBlack Tree - a type of binary tree which 
+ * automatically keeps relatively balanced during
+ * inserts and deletions.
+ *   original author: Shane Saunders
+ *   adapted into local conventions: Jim Kent
+ */
+#ifndef RBTREE_H
+#define RBTREE_H
+
+typedef enum {rbTreeRed,rbTreeBlack} rbTreeColor;
+
+
+/* Structure type for nodes in the red-black tree. */
+struct rbTreeNode 
+    {
+    struct rbTreeNode *left, *right;		/* Children. */
+    rbTreeColor color;				/* Heart of algorithm. */
+    void *item;					/* Item stored in tree */
+    };
+
+/* Structure type for the red-black tree. */
+struct rbTree 
+    {
+    struct rbTreeNode *root;			/* Root of tree */
+    int n;					/* Number of items in tree. */
+    int (* compare)(const void *, const void *);/* Comparison function */
+    struct rbTreeNode **stack;                  /* Ancestor stack. */
+    struct lm *lm;	                        /* Local memory pool. */
+    struct rbTreeNode *freeList;		/* List of nodes to reuse. */
+    };
+
+struct rbTree *rbTreeNew(int (*compare)(const void *, const void *));
+/* Allocates space for a red-black tree and returns a pointer
+ * to it.  The function compare compares they keys of two items, and returns a
+ * negative, zero, or positive integer depending on whether the first item is
+ * less than, equal to, or greater than the second. */
+
+void rbTreeFree(struct rbTree **pTree);
+/* Frees space used by the red-black tree pointed to by t. */
+
+void *rbTreeAdd(struct rbTree *t, void *item);
+/* Inserts an item into the red-black tree pointed to by t,
+ * according the the value its key.  The key of an item in the red-black
+ * tree must be unique among items in the tree.  If an item with the same key
+ * already exists in the tree, a pointer to that item is returned.  Otherwise,
+ * NULL is returned, indicating insertion was successful.
+ */
+
+void *rbTreeFind(struct rbTree *t, void *item);
+/* Find an item in the red-black tree with the same key as the
+ * item pointed to by `item'.  Returns a pointer to the item found, or NULL
+ * if no item was found.
+ */
+
+void *rbTreeRemove(struct rbTree *t, void *item);
+/* rbTreeRemove() - Delete an item in the red-black tree with the same key as
+ * the item pointed to by `item'.  Returns a pointer to the  deleted item,
+ * and NULL if no item was found.
+ */
+
+void rbTreeDump(struct rbTree *tree, FILE *f, 
+	void (*dumpItem)(const void *item, FILE *f));
+/* Dump out rb tree to file, mostly for debugging. */
+
+#endif /* RBTREE_H */
+
