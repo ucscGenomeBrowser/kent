e21fafad0cd1ac0fe1f36f9f17fb5a9860878f76
kent <kent>
2002-11-14 06:32:15 +0000

    Fixed some minus strand bugs,  and cope with the case when the only part of a chain that matches a gap, is a gap in the chain as well.

--- src/hg/mouseStuff/chainNet/chainNet.c
+++ src/hg/mouseStuff/chainNet/chainNet.c
@@ -10,6 +10,8 @@
 int minSpace = 25;	/* Minimum gap size to fill. */
 int minScore = 2000;	/* Minimum chain score to look at. */
 
+boolean uglier = FALSE;
+
 void usage()
 /* Explain usage and exit. */
 {
@@ -35,7 +37,7 @@ struct fill
 /* Some alignments that hopefully fill some gaps. */
     {
     struct fill *next;	   /* Next fill in list. */
-    int start,end;	   /* Range covered. */
+    int start,end;	   /* Range covered, always plus strand. */
     struct gap *gapList;   /* List of internal gaps. */
     struct chain *chain;   /* Alignment chain (not all is necessarily used) */
     };
@@ -44,7 +46,7 @@ struct space
 /* A part of a gap. */
     {
     struct gap *gap;    /* The actual gap. */
-    int start, end;	/* Portion of gap covered. */
+    int start, end;	/* Portion of gap covered, always plus strand. */
     };
 
 struct chrom
@@ -71,6 +73,16 @@ int fillCmpStart(const void *va, const void *vb)
 {
 const struct fill *a = *((struct fill **)va);
 const struct fill *b = *((struct fill **)vb);
+return a->start - b->start;
+}
+
+int cmpSpaceRef(const void *va, const void *vb)
+/* Compare to sort based on start. */
+{
+const struct slRef *ra = *((struct slRef **)va);
+const struct slRef *rb = *((struct slRef **)vb);
+struct space *a = ra->val;
+struct space *b = rb->val;
 return a->start - b->start;
 }
 
@@ -130,21 +142,19 @@ struct gap *addGap(struct chrom *chrom, char strand,
 	int minStart, int maxEnd, int start, int end)
 /* Add gap to chromosome. */
 {
-struct gap *gap = NULL;
+struct gap *gap;
+struct space *space;
 if (strand == '-')
     reverseIntRange(&start, &end, chrom->size);
-if (start < minStart) start = minStart;
-if (end > maxEnd) end = maxEnd;
-if (end - start >= minSpace)
-    {
-    struct space *space;
-    AllocVar(gap);
-    AllocVar(space);
-    space->gap = gap;
-    space->start = gap->start = start;
-    space->end = gap->end = end;
-    rbTreeAdd(chrom->spaces, space);
-    }
+if (start <= minStart) return NULL;
+if (end >= maxEnd) return NULL;
+if (end - start < minSpace) return NULL;
+AllocVar(gap);
+AllocVar(space);
+space->gap = gap;
+space->start = gap->start = start;
+space->end = gap->end = end;
+rbTreeAdd(chrom->spaces, space);
 return gap;
 }
 
@@ -166,7 +176,7 @@ while (lineFileRow(lf, row))
     hashAddSaveName(hash, name, chrom, &chrom->name);
     chrom->size = lineFileNeedNum(lf, row, 1);
     chrom->spaces = rbTreeNew(spaceCmp);
-    chrom->root = addGap(chrom, '+', 0, chrom->size, 0, chrom->size);
+    chrom->root = addGap(chrom, '+', -1, chrom->size+1, 0, chrom->size);
     }
 lineFileClose(&lf);
 slReverse(&chromList);
@@ -174,16 +184,60 @@ slReverse(&chromList);
 *retList = chromList;
 }
 
+boolean innerBounds(struct chain *chain, boolean isQ, 
+	int inStart, int inEnd, int *retStart, int *retEnd)
+/* Return the portion of chain which is between inStart and
+ * inEnd.  Only considers blocks inbetween inStart and inEnd.  
+ * Return NULL if chain has no blocks between these. */
+{
+struct boxIn *b;
+int start = BIGNUM, end = -BIGNUM;
+char strand = (isQ ? chain->qStrand : '+');
+boolean isRev = (strand == '-');
+if (isRev)
+    reverseIntRange(&inStart, &inEnd, chain->qSize);
+for (b = chain->blockList; b != NULL; b = b->next)
+    {
+    int s, e;
+    if (isQ)
+       {
+       s = b->qStart;
+       e = b->qEnd;
+       }
+    else
+       {
+       s = b->tStart;
+       e = b->tEnd;
+       }
+    if (e <= inStart)
+        continue;
+    if (s >= inEnd)
+        break;
+    if (s < inStart) s = inStart;
+    if (e > inEnd) s = inEnd;
+    if (start > s) start = s;
+    if (end < e) end = e;
+    }
+if (start > end)
+    return FALSE;
+if (isRev)
+    reverseIntRange(&start, &end, chain->qSize);
+*retStart = start;
+*retEnd = end;
+return TRUE;
+}
+
 struct fill *fillSpace(struct chrom *chrom, struct space *space, 
-	struct chain *chain, int chainStart, int chainEnd)
+	struct chain *chain, boolean isQ)
 /* Fill in space with chain, remove existing space from chrom,
  * and add smaller spaces on either side if big enough. */
 {
 struct fill *fill;
-int s = max(chainStart, space->start);
-int e = min(chainEnd, space->end);
+int s, e;
 struct space *lSpace, *rSpace;
 
+if (!innerBounds(chain, isQ, space->start, space->end, &s, &e))
+    return NULL;
 assert(s < e);
 AllocVar(fill);
 fill->start = s;
@@ -237,25 +291,45 @@ struct slRef *spaceList;
 struct slRef *ref;
 struct boxIn *block, *nextBlock;
 struct gap *gap;
+boolean isRev = (chain->qStrand == '-'); 
+int qStart = chain->qStart, qEnd = chain->qEnd;
 
-spaceList = findSpaces(qChrom->spaces,chain->qStart,chain->qEnd);
+if (isRev)
+    reverseIntRange(&qStart, &qEnd, chain->qSize);
+spaceList = findSpaces(qChrom->spaces,qStart,qEnd);
+#ifdef DEBUG
+if (uglier) uglyf("add chain on q\n");
+if (uglier)
+    {
+    struct slRef *fullSpaceList = findSpaces(qChrom->spaces, 0, qChrom->size);
+    struct slRef *ref;
+    slSort(&fullSpaceList, cmpSpaceRef);
+    for (ref = fullSpaceList; ref != NULL; ref = ref->next)
+        {
+	struct space *space = ref->val;
+	uglyf("  %d-%d\n", space->start, space->end);
+	}
+    }
+#endif /* DEBUG */
 uglyf("Got %d qSpaces\n", slCount(spaceList));
 for (ref = spaceList; ref != NULL; ref = ref->next)
     {
     struct space *space = ref->val;
     struct fill *fill;
-    fill = fillSpace(qChrom, space, chain, chain->qStart, chain->qEnd);
-    for (block = chain->blockList; ; block = nextBlock)
-        {
-	nextBlock = block->next;
-	if (nextBlock == NULL)
-	    break;
-	if ((gap = addGap(qChrom, chain->qStrand, space->start, space->end, block->qEnd, nextBlock->qStart)) != NULL)
+    if ((fill = fillSpace(qChrom, space, chain, TRUE)) != NULL)
+	{
+	for (block = chain->blockList; ; block = nextBlock)
 	    {
-	    slAddHead(&fill->gapList, gap);
+	    nextBlock = block->next;
+	    if (nextBlock == NULL)
+		break;
+	    if ((gap = addGap(qChrom, chain->qStrand, space->start, space->end, block->qEnd, nextBlock->qStart)) != NULL)
+		{
+		slAddHead(&fill->gapList, gap);
+		}
 	    }
+	freez(&ref->val);
 	}
-    freez(&ref->val);
     }
 slFreeList(&spaceList);
 
@@ -265,18 +339,20 @@ for (ref = spaceList; ref != NULL; ref = ref->next)
     {
     struct space *space = ref->val;
     struct fill *fill;
-    fill = fillSpace(tChrom, space, chain, chain->tStart, chain->tEnd);
-    for (block = chain->blockList; ; block = nextBlock)
-        {
-	nextBlock = block->next;
-	if (nextBlock == NULL)
-	    break;
-	if ((gap = addGap(tChrom, '+', space->start, space->end, block->tEnd, nextBlock->tStart)) != NULL)
+    if ((fill = fillSpace(tChrom, space, chain, FALSE)) != NULL)
+	{
+	for (block = chain->blockList; ; block = nextBlock)
 	    {
-	    slAddHead(&fill->gapList, gap);
+	    nextBlock = block->next;
+	    if (nextBlock == NULL)
+		break;
+	    if ((gap = addGap(tChrom, '+', space->start, space->end, block->tEnd, nextBlock->tStart)) != NULL)
+		{
+		slAddHead(&fill->gapList, gap);
+		}
 	    }
+	freez(&ref->val);
 	}
-    freez(&ref->val);
     }
 slFreeList(&spaceList);
 }
@@ -299,25 +375,107 @@ for (fill = gap->fillList; fill != NULL; fill = fill->next)
 --rOutDepth;
 }
 
+static void qClipChainOut(FILE *f, struct chain *chain, 
+	int clipStart, int clipEnd)
+/* Output subsection of chain bounded by clipStart/clipEnd on q coordinates. 
+ * clipStart/clipEnd are always in plus strand coordinates. */
+{
+boolean isRev = (chain->qStrand == '-');
+int tMin = BIGNUM, tMax = -BIGNUM;
+int qMin = BIGNUM, qMax = -BIGNUM;
+struct boxIn *b;
+if (isRev)
+    reverseIntRange(&clipStart, &clipEnd, chain->qSize);
+for (b = chain->blockList; b != NULL; b = b->next)
+    {
+    int qs, qe, ts, te;	/* Clipped bounds of block */
+    if ((qe = b->qEnd) <= clipStart)
+        continue;
+    if ((qs = b->qStart) >= clipEnd)
+        break;
+    ts = b->tStart;
+    te = b->tEnd;
+    if (qs < clipStart)
+        {
+	ts += (clipStart - qs);
+	qs = clipStart;
+	}
+    if (qe > clipEnd)
+        {
+	te  -= (clipEnd - qe);
+	qe = clipEnd;
+	}
+    if (qMin > qs) qMin = qs;
+    if (qMax < qe) qMax = qe;
+    if (tMin > ts) tMin = ts;
+    if (tMax < te) tMax = te;
+    }
+if (tMin < tMax)
+    {
+    if (isRev)
+        reverseIntRange(&qMin, &qMax, chain->qSize);
+    spaceOut(f, rOutDepth);
+    fprintf(f, "fill %d %d %d %s %c %d %d %d\n", qMin, qMax, qMax-qMin, 
+    	chain->tName, chain->qStrand, tMin, tMax, tMax-tMin);
+    }
+}
+
+static void tClipChainOut(FILE *f, struct chain *chain, 
+	int clipStart, int clipEnd)
+/* Output subsection of chain bounded by clipStart/clipEnd on t coordinates. */
+{
+boolean isRev = (chain->qStrand == '-');
+int tMin = BIGNUM, tMax = -BIGNUM;
+int qMin = BIGNUM, qMax = -BIGNUM;
+struct boxIn *b;
+for (b = chain->blockList; b != NULL; b = b->next)
+    {
+    int qs, qe, ts, te;	/* Clipped bounds of block */
+    if ((te = b->tEnd) <= clipStart)
+        continue;
+    if ((ts = b->tStart) >= clipEnd)
+        break;
+    qs = b->qStart;
+    qe = b->qEnd;
+    if (ts < clipStart)
+        {
+	qs += (clipStart - ts);
+	ts = clipStart;
+	}
+    if (te > clipEnd)
+        {
+	qe  -= (clipEnd - te);
+	te = clipEnd;
+	}
+    if (qMin > qs) qMin = qs;
+    if (qMax < qe) qMax = qe;
+    if (tMin > ts) tMin = ts;
+    if (tMax < te) tMax = te;
+    }
+if (qMin < qMax)
+    {
+    if (isRev)
+        reverseIntRange(&qMin, &qMax, chain->qSize);
+    spaceOut(f, rOutDepth);
+    fprintf(f, "fill %d %d %d %s %c %d %d %d\n", tMin, tMax, tMax-tMin, 
+    	chain->qName, chain->qStrand, qMin, qMax, qMax-qMin);
+    }
+}
+
+
 static void rOutputFill(struct fill *fill, FILE *f)
 /* Recursively output fill and it's gaps. */
 {
 struct gap *gap;
 struct chain *chain = fill->chain;
 ++rOutDepth;
-spaceOut(f, rOutDepth);
-fprintf(f, "fill %d %d %d ", fill->start, fill->end, fill->end - fill->start);
 if (rOutQ)
     {
-    fprintf(f, "%s %c %d %d %d\n", chain->tName, chain->qStrand, chain->tStart,
-    	chain->tEnd, chain->tEnd - chain->tStart);
+    qClipChainOut(f, chain, fill->start, fill->end);
     }
 else
     {
-    int s = chain->qStart, e = chain->qEnd;
-    if (chain->qStrand == '-')
-        reverseIntRange(&s, &e, chain->qSize);
-    fprintf(f, "%s %c %d %d %d\n", chain->qName, chain->qStrand, s, e, e - s);
+    tClipChainOut(f, chain, fill->start, fill->end);
     }
 for (gap = fill->gapList; gap != NULL; gap = gap->next)
     rOutputGap(gap, f);
@@ -372,7 +530,7 @@ while ((chain = chainRead(lf)) != NULL)
     {
     if (chain->score < minScore) 
     	break;
-    uglyf("chain %d (%d els) %s %d-%d vs %s %d-%d\n", chain->score, slCount(chain->blockList), chain->tName, chain->tStart, chain->tEnd, chain->qName, chain->qStart, chain->qEnd);
+    uglyf("chain %f (%d els) %s %d-%d %c %s %d-%d\n", chain->score, slCount(chain->blockList), chain->tName, chain->tStart, chain->tEnd, chain->qStrand, chain->qName, chain->qStart, chain->qEnd);
     qChrom = hashMustFindVal(qHash, chain->qName);
     if (qChrom->size != chain->qSize)
         errAbort("%s is %d in %s but %d in %s", chain->qName, 
