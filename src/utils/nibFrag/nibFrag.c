2e9e42b15337a70b2836413aae93932382cc46c2
sugnet <sugnet>
2002-11-22 20:13:13 +0000

    Added a 'hardMask' option to get hardmasking out of mixed case nibs

--- src/utils/nibFrag/nibFrag.c
+++ src/utils/nibFrag/nibFrag.c
@@ -14,12 +14,13 @@ errAbort(
   "   nibFrag [options] file.nib start end strand out.fa\n"
   "options:\n"
   "   -masked - use lower case characters for masked-out bases\n"
+  "   -hardMasked - use 'N' characters for masked out bases\n"
   "   -upper - use uppper case characters for all bases\n"
   "   -name=name Use given name after '>' in output sequence\n"
   );
 }
 
-void nibFrag(int options, char *nibFile, int start, int end, char strand, char *faFile, int optUpper)
+void nibFrag(int options, char *nibFile, int start, int end, char strand, char *faFile, int optUpper, boolean hardMask)
 /* nibFrag - Extract part of a nib file as .fa. */
 {
 struct dnaSeq *seq;
@@ -36,6 +37,8 @@ if (strand == '-')
     reverseComplement(seq->dna, seq->size);
 if (optUpper == 1)
     touppers(seq->dna);
+if (hardMask)
+    lowerToN(seq->dna, seq->size);
 faWrite(faFile, optionVal("name", seq->name), seq->dna, seq->size);
 }
 
@@ -44,9 +47,22 @@ int main(int argc, char *argv[])
 {
 int options = 0;
 int optUpper = 0;
+boolean hardMask = FALSE;
 optionHash(&argc, argv);
+if(optionExists("masked") && optionExists("hardMasked"))
+    {
+    warn("\nError: Must choose 'masked' or 'hardMasked' but not both.\n");
+    usage();
+    }
 if (optionExists("masked"))
     options = NIB_MASK_MIXED;
+if(optionExists("hardMasked"))
+    {
+    /* setup to read out of nib in mixed case and then
+       convert all lower case to 'N' */
+    options = NIB_MASK_MIXED;
+    hardMask = TRUE;
+    }
 if (optionExists("upper"))
     optUpper = 1;
 
@@ -58,6 +74,6 @@ if (!isdigit(argv[2][0]) || !isdigit(argv[3][0]))
     {
     usage();
     }
-nibFrag(options, argv[1], atoi(argv[2]), atoi(argv[3]), argv[4][0], argv[5], optUpper);
+nibFrag(options, argv[1], atoi(argv[2]), atoi(argv[3]), argv[4][0], argv[5], optUpper, hardMask);
 return 0;
 }
