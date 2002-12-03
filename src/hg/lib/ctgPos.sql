9290c5f6aab2b160469806d72fa236a5fe653455
angie <angie>
2002-12-03 00:45:18 +0000

    Enlarged key size for contig to accommodate 12-char-long rat contig names.

--- src/hg/lib/ctgPos.sql
+++ src/hg/lib/ctgPos.sql
@@ -11,7 +11,7 @@ CREATE TABLE ctgPos (
     chromStart int unsigned not null,	# Start in chromosome
     chromEnd int unsigned not null,	# End in chromosome
               #Indices
-    PRIMARY KEY(contig(10)),
+    PRIMARY KEY(contig(12)),
     UNIQUE(chrom(16),chromStart),
     UNIQUE(chrom(16),chromEnd)
 );
