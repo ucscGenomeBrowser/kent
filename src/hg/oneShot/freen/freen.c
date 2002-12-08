9be8045dfb4a223014fff4b0655a33c51f71980f
kent <kent>
2002-12-08 11:47:34 +0000

    Further massaging my pet freen.

--- src/hg/oneShot/freen/freen.c
+++ src/hg/oneShot/freen/freen.c
@@ -1,23 +1,30 @@
 /* freen - My Pet Freen. */
 #include "common.h"
 #include "linefile.h"
-#include "obscure.h"
-#include "dlist.h"
-#include "fa.h"
+#include "vGfx.h"
 
 void usage()
 /* Print usage and exit. */
 {
-errAbort("usage: freen count");
+errAbort("usage: freen file");
 }
 
-void freen(char *s)
+void freen(char *fileName)
 /* Print status code. */
 {
-struct lineFile *lf = lineFileOpen(s, TRUE);
-static struct dnaSeq seq;
-faMixedSpeedReadNext(lf, &seq.dna, &seq.size, &seq.name);
-printf("%s has %d bases in first seq\n", s, seq.size);
+struct vGfx *vg = vgOpenPostScript(500, 500, fileName);
+// struct vGfx *vg = vgOpenGif(500, 500, fileName);
+MgFont *smallFont = mgSmallFont();
+MgFont *bigFont = mgLargeFont();
+int smallHeight = mgFontPixelHeight(smallFont);
+int bigHeight = mgFontPixelHeight(bigFont);
+int grayIx = vgFindColorIx(vg, 200, 150, 150);
+int blueIx = vgFindColorIx(vg, 150, 150, 200);
+vgBox(vg, 100, 100, 300, smallHeight, blueIx);
+vgText(vg, 100, 100, MG_BLACK, smallFont, "Help Small World");
+vgBox(vg, 100, 300, 300, bigHeight, blueIx);
+vgText(vg, 100, 300, MG_BLACK, bigFont, "Help BIG World");
+vgClose(&vg);
 }
 
 
