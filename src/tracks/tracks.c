58ec930d37660143390444cbda9c06b3ec4b9c23
kent <kent>
2002-12-10 04:04:38 +0000

    Moved in mgDrawRuler to here, since it's no longer in the library.

--- src/tracks/tracks.c
+++ src/tracks/tracks.c
@@ -1210,6 +1210,96 @@ if (!initted)
 return &tg;
 }
 
+static long figureTickSpan(long totalLength, int maxNumTicks)
+/* Figure out whether ticks on ruler should be 1, 5, 10, 50, 100, 500, 
+ * 1000, etc. units apart.  */
+{
+int roughTickLen = totalLength/maxNumTicks;
+int i;
+int tickLen = 1;
+
+for (i=0; i<9; ++i)
+    {
+    if (roughTickLen < tickLen)
+    	return tickLen;
+    tickLen *= 5;
+    if (roughTickLen < tickLen)
+	return tickLen;
+    tickLen *= 2;
+    }
+return 1000000000;
+}
+
+static void numLabelString(int num, char *label)
+/* Returns a numerical labeling string. */
+{
+char *sign = "";
+if (num < 0)
+    {
+    num = -num;
+    sign = "-";
+    }
+sprintf(label, "%s%d", sign, num);
+}
+
+void mgDrawRulerBumpText(struct memGfx *mg, int xOff, int yOff, 
+	int height, int width,
+        Color color, MgFont *font,
+        int startNum, int range, int bumpX, int bumpY)
+/* Draw a ruler inside the indicated part of mg with numbers that start at
+ * startNum and span range.  Bump text positions slightly. */
+{
+int tickSpan;
+int tickPos;
+double scale;
+int firstTick;
+int remainder;
+int end = startNum + range;
+int x;
+char tbuf[14];
+int numWid;
+int goodNumTicks;
+int niceNumTicks = width/35;
+
+numLabelString(startNum+range, tbuf);
+numWid = mgFontStringWidth(font, tbuf)+4+bumpX;
+goodNumTicks = width/numWid;
+if (goodNumTicks < 1) goodNumTicks = 1;
+if (goodNumTicks > niceNumTicks) goodNumTicks = niceNumTicks;
+
+tickSpan = figureTickSpan(range, goodNumTicks);
+
+scale = (double)width / range;
+
+firstTick = startNum + tickSpan;
+remainder = firstTick % tickSpan;
+firstTick -= remainder;
+for (tickPos=firstTick; tickPos<end; tickPos += tickSpan)
+    {
+    numLabelString(tickPos, tbuf);
+    numWid = mgFontStringWidth(font, tbuf)+4;
+    x = (int)((tickPos-startNum) * scale) + xOff;
+    mgDrawBox(mg, x, yOff, 1, height, color);
+    if (x - numWid >= xOff)
+        {
+        mgTextCentered(mg, x-numWid + bumpX, yOff + bumpY, numWid, 
+	    height, color, font, tbuf);
+        }
+    }
+}
+
+void mgDrawRuler(struct memGfx *mg, int xOff, int yOff, int height, int width,
+        Color color, MgFont *font,
+        int startNum, int range)
+/* Draw a ruler inside the indicated part of mg with numbers that start at
+ * startNum and span range.  */
+{
+mgDrawRulerBumpText(mg, xOff, yOff, height, width, color, font,
+    startNum, range, 0, 0);
+}
+
+
+
 void drawTracks(char *where, struct trackGroup *groupList, boolean compactDisplay)
 /* Draw tracks onto a gif in memory and save it. */
 {
