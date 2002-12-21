0463724ffad6a57517e7293061c628ce4105d6aa
kent <kent>
2002-12-21 21:43:32 +0000

    Removed currently unused levels 7 and 8 of net.

--- src/hg/hgTracks/netTrack.c
+++ src/hg/hgTracks/netTrack.c
@@ -21,7 +21,7 @@ struct netItem
     };
 
 static char *netClassNames[] =  {
-    "Level 1", "Level 2", "Level 3", "Level 4", "Level 5", "Level 6", "Level 7", "Level 8" };
+    "Level 1", "Level 2", "Level 3", "Level 4", "Level 5", "Level 6", };
 
 static struct netItem *makeNetItems()
 /* Make the levels for net alignment track. */
