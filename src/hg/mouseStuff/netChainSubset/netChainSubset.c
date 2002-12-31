a03102cc872e6dac65e14d5e15d55472274eebdf
kent <kent>
2002-12-31 21:42:34 +0000

    Seems to work.

--- src/hg/mouseStuff/netChainSubset/netChainSubset.c
+++ src/hg/mouseStuff/netChainSubset/netChainSubset.c
@@ -3,6 +3,8 @@
 #include "linefile.h"
 #include "hash.h"
 #include "options.h"
+#include "chain.h"
+#include "chainNet.h"
 
 void usage()
 /* Explain usage and exit. */
@@ -10,23 +12,113 @@ void usage()
 errAbort(
   "netChainSubset - Create chain file with subset of chains that appear in the net\n"
   "usage:\n"
-  "   netChainSubset XXX\n"
+  "   netChainSubset in.net in.chain out.chain\n"
   "options:\n"
-  "   -xxx=XXX\n"
+  "   -gapOut=gap.tab - Output gap sizes to file\n"
   );
 }
 
-void netChainSubset(char *XXX)
-/* netChainSubset - Create chain file with subset of chains that appear in the net. */
+struct hash *chainReadAll(char *fileName)
+/* Read chains into a hash keyed by id. */
 {
+char nameBuf[16];
+struct hash *hash = hashNew(18);
+struct chain *chain;
+struct lineFile *lf = lineFileOpen(fileName, TRUE);
+int count = 0;
+
+while ((chain = chainRead(lf)) != NULL)
+    {
+    sprintf(nameBuf, "%x", chain->id);
+    if (hashLookup(hash, nameBuf))
+        errAbort("Duplicate chain %d ending line %d of %s", 
+		chain->id, lf->lineIx, lf->fileName);
+    hashAdd(hash, nameBuf, chain);
+    ++count;
+    }
+lineFileClose(&lf);
+fprintf(stderr, "read %d chains in %s\n", count, fileName);
+return hash;
+}
+
+struct chain *chainLookup(struct hash *hash, int id)
+/* Find chain in hash. */
+{
+char nameBuf[16];
+sprintf(nameBuf, "%x", id);
+return hashMustFindVal(hash, nameBuf);
+}
+
+void gapWrite(struct chain *chain, FILE *f)
+/* Write gaps to simple two column file. */
+{
+struct boxIn *a, *b;
+a = chain->blockList;
+for (b = a->next; b != NULL; b = b->next)
+    {
+    fprintf(f, "%d\t%d\n", b->tStart - a->tEnd, b->qStart - a->qEnd);
+    a = b;
+    }
+}
+
+void convertFill(struct cnFill *fill, 
+	struct chain *chain, FILE *f, FILE *gapFile)
+/* Convert subset of chain as defined by fill to axt. */
+{
+struct chain *subChain, *chainToFree;
+
+chainSubsetOnT(chain, fill->tStart, fill->tStart + fill->tSize, 
+	&subChain, &chainToFree);
+assert(subChain != NULL);
+chainWrite(subChain, f);
+if (gapFile != NULL)
+    gapWrite(subChain, gapFile);
+chainFree(&chainToFree);
+}
+
+void rConvert(struct cnFill *fillList, 
+	struct hash *chainHash, FILE *f, FILE *gapFile)
+/* Recursively output chains in net as axt. */
+{
+struct cnFill *fill;
+for (fill = fillList; fill != NULL; fill = fill->next)
+    {
+    if (fill->chainId)
+        convertFill(fill, 
+		chainLookup(chainHash, fill->chainId), f, gapFile);
+    if (fill->children)
+        rConvert(fill->children, chainHash, f, gapFile);
+    }
+}
+
+void netChainSubset(char *netIn, char *chainIn, char *chainOut)
+/* netChainSubset - Create chain file with subset of *
+ * chains that appear in the net. */
+{
+struct hash *chainHash;
+struct chainNet *net;
+struct lineFile *lf = lineFileOpen(netIn, TRUE);
+FILE *f = mustOpen(chainOut, "w");
+char *gapFileName = optionVal("gapOut", NULL);
+FILE *gapFile = NULL;
+
+if (gapFileName)
+    gapFile = mustOpen(gapFileName, "w");
+chainHash = chainReadAll(chainIn);
+while ((net = chainNetRead(lf)) != NULL)
+    {
+    fprintf(stderr, "Processing %s\n", net->name);
+    rConvert(net->fillList, chainHash, f, gapFile);
+    chainNetFree(&net);
+    }
 }
 
 int main(int argc, char *argv[])
 /* Process command line. */
 {
 optionHash(&argc, argv);
-if (argc != 2)
+if (argc != 4)
     usage();
-netChainSubset(argv[1]);
+netChainSubset(argv[1], argv[2], argv[3]);
 return 0;
 }
--- src/hg/mouseStuff/netSplit/netSplit.c
+++ src/hg/mouseStuff/netSplit/netSplit.c
@@ -3,6 +3,8 @@
 #include "linefile.h"
 #include "hash.h"
 #include "options.h"
+#include "portable.h"
+#include "chainNet.h"
 
 void usage()
 /* Explain usage and exit. */
@@ -10,23 +12,38 @@ void usage()
 errAbort(
   "netSplit - Split a genome net file into chromosome net files\n"
   "usage:\n"
-  "   netSplit XXX\n"
+  "   netSplit in.net outDir\n"
   "options:\n"
   "   -xxx=XXX\n"
   );
 }
 
-void netSplit(char *XXX)
+void netSplit(char *inNet, char *outDir)
 /* netSplit - Split a genome net file into chromosome net files. */
 {
+char fileName[512];
+struct lineFile *lf = lineFileOpen(inNet, TRUE);
+FILE *f;
+struct chainNet *net;
+
+makeDir(outDir);
+while ((net = chainNetRead(lf)) != NULL)
+    {
+    sprintf(fileName, "%s/%s.net", outDir, net->name);
+    f = mustOpen(fileName, "w");
+    printf("Writing %s\n", fileName);
+    chainNetWrite(net, f);
+    carefulClose(&f);
+    chainNetFree(&net);
+    }
 }
 
 int main(int argc, char *argv[])
 /* Process command line. */
 {
 optionHash(&argc, argv);
-if (argc != 2)
+if (argc != 3)
     usage();
-netSplit(argv[1]);
+netSplit(argv[1], argv[2]);
 return 0;
 }
