8a2a4884b408a22c95e1ff8097d8989c5a23e26c
kent <kent>
2003-01-02 19:31:30 +0000

    Seems to work, much faster than the avg in perl.

--- src/utils/ave/ave.c
+++ src/utils/ave/ave.c
@@ -4,21 +4,83 @@
 #include "hash.h"
 #include "options.h"
 
+int col = 1;
+
 void usage()
 /* Explain usage and exit. */
 {
 errAbort(
   "ave - Compute average and basic stats\n"
   "usage:\n"
-  "   ave XXX\n"
+  "   ave file\n"
   "options:\n"
-  "   -xxx=XXX\n"
+  "   -col=N Which column to use.  Default 1\n"
   );
 }
 
-void ave(char *XXX)
+int cmpInt(const void *va, const void *vb)
+/* Compare two slNames. */
+{
+const int *a = va;
+const int *b = vb;
+return *a - *b;
+}
+
+void showStats(int *array, int count)
+/* Compute stats on sorted array */
+{
+int val, minVal = BIGNUM, maxVal = -BIGNUM;
+double total = 0, average;
+int i;
+double oneVar, totalVar = 0;
+
+for (i=0; i<count; ++i)
+    {
+    val = array[i];
+    if (minVal > val) minVal = val;
+    if (maxVal < val) maxVal = val;
+    total += val;
+    }
+average = total/count;
+
+printf("median %d\n", array[count/2]);
+printf("average %f\n", average);
+printf("min %d\n", minVal);
+printf("max %d\n", maxVal);
+printf("count %d\n", count);
+printf("total %f\n", total);
+
+for (i=0; i<count; ++i)
+    {
+    val = array[i];
+    oneVar = (average-val);
+    totalVar += oneVar*oneVar;
+    }
+printf("standard deviation %f\n", sqrt(totalVar)/count);
+}
+
+void ave(char *fileName)
 /* ave - Compute average and basic stats. */
 {
+int count = 0, alloc = 1024;
+int *array;
+struct lineFile *lf = lineFileOpen(fileName, TRUE);
+char *words[128];
+int wordCount;
+int wordIx = col-1;
+
+AllocArray(array, alloc);
+while ((wordCount = lineFileChop(lf, words)) > 0)
+    {
+    if (count >= alloc)
+        {
+	alloc <<= 1;
+	ExpandArray(array, count, alloc);
+	}
+    array[count++] = lineFileNeedNum(lf, words, wordIx);
+    }
+qsort(array, count, sizeof(array[0]), cmpInt);
+showStats(array, count);
 }
 
 int main(int argc, char *argv[])
@@ -27,6 +89,7 @@ int main(int argc, char *argv[])
 optionHash(&argc, argv);
 if (argc != 2)
     usage();
+col = optionInt("col", col);
 ave(argv[1]);
 return 0;
 }
