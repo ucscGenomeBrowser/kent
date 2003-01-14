be09dd0c03f8fbd79008a40faacb39d13f5a2404
ctrople <ctrople>
2003-01-14 03:33:37 +0000

    *** empty log message ***

--- src/hg/gpcrParser/gpcrParser.c
+++ src/hg/gpcrParser/gpcrParser.c
@@ -14,14 +14,15 @@ void usage(char **argv);
 
 void usage(char **argv)
 {
-   if(argv[1] == NULL || argv[2] == NULL) 
+   if(argv[1] == NULL) 
         {
         errAbort(
           "gpcrParser - Create xml files for gpcr snakeplots.\n"
-          "usage:\n   gpcrParser TMHMMresult.htm path\n"
+          "usage:\n   gpcrParser TMHMMresult.htm [path]\n"
           "    The first parameter is the html output from www.cbs.dtu.dk\n"
-          "    Path is the working directory holding the *.seq files needed to\n"
+          "    Path is the working directory holding the seq files needed to\n"
           "    create the xml files. The xml files will be created in the path location.\n"
+          "    If no path location is entered, the seq files should be in your cwd\n"
            );
          }
 }
@@ -41,25 +42,26 @@ carefulClose(&infile);
 }
 
 void finishXMLFile(FILE *outfile, char *proteinID, int seqLength)
-/** Add the closing lines to the filed*/
+/** Add the closing lines to the file */
 {
 fprintf(outfile, 
-   "\t\t\n</secondary-structure>\n\n\t</protein>\n\n
-    \t<diagram>\n\n\t\t<colors>\n\n
-    \t\t\t<define color=\"white\" red=\"255\" green=\"255\" blue=\"255\"/>\n\n
-    \t\t\t<define color=\"red\" red=\"255\" green=\"0\" blue=\"0\"/>\n\n
-    \t\t\t<color-scheme name=\"GPCRDB\">\n\n
-    \t\t\t\t<residue-background color=\"white\"/>\n\n\t\t\t</color-scheme>\n\n
-    \t\t\t<residue-color position=\"20\" color=\"white\"/>\n\n
-    \t\t\t<residue-color position=\"21\" color=\"red\"/>\n\n
-    \t\t</colors>\n\n\t\t<elipses>\n\n
-    \t\t\t<exclude residueRange=\"1-%d\" />\n\n
-    \t\t\t<include residueRange=\"1-1\"/>\n\n\t\t</elipses>\n\n
-    \t\t<output>\n\n\t\t\t<image format=\"GIF\" filename=\"%s.gif\"/>\n\n
-    \t\t\t<page template=\"H:\\dev\\crover\\nrdb\\sample-template.xsl\" filename=\"gpcr-rbdg-out.xml\" diagram-name=\"gpcr\"/>\n\n
-    \t\t\t<image-map filename=\"%s.map\"/>\n\n\t\t</output>\n\n
-    \t</diagram>\n\n</rbde-diagram>", 
-    seqLength-1, proteinID, proteinID);
+   "\t\t\n</secondary-structure>\n\n\t</protein>\n\n\t<diagram>\n\n\t\t<colors>\n\n"
+   "\t\t\t<define color=\"white\" red=\"255\" green=\"255\" blue=\"255\"/>\n\n"
+   "\t\t\t<define color=\"red\" red=\"255\" green=\"0\" blue=\"0\"/>\n\n"
+   "\t\t\t<color-scheme name=\"GPCRDB\">\n\n"
+   "\t\t\t\t<residue-background color=\"white\"/>\n\n\t\t\t</color-scheme>\n\n"
+   "\t\t\t<residue-color position=\"20\" color=\"white\"/>\n\n"
+   "\t\t\t<residue-color position=\"21\" color=\"red\"/>\n\n"
+   "\t\t</colors>\n\n\t\t<elipses>\n\n"
+   "\t\t\t<exclude residueRange=\"1-%d\" />\n\n"
+   "\t\t\t<include residueRange=\"1-1\"/>\n\n\t\t</elipses>\n\n"
+   "\t\t<output>\n\n\t\t\t<image format=\"GIF\" filename=\"%s.gif\"/>\n\n"
+   "\t\t\t<page template=\"H:\\dev\\crover\\nrdb\\sample-template.xsl\""
+   "filename=\"gpcr-rbdg-out.xml\" diagram-name=\"gpcr\"/>\n\n"
+   "\t\t\t<image-map filename=\"%s.map\"/>\n\n\t\t</output>\n\n"
+   "\t</diagram>\n\n</rbde-diagram>", 
+    seqLength-1, proteinID, proteinID
+       );
 }
 
 void addCoordinatesToXMLFile(FILE *outfile, int start, int end, char* mapTo, int count)
@@ -84,12 +86,15 @@ void populateXMLFile(FILE *outfile, int tmNumber, char *proteinID, char *path)
 /** Adds the number of TM helices and sequence the the xml file */
 {
 struct dyString *filename = newDyString(128);
-dyStringPrintf(filename, "%s%s.seq", path, proteinID);
+    if(path == NULL)
+        dyStringPrintf(filename, "%s.seq", proteinID);
+    else
+        dyStringPrintf(filename, "%s%s.seq", path, proteinID);
 fprintf(outfile, 
-    "%d\" id-prefix=\"TM\" nterm-id=\"N-term\" cterm-id=\"C-term\" direction=\"down\" />\n\n
-    \t</diagram-layout>\n\n
-    \t<protein>\n\n\t\t<name>%s</name>\n\n\t\t<residue-codes>\n\n", 
-    tmNumber, proteinID);
+    "%d\" id-prefix=\"TM\" nterm-id=\"N-term\" cterm-id=\"C-term\" direction=\"down\" />\n\n"
+    "\t</diagram-layout>\n\n\t<protein>\n\n\t\t<name>%s</name>\n\n\t\t<residue-codes>\n\n", 
+    tmNumber, proteinID
+       );
 catSeqFile(outfile, filename->string);
 fprintf(outfile, "\n\n\t\t</residue-codes>\n\n\t\t<secondary-structure>\n\n");
 freeDyString(&filename);
@@ -99,13 +104,17 @@ FILE* createXMLFile(char* proteinID, char *path)
 {
 FILE* outfile;
 struct dyString *filename = newDyString(128);
-dyStringPrintf(filename, "%s%s.xml", path, proteinID);
+    if(path == NULL)
+        dyStringPrintf(filename, "%s.xml", proteinID);
+    else
+        dyStringPrintf(filename, "%s%s.xml", path, proteinID);
 outfile = mustOpen(filename->string, "w");
 fprintf(outfile, 
-    "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\n\n
-    <rbde-diagram xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:noNamespaceSchemaLocation=\"H:\\dev\\crover\\xml\\xsd\\residue-based-diagram.xsd\">\n\n
-    \t<diagram-layout>\n\n
-    \t\t<tm-bundle tm-number=\"");
+    "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\n\n"
+    "<rbde-diagram xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\""
+    " xsi:noNamespaceSchemaLocation=\"H:\\dev\\crover\\xml\\xsd\\residue-based-diagram.xsd\">\n\n"
+    "\t<diagram-layout>\n\n\t\t<tm-bundle tm-number=\""
+       );
 freeDyString(&filename);
 return outfile;
 }
@@ -125,7 +134,6 @@ while( ( line  = readLine(infile) ) != NULL )
                 {  /*create a new xml file*/
                 token = nextWord(&line);
        	        dyStringAppend(proteinID, token);
-                outfile = createXMLFile(proteinID->string, path);
                 token = lastWordInLine(line);
                 seqLength = atoi(token);
                 }
@@ -133,7 +141,11 @@ while( ( line  = readLine(infile) ) != NULL )
                 {   /*grab the number of transmembrane helices*/
                 token = lastWordInLine(line);
                 tmNumber = atoi(token);
-                populateXMLFile(outfile, tmNumber, proteinID->string, path);
+                    if(tmNumber > 0)
+                        {
+                        outfile = createXMLFile(proteinID->string, path);
+                        populateXMLFile(outfile, tmNumber, proteinID->string, path);
+                        }
                 }
             if( sameString(token,"</PRE>") )
                 {  /*close the xml file*/
@@ -160,7 +172,6 @@ while( ( line  = readLine(infile) ) != NULL )
                         }
                 }
         } /*end inner while*/
-   /* freeMem(line);*/
     }   /*end outer while*/
     freeMem(line);
     freeDyString(&proteinID);
@@ -171,9 +182,12 @@ int main(int argc, char** argv)
 FILE *infile;
 struct dyString *path = newDyString(128);
 usage(argv);
-dyStringAppend(path, argv[2]);
-    if(!endsWith(path->string, "/"))
-       dyStringAppend(path, "/");
+    if(argv[2] != NULL)
+        {
+        dyStringAppend(path, argv[2]);
+        if(!endsWith(path->string, "/"))
+            dyStringAppend(path, "/");
+        }
 infile = mustOpen(argv[1], "r");
 getTransFromFile(infile, path->string);
 carefulClose(&infile);
