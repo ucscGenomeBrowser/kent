3e72a5ba25a956aa681ccf970e864318ab9a2b3a
markd <markd>
2003-01-17 17:01:20 +0000

    added support for using nib subsequences

--- src/hg/pslIntronsOnly/pslIntronsOnly.c
+++ src/hg/pslIntronsOnly/pslIntronsOnly.c
@@ -13,102 +13,147 @@ void usage()
 errAbort(
   "pslIntronsOnly - Filter psl files to only include those with introns\n"
   "usage:\n"
-  "   pslIntronsOnly in.psl in.nib|in.fa out.psl\n");
+  "   pslIntronsOnly in.psl genoFile out.psl\n"
+  "\n"
+  "The genoFile can be a fasta file, a chromosome nib, or a nib subrange.\n"
+  "Subranges of nib files may specified using the syntax:\n"
+  "   /path/file.nib:seqid:start-end\n"
+  "or\n"
+  "   /path/file.nib:start-end\n"
+  "With the second form, a sequence id of file:start-end will be used.\n");
 }
 
-void nibPslIntronsOnly(char *inPslName, char *inNibName, char *outPslName)
-/* Filter psl files to only include those with introns, with sequence from
- * nib */
-{
-struct dnaSeq *nibSeq = NULL;
+/* cache of current sequence */
+struct dnaSeq *genoSeqCache = NULL;
+
+/* input file */
+char genoFileName[PATH_LEN];
+
+/* data for seqments or subrange nibs */
+boolean nibSubrange = FALSE;
+FILE *nibFile = NULL;
 int nibStart = 0, nibEnd = 0, nibSize = 0;
 int nibSegTargetSize = 32*1024*1024;
-int nibSegSize = 0, pslSegSize = 0;
-struct lineFile *lf = NULL;
-FILE *nibFile = NULL, *outFile = NULL;
-struct psl *psl;
-int count = 0, intronCount = 0;
 
-lf = pslFileOpen(inPslName);
-nibOpenVerify(inNibName, &nibFile, &nibSize);
-outFile = mustOpen(outPslName, "w");
-while ((psl = pslNext(lf)) != NULL)
+/* data for fasta sequences */
+struct dnaSeq *faAllSeqs = NULL;
+
+struct dnaSeq* chromNibGet(char *tName, int tStart, int tEnd, int *tOffsetPtr)
+/* Get the DNA sequence from a chromosome nib */
+{
+int pslSegSize = tEnd - tStart;
+if (rangeIntersection(tStart, tEnd, nibStart, nibEnd) < pslSegSize)
     {
-    ++count;
-    pslSegSize = psl->tEnd - psl->tStart;
-    if (rangeIntersection(psl->tStart, psl->tEnd, nibStart, nibEnd) < pslSegSize)
-        {
-	freeDnaSeq(&nibSeq);
-	nibStart = psl->tStart;
-	nibEnd = nibStart + nibSegTargetSize;
-	if (psl->tEnd > nibEnd) nibEnd = psl->tEnd;
-	if (nibEnd > nibSize) nibEnd = nibSize;
-	uglyf("Processing %s:%d-%d\n", inNibName, nibStart, nibEnd);
-	nibSeq = nibLdPart(inNibName, nibFile, nibSize, nibStart, nibEnd - nibStart);
-	}
-    if (pslHasIntron(psl, nibSeq, nibStart))
-        {
-	++intronCount;
-	pslTabOut(psl, outFile);
-	}
-    pslFree(&psl);
+    freeDnaSeq(&genoSeqCache);
+    nibStart = tStart;
+    nibEnd = nibStart + nibSegTargetSize;
+    if (tEnd > nibEnd)
+        nibEnd = tEnd;
+    if (nibEnd > nibSize)
+        nibEnd = nibSize;
+    genoSeqCache = nibLdPart(genoFileName, nibFile, nibSize, nibStart,
+                             nibEnd - nibStart);
+    tolowers(genoSeqCache->dna);
     }
-carefulClose(&outFile);
-carefulClose(&nibFile);
-lineFileClose(&lf);
-printf("%d of %d in %s have introns\n", intronCount, count, inPslName);
+*tOffsetPtr = nibStart;
+return genoSeqCache;
 }
 
-struct dnaSeq *findFaDna(char *id, struct dnaSeq *allSeqs, char *inFaName)
-/* find the specified sequence id in a list of sequences from a fasta file */
+struct dnaSeq* subNibGet(char *tName, int tStart, int tEnd, int *tOffsetPtr)
+/* Get the DNA sequence from a subrange nib */
 {
-struct dnaSeq *seq;
-for (seq = allSeqs; seq != NULL; seq = seq->next)
-    if (sameString(seq->name, id))
-        return seq;
-errAbort("sequence %s not found in %s", id, inFaName);
-return NULL;
+if (!sameString(tName, genoSeqCache->name))
+    errAbort("sequence name %s does not match nib subrange name %s from %s",
+             tName, genoSeqCache->name, genoFileName);
+*tOffsetPtr = 0;
+return genoSeqCache;
 }
 
-void faPslIntronsOnly(char *inPslName, char *inFaName, char *outPslName)
-/* Filter psl files to only include those with introns, with sequence from
- * a fasta file */
+struct dnaSeq* faGet(char *tName, int tStart, int tEnd, int *tOffsetPtr)
+/* Get the DNA sequence from a fasta file */
 {
-struct dnaSeq *allSeqs = faReadAllDna(inFaName);
-struct dnaSeq *seq = NULL;
-struct lineFile *lf = NULL;
-FILE *outFile = NULL;
-struct psl *psl;
-int count = 0, intronCount = 0;
-
-lf = pslFileOpen(inPslName);
-
-outFile = mustOpen(outPslName, "w");
-while ((psl = pslNext(lf)) != NULL)
+if ((genoSeqCache == NULL) || !sameString(tName, genoSeqCache->name))
     {
-    ++count;
-    seq = findFaDna(psl->tName, allSeqs, inFaName);
-
-    if (pslHasIntron(psl, seq, 0))
+    for (genoSeqCache = faAllSeqs; genoSeqCache != NULL;
+         genoSeqCache = genoSeqCache->next)
         {
-	++intronCount;
-	pslTabOut(psl, outFile);
-	}
-    pslFree(&psl);
+        if (sameString(tName, genoSeqCache->name))
+            break;
+        }
+    if (genoSeqCache == NULL)
+        errAbort("sequence %s not found in %s", tName, genoFileName);
+    tolowers(genoSeqCache->dna);
     }
-carefulClose(&outFile);
-freeDnaSeqList(&allSeqs);
-lineFileClose(&lf);
-printf("%d of %d in %s have introns\n", intronCount, count, inPslName);
+*tOffsetPtr = 0;
+return genoSeqCache;
+}
+
+void initGenoRead(char *inGenoFile)
+/* initialize for reading the genome sequence */
+{
+strcpy(genoFileName, inGenoFile);
+if (isNibSubrange(inGenoFile))
+    {
+    nibSubrange = TRUE;
+    genoSeqCache = nibLoadAllMasked(NIB_MASK_MIXED|NIB_BASE_NAME,
+                                    genoFileName);
+    tolowers(genoSeqCache->dna);
+    }
+else if (isNib(inGenoFile))
+    {
+    nibOpenVerify(genoFileName, &nibFile, &nibSize);
+    }
+else
+    {
+    faAllSeqs = faReadAllDna(genoFileName);
+    }
+}
+
+struct dnaSeq *getDnaSeq(char *tName, int tStart, int tEnd, int *tOffsetPtr)
+/* get the DNA sequence containing the range. */
+{
+struct dnaSeq *genoSeq;
+if (nibSubrange)
+    genoSeq = subNibGet(tName, tStart, tEnd, tOffsetPtr);
+else if (nibFile != NULL)
+    genoSeq = chromNibGet(tName, tStart, tEnd, tOffsetPtr);
+else
+    genoSeq = faGet(tName, tStart, tEnd, tOffsetPtr);
+if (tEnd > (genoSeq->size+*tOffsetPtr))
+    errAbort("sequence %s in PSL is longer than sequence in %s", tName,
+             genoFileName);
+return genoSeq;
 }
 
 void pslIntronsOnly(char *inPslName, char *inName, char *outPslName)
 /* pslIntronsOnly - Filter psl files to only include those with introns. */
 {
-if (isNib(inName))
-    nibPslIntronsOnly(inPslName, inName, outPslName);
-else
-    faPslIntronsOnly(inPslName, inName, outPslName);
+struct lineFile *lf = NULL;
+FILE *outFile = NULL;
+int genoSeqOff = 0;
+struct psl *psl;
+int count = 0, intronCount = 0;
+
+initGenoRead(inName);
+
+lf = pslFileOpen(inPslName);
+outFile = mustOpen(outPslName, "w");
+while ((psl = pslNext(lf)) != NULL)
+    {
+    struct dnaSeq* genoSeq = getDnaSeq(psl->tName, psl->tStart, psl->tEnd,
+                                       &genoSeqOff);
+    if (pslHasIntron(psl, genoSeq, genoSeqOff))
+        {
+	++intronCount;
+	pslTabOut(psl, outFile);
+	}
+    pslFree(&psl);
+    ++count;
+    }
+carefulClose(&outFile);
+carefulClose(&nibFile);
+lineFileClose(&lf);
+printf("%d of %d in %s have introns\n", intronCount, count, inPslName);
 }
 
 int main(int argc, char *argv[])
