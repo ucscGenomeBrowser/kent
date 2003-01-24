a41e97e5a1e68a10c95803192cf794e7a008863c
fanhsu <fanhsu>
2003-01-24 20:11:22 +0000

    Fixed a bug that does not display hugo/refseq gene names for Known Gene track during Pack mode.

--- src/hg/hgTracks/hgTracks.c
+++ src/hg/hgTracks/hgTracks.c
@@ -2866,7 +2866,7 @@ void loadknownGene(struct track *tg)
 /* Load up known genes. */
 {
 tg->items = lfFromGenePredInRange("knownGene", chromName, winStart, winEnd);
-if (limitVisibility(tg) == tvFull)
+if ((limitVisibility(tg) == tvFull) ||  (limitVisibility(tg) == tvPack) )
     {
     lookupKnownGeneNames(tg->items);
     }
