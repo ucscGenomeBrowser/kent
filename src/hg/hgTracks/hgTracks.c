c37c560c7e769cf4a14c8e3c860375975dccf44c
weber <weber>
2003-01-27 12:46:20 +0000

    added line in the lf loader (struct linkedFeatures *lfFromSample(struct sample *sample)) for sample tracks to set the lf->start explicitly instead of letting linkedFeaturesBoundsAndGrays(lf) set it.
    This fixes the problem where the first samplePosition had to be 0, to make the details page appear properly. Now this is not required.

--- src/hg/hgTracks/hgTracks.c
+++ src/hg/hgTracks/hgTracks.c
@@ -2082,6 +2082,7 @@ for (i=0; i<sampleCount; ++i)
 slReverse(&sfList);
 lf->components = sfList;
 linkedFeaturesBoundsAndGrays(lf);
+lf->start = sample->chromStart;
 lf->end = sample->chromEnd;
 return lf;
 }
