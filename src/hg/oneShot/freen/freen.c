a43c7a47982800464a7acc75752ec3d36e0beb65
kent <kent>
2003-03-05 17:36:08 +0000

    Freen tests md5.

--- src/hg/oneShot/freen/freen.c
+++ src/hg/oneShot/freen/freen.c
@@ -2,8 +2,8 @@
 #include "common.h"
 #include "linefile.h"
 #include "hash.h"
-#include "jksql.h"
-#include "fa.h"
+#include "obscure.h"
+#include "md5.h"
 
 void usage()
 /* Print usage and exit. */
@@ -11,29 +11,58 @@ void usage()
 errAbort("usage: freen file");
 }
 
-void freen(char *database, char *outName)
-/* Print borf as fasta. */
+/*
+ * those are the standard RFC 1321 test vectors
+ */
+
+static char *msg[] =
 {
-struct sqlConnection *conn = sqlConnect(database);
-struct sqlResult *sr;
-char **row;
-FILE *f = mustOpen(outName, "w");
+    "",
+    "a",
+    "abc",
+    "message digest",
+    "abcdefghijklmnopqrstuvwxyz",
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789",
+    "12345678901234567890123456789012345678901234567890123456789012" \
+	"345678901234567890"
+};
 
-/* Get list of spliced ones. */
-sr = sqlGetResult(conn, 
-	" select name,seq from refMrna");
-while ((row = sqlNextRow(sr)) != NULL)
-    {
-    faWriteNext(f,row[0], row[1], strlen(row[1]));
-    }
+static char *val[] =
+{
+    "d41d8cd98f00b204e9800998ecf8427e",
+    "0cc175b9c0f1b6a831c399e269772661",
+    "900150983cd24fb0d6963f7d28e17f72",
+    "f96b697d7cb7938d525a2f31aaf161d0",
+    "c3fcd3d76192e4007dfb496cca67e13b",
+    "d174ab98d277d9f5a5611c2c9f419d9f",
+    "57edf4a22be3c955ac49da2e2107b67a"
+};
 
+void freen( char *fileName )
+{
+char *buf;
+size_t size;
+int i, j;
+char output[33];
+struct md5_context ctx;
+unsigned char md5sum[16];
+
+readInGulp(fileName, &buf, &size);
+md5_starts(&ctx);
+md5_update(&ctx, (uint8 *)buf, size);
+md5_finish(&ctx, md5sum);
+for( j = 0; j < 16; j++ )
+    printf( "%02x", md5sum[j] );
+printf("\n");
 }
+
 
 
 int main(int argc, char *argv[])
 /* Process command line. */
 {
-if (argc != 3)
+if (argc != 2)
    usage();
-freen(argv[1], argv[2]);
+freen(argv[1]);
+return 0;
 }
