4d9446782e51b2d9b49a197e4e97545aa78fb2de
kent <kent>
2003-03-09 18:04:30 +0000

    Seems to work - and 5x as fast as wc, which is good on these huge files!

--- src/utils/lineCount/lineCount.c
+++ src/utils/lineCount/lineCount.c
@@ -10,23 +10,40 @@ void usage()
 errAbort(
   "lineCount - Count lines in a file\n"
   "usage:\n"
-  "   lineCount XXX\n"
+  "   lineCount file(s)\n"
   "options:\n"
   "   -xxx=XXX\n"
   );
 }
 
-void lineCount(char *XXX)
+void lineCount(int fileCount, char *fileNames[])
 /* lineCount - Count lines in a file. */
 {
+long total = 0;
+long oneFile = 0;
+struct lineFile *lf;
+char *line;
+int i;
+for (i=0; i<fileCount; ++i)
+    {
+    lf = lineFileOpen(fileNames[i], FALSE);
+    oneFile = 0;
+    while (lineFileNext(lf, &line, NULL))
+        ++oneFile;
+    printf("%ld\t%s\n", oneFile, lf->fileName);
+    lineFileClose(&lf);
+    total += oneFile;
+    }
+if (fileCount > 1)
+    printf("%ld\n", total);
 }
 
 int main(int argc, char *argv[])
 /* Process command line. */
 {
 optionHash(&argc, argv);
-if (argc != 2)
+if (argc < 2)
     usage();
-lineCount(argv[1]);
+lineCount(argc-1, argv+1);
 return 0;
 }
