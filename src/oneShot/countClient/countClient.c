5c3c8e0b21b04cc6fb384643b4d4ff8fb413ade4
kent <kent>
2003-03-15 17:14:38 +0000

    Seems to work.

--- src/oneShot/countClient/countClient.c
+++ src/oneShot/countClient/countClient.c
@@ -3,6 +3,7 @@
 #include "linefile.h"
 #include "hash.h"
 #include "options.h"
+#include "net.h"
 
 void usage()
 /* Explain usage and exit. */
@@ -10,23 +11,39 @@ void usage()
 errAbort(
   "countClient - Connect repeatedly to the count server\n"
   "usage:\n"
-  "   countClient XXX\n"
+  "   countClient host port count\n"
   "options:\n"
   "   -xxx=XXX\n"
   );
 }
 
-void countClient(char *XXX)
+void countClient(char *host, char *portName, char *countString)
 /* countClient - Connect repeatedly to the count server. */
 {
+int i, count = atoi(countString);
+int socket;
+char buf[16];
+
+for (i=0; i<count; ++i)
+    {
+    socket = netMustConnectTo(host, portName);
+    snprintf(buf, sizeof(buf), "%d", i);
+    write(socket, buf, strlen(buf));
+    close(socket);
+    }
+socket = netMustConnectTo(host, portName);
+strcpy(buf, "quit");
+write(socket, buf, strlen(buf));
+close(socket);
+printf("Bye");
 }
 
 int main(int argc, char *argv[])
 /* Process command line. */
 {
 optionHash(&argc, argv);
-if (argc != 2)
+if (argc != 4)
     usage();
-countClient(argv[1]);
+countClient(argv[1], argv[2], argv[3]);
 return 0;
 }
--- src/oneShot/countServer/countServer.c
+++ src/oneShot/countServer/countServer.c
@@ -3,6 +3,7 @@
 #include "linefile.h"
 #include "hash.h"
 #include "options.h"
+#include "net.h"
 
 void usage()
 /* Explain usage and exit. */
@@ -10,15 +11,48 @@ void usage()
 errAbort(
   "countServer - A server that just returns a steadily increasing stream of numbers\n"
   "usage:\n"
-  "   countServer XXX\n"
+  "   countServer port\n"
   "options:\n"
   "   -xxx=XXX\n"
   );
 }
 
-void countServer(char *XXX)
+void countServer(char *portName)
 /* countServer - A server that just returns a steadily increasing stream of numbers. */
 {
+int port = atoi(portName);
+int ear, socket, size;
+char buf[1024];
+int count = 0;
+
+ear = netAcceptingSocket(port, 1000);
+if (ear < 0)
+    errnoAbort("Couldn't open ear on %d", port);
+socket = netAccept(ear);
+if (socket < 0)
+    errnoAbort("Couldn't accept first");
+size = netReadAll(socket, buf, sizeof(buf));
+close(socket);
+if (size < 0)
+    errnoAbort("Couldn't read first");
+buf[size] = 0;
+printf("Got first connection '%s'\n", buf);
+++count;
+sleep(10);
+for (;;)
+    {
+    socket = netAccept(ear);
+    if (socket < 0)
+	errnoAbort("Couldn't accept first");
+    size = netReadAll(socket, buf, sizeof(buf));
+    close(socket);
+    if (size < 0)
+	errnoAbort("Couldn't read first");
+    if (startsWith("quit", buf))
+        break;
+    ++count;
+    }
+printf("All done after %d\n", count);
 }
 
 int main(int argc, char *argv[])
