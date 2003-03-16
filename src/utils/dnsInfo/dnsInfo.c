f74641a64233f2557d486ca9b4527e0b61cc4a63
kent <kent>
2003-03-16 05:01:10 +0000

    Seems to work.

--- src/utils/dnsInfo/dnsInfo.c
+++ src/utils/dnsInfo/dnsInfo.c
@@ -1,5 +1,8 @@
 /* dnsInfo - Get info from DNS about a machine. */
 #include "common.h"
+#include <netdb.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 #include "linefile.h"
 #include "hash.h"
 #include "options.h"
@@ -10,15 +13,38 @@ void usage()
 errAbort(
   "dnsInfo - Get info from DNS about a machine\n"
   "usage:\n"
-  "   dnsInfo XXX\n"
+  "   dnsInfo machine\n"
   "options:\n"
   "   -xxx=XXX\n"
   );
 }
 
-void dnsInfo(char *XXX)
+void dnsInfo(char *machine)
 /* dnsInfo - Get info from DNS about a machine. */
 {
+struct hostent *h;
+char **aliases, **addresses;
+char str[INET6_ADDRSTRLEN];
+if ((h = gethostbyname(machine)) == NULL)
+    errAbort("Couldn't gethostbyname %s", hstrerror(h_errno));
+printf("official hostname: %s\n", h->h_name);
+for (aliases = h->h_aliases; *aliases != NULL; ++aliases)
+    printf("\talias: %s\n", *aliases);
+switch (h->h_addrtype)
+    {
+    case AF_INET:
+    case AF_INET6:
+        addresses = h->h_addr_list;
+        for (addresses = h->h_addr_list; *addresses != NULL; ++addresses)
+            {
+	    printf("\taddress: %s\n",
+	    	inet_ntop(h->h_addrtype, *addresses, str, sizeof(str)));
+            }
+	break;
+    default:
+        errAbort("unknown address type %d", (int)h->h_addrtype);
+	break;
+    }
 }
 
 int main(int argc, char *argv[])
