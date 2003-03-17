4f859f8bf2c73f5b79290d3494b83bf7e1a00b38
kent <kent>
2003-03-17 16:05:25 +0000

    Adding info to error message.

--- src/parasol/paraNodeStatus/paraNodeStatus.c
+++ src/parasol/paraNodeStatus/paraNodeStatus.c
@@ -118,7 +118,7 @@ while (lineFileRow(lf, row))
 		}
 	    else
 		{
-		printf("%s no status return. strerror(errno)\n", name);
+		printf("%s no status return: %s\n", name, strerror(errno));
 		}
 	    }
 	close(sd);
