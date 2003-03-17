d9322bcb2faf64e83f20e4a5cdc30c859406abbd
kent <kent>
2003-03-17 16:05:45 +0000

    Seems to work.

--- src/utils/dnsInfo/dnsInfo.c
+++ src/utils/dnsInfo/dnsInfo.c
@@ -3,6 +3,7 @@
 #include <netdb.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <sys/utsname.h>
 #include "linefile.h"
 #include "hash.h"
 #include "options.h"
@@ -14,8 +15,6 @@ errAbort(
   "dnsInfo - Get info from DNS about a machine\n"
   "usage:\n"
   "   dnsInfo machine\n"
-  "options:\n"
-  "   -xxx=XXX\n"
   );
 }
 
@@ -25,6 +24,21 @@ void dnsInfo(char *machine)
 struct hostent *h;
 char **aliases, **addresses;
 char str[INET6_ADDRSTRLEN];
+
+if (sameString(machine, "localhost"))
+    {
+    struct utsname myname;
+    if (uname(&myname) < 0)
+         errnoAbort("Couldn't uname.");
+    printf("localhost: %s\n", myname.nodename);
+    machine = myname.nodename;
+        {
+	char buf[256];
+	if (getdomainname(buf, sizeof(buf)) < 0)
+	    errnoAbort("getdomainname error");
+	printf("domain name: %s\n", buf);
+	}
+    }
 if ((h = gethostbyname(machine)) == NULL)
     errAbort("Couldn't gethostbyname: %s", hstrerror(h_errno));
 printf("official hostname: %s\n", h->h_name);
