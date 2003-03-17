9613242a1bd684ef2abf4a8291f9e481af0c7f48
kent <kent>
2003-03-17 16:01:33 +0000

    Freen does some stats calcs.

--- src/hg/oneShot/freen/freen.c
+++ src/hg/oneShot/freen/freen.c
@@ -15,26 +15,37 @@ void usage()
 errAbort("usage: freen file/dir");
 }
 
+double numPieces = 32080159/100000;
+double totalSize = 947469933/100000;
+
+/* 30 per kb.
+ */
+
 void freen(char *host)
 /* Test some hair-brained thing. */
 {
-long start, end;
-int i;
-start = clock1000();
-gethostbyname(host); 
-end = clock1000(); 
-printf("time 1 = %ld\n", end - start); 
-start = clock1000();
-for (i=0; i<10; ++i)
-    gethostbyname(host);
-end = clock1000();
-printf("time 10 = %ld\n", end - start);
-start = clock1000();
-for (i=0; i<100; ++i)
-    gethostbyname(host);
-end = clock1000();
-printf("time 100 = %ld\n", end - start);
+int pieceSize;
+
+#ifdef SOON 
+for (pieceSize = 1; pieceSize <= 1000; ++pieceSize)
+     {
+     double x = pieceSize/1000.0;
+     double logX = log(x);
+     double nLogX = logX * (30-1);
+     double enLogX = exp(nLogX);
+     double y = 30 * enLogX;
+     printf("%d %e %e %e %e %e\n", pieceSize, x, logX, nLogX, enLogX, y);
+     }
+#endif /* SOON  */
+printf("exp(1) = %f, log(2.7) = %f, exp(log(1)) = %f\n", exp(1), log(2.7), exp(log(1)));
+
+for (pieceSize = 1; pieceSize <= 1000; ++pieceSize)
+     {
+     double x = pieceSize/1000.0;
+     printf("%d\t%f\n", pieceSize, 30 * pow(x, 29));
+     }
 }
+// 30 3.000000e-02 -3.506558e+00 -1.016902e+02 6.863038e-45 2.058911e-43
 
 
 int main(int argc, char *argv[])
