ccd4c208ef531996ca214292b839069642c7a352
kent <kent>
2003-03-18 06:27:24 +0000

    Test something

---
+++ src/oneShot/test/test.c
@@ -0,0 +1,32 @@
+/* test - Test something. */
+#include "common.h"
+#include "linefile.h"
+#include "hash.h"
+#include "options.h"
+
+void usage()
+/* Explain usage and exit. */
+{
+errAbort(
+  "test - Test something\n"
+  "usage:\n"
+  "   test XXX\n"
+  "options:\n"
+  "   -xxx=XXX\n"
+  );
+}
+
+void test(char *XXX)
+/* test - Test something. */
+{
+}
+
+int main(int argc, char *argv[])
+/* Process command line. */
+{
+optionHash(&argc, argv);
+if (argc != 2)
+    usage();
+test(argv[1]);
+return 0;
+}
