beaf5a5a2a1a86899f375b6ab26a8c9d0295ea5f
kent <kent>
2003-03-18 17:23:40 +0000

    Freen polls resolution of gettimeofday clock, which does seem to be close to 1 microsecond.

--- src/hg/oneShot/freen/freen.c
+++ src/hg/oneShot/freen/freen.c
@@ -1,6 +1,7 @@
 /* freen - My Pet Freen. */
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <sys/time.h>
 #include <limits.h>
 #include <dirent.h>
 #include <netdb.h>
@@ -15,37 +16,36 @@ void usage()
 errAbort("usage: freen file/dir");
 }
 
-double numPieces = 32080159/100000;
-double totalSize = 947469933/100000;
-
-/* 30 per kb.
- */
-
 void freen(char *host)
 /* Test some hair-brained thing. */
 {
-int pieceSize;
+struct timeval origTime, lastTime, tv;
+int count = 0, i;
+int diffs[20];
 
-#ifdef SOON 
-for (pieceSize = 1; pieceSize <= 1000; ++pieceSize)
-     {
-     double x = pieceSize/1000.0;
-     double logX = log(x);
-     double nLogX = logX * (30-1);
-     double enLogX = exp(nLogX);
-     double y = 30 * enLogX;
-     printf("%d %e %e %e %e %e\n", pieceSize, x, logX, nLogX, enLogX, y);
-     }
-#endif /* SOON  */
-printf("exp(1) = %f, log(2.7) = %f, exp(log(1)) = %f\n", exp(1), log(2.7), exp(log(1)));
 
-for (pieceSize = 1; pieceSize <= 1000; ++pieceSize)
-     {
-     double x = pieceSize/1000.0;
-     printf("%d\t%f\n", pieceSize, 30 * pow(x, 29));
-     }
+gettimeofday(&origTime, NULL);
+gettimeofday(&lastTime, NULL);
+for (i=0; i<20; ++i)
+    {
+    for (;;)
+	{
+	++count;
+	gettimeofday(&tv, NULL);
+	if (tv.tv_sec != lastTime.tv_sec || tv.tv_usec != lastTime.tv_usec)
+	    {
+	    int diff = (lastTime.tv_sec - tv.tv_sec) * 1000000 +
+	    		(lastTime.tv_usec - tv.tv_usec);
+	    diffs[i] = -diff;
+	    lastTime = tv;
+	    break;
+	    }
+	}
+    }
+for (i=0; i<20; ++i)
+    printf("%d\n", diffs[i]);
+printf("Count %d, ave %f\n", count, count/20.0);
 }
-// 30 3.000000e-02 -3.506558e+00 -1.016902e+02 6.863038e-45 2.058911e-43
 
 
 int main(int argc, char *argv[])
