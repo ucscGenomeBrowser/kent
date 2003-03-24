95f8059849683bc367bed0b33324a0735070bd8c
kent <kent>
2003-03-24 10:39:51 +0000

    Putting back in -ggdb

--- src/inc/common.mk
+++ src/inc/common.mk
@@ -1,4 +1,4 @@
-COPT= -O
+COPT= -O -ggdb
 
 .c.o:
 	gcc -D_FILE_OFFSET_BITS=64 -D_LARGEFILE_SOURCE -D_GNU_SOURCE ${COPT} -Wformat -Wimplicit -Wuninitialized -Wreturn-type -I../inc -I../../inc -I../../../inc ${XINC} -c $*.c
