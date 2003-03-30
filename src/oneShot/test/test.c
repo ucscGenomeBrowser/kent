4db270d5378d1f5037438b2a9ac80907e2d3e462
kent <kent>
2003-03-30 06:18:18 +0000

    Converting it to a test shell for banded aligner - just starting.

--- src/oneShot/test/test.c
+++ src/oneShot/test/test.c
@@ -3,8 +3,7 @@
 #include "linefile.h"
 #include "hash.h"
 #include "options.h"
-#include "net.h"
-#include "rudp.h"
+#include "fa.h"
 
 void usage()
 /* Explain usage and exit. */
@@ -12,24 +11,27 @@ void usage()
 errAbort(
   "test - Test something\n"
   "usage:\n"
-  "   test XXX\n"
+  "   test a.fa b.fa\n"
   "options:\n"
   "   -xxx=XXX\n"
   );
 }
 
-void test(char *XXX)
+void test(char *aFile, char *bFile)
 /* test - Test something. */
 {
-rudpTest();
+struct dnaSeq *aSeq = faReadDna(aFile);
+struct dnaSeq *bSeq = faReadDna(bFile);
+printf("%s %s %d\n", aFile, aSeq->name, aSeq->size);
+printf("%s %s %d\n", bFile, bSeq->name, bSeq->size);
 }
 
 int main(int argc, char *argv[])
 /* Process command line. */
 {
 optionHash(&argc, argv);
-if (argc != 2)
+if (argc != 3)
     usage();
-test(argv[1]);
+test(argv[1], argv[2]);
 return 0;
 }
