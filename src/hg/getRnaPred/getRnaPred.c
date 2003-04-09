cd5c97450bcf3ec6190398cc210b59a8ed65130e
kent <kent>
2003-04-09 18:14:41 +0000

    Added option to get only those with weird splice sites.

--- src/hg/getRnaPred/getRnaPred.c
+++ src/hg/getRnaPred/getRnaPred.c
@@ -2,11 +2,11 @@
 #include "common.h"
 #include "linefile.h"
 #include "hash.h"
-#include "cheapcgi.h"
 #include "genePred.h"
 #include "fa.h"
 #include "jksql.h"
 #include "hdb.h"
+#include "options.h"
 
 void usage()
 /* Explain usage and exit. */
@@ -16,13 +16,14 @@ errAbort(
   "usage:\n"
   "   getRnaPred database table chromosome output.fa\n"
   "options:\n"
-  "   -xxx=XXX\n"
+  "   -weird Only get ones with weird splice sites\n"
   );
 }
 
 void getRna(char *table, char *chrom, char *faOut)
 /* getRna - Get RNA for gene predictions and write to file. */
 {
+boolean weird = optionExists("weird");
 int rowOffset;
 char **row;
 int i, start, end, size;
@@ -40,6 +41,31 @@ while ((row = sqlNextRow(sr)) != NULL)
 
     /* Load exons one by one into dna string. */
     dyStringClear(dna);
+    if (weird)
+        {
+	boolean gotOdd = FALSE;
+	for (i=1; i<gp->exonCount; ++i)
+	    {
+	    start = gp->exonEnds[i-1];
+	    end = gp->exonStarts[i];
+	    size = end - start;
+	    if (size > 0)
+		{
+		DNA *s, *e;
+		seq = hDnaFromSeq(chrom, start, end, dnaLower);
+		s = seq->dna;
+		e = seq->dna + seq->size;
+		uglyf("%s %c%c/%c%c\n", gp->name, s[0], s[1], e[-2], e[-1]);
+		if (gp->strand[0] == '-')
+		    reverseComplement(seq->dna, seq->size);
+		if (s[0] != 'g' || (s[1] != 't' && s[1] != 'c') || e[-2] != 'a' || e[-1] != 'g')
+		    gotOdd = TRUE;
+		freeDnaSeq(&seq);
+		}
+	    }
+	if (!gotOdd)
+	    continue;
+	}
     for (i=0; i<gp->exonCount; ++i)
         {
 	start = gp->exonStarts[i];
@@ -77,7 +103,7 @@ getRna(table, chrom, faOut);
 int main(int argc, char *argv[])
 /* Process command line. */
 {
-cgiSpoof(&argc, argv);
+optionHash(&argc, argv);
 if (argc != 5)
     usage();
 getRnaPred(argv[1], argv[2], argv[3], argv[4]);
