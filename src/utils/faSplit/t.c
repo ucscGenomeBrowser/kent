81992916666178746fc67f641fe9a94bc52dc5d9
kate <kate>
2003-05-05 05:40:03 +0000

    Test

---
+++ src/utils/faSplit/t.c
@@ -0,0 +1,33 @@
+#pragma comment(exestr, "@(#) UCSC Hg $Id: t.c,v 1.1 2003/05/05 05:40:03 kate Exp $")
+/* Test program */
+
+#include <stdio.h>
+
+int countFaRecords(char *file) 
+/* Count fasta records in a file */
+{
+int c;
+FILE *f;
+int count = 0;
+int i = 0;
+
+if ((f = fopen(file, "r")) == NULL)
+   {
+   fprintf(stderr, "Couldn't open %s", file);
+   perror(" ");
+   return 0;
+   }
+while ((c = getc(f)) != EOF)
+    {
+    if (c == '>') 
+	count++;
+    }
+fclose(f);
+return count;
+}
+
+main(int argc, char *argv[]) {
+int count = countFaRecords("foo");
+printf("Count=%d\n", count);
+}
+
