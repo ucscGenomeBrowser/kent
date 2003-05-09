c74fbb4d9f3772923806dd6998a368bc5bda1d6d
kent <kent>
2003-05-09 16:20:25 +0000

    Making it so that score line in base-by-base display is wiggle/density depending on full/dense setting, and so that bases are shown in dense mode.

--- src/hg/hgTracks/mafTrack.c
+++ src/hg/hgTracks/mafTrack.c
@@ -10,15 +10,8 @@
 #include "maf.h"
 #include "scoredRef.h"
 
-static char const rcsid[] = "$Id: mafTrack.c,v 1.6 2003/05/06 07:22:20 kate Exp $";
+static char const rcsid[] = "$Id: mafTrack.c,v 1.7 2003/05/09 16:20:25 kent Exp $";
 
-enum mafState {
-/* Our three display modes */
-    mafShort, 	/* Grayscale line. */
-    mafTall, 	/* Wiggle plot. */
-    mafBases,   /* Base-by-base view. */
-    };
-	
 struct mafItem
 /* A maf track item. */
     {
@@ -26,6 +19,7 @@ struct mafItem
     char *name;		/* Common name */
     char *db;		/* Database */
     int ix;		/* Position in list. */
+    int height;		/* Pixel height of item. */
     };
 
 static void mafItemFree(struct mafItem **pEl)
@@ -71,7 +65,7 @@ else
     }
 }
 
-static struct mafItem *mafBaseByBaseItems(struct track *tg)
+static struct mafItem *mafBaseByBaseItems(struct track *tg, int scoreHeight)
 /* Load up what we need for base-by-base view. */
 {
 struct mafItem *miList = NULL, *mi;
@@ -89,11 +83,13 @@ tg->customPt = mafList;
 AllocVar(mi);
 snprintf(buf, sizeof(buf), "%s Inserts", myOrg);
 mi->name = cloneString(buf);
+mi->height = tl.fontHeight;
 slAddHead(&miList, mi);
 
 /* Make up item for this organism. */
 AllocVar(mi);
 mi->name = myOrg;
+mi->height = tl.fontHeight;
 mi->db = cloneString(database);
 slAddHead(&miList, mi);
 
@@ -113,6 +109,7 @@ slAddHead(&miList, mi);
 		AllocVar(mi);
 		mi->db = cloneString(buf);
 		mi->name = hOrganism(mi->db);
+		mi->height = tl.fontHeight;
 		slAddHead(&miList, mi);
 		hashAdd(hash, mi->db, mi);
 		}
@@ -124,6 +121,7 @@ slAddHead(&miList, mi);
 /* Make up item that will show the score as grayscale. */
 AllocVar(mi);
 mi->name = cloneString("Score");
+mi->height = scoreHeight;
 slAddHead(&miList, mi);
 
 hFreeConn(&conn);
@@ -135,34 +133,23 @@ static void mafLoad(struct track *tg)
 /* Load up maf tracks.  What this will do depends on
  * the zoom level and the display density. */
 {
-enum mafState display;
 struct mafItem *miList = NULL;
-
-/* First figure out which state we are going to draw in. 
- * Save this in customInt. */
-if (tg->visibility == tvDense)
-    display = mafShort;
-else if (zoomedToBaseLevel)
-    display = mafBases;
-else
-    display = mafTall;
-tg->customInt = display;
+int scoreHeight = tl.fontHeight;
 
 /* Create item list and set height depending
  * on display type. */
-tg->heightPer = tl.fontHeight;
-if (display == mafBases)
+if (tg->visibility == tvFull)
+    scoreHeight *= 4;
+if (zoomedToBaseLevel)
     {
-    miList = mafBaseByBaseItems(tg);
+    miList = mafBaseByBaseItems(tg, scoreHeight);
     }
 else
     {
     AllocVar(miList);
-    miList->name = tg->shortLabel;
-    if (display == mafTall)
-        tg->heightPer *= 4;
+    miList->name = cloneString(tg->shortLabel);
+    miList->height = scoreHeight;
     }
-tg->lineHeight = tg->heightPer+1;
 tg->items = miList;
 }
 
@@ -170,8 +157,19 @@ static int mafTotalHeight(struct track *tg,
 	enum trackVisibility vis)
 /* Return total height of maf track.  */
 {
-tg->height =  slCount(tg->items) * tg->lineHeight;
+struct mafItem *mi;
+int total = 0;
+for (mi = tg->items; mi != NULL; mi = mi->next)
+    total += mi->height;
+tg->height =  total;
 return tg->height;
+}
+
+static int mafItemHeight(struct track *tg, void *item)
+/* Return total height of maf track.  */
+{
+struct mafItem *mi = item;
+return mi->height;
 }
 
 static void mafFree(struct track *tg)
@@ -222,9 +220,9 @@ for (i=0; i<textSize && outIx < outSize;  ++i)
     }
 }
 
-static void processScore(struct mafAli *maf, char *masterText,
-	char *outLine, int outSize)
-/* Make an array of colors to output corresponding to score. */
+static void getNormalizedScores(struct mafAli *maf, char *masterText,
+	double *scores, int scoreCount)
+/* Make an array of normalized scores, one per base. */
 {
 int textIx, outIx = 0;
 double maxScore, minScore;
@@ -233,131 +231,20 @@ double score;
 int shade;
 
 mafColMinMaxScore(maf, &minScore, &maxScore);
-scoreScale = maxShade/(maxScore - minScore);
-memset(outLine, MG_WHITE, outSize);
+scoreScale = 1.0/(maxScore - minScore);
 for (textIx = 0; textIx < maf->textSize; ++textIx)
     {
     if (masterText[textIx] != '-')
         {
-	score = mafScoreRangeMultiz(maf, textIx, 1);
-	shade = (score - minScore) * scoreScale;
-	if (shade < 0)
-	    shade = 0;
-	outLine[outIx] = shadesOfGray[shade];
+	score = (mafScoreRangeMultiz(maf, textIx, 1) - minScore)*scoreScale;
+	if (score < 0.0) score = 0.0;
+	if (score > 1.0) score = 1.0;
+	scores[outIx] = score;
 	++outIx;
 	}
     }
 }
 
-
-static void mafDrawBases(struct track *tg, int seqStart, int seqEnd,
-        struct vGfx *vg, int xOff, int yOff, int width, 
-        MgFont *font, Color color, enum trackVisibility vis)
-/* Draw base-by-base view. */
-{
-struct mafItem *miList = tg->items, *mi;
-struct mafAli *mafList = tg->customPt, *maf, *sub;
-int lineCount = slCount(miList);
-char **lines = NULL, *scoreLine, *selfLine, *insertLine;
-int i, y = yOff;
-struct dnaSeq *seq = NULL;
-struct hash *miHash = newHash(9);
-char dbChrom[64];
-
-/* Allocate a line of characters for each item. */
-AllocArray(lines, lineCount);
-for (i=0; i<lineCount; ++i)
-    {
-    lines[i] = needMem(winBaseCount+1);
-    memset(lines[i], ' ', winBaseCount);
-    }
-
-/* Give nice names to first three. */
-insertLine = lines[0];
-selfLine = lines[1];
-scoreLine = lines[lineCount-1];
-
-/* Load up self-line with DNA */
-seq = hChromSeq(chromName, seqStart, seqEnd);
-memcpy(selfLine, seq->dna, winBaseCount);
-toUpperN(selfLine, winBaseCount);
-freeDnaSeq(&seq);
-
-/* Set insert line and score line to defaults. */
-memset(scoreLine, '0', winBaseCount);
-
-/* Make hash of items keyed by database. */
-i = 0;
-for (mi = miList; mi != NULL; mi = mi->next)
-    {
-    mi->ix = i++;
-    if (mi->db != NULL)
-	hashAdd(miHash, mi->db, mi);
-    }
-
-/* Go through the mafs saving relevant info in lines. */
-safef(dbChrom, sizeof(dbChrom), "%s.%s", database, chromName);
-for (maf = mafList; maf != NULL; maf = maf->next)
-    {
-    sub = mafSubset(maf, dbChrom, winStart, winEnd);
-    if (sub != NULL)
-        {
-	struct mafComp *mc, *mcMaster;
-	char db[64];
-	int subStart,subEnd;
-	int lineOffset, subSize;
-	mcMaster = mafFindComponent(sub, dbChrom);
-	if (mcMaster->strand == '-')
-	    mafFlipStrand(sub);
-	subStart = mcMaster->start;
-	subEnd = subStart + mcMaster->size;
-	subSize = subEnd - subStart;
-	lineOffset = subStart - seqStart;
-	for (mc = sub->components; mc != NULL; mc = mc->next)
-	    {
-	    dbPartOfName(mc->src, db, sizeof(db));
-	    if (mc == mcMaster)
-		{
-		processInserts(mc->text, sub->textSize, 
-			insertLine+lineOffset, subSize);
-		}
-	    else
-	        {
-		mi = hashMustFindVal(miHash, db);
-		processOtherSeq(mc->text, mcMaster->text, sub->textSize, 
-			lines[mi->ix] + lineOffset, subSize);
-		}
-	    }
-	processScore(sub, mcMaster->text, scoreLine + lineOffset, subSize);
-	}
-    mafAliFree(&sub);
-    }
-
-/* Draw lines with letters . */
-for (i=0; i<lineCount-1; ++i)
-    {
-    char *line = lines[i];
-    int x = xOff;
-    if (line == insertLine)
-        {
-	int x1, x2;
-	x -= (width/winBaseCount)/2;
-	}
-    spreadString(vg, x, y, width, tg->heightPer, color, font, 
-    	line, winBaseCount);
-    y += tg->lineHeight;
-    }
-/* Draw score line. */
-for (i=0; i<winBaseCount; ++i)
-    {
-    int x1 = i * width/winBaseCount;
-    int x2 = (i+1) * width/winBaseCount;
-    vgBox(vg, x1+xOff, y, x2-x1, tg->heightPer, scoreLine[i]);
-    }
-y += tg->lineHeight;
-
-hashFree(&miHash);
-}
 
 void mafFillInPixelScores(struct mafAli *maf, struct mafComp *mcMaster,
 	double *scores, int numScores)
@@ -471,7 +358,8 @@ struct mafAli *mafList, *full, *sub = NULL, *maf = NULL;
 struct mafComp *mcMaster, mc;
 char dbChrom[64];
 struct sqlConnection *conn = hAllocConn();
-int height1 = tg->heightPer-1;
+struct mafItem *mi = tg->items;
+int height1 = mi->height-2;
 
 safef(dbChrom, sizeof(dbChrom), "%s.%s", database, chromName);
 mafList = mafLoadInRegion(conn, tg->mapName, chromName, seqStart, seqEnd);
@@ -511,9 +399,10 @@ for (full = mafList; full != NULL; full = full->next)
 		int shade = pixelScores[i] * maxShade;
 		Color c = shadesOfGray[shade];
 		vgBox(vg, i+mafPixelStart+xOff, yOff, 
-		    1, tg->heightPer, c);
+		    1, mi->height-1, c);
 		}
 	    }
+	freez(&pixelScores);
 	}
     mafAliFree(&sub);
     }
@@ -539,14 +428,144 @@ else
     }
 }
 
+static void mafDrawBases(struct track *tg, int seqStart, int seqEnd,
+        struct vGfx *vg, int xOff, int yOff, int width, 
+        MgFont *font, Color color, enum trackVisibility vis)
+/* Draw base-by-base view. */
+{
+struct mafItem *miList = tg->items, *mi;
+struct mafAli *mafList = tg->customPt, *maf, *sub;
+int lineCount = slCount(miList);
+char **lines = NULL, *scoreLine, *selfLine, *insertLine;
+double *scores;
+double scoreScale;
+int i, y = yOff;
+struct dnaSeq *seq = NULL;
+struct hash *miHash = newHash(9);
+char dbChrom[64];
+
+
+/* Allocate a line of characters for each item. */
+AllocArray(lines, lineCount-1);
+for (i=0; i<lineCount-1; ++i)
+    {
+    lines[i] = needMem(winBaseCount+1);
+    memset(lines[i], ' ', winBaseCount);
+    }
+AllocArray(scores, winBaseCount);
+
+/* Give nice names to first three. */
+insertLine = lines[0];
+selfLine = lines[1];
+
+/* Load up self-line with DNA */
+seq = hChromSeq(chromName, seqStart, seqEnd);
+memcpy(selfLine, seq->dna, winBaseCount);
+toUpperN(selfLine, winBaseCount);
+freeDnaSeq(&seq);
+
+/* Make hash of items keyed by database. */
+i = 0;
+for (mi = miList; mi != NULL; mi = mi->next)
+    {
+    mi->ix = i++;
+    if (mi->db != NULL)
+	hashAdd(miHash, mi->db, mi);
+    }
+
+/* Go through the mafs saving relevant info in lines. */
+safef(dbChrom, sizeof(dbChrom), "%s.%s", database, chromName);
+for (maf = mafList; maf != NULL; maf = maf->next)
+    {
+    sub = mafSubset(maf, dbChrom, winStart, winEnd);
+    if (sub != NULL)
+        {
+	struct mafComp *mc, *mcMaster;
+	char db[64];
+	int subStart,subEnd;
+	int lineOffset, subSize;
+	mcMaster = mafFindComponent(sub, dbChrom);
+	if (mcMaster->strand == '-')
+	    mafFlipStrand(sub);
+	subStart = mcMaster->start;
+	subEnd = subStart + mcMaster->size;
+	subSize = subEnd - subStart;
+	lineOffset = subStart - seqStart;
+	for (mc = sub->components; mc != NULL; mc = mc->next)
+	    {
+	    dbPartOfName(mc->src, db, sizeof(db));
+	    if (mc == mcMaster)
+		{
+		processInserts(mc->text, sub->textSize, 
+			insertLine+lineOffset, subSize);
+		}
+	    else
+	        {
+		mi = hashMustFindVal(miHash, db);
+		processOtherSeq(mc->text, mcMaster->text, sub->textSize, 
+			lines[mi->ix] + lineOffset, subSize);
+		}
+	    }
+	getNormalizedScores(sub, mcMaster->text, scores + lineOffset, subSize);
+	}
+    mafAliFree(&sub);
+    }
+
+/* Draw lines with letters . */
+for (mi = miList, i=0; mi->next != NULL; mi = mi->next, ++i)
+    {
+    char *line = lines[i];
+    int x = xOff;
+    if (line == insertLine)
+        {
+	int x1, x2;
+	x -= (width/winBaseCount)/2;
+	}
+    spreadString(vg, x, y, width, mi->height-1, color, font, 
+    	line, winBaseCount);
+    y += mi->height;
+    }
+
+/* Draw score line. */
+if (vis == tvDense)
+    scoreScale = (maxShade);
+else
+    scoreScale = (mi->height-2);
+for (i=0; i<winBaseCount; ++i)
+    {
+    int x1 = i * width/winBaseCount;
+    int x2 = (i+1) * width/winBaseCount;
+    if (vis == tvDense)
+	{
+	int shade = scores[i] * scoreScale;
+	int color = shadesOfGray[shade];
+	vgBox(vg, x1+xOff, y, x2-x1, mi->height-1, color);
+	}
+    else
+        {
+	int wiggleH = scores[i] * scoreScale;
+	vgBox(vg, x1+xOff, y + mi->height-2 - wiggleH, 
+	    x2-x1, wiggleH+1, color);
+	}
+    }
+y += mi->height;
+
+/* Clean up */
+for (i=0; i<lineCount-1; ++i)
+    freeMem(lines[i]);
+freez(&lines);
+freez(&scores);
+hashFree(&miHash);
+}
+
+
 static void mafDraw(struct track *tg, int seqStart, int seqEnd,
         struct vGfx *vg, int xOff, int yOff, int width, 
         MgFont *font, Color color, enum trackVisibility vis)
 /* Draw routine for mafAlign type tracks.  This will load
  * the items as well as drawing them. */
 {
-enum mafState display = tg->customInt;
-if (display == mafBases)
+if (zoomedToBaseLevel)
     mafDrawBases(tg, seqStart, seqEnd, vg, xOff, yOff, width, font, color, vis);
 else 
     mafDrawGraphic(tg, seqStart, seqEnd, vg, xOff, yOff, width, font, color, vis);
@@ -563,7 +582,7 @@ tg->drawItems = mafDraw;
 tg->itemName = mafName;
 tg->mapItemName = mafName;
 tg->totalHeight = mafTotalHeight;
-tg->itemHeight = tgFixedItemHeight;
+tg->itemHeight = mafItemHeight;
 tg->itemStart = tgItemNoStart;
 tg->itemEnd = tgItemNoEnd;
 tg->mapsSelf = TRUE;
