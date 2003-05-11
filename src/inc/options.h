992eb2f0a23441e505e8a95fb310dc0d96e0bd19
markd <markd>
2003-05-11 20:27:42 +0000

    add option validation support

--- src/inc/options.h
+++ src/inc/options.h
@@ -5,9 +5,19 @@
 
 #ifndef OPTIONS_H
 
-#ifndef HASH_H
-#include "hash.h"
-#endif
+/* Types for options */
+#define OPTION_BOOLEAN  0x01
+#define OPTION_STRING   0x02
+#define OPTION_INT      0x04
+#define OPTION_FLOAT    0x10
+
+struct optionSpec
+/* Specification of a single option.  An array of these are passed
+ * to optionInit() to validate options. */
+{
+    char *name;      /* option name */
+    unsigned flags;  /* Flags for option, specifies types */
+};
 
 char *optionVal(char *name, char *defaultVal);
 /* Return named option if in options hash, otherwise default. */
@@ -22,6 +32,24 @@ float optionFloat(char *name, float defaultVal);
 boolean optionExists(char *name);
 /* Return TRUE if option has been set. */
 
+void optionInit(int *pArgc, char *argv[], struct optionSpec *optionSpecs);
+/* Read options in command line into options hash.
+ * Options come in three forms:
+ *      -option         words starting with dash
+ *      option=val      words with = in the middle
+ *      -option=val     combining the two.
+ * The resulting hash will be keyed by the option name with the val
+ * string for value.  For '-option' types the value is 'on'.
+ * The words in argv are parsed in assending order.  If a word of
+ * "--" is encountered, argument parsing stops.
+ * If optionSpecs is not NULL, it is an array of optionSpec that are
+ * used to validate the options.  An option must exist in the array
+ * and the value must be convertable to the type specified in flags.
+ * Boolean options must no value, all other options must have one.
+ * Array is terminated by a optionSpec with a NULL name.
+ * If array NULL, no validation is done.
+ */
+
 void optionHash(int *pArgc, char *argv[]);
 /* Read options in command line into options hash.   
  * Options come in three forms:
@@ -29,7 +57,9 @@ void optionHash(int *pArgc, char *argv[]);
  *      option=val      words with = in the middle
  *      -option=val     combining the two.
  * The resulting hash will be keyed by the option name with the val
- * string for value.  For '-option' types the value is 'on'. */
+ * string for value.  For '-option' types the value is 'on'.
+ * The words in argv are parsed in assending order.  If a word of
+ * "--" is encountered, argument parsing stops. */
 
 void optionHashSome(int *pArgc, char *argv[], boolean justFirst);
 /* Set up option hash from command line, optionally only adding
--- src/lib/options.c
+++ src/lib/options.c
@@ -10,7 +10,128 @@
 #include "hash.h"
 #include "options.h"
 
-static char const rcsid[] = "$Id: options.c,v 1.9 2003/05/06 07:33:43 kate Exp $";
+static char const rcsid[] = "$Id: options.c,v 1.10 2003/05/11 20:27:42 markd Exp $";
+
+/* mask for type in optionSpec.flags */
+#define OPTION_TYPE_MASK (OPTION_BOOLEAN|OPTION_STRING|OPTION_INT|OPTION_FLOAT)
+
+
+static void validateOption(char *name, char *val, struct optionSpec *optionSpecs)
+/* validate an option against a list of values */
+{
+struct optionSpec *optionSpec = optionSpecs;
+char *valEnd;
+
+while ((optionSpec->name != NULL) && !sameString(optionSpec->name, name))
+    optionSpec++;
+
+if (optionSpec->name == NULL)
+    errAbort("-%s is not a valid option", name);
+
+switch (optionSpec->flags & OPTION_TYPE_MASK) {
+case OPTION_BOOLEAN:
+    if (val != NULL)
+        errAbort("boolean option -%s must not have value", name);
+    break;
+case OPTION_STRING:
+    if (val == NULL)
+        errAbort("string option -%s must have a value", name);
+    break;
+case OPTION_INT:
+    if (val == NULL)
+        errAbort("int option -%s must have a value", name);
+    strtol(val, &valEnd, 10);
+    if ((*val == '\0') || (*valEnd != '\0'))
+        errAbort("value of -%s is not a valid integer: \"%s\"",
+                 name, val);
+    break;
+case OPTION_FLOAT:
+    if (val == NULL)
+        errAbort("float option -%s must have a value", name);
+    strtod(val, &valEnd);
+    if ((*val == '\0') || (*valEnd != '\0'))
+        errAbort("value of -%s is not a valid float: \"%s\"",
+                 name, val);
+    break;
+default:
+    errAbort("bug: invalid type in optionSpec for %s", optionSpec->name);
+}
+}
+
+static boolean parseAnOption(struct hash *hash, char *arg, struct optionSpec *optionSpecs)
+/* Parse a single option argument and add to the hash, validating if
+ * optionSpecs is not NULL.  Return TRUE if it's arg is an option argument
+ * FALSE if it's not.
+ */
+{
+char *name, *val;
+char *eqPtr = strchr(arg, '=');
+
+if (!((eqPtr != NULL) || (arg[0] == '-')))
+    return FALSE;  /* not an option */
+
+name = arg;
+if (name[0] == '-')
+    name++;
+if (eqPtr != NULL)
+    {
+    *eqPtr = '\0';
+    val = eqPtr+1;
+    }
+else
+    val = NULL;
+
+if (optionSpecs != NULL)
+    validateOption(name, val, optionSpecs);
+if (val == NULL)
+    val = "on";
+hashAdd(hash, name, val);
+if (eqPtr != NULL)
+    *eqPtr = '=';
+return TRUE;
+}
+
+
+static struct hash *parseOptions(int *pArgc, char *argv[], boolean justFirst,
+                                 struct optionSpec *optionSpecs)
+/* Parse and optionally validate options */
+{
+int i, origArgc, newArgc = 1;
+char **rdPt = argv+1, **wrPt = argv+1;
+struct hash *hash = newHash(6);
+
+origArgc = *pArgc;
+
+/* parse arguments */
+for (i=1; i<origArgc; ++i)
+    {
+    if (sameString(*rdPt, "--"))
+        {
+        rdPt++;
+        break;
+        }
+    if (!parseAnOption(hash, *rdPt, optionSpecs))
+        {
+        /* not an option */
+        if (justFirst)
+            break;
+        *wrPt++ = *rdPt;
+        newArgc++;
+        }
+    rdPt++;
+    }
+
+/* copy any remaining positional args */
+for (; i<origArgc; ++i)
+    {
+    *wrPt++ = *rdPt++;
+    newArgc++;
+    }
+
+*pArgc = newArgc;
+*wrPt = NULL; 
+return hash;
+}
 
 struct hash *optionParseIntoHash(int *pArgc, char *argv[], boolean justFirst)
 /* Read options in command line (only up to first real argument) into
@@ -21,58 +142,17 @@ struct hash *optionParseIntoHash(int *pArgc, char *argv[], boolean justFirst)
  * The resulting hash will be keyed by the option name with the val
  * string for value.  For '-option' types the value is 'on'. */
 {
-int i, origArgc, curArgc;
-char **rdPt = argv+1, **wrPt = argv+1;
-boolean gotReal = FALSE;
-struct hash *hash = newHash(6);
-
-origArgc = curArgc = *pArgc;
-
-for (i=1; i<origArgc; ++i)
-    {
-    boolean startDash;
-    if (gotReal)
-        *wrPt++ = *rdPt++;
-    else
-        {
-	char *name = *rdPt++;
-	boolean startDash = (name[0] == '-' && name[1] != 0);
-	char *eqPos = strchr(name, '=');
-	if (startDash || eqPos != NULL)
-	    {
-	    char *val;
-	    if (startDash)
-	        ++name;
-	    if (eqPos != NULL)
-	        {
-		val = eqPos+1;
-		*eqPos = 0;
-		}
-	    else
-	        val = "on";
-	    hashAdd(hash, name, val);
-	    curArgc -= 1;
-	    }
-	else
-	    {
-	    *wrPt++ = name;
-	    if (justFirst)
-		gotReal = TRUE;
-	    }
-	}
-    }
-*pArgc = curArgc;
-return hash;
+return parseOptions(pArgc, argv, justFirst, NULL);
 }
 
-static struct hash *options;
+static struct hash *options = NULL;
 
 void optionHashSome(int *pArgc, char *argv[], boolean justFirst)
 /* Set up option hash from command line, optionally only adding
  * up to first non-optional word. */
 {
 if (options == NULL)
-    options = optionParseIntoHash(pArgc, argv, justFirst);
+    options = parseOptions(pArgc, argv, justFirst, NULL);
 }
 
 void optionHash(int *pArgc, char *argv[])
@@ -85,6 +165,28 @@ void optionHash(int *pArgc, char *argv[])
  * string for value.  For '-option' types the value is 'on'. */
 {
 optionHashSome(pArgc, argv, FALSE);
+}
+
+void optionInit(int *pArgc, char *argv[], struct optionSpec *optionSpecs)
+/* Read options in command line into options hash.
+ * Options come in three forms:
+ *      -option         words starting with dash
+ *      option=val      words with = in the middle
+ *      -option=val     combining the two.
+ * The resulting hash will be keyed by the option name with the val
+ * string for value.  For '-option' types the value is 'on'.
+ * The words in argv are parsed in assending order.  If a word of
+ * "--" is encountered, argument parsing stops.
+ * If optionSpecs is not NULL, it is an array of optionSpec that are
+ * used to validate the options.  An option must exist in the array
+ * and the value must be convertable to the type specified in flags.
+ * Boolean options must no value, all other options must have one.
+ * Array is terminated by a optionSpec with a NULL name.
+ * If array NULL, no validation is done.
+ */
+{
+if (options == NULL)
+    options = parseOptions(pArgc, argv, FALSE, optionSpecs);
 }
 
 static char *optGet(char *name)
@@ -109,26 +211,31 @@ int optionInt(char *name, int defaultVal)
  * if not set. */
 {
 char *s = optGet(name);
+char *valEnd;
+int val;
 if (s == NULL)
     return defaultVal;
-if ((s[0] == '-' && isdigit(s[1])) || isdigit(s[0]))
-    return atoi(s);
 if (sameString(s,"on"))
     return defaultVal;
-errAbort("option %s has to be integer valued", name);
-return 0;
+val = strtol(s, &valEnd, 10);
+if ((*s == '\0') || (*valEnd != '\0'))
+    errAbort("value of -%s is not a valid integer: \"%s\"", name, s);
+return val;
 }
 
 float optionFloat(char *name, float defaultVal)
 /* Return floating point value or default value if not set. */
 {
 char *s = optGet(name);
+char *valEnd;
+float val;
 if (s == NULL)
     return defaultVal;
-if ((s[0] == '-' && isdigit(s[1])) || isdigit(s[0]))
-    return atof(s);
-errAbort("option %s has to be float valued", name);
-return 0;
+
+val = strtod(s, &valEnd);
+if ((*s == '\0') || (*valEnd != '\0'))
+    errAbort("value of -%s is not a valid float: \"%s\"", name, s);
+return val;
 }
 
 boolean optionExists(char *name)
