23b308b9bc5ff6633a2cf90b53f0bec1f4d86a86
kent <kent>
2003-05-17 23:47:15 +0000

    Adding mafFromAxt.c

--- src/lib/mafFromAxt.c
+++ src/lib/mafFromAxt.c
@@ -3,7 +3,7 @@
 #include "axt.h"
 #include "maf.h"
 
-static char const rcsid[] = "$Id: mafFromAxt.c,v 1.3 2003/05/06 07:33:43 kate Exp $";
+static char const rcsid[] = "$Id: mafFromAxt.c,v 1.4 2003/05/17 23:47:15 kent Exp $";
 
 void mafFromAxtTemp(struct axt *axt, int tSize, int qSize,
 	struct mafAli *temp)
@@ -35,3 +35,44 @@ tComp.text = axt->tSym;
 slAddHead(&temp->components, &tComp);
 }
 
+struct mafAli *mafFromAxt(struct axt *axt, int tSize, 
+	char *tPrefix, int qSize, char *qPrefix)
+/* Make up a maf file from axt.  Slower than mafFromAxtTemp,
+ * but the axt and maf are independent afterwards. */
+{
+struct mafAli *maf;
+struct mafComp *mc;
+char name[256];
+AllocVar(maf);
+maf->score = axt->score;
+maf->textSize = axt->symCount;
+AllocVar(mc);
+if (qPrefix == NULL)
+    mc->src = cloneString(axt->qName);
+else
+    {
+    safef(name, sizeof(name), "%s.%s", qPrefix, axt->qName);
+    mc->src = cloneString(name);
+    }
+mc->srcSize = qSize;
+mc->strand = axt->qStrand;
+mc->start = axt->qStart;
+mc->size = axt->qEnd - axt->qStart;
+mc->text = cloneStringZ(axt->qSym, axt->symCount);
+slAddHead(&maf->components, mc);
+AllocVar(mc);
+if (tPrefix == NULL)
+    mc->src = cloneString(axt->tName);
+else
+    {
+    safef(name, sizeof(name), "%s.%s", tPrefix, axt->tName);
+    mc->src = cloneString(name);
+    }
+mc->srcSize = tSize;
+mc->strand = axt->tStrand;
+mc->start = axt->tStart;
+mc->size = axt->tEnd - axt->tStart;
+mc->text = cloneStringZ(axt->tSym, axt->symCount);
+slAddHead(&maf->components, mc);
+return maf;
+}
