91fb129c4e975282f5f4e0da619de5af1d870974
baertsch <baertsch>
2003-05-25 23:18:52 +0000

    void axtListReverse(struct axt **axtList, char *queryDb);  /* reverse complement an entire axtList */
     struct axt *createAxtGap(char *nibFile, char *chrom,
                                   int start, int end, char strand);
     /* return an axt alignment with the query all deletes - null aligment */
    
     void axtFillGap(struct axt **aList, char *nib);
     /* fill gaps between blocks with null axts with seq on t and q seq all gaps*/
    
     char *getAxtFileName(char *chrom, char *toDb, char *alignment, char *fromDb);
     /* return file name for a axt alignment */

--- src/hg/lib/axtLib.c
+++ src/hg/lib/axtLib.c
@@ -1,4 +1,3 @@
-
 /* axtLib - Convert chain to axt */
 #include "common.h"
 #include "linefile.h"
@@ -10,6 +9,8 @@
 #include "dnaseq.h"
 #include "nib.h"
 #include "axt.h"
+#include "axtInfo.h"
+#include "hdb.h"
 
 
 boolean qChain = FALSE;  /* Do chain from query side. */
@@ -235,4 +236,114 @@ chainFree(&chainToFree);
 freeDnaSeq(&qSeq);
 return axtList;
 }
+
+void axtListReverse(struct axt **axtList, char *queryDb)
+/* reverse complement an entire axtList */
+{
+struct axt *axt;
+int tmp;
+
+for (axt = *axtList; axt != NULL; axt = axt->next)
+    {
+    int qSize = 0;
+        
+    if (sameString(axt->qName , "gap"))
+        qSize = axt->qEnd;
+    else
+        qSize = hdbChromSize(queryDb, axt->qName);
+
+    reverseComplement(axt->qSym, axt->symCount);
+    reverseComplement(axt->tSym, axt->symCount);
+    tmp = qSize - axt->qStart;
+    axt->qStart = qSize - axt->qEnd;
+    axt->qEnd = tmp;
+    }
+slReverse(axtList);
+}
+
+struct axt *createAxtGap(char *nibFile, char *chrom, 	
+			 int start, int end, char strand)
+/* return an axt alignment with the query all deletes - null aligment */
+{
+struct axt *axt;
+int size = end-start+1;
+char *gapPt = needLargeMem(size+1);
+char *p;
+struct dnaSeq *seq = NULL;
+
+for (p=gapPt;p<=gapPt+size;p++)
+    *p = '-';
+AllocVar(axt);
+axt->tName = chrom;
+axt->tStart = start;
+axt->tEnd = end;
+axt->tStrand = strand;
+axt->qName = "gap";
+axt->qStart = 1;
+axt->qEnd = size;
+axt->qStrand = ' ';
+axt->symCount = size;
+axt->score = 0;
+seq = nibLoadPart(nibFile, start,size);
+axt->tSym = cloneMem(seq->dna, size+1);
+axt->qSym = cloneMem(gapPt, size+1);
+return axt;
+}
+
+void axtFillGap(struct axt **aList, char *nib)
+/* fill gaps between blocks with null axts with seq on t and q seq all gaps*/
+{
+struct axt *axt, *next, *axtGap, *tmp, *prevAxt = NULL;
+int prevEnd = 0;
+int prevStart = 0;
+
+for (axt = *aList; axt != NULL; axt = next)
+    {
+    if (((axt->tStart)-1) > prevEnd  && prevEnd > 0)
+        {
+        assert(prevAxt != NULL);
+        tmp = prevAxt->next;
+        axtGap = createAxtGap(nib,axt->tName,prevEnd,(axt->tStart)-1,axt->qStrand);
+        axtGap->next = tmp;
+        prevAxt->next = axtGap;
+        }
+    prevEnd = axt->tEnd;
+    prevStart = axt->tStart;
+    prevAxt = axt;
+    next = axt->next;
+    }
+}
+char *getAxtFileName(char *chrom, char *toDb, char *alignment, char *fromDb)
+/* return file name for a axt alignment */
+{
+char query[256];
+struct sqlResult *sr;
+struct sqlConnection *conn = hAllocConnDb(fromDb);
+char **row;
+struct axtInfo *ai = NULL;
+
+if (alignment != NULL)
+    snprintf(query, sizeof(query),
+	     "select * from axtInfo where chrom = '%s' and species = '%s' and alignment = '%s'",
+	     chrom, toDb, alignment);
+else
+    snprintf(query, sizeof(query),
+	     "select * from axtInfo where chrom = '%s' and species = '%s'",
+	     chrom, toDb);
+sr = sqlGetResult(conn, query);
+if ((row = sqlNextRow(sr)) != NULL)
+    {
+    ai = axtInfoLoad(row );
+    }
+if (ai == NULL)
+    {
+    printf("\nNo alignments available for %s (database %s).\n\n",
+	   hFreezeFromDb(toDb), toDb);
+    axtInfoFree(&ai);
+    return NULL;
+    }
+//axtInfoFree(&ai);
+return cloneString(ai->fileName);
+}
+
 
