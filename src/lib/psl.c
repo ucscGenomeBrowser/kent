aff19dcf749ab9cf8ec0de1e29bf27620ede9bc5
markd <markd>
2003-06-09 20:47:04 +0000

    improved readability of pslCheck error messages

--- src/lib/psl.c
+++ src/lib/psl.c
@@ -16,7 +16,7 @@
 #include "fuzzyFind.h"
 #include "aliType.h"
 
-static char const rcsid[] = "$Id: psl.c,v 1.22 2003/05/06 07:33:43 kate Exp $";
+static char const rcsid[] = "$Id: psl.c,v 1.23 2003/06/09 20:47:04 markd Exp $";
 
 static char *createString = 
 "CREATE TABLE %s (\n"
@@ -991,20 +991,31 @@ dyStringFree(&sqlCmd);
 return sqlCmdStr;
 }
 
-static int chkRanges(char* pslDesc, FILE* out, char* pName, char* pLabel,
-                     char pCLabel, char pStrand, unsigned pSize,
-                     unsigned pStart, unsigned pEnd,
-                     unsigned blockCount, unsigned* blockSizes,
-                     unsigned* pBlockStarts)
-/* check the target or query ranges in a PSL */
+static void printPslDesc(char* pslDesc, FILE* out, struct psl* psl)
+/* print description of a PSL on first error */
 {
-int errCount = 0;
+fprintf(out, "Error: invalid PSL: %s:%u-%u %s:%u-%u %s\n",
+        psl->qName, psl->qStart, psl->qEnd,
+        psl->tName, psl->tStart, psl->tEnd,
+        pslDesc);
+}
+
+static void chkRanges(char* pslDesc, FILE* out, struct psl* psl,
+                      char* pName, char* pLabel, char pCLabel, char pStrand,
+                      unsigned pSize, unsigned pStart, unsigned pEnd,
+                      unsigned blockCount, unsigned* blockSizes,
+                      unsigned* pBlockStarts, int* errCountPtr)
+/* check the target or query ranges in a PSL, increment errorCnt */
+{
+int errCount = *errCountPtr;
 unsigned iBlk, prevBlkEnd = 0;
 
 if (pStart >= pEnd)
     {
-    fprintf(out, "Error: %s: %s %cStart %u >= %cEnd %u\n",
-            pslDesc, pName, pCLabel, pStart, pCLabel, pEnd);
+    if (errCount == 0)
+        printPslDesc(pslDesc, out, psl);
+    fprintf(out, "\t%s %cStart %u >= %cEnd %u\n",
+            pName, pCLabel, pStart, pCLabel, pEnd);
     errCount++;
     }
 for (iBlk = 0; iBlk < blockCount; iBlk++)
@@ -1017,49 +1028,62 @@ for (iBlk = 0; iBlk < blockCount; iBlk++)
 
     if (blockSizes[iBlk] == 0)
         {
-        fprintf(out, "Error: %s: %s %s block %u size is 0\n",
-                pslDesc, pName, pLabel, iBlk);
+        if (errCount == 0)
+            printPslDesc(pslDesc, out, psl);
+        fprintf(out, "\t%s %s block %u size is 0\n", pName, pLabel, iBlk);
         errCount++;
         }
     if ((pSize > 0) && (blkEnd > pSize))
         {
-        fprintf(out, "Error: %s: %s %s block %u end %u > %cSize %u\n",
-                pslDesc, pName, pLabel, iBlk, blkEnd, pCLabel, pSize);
+        if (errCount == 0)
+            printPslDesc(pslDesc, out, psl);
+        fprintf(out, "\t%s %s block %u end %u > %cSize %u\n",
+                pName, pLabel, iBlk, blkEnd, pCLabel, pSize);
         errCount++;
         }
     if (gBlkStart < pStart)
         {
-        fprintf(out, "Error: %s: %s %s block %u translated start %u < %cStart %u\n",
-                pslDesc, pName, pLabel, iBlk, gBlkStart, pCLabel, pStart);
+        if (errCount == 0)
+            printPslDesc(pslDesc, out, psl);
+        fprintf(out, "\t%s %s block %u translated start %u < %cStart %u\n",
+                pName, pLabel, iBlk, gBlkStart, pCLabel, pStart);
         errCount++;
         }
     if (gBlkStart >= pEnd)
         {
-        fprintf(out, "Error: %s: %s %s block %u translated start %u >= %cEnd %u\n",
-                pslDesc, pName, pLabel, iBlk, gBlkStart, pCLabel, pEnd);
+        if (errCount == 0)
+            printPslDesc(pslDesc, out, psl);
+        fprintf(out, "\t%s %s block %u translated start %u >= %cEnd %u\n",
+                pName, pLabel, iBlk, gBlkStart, pCLabel, pEnd);
         errCount++;
         }
     if (gBlkEnd < pStart)
         {
-        fprintf(out, "Error: %s: %s %s block %u translated end %u < %cStart %u\n",
-                pslDesc, pName, pLabel, iBlk, gBlkEnd, pCLabel, pStart);
+        if (errCount == 0)
+            printPslDesc(pslDesc, out, psl);
+        fprintf(out, "\t%s %s block %u translated end %u < %cStart %u\n",
+                pName, pLabel, iBlk, gBlkEnd, pCLabel, pStart);
         errCount++;
         }
     if (gBlkEnd > pEnd)
         {
-        fprintf(out, "Error: %s: %s %s block %u translated end %u > %cEnd %u\n",
-                pslDesc, pName, pLabel, iBlk, gBlkEnd, pCLabel, pEnd);
+        if (errCount == 0)
+            printPslDesc(pslDesc, out, psl);
+        fprintf(out, "\t%s %s block %u translated end %u > %cEnd %u\n",
+                pName, pLabel, iBlk, gBlkEnd, pCLabel, pEnd);
         errCount++;
         }
     if ((iBlk > 0) && (blkStart < prevBlkEnd))
         {
-        fprintf(out, "Error: %s: %s %s block %u start %u < previous block end %u\n",
-                pslDesc, pName, pLabel, iBlk, blkStart, prevBlkEnd);
+        if (errCount == 0)
+            printPslDesc(pslDesc, out, psl);
+        fprintf(out, "\t%s %s block %u start %u < previous block end %u\n",
+                pName, pLabel, iBlk, blkStart, prevBlkEnd);
         errCount++;
         }
     prevBlkEnd = blkEnd;
     }
-return errCount;
+*errCountPtr = errCount;
 }
 
 int pslCheck(char *pslDesc, FILE* out, struct psl* psl)
@@ -1080,22 +1104,25 @@ for (i = 0; VALID_STRANDS[i] != NULL; i++)
     }
 if (VALID_STRANDS[i] == NULL)
     {
-    fprintf(out, "Error: %s: invalid PSL strand: \"%s\"\n",
-            pslDesc, psl->strand);
+    if (errCount == 0)
+        printPslDesc(pslDesc, out, psl);
+    fprintf(out, "\tinvalid PSL strand: \"%s\"\n", psl->strand);
     errCount++;
     }
 
 /* check target */
 strand = ((psl->strand[1] == '\0') ? '+' : psl->strand[1]);
-errCount +=  chkRanges(pslDesc,  out, psl->tName, "target", 't',
-                       strand, psl->tSize, psl->tStart, psl->tEnd,
-                       psl->blockCount, psl->blockSizes, psl->tStarts);
+chkRanges(pslDesc, out, psl, psl->tName, "target", 't',
+          strand, psl->tSize, psl->tStart, psl->tEnd,
+          psl->blockCount, psl->blockSizes, psl->tStarts,
+          &errCount);
 
 /* check query */
 strand = psl->strand[0];
-errCount += chkRanges(pslDesc, out, psl->qName, "query", 'q',
-                      strand, psl->qSize, psl->qStart, psl->qEnd,
-                      psl->blockCount, psl->blockSizes, psl->qStarts);
+chkRanges(pslDesc, out, psl, psl->qName, "query", 'q',
+          strand, psl->qSize, psl->qStart, psl->qEnd,
+          psl->blockCount, psl->blockSizes, psl->qStarts,
+          &errCount);
 return errCount;
 }
 
