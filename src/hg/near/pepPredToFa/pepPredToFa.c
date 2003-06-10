905f6b71413c7694df9838706619827182d39c08
kent <kent>
2003-06-10 16:10:03 +0000

    Added ident string.

--- src/hg/near/pepPredToFa/pepPredToFa.c
+++ src/hg/near/pepPredToFa/pepPredToFa.c
@@ -4,13 +4,15 @@
 #include "hash.h"
 #include "options.h"
 
+static char const rcsid[] = "$Id: pepPredToFa.c,v 1.2 2003/06/10 16:10:03 kent Exp $";
+
 void usage()
 /* Explain usage and exit. */
 {
 errAbort(
   "pepPredToFa - Convert a pepPred table to fasta format\n"
   "usage:\n"
-  "   pepPredToFa XXX\n"
+  "   pepPredToFa database table\n"
   "options:\n"
   "   -xxx=XXX\n"
   );
@@ -20,7 +22,7 @@ static struct optionSpec options[] = {
    {NULL, 0},
 };
 
-void pepPredToFa(char *XXX)
+void pepPredToFa(char *database, char *table)
 /* pepPredToFa - Convert a pepPred table to fasta format. */
 {
 }
@@ -29,8 +31,8 @@ int main(int argc, char *argv[])
 /* Process command line. */
 {
 optionInit(&argc, argv, options);
-if (argc != 2)
+if (argc != 3)
     usage();
-pepPredToFa(argv[1]);
+pepPredToFa(argv[1], argv[2]);
 return 0;
 }
