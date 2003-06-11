9447559d5d39ac19a2498226d2c04fc71a7f6480
kent <kent>
2003-06-11 03:22:22 +0000

    Seems to work.

--- src/hg/near/pepPredToFa/pepPredToFa.c
+++ src/hg/near/pepPredToFa/pepPredToFa.c
@@ -3,8 +3,11 @@
 #include "linefile.h"
 #include "hash.h"
 #include "options.h"
+#include "jksql.h"
+#include "pepPred.h"
+#include "fa.h"
 
-static char const rcsid[] = "$Id: pepPredToFa.c,v 1.2 2003/06/10 16:10:03 kent Exp $";
+static char const rcsid[] = "$Id: pepPredToFa.c,v 1.3 2003/06/11 03:19:41 kent Exp $";
 
 void usage()
 /* Explain usage and exit. */
@@ -12,7 +15,7 @@ void usage()
 errAbort(
   "pepPredToFa - Convert a pepPred table to fasta format\n"
   "usage:\n"
-  "   pepPredToFa database table\n"
+  "   pepPredToFa database table output.fa\n"
   "options:\n"
   "   -xxx=XXX\n"
   );
@@ -22,17 +25,31 @@ static struct optionSpec options[] = {
    {NULL, 0},
 };
 
-void pepPredToFa(char *database, char *table)
+void pepPredToFa(char *database, char *table, char *outName)
 /* pepPredToFa - Convert a pepPred table to fasta format. */
 {
+FILE *f = mustOpen(outName, "w");
+struct sqlConnection *conn = sqlConnect(database);
+struct sqlResult *sr;
+char **row;
+char query[512];
+struct pepPred pp;
+
+safef(query, sizeof(query), "select * from %s", table);
+sr = sqlGetResult(conn, query);
+while((row = sqlNextRow(sr)) != NULL)
+    {
+    pepPredStaticLoad(row, &pp);
+    faWriteNext(f, pp.name, pp.seq, strlen(pp.seq));
+    }
 }
 
 int main(int argc, char *argv[])
 /* Process command line. */
 {
 optionInit(&argc, argv, options);
-if (argc != 3)
+if (argc != 4)
     usage();
-pepPredToFa(argv[1], argv[2]);
+pepPredToFa(argv[1], argv[2], argv[3]);
 return 0;
 }
