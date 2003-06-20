8918e05b571dda5e76eefdeedd2481ef3dec54c0
kent <kent>
2003-06-20 18:37:01 +0000

    Making cartDump so that it can alter cart variables too.

--- src/hg/cartDump/cartDump.c
+++ src/hg/cartDump/cartDump.c
@@ -5,19 +5,46 @@
 #include "cheapcgi.h"
 #include "cart.h"
 
-static char const rcsid[] = "$Id: cartDump.c,v 1.4 2003/05/06 07:22:14 kate Exp $";
+static char const rcsid[] = "$Id: cartDump.c,v 1.5 2003/06/20 18:37:01 kent Exp $";
 
 void doMiddle(struct cart *cart)
 /* cartDump - Dump contents of cart. */
 {
+char *vName = "cartDump.varName";
+char *vVal = "cartDump.newValue";
+
+if (cgiVarExists("submit"))
+    {
+    char *varName = cgiOptionalString(vName);
+    char *newValue = cgiOptionalString(vVal);
+    if (varName != NULL && varName[0] != 0 && newValue != NULL && newValue[0] != 0)
+        {
+	if (sameString(newValue, "n/a"))
+	    cartRemove(cart, varName);
+	else
+	    cartSetString(cart, varName, newValue);
+	}
+    }
 printf("<TT><PRE>");
 cartDump(cart);
+printf("</TT></PRE>");
+printf("<FORM ACTION=\"../cgi-bin/cartDump\" METHOD=GET>\n");
+printf("alter variable named: ");
+cgiMakeTextVar(vName, cartUsualString(cart, vName, ""), 12);
+printf("new value");
+cgiMakeTextVar(vVal, "", 24);
+cgiMakeButton("submit", "submit");
+printf("<BR>\n");
+printf("Put n/a in for the new value to clear a variable.");
+cartRemove(cart, vVal);
 }
+
+char *excludeVars[] = { "submit", "Submit", NULL };
 
 int main(int argc, char *argv[])
 /* Process command line. */
 {
 cgiSpoof(&argc, argv);
-cartHtmlShell("Cart Dump", doMiddle, "hguid", NULL, NULL);
+cartHtmlShell("Cart Dump", doMiddle, "hguid", excludeVars, NULL);
 return 0;
 }
