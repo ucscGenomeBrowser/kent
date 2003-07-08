367fc53270f86625adcb848035b14c5533292e17
kent <kent>
2003-07-08 05:58:09 +0000

    Adding -DMACHTYPE_$(MACHTYPE) to get some library routines the chance to query the runtime environment a little.

--- src/inc/common.mk
+++ src/inc/common.mk
@@ -1,7 +1,7 @@
 CC=gcc
 COPT=-O -ggdb
 CFLAGS=
-HG_DEFS=-D_FILE_OFFSET_BITS=64 -D_LARGEFILE_SOURCE -D_GNU_SOURCE
+HG_DEFS=-D_FILE_OFFSET_BITS=64 -D_LARGEFILE_SOURCE -D_GNU_SOURCE -DMACHTYPE_$(MACHTYPE)
 HG_WARN=-Wformat -Wimplicit -Wuninitialized -Wreturn-type
 HG_INC=-I../inc -I../../inc -I../../../inc
 
