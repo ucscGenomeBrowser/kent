9f889ad92031b5d925cc6d8ea37c4d75fc58b42a
fanhsu <fanhsu>
2003-07-23 02:16:03 +0000

    Updated to add pfamID field.

--- src/hg/lib/pfamDesc.sql
+++ src/hg/lib/pfamDesc.sql
@@ -1,6 +1,8 @@
 CREATE TABLE pfamDesc (
   pfamAC          varchar(40) NOT NULL,
-  description     varchar(40),
+  pfamID          varchar(255) NOT NULL,
+  description     varchar(255),
+  KEY  (pfamID),
   KEY  (pfamAC)
 ) TYPE=MyISAM;
 
