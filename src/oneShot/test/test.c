02c2443190fb7d199ab3b00af137e47a1925cc4a
kent <kent>
2003-08-01 03:25:14 +0000

    Testing something new.

--- src/oneShot/test/test.c
+++ src/oneShot/test/test.c
@@ -3,8 +3,7 @@
 #include "linefile.h"
 #include "hash.h"
 #include "options.h"
-#include "math.h"
-#include "maf.h"
+#include "obscure.h"
 
 void usage()
 /* Explain usage and exit. */
@@ -17,30 +16,12 @@ errAbort(
   );
 }
 
-double mafScoreParts(struct mafAli *maf, int stepSize)
-/* Figure score by doing it by parts. */
-{
-int textSize = maf->textSize;
-double acc = 0;
-int i, end;
-for (i=0; i<textSize; i = end)
-    {
-    end = i + stepSize;
-    if (end > textSize) end = textSize;
-    acc += mafScoreRangeMultiz(maf, i, end - i);
-    }
-return acc;
-}
-
-void test(char *fileName)
+void test(char *s)
 /* test - Test something. */
 {
-signed char c;
-
-c = 127;
-printf("%x\n", c);
-c = -127;
-printf("%x\n", c);
+struct slName *list = stringToSlNames(s), *el;
+for (el = list; el != NULL; el = el->next)
+    printf("%s\n", el->name);
 }
 
 int main(int argc, char *argv[])
