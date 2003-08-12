bff52ea53202c4453b4fdbc50160064c7906bbfe
hiram <hiram>
2003-08-12 18:18:02 +0000

    Not all mkdir commands are created equal.  May need special.  Add MKDIR

--- src/inc/common.mk
+++ src/inc/common.mk
@@ -5,6 +5,7 @@ HG_DEFS=-D_FILE_OFFSET_BITS=64 -D_LARGEFILE_SOURCE -D_GNU_SOURCE -DMACHTYPE_$(MA
 HG_WARN=-Wformat -Wimplicit -Wuninitialized -Wreturn-type
 HG_INC=-I../inc -I../../inc -I../../../inc
 CGI_BIN=/usr/local/apache/cgi-bin
+MKDIR=mkdir -p
 
 .c.o:
 	${CC} ${COPT} ${CFLAGS} ${HG_DEFS} ${HG_WARN} ${HG_INC} ${XINC} -c $*.c
