4f4218486fe55c63a53a4626684e789c39d49d93
hiram <hiram>
2003-08-12 18:06:38 +0000

    Adding a CGI_BIN variable

--- src/inc/common.mk
+++ src/inc/common.mk
@@ -4,6 +4,7 @@ CFLAGS=
 HG_DEFS=-D_FILE_OFFSET_BITS=64 -D_LARGEFILE_SOURCE -D_GNU_SOURCE -DMACHTYPE_$(MACHTYPE)
 HG_WARN=-Wformat -Wimplicit -Wuninitialized -Wreturn-type
 HG_INC=-I../inc -I../../inc -I../../../inc
+CGI_BIN=/usr/local/apache/cgi-bin
 
 .c.o:
 	${CC} ${COPT} ${CFLAGS} ${HG_DEFS} ${HG_WARN} ${HG_INC} ${XINC} -c $*.c
