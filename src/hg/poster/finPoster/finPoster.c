1d71f91367a1958621188dea73ccd2cb97aa583d
kent <kent>
2003-08-12 17:40:35 +0000

    Adding disease and model organ ortholog info to known genes.

--- src/hg/poster/finPoster/finPoster.c
+++ src/hg/poster/finPoster/finPoster.c
@@ -26,7 +26,7 @@
 #include "hCommon.h"
 #include "axt.h"
 
-static char const rcsid[] = "$Id: finPoster.c,v 1.1 2003/08/11 18:16:12 kent Exp $";
+static char const rcsid[] = "$Id: finPoster.c,v 1.2 2003/08/12 17:40:35 kent Exp $";
 
 /* Which database to use */
 char *database = "hg15";
@@ -34,14 +34,11 @@ char *database = "hg15";
 /* Location of mouse/human axt files. */
 char *axtDir = "/cluster/store2/mm.2002.02/mm2/bed/blastz.gs11.2002-06-05/axtBestUnzip";
 
-/* File with human/mouse orthologs. */
-char *orthoFile = "/cluster/store2/mm.2002.02/mm2/bed/poster/ortholog.rpt";
+/* File with human/worm or mouse orthologs. */
+char *orthoFile = "hugo_with_worm_or_fly.txt";
 
-/* File with OMIM morbid map. */
-char *morbidMapFile = "/cluster/store2/mm.2002.02/mm2/bed/poster/morbidMap";
-
-/* File with orthologs to human disease genes. */
-char *diseaseFile = "/cluster/store2/mm.2002.02/mm2/bed/poster/disease.rpt";
+/* File with disease genes. */
+char *diseaseFile = "hugo_with_disease.txt";
 
 /* File with synteny info */
 char *syntenyFile = "/cluster/store2/mm.2002.02/mm2/bed/synteny/synteny.bed";
@@ -190,52 +187,18 @@ while (lineFileRow(lf, row))
 lineFileClose(&lf);
 }
 
-struct hash *parseMorbidMap(char *fileName)
-/* Parse morbidMap into hash */
+struct hash *hashFromTwoColumn(char *fileName, int column)
+/* Parse column of two column file into hash. */
 {
 struct hash *hash = newHash(0);
 struct lineFile *lf = lineFileOpen(fileName, TRUE);
-char *line;
-char *fields[4], *names[16];
-int fieldCount, nameCount;
-int loci = 0, syns = 0;
-int i;
-
-while (lineFileNext(lf, &line, NULL))
-    {
-    line = skipLeadingSpaces(line);
-    if (line[0] == '#' || line[0] == 0)
-        continue;
-    fieldCount = chopString(line, "|", fields, ArraySize(fields));
-    if (fieldCount < 3)
-        errAbort("Expecting at least 3 | separated fields line %d of %s",
-		lf->lineIx, lf->fileName);
-    nameCount = chopString(fields[1], ", ", names, ArraySize(names));
-    for (i=0; i<nameCount; ++i)
-        {
-	hashStore(hash, names[i]);
-	++syns;
-	}
-    ++loci;
-    }
-return hash;
-}
-
-struct hash *makeDiseaseOrthoHash(char *orthoFile, struct hash *morbidHash)
-/* Fill in hash of orthologs of human disease genes keyed by MGI id . */
-{
-struct lineFile *lf = lineFileOpen(orthoFile, TRUE);
-char *row[6];
+char *row[2];
 int count = 0;
-struct hash *hash = newHash(0);
 
 while (lineFileRow(lf, row))
     {
-    if (hashLookup(morbidHash, row[5]))
-       {
-       ++count;
-       hashStore(hash, row[0]);
-       }
+    hashStore(hash, row[column]);
+    ++count;
     }
 return hash;
 }
@@ -326,7 +289,7 @@ return TRUE;
 void getKnownGenes(struct chromGaps *cg, char *chrom, 
 	struct sqlConnection *conn, FILE *f, 
 	struct hash *dupHash, struct hash *resolvedDupHash, 
-        struct hash *redHash) 
+        struct hash *diseaseHash, struct hash *orthoHash) 
 /* Get info on known genes. */
 {
 int rowOffset;
@@ -336,7 +299,9 @@ char query[256];
 struct genePred *gpList = NULL, *gp;
 char geneName[64];
 static struct rgbColor red = {255, 0, 0};
+static struct rgbColor lightRed = {255, 100, 100};
 static struct rgbColor blue = {0, 0, 220};
+static struct rgbColor lightBlue = {100, 100, 220};
 struct rgbColor *col;
 boolean keepGene;
 struct dlList *geneList = newDlList();
@@ -374,6 +339,8 @@ for (gp = gpList; gp != NULL; gp = gp->next)
 	    && !sameString(geneName, "pseudo") 
 	    && !startsWith("DKFZ", geneName) 
 	    && !startsWith("KIAA", geneName) 
+	    && !startsWith("FLJ", geneName) 
+	    && !(startsWith("MGC", geneName) && len > 5)
 	    && !stringIn("orf", geneName)
 	    && !(startsWith("LOC", geneName) && len > 5))
 	    {
@@ -397,7 +364,7 @@ for (gp = gpList; gp != NULL; gp = gp->next)
 	    showIt = TRUE;
 	    }
 	if (ap->list == NULL || !sameString(ap->list->chrom, chrom) || 
-		abs(gp->cdsStart - ap->list->chromStart) > 300000)
+		abs(gp->cdsStart - ap->list->chromStart) > 400000)
 	    {
 	    AllocVar(op);
 	    op->chrom = chrom;
@@ -438,16 +405,26 @@ for (gp = gpList; gp != NULL; gp = gp->next)
 for (node = geneList->head; !dlEnd(node); node = node->next)
     {
     int len;
-    char *acc;
+    char *name;
     kg = node->val;
 
-    acc = kg->acc;
-    if (hashLookup(redHash, acc))
-        col = &red;
+    name = kg->name;
+    if (hashLookup(diseaseHash, name))
+	{
+	if (hashLookup(orthoHash, name))
+	    col = &red;
+	else
+	    col = &lightRed;
+	}
     else
-        col = &blue;
+	{
+	if (hashLookup(orthoHash, name))
+	    col = &blue;
+	else
+	    col = &lightBlue;
+	}
     printTab(f, cg, chrom, kg->start, kg->end, "hugo", 
-	    "word", col->r, col->g, col->b, kg->name);
+	    "word", col->r, col->g, col->b, name);
     }
 freeDlList(&geneList);
 }
@@ -933,36 +910,197 @@ assert(ix < ArraySize(chromColorTable));
 return &chromColorTable[ix];
 }
 
-void getSynteny(struct chromGaps *cg, char *chrom, FILE *f)
+void getSynteny(struct chromGaps *cg, char *chrom, struct sqlConnection *conn, FILE *f)
 /* Read synteny file and put relevant parts in output */
 {
-struct sqlConnection *hConn = sqlConnect(database);
-struct lineFile *lf = lineFileOpen(syntenyFile, TRUE);
-char *row[7];
-while (lineFileRow(lf, row))
+struct sqlResult *sr;
+char **row;
+char query[512];
+safef(query, sizeof(query),
+	"select chromStart,chromEnd,name,strand from syntenyMouse "
+	"where chrom = '%s' order by chromStart", chrom);
+sr = sqlGetResult(conn, query);
+while ((row = sqlNextRow(sr)) != NULL)
     {
-    if (sameString(chrom, row[0]))
+    int start = atoi(row[0]);
+    int end = atoi(row[1]);
+    char *mouseChrom = row[2];
+    char *strand = row[3];
+    struct rgbColor *col = chromColor(mouseChrom);
+    char label[32];
+    safef(label, sizeof(label), "%s%s", mouseChrom+3, strand);
+    printTab(f, cg, chrom, start, end, 
+		"synteny", "box", col->r, col->g, col->b, label);
+    }
+sqlFreeResult(&sr);
+}
+
+int countSnps(struct sqlConnection *conn, 
+	char *chrom, int chromStart, int chromEnd)
+/* Count the number of SNPs in window. */
+{
+char query[256];
+
+sprintf(query, 
+    "select count(*) from snpTsc where chrom = '%s' and chromStart < %d and chromEnd > %d",
+    chrom, chromEnd, chromStart);
+return sqlQuickNum(conn, query);
+}
+
+void slowGetSnpDensity(struct chromGaps *cg, char *chrom, int chromSize, struct sqlConnection *conn, FILE *f)
+/* Make SNP density track. */
+{
+int windowSize = 50000;
+int s, e;
+int size;
+double ratio, totalRatio = 0;
+double maxCount = 0, minCount=windowSize*2, totalCount = 0;
+
+for (s = 0; s < chromSize; s += 50000)
+    {
+    e = s + windowSize;
+    if (e > chromSize) e = chromSize;
+    size = e - s;
+    if (size > windowSize/2)
         {
-	char *hChrom = row[3];
-	int hs = lineFileNeedNum(lf, row, 4);
-	int he = lineFileNeedNum(lf, row, 5);
-	char *strand = row[6];
-	char band[65];
-	struct rgbColor *col = chromColor(hChrom);
-	band[0] = strand[0];
-	hChromBandConn(hConn, hChrom, (hs+he)/2, band+1);
-	chopSuffix(band+1);
-	printTab(f, cg, chrom, atoi(row[1]), atoi(row[2]), 
-		"synteny", "box", col->r, col->g, col->b, band);
+	ratio = 1.0 - nRatio(conn, chrom, s, e);
+	uglyf("%s:%d-%d ratio %f\n", chrom, s, e, ratio);
+	if (ratio >= 0.5)
+	    {
+	    int rawCount = countSnps(conn, chrom, s, e);
+	    double count = rawCount/ratio;
+	    uglyf(" rawCount %d, count %f\n", rawCount, count);
+	    if (count < minCount)
+	        minCount = count;
+	    if (count > maxCount)
+	        maxCount = count;
+	    totalRatio += ratio;
+	    totalCount += count;
+	    }
 	}
     }
-lineFileClose(&lf);
-sqlDisconnect(&hConn);
+printf("%s SNP density in %d windows is between %f and %f, average %f\n",
+	chrom, windowSize, minCount, maxCount, totalCount/totalRatio);
 }
+
+void getSnpDensity(struct chromGaps *cg, char *chrom, int chromSize, struct sqlConnection *conn, FILE *f)
+/* Put out SNP density info. */
+{
+int windowSize = 50000;
+double scale = 500.0;
+struct sqlResult *sr;
+char **row;
+char query[256];
+struct snp snp;
+struct agpFrag frag;
+int winsPerChrom = (chromSize + windowSize-1)/windowSize;
+int *winBases;
+int *winRepBases;
+int i;
+int baseCount, repCount;
+double density;
+double minDen = 1.0, maxDen = 0.0;
+int winStart, winEnd;
+int rowOffset;
+
+printf("  Getting SNPs in %d windows of size %d\n", winsPerChrom, windowSize);
+fflush(stdout);
+
+/* Count up SNPs in each window. */
+AllocArray(winRepBases, winsPerChrom);
+sr = hChromQuery(conn, "snpTsc", chrom, NULL, &rowOffset);
+while ((row = sqlNextRow(sr)) != NULL)
+    {
+    snpStaticLoad(row+rowOffset, &snp);
+    addSegTotals(snp.chromStart, snp.chromEnd, winRepBases, windowSize, chromSize);
+    }
+sqlFreeResult(&sr);
+
+/* Count up bases in each window. */
+AllocArray(winBases, winsPerChrom);
+sr = hChromQuery(conn, "gold", chrom, NULL, &rowOffset);
+while ((row = sqlNextRow(sr)) != NULL)
+    {
+    agpFragStaticLoad(row+rowOffset, &frag);
+    addSegTotals(frag.chromStart, frag.chromEnd, winBases, windowSize, chromSize);
+    }
+sqlFreeResult(&sr);
+
+/* Output density. */
+for (i=0; i<winsPerChrom; ++i)
+    {
+    if ((baseCount = winBases[i]) > windowSize/2)
+        {
+	repCount = winRepBases[i];
+	density = (double)repCount/(double)baseCount;
+	if (density < minDen) minDen = density;
+	if (density > maxDen) maxDen = density;
+	density *= scale;
+	if (density > 1.0) density = 1.0;
+	winStart = i*windowSize;
+	winEnd = winStart + windowSize;
+	if (winEnd > chromSize) winEnd = chromSize;
+	printTabNum(f, cg, chrom, winStart, winEnd, 
+		"SNP", "wiggle", 128, 0, 128, density);
+	}
+    }
+printf("   %s minDen %f, maxDen %f\n", chrom, minDen, maxDen);
+freeMem(winBases);
+freeMem(winRepBases);
+}
+
+void getBands(struct chromGaps *cg, char *chrom, struct sqlConnection *conn, FILE *f)
+/* Get chromosome bands stuff. */
+{
+struct sqlResult *sr;
+char **row;
+char query[256];
+struct cytoBand el;
+int r,g,b;
+char *stain;
+
+printf("  getting bands\n");
+sprintf(query, "select * from cytoBand where chrom = '%s'", chrom);
+sr = sqlGetResult(conn, query);
+
+while ((row = sqlNextRow(sr)) != NULL)
+    {
+    cytoBandStaticLoad(row, &el);
+    stain = el.gieStain;
+    if (startsWith("gneg", stain))
+        {
+	r = g = b = 220;
+	}
+    else if (startsWith ("gpos", stain))
+        {
+	int percentage = 100;
+	stain += 4;
+	if (isdigit(stain[0]))
+	    percentage = atoi(stain);
+	r = g = b = round((100.0-percentage)*2);
+	}
+    else if (startsWith("gvar", stain))
+        {
+	r = 180;
+	g = 180;
+	b = 120;
+	}
+    else
+        {
+	r = 150;
+	g = 50;
+	b = 50;
+	}
+    printTab(f, cg, chrom, el.chromStart, el.chromEnd, 
+	    "band", "box", r, g, b, el.name);
+    }
+sqlFreeResult(&sr);
+}
+
 
 void oneChrom(char *chrom, struct sqlConnection *conn, 
 	struct hash *dupHash, struct hash *resolvedDupHash, 
-	struct hash *diseaseHash, 
+	struct hash *diseaseHash, struct hash *orthoHash,
 	FILE *f)
 /* Get info for one chromosome.  */
 {
@@ -973,8 +1111,10 @@ char axtFile[512];
 printf("Processing %s\n", chrom);
 sprintf(axtFile, "%s/%s.axt", axtDir, chrom);
 
+getSnpDensity(cg, chrom, chromSize, conn, f);
+getBands(cg, chrom, conn, f);
+getSynteny(cg, chrom, conn, f);
 #ifdef SOON
-getSynteny(cg, chrom, f);
 getMouseAli(cg, chrom, chromSize, axtFile, conn, f, 
 	"HS_ALI", 50000, 1.0/0.85, 0, 0, 255);
 getMouseId(cg, chrom, chromSize, axtFile, conn, f,
@@ -992,7 +1132,7 @@ getEstTicks(cg, chrom, conn, f);
 getPredGenes(cg, chrom, conn, f, "ensGene", 160, 10, 0);
 getPredGenes(cg, chrom, conn, f, "refGene", blueGene.r, blueGene.g, blueGene.b);
 getKnownGenes(cg, chrom, conn, f, dupHash, resolvedDupHash, 
-	diseaseHash);
+	diseaseHash, orthoHash);
 }
 
 void printDupes(struct hash *dupHash, FILE *f)
@@ -1035,7 +1175,8 @@ struct sqlConnection *conn = sqlConnect(database);
 struct hash *dupHash = newHash(0);
 struct hash *resolvedDupHash = newHash(8);
 struct resolvedDup *rdList = NULL;
-struct hash *diseaseHash = newHash(0);
+struct hash *diseaseHash = hashFromTwoColumn(diseaseFile, 1);
+struct hash *orthoHash = hashFromTwoColumn(orthoFile, 0);
 
 dupeFile = mustOpen(dupeFileName, "w");
 hSetDb(database);
@@ -1047,7 +1188,7 @@ for (i=0; i<chromCount; ++i)
     sprintf(fileName, "%s.tab", chromNames[i]);
     f = mustOpen(fileName, "w");
     oneChrom(chromNames[i], conn, dupHash, resolvedDupHash, 
-    	diseaseHash, f);
+    	diseaseHash, orthoHash, f);
     fclose(f);
     }
 printDupes(dupHash, dupeFile);
