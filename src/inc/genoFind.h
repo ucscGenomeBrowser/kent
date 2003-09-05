9a72cd90a4e57cdd2bdc9b11177d3c29704055c0
kent <kent>
2003-09-05 17:51:34 +0000

    Bumped version number.

--- src/inc/genoFind.h
+++ src/inc/genoFind.h
@@ -339,7 +339,7 @@ void gfLongTransTransInMem(struct dnaSeq *query, struct genoFind *gfs[3],
 /* Chop up query into pieces, align each in translated space, and stitch back
  * together again as nucleotides. */
 
-#define gfVersion 23	/* Current BLAT version number */
+#define gfVersion 24	/* Current BLAT version number */
 
 #endif /* GENOFIND_H */
 
