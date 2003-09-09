e27689484dd10043dff263186a4970b35a4f3d87
kent <kent>
2003-09-09 00:03:36 +0000

    Testing some gap score things.

--- src/oneShot/test/test.c
+++ src/oneShot/test/test.c
@@ -3,7 +3,7 @@
 #include "linefile.h"
 #include "hash.h"
 #include "options.h"
-#include "ra.h"
+#include "fuzzyFind.h"
 
 void usage()
 /* Explain usage and exit. */
@@ -11,33 +11,62 @@ void usage()
 errAbort(
   "test - Test something\n"
   "usage:\n"
-  "   test a\n"
+  "   test hGap nGap\n"
   "options:\n"
   );
 }
 
-char *testRa = 
-"a alpha\n"
-"b beta\n"
-"c gamma\n"
-"abc alpha beta gamma\n"
-;
+static int trimGapPenalty(int hGap, int nGap)
+/* Calculate gap penalty for routine below. */
+{
+int penalty;
+if (hGap <=2 && nGap <= 2)
+    penalty = hGap + nGap;
+else if (hGap < 25)
+    penalty = 5 + 2*sqrt(hGap+nGap);
+else
+    {
+    penalty = 1.4*log(hGap + nGap + 1) + 3;
+    if (nGap > 0)
+	penalty += 4;
+    if (hGap > 400000)	/* Discourage really long introns. */
+	{
+	penalty += (hGap - 400000)/3000;
+	if (hGap > ffIntronMax)
+	    penalty += (hGap - ffIntronMax)/2000;
+	}
+    }
+return penalty;
+}
 
-void test(char *s)
+
+void test1(int hGap, int nGap)
 /* test - Test something. */
 {
-struct hash *ra = raFromString(testRa);
-struct hashEl *elList = hashElListHash(ra), *el;
-for (el = elList; el != NULL; el = el->next)
-    printf("%s '%s'\n", el->name, (char*)el->val);
+// printf("logBaseTwo(1000) %d, log(1000) %f\n", digitsBaseTwo(1000),  log(1000));
+printf("%d %d: orig %d, old %d, new %d\n", 
+	hGap, nGap, ffCalcGapPenalty(hGap, nGap, ffCdna) + 4,
+	2*ffCalcGapPenalty(hGap, nGap, ffCdna) + 4,
+	trimGapPenalty(hGap, nGap)
+	);
+}
+
+void test(int hGap, int nGap)
+/* test - Test something. */
+{
+int n,h;
+for (n = 0;  n<=1; n++)
+    for (h=1; h<=1500000; h *= 2)
+        test1(h, n);
+    
 }
 
 int main(int argc, char *argv[])
 /* Process command line. */
 {
 optionHash(&argc, argv);
-if (argc != 2)
+if (argc != 3)
     usage();
-test(argv[1]);
+test(atoi(argv[1]), atoi(argv[2]));
 return 0;
 }
