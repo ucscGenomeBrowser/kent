5c46b3ac8e9ebaaacfbe2418752a3d6b8e7ef6c4
kent <kent>
2003-09-13 04:15:47 +0000

    Testing makeQuotedString.

--- src/oneShot/test/test.c
+++ src/oneShot/test/test.c
@@ -3,7 +3,8 @@
 #include "linefile.h"
 #include "hash.h"
 #include "options.h"
-#include "fuzzyFind.h"
+#include "memalloc.h"
+#include "obscure.h"
 
 void usage()
 /* Explain usage and exit. */
@@ -11,61 +12,28 @@ void usage()
 errAbort(
   "test - Test something\n"
   "usage:\n"
-  "   test hGap nGap\n"
+  "   test something\n"
   "options:\n"
   );
 }
 
-
-static int trimGapPenalty(int hGap, int nGap, char *iStart, char *iEnd, int orientation)
-/* Calculate gap penalty for routine below. */
-{
-int penalty =  ffCalcGapPenalty(hGap, nGap, ffCdna);
-if (hGap > 2 || nGap > 2)	/* Not just a local extension. */
-				/* Score gap to favor introns. */
-    {
-    penalty <<= 1;
-    if (nGap > 0)	/* Intron gaps are not in n side at all. */
-	 penalty += 3;
-    			/* Good splice sites give you bonus 2,
-			 * bad give you penalty of six. */
-    penalty += 6 - 2*ffScoreIntron(iStart[0], iStart[1], 
-    	iEnd[-2], iEnd[-1], orientation);
-    }
-return penalty;
-}
-
-
-
-void test1(int hGap, int nGap)
+void test(char *s)
 /* test - Test something. */
 {
-char *consensus = "agtag";
-
-// printf("logBaseTwo(1000) %d, log(1000) %f\n", digitsBaseTwo(1000),  log(1000));
-printf("%d %d: orig %d, old %d, new %d\n", 
-	hGap, nGap, ffCalcGapPenalty(hGap, nGap, ffCdna) + 4,
-	2*ffCalcGapPenalty(hGap, nGap, ffCdna) + 4,
-	trimGapPenalty(hGap, nGap, consensus, consensus+4, 1)
-	);
-}
-
-void test(int hGap, int nGap)
-/* test - Test something. */
-{
-int n,h;
-for (n = 0;  n<=1; n++)
-    for (h=1; h<=1500000; h *= 2)
-        test1(h, n);
-    
+printf("%s\n", s);
+s = makeQuotedString(s, '"');
+printf("%s\n", s);
+freez(&s);
 }
 
 int main(int argc, char *argv[])
 /* Process command line. */
 {
+pushCarefulMemHandler(100000);
 optionHash(&argc, argv);
-if (argc != 3)
+if (argc != 2)
     usage();
-test(atoi(argv[1]), atoi(argv[2]));
+test(argv[1]);
+carefulCheckHeap();
 return 0;
 }
