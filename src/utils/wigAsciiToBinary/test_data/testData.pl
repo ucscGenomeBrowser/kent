2d6afead6ae819c137fd46efe3209d0abde2c5c4
hiram <hiram>
2003-09-16 00:02:24 +0000

    Updated with sparse data example

--- src/utils/wigAsciiToBinary/test_data/testData.pl
+++ src/utils/wigAsciiToBinary/test_data/testData.pl
@@ -11,19 +11,29 @@ use POSIX;	#	to define floor()
 my $argc = @ARGV;	#	argument count
 
 if ( $argc < 1 ) {
-	print "usage: $0 <# of data points> [frequency]\n";
+	print "usage: $0 <# of data points> [frequency [dataSpan]]\n";
 	print "\t# of data points - generate this many numbers\n";
 	print "\tfrequency - if given, this frequency over the range\n";
 	print "\t\tdefault is 1\n";
+	print "\tdataSpan - # of bases to each value, default 1\n";
+	print "\t\tmust have frequency for this third arg to work\n";
 	exit(255);
 }
-
-my $count = shift;	#	number of points to generate
-my $frequency = 1;
-if( $argc == 2 ) {
-	$frequency = shift;
+for( my $i = 0; $i < $argc; ++$i ) {
+	printf STDERR "arg %d: %s\n", $i, $ARGV[$i];
 }
 
+my $count = shift;		#	number of points to generate
+my $frequency = 1;
+my $dataSpan = 1;
+if( $argc > 1 ) {
+	$frequency = shift;
+}
+if( $argc == 3 ) {
+	$dataSpan = shift;
+}
+
+printf STDERR "Generating $count points, at frequency $frequency, span $dataSpan\n";
 if( $count < 2 ) {
 	print "ERROR: number of points must be > 1\n";
 	exit(255);
@@ -44,10 +54,10 @@ my $Radians;
 my $Scale = 128/2.001;
 my $ByteValue;
 
-my $i = 0;
+my $i = 1;
 for( my $j = 0; $j < 360; $j += $incr ) {
-	++$i;
 	$Radians = $frequency * ((2.0 * $PI) * $j) / 360;
 	$ByteValue = floor((1.0 + sin($Radians)) * $Scale);
 	printf "%d\t%d\n", $i, $ByteValue;
+	$i += $dataSpan;
 }
