09242fce5ad81d40c21b0987581b5f573c16135f
braney <braney>
2003-09-20 01:28:33 +0000

    add entry point for drop down with style sheet

--- src/hg/inc/hui.h
+++ src/hg/inc/hui.h
@@ -57,6 +57,9 @@ enum trackVisibility hTvFromStringNoAbort(char *s);
 char *hStringFromTv(enum trackVisibility vis);
 /* Given enum representation convert to string. */
 
+void hTvDropDownClass(char *varName, enum trackVisibility vis, boolean canPack, char *class);
+/* Make track visibility drop down for varName with style class */
+
 void hTvDropDown(char *varName, enum trackVisibility vis, boolean canPack);
 /* Make track visibility drop down for varName */
 
