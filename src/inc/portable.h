fa16f657c1173eb8fb52ea61d129755d49eb4e84
kent <kent>
2003-09-24 02:29:21 +0000

    Adding getUser.  Swapping prototypes and comments to be like rest of system.

--- src/inc/portable.h
+++ src/inc/portable.h
@@ -10,9 +10,9 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 
+struct slName *listDir(char *dir, char *pattern);
 /* Return an alphabetized list of all files that match 
  * the wildcard pattern in directory. */
-struct slName *listDir(char *dir, char *pattern);
 
 struct fileInfo 
 /* Info about a file. */
@@ -23,37 +23,37 @@ struct fileInfo
     char name[1];	/* Allocated at run time. */
     };
 
-/* Return a new fileInfo. */
 struct fileInfo *newFileInfo(char *name, long size, bool isDir);
+/* Return a new fileInfo. */
 
+struct fileInfo *listDirX(char *dir, char *pattern, boolean fullPath);
 /* Return list of files matching wildcard pattern with
  * extra info. If full path is true then the path will be
  * included in the name of each file.  You can free the
  * resulting list with slFreeList. */
-struct fileInfo *listDirX(char *dir, char *pattern, boolean fullPath);
 
-/* Return current directory. */
 char *getCurrentDir();
+/* Return current directory. */
 
-/* Set current directory.  Return FALSE if it fails. */
 boolean setCurrentDir(char *newDir);
+/* Set current directory.  Return FALSE if it fails. */
 
+boolean makeDir(char *dirName);
 /* Make dir.  Returns TRUE on success.  Returns FALSE
  * if failed because directory exists.  Prints error
  * message and aborts on other error. */
-boolean makeDir(char *dirName);
 
-/* 1000 hz clock */
 long clock1000();
+/* 1000 hz clock */
 
-/* Sleep for given number of milliseconds. */
 void sleep1000(int milli);
+/* Sleep for given number of milliseconds. */
 
-/* A 1 hz clock. */
 long clock1();
+/* A 1 hz clock. */
 
-/* Make a temp name that's almost certainly unique. */
 char *rTempName(char *dir, char *base, char *suffix);
+/* Make a temp name that's almost certainly unique. */
 
 /* This structure helps us generate temp names and use
  * them.  Since different servers locate where the cgi
@@ -65,26 +65,29 @@ struct tempName
 	char forHtml[128];
 	};
 
-/* Make a good name for a temp file. */
 void makeTempName(struct tempName *tn, char *base, char *suffix);
+/* Make a good name for a temp file. */
 
-/* Return directory to look for cgi in. */
 char *cgiDir();
+/* Return directory to look for cgi in. */
 
-/* Return cgi suffix. */
 char *cgiSuffix();
+/* Return cgi suffix. */
 
-/* Return relative speed of machine.  UCSC CSE dept. 1999 web server is 1.0 */
 double machineSpeed();
+/* Return relative speed of machine.  UCSC CSE dept. 1999 web server is 1.0 */
 
-/* Return host computer on network for mySQL database. */
 char *mysqlHost();
+/* Return host computer on network for mySQL database. */
 
-/* Get name of this machine. */
 char *getHost();
+/* Get name of this machine. */
 
-/* Invoke the debugger. */
 void uglyfBreak();
+/* Invoke the debugger. */
+
+char *getUser();
+/* Get user name */
 
 #endif /* PORTABLE_H */
 
