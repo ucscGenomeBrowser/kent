bf5471e467c6258f0bdc7199ae109ade2a85746c
braney <braney>
2003-09-29 20:00:32 +0000

    a program to convert pslx files to fasta files

---
+++ src/hg/pslxToFa/pslxToFa.c
@@ -0,0 +1,49 @@
+/* pslxToFa - convert pslx to fasta file. */
+#include "common.h"
+#include "linefile.h"
+#include "hash.h"
+#include "options.h"
+#include "psl.h"
+
+void usage()
+/* Explain usage and exit. */
+{
+errAbort(
+  "pslxToFa - convert pslx to fasta file\n"
+  "usage:\n"
+  "   pslxToFa XXX\n"
+  "options:\n"
+  "   -xxx=XXX\n"
+  );
+}
+
+static struct optionSpec options[] = {
+   {NULL, 0},
+};
+
+void pslxToFa(char *pslName, char *faName)
+/* pslxToFa - convert pslx to fasta file. */
+{
+struct lineFile *in = pslFileOpen(pslName);
+FILE *out = mustOpen(faName, "w");
+struct psl *psl;
+
+while ((psl = pslNext(in)) != NULL)
+    {
+    int ii=0;
+    fprintf(out,"%s\t%c\t%d\t%d\t%d\n>%s\n",psl->qName, psl->strand[1], psl->blockCount,psl->blockSizes[0], psl->tBaseInsert, psl->qSequence[0]);
+    for(ii=1; ii < psl->blockCount; ii++)
+	fprintf(out,">%s\t%d\t%d\t%d\n%s\n",psl->qName, ii,psl->blockSizes[ii], psl->tStarts[ii] - (psl->tStarts[ii -1] + psl->blockSizes[ii-1]), psl->qSequence[ii]);
+    pslFree(&psl);
+    }
+}
+
+int main(int argc, char *argv[])
+/* Process command line. */
+{
+optionInit(&argc, argv, options);
+if (argc != 3)
+    usage();
+pslxToFa(argv[1], argv[2]);
+return 0;
+}
