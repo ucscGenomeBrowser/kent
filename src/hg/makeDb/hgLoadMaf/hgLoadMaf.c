83e98ddb4dd44af1b8bce8732a6f287f4d1c0329
kate <kate>
2003-10-02 05:23:42 +0000

    1. Check for invalid (nan) maf scores.  2. Add -test option to suppress database loading

--- src/hg/makeDb/hgLoadMaf/hgLoadMaf.c
+++ src/hg/makeDb/hgLoadMaf/hgLoadMaf.c
@@ -12,7 +12,7 @@
 #include "scoredRef.h"
 #include "dystring.h"
 
-static char const rcsid[] = "$Id: hgLoadMaf.c,v 1.8 2003/07/28 22:56:20 kate Exp $";
+static char const rcsid[] = "$Id: hgLoadMaf.c,v 1.9 2003/10/02 05:23:42 kate Exp $";
 
 /* Command line options */
 
@@ -23,6 +23,8 @@ static char const rcsid[] = "$Id: hgLoadMaf.c,v 1.8 2003/07/28 22:56:20 kate Exp
  */
 boolean warnOption = FALSE;
 boolean warnVerboseOption = FALSE;     /* print warning detail */
+boolean test = FALSE;                   /* suppress loading .tab file in db */
+char *testFile = "";                    /* maf filename if testing */
 
 void usage()
 /* Explain usage and exit. */
@@ -35,8 +37,10 @@ errAbort(
     "   -warn   warn instead of error if empty/incomplete alignments\n"
     "           are found found.\n"
     "   -WARN   warn instead of error, with detail for the warning\n" 
+    "   -test=infile  use infile as input, and suppress loading\n"
+"                     the database. Just create .tab file in current dir.\n" 
   "The maf files need to already exist in chromosome coordinates\n"
-  "in the directory /gbdb/database/table.\n"
+  "in the directory /gbdb/database/table, unless -test option is used.\n"
   );
 }
 
@@ -62,12 +66,25 @@ struct fileInfo *fileList = NULL, *fileEl;
 struct sqlConnection *conn;
 long mafCount = 0;
 FILE *f = hgCreateTabFile(".", table);
+char ext[10];
+char file[100];
 
-hSetDb(database);
-safef(extFileDir, sizeof(extFileDir), "/gbdb/%s/%s", database, table);
-fileList = listDirX(extFileDir, "*.maf", TRUE);
-conn = hgStartUpdate();
-scoredRefTableCreate(conn, table);
+if (test)
+    {
+    if (!fileExists(testFile))
+        errAbort("Test file %s doesn't exist\n", testFile);
+    splitPath(testFile, extFileDir, file, ext);
+    strcat(file, ext);
+    fileList = listDirX(extFileDir, file, TRUE);
+    }
+else
+    {
+    hSetDb(database);
+    safef(extFileDir, sizeof(extFileDir), "/gbdb/%s/%s", database, table);
+    fileList = listDirX(extFileDir, "*.maf", TRUE);
+    conn = hgStartUpdate();
+    scoredRefTableCreate(conn, table);
+    }
 if (fileList == NULL)
     errAbort("%s doesn't exist or doesn't have any maf files", extFileDir);
 for (fileEl = fileList; fileEl != NULL; fileEl = fileEl->next)
@@ -79,31 +96,36 @@ for (fileEl = fileList; fileEl != NULL; fileEl = fileEl->next)
     struct mafAli *maf;
     off_t offset;
     int warnCount = 0;
-    HGID extId = hgAddToExtFile(fileName, conn);
     int dbNameLen = strlen(database);
+    HGID extId;
+    if (test)
+        extId = 0;
+    else
+        extId = hgAddToExtFile(fileName, conn);
 
     mf = mafOpen(fileName);
     printf("Indexing and tabulating %s\n", fileName);
     while ((maf = mafNextWithPos(mf, &offset)) != NULL)
         {
 	double maxScore, minScore;
-	mafColMinMaxScore(maf, &minScore, &maxScore);
 	++mafCount;
 	mc = findComponent(maf, database);
 	if (mc == NULL) 
             {
+            char msg[100];
+            safef(msg, sizeof(msg),
+                            "Couldn't find %s. sequence line %d of %s\n", 
+	    	                database, mf->lf->lineIx, fileName);
             if (warnOption || warnVerboseOption) 
                 {
                 warnCount++;
                 if (warnVerboseOption)
-                    printf("Couldn't find %s. sequence line %d of %s\n", 
-	    	                database, mf->lf->lineIx, fileName);
+                    printf(msg);
                 mafAliFree(&maf);
                 continue;
                 }
             else 
-                errAbort("Couldn't find %s. sequence line %d of %s\n", 
-	    	                database, mf->lf->lineIx, fileName);
+                errAbort(msg);
             }
 	ZeroVar(&mr);
 	mr.chrom = mc->src + dbNameLen + 1;
@@ -113,10 +135,54 @@ for (fileEl = fileList; fileEl != NULL; fileEl = fileEl->next)
 	    reverseIntRange(&mr.chromStart, &mr.chromEnd, mc->srcSize);
 	mr.extFile = extId;
 	mr.offset = offset;
+
+        /* The maf scores are the sum of all pairwise
+         * alignment scores.  If you have A,B,C,D
+         * species in the alignments it's the sum of
+         * the AB, AC, AD, BC, BD, CD pairwise scores.
+         * If you just had two species it would just
+         * be a single pairwise score.  Since we don't
+         * want to penalize things for missing species,
+         * we basically divide by the number of possible
+         * pairings.  To get it from the -100 to +100 (more or
+         * less) per base blastz scoring scheme to something
+         * that goes from 0 to 1, we also also divide by 200.
+         * JK */
+
+        /* scale using alignment size to get per-base score */
 	mr.score = maf->score/mc->size;
-	mr.score = (mr.score-minScore)/(maxScore-minScore);
+
+        /* get scoring range -- depends on #species in alignment */
+        mafColMinMaxScore(maf, &minScore, &maxScore);
+        if (maxScore == minScore) 
+            /* protect against degenerate case -- 1 species, produces
+             * max & min score of 0 */
+            mr.score = 0.0;
+        else
+            /* translate to get zero-based score, 
+             * then scale by blastz scoring range */
+            mr.score = (mr.score-minScore)/(maxScore-minScore);
+	if (mr.score <= 0.0) 
+            {
+            char msg[100];
+            safef(msg, sizeof(msg),
+                    "Score too small (raw %.1f scaled %.1f #species %d),"
+                           " line %d of %s\n", 
+                        maf->score, mr.score, slCount(maf->components),
+                                mf->lf->lineIx, fileName);
+            if (warnOption || warnVerboseOption) 
+                {
+                warnCount++;
+                if (warnVerboseOption)
+                    printf(msg);
+                }
+            else
+                errAbort(msg);
+            mafAliFree(&maf);
+            /* don't add this alignment to tab file */
+            continue;
+            }
 	if (mr.score > 1.0) mr.score = 1.0;
-	if (mr.score < 0.0) mr.score = 0.0;
 	fprintf(f, "%u\t", hFindBin(mr.chromStart, mr.chromEnd));
 	scoredRefTabOut(&mr, f);
 	mafAliFree(&maf);
@@ -125,6 +191,8 @@ for (fileEl = fileList; fileEl != NULL; fileEl = fileEl->next)
     if (warnCount)
         printf("%d warnings\n", warnCount);
     }
+if (test)
+    return;
 printf("Loading %s into database\n", table);
 hgLoadTabFile(conn, ".", table, &f);
 hgEndUpdate(&conn, "Add %ld mafs in %d files from %s", mafCount, slCount(fileList), extFileDir);
@@ -138,6 +206,8 @@ if (argc != 3)
     usage();
 warnOption = cgiBoolean("warn");
 warnVerboseOption = cgiBoolean("WARN");
+test = cgiVarExists("test");
+testFile = cgiOptionalString("test");
 hgLoadMaf(argv[1], argv[2]);
 return 0;
 }
