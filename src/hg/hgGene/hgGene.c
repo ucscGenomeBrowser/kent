c575aebfdce195f4d62eda30b73c81c0b18c14c0
kent <kent>
2003-10-11 00:34:02 +0000

    Adding more infrastructure including a data directory with organism and assembly subdirectories.  Printing gene name and description.

--- src/hg/hgGene/hgGene.c
+++ src/hg/hgGene/hgGene.c
@@ -13,14 +13,15 @@
 #include "ra.h"
 #include "hgGene.h"
 
-static char const rcsid[] = "$Id: hgGene.c,v 1.1 2003/10/10 21:59:59 kent Exp $";
+static char const rcsid[] = "$Id: hgGene.c,v 1.2 2003/10/11 00:34:00 kent Exp $";
 
 /* ---- Global variables. ---- */
 struct cart *cart;	/* This holds cgi and other variables between clicks. */
 struct hash *oldCart;	/* Old cart hash. */
 char *database;		/* Name of genome database - hg15, mm3, or the like. */
 char *genome;		/* Name of genome - mouse, human, etc. */
-char *geneId;		/* Name of gene. */
+char *geneId;		/* Symbolic ID of gene. */
+char *geneName;		/* Biological name of gene. */
 struct hash *genomeSettings;  /* Genome-specific settings from settings.ra. */
 
 
@@ -39,15 +40,117 @@ errAbort(
   );
 }
 
+/* --------------- Low level utility functions. ----------------- */
+
+static char *rootDir = "hgGeneData";
+
+struct hash *readRa(char *rootName)
+/* Read in ra in root, root/org, and root/org/database. */
+{
+return hgReadRa(genome, database, rootDir, rootName);
+}
+
+char *genomeSetting(char *name)
+/* Return genome setting value.   Aborts if setting not found. */
+{
+return hashMustFindVal(genomeSettings, name);
+}
+
+static void getGenomeSettings()
+/* Set up genome settings hash */
+{
+struct hash *hash = readRa("genome.ra");
+char *name;
+if (hash == NULL)
+    errAbort("Can't find anything in genome.ra");
+name = hashMustFindVal(hash, "name");
+if (!sameString(name, "global"))
+    errAbort("Can't find global ra record in genome.ra");
+genomeSettings = hash;
+}
+
+/* --------------- HTML Helpers ----------------- */
+
+void hvPrintf(char *format, va_list args)
+/* Print out some html.  Check for write error so we can
+ * terminate if http connection breaks. */
+{
+vprintf(format, args);
+if (ferror(stdout))
+    noWarnAbort();
+}
+
+void hPrintf(char *format, ...)
+/* Print out some html.  Check for write error so we can
+ * terminate if http connection breaks. */
+{
+va_list(args);
+va_start(args, format);
+hvPrintf(format, args);
+va_end(args);
+}
+
+void hPrintNonBreak(char *s)
+/* Print out string but replace spaces with &nbsp; */
+{
+char c;
+while (c = *s++)
+    {
+    if (c == ' ')
+	fputs("&nbsp;", stdout);
+    else
+        putchar(c);
+    }
+}
+
+/* --------------- Mid-level utility functions ----------------- */
+
+char *genoQuery(char *id, char *settingName, struct sqlConnection *conn)
+/* Look up sql query in genome.ra given by settingName,
+ * plug id into it, and return. */
+{
+char query[256];
+char *sql = genomeSetting(settingName);
+safef(query, sizeof(query), sql, id);
+return sqlQuickString(conn, query);
+}
+
+char *getGeneName(char *id, struct sqlConnection *conn)
+/* Return gene name associated with ID.  Freemem
+ * this when done. */
+{
+char *name = genoQuery(id, "nameSql", conn);
+if (name == NULL)
+    name = cloneString(id);
+return name;
+}
+
+/* --------------- Page printers ----------------- */
+
+void printDescription(char *id, struct sqlConnection *conn)
+/* Print out description of gene given ID. */
+{
+char *description = genoQuery(id, "descriptionSql", conn);
+if (description == NULL)
+    description = "No description available";
+hPrintf("%s", description);
+freeMem(description);
+}
 
 void cartMain(struct cart *theCart)
 /* We got the persistent/CGI variable cart.  Now
  * set up the globals and make a web page. */
 {
+struct sqlConnection *conn;
 cart = theCart;
 getDbAndGenome(cart, &database, &genome);
+hSetDb(database);
+conn = hAllocConn();
 geneId = cartString(cart, hggGene);
-cartWebStart(cart, "%s Gene %s Details", genome, geneId);
+getGenomeSettings();
+geneName = getGeneName(geneId, conn);
+cartWebStart(cart, "%s Gene %s Details", genome, geneName);
+printDescription(geneId, conn);
 cartWebEnd();
 }
 
