b6b5eacdb23ed83ba18536e478132534fb5ac01d
kent <kent>
2003-10-14 05:35:01 +0000

    Fixed memory problem in spDb.c

--- src/hg/lib/spDb.c
+++ src/hg/lib/spDb.c
@@ -6,7 +6,7 @@
 #include "jksql.h"
 #include "spDb.h"
 
-static char const rcsid[] = "$Id: spDb.c,v 1.3 2003/10/12 19:35:44 kent Exp $";
+static char const rcsid[] = "$Id: spDb.c,v 1.4 2003/10/14 05:35:01 kent Exp $";
 
 boolean spIsPrimaryAcc(struct sqlConnection *conn, char *acc)
 /* Return TRUE if this is a primary accession in database. */
@@ -28,7 +28,7 @@ if (spIsPrimaryAcc(conn, id))
     return cloneString(id);
 acc = spIdToAcc(conn, id);
 if (acc != NULL)
-    return acc;
+    return cloneString(acc);
 else
     {
     char query[256];
