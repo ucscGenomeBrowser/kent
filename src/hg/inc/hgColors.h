6d92055cdf6897a4b459cbb190cd21cf9d53f3f8
kent <kent>
2003-10-30 08:32:31 +0000

    Making symbolic names for the various shades of cream and blue.

---
+++ src/hg/inc/hgColors.h
@@ -0,0 +1,12 @@
+/* hgColors - colors used in UCSC Genome Browser pages. */
+
+#define HG_COL_HOTLINKS "2636D1"
+#define HG_COL_HEADER "D9E4F8"
+#define HG_COL_OUTSIDE "FFF9D2"
+#define HG_CL_OUTSIDE 0xFFF9D2
+#define HG_COL_INSIDE "FFFEE8"
+#define HG_CL_INSIDE 0xFFFEE8
+#define HG_COL_BORDER "888888"
+#define HG_COL_TABLE "D9F8E4"
+#define HG_COL_TABLE_LABEL "1616D1"
+
