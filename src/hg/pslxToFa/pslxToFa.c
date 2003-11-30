1f3c9cde6edef2c1a622bcaf0a920da070ce4ec4
braney <braney>
2003-11-30 18:31:13 +0000

    create lift files to protein (query), and back to genome (target)

--- src/hg/pslxToFa/pslxToFa.c
+++ src/hg/pslxToFa/pslxToFa.c
@@ -13,45 +13,61 @@ errAbort(
   "usage:\n"
   "   pslxToFa in.psl out.fa\n"
   "options:\n"
-  "   -liftFile=liftOut.lft\n"
+  "   -liftTarget=liftTarget.lft\n"
+  "   -liftQuery=liftQuery.lft\n"
   );
 }
 
 static struct optionSpec options[] = {
-    {"liftFile", OPTION_STRING},
+    {"liftTarget", OPTION_STRING},
+    {"liftQuery", OPTION_STRING},
    {NULL, 0},
 };
 
-void pslxToFa(char *pslName, char *faName, char *liftName)
+void pslxToFa(char *pslName, char *faName, char *liftTargetName, char *liftQueryName)
 /* pslxToFa - convert pslx to fasta file. */
 {
-FILE *lift = NULL;
+FILE *liftTarget = NULL;
+FILE *liftQuery = NULL;
 struct lineFile *in = pslFileOpen(pslName);
 FILE *out = mustOpen(faName, "w");
 struct psl *psl;
 
-if (liftName != NULL)
-    lift = mustOpen(liftName, "w");
+if (liftQueryName != NULL)
+    liftQuery = mustOpen(liftQueryName, "w");
+
+if (liftTargetName != NULL)
+    liftTarget = mustOpen(liftTargetName, "w");
 
 while ((psl = pslNext(in)) != NULL)
     {
     int ii=0;
-    int sum = 0;
-    if (lift != NULL)
+    int sumQuery = 0;
+    if (liftQuery != NULL)
 	{
-	fprintf(lift,"%d\t%s/%s_%d_%d\t%d\t%s\t%d\n",
+	fprintf(liftQuery,"%d\t%s/%s_%d_%d\t%d\t%s\t%d\n",
 		0, "1", psl->qName,0,psl->blockCount, strlen(psl->qSequence[0]), psl->qName, psl->qSize);
-	sum += strlen(psl->qSequence[0]);
+	sumQuery += strlen(psl->qSequence[0]);
+	}
+    if (liftTarget != NULL)
+	{
+	fprintf(liftTarget,"%d\t%s/%s_%d_%d\t%d\t%s\t%d\n",
+		psl->tStarts[0], &psl->tName[3], psl->qName,0,psl->blockCount, strlen(psl->qSequence[0]), psl->tName, psl->tSize);
 	}
     fprintf(out,">%s_%d_%d\n%s\n",psl->qName, 0, psl->blockCount, psl->qSequence[0]);
 
     for(ii=1; ii < psl->blockCount; ii++)
 	{
-	if (lift != NULL)
+	if (liftTarget != NULL)
 	    {
-	    fprintf(lift,"%d\t%s/%s_%d_%d\t%d\t%s\t%d\n",
-		sum, "1", psl->qName,ii,psl->blockCount, strlen(psl->qSequence[ii]), psl->qName, psl->qSize);
-	    sum += strlen(psl->qSequence[ii]);
+	    fprintf(liftTarget,"%d\t%s/%s_%d_%d\t%d\t%s\t%d\n",
+		psl->tStarts[ii], &psl->tName[3], psl->qName,ii,psl->blockCount, strlen(psl->qSequence[ii]), psl->tName, psl->tSize);
+	    }
+	if (liftQuery != NULL)
+	    {
+	    fprintf(liftQuery,"%d\t%s/%s_%d_%d\t%d\t%s\t%d\n",
+		sumQuery, "1", psl->qName,ii,psl->blockCount, strlen(psl->qSequence[ii]), psl->qName, psl->qSize);
+	    sumQuery += strlen(psl->qSequence[ii]);
 	    }
 	fprintf(out,">%s_%d_%d\n%s\n",psl->qName, ii, psl->blockCount,  psl->qSequence[ii]);
 	}
@@ -62,12 +78,13 @@ while ((psl = pslNext(in)) != NULL)
 int main(int argc, char *argv[])
 /* Process command line. */
 {
-char *liftFile;
+char *liftTarget, *liftQuery;
 
 optionInit(&argc, argv, options);
-liftFile = optionVal("liftFile", NULL);
+liftTarget = optionVal("liftTarget", NULL);
+liftQuery = optionVal("liftQuery", NULL);
 if (argc != 3)
     usage();
-pslxToFa(argv[1], argv[2], liftFile);
+pslxToFa(argv[1], argv[2], liftTarget, liftQuery);
 return 0;
 }
