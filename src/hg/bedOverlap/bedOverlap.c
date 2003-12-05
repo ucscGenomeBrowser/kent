630e0ca371e7aab4aabe7948405d5be199172465
baertsch <baertsch>
2003-12-05 20:35:09 +0000

    use score instead of length

--- src/hg/bedOverlap/bedOverlap.c
+++ src/hg/bedOverlap/bedOverlap.c
@@ -2,7 +2,6 @@
 #include "linefile.h"
 #include "hash.h"
 #include "options.h"
-#include "jksql.h"
 #include "hdb.h"
 #include "dnautil.h"
 #include "bed.h"
@@ -13,87 +12,99 @@ void usage()
 /* Explain usage and exit. */
 {
 errAbort(
-  "bedOverlap -  Remove Overlaps from bed files\n"
+  "bedOverlap -  Remove Overlaps from bed files - choose highest scoring bed\n"
   "usage:\n"
-  "   bedOverlap db track output.bed\n"
+  "   bedOverlap infile.bed output.bed\n"
   "options:\n"
   "   -xxx \n"
+  "Note: infile.bed must be sorted by chrom, chromStart"
   );
 }
 
-void bedOverlap(char *db , char *table, FILE *f)
+char **cloneRow(char **row, int size)
+{
+int i;
+char **result;
+
+AllocArray(result, size);
+for (i = 0 ; i < size ; i++)
+    {
+    result[i] = cloneString(row[i]);
+    }
+return result;
+}
+
+void outputRow(FILE *f, char **row, int size)
+{
+int i;
+if (row == NULL) return;
+for (i = 0 ; i<size ; i++)
+    fprintf(f,"%s ",row[i]);
+fprintf(f,"\n");
+    
+}
+
+void bedOverlap(char *aFile, FILE *f)
 /* Sort a bed file (in place, overwrites old file. */
 {
-struct lineFile *lf = NULL;
-struct bedLine *blList = NULL, *bl;
-char *line;
 char *prevChrom = cloneString("chr1");
-int lineSize;
-int size  = 300000000;
-char query[256];
-struct sqlConnection *conn;
-struct sqlResult *sr;
-boolean hasBin = 0;
-char **row;
-struct bed *bed, *list, *bestMatch = NULL, *el2;
-int overlap = 0 , bestOverlap = 0, i;
-int biggest = 0;
-int bedSize = 6;
-struct binKeeper *bk;
-struct binElement *el = NULL;
-hSetDb(db);
+int prevStart = 0, prevEnd = 0;
+bool first = TRUE;
 
-    printf("Loading Predictions from %s\n",table);
-    AllocVar(list);
-    conn = hAllocConn();
-    sprintf(query, "select * from %s order by chrom, chromStart\n",table);
-    sr = sqlGetResult(conn, query);
-    bk = binKeeperNew(0, size);
-    while ((row = sqlNextRow(sr)) != NULL)
-        {         
-        bed = bedLoadNBin(row, bedSize);
-        if (sameString(bed->chrom,prevChrom))
+struct lineFile *lf = lineFileOpen(aFile, TRUE);
+char *row[40], **bestMatch = NULL;
+int bestScore = 0, bestCount = 0;
+int i, wordCount;
+struct binElement *hitList = NULL, *hit;
+struct binKeeper *bk;
+
+printf("Loading Predictions from %s\n",aFile);
+while ((wordCount = lineFileChop(lf, row)) != 0)
+    {
+    char *name = row[0];
+    int start = lineFileNeedNum(lf, row, 1);
+    int end = lineFileNeedNum(lf, row, 2);
+    int score = lineFileNeedNum(lf, row, 4);
+
+    if (first || (sameString(name,prevChrom) && start <= prevEnd))
+        {
+        if (first)
             {
-            binKeeperAdd(bk, bed->chromStart, bed->chromEnd, bed);
-            slAddHead(&list, bed);
+            prevChrom = cloneString(name);
+            prevStart = start;
+            prevEnd = end;
+            first = FALSE;
             }
-        else
+        if (score > bestScore)
             {
-            //slReverse(&list);
-            for (el2 = list ; el2 != NULL; el2 = el2->next)
-                {
-                biggest = 0; 
-                bestMatch = NULL;
-                for (el = binKeeperFindSorted(bk, el2->chromStart, el2->chromEnd); el != NULL; el = el->next)
-                    {
-                    assert(el != NULL);
-                    bed = el->val;
-                    if (bed->chromEnd - bed->chromStart > biggest && bed->score > 0)
-                            {
-                            biggest  = bed->chromEnd - bed->chromStart;
-                            bestMatch = cloneBed(bed);
-                            assert(bestMatch->strand[0] == '+' || bestMatch->strand[0] == '-' || bestMatch->strand[0] == '0');
-                            }
-                    }
-                    if (bestMatch != NULL)
-                        fprintf(f, "%s\t%d\t%d\t%s\t%d\t%c\n", 
-                                bestMatch->chrom, bestMatch->chromStart, bestMatch->chromEnd, 
-                                bestMatch->name, bestMatch->score, bestMatch->strand[0]);
-                    if (ferror(f))
-                        {
-                        perror("Writing error\n");
-                        errAbort(" file is truncated, sorry.");
-                        }
-                }
-            binKeeperFree(&bk);
-            bk = binKeeperNew(0, size);
-            bed = bedLoadNBin(row, 4);
-            binKeeperAdd(bk, bed->chromStart, bed->chromEnd, bed);
-            } 
-        prevChrom = cloneString(bed->chrom);
+            bestMatch = cloneRow(row, wordCount);
+            bestScore = score;
+            bestCount = wordCount;
+            }
+        prevEnd = max(prevEnd, end);
         }
-    sqlFreeResult(&sr);
-    hFreeConn(&conn);
+    else
+        {
+        if (bestMatch != NULL)
+            outputRow(f, bestMatch, bestCount);
+        for (i = 0; i <bestCount ; i++)
+            freeMem(bestMatch[i]);
+        bestMatch = NULL;
+        if (ferror(f))
+            {
+            perror("Writing error\n");
+            errAbort(" file is truncated, sorry.");
+            }
+        bestScore = score;
+        bestCount = wordCount;
+        prevChrom = cloneString(name);
+        prevStart = start;
+        prevEnd = end;
+        bestMatch = cloneRow(row, wordCount);
+        } 
+    }
+    if (bestMatch != NULL)
+        outputRow(f, bestMatch, bestCount);
 fclose(f);
 }
 
@@ -101,9 +112,9 @@ int main(int argc, char *argv[])
 /* Process command line. */
 {
 FILE *f;
-if (argc != 4)
+if (argc != 3)
     usage();
-f = mustOpen(argv[3], "w");
-bedOverlap(argv[1], argv[2], f);
+f = mustOpen(argv[2], "w");
+bedOverlap(argv[1], f);
 return 0;
 }
