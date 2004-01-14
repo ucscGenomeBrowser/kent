2cfb368f6013d273bb4e2feca7c8accf892743fd
markd <markd>
2004-01-14 02:55:42 +0000

    allow program to use genePred files as well as tables

--- src/hg/getRnaPred/getRnaPred.c
+++ src/hg/getRnaPred/getRnaPred.c
@@ -8,7 +8,7 @@
 #include "hdb.h"
 #include "options.h"
 
-static char const rcsid[] = "$Id: getRnaPred.c,v 1.6 2003/05/06 07:22:19 kate Exp $";
+static char const rcsid[] = "$Id: getRnaPred.c,v 1.7 2004/01/14 02:55:41 markd Exp $";
 
 void usage()
 /* Explain usage and exit. */
@@ -17,89 +17,139 @@ errAbort(
   "getRnaPred - Get RNA for gene predictions\n"
   "usage:\n"
   "   getRnaPred database table chromosome output.fa\n"
+  "table can be a table or a file.  Specify chromosome of 'all' to\n"
+  "to process all chromosome\n"
+  "\n"
   "options:\n"
   "   -weird Only get ones with weird splice sites\n"
   );
 }
 
-void getRna(char *table, char *chrom, char *faOut)
-/* getRna - Get RNA for gene predictions and write to file. */
-{
-boolean weird = optionExists("weird");
-int rowOffset;
-char **row;
-int i, start, end, size;
-struct genePred *gp;
-struct dnaSeq *seq;
-struct dyString *dna = dyStringNew(16*1024);
-FILE *f = mustOpen(faOut, "w");
-struct sqlConnection *conn = hAllocConn();
-struct sqlResult *sr = hChromQuery(conn, table, chrom, NULL, &rowOffset);
-/* Open file and verify it's in good nibble format. */
-while ((row = sqlNextRow(sr)) != NULL)
-    {
-    /* Load gene prediction from database. */
-    gp = genePredLoad(row+rowOffset);
+static struct optionSpec options[] = {
+   {"weird", OPTION_BOOLEAN},
+   {NULL, 0},
+};
 
-    /* Load exons one by one into dna string. */
-    dyStringClear(dna);
-    if (weird)
+
+/* parsed from command line */
+boolean weird;
+
+boolean hasWeirdSplice(struct genePred *gp)
+/* see if a gene has weird splice sites */
+{
+int i;
+boolean gotOdd = FALSE;
+for (i=1; (i<gp->exonCount) && !gotOdd; ++i)
+    {
+    int start = gp->exonEnds[i-1];
+    int end = gp->exonStarts[i];
+    int size = end - start;
+    if (size > 0)
         {
-	boolean gotOdd = FALSE;
-	for (i=1; i<gp->exonCount; ++i)
-	    {
-	    start = gp->exonEnds[i-1];
-	    end = gp->exonStarts[i];
-	    size = end - start;
-	    if (size > 0)
-		{
-		DNA *s, *e;
-		seq = hDnaFromSeq(chrom, start, end, dnaLower);
-		s = seq->dna;
-		e = seq->dna + seq->size;
-		uglyf("%s %c%c/%c%c\n", gp->name, s[0], s[1], e[-2], e[-1]);
-		if (gp->strand[0] == '-')
-		    reverseComplement(seq->dna, seq->size);
-		if (s[0] != 'g' || (s[1] != 't' && s[1] != 'c') || e[-2] != 'a' || e[-1] != 'g')
-		    gotOdd = TRUE;
-		freeDnaSeq(&seq);
-		}
-	    }
-	if (!gotOdd)
-	    continue;
-	}
-    for (i=0; i<gp->exonCount; ++i)
+        struct dnaSeq *seq = hDnaFromSeq(gp->chrom, start, end, dnaLower);
+        DNA *s = seq->dna;
+        DNA *e = seq->dna + seq->size;
+        uglyf("%s %c%c/%c%c\n", gp->name, s[0], s[1], e[-2], e[-1]);
+        if (gp->strand[0] == '-')
+            reverseComplement(seq->dna, seq->size);
+        if (s[0] != 'g' || (s[1] != 't' && s[1] != 'c') || e[-2] != 'a' || e[-1] != 'g')
+            gotOdd = TRUE;
+        freeDnaSeq(&seq);
+        }
+    }
+return gotOdd;
+}
+
+void processGenePred(struct genePred *gp, struct dyString *dnaBuf, FILE* faFh)
+/* output genePred DNA, check for weird splice sites if requested */
+{
+int i;
+if (weird && !hasWeirdSplice(gp))
+    return;
+
+/* Load exons one by one into dna string. */
+dyStringClear(dnaBuf);
+for (i=0; i<gp->exonCount; ++i)
+    {
+    int start = gp->exonStarts[i];
+    int end = gp->exonEnds[i];
+    int size = end - start;
+    if (size < 0)
+        warn("%d sized exon in %s\n", size, gp->name);
+    else
         {
-	start = gp->exonStarts[i];
-	end = gp->exonEnds[i];
-	size = end - start;
-	if (size < 0)
-	    warn("%d sized exon in %s\n", size, gp->name);
-	else
-	    {
-	    seq = hDnaFromSeq(chrom, start, end, dnaLower);
-	    dyStringAppendN(dna, seq->dna, size);
-	    }
-	freeDnaSeq(&seq);
-	}
+        struct dnaSeq *seq = hDnaFromSeq(gp->chrom, start, end, dnaLower);
+        dyStringAppendN(dnaBuf, seq->dna, size);
+        freeDnaSeq(&seq);
+        }
+    }
 
     /* Reverse complement if necessary and output. */
     if (gp->strand[0] == '-')
-        reverseComplement(dna->string, dna->stringSize);
-    faWriteNext(f, gp->name, dna->string, dna->stringSize);
-    }
+        reverseComplement(dnaBuf->string, dnaBuf->stringSize);
+    faWriteNext(faFh, gp->name, dnaBuf->string, dnaBuf->stringSize);
+}
 
-/* Cleanup and go home. */
-dyStringFree(&dna);
+void getRnaForTable(char *table, char *chrom, struct dyString *dnaBuf, FILE *faFh)
+/* get RNA for a genePred table */
+{
+int rowOffset;
+char **row;
+struct sqlConnection *conn = hAllocConn();
+struct sqlResult *sr = hChromQuery(conn, table, chrom, NULL, &rowOffset);
+while ((row = sqlNextRow(sr)) != NULL)
+    {
+    /* Load gene prediction from database. */
+    struct genePred *gp = genePredLoad(row+rowOffset);
+    processGenePred(gp, dnaBuf, faFh);
+    genePredFree(&gp);
+    }
 sqlFreeResult(&sr);
 hFreeConn(&conn);
 }
 
-void getRnaPred(char *database, char *table, char *chrom, char *faOut)
-/* Set database and call function that does real work. */
+void getRnaForTables(char *table, char *chrom, struct dyString *dnaBuf, FILE *faFh)
+/* get RNA for one for possibly splite genePred table */
 {
+struct slName* chroms = NULL, *chr;
+if (sameString(chrom, "all"))
+    chroms = hAllChromNames();
+else
+    chroms = slNameNew(chrom);
+for (chr = chroms; chr != NULL; chr = chr->next)
+    getRnaForTable(table, chr->name, dnaBuf, faFh);
+}
+
+void getRnaForFile(char *table, char *chrom, struct dyString *dnaBuf, FILE *faFh)
+/* get RNA for a genePred file */
+{
+boolean all = sameString(chrom, "all");
+struct lineFile *lf = lineFileOpen(table, TRUE);
+char *row[GENEPRED_NUM_COLS];
+while (lineFileNextRowTab(lf, row, GENEPRED_NUM_COLS))
+    {
+    struct genePred *gp = genePredLoad(row);
+    if (all || sameString(gp->chrom, chrom))
+        processGenePred(gp, dnaBuf, faFh);
+    genePredFree(&gp);
+    }
+lineFileClose(&lf); 
+}
+
+void getRnaPred(char *database, char *table, char *chrom, char *faOut)
+/* getRna - Get RNA for gene predictions and write to file. */
+{
+struct dyString *dnaBuf = dyStringNew(16*1024);
+FILE *faFh = mustOpen(faOut, "w");
 hSetDb(database);
-getRna(table, chrom, faOut);
+
+if (fileExists(table))
+    getRnaForFile(table, chrom, dnaBuf, faFh);
+else
+    getRnaForTables(table, chrom, dnaBuf, faFh);
+
+dyStringFree(&dnaBuf);
+carefulClose(&faFh);
 }
 
 int main(int argc, char *argv[])
@@ -108,6 +158,7 @@ int main(int argc, char *argv[])
 optionHash(&argc, argv);
 if (argc != 5)
     usage();
+weird = optionExists("weird");
 getRnaPred(argv[1], argv[2], argv[3], argv[4]);
 return 0;
 }
