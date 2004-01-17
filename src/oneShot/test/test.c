69b8404ed62e40d9a6f69156c10612d200c0b7bc
kent <kent>
2004-01-17 19:40:40 +0000

    Testing something else.

--- src/oneShot/test/test.c
+++ src/oneShot/test/test.c
@@ -4,7 +4,7 @@
 #include "hash.h"
 #include "options.h"
 #include "memalloc.h"
-#include "obscure.h"
+#include "memgfx.h"
 
 void usage()
 /* Explain usage and exit. */
@@ -17,33 +17,11 @@ errAbort(
   );
 }
 
-void testMemLevels(int level)
-/* Test memory at a certain level. */
-{
-if (level <= 0)
-    return;
-printf("start level %d - %d blocks allocated\n", level, carefulCountBlocksAllocated());
-memTrackerStart();
-    {
-    char *a = needMem(10000);
-    char *b = needMem(10000);
-    char *c, *d;
-    testMemLevels(level-1);
-    c = needMem(20000);
-    freez(&b);
-    printf("mid level %d - %d blocks allocated\n", level, carefulCountBlocksAllocated());
-    b = needMem(40000);
-    d = needMem(123);
-    printf("late level %d - %d blocks allocated\n", level, carefulCountBlocksAllocated());
-    }
-memTrackerEnd();
-printf("end level %d - %d blocks allocated\n", level, carefulCountBlocksAllocated());
-}
 
 void test(char *s)
 /* test - Test something. */
 {
-testMemLevels(1);
+printf("%d\n", mgFontLineHeight(mgSmallFont()));
 }
 
 int main(int argc, char *argv[])
