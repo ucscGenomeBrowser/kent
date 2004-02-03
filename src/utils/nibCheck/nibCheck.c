7c1f126d84e8456f7689b9c6fae774637fce3d17
heather <heather>
2004-02-03 00:10:40 +0000

    add path

--- src/utils/nibCheck/nibCheck.c
+++ src/utils/nibCheck/nibCheck.c
@@ -76,19 +76,22 @@ void usage()
 {
     printf("nibCheck -- report N regions 100 bases or greater.\n"
            "usage:\n"
-           "   nibCheck chromname\n");
+           "   nibCheck path chromname\n");
 }
 
 int main(int argc, char *argv[])
 {
+  char path[512];
   char chromname[512];
-  if (argc < 2) {
+  if (argc < 3) {
     usage();
     return -1;
   }
+  strcpy(path, "");
+  strcat(path, argv[1]);
   strcpy(chromname, "");
-  strcat(chromname, argv[1]);
-  findNRanges("/gbdb/panTro1/nib/", chromname);
+  strcat(chromname, argv[2]);
+  findNRanges(path, chromname);
   return 0;
 }
 
