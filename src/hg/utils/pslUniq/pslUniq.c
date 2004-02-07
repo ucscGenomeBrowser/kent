3d31ea9f1b048d84e87b742ed2f1f1ffdb524ea9
braney <braney>
2004-02-07 20:28:23 +0000

    added option to allow for N entries (dumb name then eh?)

--- src/hg/utils/pslUniq/pslUniq.c
+++ src/hg/utils/pslUniq/pslUniq.c
@@ -5,20 +5,25 @@
 #include "options.h"
 #include "psl.h"
 
-static char const rcsid[] = "$Id: pslUniq.c,v 1.2 2003/11/05 05:03:50 braney Exp $";
+static char const rcsid[] = "$Id: pslUniq.c,v 1.3 2004/02/07 20:28:23 braney Exp $";
+
+int numAllow = 1;
 
 void usage()
 /* Explain usage and exit. */
 {
-errAbort("usage: pslUniq in.psl out.psl\n");
+errAbort("usage: pslUniq in.psl out.psl\n"
+"   -numAllow=N  how many of each identifier to keep (default 1)\n"
+);
 }
 
-void pslUniq( char *pslName, char *outName)
+void pslUniq( char *pslName, char *outName, int numAllow)
 {
 int size;
 char *name;
 struct psl *psl;
 struct hash *pslHash = newHash(0);
+struct hashEl *hel;
 char *start;
 FILE *out = mustOpen(outName, "w");
 struct lineFile *list;
@@ -26,11 +31,18 @@ struct lineFile *pslF = pslFileOpen(pslName);
 
 while ( psl = pslNext(pslF))
     {
-    if ( !hashLookup(pslHash, psl->qName))
+    if ( (hel = hashLookup(pslHash, psl->qName)) == NULL)
 	{
-	hashAdd(pslHash, psl->qName, psl);
+	hashAdd(pslHash, psl->qName, 1);
 	pslTabOut(psl, out); 
 	}
+    else
+	{
+	hel->val++;
+	if (hel->val <= numAllow)
+	    pslTabOut(psl, out); 
+	}
+
     }
 lineFileClose(&pslF);
 }
@@ -39,8 +51,9 @@ int main(int argc, char *argv[])
 /* Process command line. */
 {
 optionHash(&argc, argv);
+numAllow = optionInt("numAllow", 1);
 if (argc != 3)
     usage();
-pslUniq(argv[1], argv[2]);
+pslUniq(argv[1], argv[2], numAllow);
 return 0;
 }
