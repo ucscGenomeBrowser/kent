17e9f492099b3e0247a17d91740d2659225d647f
daryl <daryl>
2004-02-28 23:16:29 +0000

    Additional formatting and syntax changes from the code review.

--- src/hg/protein/pbCalPi/pbCalPi.c
+++ src/hg/protein/pbCalPi/pbCalPi.c
@@ -1,6 +1,5 @@
 /* pbCalPi - Calculate pI values from a list of protein IDs */
-#include "common.h"
-#include "hdb.h"
+#include "pbCalPi.h"
 
 void usage()
 /* Explain usage and exit. */
@@ -15,59 +14,16 @@ errAbort(
   "example: pbCalPi prot.lis sp040115 pepPi.tab\n");
 }
 
-// the calPi() is based on the parameters and algorithm obtained
-// from SWISS-PROT.  The calculation results should be identical to that
-// from the Web page: http://us.expasy.org/tools/pi_tool.html
-
-#define PH_MIN 0 /* minimum pH value */
-#define PH_MAX 14 /* maximum pH value */
-#define MAXLOOP 2000 /* maximum number of iterations */
-#define EPSI 0.0001 /* desired precision */
-
 float calPi(char *sequence)
+/* the calPi() is based on the parameters and algorithm obtained */
+/* from SWISS-PROT.  The calculation results should be identical to that */
+/* from the Web page: http://us.expasy.org/tools/pi_tool.html */
 {
-// Table of pk values :
-//  Note: the current algorithm does not use the last two columns. Each
-//  row corresponds to an amino acid starting with Ala. J, O and U are
-//  inexistant, but here only in order to have the complete alphabet.
-//
-//     Ct    Nt   Sm     Sc     Sn
-//
-
-static float cPk[26][5] = {
-3.55, 7.59, 0.   , 0.   , 0.    , // A
-3.55, 7.50, 0.   , 0.   , 0.    , // B
-3.55, 7.50, 9.00 , 9.00 , 9.00  , // C
-4.55, 7.50, 4.05 , 4.05 , 4.05  , // D
-4.75, 7.70, 4.45 , 4.45 , 4.45  , // E
-3.55, 7.50, 0.   , 0.   , 0.    , // F
-3.55, 7.50, 0.   , 0.   , 0.    , // G
-3.55, 7.50, 5.98 , 5.98 , 5.98  , // H
-3.55, 7.50, 0.   , 0.   , 0.    , // I
-0.00, 0.00, 0.   , 0.   , 0.    , // J
-3.55, 7.50, 10.00, 10.00, 10.00 , // K
-3.55, 7.50, 0.   , 0.   , 0.    , // L
-3.55, 7.00, 0.   , 0.   , 0.    , // M
-3.55, 7.50, 0.   , 0.   , 0.    , // N
-0.00, 0.00, 0.   , 0.   , 0.    , // O
-3.55, 8.36, 0.   , 0.   , 0.    , // P
-3.55, 7.50, 0.   , 0.   , 0.    , // Q
-3.55, 7.50, 12.0 , 12.0 , 12.0  , // R
-3.55, 6.93, 0.   , 0.   , 0.    , // S
-3.55, 6.82, 0.   , 0.   , 0.    , // T
-0.00, 0.00, 0.   , 0.   , 0.    , // U
-3.55, 7.44, 0.   , 0.   , 0.    , // V
-3.55, 7.50, 0.   , 0.   , 0.    , // W
-3.55, 7.50, 0.   , 0.   , 0.    , // X
-3.55, 7.50, 10.00, 10.00, 10.00 , // Y
-3.55, 7.50, 0.   , 0.   , 0.    }; // Z
-
 int   i, sequenceLength;
-int   comp[26];
+int   comp[ALPHABET_LEN];
 int   nTermResidue, cTermResidue;
 float charge, phMin, phMid, phMax;
 float carg, cter, nter, ctyr, chis, clys, casp, cglu, ccys;
-
 int    R, H, K, D, E, C, Y;
 
 R = (int)('R' - 'A');
@@ -79,13 +35,13 @@ C = (int)('C' - 'A');
 Y = (int)('Y' - 'A');
 
 sequenceLength = strlen(sequence);
-for (i=0; i<26; i++) comp[i] = 0.0;
+for (i=0; i<ALPHABET_LEN; i++) comp[i] = 0.0;
       
-// Compute the amino-acid composition.
+/* Compute the amino-acid composition. */
 for (i = 0; i < sequenceLength; i++)
     comp[sequence[i] - 'A']++;
 
-// Look up N-terminal and C-terminal residue.
+/* Look up N-terminal and C-terminal residue. */
 nTermResidue = sequence[0] - 'A';
 cTermResidue = sequence[sequenceLength - 1] - 'A';
 
@@ -94,7 +50,7 @@ phMax = PH_MAX;
 
 for (i = 0, charge = 1.0; i < MAXLOOP && (phMax - phMin) > EPSI; i++)
     {
-    phMid = phMin + (phMax - phMin) / 2;
+    phMid = phMin + (phMax - phMin) / 2.0;
 
     cter = exp10(-cPk[cTermResidue][0]) /
            (exp10(-cPk[cTermResidue][0]) + exp10(-phMid));
@@ -120,29 +76,24 @@ for (i = 0, charge = 1.0; i < MAXLOOP && (phMax - phMin) > EPSI; i++)
              (casp + cglu + ctyr + ccys + cter);
 
     if (charge > 0.0)
-	{	     
 	phMin = phMid;
-        }
-     else
-	{   
+    else
   	phMax = phMid;
-        }
     }
 return(phMid);
 }
 
 int main(int argc, char *argv[])
 {
-char *inLine;
 char *infName, *spDb, *outfName;
-int  i;
 float pI;
 char *acc;
 struct sqlConnection *conn;
 char *seq;
 char cond_str[255];
-char line[1000];
-FILE *inf, *outf1;
+struct lineFile *inf;
+FILE *outf1;
+char *row[1];
 
 if (argc != 4)
    {
@@ -153,22 +104,15 @@ infName   = argv[1];
 spDb      = argv[2];
 outfName  = argv[3];
 
-if ((inf = fopen(infName, "r")) == NULL)
-    {		
-    fprintf(stderr, "Can't open file %s.\n", infName);
-    exit(8);
-    }
-
+inf = lineFileOpen(infName, TRUE); /* input file */
 outf1 = mustOpen(outfName, "w");
 conn  = hAllocConn();
 
-while ((inLine = fgets(line, 1000, inf)) != NULL)
+while (lineFileRow(inf, row))
     {
-    *(inLine+strlen(inLine)-1) = '\0';
+    acc = row[0];
 
-    acc = inLine;
-
-    sprintf(cond_str, "acc='%s'", acc);
+    safef(cond_str, sizeof(cond_str), "acc='%s'", acc);
     seq  = sqlGetField(conn, spDb, "protein", "val", cond_str);
 
     if (seq != NULL)
