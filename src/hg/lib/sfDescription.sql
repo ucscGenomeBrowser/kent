34465460e5225ceba76b0d3331e326091eff8d13
fanhsu <fanhsu>
2004-03-03 18:19:19 +0000

    Created sfDescription.sql

--- src/hg/lib/sfDescription.sql
+++ src/hg/lib/sfDescription.sql
@@ -1,10 +1,7 @@
-# This table stores domain descriptions for the Supfamily track
-# A single Superfamily entry may contain multiple domains
 CREATE TABLE sfDescription (
-  name varchar(40) NOT NULL default '',		# ID of a Superfamily entry, 
-						# same as Ensembl transcript name
-  proteinID varchar(40) NOT NULL default '',	# ID of the corresponding Ensembl translation
-  description varchar(255) NOT NULL default '', # Description of the domain
+  name varchar(40) NOT NULL default '',
+  proteinID varchar(40) NOT NULL default '',
+  description varchar(255) NOT NULL default '',
   KEY name (name),
   KEY protein (proteinID)
 ) TYPE=MyISAM;
