4c50369f7c6a0bf5d42da1c87e0d683868b21f80
fanhsu <fanhsu>
2004-03-03 18:27:57 +0000

    Created pbAaDistT.sql

---
+++ src/hg/lib/pbAaDistT.sql
@@ -0,0 +1,5 @@
+CREATE TABLE pbAaDistT (
+  x float default NULL,
+  y float default NULL
+) TYPE=MyISAM;
+
---
+++ src/hg/lib/pbAaDistV.sql
@@ -0,0 +1,5 @@
+CREATE TABLE pbAaDistV (
+  x float default NULL,
+  y float default NULL
+) TYPE=MyISAM;
+
