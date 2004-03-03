0e325778ab4abe3e853b998c03f0cd7b8353da62
fanhsu <fanhsu>
2004-03-03 18:15:21 +0000

    Created bioCycPathway.sql

---
+++ src/hg/lib/bioCycPathway.sql
@@ -0,0 +1,9 @@
+CREATE TABLE bioCycPathway (
+  kgID varchar(40) NOT NULL default '',
+  geneID varchar(40) NOT NULL default '',
+  mapID varchar(40) NOT NULL default '',
+  KEY kgID (kgID),
+  KEY geneID (geneID),
+  KEY mapID (mapID)
+) TYPE=MyISAM;
+
