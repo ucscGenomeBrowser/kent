bdaa36cb1e355016e2e31c375328c6050905ccb6
fanhsu <fanhsu>
2004-03-03 18:16:06 +0000

    Created dupSpMrna.sql

--- src/hg/lib/dupSpMrna.sql
+++ src/hg/lib/dupSpMrna.sql
@@ -1,9 +1,8 @@
-# This table contains duplicated mRNA/Protein entries which has identical cds structure
 CREATE TABLE dupSpMrna (
-  mrnaID varchar(20) NOT NULL default '',	# mRNA ID of main entry
-  proteinID varchar(20) NOT NULL default '',	# protein ID of main entry
-  dupMrnaID varchar(20) NOT NULL default '',	# mRNA ID of duplicated entry
-  dupProteinID varchar(20) NOT NULL default '', # protein ID of duplicated entry
+  mrnaID varchar(20) NOT NULL default '',
+  proteinID varchar(20) NOT NULL default '',
+  dupMrnaID varchar(20) NOT NULL default '',
+  dupProteinID varchar(20) NOT NULL default '',
   KEY i3 (mrnaID(12)),
   KEY i4 (proteinID(12)),
   KEY i5 (dupMrnaID(12)),
