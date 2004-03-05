006d405026fcf3c070a8f6e29abe28313db4b023
baertsch <baertsch>
2004-03-05 15:09:05 +0000

    add -bScore

--- src/hg/bedIntersect/bedIntersect.c
+++ src/hg/bedIntersect/bedIntersect.c
@@ -5,9 +5,11 @@
 #include "options.h"
 #include "binRange.h"
 
-static char const rcsid[] = "$Id: bedIntersect.c,v 1.4 2003/12/01 05:40:34 baertsch Exp $";
+static char const rcsid[] = "$Id: bedIntersect.c,v 1.5 2004/03/05 15:09:05 baertsch Exp $";
 
 boolean aHitAny = FALSE;
+boolean bScore = FALSE;
+float aCoverage = 0.00001;
 
 void usage()
 /* Explain usage and exit. */
@@ -18,6 +20,8 @@ errAbort(
   "   bedIntersect a.bed b.bed output.bed\n"
   "options:\n"
   "   -aHitAny output all of a if any of it is hit by b\n"
+  "   -bMinCoverage=0.N min coverage of b to output match.  Default .00001\n"
+  "   -bScore output score from b.bed (must be at least 5 field bed)\n"
   );
 }
 
@@ -28,6 +32,17 @@ struct bed3
     char *chrom;	/* Allocated in hash. */
     int start;		/* Start (0 based) */
     int end;		/* End (non-inclusive) */
+    };
+
+struct bed5 
+/* A five field bed. */
+    {
+    struct bed5 *next;
+    char *chrom;	/* Allocated in hash. */
+    int start;		/* Start (0 based) */
+    int end;		/* End (non-inclusive) */
+    char *name;	/* Name of item */
+    int score; /* Score - 0-1000 */
     };
 
 struct hash *readBed(char *fileName)
@@ -62,10 +77,42 @@ lineFileClose(&lf);
 return hash;
 }
 
+struct hash *readBed5(char *fileName)
+/* Read bed and return it as a hash keyed by chromName
+ * with binKeeper values. */
+{
+char *row[5];
+struct lineFile *lf = lineFileOpen(fileName, TRUE);
+struct binKeeper *bk;
+struct hash *hash = newHash(0);
+struct hashEl *hel;
+struct bed5 *bed;
+
+while (lineFileRow(lf, row))
+    {
+    hel = hashLookup(hash, row[0]);
+    if (hel == NULL)
+       {
+       bk = binKeeperNew(0, 511*1024*1024);
+       hel = hashAdd(hash, row[0], bk);
+       }
+    bk = hel->val;
+    AllocVar(bed);
+    bed->chrom = hel->name;
+    bed->start = lineFileNeedNum(lf, row, 1);
+    bed->end = lineFileNeedNum(lf, row, 2);
+    bed->score = lineFileNeedNum(lf, row, 4);
+    if (bed->start > bed->end)
+        errAbort("start after end line %d of %s", lf->lineIx, lf->fileName);
+    binKeeperAdd(bk, bed->start, bed->end, bed);
+    }
+lineFileClose(&lf);
+return hash;
+}
 void bedIntersect(char *aFile, char *bFile, char *outFile)
 /* bedIntersect - Intersect two bed files. */
 {
-struct hash *bHash = readBed(bFile);
+struct hash *bHash = NULL;
 struct lineFile *lf = lineFileOpen(aFile, TRUE);
 FILE *f = mustOpen(outFile, "w");
 char *row[40];
@@ -73,7 +120,12 @@ char *name;
 int start, end, i, wordCount;
 struct binElement *hitList = NULL, *hit;
 struct binKeeper *bk;
+struct bed5 *bed;
 
+if (bScore)
+    bHash = readBed5(bFile);
+else
+    bHash = readBed(bFile);
 while ((wordCount = lineFileChop(lf, row)) != 0)
     {
     name = row[0];
@@ -89,10 +141,31 @@ while ((wordCount = lineFileChop(lf, row)) != 0)
 	    {
 	    if (hitList != NULL)
                 {
-	        fprintf(f, "%s\t%d\t%d", name, start, end);
-                for (i = 3 ; i<wordCount; i++)
-                    fprintf(f, "\t%s",row[i]);
-                fprintf(f, "\n");
+                for (hit = hitList; hit != NULL; hit = hit->next)
+                    {
+                    float overlap = (float)positiveRangeIntersection(start, end, hit->start, hit->end);
+                    if (overlap/(float)(end-start) > aCoverage)
+                        {
+                        fprintf(f, "%s\t%d\t%d", name, start, end);
+                        for (i = 3 ; i<wordCount; i++)
+                            {
+                            if (bScore && i==4)
+                                {
+                                bed = hit->val;
+                                fprintf(f, "\t%d",bed->score);
+                                }
+                            else
+                                fprintf(f, "\t%s",row[i]);
+                            }
+                        fprintf(f, "\n");
+                        break;
+                        }
+                    else
+                        {
+                        printf("filter out %s %d %d %d %d overlap %.1f %d %d %.3f\n",
+                                name,start,end, hit->start,hit->end, overlap, end-start, hit->end-hit->start,overlap/(float)(end-start));
+                        }
+                    }
                 }
 	    }
 	else
@@ -101,11 +174,20 @@ while ((wordCount = lineFileChop(lf, row)) != 0)
 	        {
 		int s = max(start, hit->start);
 		int e = min(end, hit->end);
-		if (s < e)
+                float overlap = (float)positiveRangeIntersection(start, end, hit->start, hit->end);
+		if ((s < e) && (overlap/(float)(hit->end-hit->start) > aCoverage))
                     {
 		    fprintf(f, "%s\t%d\t%d", name, s, e);
                     for (i = 3 ; i<wordCount; i++)
-                        fprintf(f, "\t%s",row[i]);
+                        {
+                        if (bScore && i==4)
+                            {
+                            bed = hit->val;
+                            fprintf(f, "\t%d",bed->score);
+                            }
+                        else
+                            fprintf(f, "\t%s",row[i]);
+                        }
                     fprintf(f, "\n");
                     }
 		}
@@ -120,6 +202,8 @@ int main(int argc, char *argv[])
 {
 optionHash(&argc, argv);
 aHitAny = optionExists("aHitAny");
+bScore = optionExists("bScore");
+aCoverage = optionFloat("aCoverage", aCoverage);
 if (argc != 4)
     usage();
 bedIntersect(argv[1], argv[2], argv[3]);
