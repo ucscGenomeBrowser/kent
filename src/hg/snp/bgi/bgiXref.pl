af6b3ee3734d65e03abfea7e0366d861942b04c7
angie <angie>
2004-03-19 17:10:34 +0000

    Since snp-gene associations are many-to-many, separate them out into bgiGeneSnp table instead of keeping in bgiSnp and bgiGeneInfo tables. Ignore intron associations that are redundant with splice site associations. Prepend annotation source to gene names to uniquify.

--- src/hg/snp/bgi/bgiXref.pl
+++ src/hg/snp/bgi/bgiXref.pl
@@ -8,9 +8,20 @@ use strict;
 
 my $infoOut = 'bgiGeneInfo.sql';
 my $snpOut = 'bgiSnp.bed';
+my $geneSnpOut = 'bgiGeneSnp.tab';
 
 my $strainExpr = '(Broiler|Layer|Silkie)';
-my $geneSrcExpr = '(GenBank_complete|GenBank_partial|HumanDiseaseGene|IMS-GSF|UMIST\(orf lessthan 300\)|UMIST\(orf morethan 300\))';
+my $geneSrcExpr = '(GenBank_complete|GenBank_partial|HumanDiseaseGene|IMS-GSF|UMIST\(orf lessthan 300\)|UMIST\(orf morethan 300\)|Hans_Cheng|Leif_Andersson|Martien_Groenen)';
+my %geneSrcAbbrev = ('GenBank_complete' => 'GBC',
+		     'GenBank_partial' => 'GBP',
+		     'HumanDiseaseGene' => 'HDG',
+		     'IMS-GSF' => 'IG',
+		     'UMIST(orf lessthan 300)' => 'UM',
+		     'UMIST(orf morethan 300)' => 'UM',
+		     'Hans_Cheng' => 'HC',
+		     'Leif_Andersson' => 'LA',
+		     'Martien_Groenen' => 'MG',
+		    );
 
 # 2 args:  file containing list of SNPlist-chr*.txt[.gz] files, and 
 # file containing list of GeneSNPcon*.txt[.gz] files.  
@@ -71,15 +82,93 @@ sub parseGo {
   return($line);
 }
 
+# Some Splice site annotations have duplicate Intron annotations; ignore.
+my %ignoreAssocs = ("GBC.AF330009	snp.11.169.100039.D.3	Intron" => 1,
+		    "GBC.AY434090	snp.13.273.21715.D.2	Intron" => 1,
+		    "GBC.CHKRPK1	snp.3.1043.108926.S.3	Intron" => 1,
+		    "GBP.GGA278103	snp.40.83.30594.D.3	Intron" => 1,
+		    "GBP.GGCD8BMR	snp.84.35.2761.D.3	Intron" => 1,
+		    "GBP.GGSCII	snp.129.135.3889.S.1	Intron" => 1,
+		    "HDG.ENST0220888.1	snp.91.32.10037.I.3	Intron" => 1,
+		    "HDG.ENST0243222.1	snp.0.76.46742.S.2	Intron" => 1,
+		    "HDG.ENST0243222.1	snp.0.76.46742.S.2	Intron" => 1,
+		    "HDG.ENST0259463.1	snp.24.85.74842.S.2	Intron" => 1,
+		    "HDG.ENST0265312.3	snp.35.126.134274.S.2	Intron" => 1,
+		    "HDG.ENST0265312.3	snp.35.126.134274.S.3	Intron" => 1,
+		    "HDG.ENST0265517.1	snp.343.17.13202.S.1	Intron" => 1,
+		    "HDG.ENST0265517.1	snp.343.17.13202.S.1	Intron" => 1,
+		    "HDG.ENST0272425.1	snp.41.264.34914.S.1	Intron" => 1,
+		    "HDG.ENST0272425.1	snp.41.264.34914.S.1	Intron" => 1,
+		    "HDG.ENST0273049.2	snp.35.126.134274.S.2	Intron" => 1,
+		    "HDG.ENST0273049.2	snp.35.126.134274.S.3	Intron" => 1,
+		    "HDG.ENST0306737.4	snp.23.381.16095.S.1	Intron" => 1,
+		    "HDG.ENST0306737.4	snp.23.381.16095.S.1	Intron" => 1,
+		    "HDG.ENST0306737.4	snp.23.381.16095.S.2	Intron" => 1,
+		    "HDG.ENST0306737.4	snp.23.381.16095.S.2	Intron" => 1,
+		    "HDG.ENST0306805.2	snp.158.95.1563.I.3	Intron" => 1,
+		    "HDG.ENST0315357.3	snp.23.381.16095.S.1	Intron" => 1,
+		    "HDG.ENST0315357.3	snp.23.381.16095.S.1	Intron" => 1,
+		    "HDG.ENST0315357.3	snp.23.381.16095.S.2	Intron" => 1,
+		    "HDG.ENST0315357.3	snp.23.381.16095.S.2	Intron" => 1,
+		    "HDG.ENST0318471.1	snp.54.44.40605.S.3	Intron" => 1,
+		    "HDG.ENST0318471.1	snp.54.44.40605.S.3	Intron" => 1,
+		    "HDG.ENST0318662.2	snp.11.169.100039.D.3	Intron" => 1,
+		    "HDG.ENST0318662.2	snp.11.169.100039.D.3	Intron" => 1,
+		    "HDG.ENST0334149.2	snp.62.137.12320.S.1	Intron" => 1,
+		    "HDG.ENST0334149.2	snp.62.137.12320.S.1	Intron" => 1,
+		    "HDG.ENST0336463.1	snp.18.589.34206.D.1	Intron" => 1,
+		    "HDG.ENST0336828.1	snp.35.126.134274.S.2	Intron" => 1,
+		    "HDG.ENST0336828.1	snp.35.126.134274.S.3	Intron" => 1,
+		    "HDG.ENST0337218.1	snp.23.381.16095.S.1	Intron" => 1,
+		    "HDG.ENST0337218.1	snp.23.381.16095.S.1	Intron" => 1,
+		    "HDG.ENST0337218.1	snp.23.381.16095.S.2	Intron" => 1,
+		    "HDG.ENST0337218.1	snp.23.381.16095.S.2	Intron" => 1,
+		    "HDG.ENST0337851.1	snp.66.124.30871.S.2	Intron" => 1,
+		    "HDG.ENST0337851.1	snp.66.124.30871.S.2	Intron" => 1,
+		    "HDG.ENST0337851.1	snp.66.124.30872.D.1	Intron" => 1,
+		    "HDG.ENST0338081.1	snp.11.169.100039.D.3	Intron" => 1,
+		    "HDG.ENST0338081.1	snp.11.169.100039.D.3	Intron" => 1,
+		    "IG.riken1_14g12	snp.153.70.10298.S.2	Intron" => 1,
+		    "IG.riken1_2i19	snp.10.221.108177.S.3	Intron" => 1,
+		    "IG.riken1_6l10	snp.14.28.57558.I.3	Intron" => 1,
+		    "UM.BX929831	snp.265.71.21998.S.1	Intron" => 1,
+		    "UM.BX933790	snp.103.61.11006.D.3	Intron" => 1,
+		    "UM.BX934145	snp.5.532.89195.S.1	Intron" => 1,
+		    "UM.BX935596	snp.32.191.7074.S.1	Intron" => 1,
+		    "UM.BX930936	snp.9.461.7584.D.2	Intron" => 1,
+		    "UM.BX931260	snp.10.218.129574.D.3	Intron" => 1,
+		    "HC.bgf_chr4_909	snp.3.661.14775.S.1	Intron" => 1,
+		    "HC.bgf_chr4_909	snp.3.661.14775.S.2	Intron" => 1,
+		    "LA.riken1_2i19	snp.10.221.108177.S.3	Intron" => 1,
+		    "MG.bgf_chr2_1915	snp.264.6.522.S.2	Intron" => 1,
+		    "MG.bgf_chr2_2004	snp.21.141.15231.S.1	Intron" => 1,
+		    "MG.bgf_chr2_2096_conform	snp.21.301.16681.S.3	Intron" => 1,
+		   );
+
+my %assocs = ();
+sub gsOut {
+  confess "Wrong #words to gsOut" if ($#_ != 5);
+  if (! defined $ignoreAssocs{join("\t", $_[0], $_[1], $_[2])}) {
+    my $line = join("\t", @_);
+    print GS "$line\n";
+    my $assoc = $_[0] . "\t" . $_[1];
+    if (defined $assocs{$assoc} && $assocs{$assoc} ne $line) {
+      print STDERR "dup assoc $assoc:\n  $assocs{$assoc}\n  $line\n";
+    }
+    $assocs{$assoc} = $line;
+  }
+}
 
 #######################################################################
 # MAIN
 
 #
-# Get core info about SNPs from the files listed in $SNPlists.
+# Get core info about SNPs from the files listed in $SNPlists,
+# dump out $snpOut as we go.
 #
-my %snpCore = ();
+my %snpIds = ();
 open(L, "$SNPlists") || die "Can't open $SNPlists: $!\n";
+open(SNP, ">$snpOut") || die "Can't open $snpOut for writing: $!\n";
 while (<L>) {
   chop;  my $fname = $_;
   if ($fname =~ m@$strainExpr.*SNPlist-(chr\w+)\.txt(.*)$@) {
@@ -104,13 +193,15 @@ while (<L>) {
       my ($rStart, $rEnd) = &parsePos($rPos);
       my ($inB, $inL, $inS) = &parseStrains($strains);
       $readDir = ($readDir eq 'F') ? '+' : '-';
-      if (defined $snpCore{$snpId}) {
+      $extra = "" if (! defined $extra);
+      if (defined $snpIds{$snpId}) {
 	die "Duplicate def for $snpId, line $. of $fname:\n$_\n";
       }
-      $snpCore{$snpId} = [$chr, $start, $end, $snpType, $rStart, $rEnd,
-			  $qual, $rQual, $change, $flankL, $flankR,
-			  $rName, $readDir, $inB, $inL, $inS, $primerL,
-			  $primerR, $conf, $extra];
+      $snpIds{$snpId} = 1;
+      print SNP "$chr\t$start\t$end\t$snpId\t$snpType\t$rStart\t$rEnd\t" .
+        "$qual\t$rQual\t$change\t" .
+        "$rName\t$readDir\t$inB\t$inL\t$inS\t$primerL\t" .
+	"$primerR\t$conf\t$extra\n";
     }
     close(IN);
   } else {
@@ -118,88 +209,90 @@ while (<L>) {
   }
 }
 close(L);
+close(SNP);
 
 #
 # Get gene info and SNP-gene relationships from the files listed in 
-# GeneSNPlists.  Dump out $infoOut as we go.
+# GeneSNPlists.  Dump out $infoOut and $geneSnpOut as we go.
 #
-open(INFO, ">$infoOut") || die "Can't open $infoOut for writing: $!\n";
-my %snpGene = ();
+my %gene2src = ();
 open(L, "$GeneSNPlists") || die "Can't open $GeneSNPlists: $!\n";
+open(INFO, ">$infoOut") || die "Can't open $infoOut for writing: $!\n";
+open(GS, ">$geneSnpOut") || die "Can't open $geneSnpOut for writing: $!\n";
 while (<L>) {
   chop;  my $fname = $_;
   if ($fname =~ m@$geneSrcExpr.*\.txt(.*)$@) {
     my ($geneSrc, $gz) = ($1, $2);
+    my $abbrev = $geneSrcAbbrev{$geneSrc};
+    die "Can't abbreviate $geneSrc" if (! defined $abbrev);
     if ($gz ne "") {
       open(IN, "gunzip -c '$fname'|") || die "Can't gunzip $fname: $!";
     } else {
       open(IN, "$fname") || die "Can't read $fname: $!";
     }
     my ($geneName, $chr, $start, $end, $strand, $pctId, $go, $ipr);
-    my $snpIds = "";
     while (<IN>) {
       chop;
       next if (/^\s*$/);
-      my @words = split(/\t/);
+      my @words = split("\t");
       if ($words[0] eq "GN") {
 	if (defined $geneName) {
-	  print INFO "INSERT INTO bgiGeneInfo VALUES ( \"$geneName\", \"$geneSrc\", \"$go\", \"$ipr\", \"$snpIds\");\n";
+	  print INFO "INSERT INTO bgiGeneInfo VALUES ( \"$geneName\", \"$geneSrc\", \"$go\", \"$ipr\");\n";
 	  $chr = $start = $end = $strand = $pctId = $go = $ipr = undef;
-	  $snpIds = "";
 	}
 	($geneName, $go, $ipr) = ($words[1], &parseGo($words[2]), $words[3]);
+	$geneName =~ s/ENST0000(.*)\|ENSG.*/ENST$1/;
+	$geneName = $abbrev . "." . $geneName;
 	$go = "" if (! defined $go);
 	$ipr = "" if (! defined $ipr);
+	if (defined $gene2src{$geneName}) {
+	  print STDERR "dup:\t$geneName\t$gene2src{$geneName}\t$geneSrc\n"
+	}
+	$gene2src{$geneName} = $geneSrc;
       } elsif ($words[0] eq "GP") {
 	(undef, $chr, $start, $end, $strand, $pctId) = @words;
 	$start -= 1;
       } elsif ($words[0] eq "S5") {
 	my $snpId = $words[1];
-	$snpGene{$snpId} = [$geneName, "5' upstream", "", ""];
-	$snpIds .= "$snpId,";
+	&gsOut($geneName, $snpId, "5' upstream", "", "", "");
       } elsif ($words[0] eq "S3") {
 	my $snpId = $words[1];
-	$snpGene{$snpId} = [$geneName, "3' downstream", "", ""];
-	$snpIds .= "$snpId,";
+	&gsOut($geneName, $snpId, "3' downstream", "", "", "");
       } elsif ($words[0] eq "U5") {
 	my $snpId = $words[1];
-	$snpGene{$snpId} = [$geneName, "5' UTR", "", ""];
-	$snpIds .= "$snpId,";
+	&gsOut($geneName, $snpId, "5' UTR", "", "", "");
       } elsif ($words[0] eq "U3") {
 	my $snpId = $words[1];
-	$snpGene{$snpId} = [$geneName, "3' UTR", "", ""];
-	$snpIds .= "$snpId,";
+	&gsOut($geneName, $snpId, "3' UTR", "", "", "");
       } elsif ($words[0] eq "CS") {
 	my ($snpId, $ccNuc, $ccPept, $phase) = ($words[1], $words[8],
 						$words[9], $words[10]);
 	my $cc = "$ccNuc ($ccPept)";
-	$snpGene{$snpId} = [$geneName, "Coding (Synonymous)", $cc, $phase];
-	$snpIds .= "$snpId,";
+	&gsOut($geneName, $snpId, "Coding (Synonymous)", $cc, $phase, "");
       } elsif ($words[0] eq "CN") {
 	my ($snpId, $ccNuc, $ccPept, $phase) = ($words[1], $words[8],
 						$words[9], $words[10]);
+	my $sift = "$words[19] $words[20] $words[21]";
+	$sift = "" if (! defined $sift);
 	my $cc = "$ccNuc ($ccPept)";
-	$snpGene{$snpId} = [$geneName, "Coding (Nonsynonymous)", $cc, $phase];
-	$snpIds .= "$snpId,";
+	&gsOut($geneName, $snpId, "Coding (Nonsynonymous)", $cc, $phase,
+	       $sift);
       } elsif ($words[0] eq "CF") {
 	my $snpId = $words[1];
-	$snpGene{$snpId} = [$geneName, "Coding (Frameshift)", "", ""];
-	$snpIds .= "$snpId,";
+	&gsOut($geneName, $snpId, "Coding (Frameshift)", "", "", "");
       } elsif ($words[0] eq "IR") {
 	my $snpId = $words[1];
-	$snpGene{$snpId} = [$geneName, "Intron", "", ""];
-	$snpIds .= "$snpId,";
+	&gsOut($geneName, $snpId, "Intron", "", "", "");
       } elsif ($words[0] eq "SS") {
 	my ($snpId, $cc) = ($words[1], $words[8]);
 	$cc = "" if ($cc eq "SSIndels");
-	$snpGene{$snpId} = [$geneName, "Splice Site", $cc, ""];
-	$snpIds .= "$snpId,";
+	&gsOut($geneName, $snpId, "Splice Site", $cc, "", "");
       } else {
 	die "Unrecognized first word $words[0] line $. of $fname:\n$_\n";
       }
     }
     if (defined $geneName) {
-      print INFO "INSERT INTO bgiGeneInfo VALUES ( \"$geneName\", \"$geneSrc\", \"$go\", \"$ipr\", \"$snpIds\");\n";
+      print INFO "INSERT INTO bgiGeneInfo VALUES ( \"$geneName\", \"$geneSrc\", \"$go\", \"$ipr\");\n";
     }
     close(IN);
   } else {
@@ -208,26 +301,5 @@ while (<L>) {
 }
 close(L);
 close(INFO);
-
-#
-# Combine SNP info and dump out $snpOut.
-#
-open(SNP, ">$snpOut") || die "Can't open $snpOut for writing: $!\n";
-foreach my $snpId (keys %snpCore) {
-  my ($chr, $start, $end, $snpType, $rStart, $rEnd,
-      $qual, $rQual, $change, $flankL, $flankR,
-      $rName, $readDir, $inB, $inL, $inS, $primerL,
-      $primerR, $conf, $extra) = @{$snpCore{$snpId}};
-  $extra = "" if (! defined $extra);
-  my ($geneName, $geneAssoc, $cc, $phase) = ("", "", "", "");
-  if (defined $snpGene{$snpId}) {
-    ($geneName, $geneAssoc, $cc, $phase) = @{$snpGene{$snpId}};
-  }
-  print SNP "$chr\t$start\t$end\t$snpId\t$snpType\t$rStart\t$rEnd\t" .
-        "$qual\t$rQual\t$change\t" .
-        "$rName\t$readDir\t$inB\t$inL\t$inS\t$primerL\t" .
-	"$primerR\t$conf\t$extra\t" .
-	"$geneName\t$geneAssoc\t$cc\t$phase\n";
-}
-close(SNP);
+close(GS);
 
