a326e1f13c613278b08643ffdd200912dd852791
hiram <hiram>
2004-03-20 00:17:55 +0000

    Currently disabled to do only summary view

--- src/hg/lib/wiggleUtils.c
+++ src/hg/lib/wiggleUtils.c
@@ -7,7 +7,7 @@
 #include "hdb.h"
 #include "wiggle.h"
 
-static char const rcsid[] = "$Id: wiggleUtils.c,v 1.1 2004/03/17 22:59:46 hiram Exp $";
+static char const rcsid[] = "$Id: wiggleUtils.c,v 1.2 2004/03/20 00:17:55 hiram Exp $";
 
 static char *currentFile = (char *) NULL;	/* the binary file name */
 static FILE *f = (FILE *) NULL;			/* file handle to binary file */
@@ -48,7 +48,7 @@ if (f != (FILE *) NULL)
 }
 
 static struct wiggleData * readWigDataRow(struct wiggle *wiggle,
-    int winStart, int winEnd)
+    int winStart, int winEnd, boolean summaryOnly)
 /*  read one row of wiggle data, return data values between winStart, winEnd */
 {
 unsigned char *readData = (unsigned char *) NULL;
@@ -68,50 +68,63 @@ double sumData = 0.0;
 double sumSquares = 0.0;
 unsigned dataArea;
 
-openWibFile(wiggle->file);
-fseek(f, wiggle->offset, SEEK_SET);
-readData = (unsigned char *) needMem((size_t) (wiggle->count + 1));
-itemsRead = fread(readData, (size_t) wiggle->count,
-	(size_t) sizeof(unsigned char), f);
-if (itemsRead != sizeof(unsigned char))
-    errAbort("readWigDataRow: can not read %u bytes from %s at offset %u",
-	wiggle->count, wiggle->file, wiggle->offset);
-
-/*	need at most this amount, perhaps less	*/
-dataArea = sizeof(struct wiggleDatum)*wiggle->validCount;
-
-data = (struct wiggleDatum *) needMem((size_t)
-    (sizeof(struct wiggleDatum)*wiggle->validCount));
-
-dataPtr = data;
-chromPosition = wiggle->chromStart;
-
-for (dataOffset = 0; dataOffset < wiggle->count; ++dataOffset)
+if (summaryOnly)
     {
-    unsigned char datum = readData[dataOffset];
-    if (datum == WIG_NO_DATA)
-	++noData;
-    else
+    upperLimit = wiggle->lowerLimit + wiggle->dataRange;
+    lowerLimit = wiggle->lowerLimit;
+    chromStart = wiggle->chromStart;
+    chromEnd = wiggle->chromEnd;
+    validCount = wiggle->validCount;
+    sumData = wiggle->sumData;
+    sumSquares = wiggle->sumSquares;
+    }
+else
+    {
+    openWibFile(wiggle->file);
+    fseek(f, wiggle->offset, SEEK_SET);
+    readData = (unsigned char *) needMem((size_t) (wiggle->count + 1));
+    itemsRead = fread(readData, (size_t) wiggle->count,
+	    (size_t) sizeof(unsigned char), f);
+    if (itemsRead != sizeof(unsigned char))
+	errAbort("readWigDataRow: can not read %u bytes from %s at offset %u",
+	    wiggle->count, wiggle->file, wiggle->offset);
+
+    /*	need at most this amount, perhaps less	*/
+    dataArea = sizeof(struct wiggleDatum)*wiggle->validCount;
+
+    data = (struct wiggleDatum *) needMem((size_t)
+	(sizeof(struct wiggleDatum)*wiggle->validCount));
+
+    dataPtr = data;
+    chromPosition = wiggle->chromStart;
+
+    for (dataOffset = 0; dataOffset < wiggle->count; ++dataOffset)
 	{
-	if (chromPosition >= winStart && chromPosition < winEnd)
+	unsigned char datum = readData[dataOffset];
+	if (datum == WIG_NO_DATA)
+	    ++noData;
+	else
 	    {
-	    dataPtr->chromStart = chromPosition;
-	    dataPtr->value = wiggle->lowerLimit +
-		(((double)datum/(double)MAX_WIG_VALUE)*wiggle->dataRange);
-	    ++validCount;
-	    if (chromStart < 0)
-		chromStart = chromPosition;
-	    chromEnd = chromPosition + 1 + wiggle->span;
-	    if (lowerLimit > dataPtr->value)
-		lowerLimit = dataPtr->value;
-	    if (upperLimit < dataPtr->value)
-		upperLimit = dataPtr->value;
-	    sumData += dataPtr->value;
-	    sumSquares += dataPtr->value * dataPtr->value;
-	    ++dataPtr;
+	    if (chromPosition >= winStart && chromPosition < winEnd)
+		{
+		dataPtr->chromStart = chromPosition;
+		dataPtr->value = wiggle->lowerLimit +
+		    (((double)datum/(double)MAX_WIG_VALUE)*wiggle->dataRange);
+		++validCount;
+		if (chromStart < 0)
+		    chromStart = chromPosition;
+		chromEnd = chromPosition + 1 + wiggle->span;
+		if (lowerLimit > dataPtr->value)
+		    lowerLimit = dataPtr->value;
+		if (upperLimit < dataPtr->value)
+		    upperLimit = dataPtr->value;
+		sumData += dataPtr->value;
+		sumSquares += dataPtr->value * dataPtr->value;
+		++dataPtr;
+		}
 	    }
+	chromPosition += wiggle->span;
 	}
-    chromPosition += wiggle->span;
     }
 if (validCount)
     {
@@ -164,7 +177,7 @@ wdPrev->next = wd;
 wd->next = (struct wiggleData *)NULL;	/*	redundant safety	*/
 }
 
-struct wiggleData *fetchWigData(char *db, char *tableName, char *chromName,
+struct wiggleData *wigFetchData(char *db, char *tableName, char *chromName,
     int winStart, int winEnd)
 /*  return linked list of wiggle data between winStart, winEnd */
 {
@@ -173,12 +186,14 @@ struct sqlConnection *conn = hAllocConn();
 struct sqlResult *sr;
 char **row;
 char *whereNULL = NULL;
+char *whereGROUP = "group by span";
 int rowOffset;
 int rowCount = 0;
 struct wiggle *wiggle;
 struct hash *spans = NULL;      /* List of spans encountered during load */
 char spanName[128];
 char whereSpan[128];
+char query[256];
 struct hashEl *el, *elList;
 int leastSpan = BIGNUM;
 int mostSpan = 0;
@@ -189,46 +204,46 @@ struct wiggleData *wdList = (struct wiggleData *) NULL;
 
 spans = newHash(0);	/*	a listing of all spans found here	*/
 
-/*	the first thing we need to do is check for spans	*/
-sr = hRangeQuery(conn, tableName, chromName, winStart, winEnd,
-        whereNULL, &rowOffset);
+snprintf(query, sizeof(query),
+    "SELECT span from %s where chrom = '%s' group by span",
+    tableName, chromName );
+
+/*	Survey the spans to see what the story is here */
+
+sr = sqlMustGetResult(conn,query);
 while ((row = sqlNextRow(sr)) != NULL)
 {
     ++rowCount;
-    wiggle = wiggleLoad(row + rowOffset);
+    unsigned span = sqlUnsigned(row[0]);
 
-    snprintf(spanName, sizeof(spanName), "%d", wiggle->span);
+    snprintf(spanName, sizeof(spanName), "%u", span);
     el = hashLookup(spans, spanName);
     if ( el == NULL)
 	{
-	if (wiggle->span > mostSpan) mostSpan = wiggle->span;
-	if (wiggle->span < leastSpan) leastSpan = wiggle->span;
+	if (span > mostSpan) mostSpan = span;
+	if (span < leastSpan) leastSpan = span;
 	++spanCount;
-	hashAddInt(spans, spanName, wiggle->span);
+	hashAddInt(spans, spanName, span);
 	}
 
-    wiggleFree(&wiggle);
     }
 sqlFreeResult(&sr);
-
 
 /*	Now, using that span list, go through each span, fetching data	*/
 cookie = hashFirst(spans);
 while ((el = hashNext(&cookie)) != NULL)
     {
     snprintf(whereSpan, sizeof(whereSpan), "span = %s", el->name);
-    sr = hRangeQuery(conn, tableName, chromName, winStart, winEnd,
+    sr = hOrderedRangeQuery(conn, tableName, chromName, winStart, winEnd,
         whereSpan, &rowOffset);
     rowCount = 0;
     while ((row = sqlNextRow(sr)) != NULL)
 	{
 	++rowCount;
 	wiggle = wiggleLoad(row + rowOffset);
-	wigData = readWigDataRow(wiggle, winStart, winEnd);
+	wigData = readWigDataRow(wiggle, winStart, winEnd, TRUE);
 	if (wigData)
-	    {
-	    insertOrderedWigData(&wdList,wigData);
-	    }
+	    slAddHead(&wdList,wigData);
 	}
     sqlFreeResult(&sr);
     }
