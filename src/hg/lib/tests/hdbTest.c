617255d8a4ed4559cadbf69d5ccef92b1872704a
kate <kate>
2004-03-31 20:11:16 +0000

    Add test for liftOver chains

--- src/hg/lib/tests/hdbTest.c
+++ src/hg/lib/tests/hdbTest.c
@@ -2,11 +2,29 @@
 
 #include "common.h"
 #include "hdb.h"
+#include "dbDb.h"
+#include "liftOverChain.h"
+#include "liftOver.h"
+
+bool testLiftOverDatabase(char *db)
+{
+struct dbDb *dbList, *dbDb;
+
+dbList = hGetLiftOverDatabases();
+for (dbDb = dbList; dbDb != NULL; dbDb = dbDb->next)
+    if (sameString(dbDb->name, db))
+        return TRUE;
+return FALSE;
+}
 
 int main(int argc, char *argv[])
 {
     hSetDb("hg15");
     printf("hg15 organism ID = %d\n", hOrganismID("hg15"));
     printf("hg15 scientific name = %s\n", hScientificName("hg15"));
+
+    if (testLiftOverDatabase("hg15"))
+        printf("hg15 has a liftOver chain\n");
+
     return 0;
 }
