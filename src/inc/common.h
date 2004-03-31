33db089e8eb169d707d20ba3f76f337004223885
angie <angie>
2004-03-31 07:28:11 +0000

    Added macros isEmpty(string), isNotEmpty(string) -- empty means NULL or "".

--- src/inc/common.h
+++ src/inc/common.h
@@ -457,6 +457,9 @@ int differentWord(char *s1, char *s2);
 #define sameString(a,b) (strcmp(a,b)==0)
 /* Returns TRUE if two strings same. */
 
+#define isEmpty(string) (string == NULL || string[0] == 0)
+#define isNotEmpty(string) (! isEmpty(string))
+
 boolean startsWith(char *start,char *string);
 /* Returns TRUE if string begins with start. */
 
