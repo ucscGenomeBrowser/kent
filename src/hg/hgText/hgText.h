cea946e978f5485cda455d8c4a066fa0288f4e07
hiram <hiram>
2004-04-02 19:56:49 +0000

    Initial contents

---
+++ src/hg/hgText/hgText.h
@@ -0,0 +1,19 @@
+/* hgText.h generic item to be shared between source files in this dir */
+
+/*	$Id: hgText.h,v 1.1 2004/04/02 19:56:49 hiram Exp $	*/
+
+#ifndef HGTEXT_H
+#define HGTEXT_H
+
+#define allFieldsPhase      "Tab-separated, All fields"
+#define chooseFieldsPhase   "Tab-separated, Choose fields..."
+#define seqOptionsPhase     "FASTA (DNA sequence)..."
+#define gffPhase            "GTF"
+#define bedOptionsPhase     "BED..."
+#define ctOptionsPhase      "Custom Track..."
+#define ctWigOptionsPhase   "Data custom track..."
+#define linksPhase          "Hyperlinks to Genome Browser"
+#define statsPhase          "Summary/Statistics"
+#define wigOptionsPhase     "Get data points"
+
+#endif	/*	HGTEXT_H	*/
