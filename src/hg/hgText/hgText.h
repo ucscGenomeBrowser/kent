a1888f071a71b3e584363dd8ad45a44e3d101a69
hiram <hiram>
2004-04-02 23:44:50 +0000

    Everything in common between hgText.c and hgWigText.c in here now

--- src/hg/hgText/hgText.h
+++ src/hg/hgText/hgText.h
@@ -1,10 +1,12 @@
-/* hgText.h generic item to be shared between source files in this dir */
-
-/*	$Id: hgText.h,v 1.1 2004/04/02 19:56:49 hiram Exp $	*/
+/* hgText.h generic item to be shared between source files in this dir
+ *
+ *	$Id: hgText.h,v 1.2 2004/04/02 23:44:50 hiram Exp $
+ */
 
 #ifndef HGTEXT_H
 #define HGTEXT_H
 
+/* Values for outputTypePosMenu	*/
 #define allFieldsPhase      "Tab-separated, All fields"
 #define chooseFieldsPhase   "Tab-separated, Choose fields..."
 #define seqOptionsPhase     "FASTA (DNA sequence)..."
@@ -15,5 +17,79 @@
 #define linksPhase          "Hyperlinks to Genome Browser"
 #define statsPhase          "Summary/Statistics"
 #define wigOptionsPhase     "Get data points"
+
+/* Other values that the "phase" var can take on: */
+#define chooseTablePhase    "table"
+#define pasteNamesPhase     "Paste in Names/Accessions"
+#define uploadNamesPhase    "Upload Names/Accessions File"
+#define outputOptionsPhase  "Advanced query..."
+#define getOutputPhase      "Get results"
+#define getSomeFieldsPhase  "Get these fields"
+#define getSequencePhase    "Get sequence"
+#define getBedPhase         "Get BED"
+#define getCtPhase          "Get Custom Track"
+#define getCtBedPhase       "Get Custom Track File"
+#define getWigglePhase      "Get data"
+#define getCtWigglePhase    "Get Custom Track Data File"
+#define intersectOptionsPhase "Intersect Results..."
+#define histPhase           "Get histogram"
+#define descTablePhase      "Describe table"
+/* Old "phase" values handled for backwards compatibility: */
+#define oldAllFieldsPhase   "Get all fields"
+#define oldSeqOptionsPhase  "Get sequence..."
+
+extern boolean typeWiggle;
+extern boolean typeWiggle2;
+
+/*	wiggle constraints, two for two possible tables */
+extern char *wigConstraint[2];
+extern struct bed *bedListWig[2];
+
+/*	wiggle data value constraints, two possible tables, and two
+ *	values for a possible range
+ */
+extern double wigDataConstraint[2][2];
+
+/*	to select one of the two tables	*/
+#define WIG_TABLE_1	0
+#define WIG_TABLE_2	1
+#define MAX_LINES_OUT	100000
+
+extern boolean (*wiggleCompare[2])(int tableId, double value,
+    boolean summaryOnly, struct wiggle *wiggle);
+
+extern char *database;
+extern char *chrom;
+extern int winStart;
+extern int winEnd;
+extern boolean tableIsSplit;
+extern boolean allGenome;
+extern char *getTableDb();
+extern void getFullTableName(char *dest, char *newChrom, char *table);
+extern char *getTableName();
+extern char *getTable2Name();
+extern char *getTrackName();
+extern char *getTableVar();
+extern void checkTableExists(char *table);
+extern boolean existsAndEqual(char *var, char *value);
+extern struct hTableInfo *getOutputHti();
+extern void saveOutputOptionsState();
+extern void saveIntersectOptionsState();
+extern char *constrainFields(char *tableId);
+extern void preserveConstraints(char *fullTblName, char *db, char *tableId);
+extern void preserveTable2();
+extern void displayPosition();
+extern char *customTrackPseudoDb;
+extern struct cart *cart;
+extern char *freezeName;
+extern char fullTableName[256];
+extern char *httpFormMethod;
+
+/*	in hgWigText.c	*/
+extern void wigDoStats(char *database, char *table, struct slName *chromList,
+    int winStart, int winEnd, int tableId, char *constraints);
+extern void wiggleConstraints(char *cmp, char *pat, int tableIndex);
+extern void doGetWiggleData(boolean doCt);
+extern void doWiggleCtOptions(boolean doCt);
 
 #endif	/*	HGTEXT_H	*/
