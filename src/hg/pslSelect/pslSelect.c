7d16556ae2c29849e875f72ef7e581f5cbb27e6f
kate <kate>
2004-05-05 22:39:52 +0000

    Add new options

--- src/hg/pslSelect/pslSelect.c
+++ src/hg/pslSelect/pslSelect.c
@@ -7,16 +7,25 @@
 #include "localmem.h"
 #include "psl.h"
 
-static char const rcsid[] = "$Id: pslSelect.c,v 1.2 2004/02/10 23:18:36 markd Exp $";
+static char const rcsid[] = "$Id: pslSelect.c,v 1.3 2004/05/05 22:39:52 kate Exp $";
 
 /* command line option specifications */
 static struct optionSpec optionSpecs[] = {
     {"qtPairs", OPTION_STRING},
+    {"queries", OPTION_STRING},
+    {"queryPairs", OPTION_STRING},
     {NULL, 0}
 };
 
+#define QT_PAIRS_MODE   1
+#define QUERY_MODE      2
+#define QUERY_PAIRS_MODE 3
+
+static int mode = 0;
+static int isPairs = TRUE;
+
 /* global data from command line */
-static char* qtPairsFile;
+static char *selectFile;
 
 void usage()
 /* Explain usage and exit. */
@@ -27,11 +36,12 @@ errAbort(
   "usage:\n"
   "   pslSelect [options] inPsl outPsl\n"
   "\n"
-  "Must specify a selection option, currently -qtPairs is the only option\n"
+  "Must specify a selection option\n"
   "\n"
   "Options:\n"
-  "   -qtPairs=file - file is tab-seperated query and targets to select\n"
-  "\n"
+  "   -qtPairs=file - file is tab-separated query and query to select\n"
+  "   -query=file - file is list of query to select\n"
+  "   -queryPairs=file - file is list of queries, with substitution value\n"
   );
 }
 
@@ -41,39 +51,53 @@ struct hash *loadSelect(char *selectFile)
 struct hash *hash = hashNew(20);
 char *row[2];
 struct lineFile *lf = lineFileOpen(selectFile, TRUE);
-while (lineFileNextRowTab(lf, row, ArraySize(row)))
-    hashAdd(hash, row[0], lmCloneString(hash->lm, row[1]));
+int wordCount = isPairs ? 2 : 1;
+while (lineFileNextRowTab(lf, row, wordCount))
+    {
+    char *value = isPairs ? row[1] : "";
+    hashAdd(hash, row[0], lmCloneString(hash->lm, value));
+    }
 lineFileClose(&lf);
 return hash;
 }
 
-boolean pairSelected(struct hash* selectHash, char *qName, char *tName)
-/* determine if the query/target pair is select.  Handle the query
- * being paired to multiple targets */
+struct hashEl *selectedItem(struct hash* selectHash, char *qName, char *tName)
+/* determine if the item is selected.  Handle the query
+ * being paired to multiple query */
 {
 struct hashEl *hel = hashLookup(selectHash, qName);
 while (hel != NULL)
     {
     char *target = hel->val;
-    if (sameString(target, tName))
-        return TRUE;
+    if (mode == QUERY_MODE || mode == QUERY_PAIRS_MODE)
+        return hel;
+    if (mode == QT_PAIRS_MODE && sameString(target, tName))
+        return hel;
     hel = hashLookupNext(hel);
     }
-return FALSE;
+return NULL;
 }
 
 void pslSelect(char *inPsl, char *outPsl)
 /* select psl */
 {
-struct hash* selectHash = loadSelect(qtPairsFile);
+struct hash *selectHash = loadSelect(selectFile);
 struct lineFile *inPslLf = pslFileOpen(inPsl);
 FILE *outPslFh = mustOpen(outPsl, "w");
 struct psl* psl;
+struct hashEl *hel;
 
 while ((psl = pslNext(inPslLf)) != NULL)
     {
-    if (pairSelected(selectHash, psl->qName, psl->tName))
+    if ((hel = selectedItem(selectHash, psl->qName, psl->tName)) != NULL)
+        {
+        if (mode == QUERY_PAIRS_MODE)
+            {
+            freeMem(psl->qName);
+            psl->qName = cloneString(hel->val);
+            }
         pslTabOut(psl, outPslFh);
+        }
     pslFree(&psl);
     }
 
@@ -87,9 +111,17 @@ int main(int argc, char *argv[])
 optionInit(&argc, argv, optionSpecs);
 if (argc != 3)
     usage();
-qtPairsFile = optionVal("qtPairs", NULL);
-if (qtPairsFile == NULL)
-    errAbort("must specify -qtPairs");
+if ((selectFile = optionVal("qtPairs", NULL)) != NULL)
+    mode = QT_PAIRS_MODE;
+else if ((selectFile = optionVal("queries", NULL)) != NULL)
+    {
+    mode = QUERY_MODE;
+    isPairs = FALSE;
+    }
+else if ((selectFile = optionVal("queryPairs", NULL)) != NULL)
+    mode = QUERY_PAIRS_MODE;
+else
+    errAbort("must specify option");
 
 pslSelect(argv[1], argv[2]);
 
