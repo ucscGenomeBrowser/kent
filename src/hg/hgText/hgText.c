756b763b3c24759afb1cd1963baef10deb5df503
hiram <hiram>
2004-05-05 23:14:13 +0000

    Getting ready to handle custom wiggle tracks

--- src/hg/hgText/hgText.c
+++ src/hg/hgText/hgText.c
@@ -34,7 +34,7 @@
 #include "wiggle.h"
 #include "hgText.h"
 
-static char const rcsid[] = "$Id: hgText.c,v 1.143 2004/05/05 22:54:53 hiram Exp $";
+static char const rcsid[] = "$Id: hgText.c,v 1.144 2004/05/05 23:14:13 hiram Exp $";
 
 /* sources of tracks, other than the current database: */
 static char *hgFixed = "hgFixed";
@@ -2164,10 +2164,21 @@ boolean isWiggle(char *db, char *table)
 /* Return TRUE if db.table is a wiggle. */
 {
 boolean typeWiggle = FALSE;
+
 if (db != NULL && table != NULL)
     {
-    struct hTableInfo *hti = maybeGetHti(db, table);
-    typeWiggle = (hti != NULL && HTI_IS_WIGGLE);
+    if (sameString(customTrackPseudoDb, db))
+	{
+	struct customTrack *ct = lookupCt(table);
+	if (ct->wiggle)
+	    typeWiggle = FALSE;	/*	TEMPORARY, until we handle this case */
+				/*	this is actually the TRUE condition */
+	}
+    else
+	{
+	struct hTableInfo *hti = maybeGetHti(db, table);
+	typeWiggle = (hti != NULL && HTI_IS_WIGGLE);
+	}
     }
 return(typeWiggle);
 }
