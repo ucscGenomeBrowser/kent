e084760a68ed854c6220f9ba65f07d5ed228faca
kent <kent>
2004-06-09 01:07:30 +0000

    bumping version

--- src/inc/genoFind.h
+++ src/inc/genoFind.h
@@ -363,7 +363,7 @@ struct gfClump *gfPcrClumps(struct genoFind *gf,
 	int minDistance, int maxDistance);
 /* Find possible PCR hits.  The fPrimer and rPrimer are on opposite strands. */
 
-#define gfVersion 28	/* Current BLAT version number */
+#define gfVersion 29	/* Current BLAT version number */
 
 #endif /* GENOFIND_H */
 
