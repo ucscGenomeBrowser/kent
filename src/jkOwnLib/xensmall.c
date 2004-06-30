6868167ad511274033a2c337f8a6e28733a2e91d
kent <kent>
2004-06-30 16:22:11 +0000

    Moving some stuff from jkOwnLib/xensmall to lib/pairHmm

--- src/jkOwnLib/xensmall.c
+++ src/jkOwnLib/xensmall.c
@@ -8,349 +8,7 @@
 #include "xenalign.h"
 #include "pairHmm.h"
 
-static char const rcsid[] = "$Id: xensmall.c,v 1.5 2004/06/30 16:09:13 kent Exp $";
-
-/* Mommy coding scheme - this is how one cell in the dynamic programming table
- * points to it's parent (mommy) cell.  Since these tables are really big,
- * rather than use a simple pointer costing four bytes, we use a encoding
- * scheme that requires only one byte. 
- *
- * Bits 0-4  the "hidden" state of the mommy.  Lets us have 32 hidden states.
- *           currently only using 7.
- * Bit  5    whether or not mommy is in previous cell in query
- * Bit  6    whether or not mommy is in previous cell in target
- * Bit  7    set during trace back for cells along optimal path
- *
- * Since the query and target advancing bits (5 and 6) are never both zero,
- * it is safe to use the value of all-bits-zero as an indicator of
- * no mommy. */
-
-/* Compress state, query, and target offset into one byte. */
-#define phmmPackMommy(stateIx, qOff, tOff) ((UBYTE)((stateIx) + ((-(qOff))<<5) + ((-(tOff))<<6)))
-
-/* Traceback sets this, really just for debugging. */
-#define phmmMommyTraceBit (1<<7)
-
-static UBYTE nullMommy = 0; /* mommy value for orphans.... */
-
-void phmmUnpackMommy(UBYTE mommy, int *retStateIx, int *retQoff, int *retToff)
-/* Unpack state, query, and target offset. */
-{
-*retStateIx = (mommy&31);
-*retQoff = -((mommy&32)>>5);
-*retToff = -((mommy&64)>>6);
-}
-
-struct phmmMommy
-/* This contains the parent info for a single state of the matrix. */
-    {
-    UBYTE mommy; /* Unlike a parent, you can only have one mommy! */
-    };
-
-struct phmmState
-/* This corresponds to a hidden Markov state.  Each one of
- * these has a two dimensional array[targetSize+1][querySize+1]
- * of cells. */
-    {
-    struct phmmMommy *cells;	/* The 2-D array containing traceback info. */
-    int *scores;		/* Scores for the current row. */
-    int *lastScores;		/* Scores for the previous row. */
-    int stateIx;		/* Numerical handle on state. */
-    char *name;			/* Name of state. */
-    char emitLetter;		/* Single letter representing state. */
-    };
-
-struct phmmMatrix
-/* The alignment matrix - has an array of states. */
-    {
-    char *query;	/* One sequence to align- all lower case. */
-    char *target;    	/* Other sequence to align. */
-    int querySize;	/* Size of query. */
-    int targetSize;	/* Size of target. */
-    int qDim;		/* One plus size of query - dimension of matrix. */
-    int tDim;		/* One plus size of target - dimension of matrix. */
-    int stateCount;	/* Number of hidden states in HMM. */
-    int stateSize;	/* Number of cells in each state's matrix. */
-    int stateByteSize;	/* Number of bytes used by each state's matrix. */
-    struct phmmState *states;  /* Array of states. */
-    struct phmmMommy *allCells; /* Memory for all matrices. */
-    int *allScores;	      /* Memory for two rows of scores. */
-    };
-
-static void phmmMatrixInit(struct phmmMatrix *am, int stateCount, 
-    char *query, int querySize, char *target, int targetSize)
-/* Allocate all memory required for an phmmMatrix. Set up dimensions. */
-{
-int i;
-struct phmmMommy *allCells;
-int allCellSize;
-int rowSize;
-int *allScores;
-
-zeroBytes(am, sizeof(*am));
-am->query = query;
-am->target = target;
-am->querySize = querySize;
-am->targetSize = targetSize;
-am->qDim = rowSize = am->querySize + 1;
-am->tDim = am->targetSize + 1;
-am->stateCount = stateCount;
-am->stateSize = rowSize * am->tDim;
-am->stateByteSize = am->stateSize * sizeof(struct phmmMommy);
-am->states = needMem(stateCount * sizeof(struct phmmState));
-
-/* Initialize matrix of cells for each state. */
-allCellSize = stateCount * am->stateByteSize;
-am->allCells = allCells = needLargeMem(allCellSize); 
-memset(allCells, 0, allCellSize);
-for (i=0; i<stateCount; ++i)
-    {
-    am->states[i].cells = allCells;
-    allCells += am->stateSize;
-    am->states[i].stateIx = i;
-    }
-
-/* Initialize two rows of scores for each state. */
-allScores = am->allScores = needMem(rowSize * 2 * stateCount * sizeof(int) );
-for (i=0; i<stateCount; ++i)
-    {
-    am->states[i].scores = allScores;
-    allScores += rowSize;
-    am->states[i].lastScores = allScores;
-    allScores += rowSize;
-    }
-}
-
-static void phmmMatrixCleanup(struct phmmMatrix *am)
-/* Free up memory required for an phmmMatrix and make sure
- * nobody reuses it. */
-{
-freeMem(am->states);
-freeMem(am->allCells);
-freeMem(am->allScores);
-zeroBytes(am, sizeof(*am));
-}
-
-static struct phmmState *phmmNameState(struct phmmMatrix *am, int stateIx, 
-	char *name, char emitLetter)
-/* Give a name to a state and return a pointer to it. */
-{
-struct phmmState *state;
-assert(stateIx < am->stateCount);
-state = &am->states[stateIx];
-state->name = name;
-state->emitLetter = emitLetter;
-return state;
-}
-
-static void phmmFindMatrixIx(struct phmmMatrix *am, struct phmmMommy *cell, 
-	int *retStateIx, int *retQix, int *retTix)
-/* Given a cell in matrix return state, query, and target index. */
-{
-int cellIx = cell - am->allCells;
-*retStateIx = cellIx/am->stateSize;
-cellIx %= am->stateSize;
-*retTix = cellIx / am->qDim;
-*retQix = cellIx % am->qDim;
-}
-
-static struct phmmMommy *phmmFindMommy(struct phmmMatrix *am, 
-	struct phmmMommy *baby)
-/* Find baby's mommy and return it. */
-{
-int momStateIx, qOff, tOff;
-int babyStateIx, qIx, tIx;
-UBYTE mommy;
-
-if ((mommy = baby->mommy) == nullMommy)
-    return NULL;
-phmmUnpackMommy(mommy, &momStateIx, &qOff, &tOff);
-phmmFindMatrixIx(am, baby, &babyStateIx, &qIx, &tIx);
-return am->states[momStateIx].cells + (tOff + tIx) * am->qDim + (qOff + qIx);
-}
-
-struct phmmAliPair
-/* Each position in alignment gets one of these. */
-    {
-    struct phmmAliPair *next;
-    int queryIx;
-    int targetIx;
-    UBYTE hiddenIx;  /* If I code one of these with more than 255 states shoot me! */
-    char querySym;
-    char targetSym;
-    char hiddenSym;
-    };
-
-static struct phmmAliPair *phmmTraceBack(struct phmmMatrix *am, struct phmmMommy *end)
-/* Create list of alignment pair by tracing back through matrix from end
- * state back to a start.*/
-{
-struct phmmAliPair *pairList = NULL, *pair;
-struct phmmMommy *cell, *parent = end;
-int parentSix, parentTix, parentQix;
-int sIx, tIx, qIx;
-
-phmmFindMatrixIx(am, parent, &parentSix, &parentQix, &parentTix);
-for (;;)
-    {
-    cell = parent;
-    sIx = parentSix;
-    tIx = parentTix;
-    qIx = parentQix;
-
-    if ((parent = phmmFindMommy(am, cell)) == NULL)
-        break;
-    phmmFindMatrixIx(am, parent, &parentSix, &parentQix, &parentTix);
-    
-    cell->mommy != phmmMommyTraceBit;
-
-    AllocVar(pair);
-    pair->hiddenIx = (UBYTE)sIx;
-    pair->hiddenSym = am->states[sIx].emitLetter; 
-
-    if (parentQix == qIx - 1 && parentTix == tIx - 1 )
-        {
-        pair->queryIx = qIx-1;
-        pair->querySym = am->query[qIx-1];
-        pair->targetIx = tIx - 1;
-        pair->targetSym = am->target[tIx-1];
-        }
-    else if (parentQix == qIx) /* && parentTix == tIx-1 */
-        {
-        pair->queryIx = -1;
-        pair->querySym = '-';
-        pair->targetIx = tIx-1;
-        pair->targetSym = am->target[tIx-1];
-        }
-    else  /* parentTix == tIx  && parentQix == qIx-1 */
-        {
-        pair->queryIx = qIx-1;
-        pair->querySym = am->query[qIx-1];
-        pair->targetIx = -1;
-        pair->targetSym = '-';
-        }
-    slAddHead(&pairList, pair);
-    }
-return pairList;
-}
-
-static void phmmPrintTrace(struct phmmMatrix *am, struct phmmAliPair *pairList, boolean showStates, 
-    FILE *f, boolean extraAtEnds)
-/* Print out trace to file. */
-{
-struct phmmAliPair *pair;
-#define lineLen 50
-char asyms[lineLen+1];
-char qsyms[lineLen+1];
-char tsyms[lineLen+1];
-char hsyms[lineLen+1];
-int lineIx = 0;
-int qs, ts;
-boolean gotQs = FALSE;
-
-if ((pair = pairList) == NULL)
-    {
-    fprintf(f, "Empty pair list\n");
-    return;
-    }
-
-qs = pair->queryIx;
-ts = pair->targetIx;
-
-/* Print out up to 25 bp of initial non-matching parts. */
-if (extraAtEnds)
-    {
-    int qStart = qs;
-    int tStart = ts;
-    int i;
-
-    for (i= -25; i < 0; ++i)
-        {
-        int qIx = qStart + i;
-        int tIx = tStart + i;
-        qsyms[lineIx] = (qIx >= 0 ? am->query[qIx] : ' ');
-        tsyms[lineIx] = (tIx >= 0 ? am->target[tIx] : ' ' );
-        asyms[lineIx] = hsyms[lineIx] = ' ';
-        if (qIx >= 0 || tIx >= 0)
-            {
-            ++lineIx;
-            if (!gotQs)
-                {
-                gotQs = TRUE;
-                qs = qIx;
-                ts = tIx;
-                }
-            }
-        }
-    }
-
-/* Print out matching parts */
-for (pair = pairList; pair != NULL; pair = pair->next)
-    {
-    qsyms[lineIx] = pair->querySym;
-    tsyms[lineIx] = pair->targetSym;
-    asyms[lineIx] = (pair->querySym == pair->targetSym ? '|' : ' ');
-    hsyms[lineIx] = pair->hiddenSym;
-    if (++lineIx == lineLen)
-        {
-        qsyms[lineIx] = asyms[lineIx] = tsyms[lineIx] = hsyms[lineIx] = 0;
-        fprintf(f, "%5d %s\n", qs, qsyms);
-        fprintf(f, "%5s %s\n", "", asyms);
-        fprintf(f, "%5d %s\n", ts, tsyms);
-        if (showStates)
-            fprintf(f, "%5s %s\n", "", hsyms);
-        fputc('\n', f);
-        lineIx = 0;
-        if (pair->next)
-            {
-            qs = pair->next->queryIx;
-            ts = pair->next->targetIx;
-            }
-        }
-    }
-
-/* Print out last bit - first get last pair. */
-if (extraAtEnds)
-    {
-    for (pair = pairList; pair->next != NULL; pair = pair->next)
-        ;
-
-        {
-        int qIx, tIx, i;
-        int residue = lineLen - lineIx;
-        for (i=1; i<=residue; i++)
-            {
-            if ((qIx = pair->queryIx+i) >= am->querySize)
-                qsyms[lineIx] = ' ';
-            else
-                qsyms[lineIx] = am->query[qIx];
-            if ((tIx = pair->targetIx+i) >= am->targetSize)
-                tsyms[lineIx] = ' ';
-            else
-                tsyms[lineIx] = am->target[tIx];
-            asyms[lineIx] = ' ';
-            hsyms[lineIx] = ' ';
-            ++lineIx;
-            assert(lineIx <= lineLen);
-            }
-        }
-    }
-
-/* Print out anything not printed out yet. */
-if (lineIx != 0)
-    {
-    qsyms[lineIx] = asyms[lineIx] = tsyms[lineIx] = hsyms[lineIx] = 0;
-    fprintf(f, "%5d %s\n", qs, qsyms);
-    fprintf(f, "%5s %s\n", "", asyms);
-    fprintf(f, "%5d %s\n", ts, tsyms);
-    if (showStates)
-        fprintf(f, "%5s %s\n", "", hsyms);
-    fputc('\n', f);
-    lineIx = 0;
-    }
-#undef lineLen
-}
-
+static char const rcsid[] = "$Id: xensmall.c,v 1.6 2004/06/30 16:22:11 kent Exp $";
 
 static double calcGcRatio(DNA *a, int aSize, DNA *b, int bSize)
 /* Figure out percentage of g/c in a and b. */
@@ -648,7 +306,7 @@ for (tIx = 1; tIx < a.tDim; tIx += 1)
     struct phmmMommy *newCell = state->cells + newCellOffset; \
     if (score <= 0) \
         { \
-        mommy = nullMommy; \
+        mommy = phmmNullMommy; \
         score = 0; \
         } \
     newCell->mommy = mommy; \
@@ -667,13 +325,12 @@ for (tIx = 1; tIx < a.tDim; tIx += 1)
     struct phmmMommy *newCell = state->cells + newCellOffset; \
     if (score <= 0) \
         { \
-        mommy = nullMommy; \
+        mommy = phmmNullMommy; \
         score = 0; \
         } \
     newCell->mommy = mommy; \
     state->newScore = score; \
     }
-
 
     rowOffset = tIx*a.qDim;
     for (qIx = 1; qIx < a.qDim; qIx += 1)
@@ -915,7 +572,7 @@ for (tIx = 1; tIx < a.tDim; tIx += 1)
     struct phmmMommy *newCell = state->cells + newCellOffset; \
     if (score <= 0) \
         { \
-        mommy = nullMommy; \
+        mommy = phmmNullMommy; \
         score = 0; \
         } \
     newCell->mommy = mommy; \
@@ -934,7 +591,7 @@ for (tIx = 1; tIx < a.tDim; tIx += 1)
     struct phmmMommy *newCell = state->cells + newCellOffset; \
     if (score <= 0) \
         { \
-        mommy = nullMommy; \
+        mommy = phmmNullMommy; \
         score = 0; \
         } \
     newCell->mommy = mommy; \
