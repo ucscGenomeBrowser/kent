01829abc623615ee784d59d151e9e651416dd68e
kent <kent>
2004-07-17 00:37:59 +0000

    Moving most of good stuff to hg/lib/joiner.c

--- src/hg/oneShot/cJoinX/cJoinX.c
+++ src/hg/oneShot/cJoinX/cJoinX.c
@@ -5,7 +5,7 @@
 #include "options.h"
 #include "joiner.h"
 
-static char const rcsid[] = "$Id: cJoinX.c,v 1.3 2004/07/16 23:58:51 kent Exp $";
+static char const rcsid[] = "$Id: cJoinX.c,v 1.4 2004/07/17 00:37:59 kent Exp $";
 
 void usage()
 /* Explain usage and exit. */
@@ -23,193 +23,6 @@ static struct optionSpec options[] = {
    {NULL, 0},
 };
 
-struct slName *commaSepToSlName(char *commaSep)
-/* Convert comma-separated list of items to slName list. */
-{
-struct slName *list = NULL, *el;
-char *s, *e;
-
-s = commaSep;
-while (s != NULL && s[0] != 0)
-    {
-    e = strchr(s, ',');
-    if (e == NULL)
-        {
-	el = slNameNew(s);
-	slAddHead(&list, el);
-	break;
-	}
-    else
-        {
-	el = slNameNewN(s, e - s);
-	slAddHead(&list, el);
-	s = e+1;
-	}
-    }
-slReverse(&list);
-return list;
-}
-
-boolean sameTable(struct joinerDtf *a, struct joinerDtf *b)
-/* Return TRUE if they are in the same database and table. */
-{
-return sameString(a->database, b->database) && sameString(a->table, b->table);
-}
-
-static struct joinerSet *identifierStack[4];	/* Help keep search from looping. */
-
-boolean identifierInArray(struct joinerSet *identifier, 
-	struct joinerSet **array, int count)
-/* Return TRUE if identifier is in array. */
-{
-int i;
-for (i=0; i<count; ++i)
-    {
-    if (identifier == array[i])
-	{
-        return TRUE;
-	}
-    }
-return FALSE;
-}
-
-struct joinerPair *joinerPairDupe(struct joinerPair *jp)
-/* Return duplicate (deep copy) of joinerPair. */
-{
-struct joinerPair *dupe;
-AllocVar(dupe);
-dupe->a = joinerDtfNew(jp->a->database, jp->a->table, jp->a->field);
-dupe->b = joinerDtfNew(jp->b->database, jp->b->table, jp->b->field);
-dupe->identifier = jp->identifier;
-return dupe;
-}
-
-struct joinerPair *rFindRoute(struct joiner *joiner, 
-	struct joinerDtf *a,  struct joinerDtf *b, int level, int maxLevel)
-/* Find a path that connects the two fields if possible. */
-{
-struct joinerPair *jpList, *jp;
-struct joinerPair *path = NULL;
-char buf[256];
-jpList = joinerRelate(joiner, a->database, a->table);
-for (jp = jpList; jp != NULL; jp = jp->next)
-    {
-    if (sameTable(jp->b, b))
-	{
-	path = joinerPairDupe(jp);
-	break;
-	}
-    }
-if (path == NULL && level < maxLevel)
-    {
-    for (jp = jpList; jp != NULL; jp = jp->next)
-        {
-	identifierStack[level] = jp->identifier;
-	if (!identifierInArray(jp->identifier, identifierStack, level))
-	    {
-	    identifierStack[level] = jp->identifier;
-	    path = rFindRoute(joiner, jp->b, b, level+1, maxLevel);
-	    if (path != NULL)
-		{
-		struct joinerPair *jpDupe = joinerPairDupe(jp);
-		slAddHead(&path, jpDupe);
-		break;
-		}
-	    }
-	}
-    }
-joinerPairFreeList(&jpList);
-return path;
-}
-
-struct joinerPair *findRoute(struct joiner *joiner, 
-	struct joinerDtf *a,  struct joinerDtf *b)
-/* Find route between a and b. */
-{
-int i;
-struct joinerPair *jpList = NULL;
-for (i=1; i<ArraySize(identifierStack); ++i)
-    {
-    jpList = rFindRoute(joiner, a, b, 0, i);
-    if (jpList != NULL)
-        break;
-    }
-return jpList;
-}
-
-
-boolean allSameTable(struct joinerDtf *fieldList)
-/* Return TRUE if all joinerPairs refer to same table. */
-{
-struct joinerDtf *first = fieldList, *field;
-if (first == NULL)
-    return TRUE;
-for (field = first->next; field != NULL; field = field->next)
-    if (!sameTable(first, field))
-        return FALSE;
-return TRUE;
-}
-
-boolean inRoute(struct joinerPair *routeList, struct joinerDtf *dtf)
-/* Return TRUE if table in dtf is already in route. */
-{
-struct joinerPair *jp;
-for (jp = routeList; jp != NULL; jp = jp->next)
-    {
-    if (sameTable(dtf, jp->a) || sameTable(dtf, jp->b))
-        return TRUE;
-    }
-return FALSE;
-}
-
-void removeDupes(struct joinerPair **pRouteList)
-/* Remove duplicate entries in route list. */
-{
-struct hash *uniqHash = newHash(0);
-struct joinerPair *newList = NULL, *jp, *next;
-char buf[256];
-
-for (jp = *pRouteList; jp != NULL; jp = next)
-    {
-    next = jp->next;
-    safef(buf, sizeof(buf), "%s.%s->%s.%s", 
-    	jp->a->database, jp->a->table, 
-    	jp->b->database, jp->b->table);
-    if (hashLookup(uniqHash, buf))
-	{
-        joinerPairFree(&jp);
-	}
-    else
-        {
-	hashAdd(uniqHash, buf, NULL);
-	slAddHead(&newList, jp);
-	}
-    }
-slReverse(&newList);
-*pRouteList = newList;
-}
-
-struct joinerPair *findRouteThroughAll(struct joiner *joiner, 
-	struct joinerDtf *jpList)
-/* Return route that gets to all tables.  */
-{
-struct joinerPair *fullRoute = NULL, *pairRoute = NULL, *routeLink;
-struct joinerDtf *first = jpList, *dtf;
-
-if (first->next == NULL)
-    return NULL;
-for (dtf = first->next; dtf != NULL; dtf = dtf->next)
-    {
-    if (!inRoute(fullRoute, dtf))
-	{
-	pairRoute = findRoute(joiner, first, dtf);
-	fullRoute = slCat(fullRoute, pairRoute);
-	}
-    }
-removeDupes(&fullRoute);
-return fullRoute;
-}
-
 void cJoinX(char *j1, char *j2, char *j3)
 /* cJoinX - Experiment in C joining.. */
 {
@@ -225,11 +38,11 @@ slAddTail(&fieldList, a);
 slAddTail(&fieldList, b);
 slAddTail(&fieldList, c);
 
-if (allSameTable(fieldList))
+if (joinerDtfAllSameTable(fieldList))
     printf("All in same table, easy enough!\n");
 else
     {
-    jpList = findRouteThroughAll(joiner, fieldList);
+    jpList = joinerFindRouteThroughAll(joiner, fieldList);
     for (jp = jpList; jp != NULL; jp = jp->next)
 	{
 	printf("%s.%s.%s -> %s.%s.%s\n", 
