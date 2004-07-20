f6c9dad29ece7c5f61e21d21a0148abdcc5b05ce
kent <kent>
2004-07-20 20:50:20 +0000

    Freen tests chromCmp

--- src/hg/oneShot/freen/freen.c
+++ src/hg/oneShot/freen/freen.c
@@ -8,7 +8,7 @@
 #include "jksql.h"
 #include "mysqlTableStatus.h"
 
-static char const rcsid[] = "$Id: freen.c,v 1.46 2004/06/10 22:11:49 galt Exp $";
+static char const rcsid[] = "$Id: freen.c,v 1.47 2004/07/20 20:50:20 kent Exp $";
 
 void usage()
 /* Print usage and exit. */
@@ -16,17 +16,55 @@ void usage()
 errAbort("usage: freen something");
 }
 
-void freen(char *in)
+int countSameNonDigit(char *a, char *b)
+/* Return count of characters in a,b that are the same
+ * up until first digit in either one. */
+{
+char cA, cB;
+int same = 0;
+for (;;)
+   {
+   cA = *a++;
+   cB = *b++;
+   if (cA != cB)
+       break;
+   if (isdigit(cA) || isdigit(cB))
+       break;
+   ++same;
+   }
+return same;
+}
+
+boolean allDigits(char *s)
+/* Return TRUE if s is all digits */
+{
+char c;
+while ((c = *s++) != 0)
+    if (!isdigit(c))
+        return FALSE;
+return TRUE;
+}
+
+int cmpChrom(char *a, char *b)
+/* Compare two chromosomes. */
+{
+int cSame = countSameNonDigit(a, b);
+int diff;
+
+a += cSame;
+b += cSame;
+uglyf("cSame %d, a %s, b %s\n", cSame, a, b);
+if (allDigits(a) && allDigits(b))
+    return atoi(a) - atoi(b);
+else
+    return strcmp(a,b);
+}
+
+void freen(char *a, char *b)
 /* Test some hair-brained thing. */
 {
-char *pt = needMem(1025);
-pt[1025] = 0;
-int i;
-for (i=0; i<100; ++i)
-    pt = needMem(100000);
-carefulCheckHeap();
-pt[-1] = 0;
-freeMem(pt);
+int diff = cmpChrom(a,b);
+uglyf( "diff = % d \n", diff);
 }
 
 
@@ -34,8 +72,8 @@ int main(int argc, char *argv[])
 /* Process command line. */
 {
 pushCarefulMemHandler(1000000);
-if (argc != 2)
+if (argc != 3)
    usage();
-freen(argv[1]);
+freen(argv[1], argv[2]);
 return 0;
 }
