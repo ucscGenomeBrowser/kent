4d722b013c7f348c70a25d6361dede4877ccadc6
hiram <hiram>
2004-07-30 19:28:40 +0000

    Add eraseCharacter(), eliminate warning

--- src/lib/common.c
+++ src/lib/common.c
@@ -7,7 +7,7 @@
 #include "common.h"
 #include "errabort.h"
 
-static char const rcsid[] = "$Id: common.c,v 1.65 2004/07/23 04:30:51 kent Exp $";
+static char const rcsid[] = "$Id: common.c,v 1.66 2004/07/30 19:28:40 hiram Exp $";
 
 void *cloneMem(void *pt, size_t size)
 /* Allocate a new buffer of given size, and copy pt to it. */
@@ -407,7 +407,7 @@ double doubleMedian(int count, double *array)
 {
 double median;
 doubleSort(count, array);
-if (count&1 == 1)
+if ((count&1) == 1)
     median = array[count>>1];
 else
     {
@@ -1132,6 +1132,24 @@ for (;;)
     if (c == 0) 
 	break;
     if (!isspace(c))
+	*out++ = c;
+    }
+*out++ = 0;
+}
+
+/* Remove specified character from a string */
+void eraseCharacter(char *s, char e)
+{
+char *in, *out;
+char c;
+
+in = out = s;
+for (;;)
+    {
+    c = *in++;
+    if (c == 0) 
+	break;
+    if (c != e)
 	*out++ = c;
     }
 *out++ = 0;
