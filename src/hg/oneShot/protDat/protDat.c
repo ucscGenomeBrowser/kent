00239ec4e050123ae061f0c3c4f959af68960f56
braney <braney>
2004-08-13 23:48:01 +0000

    update to use FlyBase or KnownGene

--- src/hg/oneShot/protDat/protDat.c
+++ src/hg/oneShot/protDat/protDat.c
@@ -13,10 +13,15 @@ errAbort(
   "          the position of the protein (using argv[2]) and various alias as defined by argv[3]\n"
   "usage:\n"
   "   protDat protein.psl proteinBlat.psl aliases outFile\n"
+  "options:\n"
+  "   -fb use FlyBase defaults\n"
+  "   -kg use KnownGene defaults\n"
   );
 }
 
 static struct optionSpec options[] = {
+   {"fb", OPTION_BOOLEAN},
+   {"kg", OPTION_BOOLEAN},
    {NULL, 0},
 };
 
@@ -38,12 +43,14 @@ struct lineFile *lf = lineFileOpen(aliasFile, TRUE);
 struct alias *alPtr;
 char buffer[1024];
 char *words[3];
+int numWords = optionExists("fb") ? 2 : 3;
 
-while (lineFileRow(lf, words))
+while (lineFileNextRow(lf, words, numWords))
     {
     AllocVar(alPtr);
     alPtr->kgName = cloneString(words[1]);
-    alPtr->spName = cloneString(words[2]);
+    if (numWords == 3)
+	alPtr->spName = cloneString(words[2]);
     hashAdd(aliasHash, cloneString(words[0]), alPtr);
     }
 
@@ -59,8 +66,12 @@ for(pslPtr = protPsls; pslPtr; pslPtr = pslPtr->next)
 	{
 	if ((alPtr = hashFindVal(aliasHash, pslPtr->qName)) != NULL)
 	    {
-	    sprintf(buffer,"%s.%s:%d-%d.%s.%s",pslPtr->qName,blatPsl->tName, 
-		blatPsl->tStart, blatPsl->tEnd,alPtr->kgName, alPtr->spName); 
+	    if (numWords == 3)
+		sprintf(buffer,"%s.%s:%d-%d.%s.%s",pslPtr->qName,blatPsl->tName, 
+		    blatPsl->tStart, blatPsl->tEnd,alPtr->kgName, alPtr->spName); 
+	    else
+		sprintf(buffer,"%s.%s:%d-%d.%s",pslPtr->qName,blatPsl->tName, 
+		    blatPsl->tStart, blatPsl->tEnd,alPtr->kgName); 
 	    pslPtr->qName = buffer;
 	    pslTabOut(pslPtr, outFile);
 	    }
