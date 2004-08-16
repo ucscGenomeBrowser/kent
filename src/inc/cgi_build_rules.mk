5d3f551ca70305d002cb24c5ac7c1cc1fe876cc7
angie <angie>
2004-08-16 17:12:41 +0000

    Added ${EXE} suffix variable for portability after all post-compile $A's. Use ${STRIP} instead of strip.

--- src/inc/cgi_build_rules.mk
+++ src/inc/cgi_build_rules.mk
@@ -1,36 +1,36 @@
 #	Common set of build rules for CGI binaries
 
 my:: compile
-	chmod a+rx $A
-	mv $A ${CGI_BIN}-${USER}/$A
+	chmod a+rx $A${EXE}
+	mv $A${EXE} ${CGI_BIN}-${USER}/$A${EXE}
 
 alpha:: strip
-	mv $A ${CGI_BIN}/$A
+	mv $A${EXE} ${CGI_BIN}/$A${EXE}
 
 beta:: strip
-	mv $A ${CGI_BIN}-beta/$A
+	mv $A${EXE} ${CGI_BIN}-beta/$A${EXE}
 
 strip::  compile
-	strip $A
-	chmod g+w $A
-	chmod a+rx $A
+	${STRIP} $A${EXE}
+	chmod g+w $A${EXE}
+	chmod a+rx $A${EXE}
 
 install::  strip
 	@if [ ! -d "${DESTDIR}${CGI_BIN}" ]; then \
 		${MKDIR} "${DESTDIR}${CGI_BIN}"; \
 	fi
-	mv $A ${DESTDIR}${CGI_BIN}/$A
+	mv $A${EXE} ${DESTDIR}${CGI_BIN}/$A${EXE}
 
 debug:: $O
 	${CC} $O ${MYLIBS} ${L}
-	mv a.out $A
+	mv a.out $A${EXE}
 
 
 lib::
 	cd ../../lib; make
 
 clean::
-	rm -f $O $A
+	rm -f $O $A${EXE}
 
 tags::
 	ctags *.h *.c ../lib/*.c ../inc/*.h ../../lib/*.c ../../inc/*.h
