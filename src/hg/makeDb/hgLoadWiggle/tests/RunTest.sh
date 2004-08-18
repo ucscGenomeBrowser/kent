a1f40be215888e11ebc27ce1dad4225e1c13147d
hiram <hiram>
2004-08-18 20:31:03 +0000

    stats output has changed ...

--- src/hg/makeDb/hgLoadWiggle/tests/RunTest.sh
+++ src/hg/makeDb/hgLoadWiggle/tests/RunTest.sh
@@ -37,19 +37,19 @@ fi
 
 oneTest "$HGWIGGLE -chr=chrM -db=ce2 gc5Base" "26963    31"
 oneTest "$HGWIGGLE -chr=chrM -noAscii -doBed -db=ce2 gc5Base" "37864     1"
-oneTest "$HGWIGGLE -chr=chrM -noAscii -doStats -db=ce2 gc5Base" "40754     2"
+oneTest "$HGWIGGLE -chr=chrM -noAscii -doStats -db=ce2 gc5Base" "41112     1"
 oneTest "$HGWIGGLE -position=chrM:4000-10000 -db=ce2 gc5Base" "15554    14"
 oneTest "$HGWIGGLE -position=chrM:4000-10000 -dataConstraint=\">\" -ll=50 -db=ce2 gc5Base" "22894     2"
 oneTest "$HGWIGGLE -position=chrM:4000-10000 -dataConstraint=\"<=\" -ll=50 -db=ce2 gc5Base" "01708    12"
 oneTest "$HGWIGGLE -position=chrM:4000-10000 -dataConstraint=\"in range\" -ll=23 -ul=60 -db=ce2 gc5Base" "24311     6"
 oneTest "$HGWIGGLE -noAscii -doBed -position=chrM:4000-10000 -dataConstraint=\">\" -ll=50 -db=ce2 gc5Base" "15056     3"
-oneTest "$HGWIGGLE -noAscii -doStats -position=chrM:4000-10000 -dataConstraint=\">\" -ll=50 -db=ce2 gc5Base" "08347     1"
-oneTest "$HGWIGGLE -doBed -doStats -position=chrM:4000-10000 -dataConstraint=\">\" -ll=50 -db=ce2 gc5Base" "65279     6"
+oneTest "$HGWIGGLE -noAscii -doStats -position=chrM:4000-10000 -dataConstraint=\">\" -ll=50 -db=ce2 gc5Base" "55754     1"
+oneTest "$HGWIGGLE -doBed -doStats -position=chrM:4000-10000 -dataConstraint=\">\" -ll=50 -db=ce2 gc5Base" "00191     5"
 oneTest "$HGWIGGLE -noAscii -doBed -position=chrM:4000-10000 -dataConstraint=\"<=\" -ll=50 -db=ce2 gc5Base" "51931     3"
-oneTest "$HGWIGGLE -doStats -doBed -position=chrM:4000-10000 -dataConstraint=\"<=\" -ll=50 -db=ce2 gc5Base" "16333    16"
+oneTest "$HGWIGGLE -doStats -doBed -position=chrM:4000-10000 -dataConstraint=\"<=\" -ll=50 -db=ce2 gc5Base" "16092    15"
 oneTest "$HGWIGGLE -noAscii -doBed -position=chrM:4000-10000 -dataConstraint=\"in range\" -ll=23 -ul=60 -db=ce2 gc5Base" "13974     7"
-oneTest "$HGWIGGLE -noAscii -doStats -position=chrM:4000-10000 -dataConstraint=\"in range\" -ll=23 -ul=60 -db=ce2 gc5Base" "40592     2"
-oneTest "$HGWIGGLE -doStats -doBed -position=chrM:4000-10000 -dataConstraint=\"in range\" -ll=23 -ul=60 -db=ce2 gc5Base" "60754    14"
+oneTest "$HGWIGGLE -noAscii -doStats -position=chrM:4000-10000 -dataConstraint=\"in range\" -ll=23 -ul=60 -db=ce2 gc5Base" "21014     1"
+oneTest "$HGWIGGLE -doStats -doBed -position=chrM:4000-10000 -dataConstraint=\"in range\" -ll=23 -ul=60 -db=ce2 gc5Base" "61592    13"
 
 if [ -n "${verbose}" ]; then
     C=`echo $tests | awk '{printf "%4d", $1}'`
