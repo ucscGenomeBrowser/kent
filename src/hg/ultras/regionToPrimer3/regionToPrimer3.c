488a9d67f1683d63c6cda06845afaea732b4bfc2
kent <kent>
2004-09-04 22:53:13 +0000

    Changing so that it will try to find close-by primers first.

--- src/hg/ultras/regionToPrimer3/regionToPrimer3.c
+++ src/hg/ultras/regionToPrimer3/regionToPrimer3.c
@@ -7,6 +7,12 @@
 #include "dnaSeq.h"
 #include "fa.h"
 
+char *exe = "primer3_core";
+double minTm = 57.0;
+double maxTm = 63.0;
+char *tempInName = "rtp_tmp.in";
+char *tempOutName = "rtp_tmp.out";
+
 void usage()
 /* Explain usage and exit. */
 {
@@ -15,7 +21,10 @@ errAbort(
   "usage:\n"
   "   regionToPrimer3 input output\n"
   "options:\n"
-  "   -xxx=XXX\n"
+  "   -exe=/path/to/primer3_core - Where to find primer3_core\n"
+  "   -minTm=X - minimum primer melting temp (default %4.1f)\n"
+  "   -maxTm=X - max primer melting temp (default %4.1f)\n"
+  , minTm, maxTm
   );
 }
 
@@ -95,12 +104,121 @@ dyStringClear(dy);
 return TRUE;
 }
 
+boolean parseWritePrimer3Output(char *seqName, int midSize, char *inFile, FILE *f)
+/* Parse inFile.  If it looks like it has valid primers write out 
+ * tab-separated result and return TRUE. */
+{
+boolean ok = FALSE;
+struct lineFile *lf = lineFileOpen(inFile, TRUE);
+char *tag, *val;
+char *left, *right, *size, *leftTm, *rightTm;
+
+left = right = size = leftTm = rightTm = NULL;
+
+while (lineFileNextReal(lf, &tag))
+    {
+    char *val = strchr(tag, '=');
+    if (val == tag)
+        break;
+    *val++ = 0;
+    if (sameString(tag, "PRIMER_LEFT_SEQUENCE"))
+        left = cloneString(val);
+    else if (sameString(tag, "PRIMER_RIGHT_SEQUENCE"))
+        right = cloneString(val);
+    else if (sameString(tag, "PRIMER_PRODUCT_SIZE"))
+        size = cloneString(val);
+    if (sameString(tag, "PRIMER_LEFT_TM"))
+        leftTm = cloneString(val);
+    if (sameString(tag, "PRIMER_RIGHT_TM"))
+        rightTm = cloneString(val);
+    }
+lineFileClose(&lf);
+
+if (left != NULL && right != NULL & size != NULL && leftTm != NULL && rightTm != NULL)
+    {
+    ok = TRUE;
+    fprintf(f, "%s", seqName);
+    fprintf(f, "\t%s", left);
+    fprintf(f, "\t%s", right);
+    fprintf(f, "\t%s", size);
+    fprintf(f, "\t%d", midSize);
+    fprintf(f, "\t%s", leftTm);
+    fprintf(f, "\t%s", rightTm);
+    fprintf(f, "\n");
+    }
+freeMem(left);
+freeMem(right);
+freeMem(size);
+freeMem(leftTm);
+freeMem(rightTm);
+return ok;
+}
+
+
+boolean primer3Works(FILE *out, char *name, int primerMinSize, DNA *dna, int startSize, int midSize, int endSize)
+/* Make system call to primer 3 to build primers that flank mid-region.
+ * Return TRUE and write result to file if it succeeds.  Otherwise
+ * return FALSE. */
+{
+char command[512];
+int totalSize = startSize + midSize + endSize;
+
+/* Create input file for primer3 */
+FILE *f = mustOpen(tempInName, "w");
+fprintf(f, "PRIMER_SEQUENCE_ID=%s\n", name);
+fprintf(f, "SEQUENCE=");
+mustWrite(f, dna, totalSize);
+fprintf(f, "\n");
+fprintf(f, "TARGET=%d,%d\n", startSize+1, midSize);
+fprintf(f, "PRIMER_EXPLAIN_FLAG=1\n");
+fprintf(f, "PRIMER_PRODUCT_SIZE_RANGE=%d-%d\n", midSize, totalSize);
+fprintf(f, "PRIMER_MIN_TM=%5.2f\n", minTm);
+fprintf(f, "PRIMER_MAX_TM=%5.2f\n", maxTm);
+fprintf(f, "PRIMER_MIN_SIZE=%d\n", primerMinSize);
+fprintf(f, "=\n");
+carefulClose(&f);
+
+/* Make system call to primer3 that will hopefully result in temp output. */
+remove(tempOutName);
+safef(command, sizeof(command), "%s < %s > %s", exe, tempInName, tempOutName);
+system(command);
+
+return parseWritePrimer3Output(name, midSize + 2*primerMinSize, tempOutName, out);
+}
+
+boolean findPrimers(FILE *f, char *name, DNA *dna, int startSize, int midSize, int endSize)
+/* Call primer3 to find primer that will amplify midSize region, and write results to file. */
+{
+int primerMinSize = 16;
+int extra;
+int totalSize = startSize + midSize + endSize;
+for (extra = 25; 
+    extra <= startSize+primerMinSize || extra <= endSize + primerMinSize;
+    extra += 25)
+    {
+    int sStart = startSize + primerMinSize - extra;
+    int sEnd = startSize + midSize - primerMinSize + extra;
+    if (sStart < 0)
+        sStart = 0;
+    if (sEnd > totalSize)
+        sEnd = totalSize;
+    if (primer3Works(f, name, primerMinSize, dna+sStart, 
+    	(startSize + primerMinSize) - sStart, midSize - 2*primerMinSize,
+	sEnd - (startSize + midSize - primerMinSize)))
+	return TRUE;
+    }
+warn("Couldn't find primers for %s", name);
+return FALSE;
+}
+
+
+
 void regionToPrimer3(char *inFile, char *outFile)
 /* regionToPrimer3 - Convert fasta region file to primer3 input. */
 {
 struct lineFile *lf = lineFileOpen(inFile, TRUE);
 FILE *f = mustOpen(outFile, "w");
-char *dna;
+DNA *dna;
 int size;
 char *name;
 int startSize, midSize, endSize;
@@ -123,15 +241,10 @@ while (regionReadNext(lf, &dna, &size, &name))
     polyToLower(pClose+1, dna+size - pClose - 1, 'C', 4);
     lowerToN(dna, size);
 
-    /* Create input record for primer3. */
-    fprintf(f, "PRIMER_SEQUENCE_ID=%s\n", name);
-    fprintf(f, "SEQUENCE=%s\n", dna);
-    fprintf(f, "TARGET=%d,%d\n", startSize+1, midSize);
-    fprintf(f, "PRIMER_EXPLAIN_FLAG=1\n");
-    fprintf(f, "PRIMER_PRODUCT_SIZE_RANGE=%d-%d\n", midSize, midSize+300);
-    fprintf(f, "PRIMER_PRODUCT_OPT_SIZE=%d\n", midSize+32);
-    fprintf(f, "=\n");
+    findPrimers(f, name, dna, startSize, midSize, endSize);
+    verboseDot();
     }
+verbose(1, "\n");
 }
 
 int main(int argc, char *argv[])
@@ -140,6 +253,9 @@ int main(int argc, char *argv[])
 optionInit(&argc, argv, options);
 if (argc != 3)
     usage();
+exe = optionVal("exe", exe);
+minTm = optionFloat("minTm", minTm);
+maxTm = optionFloat("maxTm", maxTm);
 regionToPrimer3(argv[1], argv[2]);
 return 0;
 }
