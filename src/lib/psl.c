6ee56c5f13041a3dfef55b359de065863fbd5f84
baertsch <baertsch>
2004-09-08 22:02:30 +0000

    improved error message

--- src/lib/psl.c
+++ src/lib/psl.c
@@ -18,7 +18,7 @@
 #include "aliType.h"
 #include "binRange.h"
 
-static char const rcsid[] = "$Id: psl.c,v 1.50 2004/09/01 05:14:46 markd Exp $";
+static char const rcsid[] = "$Id: psl.c,v 1.51 2004/09/08 22:02:30 baertsch Exp $";
 
 static char *createString = 
 "CREATE TABLE %s (\n"
@@ -616,7 +616,7 @@ if (wordCount == 23)
     }
 else
     {
-    errAbort("Bad line %d of %s", lf->lineIx, lf->fileName);
+    errAbort(stderr,"Bad line %d of %s wordCount is %d instead of 21 or 23\n", lf->lineIx, lf->fileName, wordCount);
     return NULL;
     }
 }
