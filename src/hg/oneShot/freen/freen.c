23489407d7602f073f777f099356d58f229b6a44
kent <kent>
2004-10-11 18:17:14 +0000

    Testing sqlQuickNum

--- src/hg/oneShot/freen/freen.c
+++ src/hg/oneShot/freen/freen.c
@@ -4,9 +4,9 @@
 #include "linefile.h"
 #include "hash.h"
 #include "options.h"
-#include "pipeline.h"
+#include "jksql.h"
 
-static char const rcsid[] = "$Id: freen.c,v 1.48 2004/09/24 15:20:32 kent Exp $";
+static char const rcsid[] = "$Id: freen.c,v 1.49 2004/10/11 18:17:14 kent Exp $";
 
 void usage()
 /* Print usage and exit. */
@@ -14,37 +14,12 @@ void usage()
 errAbort("usage: freen something");
 }
 
-struct pipeline *gzipPipe = NULL;
-
-void gzipPipeOpen()
-/* Redirect stdout to go through a pipe to gzip. */
-{
-static char *gzip[] = {"gzip", "-c", "-f", NULL};
-gzipPipe = pipelineCreateWrite1(gzip, pipelineInheritFd, NULL);
-if (dup2(pipelineFd(gzipPipe), 1) < 0)
-   errnoAbort("dup2 to stdout failed");
-}
-
-
-void gzipPipeClose()
-/* Finish up gzip pipeline. */
-{
-if (gzipPipe != NULL)
-    {
-    fclose(stdout);  /* must close first or pipe will hang */
-    pipelineWait(&gzipPipe);
-    }
-}
-
-   
-
 void freen(char *a)
 /* Test some hair-brained thing. */
 {
-gzipPipeOpen();
-uglyf("This is a message from the world famous freen program!\n");
-uglyf("Will it compress, who knows?\n");
-gzipPipeClose();
+struct sqlConnection *conn = sqlConnect("hg17");
+int count = sqlQuickNum(conn, "select size from chromInfo where chrom='chr_XXX'");
+printf("Count = %d\n", count);;
 }
 
 
