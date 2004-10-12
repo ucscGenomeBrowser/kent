73923536adb8da20c15f881c430c0044ea821a0c
fanhsu <fanhsu>
2004-10-12 21:42:49 +0000

    Back out of the "../cgi-bin/..." change.

--- src/hg/protein/pbGateway/pbGateway.c
+++ src/hg/protein/pbGateway/pbGateway.c
@@ -64,7 +64,7 @@ if (sameString(position, "proteome") || sameString(position, "hgBatch"))
     position = defaultPosition;
 
 puts(
-"<FORM ACTION=\"../cgi-bin/pbGlobal\" NAME=\"mainForm\" METHOD=\"GET\">\n"
+"<FORM ACTION=\"/cgi-bin/pbGlobal\" NAME=\"mainForm\" METHOD=\"GET\">\n"
 "<CENTER>"
 "<TABLE BGCOLOR=\"FFFEF3\" BORDERCOLOR=\"cccc99\" BORDER=0 CELLPADDING=1>\n"
 "<TR><TD><FONT SIZE=\"2\">\n"
@@ -85,7 +85,7 @@ puts("<INPUT TYPE=SUBMIT>");
 puts("</FORM>\n"
 );
 
-printf("<FORM ACTION=\"../cgi-bin/pbGateway\" METHOD=\"GET\" NAME=\"orgForm\"><input type=\"hidden\" name=\"org\" value=\"%s\">\n", organism);
+printf("<FORM ACTION=\"/cgi-bin/pbGateway\" METHOD=\"GET\" NAME=\"orgForm\"><input type=\"hidden\" name=\"org\" value=\"%s\">\n", organism);
 printf("<input type=\"hidden\" name=\"db\" value=\"%s\">\n", db);
 cartSaveSession(cart);
 puts("</FORM><BR>");
