492aee7c70faf3cfad037b086eb5998a708fd86a
angie <angie>
2004-10-19 20:17:26 +0000

    Refactored to process 2bit file in one pass instead of opening and closing the file once per sequence (which can mean hundreds of thousands of times for small scaffold assemblies).  Also fixed a bug that caused nothing to be output for sequences smaller than windowSize.

--- src/hg/makeDb/hgGcPercent/hgGcPercent.c
+++ src/hg/makeDb/hgGcPercent/hgGcPercent.c
@@ -9,17 +9,16 @@
 #include "options.h"
 #include "twoBit.h"
 
-static char const rcsid[] = "$Id: hgGcPercent.c,v 1.13 2004/10/15 23:02:00 heather Exp $";
+static char const rcsid[] = "$Id: hgGcPercent.c,v 1.14 2004/10/19 20:17:26 angie Exp $";
 
 /* Command line switches. */
-int winSize = 20000;               /* window size */
+int winSize = 20000;            /* window size */
 boolean noLoad = FALSE;		/* Suppress loading mysql table . */
 char *file = (char *)NULL;	/* file name for output */
 char *chr = (char *)NULL;	/* process only chromosome listed */
-boolean noDots = FALSE;	/* TRUE == do not display ... progress */
-boolean doGaps = FALSE;	/* TRUE == process gaps correctly */
+boolean noDots = FALSE;	        /* TRUE == do not display ... progress */
+boolean doGaps = FALSE;	        /* TRUE == process gaps correctly */
 int overlap = 0;                /* overlap size */
-boolean useTwoBit = TRUE;  /* TRUE == use 2bit file; if not found use 4bit nibs */
 
 /* command line option specifications */
 static struct optionSpec optionSpecs[] = {
@@ -30,7 +29,6 @@ static struct optionSpec optionSpecs[] = {
     {"noDots", OPTION_BOOLEAN},
     {"doGaps", OPTION_BOOLEAN},
     {"overlap", OPTION_INT},
-    {"useTwoBit", OPTION_BOOLEAN},
     {NULL, 0}
 };
 
@@ -42,16 +40,18 @@ errAbort(
   "hgGcPercent - Calculate GC Percentage in 20kb windows\n"
   "usage:\n"
   "   hgGcPercent [options] database nibDir\n"
+  "     If nibDir/database.2bit file exists, uses that; otherwise uses \n"
+  "     nibDir/*.nib.  Loads gcPercent table with counts from sequence.\n"
   "options:\n"
-  "   -win=<size> - change windows size (default 20000)\n"
+  "   -win=<size> - change windows size (default %d)\n"
   "   -noLoad - do not load mysql table - create bed file\n"
   "   -file=<filename> - output to <filename> (stdout OK) (implies -noLoad)\n"
   "   -chr=<chrN> - process only chrN from the nibDir\n"
   "   -noDots - do not display ... progress during processing\n"
   "   -doGaps - process gaps correctly (default: gaps are not counted as GC)\n"
   "   -overlap=N - overlap windows by N bases (default 0)\n"
-  "   -useTwoBit - use 2bit sequence (if not found, use 4bit sequence)\n"
-  "   -verbose=N - display details to stderr during processing");
+  "   -verbose=N - display details to stderr during processing",
+  winSize);
 }
 
 char *createTable = 
@@ -67,183 +67,170 @@ char *createTable =
     "INDEX(chrom(12), chromEnd)\n"
 ");\n";
 
-void makeGcTab(char *nibFile, char *chrom, int chromSize, FILE *f)
-/* Scan through nib file and write out GC percentage info in
- * 20 kb windows. */
+
+void makeGcLineFromSeq(struct dnaSeq *seq, char *chrom, int start, int end,
+		       FILE *f)
+/* Given a window of sequence, print out a line of BED 5 with the GC 
+ * parts per thousand (not percent) in the window. */
 {
-int start = 0, end, oneSize;
+static int dotMod = 0;
+DNA *dna = seq->dna;
 int minCount = winSize/4;
 int i, count, gcCount, val, ppt, gapCount;
-struct dnaSeq *seq = NULL;
-FILE *nf = NULL;
-struct twoBitFile *tbf;
-DNA *dna;
-int dotMod = 0;
-boolean twoBit = FALSE;
 
-printf("#	Calculating gcPercent with window size %d\n",winSize);
-
-if (twoBitIsFile(nibFile))
+if ((++dotMod&127) == 0)
     {
-    twoBit = TRUE;
+    if (!noDots)
+	{
+	verbose(1, ".");
+	if ((dotMod & 8191) == 0)
+	    verbose(1, "\n");
+	fflush(stdout);
+	}
     }
 
-if (twoBit)
+gapCount = count = gcCount = 0;
+for (i=0; i < seq->size; ++i)
     {
-    tbf = twoBitOpen(nibFile);
+    if ((val = ntVal[dna[i]]) >= 0)
+	{
+	++count;
+	if (val == G_BASE_VAL || val == C_BASE_VAL)
+	    {
+	    ++gcCount;
+	    }
+	}
+    else
+	{
+	++gapCount;
+	}
+    }
+if (count >= minCount)
+    ppt = round(1000.0*(double)gcCount/(double)count);
+else
+    ppt = 0;
+
+if (doGaps)
+    {
+    if (gapCount < seq->size)
+	fprintf(f, "%s\t%d\t%d\t%s\t%d\n", chrom, start, end, "GC", ppt);
     }
 else
     {
-    nibOpenVerify(nibFile, &nf, &chromSize);
+    fprintf(f, "%s\t%d\t%d\t%s\t%d\n", chrom, start, end, "GC", ppt);
     }
+}
 
-end = start + winSize;
-for (start=0; start<chromSize && end < chromSize; start =  end - overlap)
+
+void makeGcTabFromNib(char *nibFile, char *chrom, FILE *f)
+/* Scan through nib file and write out GC percentage info. */
+{
+int start = 0, end = 0;
+int chromSize;
+FILE *nf = NULL;
+
+nibOpenVerify(nibFile, &nf, &chromSize);
+
+for (start=0, end=0;  start < chromSize && end < chromSize;  
+     start = end - overlap)
     {
-    if ((++dotMod&127) == 0)
-	{
-	if (!noDots)
-	    {
-	    printf(".");
-	    fflush(stdout);
-	    }
-	}
+    struct dnaSeq *seq = NULL;
     end = start + winSize;
     if (end > chromSize)
         end = chromSize;
-    oneSize = end - start;
-    if (twoBit) 
-        {
-	seq = twoBitReadSeqFrag(tbf, chrom, start, end);
-	}
-    else 
-        {
-        seq = nibLdPart(nibFile, nf, chromSize, start, oneSize);
-	}
-    dna = seq->dna;
-    gapCount = count = gcCount = 0;
-    for (i=0; i<oneSize; ++i)
-        {
-	if ((val = ntVal[dna[i]]) >= 0)
-	    {
-	    ++count;
-	    if (val == G_BASE_VAL || val == C_BASE_VAL)
-		{
-	        ++gcCount;
-		}
-	    } else {
-		++gapCount;
-	    }
-	}
+    seq = nibLdPart(nibFile, nf, chromSize, start, end - start);
+    makeGcLineFromSeq(seq, chrom, start, end, f);
     freeDnaSeq(&seq);
-    if (count >= minCount)
-	ppt = round(1000.0*(double)gcCount/(double)count);
-    else
-        ppt = 0;
-    /*	Recogize windows full of N's, do no output.
-     */
-    if (doGaps)
-	{
-	if (gapCount < oneSize)
-	    fprintf(f, "%s\t%d\t%d\t%s\t%d\n", chrom, start, end, "GC", ppt);
-	}
-    else
-        {
-	fprintf(f, "%s\t%d\t%d\t%s\t%d\n", chrom, start, end, "GC", ppt);
-	}
     }
-if (twoBit)
-    {
-    twoBitClose(&tbf);
-    }
-else
-    {
-    carefulClose(&nf);
-    }
-printf("\n");
+carefulClose(&nf);
+if (!noDots)
+    verbose(1, "\n");
 }
 
-void hgGcPercent(char *database, char *nibDir)
-/* hgGcPercent - Calculate GC Percentage in 20kb windows on all chromosomes. */
+
+void makeGcTabFromTwoBit(char *twoBitFileName, FILE *f)
+/* Scan through all sequenes in .2bit file and write out GC percentage info. */
 {
-struct fileInfo *nibList, *nibEl;
-char dir[256], chrom[128], ext[64];
-char *tabFileName = "gcPercent.bed";
-FILE *tabFile = (FILE *) NULL;
-struct sqlConnection *conn;
-char query[256];
-struct sqlResult *sr;
-char **row;
-char twoBitFile[256];
-struct twoBitFile *tbf;
-struct slName *twoBitNames, *el;
-int size = 0;
+struct slName *twoBitNames = twoBitSeqNames(twoBitFileName);
+struct slName *el = NULL;
+struct twoBitFile *tbf = twoBitOpen(twoBitFileName);
 
-// output file
-if (file)
-    tabFileName = file;
-
-tabFile = mustOpen(tabFileName, "w");
-
-verbose(2, "writing to file %s\n", tabFileName);
-
-sprintf(twoBitFile, "%s/%s.2bit", nibDir, database);
-if (fileExists(twoBitFile) && useTwoBit)
+for (el = twoBitNames; el != NULL; el = el->next)
     {
-    printf("Using twoBit: %s\n", twoBitFile);
-    tbf = twoBitOpen(twoBitFile);
-    twoBitNames = twoBitSeqNames(twoBitFile);
-    for (el = twoBitNames; el != NULL; el = el->next)
-        {
-            printf("Processing twoBit %s\n", el->name);
-	    size = twoBitSeqSize(tbf, el->name);
-            makeGcTab(twoBitFile, el->name, size, tabFile);
+    int start = 0, end = 0;
+    int chromSize = twoBitSeqSize(tbf, el->name);
+    char *chrom = el->name;
+    verbose(2, "Processing twoBit sequence %s\n", chrom);
+    for (start=0, end=0;  start < chromSize && end < chromSize;  
+	 start = end - overlap)
+	{
+	struct dnaSeq *seq = NULL;
+	end = start + winSize;
+	if (end > chromSize)
+	    end = chromSize;
+	seq = twoBitReadSeqFrag(tbf, chrom, start, end);
+	makeGcLineFromSeq(seq, chrom, start, end, f);
+	freeDnaSeq(&seq);
 	}
     }
-  
+if (!noDots)
+    verbose(1, "\n");
+}
+
+
+void hgGcPercent(char *database, char *nibDir)
+/* hgGcPercent - Calculate GC Percentage on all chromosomes, load db table. */
+{
+char *tabFileName = file ? file : "gcPercent.bed";
+FILE *tabFile = mustOpen(tabFileName, "w");
+char twoBitFile[512];
+
+verbose(1, "Calculating gcPercent with window size %d\n", winSize);
+verbose(2, "Writing to tab file %s\n", tabFileName);
+
+sprintf(twoBitFile, "%s/%s.2bit", nibDir, database);
+if (fileExists(twoBitFile))
+    {
+    verbose(1, "Using twoBit: %s\n", twoBitFile);
+    makeGcTabFromTwoBit(twoBitFile, tabFile);
+    }  
 else
     {
+    struct fileInfo *nibList, *nibEl;
     nibList = listDirX(nibDir, "*.nib", TRUE);
-
-    /* Create tab file with all GC percent data. */
     for (nibEl = nibList; nibEl != NULL; nibEl = nibEl->next)
         {
+	char dir[256], chrom[128], ext[64];
         splitPath(nibEl->name, dir, chrom, ext);
         if (chr)
 	    {
-	    char chrNib[256];
-	    safef(chrNib, ArraySize(chrNib), "%s/%s.nib", nibDir, chr);
-	    verbose(2, "checking name: chrNib %s =? %s nibEl->name\n", chrNib, nibEl->name);
-	    if (sameString(chrNib,nibEl->name))
-	        {
-	        printf("Processing %s\n", nibEl->name);
-	        makeGcTab(nibEl->name, chrom, 0, tabFile);
-	        }
+	    verbose(2, "checking name: %s =? %s\n", chrom, chr);
+	    if (! sameString(chrom, chr))
+		continue;
 	    }
-        else
-	    {
-                printf("Processing %s\n", nibEl->name);
-                makeGcTab(nibEl->name, chrom, 0, tabFile);
-	    }
+	verbose(1, "Processing %s\n", nibEl->name);
+	makeGcTabFromNib(nibEl->name, chrom, tabFile);
         }
     slFreeList(&nibList);
     }
 carefulClose(&tabFile);
-printf("File %s created\n",tabFileName);
+verbose(1, "File %s created\n", tabFileName);
 
 /* Load that file in database. */
 if (!noLoad)
     {
-    conn = sqlConnect(database);
-    printf("Loading gcPercent table\n");
+    struct sqlConnection *conn = sqlConnect(database);
+    char query[1024];
+    verbose(1, "Loading gcPercent table\n");
     sqlMaybeMakeTable(conn, "gcPercent", createTable);
     sqlUpdate(conn, "DELETE from gcPercent");
-    sprintf(query, "LOAD data local infile '%s' into table gcPercent", tabFileName);
+    safef(query, sizeof(query),
+	  "LOAD data local infile '%s' into table gcPercent", tabFileName);
     sqlUpdate(conn, query);
     sqlDisconnect(&conn);
     }
-
 }
+
 
 int main(int argc, char *argv[])
 /* Process command line. */
