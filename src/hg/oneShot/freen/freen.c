4d55de2fb9d52fbc10702e6b47cfb2b60c02feb6
kent <kent>
2004-10-23 15:08:00 +0000

    Testing twoBitOpen timing

--- src/hg/oneShot/freen/freen.c
+++ src/hg/oneShot/freen/freen.c
@@ -4,9 +4,11 @@
 #include "linefile.h"
 #include "hash.h"
 #include "options.h"
-#include "jksql.h"
+#include "dnaseq.h"
+#include "twoBit.h"
+#include "portable.h"
 
-static char const rcsid[] = "$Id: freen.c,v 1.50 2004/10/12 14:19:52 kent Exp $";
+static char const rcsid[] = "$Id: freen.c,v 1.51 2004/10/23 15:08:00 kent Exp $";
 
 void usage()
 /* Print usage and exit. */
@@ -17,9 +19,16 @@ errAbort("usage: freen something");
 void freen(char *a)
 /* Test some hair-brained thing. */
 {
-struct sqlConnection *conn = sqlConnect("hg17");
-int count = sqlNeedQuickNum(conn, "select max(size) from chromInfo where chrom like 'chr%_random%'");
-printf("Count = %d\n", count);;
+long start = clock1000();
+struct twoBitFile *tbf = twoBitOpen(a);
+long openTime = clock1000();
+struct dnaSeq *seq = twoBitReadSeqFrag(tbf, "scaffold_1000", 0, 0);
+long seqTime = clock1000();
+twoBitClose(&tbf);
+long end = clock1000();
+printf("Took %ld millis to twoBitOpen %s\n", openTime - start, a);
+printf("Took %ld millis to load %s (%d bases)\n", seqTime - openTime, seq->name, seq->size);
+printf("Took %ld millis to twoBitClose %s\n", end - seqTime, a);
 }
 
 
