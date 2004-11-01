307dad24d9b15b6882806d73ec61eb02c8b81e4a
hiram <hiram>
2004-11-01 19:31:38 +0000

    Add example to the usage message

--- src/hg/makeDb/hgLoadWiggle/wigEncode.c
+++ src/hg/makeDb/hgLoadWiggle/wigEncode.c
@@ -1,6 +1,6 @@
 /* wigEncode - Convert wiggle ascii to wiggle binary format */
 
-static char const rcsid[] = "$Id: wigEncode.c,v 1.1 2004/11/01 18:59:46 hiram Exp $";
+static char const rcsid[] = "$Id: wigEncode.c,v 1.2 2004/11/01 19:31:38 hiram Exp $";
 
 #include "common.h"
 #include "wiggle.h"
@@ -10,12 +10,16 @@ void usage()
 {
 errAbort("wigEncode - convert Wiggle ascii data to binary format\n\n"
         "usage:\n"
-        "    wigEncode bedFile wigFile wibFile\n"
+        "    wigEncode wigInput wigFile wibFile\n"
 	"\n"
 	"This processes the three data input format types described at:\n"
 	"\thttp://genome.ucsc.edu/encode/submission.html#WIG\n"
 	"*without* the track type= lines\n"
-	"\t(future improvements will tolerate the track lines)");
+	"\t(future improvements will tolerate the track lines)\n"
+	"Example:\n"
+	"    hgGcPercent -wigOut -doGaps -file=stdout -win=5 xenTro1 \\\n"
+	"        /cluster/data/xenTro1 | "
+	"wigEncode stdin gc5Base.wig gc5Base.wib");
 }
 
 void wigEncode(char *bedFile, char *wigFile, char *wibFile)
