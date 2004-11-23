78e8011365c24457aff758aa368dbdc0af71ea56
hiram <hiram>
2004-11-23 19:17:09 +0000

    Now with a bed9 RGB test

--- src/hg/makeDb/hgLoadBed/tests/RunTest.sh
+++ src/hg/makeDb/hgLoadBed/tests/RunTest.sh
@@ -4,6 +4,17 @@ TESTPROGRAM=hgLoadBed
 tests=0
 failures=0
 verbose=""
+NOLOAD="-noLoad"
+export TESTPROGRAM verbose tests failures NOLOAD
+
+usage() {
+    echo "usage: `basename $0` [-verbose] [-load] [-home] [-help]"
+    echo -e "\t-verbose - show all test command lines and results"
+    echo -e "\t-load - actually load table 'bedLoadTest' into database 'test'"
+    echo -e "\t-home - use PATH $HOME/bin/$MACHTYPE/$TESTPROGRAM"
+    echo -e "\t-help - print this usage message"
+    exit 255
+}
 
 oneTest() {
     tests=`expr $tests + 1`
@@ -30,8 +41,12 @@ export failedArgCheck
 
 while [ "$#" -gt 0 ]
 do
-    if [ "$1" = "-verbose" ]; then
+    if [ "$1" = "-help" ]; then
+	usage
+    elif [ "$1" = "-verbose" ]; then
 	verbose="verbose"
+    elif [ "$1" = "-load" ]; then
+	NOLOAD=""
     elif [ "$1" = "-home" ]; then
 	TESTPROGRAM="$HOME/bin/$MACHTYPE/$TESTPROGRAM"
     else
@@ -41,7 +56,7 @@ do
     shift
 done
 
-export TESTPROGRAM verbose tests failures
+export TESTPROGRAM verbose tests failures NOLOAD
 
 if [ "$failedArgCheck" -eq 1 ]; then
     exit 255
@@ -57,9 +72,10 @@ fi
 
 rm -f bed.tab
 
-oneTest "zcat bed4.txt.gz | $TESTPROGRAM -noLoad test test stdin" "29784     1"
-oneTest "zcat bed6.txt.gz | $TESTPROGRAM -noLoad test test stdin" "01708     1"
-oneTest "zcat bed14.txt.gz | $TESTPROGRAM -noLoad test test stdin" "00496     2"
+oneTest "zcat bed4.txt.gz | $TESTPROGRAM ${NOLOAD} test bedLoadTest stdin" "29784     1"
+oneTest "zcat bed6.txt.gz | $TESTPROGRAM ${NOLOAD} test bedLoadTest stdin" "01708     1"
+oneTest "zcat bed14.txt.gz | $TESTPROGRAM ${NOLOAD} test bedLoadTest stdin" "00496     2"
+oneTest "zcat bed9.txt.gz | $TESTPROGRAM ${NOLOAD} test bedLoadTest stdin" "27920     2"
 
 if [ -n "${verbose}" ]; then
     C=`echo $tests | awk '{printf "%4d", $1}'`
