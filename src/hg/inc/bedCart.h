c30383359e370fa84aa16c8516afd209bd1221e3
hiram <hiram>
2004-11-24 19:40:29 +0000

    Add USE_ITEM_RGB signal mechanism

--- src/hg/inc/bedCart.h
+++ src/hg/inc/bedCart.h
@@ -3,6 +3,9 @@
 #ifndef BEDCART_H
 #define BEDCART_H
 
+/*	Signature to place in lf->extra to trigger coloring	*/
+#define	USE_ITEM_RGB	0x9876abcd
+
 /*	Options to be found in trackDb settings	*/
 #define OPT_ITEM_RGB "itemRgb"
 /* itemRgb when not present is default Off, or when present can be Off or On */
