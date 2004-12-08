3703577a402a30c65359509ec95fa1ab47ed529e
markd <markd>
2004-12-08 06:51:03 +0000

    added code to load rankprop and psiblast gene sorter tables

--- src/hg/protein/spLoadRankProp/spLoadRankProp.c
+++ src/hg/protein/spLoadRankProp/spLoadRankProp.c
@@ -1,17 +1,15 @@
-/* spLoadRankProp - rankProp table */
+/* spLoadRankProp - load rankProp table */
 #include "common.h"
 #include "options.h"
 #include "jksql.h"
+#include "spKgMap.h"
 #include "rankProp.h"
 #include "rankPropProt.h"
-#include "kgXref.h"
-#include "localmem.h"
-#include "hash.h"
 #include "linefile.h"
 #include "hgRelate.h"
 #include "verbose.h"
 
-static char const rcsid[] = "$Id: spLoadRankProp.c,v 1.3 2004/10/05 08:04:27 markd Exp $";
+static char const rcsid[] = "$Id: spLoadRankProp.c,v 1.4 2004/12/08 06:51:03 markd Exp $";
 
 /* command line option specifications */
 static struct optionSpec optionSpecs[] = {
@@ -27,11 +25,7 @@ boolean gKeep = FALSE;    /* keep tab file */
 boolean gLoad = TRUE;     /* load databases */
 char *gNoKgIdFile = NULL; /* output dropped sp ids to this file */
 
-struct hash *gProtToKgMap = NULL;  /* has of protein id to kg id. Also
-                                    * contains entries with a NULL value for
-                                    * proteins not found in kgProtMap so these
-                                    * can be counted. There maybe multiple
-                                    * entries for a given swissprot id. */
+struct spKgMap *gSpKgMap = NULL;  /* has of swissprot id to kg id. */
 
 void usage()
 /* Explain usage and exit. */
@@ -60,73 +54,9 @@ char createString[] =
     "    qKgId varchar(255) not null,	# known genes id of query protein\n"
     "    tKgId varchar(255) not null,	# known genes id of target protein\n"
     "    score float not null,	# rankp score\n"
-    "    qtEVal double not null,	# query to target psi-blast E-value\n"
-    "    tqEVal double not null,	# target to query psi-blast E-value\n"
     "              #Indices\n"
     "    INDEX(qKgId(12))\n"
     ");\n";
-
-void buildProtToKgMap(struct sqlConnection *conn)
-/* build gProtToKgMap from kgXRef table */
-{
-char query[1024];
-struct kgXref kgXref;
-struct sqlResult *sr;
-char *kgId;
-char **row;
-
-gProtToKgMap = hashNew(21);
-safef(query, sizeof(query), "SELECT * from kgXref");
-sr = sqlGetResult(conn, query);
-while ((row = sqlNextRow(sr)) != NULL)
-    {
-    kgXrefStaticLoad(row, &kgXref);
-    kgId = lmCloneString(gProtToKgMap->lm, kgXref.kgID);
-    hashAdd(gProtToKgMap, kgXref.spID, kgId);
-    }
-sqlFreeResult(&sr);
-}
-
-char *getKgId(char *spId)
-/* get the kgId for a swissprot.  If not found, enter in the
- * table for later counting */
-{
-struct hashEl *hel = hashStore(gProtToKgMap, spId);
-if (hel->val == NULL)
-    return NULL;
-else
-    return hel->val;
-}
-
-int countKgMissing()
-/* count the number kgIds that were not found */
-{
-int missing = 0;
-struct hashCookie cookie = hashFirst(gProtToKgMap);
-struct hashEl *hel;
-
-while ((hel = hashNext(&cookie)) != NULL)
-    {
-    if (hel->val == NULL)
-        missing++;
-    }
-return missing;
-}
-
-void listKgMissing(char *file)
-/* output the spIds for the kgIds that were not found */
-{
-FILE *fh = mustOpen(file, "w");
-struct hashCookie cookie = hashFirst(gProtToKgMap);
-struct hashEl *hel;
-
-while ((hel = hashNext(&cookie)) != NULL)
-    {
-    if (hel->val == NULL)
-        fprintf(fh, "%s\n", hel->name);
-    }
-carefulClose(&fh);
-}
 
 enum rowStat
 /* reason a row was skipped */
@@ -137,23 +67,21 @@ enum rowStat
 };
 
 void outputEntry(struct rankPropProt *inRec, FILE *tabFh,
-                 struct hashEl *qKgHel, struct hashEl *tKgHel)
+                 struct slName *qKgList, struct slName *tKgList)
 /* output entry for all combinations of kgIDs corrisponding to the swissprot
  * pair */
 {
 struct rankProp outRec;
-struct hashEl *qKgScan,  *tKgScan;
+struct slName *qKg,  *tKg;
 
 outRec.score = inRec->score;
-outRec.qtEVal = inRec->qtEVal;
-outRec.tqEVal = inRec->tqEVal;
 
-for (qKgScan = qKgHel; qKgScan != NULL; qKgScan = hashLookupNext(qKgScan))
+for (qKg = qKgList; qKg != NULL; qKg = qKg->next)
     {
-    outRec.qKgId = qKgScan->val;
-    for (tKgScan = tKgHel; tKgScan != NULL; tKgScan = hashLookupNext(tKgScan))
+    outRec.qKgId = qKg->name;
+    for (tKg = tKgList; tKg != NULL; tKg = tKg->next)
         {
-        outRec.tKgId = tKgScan->val;
+        outRec.tKgId = tKg->name;
         rankPropTabOut(&outRec, tabFh);
         }
     }
@@ -165,7 +93,7 @@ enum rowStat processRow(char **row, FILE *tabFh)
 {
 struct rankPropProt inRec;
 struct rankProp outRec;
-struct hashEl *qKgHel, *tKgHel;
+struct slName *qKgList, *tKgList;
 rankPropProtStaticLoad(row, &inRec);
 
 if ((inRec.score == 0.0) && (inRec.qtEVal < 0) && (inRec.tqEVal < 0))
@@ -176,21 +104,19 @@ if ((inRec.score == 0.0) && (inRec.qtEVal < 0) && (inRec.tqEVal < 0))
     }
 
 /* since a swissprot can be associated with multiple kgIds, need to
- * loop over all combinations.  Use hashStore so NULL entries are added
- * for ids that are not found. */
-
-qKgHel = hashStore(gProtToKgMap, inRec.qSpId);
-tKgHel = hashStore(gProtToKgMap, inRec.tSpId);
-if ((qKgHel->val == NULL) || (tKgHel == NULL))
+ * loop over all combinations. */
+qKgList = spKgMapGet(gSpKgMap, inRec.qSpId);
+tKgList = spKgMapGet(gSpKgMap, inRec.tSpId);
+if ((qKgList == NULL) || (tKgList == NULL))
     {
     verbose(2, "can't find kgXref id for %s, skipping %s <=> %s\n",
-            ((qKgHel->val == NULL) ? inRec.qSpId : inRec.tSpId),
+            ((qKgList == NULL) ? inRec.qSpId : inRec.tSpId),
             inRec.qSpId, inRec.tSpId);
     return rowNoKgXref;
     }
 else
     {
-    outputEntry(&inRec, tabFh, qKgHel, tKgHel);
+    outputEntry(&inRec, tabFh, qKgList, tKgList);
     return rowKeep;
     }
 }
@@ -217,13 +143,14 @@ lineFileClose(&inLf);
 if (statCnts[rowNoKgXref] > 0)
     {
     verbose(1, "skipped %d pairs due to not being in kgXref \n", statCnts[rowNoKgXref]);
-    verbose(1, "%d proteins could not be mapped\n", countKgMissing());
+    verbose(1, "%d proteins could not be mapped\n", 
+            spKgMapCountMissing(gSpKgMap));
     }
 if (statCnts[rowSingleton] > 0)
     verbose(1, "skipped %d entries due to no score and no edges\n", statCnts[rowSingleton]);
 verbose(1, "kept %d pairs\n", statCnts[rowKeep]);
 if (gNoKgIdFile != NULL)
-    listKgMissing(gNoKgIdFile);
+    spKgMapListMissing(gSpKgMap, gNoKgIdFile);
 
 if (gLoad)
     hgLoadTabFileOpts(conn, ".", table, SQL_TAB_FILE_ON_SERVER, &tabFh);
@@ -237,7 +164,7 @@ void spLoadRankProp(char *database, char *table, char *rankPropFile)
 char query[1024];
 struct sqlConnection *conn = sqlConnect(database);
 
-buildProtToKgMap(conn);
+gSpKgMap = spKgMapNew(conn);
 
 if (gLoad)
     {
