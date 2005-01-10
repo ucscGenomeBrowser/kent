8ce1d0cdfa0e61fd676524f0b072722cb22bad02
kent <kent>
2005-01-10 00:41:42 +0000

    Spreadsheet of the day

--- src/hg/oneShot/labSheet/labSheet.c
+++ src/hg/oneShot/labSheet/labSheet.c
@@ -4,7 +4,7 @@
 #include "hash.h"
 #include "options.h"
 
-static char const rcsid[] = "$Id: labSheet.c,v 1.2 2004/09/24 15:23:47 kent Exp $";
+static char const rcsid[] = "$Id: labSheet.c,v 1.3 2005/01/10 00:41:42 kent Exp $";
 
 void usage()
 /* Explain usage and exit. */
@@ -25,67 +25,69 @@ static struct optionSpec options[] = {
 void labSheet(char *outFile)
 /* labSheet - Make a tab-separated file for turning into a lab spreadsheet.. */
 {
-int row, col;
+int row;
 FILE *f = mustOpen(outFile, "w");
-static char *templates[] = {
-   "uc462",
-   "uc465",
-   "uc467",
-   "uc468+9",
-   "uc326+7",
-   "uc275",
-   "uc270+1",
-   "uc404",
-   "uc189",
-   };
 static char *fPrimers[] = {
-   "B04-uc462r",
-   "B06-uc465r",
-   "B07-uc467r",
-   "B08-uc468+9r",
-   "B12-uc326+7r",
-   "D02-uc275r",
-   "D04-uc270+1r",
-   "D07-uc404r",
-   "D08-uc189r",
+   "fJunk.1fwdA",
+   "fNogDown.1fwdA",
+   "uxf.357fwdA",
+   "uxf.482fwdA",
+   "uxf.336fwdA",
+   "uxf.355fwdA",
+   "uxf.123fwdA",
+   "uxf.124fwdA",
+   "uxf.125fwdA",
+   "uxf.126fwdA",
+   "uxf.322fwdA",
+   "uxf.323fwdA",
+   };
+static char *rPrimers[] = {
+   "fJunk.1revA",
+   "fNogDown.1revA",
+   "uxf.357revA",
+   "uxf.482revA",
+   "uxf.336revA",
+   "uxf.355revA",
+   "uxf.123revA",
+   "uxf.124revA",
+   "uxf.125revA",
+   "uxf.126revA",
+   "uxf.322revA",
+   "uxf.323revA",
    };
 static int sizes[] = {
-    897,
-    609,
-    1190,
-    963,
-    1199,
-    525,
-    825,
-    736,
-    680,
+   1568,
+   322,
+   326,
+   451,
+   409,
+   428,
+   526,
+   344,
+   345,
+   422,
+   229,
+   397,
    };
 
-fprintf(f, "Sample\t");
-fprintf(f, "Template\t");
-fprintf(f, "Size\t");
-fprintf(f, "Colony H20\t");
 fprintf(f, "fPrimer 20 uM 0.2ul\t");
 fprintf(f, "rPrimer 50 ng/ul 1ul\t");
+fprintf(f, "Template\t");
 fprintf(f, "10x Buffer\t");
 fprintf(f, "10mM dNTPs\t");
 fprintf(f, "H20\t");
-fprintf(f, "Ultra PFU\n");
-for (col=0; col < 9; ++col)
+fprintf(f, "Ultra PFU\t");
+fprintf(f, "Size\n");
+for (row=0; row<12; ++row)
     {
-    for (row = 'A'; row <= 'H'; ++row)
-        {
-	fprintf(f, "%c%d\t", row, col+1);
-	fprintf(f, "%s\t", templates[col]);
-	fprintf(f, "%d\t", sizes[col]);
-	fprintf(f, "5\t");
-	fprintf(f, "%s\t", fPrimers[col]);
-	fprintf(f, "M13 rev\t");
-	fprintf(f, "2\t");
-	fprintf(f, "0.4\t");
-	fprintf(f, "11.2\t");
-	fprintf(f, "0.2\n");
-	}
+    fprintf(f, "%s\t", fPrimers[row]);
+    fprintf(f, "%s\t", rPrimers[row]);
+    fprintf(f, "0.8 10ng/ul\t");
+    fprintf(f, "4\t");
+    fprintf(f, "0.8\t");
+    fprintf(f, "26\t");
+    fprintf(f, "0.4\t");
+    fprintf(f, "%d\n", sizes[row]);
     }
 carefulClose(&f);
 }
