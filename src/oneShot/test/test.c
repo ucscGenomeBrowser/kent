cd44c5ff7f3d892c0f75fea7bfa1ab5db4626785
kent <kent>
2005-01-10 00:43:51 +0000

    Testing poly A trimmer

--- src/oneShot/test/test.c
+++ src/oneShot/test/test.c
@@ -3,8 +3,7 @@
 #include "linefile.h"
 #include "hash.h"
 #include "options.h"
-#include "memalloc.h"
-#include "rbTree.h"
+#include "dnautil.h"
 
 void usage()
 /* Explain usage and exit. */
@@ -18,38 +17,21 @@ errAbort(
 }
 
 
-void dumpString(void *item, FILE *f)
-{
-fprintf(f, "%s", (char *)item);
-}
-
-void test(char *in, char *out)
+void test(char *in)
 /* test - Test something. */
 {
-struct lineFile *lf = lineFileOpen(in, TRUE);
-FILE *f = mustOpen(out, "w");
-char *line, *word;
-struct rbTree *rb = rbTreeNew(rbTreeCmpString);
-struct slRef *list, *ref;
-while (lineFileNext(lf, &line, NULL))
-    {
-    while ((word = nextWord(&line)) != NULL)
-        {
-	word = cloneString(word);
-	rbTreeAdd(rb, word);
-	}
-    }
-rbTreeDump(rb, f, dumpString);
-list = rbTreeItems(rb);
-for (ref = list; ref != NULL; ref = ref->next)
-    printf("%s\n", (char *)(ref->val));
+int len = strlen(in);
+int trimSize;
+trimSize = maskHeadPolyT(in, len);
+printf("%s - trimmed %d\n", in, trimSize);
 }
 
 int main(int argc, char *argv[])
 /* Process command line. */
 {
-if (argc != 3)
+if (argc != 2)
    usage();
-test(argv[1], argv[2]);
+dnaUtilOpen();
+test(argv[1]);
 return 0;
 }
