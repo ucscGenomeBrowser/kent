fd36e5476a4df2e745f388e1958b88c46f2d123c
markd <markd>
2005-01-16 21:00:20 +0000

    removed warning in gcc 3.4

--- src/hg/makeDb/genbank/src/selectWithPsl/selectWithPsl.c
+++ src/hg/makeDb/genbank/src/selectWithPsl/selectWithPsl.c
@@ -7,7 +7,7 @@ struct sqlConnection;
 #include "psl.h"
 #include "estOrientInfo.h"
 
-static char const rcsid[] = "$Id: selectWithPsl.c,v 1.1 2003/06/03 01:27:48 markd Exp $";
+static char const rcsid[] = "$Id: selectWithPsl.c,v 1.2 2005/01/16 21:00:20 markd Exp $";
 
 /* command line option specifications */
 static struct optionSpec optionSpecs[] = {
@@ -51,7 +51,7 @@ char key[ROW_KEY_BUFSZ];
 struct hashEl* hel;
 makeRowKey(chrom, start, end, name, key);
 hel = hashLookup(keyHash, key);
-if ((hel != NULL) && (((int)hel->val)++) == 0)
+if ((hel != NULL) && (((int)(hel->val)++) == 0))
     return TRUE;
 else
     return FALSE;
