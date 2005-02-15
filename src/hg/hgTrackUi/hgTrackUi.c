c1a94f37396a4608e02930ecf3a25895e891abb3
hiram <hiram>
2005-02-15 22:15:59 +0000

    Proper help page for EST base labelling feature

--- src/hg/hgTrackUi/hgTrackUi.c
+++ src/hg/hgTrackUi/hgTrackUi.c
@@ -23,8 +23,9 @@
 
 #define CDS_HELP_PAGE "../goldenPath/help/hgCodonColoring.html"
 #define CDS_MRNA_HELP_PAGE "../goldenPath/help/hgCodonColoringMrna.html"
+#define CDS_BASE_HELP_PAGE "../goldenPath/help/hgBaseLabel.html"
 
-static char const rcsid[] = "$Id: hgTrackUi.c,v 1.181 2005/02/15 19:04:09 hiram Exp $";
+static char const rcsid[] = "$Id: hgTrackUi.c,v 1.182 2005/02/15 22:15:59 hiram Exp $";
 
 struct cart *cart = NULL;	/* Cookie cart with UI settings */
 char *database = NULL;		/* Current database. */
@@ -382,7 +383,7 @@ baseDrawDefault = trackDbSettingOrDefault(tdb, PSL_SEQUENCE_BASES, PSL_SEQUENCE_
 drawOption = cartUsualString(cart, baseColorVar, baseDrawDefault);
 baseColorDropDown(baseColorVar, drawOption);
 printf("&nbsp;<b>bases.</b>\n");
-printf("<BR><BR><a href=%s>Help on EST base coloring</a><br>",CDS_MRNA_HELP_PAGE);
+printf("<BR><BR><a href=%s>Help on EST base coloring</a><br>",CDS_BASE_HELP_PAGE);
 }
 
 
