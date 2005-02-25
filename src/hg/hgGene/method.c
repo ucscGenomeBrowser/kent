df22835339fd026415bee90dfdd352cdf57a9a32
fanhsu <fanhsu>
2005-02-25 17:13:01 +0000

    Updated to support ccdsGene.

--- src/hg/hgGene/method.c
+++ src/hg/hgGene/method.c
@@ -7,14 +7,14 @@
 #include "spDb.h"
 #include "hgGene.h"
 
-static char const rcsid[] = "$Id: method.c,v 1.1 2004/05/19 23:21:54 fanhsu Exp $";
+static char const rcsid[] = "$Id: method.c,v 1.2 2005/02/25 17:13:01 fanhsu Exp $";
 
 static void methodPrint(struct section *section, 
 	struct sqlConnection *conn, char *geneId)
 /* Print out link to UCSC KG method and credits details. */
 {
 hPrintf("Click ");
-hPrintf("<A HREF=\"../cgi-bin/hgGene?hgg_do_kgMethod=1\" target=_blank>");
+hPrintf("<A HREF=\"../cgi-bin/hgGene?hgg_do_kgMethod=1&hgg_type=%s\" target=_blank>", curGeneType);
 hPrintf("here</A>\n");
 hPrintf(" for details.");
 }
