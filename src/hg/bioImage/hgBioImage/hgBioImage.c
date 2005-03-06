b0b8be76b3dc84ffc9613f2a8b4bd8a1331d7d2b
kent <kent>
2005-03-06 13:42:05 +0000

    Image and some associated data is displayed

--- src/hg/bioImage/hgBioImage/hgBioImage.c
+++ src/hg/bioImage/hgBioImage/hgBioImage.c
@@ -6,6 +6,7 @@
 #include "cart.h"
 #include "jksql.h"
 #include "cheapcgi.h"
+#include "portable.h"
 #include "htmshell.h"
 #include "web.h"
 #include "cart.h"
@@ -46,37 +47,130 @@ safef(path, PATH_LEN, "%s/%s", row[0], row[1]);
 sqlFreeResult(&sr);
 }
 
-void webMain()
+void bioImageOrganism(struct sqlConnection *conn, int id, char organism[PATH_LEN])
+/* Return binomial scientific name of organism associated with given image. */
 {
-#ifdef SOON
-#endif /* SOON */
-int id = cartUsualInt(cart, hgbiId, 1);
+char query[256];
+safef(query, sizeof(query),
+	"select uniProt.taxon.binomial from image,uniProt.taxon"
+         " where image.id = %d and image.taxon = uniProt.taxon.id",
+	 id);
+sqlNeedQuickQuery(conn, query, organism, PATH_LEN);
+}
+
+
+void bioImageStage(struct sqlConnection *conn, int id, char stage[PATH_LEN])
+/* Return string describing growth stage of organism */
+{
+char query[256];
+double daysPassed;
+char *startMarker;
+struct sqlResult *sr;
+
+char **row;
+safef(query, sizeof(query),
+    "select isEmbryo,age from image where id = %d", id);
+sr = sqlGetResult(conn, query);
+if ((row = sqlNextRow(sr)) == NULL)
+    errAbort("No image of id %d", id);
+if (!sameString(row[0], "0"))
+    startMarker = "fertilization";
+else
+    startMarker = "birth";
+daysPassed = atof(row[1]);
+safef(stage, PATH_LEN, "%3.1f days after %s", daysPassed, startMarker);
+sqlFreeResult(&sr);
+}
+
+void bioImageGeneName(struct sqlConnection *conn, int id, char geneName[PATH_LEN])
+/* Return gene name  - HUGO if possible, RefSeq/GenBank, etc if not. */
+{
+char query[256];
+safef(query, sizeof(query),
+	"select gene from image where id=%d", id);
+sqlNeedQuickQuery(conn, query, geneName, PATH_LEN);
+if (geneName[0] == 0)
+    {
+    safef(query, sizeof(query),
+        "select refSeq from image where id=%d", id);
+    sqlNeedQuickQuery(conn, query, geneName, PATH_LEN);
+    if (geneName[0] == 0)
+        {
+	safef(query, sizeof(query),
+	    "select genbank from image where id=%d", id);
+	sqlNeedQuickQuery(conn, query, geneName, PATH_LEN);
+	if (geneName[0] == 0)
+	    {
+	    char locusLink[32];
+	    safef(query, sizeof(query), 
+		"select locusLink from image where id=%d", id);
+	    sqlNeedQuickQuery(conn, query, locusLink, sizeof(locusLink));
+	    if (locusLink[0] == 0)
+	        {
+		safef(query, sizeof(query),
+		    "select submitId from image where id=%d", id);
+		sqlNeedQuickQuery(conn, query, geneName, PATH_LEN);
+		}
+	    else
+	        {
+		safef(geneName, PATH_LEN, "NCBI Gene ID #%s", locusLink);
+		}
+	    }
+	}
+    }
+}
+
+void bioImageAccession(struct sqlConnection *conn, int id, char accession[PATH_LEN])
+/* Return RefSeq/Genbank accession or n/a if none available. */
+{
+char query[256];
+safef(query, sizeof(query),
+	"select refSeq from image where id=%d", id);
+sqlNeedQuickQuery(conn, query, accession, PATH_LEN);
+if (accession[0] == 0)
+    {
+    safef(query, sizeof(query),
+        "select genbank from image where id=%d", id);
+    sqlNeedQuickQuery(conn, query, accession, PATH_LEN);
+    if (accession[0] == 0)
+	safef(accession, PATH_LEN, "n/a");
+    }
+}
+
+void bioImageType(struct sqlConnection *conn, int id, char type[PATH_LEN])
+/* Return RefSeq/Genbank accession or n/a if none available. */
+{
+char query[256];
+safef(query, sizeof(query),
+	"select imageType.name from image,imageType "
+	"where image.id=%d and imageType.id = image.imageType", id);
+sqlNeedQuickQuery(conn, query, type, PATH_LEN);
+if (type[0] == 0)
+    safef(type, PATH_LEN, "n/a");
+}
+
+
+void webMain(struct sqlConnection *conn, int id, char *geneName)
+{
 char query[256];
 struct sqlResult *sr;
 char **row;
-struct sqlConnection *conn = sqlConnect("bioImage");
-char fullPath[PATH_LEN];
+char fullPath[PATH_LEN], organism[PATH_LEN], stage[PATH_LEN], accession[PATH_LEN],
+	imageType[PATH_LEN];
 
-safef(query, sizeof(query), "select fullLocation,fileName,submitId,gene,refSeq,genbank,isEmbryo,age,bodyPart,sliceType,imageType,treatment from image where id = %d", id);
-uglyf("query: %s<BR>", query);
-sr = sqlGetResult(conn, query);
-row = sqlNextRow(sr);
-if (row != NULL)
-    {
-    uglyf("%s[0] %s[1] %s[2] %s[3] %s[4] %s[5] %s[6] %s[7] %s[8] %s[9] %s[10] %s[11]<BR>\n", 
-    	row[0], row[1], row[2], row[3], row[4], row[5], row[6],
-    	row[7], row[8], row[9], row[10], row[11]);
-    sqlFreeResult(&sr);
-    }
-else
-    {
-    warn("Can't find image id %d", id);
-    sqlFreeResult(&sr);
-    }
 
 bioImageFullPath(conn, id, fullPath);
-uglyf("Image path: %s<BR>\n", fullPath);
-sqlDisconnect(&conn);
+printf("<IMG SRC=\"/%s\"><BR>\n", fullPath);
+uglyf("<B>Image path:</B> %s<BR>\n", fullPath);
+bioImageOrganism(conn, id, organism);
+printf("<B>organism:</B> %s  ", organism);
+bioImageAccession(conn, id, accession);
+bioImageStage(conn, id, stage);
+bioImageType(conn, id, imageType);
+printf("<B>stage:</B> %s<BR>\n", stage);
+printf("<B>gene:</B> %s ", geneName);
+printf("<B>accession:</B> %s ", accession);
+printf("<B>type:</B> %s<BR>\n", imageType);
 }
 
 void cartMain(struct cart *theCart)
@@ -85,11 +179,17 @@ void cartMain(struct cart *theCart)
 {
 cart = theCart;
 
+
     {
+    int id = cartUsualInt(cart, hgbiId, 1);
+    struct sqlConnection *conn = sqlConnect("bioImage");
     /* Default case - start fancy web page. */
-    cartWebStart(cart, "UCSC BioImage Browser");
-    webMain();
+    char geneName[PATH_LEN];
+    bioImageGeneName(conn, id, geneName);
+    cartWebStart(cart, "UCSC BioImage Browser on %s", geneName);
+    webMain(conn, id, geneName);
     cartWebEnd();
+    sqlDisconnect(&conn);
     }
 }
 
