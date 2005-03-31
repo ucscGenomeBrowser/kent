f98c5d93d99602dc6ffcb6c88208dfaee3669fab
kent <kent>
2005-03-31 05:29:21 +0000

    Starting to add variable declarations (just of simple built-in types so far)

--- src/ai/paraFlow/compiler/main.c
+++ src/ai/paraFlow/compiler/main.c
@@ -6,6 +6,7 @@
 #include "dystring.h"
 #include "localmem.h"
 #include "paraToken.h"
+#include "pfType.h"
 
 void usage()
 /* Explain command line and exit. */
@@ -146,6 +147,61 @@ struct paraSymbol
     struct paraType *type;	/* Symbol type */
     };
 
+struct pfScope
+/* The symbol tables for this scope and a pointer to the
+ * parent scope. */
+     {
+     struct pfScope *parent;	/* Parent scope if any. */
+     struct hash *types;	/* Types defined in this scope. */
+     struct hash *vars;		/* Variables defined in this scope (including functions) */
+     };
+
+struct pfScope *pfScopeNew(struct pfScope *parent, int size)
+/* Create new scope with given parent.  Size is just a hint
+ * of what to make the size of the symbol table as a power of
+ * two.  Pass in 0 if you don't care. */
+{
+struct pfScope *scope;
+int varSize = size;
+int typeSize;
+
+if (varSize <= 0)
+    varSize = 5;
+typeSize = varSize - 2;
+if (typeSize <= 0)
+    typeSize = 3;
+AllocVar(scope);
+scope->types = hashNew(typeSize);
+scope->vars = hashNew(varSize);
+scope->parent = parent;
+return scope;
+}
+
+static void pfScopeAddType(struct pfScope *scope, char *name, struct pfType *type)
+/* Add type to scope. */
+{
+hashAdd(scope->types, name, type);
+}
+
+static void pfScopeAddVar(struct pfScope *scope, char *name, struct pfType *type)
+/* Add type to scope. */
+{
+hashAdd(scope->vars, name, type);
+}
+
+struct hashEl *pfScopeFindType(struct pfScope *scope, char *name)
+/* Find type associated with name in scope and it's parents. */
+{
+while (scope != NULL)
+    {
+    struct hashEl *hel = hashLookup(scope->types, name);
+    if (hel != NULL)
+        return hel;
+    scope = scope->parent;
+    }
+return NULL;
+}
+
 struct pfParse
 /* The para parse tree. */
     {
@@ -154,7 +210,7 @@ struct pfParse
     struct pfToken *tok;	/* Token associated with node. */
     struct pfParse *parent;	/* Parent statement if any. */
     struct pfParse *children;	/* subparts. */
-    struct hash *symbols;	/* Symbol table (may be NULL) */
+    struct pfVar *var;		/* Associated variable if any. */
     };
 
 void pfParseDump(struct pfParse *pp, int level, FILE *f)
@@ -168,13 +224,13 @@ for (child = pp->children; child != NULL; child = child->next)
 }
 
 
-struct pfParse *pfParseStatement(struct pfParse *parent, 
-	struct pfToken **pTokList);
+void pfParseStatement(struct pfParse *parent, 
+	struct pfToken **pTokList, struct pfScope *scope);
 
 static void syntaxError(struct pfToken *tok)
 /* Complain about syntax error and quit. */
 {
-errAbort("Syntax error line %d of %s", tok->source->name, tok->startLine);
+errAbort("Syntax error line %d of %s", tok->startLine, tok->source->name);
 }
 
 struct pfParse *pfParseNew(enum pfParseType type,
@@ -207,7 +263,8 @@ if (tok->type != pftName || !sameString(tok->val.s, name))
 *pTokList = tok->next;
 }
 
-static void compoundToChildren(struct pfParse *pp, struct pfToken **pTokList)
+static void compoundToChildren(struct pfParse *pp, struct pfToken **pTokList,
+	struct pfScope *scope)
 /* Parse {, a list of statements, and } and hang statements on
  * pp->children. */
 {
@@ -217,7 +274,6 @@ if (tok->type != '{')
 tok = tok->next;
 for (;;)
     {
-    struct pfParse *statement;
     if (tok == NULL)
 	{
 	tok = *pTokList;
@@ -230,23 +286,22 @@ for (;;)
 	slReverse(&pp->children);
 	return;
 	}
-    statement = pfParseStatement(pp, &tok);
-    slAddHead(&pp->children, statement);
+    pfParseStatement(pp, &tok, scope);
     }
 }
 
-struct pfParse *parseCompound(struct pfParse *parent,
-	struct pfToken **pTokList)
+static void parseCompound(struct pfParse *parent,
+	struct pfToken **pTokList, struct pfScope *scope)
 /* Parse a compound statement (statement list surrounded
  * by brackets */
 {
 struct pfParse *pp = pfParseNew(pptCompound, *pTokList, parent);
-compoundToChildren(pp, pTokList);
-return pp;
+slAddHead(&parent->children, pp);
+compoundToChildren(pp, pTokList, scope);
 }
 
 
-struct pfParse *varUse(struct pfParse *parent, struct pfToken **pTokList)
+struct pfParse *varUse(struct pfParse *parent, struct pfToken **pTokList, struct pfScope *scope)
 /* Make sure have a name, and create a varUse type node
  * based on it. */
 {
@@ -259,8 +314,8 @@ if (tok->type != pftName)
 return pp;
 }
 
-struct pfParse *parseForeach(struct pfParse *parent,
-	struct pfToken **pTokList)
+static void parseForeach(struct pfParse *parent,
+	struct pfToken **pTokList, struct pfScope *scope)
 /* Parse foreach statement */
 {
 struct pfToken *tok = *pTokList;
@@ -270,20 +325,19 @@ struct pfParse *collection;
 struct pfParse *statement;
 
 tok = tok->next;	/* Skip over 'foreach' */
-element = varUse(pp, &tok);
+element = varUse(pp, &tok, scope);
 skipRequiredName("in", &tok);
-collection = varUse(pp, &tok);
+collection = varUse(pp, &tok, scope);
 /* Make sure next token matches name, and then skip it. */
-statement = pfParseStatement(pp, &tok);
-slAddHead(&pp->children, statement);
+pfParseStatement(pp, &tok, scope);
 slAddHead(&pp->children, collection);
 slAddHead(&pp->children, element);
 *pTokList = tok;
-return pp;
+slAddHead(&parent->children, pp);
 }
 
-struct pfParse *parseClass(struct pfParse *parent,
-	struct pfToken **pTokList)
+static void parseClass(struct pfParse *parent,
+	struct pfToken **pTokList, struct pfScope *scope)
 /* Parse class declaration statement. */
 {
 struct pfToken *tok = *pTokList;
@@ -291,73 +345,113 @@ struct pfParse *pp;
 skipRequiredName("class", &tok);
 pp = pfParseNew(pptClass, tok, parent);
 tok = tok->next;
-compoundToChildren(pp, &tok);
+compoundToChildren(pp, &tok, scope);
 *pTokList = tok;
-return pp;
+slAddHead(&parent->children, pp);
 }
 
-struct pfParse *pfParseStatement(struct pfParse *parent, 
-	struct pfToken **pTokList)
-/* Parse a single statement. */
+static void varDeclareOne(struct pfParse *parent,
+	struct pfType *type, struct pfToken **pTokList, struct pfScope *scope)
 {
 struct pfToken *tok = *pTokList;
-struct pfParse *pp = NULL;
+struct pfParse *pp = pfParseNew(pptVarDec, tok, parent);
+if (tok->type != pftName)
+    errAbort("Expecting variable name line %d of %s", tok->startLine, tok->source->name);
+pfScopeAddVar(scope, tok->val.s, type);
+slAddHead(&parent->children, pp);
+*pTokList = tok->next;
+}
+
+static void parseVarDeclaration(struct pfParse *parent, char *typeName,
+	struct pfType *type, struct pfToken **pTokList, struct pfScope *scope)
+/* Parse variable declaration - something of form:
+ *    typeName varName,varName, ... varName */
+{
+struct pfToken *tok = *pTokList;
+tok = tok->next;	/* Skip over type. */
+for (;;)
+    {
+    varDeclareOne(parent, type, &tok, scope);
+    if (tok == NULL)
+        break;
+    if (tok->type != ',')
+        break;
+    tok = tok->next;
+    }
+*pTokList = tok;
+}
+
+void pfParseStatement(struct pfParse *parent, 
+	struct pfToken **pTokList, struct pfScope *scope)
+/* Parse a single statement, and add results to (head) of
+ * parent->childList. */
+{
+struct pfToken *tok = *pTokList;
 
 if (tok->type == '{')
-    pp = parseCompound(parent, pTokList);
+    parseCompound(parent, pTokList, scope);
+else if (tok->type == ';')
+    *pTokList = tok->next;
 else if (tok->type == pftName)
     {
     char *s = tok->val.s;
-    if (sameString(s, "foreach"))
-        pp = parseForeach(parent, pTokList);
+    struct hashEl *typeHel = pfScopeFindType(scope, s);
+    if (typeHel != NULL)
+	{
+	struct pfType *type = typeHel->val;
+        parseVarDeclaration(parent, typeHel->name, type, pTokList, scope);
+	}
+    else if (sameString(s, "foreach"))
+        parseForeach(parent, pTokList, scope);
     else if (sameString(s, "class"))
-        pp = parseClass(parent, pTokList);
+        parseClass(parent, pTokList, scope);
 #ifdef SOON
     else if (sameString(s, "to"))
-        pp = parseTo(parent, pTokList);
+        parseTo(parent, pTokList, scope);
     else if (sameString(s, "for"))
-        pp = parseFor(parent, pTokList);
+        parseFor(parent, pTokList, scope);
     else if (sameString(s, "while"))
-        pp = parseWhile(parent, pTokList);
-    else if (sameString(s, "flow"))
-        pp = parseFlow(parent, pTokList);
+        parseWhile(parent, pTokList);
+    else if (sameString(s, "flow", scope))
+        parseFlow(parent, pTokList);
     else if (sameString(s, "para"))
-        pp = parsePara(parent, pTokList);
+        parsePara(parent, pTokList, scope);
     else
-        pp = parseAssignOrCall(parent, pTokList);
+        parseAssignOrCall(parent, pTokList, scope);
 #endif /* SOON */
     else
+	{
 	syntaxError(tok);
+	}
     }
 else
     {
     syntaxError(tok);
     }
-return pp;
 }
 
-struct pfParse *pfParseProgram(struct pfToken *tokList)
+struct pfParse *pfParseProgram(struct pfToken *tokList, struct pfScope *scope)
 /* Convert token list to parsed program. */
 {
-struct pfToken *tok;
+struct pfToken *tok = tokList;
 struct pfParse *program = pfParseNew(pptProgram, tokList, NULL);
 
-while (tokList != NULL)
+while (tok != NULL)
     {
-    struct pfParse *statement = pfParseStatement(pptProgram, &tokList);
-    slAddHead(&program->children, statement);
+    pfParseStatement(program, &tok, scope);
     }
 slReverse(&program->children);
 return program;
 }
 
-void paraFlowOnFile(char *fileName)
+void paraFlowOnFile(char *fileName, struct pfScope *scope)
 /* Execute paraFlow on single file. */
 {
 struct pfTokenizer *pfTkz = pfTokenizerNew(fileName);
 struct pfToken *tokList = NULL, *tok;
 struct pfParse *program;
 
+scope = pfScopeNew(scope, 12);
 while ((tok = pfTokenizerNext(pfTkz)) != NULL)
     {
     if (tok->type != pftWhitespace && tok->type != pftComment)
@@ -366,17 +460,30 @@ while ((tok = pfTokenizerNext(pfTkz)) != NULL)
 	}
     }
 slReverse(&tokList);
-program = pfParseProgram(tokList);
+program = pfParseProgram(tokList, scope);
 pfParseDump(program, 0, stdout);
+}
+
+static void addBuiltInTypes(struct pfScope *scope)
+/* Add built in types . */
+{
+static char *t[] = { "var" , "string" , "bit" , "byte" , "short" , "int" , "long", "float"};
+int i;
+for (i=0; i<ArraySize(t); ++i)
+    pfScopeAddType(scope, t[i], NULL);
+
 }
 
 int main(int argc, char *argv[])
 /* Process command line. */
 {
+struct pfScope *scope;
 optionInit(&argc, argv, options);
 if (argc != 2)
     usage();
-paraFlowOnFile(argv[1]);
+scope = pfScopeNew(NULL, 14);
+addBuiltInTypes(scope);
+paraFlowOnFile(argv[1], scope);
 #ifdef SOON
 paraFlowOnFile("../testProg/easy/stringInit.pf");
 #endif /* SOON */
