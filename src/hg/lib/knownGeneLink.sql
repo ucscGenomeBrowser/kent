76aea2e66391995be14738aafe553a98caf3836e
fanhsu <fanhsu>
2005-04-12 16:19:46 +0000

    Updated indice sizes to 16.

--- src/hg/lib/knownGeneLink.sql
+++ src/hg/lib/knownGeneLink.sql
@@ -2,7 +2,7 @@ CREATE TABLE knownGeneLink (
   name varchar(40) NOT NULL default '',
   seqType char(1) NOT NULL default 'u',
   proteinID varchar(40) NOT NULL default '',
-  KEY name (name(10)),
-  KEY proteinID (proteinID(10))
+  KEY name (name(16)),
+  KEY proteinID (proteinID(16))
 ) TYPE=MyISAM;
 
