390d3edf72cb5a4d8737ddbb214d295f650c2286
kate <kate>
2005-04-20 01:34:24 +0000

    Wrong prototype removed in previous checkin -- repairing

--- src/hg/inc/jksql.h
+++ src/hg/inc/jksql.h
@@ -157,8 +157,8 @@ boolean sqlDatabaseExists(char *database);
 boolean sqlTableExists(struct sqlConnection *sc, char *table);
 /* Return TRUE if a table exists. */
 
-boolean sqlTableExistsDb(char *database, char *table);
-/* Return TRUE if a table in a database exists. */
+int sqlTableSizeIfExists(struct sqlConnection *sc, char *table);
+/* Return row count if a table exists, -1 if it doesn't. */
 
 boolean sqlTablesExist(struct sqlConnection *conn, char *tables);
 /* Check all tables in space delimited string exist. */
