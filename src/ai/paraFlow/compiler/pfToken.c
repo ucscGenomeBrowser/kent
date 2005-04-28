0cebbb32ef5b01e0033bb69e6abefb0ce09cc94f
kent <kent>
2005-04-28 03:56:28 +0000

    Making single quoted things give you a literal char rather than a string.

--- src/ai/paraFlow/compiler/pfToken.c
+++ src/ai/paraFlow/compiler/pfToken.c
@@ -393,6 +393,24 @@ else
     }
 }
 
+static void tokLiteralChar(struct pfTokenizer *tkz, struct pfToken *tok,
+	char quoteC)
+/* Tokenize single-quote bounded character into a small number. */
+{
+char *pos = tkz->pos+1;
+char c;
+c = *pos++;
+if (c == '\\')
+    c = translateEscape(&pos);
+tok->val.i = c;
+tok->type = pftInt;
+c = *pos++;
+if (c != quoteC)
+    errAt(tok, "Missing closing %c", c);
+tok->textSize = pos - tkz->pos;
+tkz->pos = pos;
+}
+
 static void tokNameOrComplexQuote(struct pfTokenizer *tkz, 
 	struct pfToken *tok)
 /* Parse out name from c until first non-white-space.
@@ -660,6 +678,8 @@ tok->text = tkz->pos;
 switch (c)
     {
     case '\'':
+        tokLiteralChar(tkz, tok, c);
+	break;
     case '"':
 	tokString(tkz, tok, c);
 	break;
