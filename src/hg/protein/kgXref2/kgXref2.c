8022e9acf8bdc957d6534d1f782ad1b8237f665d
fanhsu <fanhsu>
2005-05-16 18:48:43 +0000

    Updated for the latest new KG build.  Added logic to deal with variant splice proteins.

--- src/hg/protein/kgXref2/kgXref2.c
+++ src/hg/protein/kgXref2/kgXref2.c
@@ -9,11 +9,11 @@ void usage()
 errAbort(
   "kgXref2 - create new Known Gene cross reference table kgXref2.tab file."
   "usage:\n"
-  "   kgXref2 <db> <proteinsYYMMDD> <ro_db>\n"
-  "          <db> is known Genes database under construction\n"
-  "          <proteinsYYMMDD> is protein database name \n"
-  "          <ro_db> is target organism database\n"
-  "example: kgXref2 kgDB proteins040115 hg16\n");
+  "   kgXref2 <tmpDb> <YYMMDD> <roDb>\n"
+  "          <tmpDb> is temp KG database under construction\n"
+  "          <YYMMDD> is protein database release date \n"
+  "          <roDb> is read only target organism database\n"
+  "example: kgXref2 kgHg17FTempgDB 050415 hg17\n");
 }
 
 int main(int argc, char *argv[])
@@ -24,53 +24,83 @@ int main(int argc, char *argv[])
     char **row, **row2;
     char cond_str[256];  
   
+    char *protDbDate;
     char *kgID;
-    char *proteinDisplayID;
+    char *protDisplayId;
     
     char *seqType;	/* sequence type m=mRNA g=genomic u=undefined */
 
     FILE *o1;
-    char *database;
-    char *proteinDB;
+    char *kgTempDb;
+    char spDb[255],proteinsDb[255];
     char *ro_DB;
     char *refSeqName;
     char *hugoID;
     char *protAcc;	/* protein Accession number from NCBI */
     char *answer;
-
+    char *emptyStr;
+    char *parSpID;
+    
     int leg;		/* marker for debugging */
-    char *mRNA, *spID, *spDisplayID, *geneSymbol, *refseqID, *desc;
+    char *mRNA, *spID, *kgProteinID, *geneSymbol, *refseqID, *desc;
 
     if (argc != 4) usage();
-    database  = cloneString(argv[1]);
-    proteinDB = cloneString(argv[2]);
+    kgTempDb  = cloneString(argv[1]);
+    protDbDate = cloneString(argv[2]);
     ro_DB = cloneString(argv[3]);
+    
+    safef(spDb, sizeof(spDb), "sp%s",  protDbDate);
+    safef(proteinsDb, sizeof(proteinsDb), "proteins%s", protDbDate);
 
     conn = hAllocConn();
     conn2= hAllocConn();
     conn3= hAllocConn();
 
     o1 = mustOpen("j.dat", "w");
-	
-    sprintf(query2,"select name, proteinID from %s.knownGene;", database);
+
+    emptyStr = strdup("");
+
+    sprintf(query2,"select name, proteinID from %s.knownGene;", kgTempDb);
     sr2 = sqlMustGetResult(conn2, query2);
     row2 = sqlNextRow(sr2);
     while (row2 != NULL)
 	{
 	kgID 		= row2[0];
-	spDisplayID	= row2[1];
+	kgProteinID	= row2[1];
 	
 	refseqID 	= strdup("");
 	geneSymbol 	= strdup("");
 	desc		= strdup("");
 	protAcc		= strdup("");
 
-        sprintf(cond_str, "displayID='%s'", spDisplayID);
-        spID = sqlGetField(conn, proteinDB, "spXref3", "accession", cond_str);
-        
+        sprintf(cond_str, "displayID='%s'", kgProteinID);
+        spID = sqlGetField(conn, proteinsDb, "spXref3", "accession", cond_str);
+    
+        /* process variant splice proteins */
+	if (spID == NULL)
+	    {
+            sprintf(cond_str, "varAcc='%s'", kgProteinID);
+	    spID = kgProteinID;
+	    
+            parSpID = sqlGetField(conn, proteinsDb, "splicProt", "parAcc", cond_str);
+	    if (parSpID != NULL)
+	    	{
+        	sprintf(cond_str, "accession='%s'", parSpID);
+        	protDisplayId = sqlGetField(conn, proteinsDb, "spXref3", "displayID", cond_str);
+		}
+	    else
+	    	{
+		fprintf(stderr, "%s not found in kgXref3 nor in varProtein.\n", kgProteinID);
+		exit(1);
+		}
+	    }
+	else
+	    {
+	    protDisplayId = kgProteinID;	
+	    }
 	/* use description for the protein as default, replace it with HUGO desc if available. */
-	sprintf(cond_str, "displayID='%s'", spDisplayID);
-        desc  = sqlGetField(conn, proteinDB, "spXref3", "description", cond_str);
+	sprintf(cond_str, "displayID='%s'", protDisplayId);
+        desc  = sqlGetField(conn, proteinsDb, "spXref3", "description", cond_str);
         
         if (strstr(kgID, "NM_") != NULL)
             {
@@ -95,37 +125,25 @@ int main(int argc, char *argv[])
             }
         else
             {
-            sprintf(cond_str, "displayID = '%s'", spDisplayID);
-            hugoID = sqlGetField(conn, proteinDB, "spXref3", "hugoSymbol", cond_str);
+            sprintf(cond_str, "displayID = '%s'", protDisplayId);
+            hugoID = sqlGetField(conn, proteinsDb, "spXref3", "hugoSymbol", cond_str);
             if (!((hugoID == NULL) || (*hugoID == '\0')) )
                 {
 		leg = 21;
                 geneSymbol = cloneString(hugoID);
 
-            	sprintf(cond_str, "displayID = '%s'", spDisplayID);
-            	desc = sqlGetField(conn, proteinDB, "spXref3", "hugoDesc", cond_str);
+            	sprintf(cond_str, "displayID = '%s'", protDisplayId);
+            	desc = sqlGetField(conn, proteinsDb, "spXref3", "hugoDesc", cond_str);
 		if (desc == NULL) 
 		    {
-		    printf("%s don't have hugo desc ...\n", spDisplayID);
+		    printf("%s/%s don't have hugo desc ...\n", kgProteinID, protDisplayId);
 		    fflush(stdout);
 		    }
 		}
 
-            sprintf(cond_str, "mrna = '%s'", kgID);
-            answer = sqlGetField(conn, database, "mrnaRefseq", "refseq", cond_str);
-	    if (answer != NULL) 
-		{
-		leg = 22;
-		refseqID = strdup(answer);
-		
-		sprintf(cond_str, "mrnaAcc='%s'", refseqID);
-        	answer = sqlGetField(conn3, ro_DB, "refLink", "protAcc", cond_str);
-        	if (answer != NULL)
-            	    {
-	    	    protAcc = strdup(answer);
-	    	    }
-		}
-            	
+	    refseqID = emptyStr;
+	    protAcc  = emptyStr;
+	    
 	    if (strlen(geneSymbol) == 0)
 		{ 
 		leg = 23;
@@ -161,7 +179,7 @@ int main(int argc, char *argv[])
 		}
 	
 	fprintf(o1, "%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n", 
-		kgID, kgID, spID, spDisplayID, geneSymbol, refseqID, protAcc, desc);
+		kgID, kgID, spID, protDisplayId, geneSymbol, refseqID, protAcc, desc);
 	row2 = sqlNextRow(sr2);
 	}
 
