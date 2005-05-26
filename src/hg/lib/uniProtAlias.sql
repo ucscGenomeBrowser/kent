5af77faeb45f3ccfeaf1067a6dd33f08e3895c8d
fanhsu <fanhsu>
2005-05-26 20:11:54 +0000

    Removed the 2nd index to speed up loading.  Create the index after loading.

--- src/hg/lib/uniProtAlias.sql
+++ src/hg/lib/uniProtAlias.sql
@@ -3,7 +3,6 @@ CREATE TABLE uniProtAlias (
   alias varchar(40) default NULL,	# alias, could be acc, old and new display IDs, etc.
   aliasSrc varchar(4) default NULL,	# source of this type of alias is from
   aliasSrcDate date default NULL,	# date of the source data
-  KEY acc (acc),
-  KEY alias (alias)
+  KEY acc (acc)
 ) TYPE=MyISAM;
 
