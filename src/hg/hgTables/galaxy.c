0fdd5a5e0b0917ee3389a04877b09424111cd7f3
giardine <giardine>
2005-06-07 14:04:08 +0000

    preparing to add Galaxy to genome-test

--- src/hg/hgTables/galaxy.c
+++ src/hg/hgTables/galaxy.c
@@ -20,7 +20,7 @@
 #include "htmlPage.h"
 #include "wiggle.h"
 
-static char const rcsid[] = "$Id: galaxy.c,v 1.1.2.1 2005/04/26 19:21:54 giardine Exp $";
+static char const rcsid[] = "$Id: galaxy.c,v 1.1.2.2 2005/06/07 14:04:08 giardine Exp $";
 
 char *getGalaxyUrl()
 /* returns the url for the galaxy cgi, based on script name */
@@ -30,6 +30,11 @@ char *script = cgiScriptName();
 if (script == NULL || strstr(script, "-test"))
     {
     url = cloneString("http://galaxy-test.cse.psu.edu/cgi-bin/galaxy");
+    /* allow to be overwritten by galaxyUrl parameter */
+    if (cartVarExists(cart, "galaxyUrl"))
+        {
+        url = cartString(cart, "galaxyUrl");
+        }
     }
 else
     {
@@ -50,7 +55,7 @@ htmlOpen("Output %s as %s", "results to Galaxy", shortLabel);
 hPrintf("<FORM ACTION=\"%s\" METHOD=POST>\n", getGalaxyUrl());
 hPrintf("%s\n", "<A HREF=\"/goldenPath/help/hgTextHelp.html#FeatureBits\">"
      "<B>Help</B></A><P>");
-/* copy cart parameters into hidden fields to send to GALA */
+/* copy cart parameters into hidden fields to send to Galaxy */
 hPrintf("\n"); /* make more readable */
 cgiMakeHiddenVar("galaDoTbQuery", "1");
 hPrintf("\n"); 
@@ -59,8 +64,11 @@ if (hguid > 0)
     char id[25];
     safef(id, sizeof(id), "%u", hguid);
     cgiMakeHiddenVar("hguid", id);
+    cgiMakeHiddenVar("userID", id);
     hPrintf("\n"); 
     }
+cgiMakeHiddenVar("galaxyFreeze", database);
+cgiMakeHiddenVar("galaxyFileFormat", "bed");
 /* this is part of list below
 if (track != NULL)
     cgiMakeHiddenVar("hgta_track", track->tableName);
@@ -92,7 +100,7 @@ cgiMakeButton(hgtaDoGetGalaxyQuery, "Send query to Galaxy");
 hPrintf("</FORM>\n");
 hPrintf(" ");
 /* new form as action is different */
-hPrintf("<FORM ACTION=\"..%s\" METHOD=GET>\n", getScriptName());
+hPrintf("<FORM ACTION=\"..%s\" METHOD=GET>\n", cgiScriptName());
 cgiMakeButton(hgtaDoMainPage, "Cancel");
 hPrintf("</FORM>\n");
 htmlClose();
@@ -104,8 +112,6 @@ void doGalaxyQuery(struct sqlConnection *conn)
 char *table = curTable;
 struct hTableInfo *hti = getHti(database, table);
 struct featureBits *fbList = NULL, *fbPtr;
-//struct customTrack *ctNew = NULL;
-//char *ctWigOutType = cartCgiUsualString(cart, hgtaCtWigOutType, outWigData);
 char *fbQual = fbOptionsToQualifier();
 char fbTQ[128];
 int fields = hTableInfoBedFieldCount(hti);
@@ -116,20 +122,16 @@ FILE *f;
 
 makeTempName(&tn, hgtaCtTempNamePrefix, ".bed");
 f = mustOpen(tn.forCgi, "w");
-//debug = mustOpen("./debug.out", "w");
-//fprintf(stderr, "TESTING GALAXY writing results to %s\n", tn.forCgi);
 textOpen();
 for (region = regionList; region != NULL; region = region->next)
     { /* print chrom at a time to save memory usage */
     struct bed *bedList = NULL, *bed;
     struct lm *lm = lmInit(64*1024);
     printf("starting %s\n", region->chrom); /* keep apache happy */
-    //fprintf(stderr, "TESTING GALAXY starting %s\n", region->chrom);
 
     /* put if here for wiggle data points or rest */
     bedList = cookedBedList(conn, curTable, region, lm, NULL);
     printf(" have list\n"); /* keep apache happy */
-    //fprintf(stderr, "TESTING GALAXY done cookedBedList\n");
 
     if ((fbQual == NULL) || (fbQual[0] == 0))
         {
@@ -168,25 +170,13 @@ void galaxyHandler (char *format, va_list args)
 /* error Handler that passes error on to Galaxy */
 {
 char msg[512];
-//fprintf(stderr, "TESTING in error handler\n");
 sprintf(msg, format, args);
-//fprintf(stderr, "Galaxy error: %s\n", msg);
-//sendResultMessageToGala (msg, 0, 0, cartUserId(cart));
 noWarnAbort();
 }
 
 void doGalaxyPrintGenomes()
 /* print the genomes list as text for GALAxy */
 {
-//struct dbDb *next;  /* Next in singly linked list. */
-//char *name; /* Short name of database.  'hg8' or the like */
-//char *description;  /* Short description - 'Aug. 8, 2001' or the like */
-//char *nibPath;      /* Path to packed sequence files */
-//char *organism;     /* Common name of organism - first letter capitalized */
-//char *defaultPos;   /* Default starting position */
-//int active; /* Flag indicating whether this db is in active use */
-//int orderKey;       /* Int used to control display order within a genome */
-//char *genome;       /* Unifying genome collection to which an assembly belongs */
 
 struct dbDb *dbs, *dbList = hDbDbList();
 textOpen();
