b7cbbaa7f99f99d97ae48a11f93534299a0fd561
braney <braney>
2005-08-21 21:17:55 +0000

    use long long for big files

--- src/utils/faSplit/faSplit.c
+++ src/utils/faSplit/faSplit.c
@@ -9,7 +9,7 @@
 #include "options.h"
 #include "bits.h"
 
-static char const rcsid[] = "$Id: faSplit.c,v 1.26 2005/08/08 18:28:50 hiram Exp $";
+static char const rcsid[] = "$Id: faSplit.c,v 1.27 2005/08/21 21:17:55 braney Exp $";
 
 void usage()
 /* Explain usage and exit. */
@@ -78,14 +78,15 @@ static struct optionSpec optionSpecs[] = {
 
 static int outDirDepth = 0;
 
-unsigned long estimateFaSize(char *fileName)
+unsigned long long estimateFaSize(char *fileName)
 /* Estimate number of bases from file size. */
 {
-unsigned long size = fileSize(fileName);
+off_t size = fileSize(fileName);
+printf("size is %lld\n",size);
 return 0.5 + size * 0.99;
 }
 
-unsigned long calcNextEnd(int fileIx, int totalFiles, unsigned long estSize)
+unsigned long calcNextEnd(int fileIx, int totalFiles, unsigned long long estSize)
 /* Return next end to break at. */
 {
 if (fileIx == totalFiles)
@@ -124,7 +125,7 @@ if (digits)
     sprintf(outPath+strlen(outPath), "%s%0*d.fa", fname, digits, fileCount);
 }
 
-void splitByBase(char *inName, int splitCount, char *outRoot, unsigned long estSize)
+void splitByBase(char *inName, int splitCount, char *outRoot, unsigned long long estSize)
 /* Split into a file base by base. */
 {
 struct lineFile *lf = lineFileOpen(inName, TRUE);
@@ -208,7 +209,7 @@ if (f != NULL)
 lineFileClose(&lf);
 }
 
-void splitByRecord(char *inName, int splitCount, char *outRoot, unsigned long estSize)
+void splitByRecord(char *inName, int splitCount, char *outRoot, unsigned long long estSize)
 /* Split into a file base by base. */
 {
 struct dnaSeq seq;
@@ -407,10 +408,10 @@ for (i=0; i<size; ++i)
     }
 }
 
-void splitByCount(char *inName, int pieceSize, char *outRoot, unsigned long estSize)
+void splitByCount(char *inName, int pieceSize, char *outRoot, unsigned long long estSize)
 /* Split up file into pieces pieceSize long. */
 {
-unsigned long pieces = (estSize + pieceSize-1)/pieceSize;
+unsigned long long pieces = (estSize + pieceSize-1)/pieceSize;
 int digits = digitsBaseTen(pieces);
 int maxN = optionInt("maxN", pieceSize-1);
 boolean oneFile = optionExists("oneFile");
@@ -565,11 +566,11 @@ if (!oneFile)
     carefulClose(&f);
 }
 
-void splitByGap(char *inName, int pieceSize, char *outRoot, int estSize)
+void splitByGap(char *inName, int pieceSize, char *outRoot, long long estSize)
 /* Split up file into pieces at most pieceSize bases long, at gap boundaries 
  * if possible. */
 {
-unsigned long pieces = (estSize + pieceSize-1)/pieceSize;
+unsigned long long pieces = (estSize + pieceSize-1)/pieceSize;
 int digits = digitsBaseTen(pieces);
 int minGapSize = optionInt("minGapSize", 1000);
 boolean noGapDrops = optionExists("noGapDrops");
@@ -686,7 +687,7 @@ printf("%d pieces of %d written\n", writeCount, pieceIx);
 int main(int argc, char *argv[])
 /* Process command line. */
 {
-unsigned long estSize;
+unsigned long long estSize;
 char *how = NULL;
 char *inName = NULL;
 int count;
