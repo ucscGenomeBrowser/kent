efc6a5e2b50b7dd160793c9c0c9c509d48c9dfad
heather <heather>
2005-10-14 04:20:00 +0000

    random position, fixed chrom

--- src/hg/perf/hgTracksRandom.c
+++ src/hg/perf/hgTracksRandom.c
@@ -7,7 +7,7 @@
 #include "portable.h"
 #include "qa.h"
 
-static char const rcsid[] = "$Id: hgTracksRandom.c,v 1.1 2005/10/14 04:01:46 heather Exp $";
+static char const rcsid[] = "$Id: hgTracksRandom.c,v 1.2 2005/10/14 04:20:00 heather Exp $";
 
 
 void usage()
@@ -32,14 +32,32 @@ endTime = clock1000();
 printf("time = %ld\n", endTime - startTime);
 }
 
+int getStartPos(int chromSize, int windowSize)
+{
+if (windowSize >= chromSize)
+    return 0;
+return rand() % (chromSize - windowSize);
+}
+
 int main(int argc, char *argv[])
 {
 struct dyString *dy = NULL;
+int startPos = 1;
+char *chrom = "chr1";
+int chromSize = 245522847;
+int windowSize = 100000;
+
 if (argc != 2)
     usage();
+
+srand( (unsigned) time(NULL) );
+
+startPos = getStartPos(chromSize, windowSize);
+
 dy = newDyString(256);
-dyStringPrintf(dy, "%s?db=hg17&position=chr1:1-100000", argv[1]);
+dyStringPrintf(dy, "%s?db=hg17&position=%s:%d-%d", argv[1], chrom, startPos, startPos + windowSize);
 printf("url = %s\n", dy->string);
+
 hgTracksRandom(dy->string);
 return 0;
 }
