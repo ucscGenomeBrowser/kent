a1905c8167be7c5689d3a6cd84ca313787daf485
baertsch <baertsch>
2005-10-16 23:31:41 +0000

    /* fetch ec codes and descriptions and output html */

---
+++ src/hg/inc/ec.h
@@ -0,0 +1,7 @@
+/* ec - handles reading of EC codes, attributes and descriptions */
+#ifndef EC_H
+#define EC_H
+
+void getEcHtml(char *ecNumber)
+/* fetch ec codes and descriptions and output html */
+#endif EC_H
