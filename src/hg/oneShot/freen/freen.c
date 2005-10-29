f1d4c9a293981f00e91885551a2c13a027831172
kent <kent>
2005-10-29 17:30:15 +0000

    Freen goes to test some print format strings.

--- src/hg/oneShot/freen/freen.c
+++ src/hg/oneShot/freen/freen.c
@@ -3,12 +3,13 @@
 #include "memalloc.h"
 #include "linefile.h"
 #include "hash.h"
+#include "dystring.h"
 #include "obscure.h"
 #include "dnautil.h"
 #include "jksql.h"
 #include "visiGene.h"
 
-static char const rcsid[] = "$Id: freen.c,v 1.57 2005/10/26 20:15:51 kent Exp $";
+static char const rcsid[] = "$Id: freen.c,v 1.58 2005/10/29 17:30:15 kent Exp $";
 
 void usage()
 {
@@ -19,22 +20,11 @@ errAbort("freen - test some hair brained thing.\n"
 void freen(char *fileName)
 /* Test some hair-brained thing. */
 {
-struct sqlConnection *conn = sqlConnect("visiGene");
-struct slInt *gene, *geneList = sqlQuickNumList(conn,
-	"select id from gene limit 10");
-struct slName *list, *el;
-printf("got %d genes\n", slCount(geneList));
-for (gene = geneList; gene != NULL; gene = gene->next)
-    {
-    char *name = vgGeneNameFromId(conn, gene->val);
-    printf("id %d, name %s\n", gene->val, name);
-    freeMem(name);
-    }
-
-list = visiGeneGeneName(conn, 16337);
-for (el = list; el != NULL; el = el->next)
-    printf(" %s", el->name);
-printf("\n");
+struct dyString *dy = dyStringNew(0);
+printf("%.s\n", fileName);
+dyStringPrintf(dy, "%0.4s", fileName);
+puts(dy->string);
+dyStringFree(&dy);
 }
 
 int main(int argc, char *argv[])
