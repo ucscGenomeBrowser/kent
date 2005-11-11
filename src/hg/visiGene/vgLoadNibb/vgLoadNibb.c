1af430fddd5e282a27572035a63d883b40a00966
kent <kent>
2005-11-11 19:57:36 +0000

    Fixing a few small problems in .ra file

--- src/hg/visiGene/vgLoadNibb/vgLoadNibb.c
+++ src/hg/visiGene/vgLoadNibb/vgLoadNibb.c
@@ -10,7 +10,7 @@
 #include "dnaseq.h"
 #include "fa.h"
 
-static char const rcsid[] = "$Id: vgLoadNibb.c,v 1.4 2005/11/11 16:51:05 kent Exp $";
+static char const rcsid[] = "$Id: vgLoadNibb.c,v 1.5 2005/11/11 19:57:36 kent Exp $";
 
 void usage()
 /* Explain usage and exit. */
@@ -86,14 +86,14 @@ fprintf(f, "submitSet nibbXenopusLaevis3\n");
 fprintf(f, "fullDir ../visiGene/full/inSitu/XenopusLaevis/nibb\n");
 fprintf(f, "thumbDir ../visiGene/200/inSitu/XenopusLaevis/nibb\n");
 fprintf(f, "priority 1200\n");
-fprintf(f, "sliceType wholeMount\n");
+fprintf(f, "sliceType whole mount\n");
 fprintf(f, "submissionSource Japanese National Institute of Basic Biology (NIBB) XDB project\n");
 fprintf(f, "taxon 8355\n");
 fprintf(f, "genotype wild type\n");
 fprintf(f, "acknowledgement Thanks to Naoto Ueno and colleagues at NIBB for helping make these images available in VisiGene\n");
 
 /* Still need to fill in contributor, publication, journal, journalUrl, itemUrl */
-fprintf(f, "contributor Ueno N.,Kitayama A.,Terasaka C.,Nomoto K.,Shibamoto K.,Hiroyo N.\n");
+fprintf(f, "contributor Ueno N., Kitayama A., Terasaka C., Nomoto K., Shibamoto K., Hiroyo N.\n");
 fprintf(f, "year 2005\n");
 fprintf(f, "setUrl http://xenopus.nibb.ac.jp\n");
 fprintf(f, "probeColor purple\n");
