f5144e3b32d07ca3856e9dae06b9f0a32a012250
donnak <donnak>
2005-11-14 17:51:11 +0000

    Added doHelp(), which prints an About VisiGene help section on the hgVisiGene gateway page. Also modified the text printed under the search box on that page.

--- src/hg/visiGene/hgVisiGene/hgVisiGene.c
+++ src/hg/visiGene/hgVisiGene/hgVisiGene.c
@@ -450,6 +450,83 @@ printf("</frameset>\n");
 printf("</HTML>\n");
 }
 
+void doHelp()
+/* Print help section */
+{
+puts(
+"<P> \n"
+"VisiGene is a browser for viewing <em>in situ</em> images. \n"
+"It enables the user to examine cell-by-cell as well as tissue-by-tissue \n"
+"expression patterns. The browser serves as a virtual microscope, allowing \n" 
+"users to retrieve images that meet specific search criteria, then \n"
+"interactively zoom and scroll across the collection.</P>\n"
+"<H3>Images Available</H3>\n"
+"<P> \n"
+"The following image collections are currently available for browsing: \n"
+"<UL> \n"
+"<LI>Mouse <em>in situ</em> images from the \n"
+"<A HREF=\"http://www.informatics.jax.org/menus/expression_menu.shtml\" \n"
+"TARGET=_blank>Jackson Labs Gene Expression Database</A> (GED) \n" 
+"<LI>Transcription factors in mouse embryos from the \n"
+"<A HREF=\"http://mahoney.chip.org/mahoney/\" TARGET=_blank>Mahoney Center \n"
+"for Neuro-Oncology</A> \n"
+"<LI><em>Xenopus laevis</em> <em>in situ</em> images from the \n"
+"<A HREF=\"http://www.nibb.ac.jp/en/index.php\" TARGET=_blank>National \n"
+"Institute for Basic Biology</A> (NIBB) XDB project \n"
+"</UL> \n"
+"<H3>Searching the Image Database</H3>\n"
+"<P> \n"
+"The image database may be searched by organism names, gene names, authors, \n"
+"body parts, year of publication, GenBank or UniProt accessions, \n"
+"<A HREF=\"http://genex.hgu.mrc.ac.uk/Atlas/intro.html\" \n"
+"TARGET=_blank>Theiler stages</A>, and \n"
+"<A HREF=\"http://www.xenbase.org/atlas/NF/NF-all.html\" \n"
+"TARGET=_blank>Nieuwkoop/Faber stages</A>. \n"
+"The search returns only those images that match all the specified criteria. \n"
+"<P> \n"
+"The wildcard characters * and ? are supported for gene name searches. \n"
+"For example, to view the images of all genes in the Hox A cluster, search \n"
+"for <em>hoxa*</em>. \n"  
+"When searching on author names that include initials, \n"
+"use the format <em>Smith AJ</em>. </P>\n"
+"<H3>Image Navigation</H3>\n"
+"<P> \n"
+"Following a successful search, VisiGene displays a list of thumbnails of \n"
+"images matching the search criteria \n"
+"in the lefthand pane of the browser. By default, the image corresponding to \n"
+"the first thumbnail in the list is displayed in the main image pane. \n"
+"If more than 50 images meet the search criteria, links at the bottom of \n"
+"the thumbnail pane allow the user to toggle among pages of search results. \n"
+"To display a different image in the main browser pane, \n"
+"click the thumbnail of the image you wish to view. </P>\n"
+"<P> \n"
+"By default, an image is displayed at a resolution that provides \n"
+"optimal viewing of the overall image. This size varies among images. \n"
+"The image may be zoomed in or out, and the viewing area may be moved or \n"
+"scrolled in any direction to focus on areas of interest. \n"
+"<P> \n"
+"<B>Zooming in:</B> Set the Zoom option above the image to <em>in</em>, \n"
+"then click on the image. Each click repetition enlarges the image 2X and \n"
+"centers the image on the location of the mouse click. Keyboard shortcut: \n"
+"Click on the image, then use the <em>+</em> key.</P>\n"
+"<P> \n"
+"<B>Zooming out:</B> Set the Zoom option above the image to <em>out</em>, \n"
+"then click on the image. Each click repetition reduces the image 2X and \n"
+"centers the image on the location of the mouse click.\n"
+"Click on the image, then use the <em>-</em> key. </P> \n"
+"<P> \n"
+"<B>Moving the image:</B> To move the image viewing area in any direction, \n"
+"click and drag the image using the mouse. Alternatively, the following \n"
+"keyboard shortcuts may be used after clicking on the image: \n"
+"<UL>"
+"<LI><B>Scroll left in the image: </B>Left-arrow key or <em>Home</em> key \n"
+"<LI><B>Scroll right in the image: </B>Right-arrow key or <em>End</em> key \n"
+"<LI><B>Scroll up in the image: </B>Up-arrow key or <em>PgUp</em> key \n"
+"<LI><B>Scroll down in the image: </B>Down-arrow key or <em>PgDn</em> key \n"
+"</UL></P> \n"
+);
+}
+
 void doInitialPage()
 /* Put up page with search box that explains program and
  * some good things to search on. */
@@ -463,15 +540,19 @@ cgiMakeTextVar(hgpListSpec, listSpec, 30);
 cgiMakeButton(hgpDoSearch, "search");
 printf("<BR>\n");
 puts(
-"You can search on gene names, authors, body parts, year of publication, "
-"GenBank or UniProt accessions, and Theiler stages.  "
-"For gene names you can include * and ? wildcard characters.  For instance to "
-"see images of all genes in the Hox A cluster search for hoxa*.  When searching on "
-"author names you can include the initials after the last name as in \"Smith AJ\".");
+"<P> \n"
+"Valid search terms include organism names, gene names, authors, body parts, \n"
+"year of publication, GenBank or UniProt accessions, Theiler stages, and \n"
+"Nieuwkoop/Faber stages. \n"
+"See &quot;About VisiGene&quot; below for more \n"
+"information about the VisiGene browser, search options, and image \n"
+"navigation. </P> \n" 
+);
 printf("</FORM>\n");
+webNewSection("About VisiGene");
+doHelp();
 webEnd();
 }
-
 
 void doDefault(struct sqlConnection *conn, boolean newSearch)
 /* Put up default page - if there is no specific do variable. */
