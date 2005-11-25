10ac533783e46bc99a6e8c27fce96328d8377cc7
kent <kent>
2005-11-25 05:44:50 +0000

    Adding in .ra file output.  Cleaning up a few things in tab file.

--- src/hg/visiGene/gensat/vgLoadGensat/vgLoadGensat.c
+++ src/hg/visiGene/gensat/vgLoadGensat/vgLoadGensat.c
@@ -3,11 +3,12 @@
 #include "linefile.h"
 #include "hash.h"
 #include "options.h"
+#include "portable.h"
 #include "xp.h"
 #include "xap.h"
 #include "../lib/gs.h"
 
-static char const rcsid[] = "$Id: vgLoadGensat.c,v 1.4 2005/11/21 17:38:46 kent Exp $";
+static char const rcsid[] = "$Id: vgLoadGensat.c,v 1.5 2005/11/25 05:44:50 kent Exp $";
 
 void usage()
 /* Explain usage and exit. */
@@ -48,15 +49,30 @@ if (sameString(s, "not-done"))
 return s;
 }
 
-
-void vgLoadGensat(char *gensatXml, char *outTab)
-/* vgLoadGensat - Parse gensat XML file and turn it into VisiGene load files. */
+void makeTabFile(char *gensatXml, char *tabFile)
+/* Parse XML file and turn it into tab-separated file. */
 {
 struct xap *xap = xapListOpen(gensatXml, "GensatImageSet",
     gsStartHandler, gsEndHandler);
 struct gsGensatImage *image;
-FILE *f = mustOpen(outTab, "w");
+struct hash *tabFileHash = hashNew(0);
+FILE *f = mustOpen(tabFile, "w");
 int i=0;
+char *headDir = "Institutions/";
+int headDirLen = strlen(headDir);
+
+fprintf(f, "#");
+fprintf(f, "submitId\t");
+fprintf(f, "gene\t");
+fprintf(f, "locusLink\t");
+fprintf(f, "genbank\t");
+fprintf(f, "bac\t");
+fprintf(f, "sex\t");
+fprintf(f, "age\t");
+fprintf(f, "sliceType\t");
+fprintf(f, "imageWidth\t");
+fprintf(f, "imageHeight\t");
+fprintf(f, "fileName\n");
 
 while ((image = xapListNext(xap, "GensatImage")) != NULL)
     {
@@ -70,11 +86,18 @@ while ((image = xapListNext(xap, "GensatImage")) != NULL)
     int id = image->gsGensatImageId->text;	/** Single instance required. **/
     char *symbol = "";
     char *name = "";
-    int geneId = 0;
+    int locusLinkId = 0;
     char *acc = "";
-    char *bac = NULL;
+    char *rawAge = NULL;
+    double age = 0;
+    char *sex = "";
+    char *bac = "";
+    int daysToBirth = 19;
+    int daysToAdult = 61;
+    int averageAdult = 100;
+
     struct gsGensatImageSeqInfo *gsGensatImageSeqInfo;	/** Single instance required. **/
-    char *section = image->gsGensatImageSectionPlane->value;	/** Single instance required. **/
+    char *sliceType = image->gsGensatImageSectionPlane->value;	/** Single instance required. **/
     char *level = image->gsGensatImageSectionLevel->text;	/** Single instance required. **/
 
     if (image->gsGensatImageGeneInfo->gsGensatGeneInfo->gsGensatGeneInfoGeneSymbol != NULL)
@@ -82,20 +105,49 @@ while ((image = xapListNext(xap, "GensatImage")) != NULL)
     if (image->gsGensatImageGeneInfo->gsGensatGeneInfo->gsGensatGeneInfoGeneName != NULL)
 	name = image->gsGensatImageGeneInfo->gsGensatGeneInfo->gsGensatGeneInfoGeneName->text;	
     if (image->gsGensatImageGeneInfo->gsGensatGeneInfo->gsGensatGeneInfoGeneId != NULL)
-	geneId = image->gsGensatImageGeneInfo->gsGensatGeneInfo->gsGensatGeneInfoGeneId->text;	
+	locusLinkId = image->gsGensatImageGeneInfo->gsGensatGeneInfo->gsGensatGeneInfoGeneId->text;	
     if (image->gsGensatImageGeneInfo->gsGensatGeneInfo->gsGensatGeneInfoBacAddress != NULL)
         bac = image->gsGensatImageGeneInfo->gsGensatGeneInfo->gsGensatGeneInfoBacAddress->text;
     if (image->gsGensatImageSeqInfo->gsGensatSequenceInfo->gsGensatSequenceInfoAccession != NULL)
 	acc = image->gsGensatImageSeqInfo->gsGensatSequenceInfo->gsGensatSequenceInfoAccession->text;	
+    if (image->gsGensatImageAge != NULL)
+        rawAge = image->gsGensatImageAge->value;
+    if (image->gsGensatImageSex != NULL)
+        sex = image->gsGensatImageSex->value;
+    if (image->gsGensatImageGeneInfo->gsGensatGeneInfo->gsGensatGeneInfoGeneSymbol != NULL)
+	symbol = image->gsGensatImageGeneInfo->gsGensatGeneInfo->gsGensatGeneInfoGeneSymbol->text;	
+
+    /* Process a few fields. */
+    tolowers(sex);
+    if (sameString(rawAge, "adult"))
+        age = averageAdult;
+    else 
+	{
+	if (!isdigit(rawAge[1]))
+	    errAbort("Don't understand age %s", rawAge);
+	if (rawAge[0] == 'p')
+	    age = daysToBirth + atof(rawAge+1);
+	else if (rawAge[0] == 'e')
+	    age = atof(rawAge+1);
+	else
+	    errAbort("Don't understand age %s", rawAge);
+	}
+    if (!startsWith(headDir, fileName))
+        errAbort("Expecting %s to begin with %s", fileName, headDir);
+    fileName += headDirLen;
+
     /* Print out fields */
     fprintf(f, "%d\t", id);
     fprintf(f, "%s\t", symbol);
-    // fprintf(f, "%s\t", name);
-    fprintf(f, "%d\t", geneId);
+    fprintf(f, "%d\t", locusLinkId);
     fprintf(f, "%s\t", acc);
     fprintf(f, "%s\t", bac);
-    fprintf(f, "%s\t", section);
-    fprintf(f, "%s\t", level);
+    fprintf(f, "%s\t", sex);
+    if (rawAge != NULL)
+	fprintf(f, "%f\t", age);
+    else
+        fprintf(f, "\t");
+    fprintf(f, "%s\t", sliceType);
     fprintf(f, "%d\t", width);
     fprintf(f, "%d\t", height);
     fprintf(f, "%s\n", fileName);
@@ -129,6 +181,43 @@ while ((image = xapListNext(xap, "GensatImage")) != NULL)
     }
 xapFree(&xap);
 carefulClose(&f);
+}
+
+void makeRaFile(char *fileName)
+/* Make up ra file - that describes whole gensat set. */
+{
+FILE *f = mustOpen(fileName, "w");
+
+fprintf(f, "submissionSet gensat1\n");
+fprintf(f, "fullDir ../visiGene/full/inSitu/Mouse/gensat\n");
+fprintf(f, "thumbDir ../visiGene/200/inSitu/Mouse/gensat\n");
+fprintf(f, "priority 2000\n");
+fprintf(f, "submissionSource gensat\n");
+fprintf(f, "acknowledgement Thanks to Michael Dicuccio at NCBI for helping "
+	   "load these images into VisiGene.\n");
+fprintf(f, "contributor Heintz N., Curran T., Hatten M., Magdaleno S., Jensen P., Gong S., Mehta S., Wang C., Concepcion A., Kowalic E., Losos K., Feng H., Thompson T., Ford B., Baker S., Doughty M., Dinzey J., Dyer A., Grevstad C., Hinkle A., Kizima L., Madden C., Pariser E., Zheng C., Kus L., Milosevic A., Didkovsky N., Nowak N., Joyner A., Lehman K., Cheung T., Asbury A., Eden C., Batten D.,\n");
+fprintf(f, "publication A gene expression atlas of the central nervous system based on bacterial artificial chromosomes\n");
+fprintf(f, "journal Nature\n");
+fprintf(f, "journalUrl http://www.nature.com\n");
+fprintf(f, "year 2003\n");
+fprintf(f, "setUrl http://www.ncbi.nlm.nih.gov/projects/gensat/\n");
+fprintf(f, "itemUrl http://www.ncbi.nlm.nih.gov/entrez/query.fcgi?CMD=search&DB=gensat&term=%%s\n");
+
+
+carefulClose(&f);
+}
+
+
+void vgLoadGensat(char *gensatXml, char *outDir)
+/* vgLoadGensat - Parse gensat XML file and turn it into VisiGene load files. */
+{
+char tabFile[PATH_LEN], raFile[PATH_LEN];;
+
+makeDir(outDir);
+safef(tabFile, sizeof(tabFile), "%s/gensat.tab", outDir);
+safef(raFile, sizeof(raFile), "%s/gensat.ra", outDir);
+makeRaFile(raFile);
+makeTabFile(gensatXml, tabFile);
 }
 
 int main(int argc, char *argv[])
