344ca3f755f5562e664a61f628b7d9891689752b
kent <kent>
2005-11-26 17:21:50 +0000

    Changed fixer to do missing symbolic links.

--- src/hg/visiGene/gensat/gensatFixFull/gensatFixFull.c
+++ src/hg/visiGene/gensat/gensatFixFull/gensatFixFull.c
@@ -1,25 +1,26 @@
-/* gensatFixFull - Fix a problem that arose from image magic convert not handling .full in filename during tiling.  Should be a one-time fix.. */
+/* gensatFixFull - Fix a problem that arose from image magic convert not handling .full in filename 
+ * during tiling.  Should be a one-time fix.. */
 #include "common.h"
 #include "linefile.h"
 #include "hash.h"
 #include "options.h"
 #include "portable.h"
 
-static char const rcsid[] = "$Id: gensatFixFull.c,v 1.1 2005/11/26 17:05:49 kent Exp $";
+static char const rcsid[] = "$Id: gensatFixFull.c,v 1.2 2005/11/26 17:21:50 kent Exp $";
 
 char *database = "visiGene";
 char *thumbDir = "/cluster/store11/visiGene/gbdb/200/inSitu/Mouse/gensat";
 char *fullDir = "/cluster/store11/visiGene/gbdb/full/inSitu/Mouse/gensat";
 char *downloadDir = "/cluster/store11/visiGene/offline/gensat/Institutions";
-char *stripper = ".full";
 
 void usage()
 /* Explain usage and exit. */
 {
 errAbort(
-  "gensatFixFull - Fix a problem that arose from image magic convert not handling .full in filename during tiling.  Should be a one-time fix.\n"
+  "gensatFixFull - Fix a problem that arose from image magic convert not handling .full "
+  "in filename during tiling.  Should be a one-time fix.\n"
   "usage:\n"
-  "   gensatFixFull out.sh out.sql\n"
+  "   gensatFixFull now\n"
   "options:\n"
   "   -xxx=XXX\n"
   );
@@ -29,84 +30,47 @@ static struct optionSpec options[] = {
    {NULL, 0},
 };
 
-void fixDir(FILE *f, char *dir)
+void fixDir(char *dir)
 /* Fix file names in directory and any subdirectories. */
 {
 struct fileInfo *el, *list = listDirX(dir, "*", TRUE);
-int dotEvery = 10;
-int dot = 0;
-int lineEvery = 50;
-int line = 0;
 
 /* Recurse on dirs (better to do this first before they are
  * renamed). */
 for (el = list; el != NULL; el = el->next)
     {
     if (el->isDir)
-        fixDir(f, el->name);
+        fixDir(el->name);
     }
 
-/* Do the renaming. */
+/* Do the linking. */
 for (el = list; el != NULL; el = el->next)
     {
-    char *source = el->name;
-    char dir[PATH_LEN], file[PATH_LEN], ext[PATH_LEN];
-    splitPath(source, dir, file,ext);
-    if (stringIn(stripper, file) || stringIn(stripper, ext))
+    char *commonTail;
+    char linkName[PATH_LEN];
+    if (endsWith(el->name, ".jpg"))
         {
-	char dest[PATH_LEN];
-	stripString(file, stripper);
-	stripString(ext, stripper);
-	safef(dest, sizeof(dest), "%s%s%s", dir, file, ext);
-	rename(source, dest);
-	fprintf(f, "mv %s %s\n", source, dest);
+	commonTail = el->name + strlen(downloadDir);
+	safef(linkName, sizeof(linkName), "%s%s", fullDir, commonTail);
+	uglyf("ln -s %s %s\n", el->name, linkName);
 	}
-    else
-        fprintf(f, "not moving %s\n", source);
     }
 slFreeList(&list);
 }
 
-void fixFiles(char *shellScript)
-/* Get rid of .full in file names in both thumb and full dirs. */
-{
-FILE *f = mustOpen(shellScript, "w");
-printf("#Fixing thumbnails\n");
-fixDir(f, thumbDir);
-printf("\n");
-printf("#Fixing full\n");
-fixDir(f, fullDir);
-printf("\n");
-printf("#Fixing download\n");
-fixDir(f, downloadDir);
-printf("\n");
-carefulClose(&f);
-}
-
-void fixDb(char *sqlScript)
-/* Get rid of .full in image file names. */
-{
-FILE *f = mustOpen(sqlScript, "w");
-
-fprintf(f, "update imageFile set fileName=REPLACE(fileName, '.full', '') where submitId=707;\n");
-
-carefulClose(&f);
-}
-
-void gensatFixFull(char *outSh, char *outSql)
+void gensatFixFull(char *logFile)
 /* gensatFixFull - Fix a problem that arose from image magic convert not 
  * handling .full in filename during tiling.  Should be a one-time fix.. */
 {
-fixFiles(outSh);
-fixDb(outSql);
+fixDir(downloadDir);
 }
 
 int main(int argc, char *argv[])
 /* Process command line. */
 {
 optionInit(&argc, argv, options);
-if (argc != 3)
+if (argc != 2)
     usage();
-gensatFixFull(argv[1], argv[2]);
+gensatFixFull(argv[1]);
 return 0;
 }
