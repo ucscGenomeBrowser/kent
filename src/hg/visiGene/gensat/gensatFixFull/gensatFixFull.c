37b46385d86275919ee6b8207de2e4fb7f5dcf90
kent <kent>
2005-11-26 22:01:35 +0000

    Patching missing captions.

--- src/hg/visiGene/gensat/gensatFixFull/gensatFixFull.c
+++ src/hg/visiGene/gensat/gensatFixFull/gensatFixFull.c
@@ -5,13 +5,16 @@
 #include "hash.h"
 #include "options.h"
 #include "portable.h"
+#include "jksql.h"
+#include "dystring.h"
 
-static char const rcsid[] = "$Id: gensatFixFull.c,v 1.2 2005/11/26 17:21:50 kent Exp $";
+static char const rcsid[] = "$Id: gensatFixFull.c,v 1.3 2005/11/26 22:01:35 kent Exp $";
 
 char *database = "visiGene";
 char *thumbDir = "/cluster/store11/visiGene/gbdb/200/inSitu/Mouse/gensat";
 char *fullDir = "/cluster/store11/visiGene/gbdb/full/inSitu/Mouse/gensat";
 char *downloadDir = "/cluster/store11/visiGene/offline/gensat/Institutions";
+int gensatId = 706;
 
 void usage()
 /* Explain usage and exit. */
@@ -20,7 +23,7 @@ errAbort(
   "gensatFixFull - Fix a problem that arose from image magic convert not handling .full "
   "in filename during tiling.  Should be a one-time fix.\n"
   "usage:\n"
-  "   gensatFixFull now\n"
+  "   gensatFixFull caption.txt\n"
   "options:\n"
   "   -xxx=XXX\n"
   );
@@ -30,39 +33,40 @@ static struct optionSpec options[] = {
    {NULL, 0},
 };
 
-void fixDir(char *dir)
-/* Fix file names in directory and any subdirectories. */
+
+void gensatFixFull(char *captionFile)
+/* Fix missing captions. */
 {
-struct fileInfo *el, *list = listDirX(dir, "*", TRUE);
-
-/* Recurse on dirs (better to do this first before they are
- * renamed). */
-for (el = list; el != NULL; el = el->next)
+struct lineFile *lf = lineFileOpen(captionFile, TRUE);
+char *row[2];
+struct dyString *sql = dyStringNew(0);
+struct sqlConnection *conn = sqlConnect(database);
+struct hash *capHash = newHash(16);
+while (lineFileRowTab(lf, row))
     {
-    if (el->isDir)
-        fixDir(el->name);
-    }
-
-/* Do the linking. */
-for (el = list; el != NULL; el = el->next)
-    {
-    char *commonTail;
-    char linkName[PATH_LEN];
-    if (endsWith(el->name, ".jpg"))
+    int captionId;
+    char *submitId = row[0];
+    char *caption = row[1];
+    captionId = hashIntValDefault(capHash, caption, 0);
+    if (captionId == 0)
         {
-	commonTail = el->name + strlen(downloadDir);
-	safef(linkName, sizeof(linkName), "%s%s", fullDir, commonTail);
-	uglyf("ln -s %s %s\n", el->name, linkName);
+	dyStringClear(sql);
+	dyStringAppend(sql, "insert into caption values(default, \"");
+	dyStringAppend(sql, caption);
+	dyStringAppend(sql, "\")");
+	sqlUpdate(conn, sql->string);
+	verbose(1, "%s\n", sql->string);
+	captionId = sqlLastAutoId(conn);
+	hashAddInt(capHash, caption, captionId);
 	}
+    dyStringClear(sql);
+    dyStringPrintf(sql, "update imageFile set caption=%d ", captionId);
+    dyStringPrintf(sql, "where submissionSet=%d ", gensatId);
+    dyStringPrintf(sql, "and submitId = \"%s\"", submitId);
+    sqlUpdate(conn, sql->string);
+    verbose(1, "%s\n", sql->string);
     }
-slFreeList(&list);
-}
-
-void gensatFixFull(char *logFile)
-/* gensatFixFull - Fix a problem that arose from image magic convert not 
- * handling .full in filename during tiling.  Should be a one-time fix.. */
-{
-fixDir(downloadDir);
+dyStringFree(&sql);
 }
 
 int main(int argc, char *argv[])
