4156077b288168a7dc2e3e1946e63bf0891b38c1
kent <kent>
2005-11-28 04:42:10 +0000

    Simplifying command line.  First draft of doc file.

--- src/hg/sqlToXml/sqlToXml.c
+++ src/hg/sqlToXml/sqlToXml.c
@@ -9,22 +9,39 @@
 #include "jksql.h"
 #include "asParse.h"
 
-static char const rcsid[] = "$Id: sqlToXml.c,v 1.1 2005/11/27 11:47:39 kent Exp $";
+static char const rcsid[] = "$Id: sqlToXml.c,v 1.2 2005/11/28 04:42:10 kent Exp $";
 
 void usage()
 /* Explain usage and exit. */
 {
 errAbort(
-  "sqlToXml - Given a database, .as file, .dumpSpec file, and a sql select\n"
-  "statement, dump out results as XML.\n"
+  "sqlToXml - dump out all or part of a relational database to XML, guided\n"
+  "by a dump specification.  See sqlToXml.doc for additional information.\n"
   "usage:\n"
-  "   sqlToXml database table 'query' db.as xml.dumpSpec output.xml\n"
+  "   sqlToXml database asFile dumpSpec output.xml\n"
   "options:\n"
-  "   -xxx=XXX\n"
+  "   -topTag=name - Give the top level XML tag the given name.  By\n"
+  "               default it will be the same as the database name.\n"
+  "   -query=file.sql - Instead of dumping whole database, just dump those\n"
+  "                  records matching SQL select statement in file.sql.\n"
+  "                  This statement should be of the form:\n"
+  "           select * from table where ...\n"
+  "                   or\n"
+  "           select table.* from table,otherTables where ...\n"
+  "                   Where the table is the same as the table in the first\n"
+  "                   line of dumpSpec.\n"
+  "   -tab=N - number of spaces betweeen tabs in xml.dumpSpec - by default it's 8.\n"
+  "            (It may be best just to avoid tabs in that file though.)\n"
   );
 }
 
+int tabSize = 8;
+char *topTag = NULL;
+
 static struct optionSpec options[] = {
+   {"topTag", OPTION_STRING},
+   {"query", OPTION_STRING},
+   {"tab", OPTION_INT},
    {NULL, 0},
 };
 
@@ -35,7 +52,8 @@ struct specTree
    {
    struct specTree *next;	/* Sibling */
    struct specTree *children;
-   char *field;		/* Field in table to link on */
+   char *field;		/* Field in table to link on. 
+                         * At top level is master table name instead.  */
    char *target;	/* Target table.field  */
    };
 
@@ -49,6 +67,28 @@ for (tree = parent->children; tree != NULL; tree = tree->next)
 return tree;
 }
 
+int countIndent(char *s, int size)
+/* Count the number of spaces and tabs. */
+{
+int count = 0;
+int i;
+char c;
+for (i=0; i<size; ++i)
+    {
+    c = s[i];
+    if (c == ' ')
+        count += 1;
+    else if (c == '\t')
+        {
+	int nextTab = (count + tabSize)/tabSize * tabSize;
+	count = nextTab;
+	}
+    else
+        internalErr();
+    }
+return count;
+}
+
 void rSpecTreeLoad(struct lineFile *lf, struct specTree *parent)
 /* Load all children of parent. */
 {
@@ -58,14 +98,14 @@ struct specTree *branch = NULL;
 if (!lineFileNextReal(lf, &line))
     return;
 pastSpace = skipLeadingSpaces(line);
-curIndent = pastSpace - line;
+curIndent = countIndent(line, pastSpace - line);
 lineFileReuse(lf);
 for (;;)
     {
     if (!lineFileNextReal(lf, &line))
         break;
     pastSpace = skipLeadingSpaces(line);
-    indent = pastSpace - line;
+    indent = countIndent(line, pastSpace - line);
     if (indent > curIndent)
 	{
 	lineFileReuse(lf);
@@ -104,7 +144,7 @@ AllocVar(root);
 if (!lineFileNextReal(lf, &line))
     lineFileUnexpectedEnd(lf);
 line = trimSpaces(line);
-verbose(1, "%s starts with %s\n", fileName, line);
+root->field = cloneString(line);
 rSpecTreeLoad(lf, root);
 
 lineFileClose(&lf);
@@ -154,14 +194,14 @@ while ((row = sqlNextRow(sr)) != NULL)
         fprintf(f, "/>\n");
     else
         {
+	struct specTree *branch;
 	fprintf(f, ">\n");
 	rowIx = 0;
-	for (col = as->columnList; col != NULL; col = col->next, rowIx+=1)
+	for (branch = tree->children; branch != NULL; branch = branch->next)
 	    {
-	    struct specTree *branch = specTreeTarget(tree, col->name);
-	    if (branch != NULL)
-	        {
-		char *target = branch->target;
+	    char *target = branch->target;
+	    if (!sameString(target, "hide"))
+		{
 		char targetTable[256];
 		char query[512];
 		char *targetField = strchr(target, '.');
@@ -193,8 +233,8 @@ for (as = asList; as != NULL; as = as->next)
 return hash;
 }
 
-void sqlToXml(char *database, char *table, char *query, char *asFile, 
-	char *dumpSpec, char *outputXml)
+
+void sqlToXml(char *database, char *asFile, char *dumpSpec, char *outputXml)
 /* sqlToXml - Given a database, .as file, .joiner file, and a sql select 
  * statement, dump out results as XML. */
 {
@@ -203,18 +243,36 @@ struct sqlConnection *conn = sqlAllocConnection(cc);
 struct specTree *tree = specTreeLoad(dumpSpec);
 struct hash *asHash = asHashFile(asFile);
 FILE *f = mustOpen(outputXml, "w");
+char *topTag = optionVal("topTag", database);
+char *table = tree->field;	/* Top level tree stores table in field! */
+char queryBuf[512], *query = NULL;
+
+if (optionExists("query"))
+    {
+    char *queryFile = optionVal("query", NULL);
+    readInGulp(queryFile, &query, NULL);
+    if (!stringIn(table, query))
+	errAbort("No mention of table %s in %s.", table, queryFile);
+    }
+else
+    {
+    safef(queryBuf, sizeof(queryBuf),
+        "select * from %s", table);
+    query = queryBuf;
+    }
+
 
 if (!sqlTableExists(conn, table))
     errAbort("No table %s in %s", table, database);
-if (!stringIn(table, query))
-    errAbort("No mention of table %s in query.", table);
 sqlFreeConnection(cc, &conn);
 
 verbose(1, "%d tables in %s\n",
 	asHash->elCount,  asFile);
 
 escaper = dyStringNew(0);
-rSqlToXml(cc, table, "", query, asHash, tree, f, 0);
+fprintf(f, "<%s>\n", topTag);
+rSqlToXml(cc, table, "", query, asHash, tree, f, 1);
+fprintf(f, "</%s>\n", topTag);
 carefulClose(&f);
 }
 
@@ -222,8 +280,9 @@ int main(int argc, char *argv[])
 /* Process command line. */
 {
 optionInit(&argc, argv, options);
-if (argc != 7)
+if (argc != 5)
     usage();
-sqlToXml(argv[1], argv[2], argv[3], argv[4], argv[5], argv[6]);
+tabSize = optionInt("tab", tabSize);
+sqlToXml(argv[1], argv[2], argv[3], argv[4]);
 return 0;
 }
