739311192a8433ea5b9ebadfba642168b73e72c0
braney <braney>
2006-01-09 18:22:19 +0000

    some new routines used by the element tree construction

--- src/inc/phyloTree.h
+++ src/inc/phyloTree.h
@@ -10,10 +10,13 @@ struct phyloTree
 {
     struct phyloTree *next;		/* next in single linked list */
     struct phyloTree *parent;		/* the parent of this node */
-    struct phyloTree *left;		/* the left child of this node */
-    struct phyloTree *right;		/* the right child of this node */
+    struct phyloTree **edges;
     struct phyloName *ident;		/* the name and branch length */
     struct phyloTree *mark;		/* probably the favorite child */
+    int numEdges;
+    int allocedEdges;
+
+    void *priv;
 };
 
 extern struct phyloTree *phyloReadTree(struct lineFile *lf);
@@ -23,6 +26,9 @@ extern struct phyloTree *phyloParseString(char *string);
 /* build a phyloTree from a string */
 
 extern void phyloPrintTree( struct phyloTree *,FILE *f);
+/* print a phyloTree to f in Newick format */
+
+extern void phyloDebugTree( struct phyloTree *,FILE *f);
 /* print a phyloTree to f */
 
 extern char *phyloFindPath(struct phyloTree *tree, char *ref, char *cross);
@@ -31,3 +37,24 @@ extern char *phyloFindPath(struct phyloTree *tree, char *ref, char *cross);
 
 extern char *phyloNodeNames(struct phyloTree *tree);
 /* return list of all the node names separated by spaces */
+
+extern struct phyloTree *phyloFindName( struct phyloTree *tree,char *name );
+/* find the node with this name */
+
+extern struct phyloTree *phyloReRoot(struct phyloTree *inTree);
+/* return a tree whose root is inTree and what were parents are now "right" children */
+
+extern void phyloDeleteEdge(struct phyloTree *tree, struct phyloTree *edge);
+/* delete an edge from a node.  Aborts on error */
+
+extern struct phyloTree *phyloAddEdge(struct phyloTree *parent, struct phyloTree *child);
+/* add an edge to a phyloTree node */
+
+extern void phyloClearTreeMarks(struct phyloTree *tree);
+/* clear the favorite child marks */
+
+extern struct phyloTree *phyloFindMarkUpTree(struct phyloTree *tree);
+/* find a marked node somewhere above this node */
+
+extern void phyloMarkUpTree(struct phyloTree *tree);
+/* mark all the nodes from this one up to the top of the tree */
