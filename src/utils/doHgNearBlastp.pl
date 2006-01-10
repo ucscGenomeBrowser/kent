7b56ddab8ec17f6df72dab44b1058f9424c91e82
angie <angie>
2006-01-10 00:43:24 +0000

    First cut at script that automates the protein blast runs that build *BlastTab tables for hgNear.  It works from a simple config.ra file that defines the databases and paths to use for the run.  Still needs more testing before it's ready for group consumption (currently testing on dm2).

---
+++ src/utils/doHgNearBlastp.pl
@@ -0,0 +1,369 @@
+#!/usr/bin/env perl
+
+# DO NOT EDIT the /cluster/bin/scripts copy of this file -- 
+# edit ~/kent/src/utils/doHgNearBlastp.pl instead.
+
+# $Id: doHgNearBlastp.pl,v 1.1 2006/01/10 00:43:24 angie Exp $
+
+use Getopt::Long;
+use warnings;
+use strict;
+
+# Hardcoded paths/command sequences:
+my $blastpath = '/san/sanvol1/scratch/blast64/blast-2.2.11';
+my $blastpParams = '-e 0.01 -m 8 -b'; # Keep -b at end -- value provided below.
+my $tSplitCount = 2000;
+
+# Option variable names:
+use vars qw/
+    $opt_clusterHub
+    $opt_distrHost
+    $opt_dbHost
+    $opt_debug
+    $opt_verbose
+    $opt_help
+    /;
+
+# Option defaults:
+my $clusterHub = 'pk';
+my $distrHost = 'pk';
+my $dbHost = 'hgwdev';
+my $defaultVerbose = 1;
+
+sub usage {
+  # Usage / help / self-documentation:
+  my ($status, $detailed) = @_;
+  my $base = $0;
+  $base =~ s/^(.*\/)?//;
+  print STDERR "
+usage: $base config.ra
+options:
+    -clusterHub mach	Parasol hub for blastp cluster runs.  Default: $clusterHub
+    -distrHost mach	Host which distributes files to the cluster-attached
+			fast storage for the blastp cluster runs.
+    -dbHost mach	Mysql server into which we load *BlastTab tables.
+    -debug		Don't actually run commands, just display them.
+    -verbose num	Set verbose level to num (default $defaultVerbose).
+    -help		Show detailed help (config.ra variables) and exit.
+Automates the protein blast runs for hgNear and loads the *BlastTab tables.
+config.ra specifies one target db and multiple query dbs.  Self-blastp for
+the target and pairwise blastp for the target vs. all queries (and all
+queries vs. the target) are performed.
+To see detailed information about what should appear in config.ra,
+run \"$base -help\".
+";
+  # Detailed help (-help):
+  print STDERR "
+config.ra must define the following settings:
+
+targetGenesetPrefix xxx
+  - xxx is the prefix used to name the self *BlastTab table.
+
+targetDb xxx
+  - xxx is the target database (the one that we just got new genes for)
+
+queryDbs xxx yyy zzz ...
+  - space-separated list of query databases (the other hgNear dbs)
+
+xxxFa fff
+yyyFa ggg
+...
+  - for each db in queryDbs, define a *Fa variable with the complete path
+    of a single fasta file containing protein sequence for genes.
+
+buildDir xxx
+  - Working directory (usually /cluster/data/targetDb/bed/hgNearBlastp)
+    in which scriptlets and results will be stored.
+
+scratchDir xxx
+  - Cluster-attached fast storage (usually /san/sanvol1/scratch/... or
+    /panasas/store/...) where fasta will be split/formatted and used during
+    cluster blastp runs.
+
+" if ($detailed);
+  exit $status;
+} # usage
+
+# Globals:
+my $CONFIG;
+
+sub checkOptions {
+  # Make sure command line options are valid/supported.
+  my $ok = GetOptions("clusterHub=s",
+		      "distrHost=s",
+		      "dbHost=s",
+		      "verbose=n",
+		      "debug",
+		      "help");
+  &usage(1) if (!$ok);
+  &usage(0, 1) if ($opt_help);
+  $opt_verbose = $defaultVerbose if (! defined $opt_verbose);
+  $clusterHub = $opt_clusterHub if (defined $opt_clusterHub);
+  $distrHost = $opt_distrHost if (defined $opt_distrHost);
+  $dbHost = $opt_dbHost if (defined $opt_dbHost);
+} # checkOptions
+
+sub openOrDie {
+  # Open a file or else die with informative error message.
+  my ($fileSpec) = @_;
+  open(my $handle, $fileSpec)
+    || die "Couldn't open \"$fileSpec\": $!\n";
+  return $handle;
+} # openOrDie
+
+my ($buildDir, $scratchDir, $tGenesetPrefix, $tDb, @qDbs, %dbToFasta);
+sub parseConfig {
+  # Parse config.ra file, make sure it contains the required variables.
+  my ($config) = @_;
+  my $fh = &openOrDie($config);
+  my %vars;
+  while (<$fh>) {
+    next if (/^\s*#/ || /^\s*$/);
+    if (/^\s*(\w+)\s*(.*)$/) {
+      $vars{$1} = $2;
+    } else {
+      die "Parse error line $. of $config:\n$_\n--";
+    }
+  }
+  close($fh);
+  # Required variables.
+  $buildDir = $vars{'buildDir'}
+    || die "Error: $config is missing required variable buildDir.\n";
+  delete $vars{'buildDir'};
+  $scratchDir = $vars{'scratchDir'}
+    || die "Error: $config is missing required variable scratchDir.\n";
+  delete $vars{'scratchDir'};
+  $tDb = $vars{'targetDb'}
+    || die "Error: $config is missing required variable targetDb.\n";
+  delete $vars{'targetDb'};
+  $tGenesetPrefix = $vars{'targetGenesetPrefix'}
+    || die "Error: $config is missing required variable targetGenesetPrefix.\n";
+  delete $vars{'targetGenesetPrefix'};
+  my $queryDbs = $vars{'queryDbs'}
+    || die "Error: $config is missing required variable queryDbs.\n";
+  delete $vars{'queryDbs'};
+  @qDbs = split(/\s+/, $queryDbs);
+  # Variables we expect to be there given the contents of $queryDbs:
+  foreach my $db ($tDb, @qDbs) {
+    my $faVar = $db . "Fa";
+    my $fa = $vars{$faVar}
+      || die "Error: $config is missing required variable $faVar.\n";
+    $dbToFasta{$db} = $fa;
+    delete $vars{$faVar};
+  }
+  if (scalar(keys %vars) > 0) {
+    die "Error: $config contains extra variable(s) that I don't know " .
+      "what to do with: " . join(", ", sort keys %vars) . "\n";
+  }
+} # parseConfig
+
+# verbatim from doBlastz... this should go in a lib:
+sub verbose {
+  my ($level, $message) = @_;
+  print STDERR $message if ($opt_verbose >= $level);
+}
+
+# verbatim from doBlastz... this should go in a lib:
+sub run {
+  # Run a command in sh (unless -debug).
+  my ($cmd) = @_;
+  if ($opt_debug) {
+    print "# $cmd\n";
+  } else {
+    verbose(1, "# $cmd\n");
+    system($cmd) == 0 || die "Command failed:\n$cmd\n";
+  }
+} # run
+
+# verbatim from doBlastz... this should go in a lib:
+sub mustMkdir {
+  # mkdir || die.  Immune to -debug -- we need to create the dir structure 
+  # and dump out the scripts even if we don't actually execute the scripts.
+  my ($dir) = @_;
+  system("mkdir -p $dir") == 0 || die "Couldn't mkdir $dir\n";
+}
+
+sub splitSequence {
+  # Split the target gene fasta file into small pieces for cluster run.
+  my ($tDb, $tFasta) = @_;
+
+  my $runDir = $buildDir;
+  my $bossScript = "$runDir/doSplit.csh";
+  my $fh = &openOrDie(">$bossScript");
+  print $fh <<_EOF_
+#!/bin/csh -efx
+# This script was automatically generated by $0
+# from $CONFIG.
+# It is to be executed on $distrHost.
+# It runs faSplit on a monolithic protein fasta file.
+# This script will fail if any of its commands fail.
+
+mkdir $scratchDir/$tDb.split
+faSplit sequence $tFasta $tSplitCount $scratchDir/$tDb.split/$tDb
+_EOF_
+    ;
+  close($fh);
+  &run("chmod a+x $bossScript");
+  &run("ssh -x $distrHost nice $bossScript");
+}
+
+sub formatSequence {
+  # Run formatdb on the specified query ($scratchDir should be cluster-mounted)
+  my ($qDb, $qFasta) = @_;
+
+  my $runDir = $buildDir;
+  &mustMkdir($runDir);
+  my $bossScript = "$runDir/doFormat_$qDb.csh";
+  my $fh = &openOrDie(">$bossScript");
+  print $fh <<_EOF_
+#!/bin/csh -efx
+# This script was automatically generated by $0
+# from $CONFIG.
+# It is to be executed on $distrHost.
+# It runs formatdb to preprocess query protein fasta.
+# This script will fail if any of its commands fail.
+
+mkdir $scratchDir/$qDb.formatdb
+cd $scratchDir/$qDb.formatdb
+$blastpath/bin/formatdb -i $qFasta -t $qDb -n $qDb
+_EOF_
+    ;
+  close($fh);
+  &run("chmod a+x $bossScript");
+  &run("ssh -x $distrHost nice $bossScript");
+} # formatSequence
+
+sub runPairwiseBlastp {
+  # Do a pairwise blastp cluster run for the given query.  
+  # $b is the blastp -b param value... note -b is at the end of $blastpParams.
+  my ($tDb, $qDb, $b) = @_;
+
+  my $runDir = "$buildDir/run.$tDb.$qDb";
+  &mustMkdir($runDir);
+  my $fh = &openOrDie(">$runDir/blastSome");
+  print $fh <<_EOF_
+#!/bin/csh -ef
+setenv BLASTMAT $blastpath/data
+$blastpath/bin/blastall -p blastp -d $scratchDir/$qDb.formatdb/$qDb \\
+    -i \$1 -o \$2 $blastpParams $b
+_EOF_
+    ;
+  close($fh);
+
+  $fh = &openOrDie(">$runDir/gsub");
+  print $fh <<_EOF_
+#LOOP
+blastSome {check in line+ \$(path1)} {check out line out/\$(root1).tab}
+#ENDLOOP
+_EOF_
+    ;
+  close($fh);
+
+  my $bossScript = "$runDir/doBlastp.csh";
+  $fh = &openOrDie(">$bossScript");
+  print $fh <<_EOF_
+#!/bin/csh -efx
+# This script was automatically generated by $0
+# from $CONFIG.
+# It is to be executed on $clusterHub in $runDir .
+# It does the blastp cluster run.
+# This script will fail if any of its commands fail.
+
+cd $runDir
+mkdir out
+chmod a+x blastSome
+ls -1S $scratchDir/$tDb.split/*.fa > split.lst
+gensub2 split.lst single gsub spec
+para make spec
+para time
+
+_EOF_
+    ;
+  close($fh);
+  &run("chmod a+x $bossScript");
+  &run("ssh -x $clusterHub $bossScript");
+} # runPairwiseBlastp
+
+
+sub dbToPrefix {
+  # Condense database name to a 2-character prefix for the *BlastTab table.
+  my ($db) = @_;
+  my $prefix;
+  if ($db =~ /^(\w\w)\d+$/) {
+    $prefix = $1;
+  } elsif ($db =~ /^(\w)\w\w(\w)\w\w\d+$/) {
+    $prefix = $1 . $2;
+  } else {
+    die "Error: dbToPrefix: expecting database name, got $db";
+  }
+  return $prefix;
+}
+
+sub loadPairwise {
+  my ($tDb, $qDb, $tablePrefix) = @_;
+
+  my $tableName = $tablePrefix . 'BlastTab';
+  my $runDir = "$buildDir/run.$tDb.$qDb/out";
+  my $bossScript = "$buildDir/run.$tDb.$qDb/loadPairwise.csh";
+  my $fh = openOrDie(">$bossScript");
+  print $fh <<_EOF_
+#!/bin/csh -efx
+# This script was automatically generated by $0
+# from $CONFIG.
+# It is to be executed on $dbHost in $runDir .
+# It does the blastp cluster run.
+# This script will fail if any of its commands fail.
+
+cd $runDir
+hgLoadBlastTab $tDb $tableName -maxPer=1 *.tab
+_EOF_
+    ;
+  close($fh);
+  &run("chmod a+x $bossScript");
+  &run("ssh -x $dbHost nice $bossScript");
+} # loadPairwise
+
+
+sub cleanup {
+  &run("ssh -x $distrHost rm -rf $scratchDir");
+} # cleanup
+
+
+###########################################################################
+# MAIN
+
+&checkOptions();
+
+# Parse config file into target and query db specs and fasta paths.
+&usage(1) if (scalar(@ARGV) != 1);
+($CONFIG) = @ARGV;
+&parseConfig($CONFIG);
+
+# Split target fasta.
+&run("ssh -x $distrHost mkdir $scratchDir");
+my $tFasta = $dbToFasta{$tDb};
+&splitSequence($tDb, $tFasta);
+
+# Self blastp.
+&formatSequence($tDb, $tFasta);
+&runPairwiseBlastp($tDb, $tDb, 1000);
+&loadPairwise($tDb, $tDb, $tGenesetPrefix);
+
+# For each query db, pairwise blastp.
+my $tPrefix = &dbToPrefix($tDb);
+foreach my $qDb (@qDbs) {
+  my $qFasta = $dbToFasta{$qDb};
+  my $qPrefix = &dbToPrefix($qDb);
+  # tDb vs qDb
+  &formatSequence($qDb, $qFasta);
+  &runPairwiseBlastp($tDb, $qDb, 1);
+  &loadPairwise($tDb, $qDb, $qPrefix);
+  # qDb vs tDb
+  &splitSequence($qDb, $qFasta);
+  &runPairwiseBlastp($qDb, $tDb, 1);
+  &loadPairwise($qDb, $tDb, $tPrefix);
+}
+
+# Clean up.
+&cleanup();
+
