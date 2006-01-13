5c4a9e2b86b44586f4c8f2bdb51fe74d30135a63
heather <heather>
2006-01-13 05:34:54 +0000

    rework; add DeletionClassWrongObserved

--- src/hg/snp/snpLoad/snpExceptions.c
+++ src/hg/snp/snpLoad/snpExceptions.c
@@ -5,10 +5,11 @@
 #include "snp125.h"
 #include "snp125Exceptions.h"
 
-static char const rcsid[] = "$Id: snpExceptions.c,v 1.1 2006/01/12 20:54:17 heather Exp $";
+static char const rcsid[] = "$Id: snpExceptions.c,v 1.2 2006/01/13 05:34:54 heather Exp $";
 
 char *database = NULL;
 static struct slName *chromList = NULL;
+struct snp125 *listSimple=NULL, *listDeletion=NULL, *listInsertion=NULL, *listRange=NULL;
 
 void usage()
 /* Explain usage and exit. */
@@ -27,7 +28,7 @@ snp125ExceptionsTableCreate(conn);
 hFreeConn(&conn);
 }
 
-struct snp125 *readSnps(char *chromName)
+void readSnps(char *chromName)
 /* slurp in chrN_snp125 */
 {
 struct snp125 *list=NULL, *el = NULL;
@@ -44,7 +45,7 @@ strcat(tableName, chromName);
 strcat(tableName, "_snp125");
 
 if (!sqlTableExists(conn, tableName))
-    return list;
+    return;
 
 /* not checking chrom */
 safef(query, sizeof(query), "select chrom, chromStart, chromEnd, name, strand, refNCBI, refUCSC, "
@@ -65,20 +66,25 @@ while ((row = sqlNextRow(sr)) != NULL)
     el->refUCSC = cloneString(row[6]);
     el->observed = cloneString(row[7]);
     el->class = cloneString(row[8]);
-    slAddHead(&list,el);
+    if (sameString(el->class, "simple")) slAddHead(&listSimple,el);
+    else if (sameString(el->class, "deletion")) slAddHead(&listDeletion,el);
+    else if (sameString(el->class, "insertion")) slAddHead(&listInsertion,el);
+    else if (sameString(el->class, "range")) slAddHead(&listRange,el);
     count++;
     }
 sqlFreeResult(&sr);
 sqlDisconnect(&conn);
 verbose(1, "%d snps found\n", count);
-slReverse(&list);
-return list;
+slReverse(&listSimple);
+slReverse(&listDeletion);
+slReverse(&listInsertion);
+slReverse(&listRange);
 }
 
 void writeOneException(FILE *f, struct snp125 *el, char exception[])
 {
 verbose(1, "%s:%d-%d (%s) %s\n", el->chrom, el->chromStart, el->chromEnd, el->name, exception);
-fprintf(f, "chr%s\t", el->chrom);
+fprintf(f, "%s\t", el->chrom);
 fprintf(f, "%d\t", el->chromStart);
 fprintf(f, "%d\t", el->chromEnd);
 fprintf(f, "%s\t", el->name);
@@ -86,54 +92,112 @@ fprintf(f, "%s\t", exception);
 fprintf(f, "\n");
 }
 
-void writeSizeExceptions(FILE *f, struct snp125 *list)
+void writeSizeExceptions(FILE *f)
 {
 struct snp125 *el = NULL;
 int size = 0;
 
-for (el = list; el != NULL; el = el->next)
+for (el = listSimple; el != NULL; el = el->next)
     {
-
-    if (sameString(el->class, "unknown"))
-        continue;
-
     size = el->chromEnd - el->chromStart;
-
-    if (sameString(el->class, "simple"))
-        {
-        if (size != 1) 
-	    writeOneException(f, el, "SimpleClassWrongSize");
-	continue;
-        }
-
-    if (sameString(el->class, "insertion"))
-        {
-        if (size != 0) 
-            writeOneException(f, el, "InsertionClassWrongSize");
-	continue;
-        }
-
-    if (sameString(el->class, "deletion"))
-        {
-        if (size == 0) 
-            writeOneException(f, el, "DeletionClassWrongSize");
-	continue;
-        }
-
-    if (sameString(el->class, "range"))
-        {
-        if (size <= 1) 
-            writeOneException(f, el, "RangeClassWrongSize");
-        }
-
+    if (size != 1) 
+       writeOneException(f, el, "SimpleClassWrongSize");
     }
 
+for (el = listInsertion; el != NULL; el = el->next)
+    {
+    size = el->chromEnd - el->chromStart;
+    if (size != 0) 
+        writeOneException(f, el, "InsertionClassWrongSize");
+    }
+
+for (el = listDeletion; el != NULL; el = el->next)
+    {
+    size = el->chromEnd - el->chromStart;
+    if (size == 0) 
+        writeOneException(f, el, "DeletionClassWrongSize");
+    }
+
+for (el = listRange; el != NULL; el = el->next)
+    {
+    size = el->chromEnd - el->chromStart;
+    if (size <= 1) 
+        writeOneException(f, el, "RangeClassWrongSize");
+    }
 }
 
-void writeObservedExceptions(FILE *f, struct snp125 *list)
+boolean triAllelic(char *observed)
+{
+    if (sameString(observed, "A/C/G")) return TRUE;
+    if (sameString(observed, "A/C/T")) return TRUE;
+    if (sameString(observed, "C/G/T")) return TRUE;
+    return FALSE;
+}
+
+boolean quadAllelic(char *observed)
+{
+    if (sameString(observed, "A/C/G/T")) return TRUE;
+    return FALSE;
+}
+
+boolean validSimpleObserved(char *observed)
+{
+    if (sameString(observed, "n/a")) return TRUE;
+    if (sameString(observed, "A/C")) return TRUE;
+    if (sameString(observed, "A/G")) return TRUE;
+    if (sameString(observed, "A/T")) return TRUE;
+    if (sameString(observed, "C/G")) return TRUE;
+    if (sameString(observed, "C/T")) return TRUE;
+    if (sameString(observed, "G/T")) return TRUE;
+    return FALSE;
+}
+
+
+void writeSimpleObservedExceptions(FILE *f)
+/* There are 3 exceptions here:
+   SimpleClassWrongObserved, SimpleClassTriAllelic, SimpleClassQuadAllelic */
+{
+struct snp125 *el = NULL;
+
+for (el = listSimple; el != NULL; el = el->next)
+    {
+    if (quadAllelic(el->observed))
+        {
+        writeOneException(f, el, "SimpleClassQuadAllelic");
+	continue;
+        }
+    if (triAllelic(el->observed))
+        {
+        writeOneException(f, el, "SimpleClassTriAllelic");
+	continue;
+        }
+    if (!validSimpleObserved(el->observed))
+        writeOneException(f, el, "SimpleClassWrongObserved");
+    }
+}
+
+void writeDeletionObservedExceptions(FILE *f)
+/* Look for DeletionClassWrongObserved */
+{
+struct snp125 *el = NULL;
+
+for (el = listDeletion; el != NULL; el = el->next)
+    {
+    if (sameString(el->observed, "n/a")) continue;
+    if (strlen(el->observed) < 2)
+        {
+	writeOneException(f, el, "DeletionClassWrongObserved");
+	continue;
+	}
+    if (el->observed[0] == '-' && el->observed[1] == '/') continue;
+    writeOneException(f, el, "DeletionClassWrongObserved");
+    }
+}
+
+void writeReferenceExceptions(FILE *f)
+/* RefNCBINotInObserved, RefUCSCNotInObserved. */
 {
 }
-
 
 void loadDatabase(FILE *f, char *fileName)
 {
@@ -146,7 +210,6 @@ hFreeConn(&conn);
 int main(int argc, char *argv[])
 /* Write chrN_snp125Exceptions.tab file for each chrom. */
 {
-struct snp125 *list=NULL, *el;
 struct slName *chromPtr;
 FILE *f;
 char fileName[64];
@@ -170,8 +233,8 @@ for (chromPtr = chromList; chromPtr != NULL; chromPtr = chromPtr->next)
 for (chromPtr = chromList; chromPtr != NULL; chromPtr = chromPtr->next)
     {
     verbose(1, "chrom = %s\n", chromPtr->name);
-    list = readSnps(chromPtr->name);
-    if (list == NULL) 
+    readSnps(chromPtr->name);
+    if (listSimple == NULL && listDeletion == NULL && listInsertion == NULL && listRange == NULL) 
         {
 	printf("no SNPs for chr%s\n", chromPtr->name);
         verbose(1, "---------------------------------------\n");
@@ -183,12 +246,17 @@ for (chromPtr = chromList; chromPtr != NULL; chromPtr = chromPtr->next)
     strcat(fileName, "_snp125Exceptions");
     f = hgCreateTabFile(".", fileName);
 
-    writeSizeExceptions(f, list);
-    writeObservedExceptions(f, list);
+    writeSizeExceptions(f);
+    writeSimpleObservedExceptions(f);
+    writeDeletionObservedExceptions(f);
 
     loadDatabase(f, fileName);
-    slFreeList(&list);
+    slFreeList(&listSimple);
+    slFreeList(&listDeletion);
+    slFreeList(&listInsertion);
+    slFreeList(&listRange);
     verbose(1, "---------------------------------------\n");
+    break;
     }
 
 return 0;
