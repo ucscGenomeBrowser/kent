6cc7d080657d88457a0233004fc8a87825cd4ec8
heather <heather>
2006-01-15 05:03:53 +0000

    get class right

--- src/hg/lib/snp125.c
+++ src/hg/lib/snp125.c
@@ -8,7 +8,7 @@
 #include "jksql.h"
 #include "snp125.h"
 
-static char const rcsid[] = "$Id: snp125.c,v 1.3 2006/01/15 03:08:18 heather Exp $";
+static char const rcsid[] = "$Id: snp125.c,v 1.4 2006/01/15 05:03:53 heather Exp $";
 
 void snp125StaticLoad(char **row, struct snp125 *ret)
 /* Load a row from snp125 table into ret.  The contents of ret will
@@ -268,7 +268,8 @@ char *createString =
 "    refUCSC       blob not null,\n"
 "    observed      blob not null,\n"
 "    molType       enum('unknown', 'genomic', 'cDNA', 'mito', 'chloro') DEFAULT 'unknown' not null,\n"
-"    class         enum('unknown', 'simple', 'insertion', 'deletion', 'range') \n"
+"    class         enum('unknown', 'single', 'in-del', 'heterozygous', 'microsatelite',
+"                  'named', 'no var', 'mixed', 'mnp') \n"
 "                  DEFAULT 'unknown' NOT NULL,\n"
 "    valid         set('unknown', 'by-frequency', 'by-cluster', 'by-submitter', \n"
 "                  'by-2hit-2allele', 'by-hapmap') \n"
