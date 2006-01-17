8ef07a5b5c1aa3bcff2057bb966902b37ec4e81c
heather <heather>
2006-01-17 23:30:56 +0000

    add snp125ColorSourceLocType

--- src/hg/inc/snp125Ui.h
+++ src/hg/inc/snp125Ui.h
@@ -24,6 +24,7 @@ enum snp125ColorSourceEnum {
     snp125ColorSourceClass,
     snp125ColorSourceValid,
     snp125ColorSourceFunc,
+    snp125ColorSourceLocType,
 };
 
 extern char *snp125ColorSourceLabels[];
