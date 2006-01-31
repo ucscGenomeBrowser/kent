599b8c37f244516c595505837eef500d9151587a
braney <braney>
2006-01-31 06:35:03 +0000

    the beginnings of some ordered elements

--- src/hg/history/inc/element.h
+++ src/hg/history/inc/element.h
@@ -1,18 +1,10 @@
 
-struct element
+struct adjacency
 {
-    struct element *next;
-    struct genome *genome;
-    char *species;
-    char *name;
-    char *version;
-    int count;
-    int isFlipped;
-    int allocedEdges;
-    int numEdges;
-    struct element *parent;
-    struct element **edges;
+    struct hash *prevHash;  
+    struct hash *nextHash;
 };
+
 
 struct genome
 {
@@ -21,7 +13,6 @@ struct genome
     struct phyloTree *node;
     char *name;
     struct element *elements;
-    struct hash *pairOrders;
 };
 
 struct distance
@@ -40,6 +31,26 @@ struct eleDistance
     struct distance *distance;
 };
 
+struct element
+{
+    struct element *next;
+    struct genome *genome;
+    char *species;
+    char *name;
+    char *version;
+    int count;
+    int isFlipped;
+    struct adjacency up;
+    struct adjacency down;
+    struct adjacency mix;
+    struct eleDistance *dist;
+    //struct hash *nextHash;
+    //struct hash *prevHash;
+    int allocedEdges;
+    int numEdges;
+    struct element *parent;
+    struct element **edges;
+} ;
 
 extern void setElementDist(struct element *e1, struct element *e2, double dist,
     struct distance **distanceList, struct hash **pDistHash, struct hash **pDistEleHash);
@@ -55,3 +66,4 @@ extern struct phyloTree *eleReadTree(char *fileName);
 extern void printElementTrees(struct phyloTree *node, int depth);
 extern struct element *newElement(struct genome *g, char *name, char *version);
 extern void outElementTrees(FILE *f, struct phyloTree *node);
+extern struct genome *getGenome(char *file, char *name);
