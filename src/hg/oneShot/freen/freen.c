09ccb7e92afa35088502a9f01ebe7578b0636ff8
kent <kent>
2006-02-02 16:24:48 +0000

    Freen checks on join timing.

--- src/hg/oneShot/freen/freen.c
+++ src/hg/oneShot/freen/freen.c
@@ -1,63 +1,151 @@
 /* freen - My Pet Freen. */
 #include "common.h"
 #include "memalloc.h"
+#include "dystring.h"
 #include "linefile.h"
 #include "hash.h"
-#include "xap.h"
+#include "obscure.h"
+#include "jksql.h"
 
-static char const rcsid[] = "$Id: freen.c,v 1.63 2005/12/20 17:52:26 kent Exp $";
+static char const rcsid[] = "$Id: freen.c,v 1.64 2006/02/02 16:24:48 kent Exp $";
 
 void usage()
 {
 errAbort("freen - test some hairbrained thing.\n"
-         "usage:  freen file tag\n");
+         "usage:  freen file \n");
 }
 
 int level = 0;
 
-void *atStart(struct xap *xap, char *name, char **atts)
-{
-char *att;
-spaceOut(stdout, level*2);
-printf("<%s", name);
-while ((att = *atts++) != NULL)
-     {
-     printf(" %s=\"", att);
-     att = *atts++;
-     printf("%s\"", att);
-     }
-printf(">\n");
-level += 1;
-return name;
-}
 
-void atEnd(struct xap *xap, char *name)
-{
-char *text = trimSpaces(xap->stack->text->string);
-if (text[0] != 0)
-    {
-    spaceOut(stdout, level*2);
-    printf("%s\n", trimSpaces(text));
-    }
-level -= 1;
-spaceOut(stdout, level*2);
-printf("</%s>\n", name);
-}
-
-void freen(char *fileName, char *tag)
+void freen(char *fileName)
 /* Test some hair-brained thing. */
 {
-struct xap *xap = xapOpen(fileName, atStart, atEnd);
+struct slName *id, *idList = readAllLines(fileName);
+struct sqlConnection *conn = sqlConnect("visiGene");
+int i;
+char query[256];
 char *s;
-while ((s = xapNext(xap, tag)) != NULL)
-    ;
+double randSeed;
+struct sqlResult *sr;
+char **row;
+struct dyString *dy = dyStringNew(10*1024);
+int batchSize = 80000;
+struct hash *hash = hashNew(18);
+
+for (id = idList; id != NULL; id = id->next)
+    {
+    hashAdd(hash, id->name, NULL);
+    }
+
+safef(query, sizeof(query), "select rand()");
+randSeed = sqlQuickDouble(conn, query);
+srand(randSeed*12345);
+
+shuffleList(idList, 1);
+id = idList;
+uglyTime(NULL);
+
+#ifdef SOON
+for (i=0; i<batchSize; ++i)
+     {
+     safef(query, sizeof(query), "select preparation from image where id=%s",
+     	id->name);
+     s = sqlQuickString(conn, query);
+     id = id->next;
+     }
+uglyTime("%d like %s", batchSize, query);
+
+for (i=0; i<batchSize; ++i)
+     {
+     safef(query, sizeof(query), 
+     	"select submissionSet.privateUser from image,submissionSet "
+	"where image.id=%s and image.submissionSet=submissionSet.id"
+	, id->name);
+     s = sqlQuickString(conn, query);
+     id = id->next;
+     }
+uglyTime("%d like %s", batchSize, query);
+
+for (i=0; i<batchSize; ++i)
+     {
+     safef(query, sizeof(query), 
+     	"select genotype.alleles from image,specimen,genotype "
+	"where image.id=%s and image.specimen=specimen.id "
+	"and specimen.genotype=genotype.id"
+	, id->name);
+     s = sqlQuickString(conn, query);
+     id = id->next;
+     }
+uglyTime("%d like %s", batchSize, query);
+
+dyStringClear(dy);
+dyStringAppend(dy,
+    "select id,preparation from image where id in (");
+for (i=0; i<batchSize; ++i)
+    {
+    if (i != 0)
+        dyStringAppendC(dy, ',');
+    dyStringAppend(dy, id->name);
+    id = id->next;
+    }
+dyStringAppend(dy, ")");
+sr = sqlGetResult(conn, dy->string);
+while ((row = sqlNextRow(sr)) != NULL)
+    {
+    hashLookup(hash, row[0]);
+    }
+uglyTime("%d simple in", batchSize);
+
+dyStringClear(dy);
+dyStringAppend(dy,
+    "select image.id,submissionSet.privateUser from image,submissionSet "
+    "where image.submissionSet=submissionSet.id "
+    "and image.id in (");
+for (i=0; i<batchSize; ++i)
+    {
+    if (i != 0)
+        dyStringAppendC(dy, ',');
+    dyStringAppend(dy, id->name);
+    id = id->next;
+    }
+dyStringAppend(dy, ")");
+sr = sqlGetResult(conn, dy->string);
+while ((row = sqlNextRow(sr)) != NULL)
+    {
+    hashLookup(hash, row[0]);
+    }
+uglyTime("%d single join in", batchSize);
+#endif /* SOON */
+
+dyStringClear(dy);
+dyStringAppend(dy,
+    "select image.id,genotype.alleles from image,specimen,genotype "
+    "where genotype.id=specimen.genotype "
+    "and specimen.id = image.specimen "
+    "and image.id in (");
+for (i=0; i<batchSize; ++i)
+    {
+    if (i != 0)
+        dyStringAppendC(dy, ',');
+    dyStringAppend(dy, id->name);
+    id = id->next;
+    }
+dyStringAppend(dy, ")");
+sr = sqlGetResult(conn, dy->string);
+while ((row = sqlNextRow(sr)) != NULL)
+    {
+    hashLookup(hash, row[0]);
+    }
+uglyTime("%d double join in", batchSize);
+
 }
 
 int main(int argc, char *argv[])
 /* Process command line. */
 {
-if (argc != 3)
+if (argc != 2)
    usage();
-freen(argv[1], argv[2]);
+freen(argv[1]);
 return 0;
 }
