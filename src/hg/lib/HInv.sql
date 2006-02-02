eaf74530590569555e8c8b1c361edcedd5bb3d25
fanhsu <fanhsu>
2006-02-02 23:23:46 +0000

    Created HInv.sql.

---
+++ src/hg/lib/HInv.sql
@@ -0,0 +1,7 @@
+CREATE TABLE HInv (
+geneId varchar(255) NOT NULL default '',
+clusterId varchar(255) NOT NULL default '',
+mrnaAcc varchar(255) NOT NULL default '',
+PRIMARY KEY  (geneId)
+) TYPE=MyISAM;
+	
