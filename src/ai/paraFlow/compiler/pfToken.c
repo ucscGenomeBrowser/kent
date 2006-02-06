f7c74387de486cf4398d0455ba850cdc930cb751
kent <kent>
2006-02-06 20:30:06 +0000

    Making a function static.

--- src/ai/paraFlow/compiler/pfToken.c
+++ src/ai/paraFlow/compiler/pfToken.c
@@ -39,7 +39,7 @@ for (;;)
     }
 }
 
-void pfTokenFileLineCol(struct pfToken *tok, 
+static void pfTokenFileLineCol(struct pfToken *tok, 
     char **retFileName, int *retLine, int *retCol)
 /* Return file name, line, and column (zero based) of token start. */
 {
