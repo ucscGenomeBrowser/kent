0599ef18a8fe2b674e451105505bf1b7d28e9cbe
kent <kent>
2006-02-07 02:01:08 +0000

    Splitting pfCheck.h into checkPara.h and checkPoly.h

---
+++ src/ai/paraFlow/compiler/checkPoly.h
@@ -0,0 +1,11 @@
+/* checkPoly - check for polymorphism consistency. */
+/* Copyright 2005-6 Jim Kent.  All rights reserved. */
+
+#ifndef CHECKPOLY_H
+#define CHECKPOLY_H
+
+
+void checkPolymorphic(struct pfCompile *pfc, struct pfScope *scopeList);
+/* Check for polymorphism consistency */
+
+#endif /* CHECKPOLY_H */
