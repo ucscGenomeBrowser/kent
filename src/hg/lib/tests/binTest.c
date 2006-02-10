fc84ce945d381f373fc4d1193495ee6107c245e0
hiram <hiram>
2006-02-10 18:15:05 +0000

    Now doing both old and new schemes, plus single location is possible

--- src/hg/lib/tests/binTest.c
+++ src/hg/lib/tests/binTest.c
@@ -7,14 +7,22 @@
 #include "hdb.h"
 #include "binRange.h"
 
-static char const rcsid[] = "$Id: binTest.c,v 1.2 2006/02/09 22:22:18 hiram Exp $";
+static char const rcsid[] = "$Id: binTest.c,v 1.3 2006/02/10 18:15:05 hiram Exp $";
 
 boolean newStyle = FALSE; /* if not specified, will remain in old style range*/
 boolean oldStyle = FALSE; /* if not specified, will remain in old style range*/
+boolean sqlOnly = FALSE; /* if not specified, will remain in old style range*/
+boolean binOnly = FALSE; /* if not specified, will remain in old style range*/
+int testStart = 0;	/*	for a single test of one coordinate range */
+int testEnd = 0;	/*	for a single test of one coordinate range */
 
 static struct optionSpec optionSpecs[] = {
     {"newStyle", OPTION_BOOLEAN},
     {"oldStyle", OPTION_BOOLEAN},
+    {"sqlOnly", OPTION_BOOLEAN},
+    {"binOnly", OPTION_BOOLEAN},
+    {"start", OPTION_INT},
+    {"end", OPTION_INT},
     {NULL, 0}
 };
 
@@ -24,7 +32,9 @@ void usage()
 errAbort("usage: binTest [options]\n"
 "options:\n"
 "\t-newStyle - exercise expanded bin scheme over 512M\n"
-"\t-oldStyle - exercise standard bin scheme up to 512M");
+"\t-oldStyle - exercise standard bin scheme up to 512M\n"
+"\t-sqlOnly  - exercise only the sql statements\n"
+"\t-binOnly  - exercise only the bin scheme");
 }
 
 static void testOneSql(int start, int end)
@@ -37,11 +47,12 @@ freeDyString(&sqlQuery);
 }
 
 static void testOneBin(int start, int end, int expected)
+/*	expected < 0 == do not check, do not know the answer */
 {
 int bin;
 
 bin = binFromRange(start,end);
-if (expected != bin)
+if ((expected >= 0) && (expected != bin))
     verbose(1,"#\tFAILED %d != %d = binFromRange(%d, %d)\n",
 	expected, bin, start, end);
 verbose(2,"#\t%5d = binFromRange(%d, %d)\n", bin, start, end);
@@ -57,58 +68,60 @@ int expect;
 int binCount = 0;
 int level = 0;
 
-/*	first check full range of scheme, should be bin 0	*/
-verbose(2,"#\ttop level, whole chromosome\n");
-start = 0; end = maxEnd; expect = 0;
-testOneBin(start,end, expect);
-binCount += 1;
+if (! sqlOnly)
+    {
+    /*	first check full range of scheme, should be bin 0	*/
+    verbose(2,"#\ttop level, whole chromosome\n");
+    start = 0; end = maxEnd; expect = 0;
+    testOneBin(start,end, expect);
+    binCount += 1;
 
-for (level = 1; level < 5; ++level)
-    {
-    verbose(2,"#\tlevel %d of %d bins\n", level, 1 << (level*3));
-    binSize = maxEnd >> (level*3);
-    for (start = 0; start < maxEnd; start += binSize << ((level-1)*3) )
-    {
-	expect = (start / binSize) + binCount;
-	end = start + binSize;
-	testOneBin(start,end, expect);
-    }
-    if (level > 1)
+    for (level = 1; level < 5; ++level)
 	{
-	/*	plus the last one at this level (when more than 8)	*/
-	start = maxEnd - binSize;
-	end = start + binSize;
-	expect = (start / binSize) + binCount;
-	testOneBin(start,end, expect);
-	}
-    binCount += 1 << (level*3);
-    }
-
-/*	first check full range of scheme, should be bin 0	*/
-verbose(2,"#\ttop level, whole chromosome\n");
-start = 0; end = maxEnd; expect = 0;
-testOneSql(start,end);
-binCount += 1;
-
-for (level = 1; level < 5; ++level)
-    {
-    verbose(2,"#\tlevel %d of %d bins\n", level, 1 << (level*3));
-    binSize = maxEnd >> (level*3);
-    for (start = 0; start < maxEnd; start += binSize << ((level-1)*3) )
-    {
-	expect = (start / binSize) + binCount;
-	end = start + binSize;
-	testOneSql(start,end);
-    }
-    if (level > 1)
+	verbose(2,"#\tlevel %d of %d bins\n", level, 1 << (level*3));
+	binSize = maxEnd >> (level*3);
+	for (start = 0; start < maxEnd; start += binSize << ((level-1)*3) )
 	{
-	/*	plus the last one at this level (when more than 8)	*/
-	start = maxEnd - binSize;
-	end = start + binSize;
-	expect = (start / binSize) + binCount;
-	testOneSql(start,end);
+	    expect = (start / binSize) + binCount;
+	    end = start + binSize;
+	    testOneBin(start,end, expect);
 	}
-    binCount += 1 << (level*3);
+	if (level > 1)
+	    {
+	    /*	plus the last one at this level (when more than 8)	*/
+	    start = maxEnd - binSize;
+	    end = start + binSize;
+	    expect = (start / binSize) + binCount;
+	    testOneBin(start,end, expect);
+	    }
+	binCount += 1 << (level*3);
+	}
+    }
+
+if (! binOnly)
+    {
+    /*	first check full range of scheme, should be bin 0	*/
+    verbose(2,"#\ttop level, whole chromosome\n");
+    start = 0; end = maxEnd;
+    testOneSql(start,end);
+
+    for (level = 1; level < 5; ++level)
+	{
+	verbose(2,"#\tlevel %d of %d bins\n", level, 1 << (level*3));
+	binSize = maxEnd >> (level*3);
+	for (start = 0; start < maxEnd; start += binSize << ((level-1)*3) )
+	{
+	    end = start + binSize;
+	    testOneSql(start,end);
+	}
+	if (level > 1)
+	    {
+	    /*	plus the last one at this level (when more than 8)	*/
+	    start = maxEnd - binSize;
+	    end = start + binSize;
+	    testOneSql(start,end);
+	    }
+	}
     }
 
 }	/*	static void standardBinScheme()	*/
@@ -131,34 +144,72 @@ practicalMaxEnd = (int)((actualMaxEnd >> 1) - 1L);
 verbose(2,"#\tpracticalMaxEnd: %d\n", practicalMaxEnd);
 binSize = actualMaxEnd;
 
-/*	first check full range of new scheme, should be bin extendedOffset */
-verbose(2,"#\ttop level, whole chromosome, binSize: %lld\n", binSize);
-start = 0; end = practicalMaxEnd;
-expect = extendedOffset;
-testOneBin(start,end, expect);
-binCount += 1;
-
-for (level = 1; level < 6; ++level)
+if (!sqlOnly)
     {
-    binSize = actualMaxEnd >> (level*3);
-    verbose(2,"#\tlevel %d of %d bins, binSize: %lld\n",
-	level, 1 << (level*3), binSize);
-    for (start = standardMaxEnd; (start > 0) &&
-	(start < (int)((long long)practicalMaxEnd-binSize));
-	    start += binSize << ((level-1)*3) )
-    {
-	expect = (int)((long long)start / binSize) + binCount + extendedOffset;
-	end = start + binSize;
-	if (end >= practicalMaxEnd) break;
-	testOneBin(start,end, expect);
-    }
-    /*	plus the last one at this level */
-    start = practicalMaxEnd - binSize + 1;
-    end = practicalMaxEnd;
-    expect = (int)((long long)start / binSize) + binCount + extendedOffset;
+    /*	first check full range of new scheme, should be bin extendedOffset */
+    verbose(2,"#\ttop level, whole chromosome, binSize: %lld\n", binSize);
+    start = 0; end = practicalMaxEnd;
+    expect = extendedOffset;
     testOneBin(start,end, expect);
-    binCount += 1 << (level*3);
+    binCount += 1;
+
+    for (level = 1; level < 6; ++level)
+	{
+	binSize = actualMaxEnd >> (level*3);
+	verbose(2,"#\tlevel %d of %d bins, binSize: %lld\n",
+	    level, 1 << (level*3), binSize);
+	for (start = standardMaxEnd; (start > 0) &&
+	    (start < (int)((long long)practicalMaxEnd-binSize));
+		start += binSize << ((level-1)*3) )
+	{
+	    expect = (int)((long long)start / binSize) + binCount + extendedOffset;
+	    end = start + binSize;
+	    if (end >= practicalMaxEnd) break;
+	    testOneBin(start,end, expect);
+	}
+	/*	plus the last one at this level */
+	start = practicalMaxEnd - binSize + 1;
+	end = practicalMaxEnd;
+	expect = (int)((long long)start / binSize) + binCount + extendedOffset;
+	testOneBin(start,end, expect);
+	binCount += 1 << (level*3);
+	}
     }
+
+if (!binOnly)
+    {
+    binCount = 0;
+    binSize = actualMaxEnd;
+    /*	first check full range of new scheme, should be bin extendedOffset */
+    verbose(2,"#\ttop level, whole chromosome, binSize: %lld\n", binSize);
+    start = 0; end = practicalMaxEnd;
+    expect = extendedOffset;
+    testOneSql(start,end);
+    binCount += 1;
+
+    for (level = 1; level < 3; ++level)
+	{
+	binSize = actualMaxEnd >> (level*3);
+	verbose(2,"#\tlevel %d of %d bins, binSize: %lld\n",
+	    level, 1 << (level*3), binSize);
+	for (start = standardMaxEnd; (start > 0) &&
+	    (start < (int)((long long)practicalMaxEnd-binSize));
+		start += binSize << ((level-1)*3) )
+	{
+	    expect = (int)((long long)start / binSize) + binCount + extendedOffset;
+	    end = start + binSize;
+	    if (end >= practicalMaxEnd) break;
+	    testOneSql(start,end);
+	}
+	/*	plus the last one at this level */
+	start = practicalMaxEnd - binSize + 1;
+	end = practicalMaxEnd;
+	expect = (int)((long long)start / binSize) + binCount + extendedOffset;
+	testOneSql(start,end);
+	binCount += 1 << (level*3);
+	}
+    }
+
 }	/*	static void expandedBinScheme()	*/
 
 int main(int argc, char *argv[])
@@ -167,9 +218,22 @@ optionInit(&argc, argv, optionSpecs);
 
 newStyle = optionExists("newStyle");
 oldStyle = optionExists("oldStyle");
+sqlOnly = optionExists("sqlOnly");
+binOnly = optionExists("binOnly");
 
-if ( ! (newStyle || oldStyle) )
+testStart = optionInt("start",0);
+testEnd = optionInt("end",0);
+
+if ( ! (newStyle || oldStyle || (testEnd > 0)) )
     usage();
+
+if (testEnd > 0)
+{
+if (!sqlOnly)
+    testOneBin(testStart, testEnd, -1);
+if (!binOnly)
+    testOneSql(testStart, testEnd);
+}
 
 if (oldStyle)
     standardBinScheme();
