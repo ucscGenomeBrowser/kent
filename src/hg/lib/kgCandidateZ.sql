74a2e11d6e3d77cbb5d5b0916674c817df0f9229
fanhsu <fanhsu>
2006-02-27 22:28:17 +0000

    Created kgCandidateZ.sql.

---
+++ src/hg/lib/kgCandidateZ.sql
@@ -0,0 +1,10 @@
+CREATE TABLE kgCandidateZ (
+  cdsId varchar(10),
+  ranking float,
+  protDbId char(1),
+  score int,
+  mrnaID varchar(40),
+  protAcc varchar(40),
+  alignID varchar(40)
+) TYPE=MyISAM;
+
