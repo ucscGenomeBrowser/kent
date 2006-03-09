f13e9f86a075a843f3b9407ce82b7c2c760356a0
heather <heather>
2006-03-09 03:17:44 +0000

    rewrite to use hash

--- src/hg/snp/snpLoad/snpSNP.c
+++ src/hg/snp/snpLoad/snpSNP.c
@@ -1,8 +1,7 @@
 /* snpSNP - tenth step in dbSNP processing.
  * Read the chrN_snpTmp tables into memory.
  * Do lookups into SNP for validation status and heterozygosity.
- * Could use hash here! 
- * Also don't need to store into snpTmp list!!
+ * Report if missing.
  * Use UCSC chromInfo. */
 
 #include "common.h"
@@ -10,27 +9,18 @@
 #include "hash.h"
 #include "hdb.h"
 
-static char const rcsid[] = "$Id: snpSNP.c,v 1.4 2006/03/08 22:32:29 heather Exp $";
+static char const rcsid[] = "$Id: snpSNP.c,v 1.5 2006/03/09 03:17:44 heather Exp $";
 
-struct snpTmp
+struct snpData
     {
-    struct snpTmp *next;  	        
-    int snp_id;			
-    int start;            
-    int end;
-    int loc_type;
-    char *class;
-    int orientation;
-    char *molType;
-    char *fxn_class;
-    char *allele;	
-    char *refUCSC;
-    char *refUCSCReverseComp;
-    char *observed;
+    int validation_status;
+    float avHet;
+    float avHetSE;
     };
 
 static char *snpDb = NULL;
 FILE *errorFileHandle = NULL;
+static struct hash *snpDataHash = NULL;
 
 void usage()
 /* Explain usage and exit. */
@@ -43,138 +33,79 @@ errAbort(
 }
 
 
-struct snpTmp *snpTmpLoad(char **row)
-/* Load a snpTmp from row fetched from snp table
- * in database.  Dispose of this with snpTmpFree(). */
+void readSNP()
+/* get validation status and heterozygosity from SNP table */
+/* store in global snpDataHash */
 {
-struct snpTmp *ret;
+char query[512];
+struct sqlConnection *conn = hAllocConn();
+struct sqlResult *sr;
+char **row;
+int validInt = 0;
 
-AllocVar(ret);
-ret->snp_id = sqlUnsigned(row[0]);
-ret->start = sqlUnsigned(row[1]);
-ret->end = sqlUnsigned(row[2]);
-ret->loc_type = sqlUnsigned(row[3]);
-ret->class = cloneString(row[4]);
-ret->orientation = sqlUnsigned(row[5]);
-ret->molType = cloneString(row[6]);
-ret->fxn_class = cloneString(row[7]);
-ret->allele   = cloneString(row[8]);
-ret->refUCSC   = cloneString(row[9]);
-ret->refUCSCReverseComp   = cloneString(row[10]);
-ret->observed   = cloneString(row[11]);
-
-return ret;
-
-}
-
-
-void snpTmpFree(struct snpTmp **pEl)
-/* Free a single dynamically allocated snpTmp such as created with snpTmpLoad(). */
-{
-struct snpTmp *el;
-
-if ((el = *pEl) == NULL) return;
-freeMem(el->class);
-freeMem(el->molType);
-freeMem(el->fxn_class);
-freeMem(el->allele);
-freeMem(el->refUCSC);
-freeMem(el->refUCSCReverseComp);
-freeMem(el->observed);
-freez(pEl);
-}
-
-void snpTmpFreeList(struct snpTmp **pList)
-/* Free a list of dynamically allocated snpTmp's */
-{
-struct snpTmp *el, *next;
-
-for (el = *pList; el != NULL; el = next)
+/* could increase the default size */
+snpDataHash = newHash(0);
+safef(query, sizeof(query), "select snp_id, validation_status, avg_heterozygosity, het_se from SNP");
+sr = sqlGetResult(conn, query);
+while ((row = sqlNextRow(sr)) != NULL)
     {
-    next = el->next;
-    snpTmpFree(&el);
+    validInt = sqlUnsigned(row[1]);
+    if (validInt > 31)
+        {
+        fprintf(errorFileHandle, 
+	        "unexpected validation_status %d for snp_id %s\n", validInt, row[0]);
+	validInt = 0;
+	}
+
     }
-*pList = NULL;
+sqlFreeResult(&sr);
+hFreeConn(&conn);
 }
 
-
-struct snpTmp *readSnps(char *chromName)
-/* slurp in all rows for this chrom */
+void readSnps(char *chromName)
+/* read through chrN_snpTmp tables */
+/* lookup into snpDataHash */
+/* write to output file */
 {
-struct snpTmp *list=NULL, *el;
 char query[512];
 struct sqlConnection *conn = hAllocConn();
 struct sqlResult *sr;
 char **row;
 char tableName[64];
-
-safef(tableName, ArraySize(tableName), "%s_snpTmp", chromName);
-if (!hTableExists(tableName)) return list;
-
-safef(query, sizeof(query), 
-     "select snp_id, chromStart, chromEnd, loc_type, class, orientation, molType, "
-     "fxn_class, allele, refUCSC, refUCSCReverseComp, observed from %s ", tableName);
-
-sr = sqlGetResult(conn, query);
-while ((row = sqlNextRow(sr)) != NULL)
-    {
-    el = snpTmpLoad(row);
-    slAddHead(&list,el);
-    }
-sqlFreeResult(&sr);
-slReverse(&list);
-hFreeConn(&conn);
-return list;
-}
-
-
-void readSNP(char *chromName, struct snpTmp *list)
-/* get validation status and heterozygosity from SNP table */
-/* expecting a one-to-one correspondence */
-/* should check with joinerCheck */
-{
-struct snpTmp *el;
-char query[512];
-struct sqlConnection *conn = hAllocConn();
-struct sqlResult *sr;
-char **row;
 char fileName[64];
 FILE *f;
-int validInt = 0;
-boolean gotMatch = FALSE;
+struct snpData *sel = NULL;
 
+safef(tableName, ArraySize(tableName), "%s_snpTmp", chromName);
 safef(fileName, ArraySize(fileName), "%s_snpTmp.tab", chromName);
 f = mustOpen(fileName, "w");
 
-for (el = list; el != NULL; el = el->next)
+safef(query, sizeof(query), 
+     "select snp_id, chromStart, chromEnd, loc_type, class, orientation, molType, fxn_class, "
+     "allele, refUCSC, refUCSCReverseComp, observed from %s ", tableName);
+sr = sqlGetResult(conn, query);
+while ((row = sqlNextRow(sr)) != NULL)
     {
-    gotMatch = FALSE;
-    safef(query, sizeof(query), "select validation_status, avg_heterozygosity, het_se from SNP where snp_id = %d", el->snp_id);
-
-    sr = sqlGetResult(conn, query);
-    while ((row = sqlNextRow(sr)) != NULL)
+    sel = hashFindVal(snpDataHash, row[0]);
+    if (sel == NULL)
         {
-	gotMatch = TRUE;
-        validInt = sqlUnsigned(row[0]);
-        if (validInt > 31)
-            {
-            fprintf(errorFileHandle, "unexpected validation_status %s for snp_id %d\n", row[0], el->snp_id);
-	    validInt = 0;
-	    }
-        fprintf(f, "%d\t%d\t%d\t%d\t%s\t%d\t%s\t%s\t%d\t%s\t%s\t%s\t%s\t%s\t%s\t\n", 
-            el->snp_id, el->start, el->end, el->loc_type, el->class, el->orientation, el->molType, el->fxn_class,
-	    validInt, row[1], row[2],
-	    el->allele, el->refUCSC, el->refUCSCReverseComp, el->observed);
+        fprintf(f, "%s\t%s\t%s\t%s\t", row[0], row[1], row[2], row[3]);
+	fprintf(f, "%s\t%s\t%s\t%s\t", row[4], row[5], row[6], row[7]);
+	fprintf(f, "0\t0.0\t0.0\t");
+	fprintf(f, "%s\t%s\t%s\t%s\n", row[8], row[9], row[10], row[11]);
+        fprintf(errorFileHandle, "no match for snp_id %s\n", row[0]);
+	continue;
 	}
-    if (!gotMatch)
-        fprintf(errorFileHandle, "no match for snp_id %d\n", el->snp_id);
-
-    sqlFreeResult(&sr);
+    fprintf(f, "%s\t%s\t%s\t%s\t", row[0], row[1], row[2], row[3]);
+    fprintf(f, "%s\t%s\t%s\t%s\t", row[4], row[5], row[6], row[7]);
+    fprintf(f, "%d\t%f\t%f\t", sel->validation_status, sel->avHet, sel->avHetSE);
+    fprintf(f, "%s\t%s\t%s\t%s\n", row[8], row[9], row[10], row[11]);
     }
+
+sqlFreeResult(&sr);
 hFreeConn(&conn);
 carefulClose(&f);
 }
-
 
 void recreateDatabaseTable(char *chromName)
 /* create a new chrN_snpTmp table with new definition */
@@ -234,7 +165,6 @@ int main(int argc, char *argv[])
 /* read chrN_snpTmp, lookup in snpFasta, rewrite to individual chrom tables */
 {
 struct slName *chromList, *chromPtr;
-struct snpTmp *snpList = NULL;
 char tableName[64];
 
 if (argc != 2)
@@ -250,6 +180,10 @@ if (chromList == NULL)
     }
 
 errorFileHandle = mustOpen("snpSNP.errors", "w");
+
+/* read into global hash */
+verbose(1, "reading SNP table...\n");
+readSNP();
     
 for (chromPtr = chromList; chromPtr != NULL; chromPtr = chromPtr->next)
     {
@@ -258,21 +192,19 @@ for (chromPtr = chromList; chromPtr != NULL; chromPtr = chromPtr->next)
  
     verbose(1, "chrom = %s\n", chromPtr->name);
 
-    snpList = readSnps(chromPtr->name);
-    readSNP(chromPtr->name, snpList);
-    snpTmpFreeList(&snpList);
+    readSnps(chromPtr->name);
     }
 
 carefulClose(&errorFileHandle);
 
-for (chromPtr = chromList; chromPtr != NULL; chromPtr = chromPtr->next)
-    {
-    safef(tableName, ArraySize(tableName), "%s_snpTmp", chromPtr->name);
-    if (!hTableExists(tableName)) continue;
-    recreateDatabaseTable(chromPtr->name);
-    verbose(1, "loading chrom = %s\n", chromPtr->name);
-    loadDatabase(chromPtr->name);
-    }
+// for (chromPtr = chromList; chromPtr != NULL; chromPtr = chromPtr->next)
+    // {
+    // safef(tableName, ArraySize(tableName), "%s_snpTmp", chromPtr->name);
+    // if (!hTableExists(tableName)) continue;
+    // recreateDatabaseTable(chromPtr->name);
+    // verbose(1, "loading chrom = %s\n", chromPtr->name);
+    // loadDatabase(chromPtr->name);
+    // }
 
 return 0;
 }
