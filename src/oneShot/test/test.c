faeb5885302ab8da420161276dcabf7b3ef302e8
kent <kent>
2006-03-30 18:27:00 +0000

    Testing one more thing.

--- src/oneShot/test/test.c
+++ src/oneShot/test/test.c
@@ -17,44 +17,12 @@ errAbort(
   );
 }
 
-int level = 0;
-
-void atStart(void *userData, char *name, char **atts)
+int max = 10;
+void loop()
 {
-char *att;
-spaceOut(stdout, level*2);
-printf("<%s", name);
-while ((att = *atts++) != NULL)
-     {
-     printf(" %s=\"", att);
-     att = *atts++;
-     printf("%s\"", att);
-     }
-printf(">\n");
-level += 1;
-}
-
-void atEnd(void *userData, char *name, char *text)
-{
-text = trimSpaces(text);
-if (text[0] != 0)
-    {
-    spaceOut(stdout, level*2);
-    printf("%s\n", trimSpaces(text));
-    }
-level -= 1;
-spaceOut(stdout, level*2);
-printf("</%s>\n", name);
-}
-
-
-void test(char *in)
-/* test - Test something. */
-{
-FILE *f = mustOpen(in, "r");
-struct xp *xp = xpNew(f, atStart, atEnd, xpReadFromFile, in);
-while (xpParseNext(xp, "interest"))
-    ;
+int i;
+for (i=0; i<max; ++i)
+    printf("%d\n", i);
 }
 
 int main(int argc, char *argv[], char *env[])
