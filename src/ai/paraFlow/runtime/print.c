33daf923253ad3a01c87a287a7ec76974b6a41a1
kent <kent>
2006-04-03 18:21:49 +0000

    Adding more debugging type-specific simple printers.

--- src/ai/paraFlow/runtime/print.c
+++ src/ai/paraFlow/runtime/print.c
@@ -385,21 +385,39 @@ void pf_ugly(_pf_Stack *stack)
 pf_print(stack);
 }
 
+void pf__printByte(_pf_Stack *stack)
+/* Print integer, just for debugging really */
+{
+printf("%d\n", stack->Byte);
+}
+
+void pf__printShort(_pf_Stack *stack)
+/* Print integer, just for debugging really */
+{
+printf("%d\n", stack->Short);
+}
+
 void pf__printInt(_pf_Stack *stack)
 /* Print integer, just for debugging really */
 {
 printf("%d\n", stack->Int);
 }
 
-void pf__printDouble(_pf_Stack *stack)
-/* Print double, just for debugging really */
-{
-printf("%f\n", stack->Double);
-}
-
 void pf__printLong(_pf_Stack *stack)
 /* Print long, just for debugging really */
 {
 printf("%lld\n", stack->Long);
+}
+
+void pf__printFloat(_pf_Stack *stack)
+/* Print double, just for debugging really */
+{
+printf("%f\n", stack->Float);
+}
+
+void pf__printDouble(_pf_Stack *stack)
+/* Print double, just for debugging really */
+{
+printf("%f\n", stack->Double);
 }
 
