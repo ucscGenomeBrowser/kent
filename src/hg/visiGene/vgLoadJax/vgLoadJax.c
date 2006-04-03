d60f043d9ee5c5619409f348a95e10aee3c0cc81
galt <galt>
2006-04-03 22:24:15 +0000

    fixed date

--- src/hg/visiGene/vgLoadJax/vgLoadJax.c
+++ src/hg/visiGene/vgLoadJax/vgLoadJax.c
@@ -303,7 +303,7 @@ ra = mustOpen(raName, "w");
 fprintf(ra, "submissionSource MGI\n");
 fprintf(ra, "acknowledgement Thanks to the Gene Expression Database group at "
             "Mouse Genome Informatics (MGI) for collecting, annotating and sharing "
-	    "this image. The MGI images were last updated in VisiGene on June 17, 2005. "
+	    "this image. The MGI images were last updated in VisiGene on March 28, 2006. "
 	    "Additional and more up to date annotations and images may be available "
 	    "directly at <A HREF='http://www.informatics.jax.org'>MGI.</A>\n");
 fprintf(ra, "submitSet jax%s\n", ref);
