6c9a93132f4708c764535b64f6f6acf862e4fee6
hiram <hiram>
2006-04-04 19:25:02 +0000

    Well, at least have a usage message yourself ...

--- src/utils/mkUsageIndex.sh
+++ src/utils/mkUsageIndex.sh
@@ -2,12 +2,26 @@
 #	mkUsageIndex.sh - create index of usage messages from current
 #	binaries on kolossus in /cluster/bin/x86_64
 
-#	$Id: mkUsageIndex.sh,v 1.1 2006/04/04 19:19:52 hiram Exp $
+#	$Id: mkUsageIndex.sh,v 1.2 2006/04/04 19:25:02 hiram Exp $
 
+usage() {
+    echo "mkUsageIndex.sh - create index of usage messages"
+    echo "usage: mkUsageIndex.sh <anything> > useMessageIndex.html"
+    echo "expects to be working on kolossus"
+    exit 255
+}
+
+if [ $# -lt 1 ]; then
+    usage
+fi
 
 cd /cluster/bin/x86_64
 find . -type f -newer randomPlacement \
 	| sed -e "s/^\.\///" | sort > /tmp/x86_64.list
+
+echo "<HTML><HEAD><TITLE>kent source tree programs</TITLE></HEAD>"
+echo "<BODY>"
+echo "<TABLE>"
 
 cd /tmp
 for F in `cat x86_64.list | egrep -v "windowmasker|hgCGAP"`
@@ -24,3 +38,7 @@ do
     fi
     echo "<TR><TD ALIGN=LEFT>${F}:</TD><TD>${TEXT}</TD></TR>"
 done
+
+echo "</TABLE>"
+echo "</BODY></HTML>"
+
