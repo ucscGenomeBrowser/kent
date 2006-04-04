0936c492e933ae2f932c9087043c3724d5052ea0
kent <kent>
2006-04-04 03:27:52 +0000

    Doing casts of int to long, int to float, int to double

--- src/ai/paraFlow/compiler/pentCast.c
+++ src/ai/paraFlow/compiler/pentCast.c
@@ -20,35 +20,51 @@ struct isxAddress *dest = isx->dest;
 struct isxReg *reg = pentFreeReg(isx, dest->valType, nextNode, coder);
 if (source->reg != reg)
     {
-    char *destName = isxRegName(reg, ivInt);
     pentPrintAddress(coder, source, coder->sourceBuf);
-    pentCoderAdd(coder, "movl", coder->sourceBuf, destName);
+    pentCoderAdd(coder, "movl", coder->sourceBuf, isxRegName(reg, ivInt));
     }
 pentLinkRegSave(dest, reg, coder);
 }
 
-static void castFromInt(struct pfCompile *pfc, 
-	struct isx *isx,
+static void castByOneInstruction(struct isx *isx, struct dlNode *nextNode,
+	struct pentCoder *coder, char *op)
+/* Cast int to long. */
+{
+struct isxAddress *source = isx->left;
+struct isxAddress *dest = isx->dest;
+struct isxReg *reg = pentFreeReg(isx, dest->valType, nextNode, coder);
+pentPrintAddress(coder, source, coder->sourceBuf);
+pentCoderAdd(coder, op, coder->sourceBuf, isxRegName(reg, dest->valType));
+pentLinkRegSave(dest, reg, coder);
+}
+
+static void castIntToBit(struct isx *isx, struct dlNode *nextNode,
+	struct pentCoder *coder)
+/* Convert byte/short/long to bit using combination of test/setnz */
+{
+}
+
+static void castFromInt(struct pfCompile *pfc, struct isx *isx,
 	struct dlNode *nextNode, struct pentCoder *coder)
 /* Create code for cast from long to something else. */
 {
 switch (isx->dest->valType)
     {
     case ivBit:
-        internalErr();
+	uglyAbort("BIts from ints?!");
 	break;
     case ivByte:
     case ivShort:
 	castViaMovl(isx, nextNode, coder);
 	break;
     case ivLong:
-        internalErr();
+	castByOneInstruction(isx, nextNode, coder, "movd");
 	break;
     case ivFloat:
-        internalErr();
+	castByOneInstruction(isx, nextNode, coder, "cvtsi2ss");
 	break;
     case ivDouble:
-        internalErr();
+	castByOneInstruction(isx, nextNode, coder, "cvtsi2sd");
 	break;
     default:
         internalErr();
