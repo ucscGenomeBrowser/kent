71db00c0ba3c0ba308ee3614e4663abbd4f4f336
kent <kent>
2006-05-05 18:45:30 +0000

    doing some testing on chains for Wes Barris

--- src/hg/oneShot/freen/freen.c
+++ src/hg/oneShot/freen/freen.c
@@ -6,8 +6,9 @@
 #include "hash.h"
 #include "obscure.h"
 #include "jksql.h"
+#include "chain.h"
 
-static char const rcsid[] = "$Id: freen.c,v 1.64 2006/02/02 16:24:48 kent Exp $";
+static char const rcsid[] = "$Id: freen.c,v 1.65 2006/05/05 18:45:30 kent Exp $";
 
 void usage()
 {
@@ -21,124 +22,11 @@ int level = 0;
 void freen(char *fileName)
 /* Test some hair-brained thing. */
 {
-struct slName *id, *idList = readAllLines(fileName);
-struct sqlConnection *conn = sqlConnect("visiGene");
-int i;
-char query[256];
-char *s;
-double randSeed;
-struct sqlResult *sr;
-char **row;
-struct dyString *dy = dyStringNew(10*1024);
-int batchSize = 80000;
-struct hash *hash = hashNew(18);
-
-for (id = idList; id != NULL; id = id->next)
-    {
-    hashAdd(hash, id->name, NULL);
-    }
-
-safef(query, sizeof(query), "select rand()");
-randSeed = sqlQuickDouble(conn, query);
-srand(randSeed*12345);
-
-shuffleList(idList, 1);
-id = idList;
-uglyTime(NULL);
-
-#ifdef SOON
-for (i=0; i<batchSize; ++i)
-     {
-     safef(query, sizeof(query), "select preparation from image where id=%s",
-     	id->name);
-     s = sqlQuickString(conn, query);
-     id = id->next;
-     }
-uglyTime("%d like %s", batchSize, query);
-
-for (i=0; i<batchSize; ++i)
-     {
-     safef(query, sizeof(query), 
-     	"select submissionSet.privateUser from image,submissionSet "
-	"where image.id=%s and image.submissionSet=submissionSet.id"
-	, id->name);
-     s = sqlQuickString(conn, query);
-     id = id->next;
-     }
-uglyTime("%d like %s", batchSize, query);
-
-for (i=0; i<batchSize; ++i)
-     {
-     safef(query, sizeof(query), 
-     	"select genotype.alleles from image,specimen,genotype "
-	"where image.id=%s and image.specimen=specimen.id "
-	"and specimen.genotype=genotype.id"
-	, id->name);
-     s = sqlQuickString(conn, query);
-     id = id->next;
-     }
-uglyTime("%d like %s", batchSize, query);
-
-dyStringClear(dy);
-dyStringAppend(dy,
-    "select id,preparation from image where id in (");
-for (i=0; i<batchSize; ++i)
-    {
-    if (i != 0)
-        dyStringAppendC(dy, ',');
-    dyStringAppend(dy, id->name);
-    id = id->next;
-    }
-dyStringAppend(dy, ")");
-sr = sqlGetResult(conn, dy->string);
-while ((row = sqlNextRow(sr)) != NULL)
-    {
-    hashLookup(hash, row[0]);
-    }
-uglyTime("%d simple in", batchSize);
-
-dyStringClear(dy);
-dyStringAppend(dy,
-    "select image.id,submissionSet.privateUser from image,submissionSet "
-    "where image.submissionSet=submissionSet.id "
-    "and image.id in (");
-for (i=0; i<batchSize; ++i)
-    {
-    if (i != 0)
-        dyStringAppendC(dy, ',');
-    dyStringAppend(dy, id->name);
-    id = id->next;
-    }
-dyStringAppend(dy, ")");
-sr = sqlGetResult(conn, dy->string);
-while ((row = sqlNextRow(sr)) != NULL)
-    {
-    hashLookup(hash, row[0]);
-    }
-uglyTime("%d single join in", batchSize);
-#endif /* SOON */
-
-dyStringClear(dy);
-dyStringAppend(dy,
-    "select image.id,genotype.alleles from image,specimen,genotype "
-    "where genotype.id=specimen.genotype "
-    "and specimen.id = image.specimen "
-    "and image.id in (");
-for (i=0; i<batchSize; ++i)
-    {
-    if (i != 0)
-        dyStringAppendC(dy, ',');
-    dyStringAppend(dy, id->name);
-    id = id->next;
-    }
-dyStringAppend(dy, ")");
-sr = sqlGetResult(conn, dy->string);
-while ((row = sqlNextRow(sr)) != NULL)
-    {
-    hashLookup(hash, row[0]);
-    }
-uglyTime("%d double join in", batchSize);
-
+struct chain *chainList = NULL, *chain;
+struct lineFile *lf = lineFileOpen(fileName, TRUE);
+while ((chain = chainRead(lf)) != NULL)
+    slAddHead(&chainList, chain);
+printf("Got %d chains\n", slCount(chainList));
 }
 
 int main(int argc, char *argv[])
