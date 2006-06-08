13639fb5622d21e13a40a96ed4d45a2df15dd7a7
angie <angie>
2006-06-08 15:12:03 +0000

    Resolved -Wall warning: unused variable.

--- src/hg/makeDb/hgMrnaRefseq/hgMrnaRefseq.c
+++ src/hg/makeDb/hgMrnaRefseq/hgMrnaRefseq.c
@@ -18,11 +18,9 @@ errAbort(
 int main(int argc, char *argv[])
 {
 struct sqlConnection *conn, *conn2, *conn3;
-char query[256], query2[256], query3[256];
-struct sqlResult *sr, *sr2, *sr3;
-char **row, **row2, **row3;
-
-char *r1, *r2, *r3, *r4;
+char query[256], query2[256];
+struct sqlResult *sr, *sr2;
+char **row, **row2;
     
 char *chp;
 FILE *o1;
