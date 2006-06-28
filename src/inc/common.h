d08f1d3216e19e485b65259c902c7db51c867d96
kent <kent>
2006-06-28 17:59:35 +0000

    Adding readBits64/writeBits64

--- src/inc/common.h
+++ src/inc/common.h
@@ -818,7 +818,13 @@ boolean fastReadString(FILE *f, char buf[256]);
 /* Read a string into buffer, which must be long enough
  * to hold it.  String is in 'writeString' format. 
  * Returns FALSE at EOF. */
- 
+
+void writeBits64(FILE *f, bits64 x);
+/* Write out 64 bit number in manner that is portable across architectures */
+
+bits64 readBits64(FILE *f);
+/* Write out 64 bit number in manner that is portable across architectures */
+
 void carefulClose(FILE **pFile);
 /* Close file if open and null out handle to it. */
 
