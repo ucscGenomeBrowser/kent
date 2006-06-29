bf4d7fc09443b4b45be2c34310b8892921fd4707
kent <kent>
2006-06-29 01:27:45 +0000

    Freen tests read/writeBits64

--- src/hg/oneShot/freen/freen.c
+++ src/hg/oneShot/freen/freen.c
@@ -6,7 +6,7 @@
 #include "hash.h"
 #include "binRange.h"
 
-static char const rcsid[] = "$Id: freen.c,v 1.66 2006/05/07 18:21:55 kent Exp $";
+static char const rcsid[] = "$Id: freen.c,v 1.67 2006/06/29 01:27:45 kent Exp $";
 
 void usage()
 {
@@ -20,8 +20,14 @@ int level = 0;
 void freen(char *fileName)
 /* Test some hair-brained thing. */
 {
-int size = atoi(fileName);
-binKeeperNew(0, size);
+FILE *f = mustOpen(fileName, "wb");
+bits64 x = 0x123456789ABCDEF0;
+uglyf("before = %llx\n", x);
+writeBits64(f, x);
+carefulClose(&f);
+f = mustOpen(fileName, "rb");
+x = readBits64(f);
+uglyf("after = %llx\n", x);
 }
 
 int main(int argc, char *argv[])
