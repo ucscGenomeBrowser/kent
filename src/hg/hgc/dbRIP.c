28c7d72c316317314651348eacc281a0af389d5a
hiram <hiram>
2006-07-14 18:06:51 +0000

    Now doing the allele frequency table, still need to know how to do Unbiased Heterozygosity

--- src/hg/hgc/dbRIP.c
+++ src/hg/hgc/dbRIP.c
@@ -9,11 +9,112 @@
 #include "cheapcgi.h"
 #include "htmshell.h"
 #include "dbRIP.h"
+#include "polyGenotype.h"
 
-static char const rcsid[] = "$Id: dbRIP.c,v 1.1 2006/07/13 18:04:06 hiram Exp $";
+static char const rcsid[] = "$Id: dbRIP.c,v 1.2 2006/07/14 18:06:51 hiram Exp $";
 
-extern void genericClickHandlerPlus(
-        struct trackDb *tdb, char *item, char *itemForUrl, char *plus);
+
+static int sortEthnicGroup(const void *e1, const void *e2)
+/* used by slSort to sort the polyGenotype in order of ethnic group name */
+{
+const struct polyGenotype *p1 = *((struct polyGenotype**)e1);
+const struct polyGenotype *p2 = *((struct polyGenotype**)e2);
+return(strcmp(p1->ethnicGroup, p2->ethnicGroup));
+}
+
+static void polyTable(char *databaseId)
+{
+struct sqlConnection *conn = hAllocConn();
+char query[256];
+struct sqlResult *sr;
+char **row;
+struct polyGenotype *pgList = NULL;
+
+sprintf(query, "select * from polyGenotype where databaseId = '%s'", databaseId);
+
+sr = sqlGetResult(conn, query);
+while ((row = sqlNextRow(sr)) != NULL)
+    {
+    struct polyGenotype *pg;
+
+    AllocVar(pg);
+    pg = polyGenotypeLoad(row);
+    slAddHead(&pgList, pg);
+    }
+sqlFreeResult(&sr);
+hFreeConn(&conn);
+if (slCount(pgList) > 0)
+    {
+    struct polyGenotype *pg;
+    int totalPlusPlus = 0;
+    int totalPlusMinus = 0;
+    int totalMinusMinus = 0;
+    int totalSamples = 0;
+    double alleleFrequency = 0.0;
+    double unbiasedHeterozygosity = 0.0;
+    int sampleSize = 0;
+    int plusAlleles = 0;
+    int minusAlleles = 0;
+
+    slSort(&pgList,sortEthnicGroup);
+
+    printf("<B>Polymorphism Frequencies and Genotypes:</B><BR>\n");
+    /*	This surrounding table avoids the problem of the border lines of
+     *	the second table go missing.  Something about the style sheet ...
+     */
+    printf("<TABLE BGCOLOR=\"#FFFEE8\" CELLPADDING=0><TR><TD>\n");
+    printf("<TABLE BORDER=2 bgcolor=\"#e4cdf2\">\n");
+    printf("<TR bgcolor=\"#d2abf2\"><TH align=center>Ethnic Group</TH>");
+    printf("<TH align=center>Sample Size</TH>");
+    printf("<TH align=center>Allele Frequency</TH>");
+    printf("<TH align=center>+/+</TH>");
+    printf("<TH align=center>+/-</TH>");
+    printf("<TH align=center>-/-</TH>");
+    printf("<TH align=center>Unbiased Heterozygosity</TH>");
+    printf("</TR>\n");
+    for (pg = pgList; pg != NULL; pg = pg->next)
+	{
+	plusAlleles = (pg->plusPlus << 1) + pg->plusMinus;
+	minusAlleles = (pg->minusMinus << 1) + pg->plusMinus;
+
+	sampleSize = pg->plusPlus + pg->plusMinus + pg->minusMinus;
+	alleleFrequency = (double)plusAlleles /
+		(double)(plusAlleles + minusAlleles);
+	unbiasedHeterozygosity = 1.0 - (alleleFrequency*alleleFrequency);
+
+	printf("<TR><TD align=left>%s</TD>", pg->ethnicGroup);
+	printf("<TD align=center>%d</TD>", sampleSize);
+	printf("<TD align=center>%.3f</TD>", alleleFrequency);
+	printf("<TD align=center>%d</TD>", pg->plusPlus);
+	printf("<TD align=center>%d</TD>", pg->plusMinus);
+	printf("<TD align=center>%d</TD>", pg->minusMinus);
+	printf("<TD align=center>%.3f</TD>", unbiasedHeterozygosity);
+	printf("</TR>\n");
+
+	totalSamples += sampleSize;
+	totalPlusPlus += pg->plusPlus;
+	totalMinusMinus += pg->minusMinus;
+	totalPlusMinus += pg->plusMinus;
+	}
+
+    plusAlleles = (totalPlusPlus << 1) + totalPlusMinus;
+    minusAlleles = (totalMinusMinus << 1) + totalPlusMinus;
+    alleleFrequency = (double)plusAlleles/(double)(plusAlleles + minusAlleles);
+    unbiasedHeterozygosity = 1.0 - (alleleFrequency*alleleFrequency);
+    printf("<TR><TH align=left>All Samples</TH>");
+    printf("<TH align=center>%d</TH>", totalSamples);
+    printf("<TH align=center>%.3f</TH>", alleleFrequency);
+    printf("<TH align=center>%d</TH>", totalPlusPlus);
+    printf("<TH align=center>%d</TH>", totalPlusMinus);
+    printf("<TH align=center>%d</TH>", totalMinusMinus);
+    printf("<TH align=center>%.3f</TH>", unbiasedHeterozygosity);
+    printf("</TR>\n");
+    printf("</TABLE>\n");
+    printf("</TD></TR></TABLE>\n");
+    }
+else
+    printf("<B>Polymorphism Frequencies and Genotypes:</B>&nbsp;NA<BR>\n");
+}
 
 void dbRIP(struct trackDb *tdb, char *item, char *itemForUrl)
 /* Put up dbRIP track info */
@@ -76,11 +177,11 @@ while ((row = sqlNextRow(sr)) != NULL)
     printf("<B>Ascertaining Method:</B>&nbsp;%s<BR>\n", loadItem->ascertainingMethod);
     printf("<B>Genome Source:</B>&nbsp;%s<BR>\n", loadItem->polySource);
     printf("<B>Remarks:</B>&nbsp;%s<BR>\n", loadItem->remarks);
-    printf("<B>Polymorphism Frequencies and Genotypes:</B>&nbsp;%s<BR>\n", loadItem->genoRegion);
     if (strlen(loadItem->genoRegion) > 0)
 	printf("<B>Genomic region:</B>&nbsp;%s<BR>\n", loadItem->genoRegion);
     else
 	printf("<B>Genomic region:</B>&nbsp;NA<BR>\n");
+    polyTable(loadItem->name);
     printPosOnChrom(loadItem->chrom, loadItem->chromStart, loadItem->chromEnd,
 	loadItem->strand, TRUE, loadItem->name);
     printf("<HR>\n<B>Reference(s):</B>&nbsp;%s\n<HR>\n", loadItem->reference);
