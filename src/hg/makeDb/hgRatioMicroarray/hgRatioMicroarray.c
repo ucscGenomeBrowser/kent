2c1a64b8c9ec5362b207787e4192a4dc58d055dc
aamp <aamp>
2006-07-21 21:57:10 +0000

    Fixed a bug that would treat missing data in microarrays (designated -10000) as real.  Along with all this, the new microarray.c is used instead of the ratio/median business that used to be here (which is duplicated often throughout microarray CGIs and utilities).  One thing to note is that this version doesn't discard probes with minimal expression, rather it'll set them all as missing data.

--- src/hg/makeDb/hgRatioMicroarray/hgRatioMicroarray.c
+++ src/hg/makeDb/hgRatioMicroarray/hgRatioMicroarray.c
@@ -8,8 +8,9 @@
 #include "portable.h"
 #include "expData.h"
 #include "hgRelate.h"
+#include "microarray.h"
 
-static char const rcsid[] = "$Id: hgRatioMicroarray.c,v 1.4 2006/05/09 14:36:05 angie Exp $";
+static char const rcsid[] = "$Id: hgRatioMicroarray.c,v 1.5 2006/07/21 21:57:10 aamp Exp $";
 
 /* Command line overridable options. */
 char *database = "hgFixed";
@@ -70,196 +71,46 @@ static struct optionSpec options[] = {
    {NULL, 0},
 };
 
-struct medSpec
-/* A specification for median. */
-    {
-    struct medSpec *next;
-    char *name;		/* Column name. */
-    char *group;	/* Tissue/group name. */
-    int count;		/* Count of experiments to median. */
-    int *ids;		/* Id's (indexes) of experiments to median. */
-    };
-
-struct medSpec *medSpecReadAll(char *fileName)
-/* Read in file and parse it into medSpecs. */
-{
-struct lineFile *lf = lineFileOpen(fileName, TRUE);
-struct medSpec *medList = NULL, *med;
-char *row[256], *line;
-int i, count;
-
-while (lineFileNextReal(lf, &line))
-    {
-    char *name = nextQuotedWord(&line);
-    char *group = nextQuotedWord(&line);
-    if (group == NULL)
-	lineFileShort(lf);
-    count = chopLine(line, row);
-    if (count < 1)
-        lineFileShort(lf);
-    if (count >= ArraySize(row))
-        errAbort("Too many experiments in median line %d of %s",
-		lf->lineIx, lf->fileName);
-    AllocVar(med);
-    med->name = cloneString(name);
-    med->group = cloneString(group);
-    med->count = count;
-    AllocArray(med->ids, count);
-    for (i=0; i<count; ++i)
-        {
-	char *asc = row[i];
-	if (!isdigit(asc[0]))
-	    errAbort("Expecting number got %s line %d of %s", asc,
-	    	lf->lineIx, lf->fileName);
-	med->ids[i] = atoi(asc);
-	}
-    slAddHead(&medList, med);
-    }
-lineFileClose(&lf);
-slReverse(&medList);
-return medList;
-}
-
-static void clumpMedians(float *vals, int valCount,
-	struct medSpec *clumpList, double *clumpedVals)
-/* Fill in medians of full in clump list. */
-{
-struct medSpec *clump;
-int clumpIx = 0;
-double *selVals, val;
-float median;
-AllocArray(selVals, valCount);
-for (clump = clumpList; clump != NULL; clump = clump->next, ++clumpIx)
-    {
-    int i, realCount = 0;
-    if (clump->count > valCount)
-        internalErr();
-    for (i=0; i<clump->count; ++i)
-	{
-	int ix = clump->ids[i];
-	if (ix >= valCount)
-	    errAbort("%d index in median list, but only %d total", 
-		    ix, valCount);
-	val = vals[ix];
-	selVals[realCount] = vals[ix];
-	++realCount;
-	}
-    if (realCount < 1)
-	median = 0;
-    else
-	median = doubleMedian(realCount, selVals);
-    clumpedVals[clumpIx] = median;
-    }
-freeMem(selVals);
-}
-
-static float getDenominator(struct expData *ex, boolean doAve,
-	struct medSpec *clumpList)
-/* Figure out median or average to use as denominator for ratios. */
-{
-double *data = NULL;
-int i, dataCount;
-float q;
-int goodCount = 0;
-if (clumpList != NULL)
-    {
-    dataCount = slCount(clumpList);
-    AllocArray(data, dataCount);
-    clumpMedians(ex->expScores, ex->expCount, clumpList, data);
-    }
-else
-    {
-    dataCount = ex->expCount;
-    AllocArray(data, dataCount);
-    for (i=0; i<dataCount; ++i)
-	data[i] = ex->expScores[i];
-    }
-for (i=0; i<dataCount; ++i)
-    if (data[i] >= minMaxVal)
-        ++goodCount;
-if (goodCount < 1)
-    return -1;
-if (doAve)
-    {
-    float sum = 0;
-    for (i=0; i<dataCount; ++i)
-	 sum += data[i];
-    q = sum/dataCount;
-    }
-else
-    {
-    q = doubleMedian(dataCount, data);
-    }
-freeMem(data);
-return q;
-}
-
-static boolean convertToRatio(struct expData *ex, boolean doAve,
-	struct medSpec *clumpList)
-/* Convert ex->expScores to ratios.  Returns FALSE if can't. */
-{
-float q = getDenominator(ex, doAve, clumpList);
-float invQ;
-int i;
-
-if (q <= 0)
-    return FALSE;
-invQ = 1.0/q;
-for (i=0; i<ex->expCount; ++i)
-    {
-    ex->expScores[i] = logBase2(ex->expScores[i]*invQ);
-    }
-return TRUE;
-}
-
-static void clipMin(int count, float *scores, float minGood, float minVal)
-/* Clip everything below minGood and set it to minVal. */
-{
-int i;
-for (i=0; i<count; ++i)
-    {
-    if (scores[i] < minGood)
-        scores[i] = minVal;
-    }
-}
 
 void hgRatioMicroarray(char *absTable, char *relTable)
 /* hgRatioMicroarray - Create a ratio form of microarray. */
 {
-struct medSpec *clumpList = NULL;
+struct maMedSpec *clumpList = NULL;
 struct sqlConnection *conn = sqlConnect(database);
 struct sqlResult *sr;
 char **row;
 char query[512];
 struct expData *ex;
+struct expData *expList = NULL;
 FILE *f = hgCreateTabFile(tabDir, relTable);
 int rowCount = 0;
 
 if (clump != NULL)
-    clumpList = medSpecReadAll(clump);
+    clumpList = maMedSpecReadAll(clump);
+
 safef(query, sizeof(query),
 	"select * from %s", absTable);
 sr = sqlGetResult(conn, query);
 while ((row = sqlNextRow(sr)) != NULL)
     {
     ex = expDataLoad(row);
-    clipMin(ex->expCount, ex->expScores, minAbsVal, minAbsVal*0.5);
-    if (convertToRatio(ex, doAverage, clumpList))
-	expDataTabOut(ex, f);
-    expDataFree(&ex);
-    ++rowCount;
+    slAddHead(&expList, ex);
     if (limit != 0 && rowCount >= limit)
         break;
     }
 sqlFreeResult(&sr);
-
+slReverse(&expList);
+maExpDataClipMin(expList, minAbsVal, minAbsVal * 0.5);
+maExpDataDoLogRatioGivenMedSpec(expList, clumpList, (doAverage) ? useMean : useMedian);
+for (ex = expList; ex != NULL; ex = ex->next)
+    expDataTabOut(ex, f);
 if (doLoad)
     {
     expDataCreateTable(conn, relTable);
     hgLoadTabFile(conn, tabDir, relTable, &f);
     hgRemoveTabFile(tabDir, relTable);
     }
-
+expDataFreeList(&expList);
 sqlDisconnect(&conn);
 }
 
