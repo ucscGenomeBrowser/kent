c71b10f4c9ac58cf636f24e758395945913c2915
markd <markd>
2006-07-26 04:55:36 +0000

    added one more level to includes to handle outside directory

--- src/inc/common.mk
+++ src/inc/common.mk
@@ -5,7 +5,7 @@ endif
 CFLAGS=
 HG_DEFS=-D_FILE_OFFSET_BITS=64 -D_LARGEFILE_SOURCE -D_GNU_SOURCE -DMACHTYPE_${MACHTYPE}
 HG_WARN=-Wformat -Wimplicit -Wuninitialized -Wreturn-type
-HG_INC=-I../inc -I../../inc -I../../../inc -I../../../../inc
+HG_INC=-I../inc -I../../inc -I../../../inc -I../../../../inc -I../../../../../inc
 
 # add the follow to makefiles to enable stronger warning checks
 # HG_WARN = ${HG_WARN_ERR}
