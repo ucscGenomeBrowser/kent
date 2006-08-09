ebcd671135adc0046584586c27108e31f22e4356
heather <heather>
2006-08-09 02:03:06 +0000

    off by one in row index

--- src/hg/snp/snpLoad/snpContigLocFilter125.c
+++ src/hg/snp/snpLoad/snpContigLocFilter125.c
@@ -10,7 +10,7 @@
 #include "hash.h"
 #include "hdb.h"
 
-static char const rcsid[] = "$Id: snpContigLocFilter125.c,v 1.3 2006/08/09 01:58:37 heather Exp $";
+static char const rcsid[] = "$Id: snpContigLocFilter125.c,v 1.4 2006/08/09 02:03:06 heather Exp $";
 
 static char *snpDb = NULL;
 static char *contigGroup = NULL;
@@ -210,7 +210,7 @@ while ((row = sqlNextRow(sr)) != NULL)
 	if (end == -1) continue;
 
 	fprintf(f, "%s\t%s\t%s\t%s\t%d\t%d\t%s\t%s\t%s\n", 
-	            row[0], row[1], (char *)el1->val, row[2], start, end, row[6], row[7], (char *)el2->val);
+	            row[0], row[1], (char *)el1->val, row[2], start, end, row[5], row[6], (char *)el2->val);
 	}
     }
 sqlFreeResult(&sr);
