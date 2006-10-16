637496e2908c1a172bb952a192d5beff080d9856
fanhsu <fanhsu>
2006-10-16 16:46:48 +0000

    Restructured and updated.  Changed into offset/length based instead of text files output.

--- src/hg/protein/omimParseRec/omimParseRec.c
+++ src/hg/protein/omimParseRec/omimParseRec.c
@@ -1,4 +1,5 @@
-/* omimParseRec - parse text of OMIM records into files according to different field types */
+/* omimParseRec - parse text of OMIM records by different field types */
+
 #include "common.h"
 #include "linefile.h"
 #include "hash.h"
@@ -8,59 +9,19 @@
 #include "portable.h"
 #include "obscure.h"
 
-static char const rcsid[] = "$Id: omimParseRec.c,v 1.1 2006/10/09 21:21:13 fanhsu Exp $";
+static char const rcsid[] = "$Id: omimParseRec.c,v 1.2 2006/10/16 16:46:48 fanhsu Exp $";
 
-FILE *omimTitle;
-FILE *omimTitleAlt;
-
-FILE *omimAv;
-FILE *omimCd;
-FILE *omimCn;
-FILE *omimCs;
-FILE *omimEd;
-FILE *omimRf;
-FILE *omimSa;
-FILE *omimTx;
-
-#define MAX_TX_SEC_TYPE 27
-
-char txSecType[MAX_TX_SEC_TYPE][80]={
-"ANIMAL MODEL",
-"BIOCHEMICAL FEATURES",
-"CLINICAL FEATURES",
-"CLINICAL MANAGEMENT",
-"CLONING",
-"CYTOGENETICS AND MAPPING",
-"CYTOGENETICS",
-"DESCRIPTION",
-"DIAGNOSIS",
-"EVOLUTION",
-"GENE FAMILY",
-"GENE FUNCTION",
-"GENE STRUCTURE",
-"GENE THERAPY",
-"GENETIC VARIABILITY",
-"GENOTYPE",
-"GENOTYPE/PHENOTYPE CORRELATIONS",
-"HETEROGENEITY",
-"HISTORY",
-"INHERITANCE",
-"MAPPING",
-"MOLECULAR GENETICS",
-"NOMENCLATURE",
-"OTHER FEATURES",
-"PATHOGENESIS",
-"PHENOTYPE",
-"POPULATION GENETICS"
-};
+FILE *fh2; /* 2nd file handle pointing to the OMIM text file */
+FILE *recFh;
+FILE *fieldFh;
 
 void usage()
 /* Explain usage and exit. */
 {
 errAbort(
-  "omimParseRec - parse text of OMIM records into files according to different field types\n"
+  "omimParseRec - parse OMIM records by different field types\n"
   "usage:\n"
-  "   omimParseRec omimText outDir\n"
+  "   omimParseRec omimText outRecFile outFieldFile\n"
   );
 }
 
@@ -76,6 +37,9 @@ struct omimRecord
     char *title;	/* OMIM title */
     char *geneSymbol;	/* gene symbol */
 
+    int  startPos;
+    int  endPos;
+    
     char *av;
     char *cd;
     char *cn;
@@ -85,6 +49,17 @@ struct omimRecord
     char *sa;
     char *tx;
   };
+
+struct omimField
+/* An OMIM field. */
+    {
+    char *omimId;	/* OMIM ID */
+    char *type;		/* field type */
+    char *title;	/* field title */
+
+    int  startPos;
+    int  endPos;
+    };
 
 struct omimLitRef 
 /* A OMIM literature reference. */
@@ -101,9 +76,29 @@ struct omimLitRef
     char *doiId;	/* DOI ID, may be NULL. */
     };
 
+char buffer[80*10000];
+
+void printField(struct omimField *omimFd, FILE *outf)
+/* print out field info */
+{
+long bytesRead;
+
+fprintf(outf, "%s\t%s\t%d\t%d\n", 
+       omimFd->omimId, omimFd->type, omimFd->startPos, omimFd->endPos - omimFd->startPos + 1);
+       fflush(stdout);
+       //omimFd->omimId, omimFd->type, omimFd->startPos, omimFd->endPos);fflush(stdout);
+	
+fseek(fh2, (long)(omimFd->startPos), SEEK_SET);
+bytesRead = (long)fread(buffer, (size_t)1, (size_t)(omimFd->endPos - omimFd->startPos + 1), fh2);
+*(buffer+bytesRead) = '\0';
+printf("%s\t%s===>%s<===%s\n", omimFd->omimId, omimFd->type, buffer, omimFd->type); fflush(stdout);
+}
+    
 struct omimRecord *omimRecordNext(struct lineFile *lf, 
 	struct lm *lm, 	/* Local memory pool for this structure. */
-	struct dyString *dy)	/* Scratch string to use. */
+	struct dyString *dy,	/* Scratch string to use. */
+	FILE *recFh,
+	FILE *fieldFh)
 /* Read next record from file and parse it into omimRecord structure
  * that is allocated in memory. */
 {
@@ -119,28 +114,37 @@ boolean recDone, fieldDone;
 char *row[1];
 char *upperStr;
 
-char avPosLine[255];
+boolean endOfFile = FALSE;
+boolean endOfRec  = FALSE;
 
-char avPosLine1[255];
-char avPosLine2[255];
-char avPosLine3[255];
-char avPosLine4[255];
+boolean firstFieldDone = FALSE;
+
+char avPosLine[255];
+struct omimField *omimPrevFd = NULL;
+struct omimField *omimFd = NULL;
 
 recDone = FALSE;
-    /* Parse record number and title lines. */
+
+/* Parse record number and title lines. */
     if (!lineFileRow(lf, row))
 	return NULL;
     if (!sameString(row[0], "*RECORD*"))
 	{
 	errAbort("Expecting *RECORD* line %d of %s", lf->lineIx, lf->fileName);
     	}
+    
+    lmAllocVar(lm, omimr);
+    omimr->startPos = lf->lineStart + lf->bufOffsetInFile;
+    
+    //lmAllocVar(lm, omimFd);
+
     if (!lineFileNext(lf, &line, &lineSize))
 	return NULL;
+    
     if (!sameString(line, "*FIELD* NO"))
 	errAbort("Expecting *FIELD* NO line %d of %s", lf->lineIx, lf->fileName);
     
     if (!lineFileNextReal(lf, &line)) errAbort("%s ends in middle of a record", lf->fileName);
-    lmAllocVar(lm, omimr);
     omimr->id = lmCloneString(lm, line);
     
     if (!lineFileNextReal(lf, &line)) errAbort("%s ends in middle of a record", lf->fileName);
@@ -174,10 +178,11 @@ recDone = FALSE;
    	} 
     chp = chp + strlen(omimr->id); chp++;
     omimr->title = lmCloneString(lm, chp);
-    fprintf(omimTitle, "%s\t%c\t%s\t%s\n", omimr->id, omimr->type, omimr->geneSymbol, omimr->title);
-    fflush(omimTitle); 
+    
+    // !!! need to enhance it later to include startPos and length info.
+    fprintf(recFh, "%s\t%c\t%s\t%s\n", omimr->id, omimr->type, omimr->geneSymbol, omimr->title);
    
-    printf("%s|%s|%s\n", omimr->id, omimr->title, omimr->geneSymbol);
+    //printf("%s|%s|%s\n", omimr->id, omimr->title, omimr->geneSymbol);
     
     /* !!! temporarily skip lines before first FIELD after title */
     /* further processing TBD */
@@ -189,14 +194,37 @@ recDone = FALSE;
     lineFileReuse(lf);
     
     /* process a field */
+    firstFieldDone = FALSE;
     fieldDone = FALSE;
     while (!recDone)
     	{
-        if (!lineFileNext(lf, &line, &lineSize)) break;
-	if (strstr(line, "*RECORD*") != NULL) 
+        if (!lineFileNext(lf, &line, &lineSize)) 
+	    {
+	    endOfFile = TRUE;
+	    }
+	    
+	/* "*THEEND*" signals the end of the OMIM text file */
+	if (sameWord(line, "*THEEND*")) endOfFile = TRUE;
+	
+	if (!endOfFile && (strstr(line, "*RECORD*") != NULL)) 
+	    {
+	    endOfRec = TRUE;
+	    }
+	    
+	/* handle termination of record here */    
+	if (endOfFile || endOfRec)
 	    {
 	    lineFileReuse(lf);
 	    recDone = TRUE;
+    	    if (firstFieldDone) 
+	    	{
+		/* extra minus 1 to get rid of empty line at the end */
+		omimr ->endPos = lf->lineStart + lf->bufOffsetInFile - 1 - 1;
+		omimFd->endPos = lf->lineStart + lf->bufOffsetInFile - 1 - 1;
+		
+	   	/* End of record or end of line also means end of a field.  Print the field info */
+		printField(omimFd, fieldFh);
+		} 
 	    break;
 	    }
 	
@@ -204,129 +232,46 @@ recDone = FALSE;
 	if (chp != NULL)
 	    {
 	    fieldType = chp + strlen("*FIELD* ");
-	   
-	    fh = NULL;
-	    
-	    if (sameWord(fieldType, "AV")) fh = omimAv;	 
-	    if (sameWord(fieldType, "CD")) fh = omimCd;	 
-	    if (sameWord(fieldType, "CN")) fh = omimCn;	 
-	    if (sameWord(fieldType, "CS")) fh = omimCs;	 
-	    if (sameWord(fieldType, "ED")) fh = omimEd;	 
-	    if (sameWord(fieldType, "RF")) fh = omimRf;	 
-	    if (sameWord(fieldType, "SA")) fh = omimSa;	 
-	    if (sameWord(fieldType, "TX")) fh = omimTx;	 
-	    
-	    if (fh == NULL)
+	    if (!firstFieldDone)
 	    	{
-		fprintf(stderr, "Uncognized field type: %s from line: %s\n", fieldType, line);
-		exit(1);
+    	    	lmAllocVar(lm, omimFd);
+		firstFieldDone = TRUE;
+		/* do not print anything at the first "*FIELD*" line */
+		}
+	    else
+	    	{
+		/* extra minus 1 to get rid of empty line at the end */
+		omimFd->endPos = lf->lineStart + lf->bufOffsetInFile - 1 - 1;
+	   	/* print previous field info */
+		printField(omimFd, fieldFh);
 		}
 		
-	    fprintf(fh, "***\t%s\t%s\n", omimr->id, fieldType);
-           
-	    fieldDone = FALSE;
-	    while (!fieldDone)
-	    	{
-	    	if (!lineFileNext(lf, &line, &lineSize)) 
-		   {
-		   fieldDone = TRUE;
-		   recDone   = TRUE;
-		   break;
-	    	   }
-		if (strstr(line, "*FIELD*") != NULL) 
-	    	   {
-		   fieldDone = TRUE;
-	    	   lineFileReuse(lf);
-		   break;
-		   }
-		if (strstr(line, "*RECORD*") != NULL) 
-	    	    {
-	    	    fieldDone = TRUE;
-		    recDone = TRUE;
-		    lineFileReuse(lf);
-	    	    break;
-	    	    }
-	        if (lineSize > 0)
-		    {
-		    /* further processing of TX field */
-		    /* TBD */
-		    if (sameWord(fieldType, "TX"))
-		    	{
-			for (j=0; j<MAX_TX_SEC_TYPE; j++)
-			    {
-			    if (strcmp(line, txSecType[j])==0) 
-			    	{
-				//printf("\t%s\n", line);fflush(stdout);
-				}
-			    }
-		    	}
-			
-		    /* further processing of AV field */
-		    /* TBD */
-		    if (sameWord(fieldType, "AV"))
-		    	{
-			}
-		    fprintf(fh, "%s\n", line);
-		    }
-		else
-		    {
-		    fprintf(fh, "\n");
-		    }
-		}
-	    }
-	else
-	    {
-	    printf("GOT %s\n", line);fflush(stdout);
-	    errAbort("expecting a FIELD line \n");
-	    }
-	fflush(fh);
+	    /* fill in current field info */		
+	    omimFd->omimId   = cloneString(omimr->id);
+	    omimFd->type     = cloneString(fieldType);
+	    omimFd->startPos = lf->lineEnd + lf->bufOffsetInFile;
+            }
 	}
-    
+if (endOfFile) return NULL;    
 return omimr;
 }
 
-/* ------------- Start main program ------------------  */
-
-FILE *createAt(char *dir, char *file)
-/* Open file in dir. */
+void omimParseRec(char *omimTextFile, FILE *recFh, FILE *fieldFh)
+/* omimParseRec - parse OMIM flat text file. */
 {
-char path[PATH_LEN];
-safef(path, sizeof(path), "%s/%s.txt", dir, file);
-return mustOpen(path, "w");
-}
-
-void omimParseRec(char *datFile, char *tabDir)
-/* omimParseRec - Create a relational database out of OMIM flat file. */
-{
-struct lineFile *lf = lineFileOpen(datFile, TRUE);
+struct lineFile *lf = lineFileOpen(omimTextFile, TRUE);
 struct omimRecord *omimr;
 struct dyString *dy = newDyString(4096);
-
-/* We have many tables to make this fully relational and not
- * lose any info. Better start opening files. */
-
-makeDir(tabDir);
-
-omimTitle    = createAt(tabDir, "omimTitle");
-omimTitleAlt = createAt(tabDir, "omimTitleAlt");
-omimAv 	     = createAt(tabDir, "omimAv");
-omimCd 	     = createAt(tabDir, "omimCd");
-omimCn 	     = createAt(tabDir, "omimCn");
-omimCs 	     = createAt(tabDir, "omimCs");
-omimEd 	     = createAt(tabDir, "omimEd");
-omimRf 	     = createAt(tabDir, "omimRf");
-omimSa 	     = createAt(tabDir, "omimSa");
-omimTx 	     = createAt(tabDir, "omimTx");
 
 for (;;)
     {
     struct lm *lm = lmInit(8*1024);
     char *acc;
     struct slName *n;
-    omimr = omimRecordNext(lf, lm, dy);
+    omimr = omimRecordNext(lf, lm, dy, recFh, fieldFh);
     if (omimr == NULL)
         break;
-
+    //printf("%s\t%d\t%d\n", omimr->id, omimr->startPos, omimr->endPos);fflush(stdout);
     lmCleanup(&lm);
     }
 dyStringFree(&dy);
@@ -336,18 +281,14 @@ int main(int argc, char *argv[])
 /* Process command line. */
 {
 optionInit(&argc, argv, options);
-if (argc != 3)
+if (argc != 4)
     usage();
-omimParseRec(argv[1], argv[2]);
 
-fclose(omimAv);
-fclose(omimCd);
-fclose(omimCn);
-fclose(omimCs);
-fclose(omimEd);
-fclose(omimRf);
-fclose(omimSa);
-fclose(omimTx);
+fh2     = mustOpen(argv[1], "r");
+recFh   = mustOpen(argv[2], "w");
+fieldFh = mustOpen(argv[3], "w");
+
+omimParseRec(argv[1], recFh, fieldFh);
 
 return 0;
 }
