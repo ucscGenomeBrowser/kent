3e5fd48163be07556fdb7ddc6b1e1d8c41c6376b
fanhsu <fanhsu>
2006-10-23 21:06:12 +0000

    Updated omimParseAv.c to be offset/length based.

--- src/hg/protein/omimParseAv/omimParseAv.c
+++ src/hg/protein/omimParseAv/omimParseAv.c
@@ -1,22 +1,36 @@
 /* omimParseAv - parse the AV fields of OMIM records */
+
 #include "common.h"
 #include "linefile.h"
 #include "hash.h"
+#include "hdb.h"
 #include "options.h"
 #include "localmem.h"
 #include "dystring.h"
 #include "portable.h"
 #include "obscure.h"
 
-static char const rcsid[] = "$Id: omimParseAv.c,v 1.1 2006/10/09 21:21:59 fanhsu Exp $";
+static char const rcsid[] = "$Id: omimParseAv.c,v 1.2 2006/10/23 21:06:12 fanhsu Exp $";
 
-char *omimID;
-char *avNum;
+boolean avDebug = FALSE;
+
+char buffer[80*5000];
 
 char *fieldType;
 FILE *omimAvPos;
 
 char empty[1] = {""};
+
+struct omimSubField
+/* An OMIM sub field. */
+    {
+    char *omimId;       /* OMIM ID */
+    char *fieldType;    /* field type */
+    char *subFieldId;	/* sub-field ID */
+    int  startPos;	/* offset of starting position */
+    int  subFieldLen;	/* length of text of the sub-field */
+    char *text;		/* text of the sub-field */
+    };
 
 char aaCode3[20][4] = {
 "ALA",
@@ -70,7 +84,7 @@ void usage()
 errAbort(
   "omimParseAv - parse the AV fields of OMIM records \n"
   "usage:\n"
-  "   omimParseAv avInFile avPosOutFile\n"
+  "   omimParseAv omimDb omimTextFile avSubOutFile avPosOutFile\n"
   );
 }
 
@@ -78,9 +92,9 @@ static struct optionSpec options[] = {
    {NULL, 0},
 };
 
-/* check the input line to see if it contains a patern of AA substitution (AAAnnAAA) */
-
-boolean findAaSub(char *lineIn, FILE *avPosFh)
+boolean findAaSub(char *lineIn, struct omimSubField *subFd, FILE *avPosFh)
+/* check the input line to see if it contains a patern of AA substitution (AAAnnAAA) 
+   and print out a record if it does. */
 {
 char *words[300];
 char inStr[300];
@@ -171,7 +185,8 @@ for (j=0; j<wcnt; j++)
 	    {
 	    fprintf(avPosFh, 
 	    	    "%s\t%s\t%s\t%d\t%c\t%c\n", 
-		    omimID, avNum, words[j], avPos, aa1, aa2);
+		    subFd->omimId, subFd->subFieldId, words[j], avPos, aa1, aa2);
+	    fflush(avPosFh);
 	    result = TRUE;
 	    return(result);
 	    }
@@ -180,68 +195,165 @@ for (j=0; j<wcnt; j++)
     
 return(result);
 }
-
-/* ------------- Start main program ------------------  */
-
-void omimParseAv(char *infName, FILE *outFh)
+ 
+struct omimSubField *newSubFd(struct lm *lm, char *omimId, char *fieldType)
+/* allocat and initialize a new omimSubField object */
 {
-FILE *inf;
+struct omimSubField *subFd;
+
+lmAllocVar(lm, subFd);
+subFd->omimId    = omimId;
+subFd->fieldType = fieldType;
+return(subFd);
+}
+
+void printSubField(struct omimSubField *subFd, FILE *fh2, FILE *avSubFh)
+/* print out the sub-field data */
+{
+int bytesRead;
+
+fseek(fh2, (long)(subFd->startPos), SEEK_SET);
+if (subFd->subFieldLen >= sizeof(buffer))
+    {
+    fprintf(stderr, "Subfield %s %s length %d exceeds the size of buffer %d.\n",
+             subFd->omimId, subFd->subFieldId, subFd->subFieldLen, (int)sizeof(buffer));
+    exit(1);
+    }
+bytesRead = (long)fread(buffer, (size_t)1, (size_t)(subFd->subFieldLen), fh2);
+*(buffer + subFd->subFieldLen) = '\0';
+
+fprintf(avSubFh, "%s\t%s\t%s\t%d\t%d\n", 
+       subFd->omimId, subFd->fieldType, subFd->subFieldId, subFd->startPos, subFd->subFieldLen);
+
+if (avDebug) 
+    {
+    printf("%s\t%s--->\n%s<---\n", subFd->omimId, subFd->subFieldId, buffer);fflush(stdout);
+    }
+}
+
+void omimParseAv(char *omimDb, char *omimTxtFileName, FILE *fh2, FILE *avSubFh, FILE *outFh)
+{
+struct sqlConnection *conn2, *conn3, *conn4;
+ 
+char query2[256], query3[256];
+struct sqlResult *sr2, *sr3;
+char **row2, **row3;
+
+char *omimId, *fieldType;
+char *startPos, *fieldLength;
+long bytesRead;
+
 char *line;
 int lineSize;
 char *chp, *chp2;
 boolean avPosFound = FALSE;
+size_t iStart, fieldLen;
+int subLen = 0;
+boolean entryProcessed;
 
-struct lineFile *lf = lineFileOpen(infName, TRUE);
+/* sub-field start and end position */
+int  subFdStartPos = 0;
+int  subFdEndPos   = 0;	
 
-for (;;)
+char *subFieldText;
+char *subAvNum;
+struct lm *lm;
+
+struct omimSubField *subFd;
+
+struct lineFile *lf = lineFileOpen(omimTxtFileName, TRUE);
+
+conn2= hAllocConn();
+	
+/* loop thru all recordd in the omimField table */
+sprintf(query2,"select * from %s.omimField where fieldType='AV'", omimDb);
+sr2 = sqlMustGetResult(conn2, query2);
+row2 = sqlNextRow(sr2);
+while (row2 != NULL)
     {
-    if (!lineFileNext(lf, &line, &lineSize)) break;
+    omimId 	= row2[0];
+    fieldType   = row2[1];
+    startPos    = row2[2];
+    fieldLength	= row2[3];
 
-    /* check if it is the starting of a new record */
-    if (*line == '*')
+    lm = lmInit(8*1024);
+    lmAllocVar(lm, subFd);
+    
+    iStart  	= (size_t)atoi(startPos);
+    fieldLen	= (size_t)atoi(fieldLength);
+    
+    lineFileSeek(lf, iStart, SEEK_SET);
+    entryProcessed = FALSE;
+
+    subAvNum = NULL;
+    while (!entryProcessed)
     	{
-	avPosFound = FALSE;
-    	chp = strstr(line, "***");
-    	if ((chp == line) && (chp != NULL))
+        if (!lineFileNext(lf, &line, &lineSize)) 
+	    {
+	    fprintf(stderr, "Premature end of file reached.\n");
+	    exit(1);
+	    }
+        
+	/* check if the line is the starting of a sub AV entry */
+        if (*line == '.') 
     	    {
-	    chp = strstr(line, "\t");
-	    chp++;
-	    chp2 = strstr(chp, "\t");
-	    *chp2 = '\0';
-	    omimID = strdup(chp);
+	    if (subAvNum != NULL)
+	    	{
+	    	subFdEndPos = lf->lineStart + lf->bufOffsetInFile - 1;
+		subLen = subFdEndPos - subFdStartPos;
+		
+		subFd->subFieldLen = subLen;
+		printSubField(subFd, fh2, avSubFh);
+		}
+	    subAvNum = strdup(line+1);
+    	    subFd = newSubFd(lm, omimId, fieldType);
+	
+	    subFd->subFieldId = subAvNum;
+	    subFdStartPos = lf->lineEnd + lf->bufOffsetInFile;
+	    subFd->startPos = subFdStartPos;
+	    
+	    /* reset the boolean flag, once the ".0000xx" sub AV line found */ 
+	    avPosFound = FALSE;
+	    }
+
+    	/* search for the pattern, only if it is not found for a sub entry yet */
+    	if (!avPosFound)
+    	    {
+	    avPosFound = findAaSub(line, subFd, outFh);
+	    }
+    	
+	if ((lf->lineStart + lf->bufOffsetInFile) >= (iStart + fieldLen - 1))
+	    {
+	    entryProcessed = TRUE;
+	    subFdEndPos = lf->lineStart + lf->bufOffsetInFile - 1;
+	    subLen = subFdEndPos - subFdStartPos;
+	    
+	    subFd->subFieldLen = subLen;
+	    printSubField(subFd, fh2, avSubFh);
 	    }
 	}
-	
-    /* check if the line is the starting of a sub AV entry */
-    if (*line == '.') 
-    	{
-	avNum = strdup(line+1);
-	
-	/* reset the boolean flag, once the ".xxxxx" sub AV line found */ 
-	avPosFound = FALSE;
-	}
-
-    /* search for the pattern, only if it is not found for a sub entry yet */
-    if (!avPosFound)
-    	{
-	avPosFound = findAaSub(line, outFh);
-	}
+    lmCleanup(&lm);
+    
+    row2 = sqlNextRow(sr2);
     }
+sqlFreeResult(&sr2);
+hFreeConn(&conn2);
 }
 
 int main(int argc, char *argv[])
 /* Process command line. */
 {
-int i;
-FILE *omimAvPosFh;
+FILE *omimAvSubFh, *omimAvPosFh, *fh2;
 
 optionInit(&argc, argv, options);
-if (argc != 3)
+if (argc != 5)
     usage();
 
-omimAvPosFh = mustOpen(argv[2], "w");
+fh2 = mustOpen(argv[2], "r");
+omimAvSubFh = mustOpen(argv[3], "w");
+omimAvPosFh = mustOpen(argv[4], "w");
 
-omimParseAv(argv[1], omimAvPosFh);
+omimParseAv(argv[1], argv[2], fh2, omimAvSubFh, omimAvPosFh);
 
 fclose(omimAvPosFh);
 
