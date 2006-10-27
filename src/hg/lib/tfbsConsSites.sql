7c5d978cca678df384b6b4983cce874600fc80c2
hiram <hiram>
2006-10-27 18:00:28 +0000

    Only need two indexes, one for name, and one for chrom,bin

--- src/hg/lib/tfbsConsSites.sql
+++ src/hg/lib/tfbsConsSites.sql
@@ -15,7 +15,5 @@ CREATE TABLE tfbsConsSites (
     zScore float not null,	# zScore
               #Indices
     INDEX(name(10)),
-    INDEX(chrom(12),bin),
-    INDEX(chrom(12),chromStart),
-    INDEX(chrom(12),chromEnd)
+    INDEX(chrom(12),bin)
 );
