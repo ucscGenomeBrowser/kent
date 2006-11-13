1e066da4c8c9950cb5d44451729f9942c31cbb35
kate <kate>
2006-11-13 21:40:12 +0000

    Add test with unnamed track

--- src/hg/lib/tests/customTrackTests.mk
+++ src/hg/lib/tests/customTrackTests.mk
@@ -8,11 +8,22 @@ IN = ${IN_DIR}/${TEST}
 EXP = ${EXP_DIR}/${TEST}
 OUT = ${OUT_DIR}/${TEST}
 
-test: simpleTest
+test: cgiUrl twoUrls simpleTest unnamed
+
+unnamed: mkout ${IN}/unnamed.ct ${EXP}/unnamed.ct.bed
+	${TESTER} parse ${IN}/unnamed.ct > ${OUT}/unnamed.ct.bed
+	${TESTER} check ${OUT}/unnamed.ct.bed ${EXP}/unnamed.ct.bed
+
+cgiUrl: mkout 
+	${TESTER} parse ${IN}/cgiUrl.ct > ${OUT}/cgiUrl.bed
 
 simpleTest: mkout ${IN}/simpleTest.ct ${EXP}/simpleTest.ct.bed
 	${TESTER} parse ${IN}/simpleTest.ct > ${OUT}/simpleTest.ct.bed
 	${TESTER} check ${OUT}/simpleTest.ct.bed ${EXP}/simpleTest.ct.bed
+
+twoUrls: mkout ${IN}/twoUrls.ct ${EXP}/twoUrls.ct.bed
+	${TESTER} parse ${IN}/twoUrls.ct > ${OUT}/twoUrls.ct.bed
+	${TESTER} check ${OUT}/twoUrls.ct.bed ${EXP}/twoUrls.ct.bed
 
 mkout:
 	@${MKDIR} ${OUT}
