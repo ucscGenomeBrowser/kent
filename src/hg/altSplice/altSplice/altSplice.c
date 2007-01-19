f94eaf175364ba56ff4b3746767ad3a3f89d89ae
kent <kent>
2007-01-19 21:24:19 +0000

    Changed error on empty input to warning.

--- src/hg/altSplice/altSplice/altSplice.c
+++ src/hg/altSplice/altSplice/altSplice.c
@@ -74,7 +74,7 @@
 #include "obscure.h"
 #define USUAL
 //#define AFFYSPLICE
-static char const rcsid[] = "$Id: altSplice.c,v 1.22 2007/01/08 19:23:35 sugnet Exp $";
+static char const rcsid[] = "$Id: altSplice.c,v 1.23 2007/01/19 21:24:19 kent Exp $";
 
 int cassetteCount = 0; /* Number of cassette exons counted. */
 int misSense = 0;      /* Number of cassette exons that would introduce a missense mutation. */
@@ -815,7 +815,10 @@ else
 /* Sanity check to make sure we got some loci to work
    with. */
 if(gpList == NULL)
-    errAbort("No gene boundaries were found.");
+    {
+    warn("No gene boundaries were found.");
+    return;
+    }
 slSort(&gpList, genePredCmp);
 setupTables(gpList->chrom);
 
