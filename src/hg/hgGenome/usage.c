087000d266cdc8734599a2db6aa0e76527b95ccf
ann <ann>
2007-01-23 19:58:39 +0000

    clarifying edits

--- src/hg/hgGenome/usage.c
+++ src/hg/hgGenome/usage.c
@@ -3,7 +3,7 @@
 #include "common.h"
 #include "hPrint.h"
 
-static char const rcsid[] = "$Id: usage.c,v 1.2 2007/01/23 18:07:51 kent Exp $";
+static char const rcsid[] = "$Id: usage.c,v 1.3 2007/01/23 19:58:39 ann Exp $";
 
 void printMainHelp()
 /* Put up main page help info. */
@@ -12,8 +12,9 @@ hPrintf("%s",
 
 "<P>Genome Graphs is a tool for displaying genome-wide data sets such\n"
 "as the results of genome-wide SNP association studies, linkage studies\n"
-"and homozygousity mapping. This section provided brief line-by-line\n"
-"descriptions of the controls on this page.\n"
+"and homozygosity mapping. This tool is not pre-loaded with any sample\n"
+"data. This section will help you upload your own data by providing brief\n"
+"line-by-line descriptions of the controls on this page.\n"
 "        <UL>\n"
 "        <LI><B>clade: </B>Specifies which clade the organism is in."
 "        </LI>\n"
@@ -29,27 +30,27 @@ hPrintf("%s",
 "        own data.\n"
 "        </LI>\n"
 "        <LI><B>configure: </B> Takes you to a page where you can control "
-"        many aspects of the program including the overall size of the image, "
+"        many aspects of the display including the overall size of the image, "
 "        how many graphs can be drawn, and the chromosome layout."
 "        </LI>\n"
 "        <LI><B>correlate: </B> If more than one graph is selected, takes you "
 "        to a page listing the Pearson's correlation coefficient for each "
 "        pair of graphs."
 "        </LI>\n"
-"        <LI><B>significance threshold: </B> Values over this threshold will "
+"        <LI><B>significance threshold: </B> Values above this threshold will "
 "        be considered significant. A light blue line will be drawn across "
 "        the graphs at the significance threshold of the first graph. Regions "
-"        over the threshold will be included in the region list you get with "
-"        the <B>browse regions</B> button."
+"        above the threshold will be included in the region list you get with "
+"        the <EM>browse regions</EM> button."
 "        </LI>\n"
 "        <LI><B>browse regions: </B> Takes you to a page with a list of all "
-"        regions over the significance threshold on the left, and a Genome "
-"        Browser on the right.  Clicking on a region will move the browser's "
-"        window to that region."
+"        regions above the <EM>significance threshold</EM> on the left, and a "
+"        Genome Browser on the right.  Clicking on a region will move the "
+"        browser's window to that region."
 "        </LI>\n"
-"        <LI><B>sort genes: </B> Figures out all of the genes in regions over "
-"        the significance threshold, and takes you to the Gene Sorter with a "
-"        filter set up to focus on only those genes."
+"        <LI><B>sort genes: </B> Opens the Gene Sorter with a filter to "
+"        display only those genes in regions that are above the "
+"        <EM>significance threshold</EM>."
 "        </LI>\n"
 "        </UL>\n");
 }
