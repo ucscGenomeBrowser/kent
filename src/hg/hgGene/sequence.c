ec90ed330dc887a483ebb38aea37ee2f5fd7b0ca
baertsch <baertsch>
2007-02-03 00:06:48 +0000

    add length to sequence display

--- src/hg/hgGene/sequence.c
+++ src/hg/hgGene/sequence.c
@@ -15,7 +15,7 @@
 #include "hCommon.h"
 #include "hgGene.h"
 
-static char const rcsid[] = "$Id: sequence.c,v 1.15 2006/07/01 08:32:33 kent Exp $";
+static char const rcsid[] = "$Id: sequence.c,v 1.16 2007/02/03 00:06:48 baertsch Exp $";
 
 static void printGenomicAnchor(char *table, char *itemName,
 	char *chrom, int start, int end)
@@ -110,10 +110,13 @@ static void printProteinLink(struct sqlConnection *conn, char *geneId)
 {
 char *table = genomeSetting("knownGenePep");
 char query[256];
+char protQuery[256];
 char title[128];
+safef(protQuery, sizeof(protQuery), 
+	"select proteinId from knownGene where name='%s'" ,  geneId);
 safef(query, sizeof(query), 
 	"select length(seq) from %s where name='%s'" , table,  geneId);
-safef(title, sizeof(title), "Protein (%d aa)", sqlQuickNum(conn,query));
+safef(title, sizeof(title), "Protein %s (%d aa)", sqlQuickString(conn, protQuery), sqlQuickNum(conn,query));
 printSeqLink(conn, geneId, "knownGenePep", hggDoGetProteinSeq,
 	title);
 }
@@ -185,7 +188,7 @@ sr = sqlGetResult(conn, query);
 if ((row = sqlNextRow(sr)) != NULL)
     {
     char *seq = row[0];
-    hPrintf(">%s (%s)\n", geneId, geneName);
+    hPrintf(">%s (%s) length=%d\n", geneId, geneName, (seq!=NULL) ? (int)strlen(seq): 0);
     writeSeqWithBreaks(stdout, seq, strlen(seq), 60);
     }
 sqlFreeResult(&sr);
