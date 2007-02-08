e29738cbb886754a778522d400f6a5df1c65abad
kent <kent>
2007-02-08 22:11:39 +0000

    Creating tgx instead of agx. Still a little rough code-wise but seems to work.

--- src/hg/txGraph/txBedToTxg/txBedToTxg.c
+++ src/hg/txGraph/txBedToTxg/txBedToTxg.c
@@ -6,6 +6,8 @@
 #include "bed.h"
 #include "ggMrnaAli.h"
 #include "geneGraph.h"
+#include "dystring.h"
+#include "txGraph.h"
 
 void usage()
 /* Explain usage and exit. */
@@ -92,6 +94,161 @@ slReverse(&maList);
 return maList;
 }
 
+static boolean vertexUsed(struct geneGraph *gg, int vertexIx)
+/* Is the vertex attached to anything in the edgeMatrix? */
+// JK - duplicate code from geneGraph.h.  Fix
+{
+bool **em = gg->edgeMatrix;
+int vC = gg->vertexCount;
+int i=0;
+/* First check the row. */
+for(i=0; i<vC; i++)
+    if(em[vertexIx][i])
+	return TRUE;
+/* Now check the column. */
+for(i=0; i<vC; i++)
+    if(em[i][vertexIx])
+	return TRUE;
+
+/* Guess it isn't used. */
+return FALSE;
+}
+
+static int countUsed(struct geneGraph *gg, int vCount, int *translator)
+/* Count number of vertices actually used. */
+// JK - duplicate code from geneGraph.h.  Fix
+{
+int i;
+int count = 0;
+struct ggVertex *v = gg->vertices;
+for (i=0; i<vCount; ++i)
+    {
+    translator[i] = count;
+    if (v[i].type != ggUnused && vertexUsed(gg,i))
+	++count;
+    }
+return count;
+}
+
+enum ggEdgeType ggClassifyEdge(struct geneGraph *gg, int v1, int v2);
+/* Classify and edge as ggExon or ggIntron. ggExon is not
+ * necessarily coding. */
+// JK - duplicate code from geneGraph.h.  Fix
+
+struct txGraph *txGraphFromGeneGraph(struct geneGraph *gg)
+/* Convert a gene graph to an altGraphX data structure for storage in
+ * database. */
+{
+struct txGraph *tg = NULL;
+bool **em = gg->edgeMatrix;
+int edgeCount = 0;
+int totalVertexCount = gg->vertexCount;
+int usedVertexCount;
+struct dyString *accessionList = NULL;
+int *translator;	/* Translates away unused vertices. */
+struct ggVertex *vertices = gg->vertices;
+struct ggEvidence *ev = NULL;
+int i,j;
+UBYTE *vTypes;
+int *vPositions;
+
+AllocArray(translator, totalVertexCount);
+usedVertexCount = countUsed(gg, totalVertexCount, translator);
+for (i=0; i<totalVertexCount; ++i)
+    {
+    bool *waysOut = em[i];
+    for (j=0; j<totalVertexCount; ++j)
+	if (waysOut[j] && gg->vertices[j].type != ggUnused)
+	    ++edgeCount;
+    }
+AllocVar(tg);
+snprintf(tg->strand, sizeof(tg->strand), "%s", gg->strand);
+tg->tName = cloneString(gg->tName);
+tg->tStart = gg->tStart;
+tg->tEnd = gg->tEnd;
+tg->name = cloneString("NA");
+tg->vertexCount = usedVertexCount;
+tg->vTypes = AllocArray(vTypes, usedVertexCount);
+tg->vPositions = AllocArray(vPositions, usedVertexCount);
+tg->mrnaRefCount = gg->mrnaRefCount;
+accessionList = newDyString(10*gg->mrnaRefCount);
+/* Have to print the accessions all out in the same string to conform
+   to how the memory is handled later. */
+for(i=0; i<gg->mrnaRefCount; i++)
+    dyStringPrintf(accessionList, "%s,", gg->mrnaRefs[i]);
+sqlStringDynamicArray(accessionList->string, &tg->mrnaRefs, &tg->mrnaRefCount);
+dyStringFree(&accessionList);
+if(gg->mrnaRefCount > 0)
+    {
+    tg->mrnaTissues = CloneArray(gg->mrnaTissues, gg->mrnaRefCount);
+    tg->mrnaLibs = CloneArray(gg->mrnaLibs, gg->mrnaRefCount);
+    }
+
+/* convert vertexes */
+for (i=0,j=0; i<totalVertexCount; ++i)
+    {
+    struct ggVertex *v = vertices+i;
+    if (v->type != ggUnused && vertexUsed(gg, i))
+	{
+	vTypes[j] = v->type;
+	vPositions[j] = v->position;
+	++j;
+	}
+    }
+
+/* convert edges */
+tg->edgeCount = edgeCount;
+AllocArray(tg->edgeStarts, edgeCount);
+AllocArray(tg->edgeEnds, edgeCount);
+AllocArray(tg->edgeTypes, edgeCount);
+tg->evidence = NULL;
+edgeCount = 0;
+for (i=0; i<totalVertexCount; ++i)
+    {
+    bool *waysOut = em[i];
+    if(gg->vertices[i].type == ggUnused)
+	{	
+	for (j=0; j<totalVertexCount; ++j)
+	    {
+	    if(gg->edgeMatrix[i][j])
+		errAbort("Shouldn't be any connections to an unused vertex %d-%d.", i, j);
+	    }
+	continue;
+	}
+    for (j=0; j<totalVertexCount; ++j)
+	{
+	if (waysOut[j] && gg->vertices[j].type != ggUnused)
+	    {
+	    /* Store the mrna evidence. */
+	    struct txEvList *evList;
+	    AllocVar(evList);
+	    evList->evCount = slCount(gg->evidence[i][j]);
+	    // evList->mrnaIds = AllocArray(evList->mrnaIds, evList->evCount);
+	    for(ev = gg->evidence[i][j]; ev != NULL; ev = ev->next)
+		{
+		struct txEvidence *txEv;
+		AllocVar(txEv);
+		txEv->mrnaId = ev->id;
+		txEv->start = ev->start;
+		txEv->end = ev->end;
+		slAddHead(&evList->evList, txEv);
+		}
+	    slReverse(&evList->evList);
+	    slAddHead(&tg->evidence, evList);
+
+	    /* Store the edge information. */
+	    tg->edgeStarts[edgeCount] = translator[i];
+	    tg->edgeEnds[edgeCount] = translator[j];
+	    tg->edgeTypes[edgeCount] = ggClassifyEdge(gg, i, j);
+	    ++edgeCount;
+	    }
+	}
+    }
+slReverse(&tg->evidence);
+freeMem(translator);
+return tg;
+}
+
 
 void txBedToTxg(char *inBed, char *outGraph)
 /* txBedToTxg - Create a transcription graph out of a bed-12 file.. */
@@ -107,16 +264,16 @@ verbose(1, "Reduced to %d clusters\n", slCount(mcList));
 for (mc = mcList; mc != NULL; mc = mc->next)
     {
     struct geneGraph *gg = ggGraphConsensusCluster(mc, ci, NULL, FALSE);
-    struct altGraphX *ag = ggToAltGraphX(gg);
-    if (ag != NULL)
+    struct txGraph *tg = txGraphFromGeneGraph(gg);
+    if (tg != NULL)
         {
 	static int id=0;
 	char name[16];
 	safef(name, sizeof(name), "a%d", ++id);
-	freez(&ag->name);
-	ag->name = name;
-	altGraphXTabOut(ag, f);
-	ag->name = NULL;
+	freez(&tg->name);
+	tg->name = name;
+	txGraphTabOut(tg, f);
+	tg->name = NULL;
 	}
     }
 carefulClose(&f);
