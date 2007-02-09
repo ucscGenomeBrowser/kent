fc0169a8f2f1be32d9a8bab9d719805f8fc44ee2
hiram <hiram>
2007-02-09 22:24:45 +0000

    Initial source

---
+++ src/hg/inc/trashDir.h
@@ -0,0 +1,9 @@
+/* trashDir.c - temporary file creation and directory creation in /trash */
+
+#ifndef TRASHDIR_H
+#define TRASHDIR_H
+
+void trashDirFile(struct tempName *tn, char *suffix, char *dirName);
+/*	obtain a trash file name in the specified trash dirName */
+
+#endif	/*	TRASHDIR_H	*/
