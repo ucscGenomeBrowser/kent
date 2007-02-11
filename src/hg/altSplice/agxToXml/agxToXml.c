be0b583510055935eb63c7321cf98ebc24cdb5bc
kent <kent>
2007-02-11 07:11:29 +0000

    Seems to work.

---
+++ src/hg/altSplice/agxToXml/agxToXml.c
@@ -0,0 +1,97 @@
+/* agxToXml - Convert axt format splicing graph to XML format.. */
+#include "common.h"
+#include "linefile.h"
+#include "hash.h"
+#include "options.h"
+#include "geneGraph.h"
+#include "altGraphX.h"
+
+boolean showEvidence = TRUE;
+
+void usage()
+/* Explain usage and exit. */
+{
+errAbort(
+  "agxToXml - Convert axt format splicing graph to XML format.\n"
+  "usage:\n"
+  "   agxToXml in.agx out.xml\n"
+  "options:\n"
+  "   -noEvidence\n"
+  );
+}
+
+static struct optionSpec options[] = {
+   {"noEvidence", OPTION_BOOLEAN},
+   {NULL, 0},
+};
+
+void agxWriteXml(struct altGraphX *graph, FILE *f)
+/* Write out graph as xml. */
+{
+fprintf(f, "<altGraphX name=\"%s\" tName=\"%s\" strand=\"%s\" tStart=\"%d\" tEnd=\"%d\">\n",
+	graph->name, graph->tName, graph->strand, graph->tStart, graph->tEnd);
+int i;
+fprintf(f, "  <vertices count=\"%d\">\n", graph->vertexCount);
+for (i=0; i<graph->vertexCount; ++i)
+    {
+    fprintf(f, "    <vertex type=\"%s\" pos=\"%d\" i=\"%d\"/>\n",
+    	ggVertexTypeAsString(graph->vTypes[i]),
+	graph->vPositions[i], i);
+    }
+fprintf(f, "  </vertices>\n");
+fprintf(f, "  <edges count=\"%d\">\n", graph->edgeCount);
+struct evidence *evList =  graph->evidence;
+for (i=0; i<graph->edgeCount; ++i)
+    {
+    int i1 = graph->edgeStarts[i];
+    int i2 = graph->edgeEnds[i];
+    char *t1 = ggVertexTypeAsString(graph->vTypes[i1]);
+    char *t2 = ggVertexTypeAsString(graph->vTypes[i2]);
+    int x1 = graph->vPositions[i1];
+    int x2 = graph->vPositions[i2];
+    fprintf(f, "    <edge type=\"%s\" t1=\"%s\" t2=\"%s\" size=\"%d\" x1=\"%d\" x2=\"%d\" i1=\"%d\" i2=\"%d\"", 
+        (graph->edgeTypes[i] == ggExon ? "exon  " : "intron"),
+	t1, t2, x2-x1, x1, x2, i1, i2);
+    if (showEvidence)
+        {
+	fprintf(f, ">\n");
+	int j;
+	for (j=0; j<evList->evCount; ++j)
+	    {
+	    int mrnaId = evList->mrnaIds[j];
+	    fprintf(f, "      <ev acc=\"%s\"/>\n", graph->mrnaRefs[mrnaId]);
+	    }
+	fprintf(f, "    </edge>\n");
+	}
+    else
+        {
+	fprintf(f, "/>\n");
+	}
+    evList = evList->next;
+    }
+fprintf(f, "  </edges>\n");
+fprintf(f, "</altGraphX>\n");
+}
+
+void agxToXml(char *inTxg, char *outXml)
+/* agxToXml - Convert agx to an XML format.. */
+{
+struct altGraphX *agx, *agxList = altGraphXLoadAll(inTxg);
+FILE *f = mustOpen(outXml, "w");
+for (agx = agxList; agx != NULL; agx = agx->next)
+    {
+    agxWriteXml(agx, f);
+    }
+carefulClose(&f);
+}
+
+int main(int argc, char *argv[])
+/* Process command line. */
+{
+optionInit(&argc, argv, options);
+showEvidence = !optionExists("noEvidence");
+if (argc != 3)
+    usage();
+agxToXml(argv[1], argv[2]);
+return 0;
+}
