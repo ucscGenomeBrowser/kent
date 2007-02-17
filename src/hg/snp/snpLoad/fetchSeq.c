07f16900abe0b3bb94b0a3108f5f2e330f614299
heather <heather>
2007-02-17 08:00:25 +0000

    rewritten.  Read from file, use 2bit

--- src/hg/snp/snpLoad/fetchSeq.c
+++ src/hg/snp/snpLoad/fetchSeq.c
@@ -1,157 +1,87 @@
-/* fetchSeq -- get sequences for ortho pipeline. */
-/* Write to chrN_snp126hg18orthoPrelim.tab. */
+/* fetchSeq  */
+/* Input is bed 6. */
+/* Output adds sequence as column 7. */
+/* The key thing here is doing a reverse complement for data on the negative strand. */
+/* Not checking for coords off the end of the chrom. */
 
 #include "common.h"
+#include "dnaseq.h"
+#include "linefile.h"
+#include "sqlNum.h"
+#include "twoBit.h"
 
-#include "chromInfo.h"
-#include "hash.h"
-#include "hdb.h"
-
-static char const rcsid[] = "$Id: fetchSeq.c,v 1.7 2006/12/05 17:59:28 heather Exp $";
-
-static char *snpDb = NULL;
-static struct hash *chromHash = NULL;
-FILE *errorFileHandle = NULL;
+static char const rcsid[] = "$Id: fetchSeq.c,v 1.8 2007/02/17 08:00:25 heather Exp $";
 
 void usage()
 /* Explain usage and exit. */
 {
 errAbort(
-    "fetchSeq - get sequences for ortho pipeline\n"
+    "fetchSeq - append sequence to bed 6\n"
     "usage:\n"
-    "    fetchSeq database sequence\n");
+    "    fetchSeq input sequence\n");
 }
 
 
-struct hash *loadChroms()
-/* hash from UCSC chromInfo */
+void doFetch(char *inputFileName, char *sequenceFileName)
+/* lookup sequence for each line */
 {
-struct hash *ret;
-char query[512];
-struct sqlConnection *conn = hAllocConn();
-struct sqlResult *sr;
-char **row;
-struct chromInfo *el;
-char tableName[64];
+struct lineFile *lf = NULL;
+char *line;
+char *row[6];
+int elementCount;
+struct twoBitFile *tbf;
 
-ret = newHash(0);
-safef(query, sizeof(query), "select chrom, size from chromInfo");
-sr = sqlGetResult(conn, query);
-while ((row = sqlNextRow(sr)) != NULL)
-    {
-    safef(tableName, ArraySize(tableName), "%s_snp126hg18ortho", row[0]);
-    if (!hTableExists(tableName)) continue;
-    el = chromInfoLoad(row);
-    hashAdd(ret, el->chrom, (void *)(& el->size));
-    }
-sqlFreeResult(&sr);
-hFreeConn(&conn);
-return ret;
-}
-
-unsigned getChromSize(char *chrom)
-/* Return size of chrom.  */
-{
-struct hashEl *el = hashLookup(chromHash,chrom);
-
-if (el == NULL)
-    errAbort("Couldn't find size of chrom %s", chrom);
-return *(unsigned *)el->val;
-}
-
-
-void doFetch(char *sequenceFile, char *chromName)
-/* get sequence for each row */
-{
-char query[512];
-struct sqlConnection *conn = hAllocConn();
-struct sqlResult *sr;
-char **row;
-char tableName[64];
-char fileName[64];
-FILE *f;
-struct dnaSeq *seq;
-char *seqPtr = NULL;
+char *fileChrom = NULL;
 int start = 0;
 int end = 0;
-int chromSize = 0;
-char allele[2];
-char *snpId = NULL;
+char *name = NULL;
+int score = 0;
+char *strand = NULL;
 
-safef(tableName, sizeof(tableName), "%s_snp126hg18ortho", chromName);
-if (!hTableExists(tableName)) return;
-safef(fileName, ArraySize(fileName), "%s_snp126hg18orthoPrelim.tab", chromName);
-chromSize = getChromSize(chromName);
+struct dnaSeq *chunk = NULL;
 
-f = mustOpen(fileName, "w");
-seq = hFetchSeq(sequenceFile, chromName, 0, chromSize-1);
-touppers(seq->dna);
-seqPtr = seq->dna;
+tbf = twoBitOpen(sequenceFileName);
 
-/* humanAllele and humanObserved are available here, but not human position. */
-/* later in the pipeline we'll get all of those using snpOrthoLookup */
-safef(query, sizeof(query), "select chromStart, chromEnd, name, strand from %s", tableName);
-
-sr = sqlGetResult(conn, query);
-while ((row = sqlNextRow(sr)) != NULL)
+lf = lineFileOpen(inputFileName, TRUE);
+while (lineFileNext(lf, &line, NULL))
     {
-    start = sqlUnsigned(row[0]);
-    end = sqlUnsigned(row[1]);
-    snpId = cloneString(row[2]);
-    if (end != start + 1)
-	{
-        fprintf(errorFileHandle, "rs%s has size %d\n", row[0], end - start);
-        continue;
-	}
-    if (start > chromSize)
-        {
-        fprintf(errorFileHandle, "rs%s at %s:%d-%d; chromSize=%d\n", row[0], chromName, start, end, chromSize);
-	continue;
-	}
+    elementCount = chopString(line, "\t", row, ArraySize(row));
+    if (elementCount != 6) continue;
 
-    safef(allele, sizeof(allele), "%c", seqPtr[start]);
-    if (sameString(row[3], "-"))
-        reverseComplement(allele, 1);
-    fprintf(f, "%s\t%d\t%d\t%s\t%s\t%s\t%s\n", chromName, start, end, snpId, snpDb, row[3], allele);
+    fileChrom = cloneString(row[0]);
+    start = sqlUnsigned(row[1]);
+    end = sqlUnsigned(row[2]);
+    name = cloneString(row[3]);
+    score = sqlUnsigned(row[4]);
+    strand = cloneString(row[5]);
 
+    if (start == end) continue;
+    assert (end > start);
+
+    chunk = twoBitReadSeqFrag(tbf, fileChrom, start, end);
+    touppers(chunk->dna);
+    if (sameString(strand, "-"))
+        reverseComplement(chunk->dna, chunk->size);
+    printf("%s\t%d\t%d\t%s\t%d\t%s\t%s\n", fileChrom, start, end, name, score, strand, chunk->dna);
+    dnaSeqFree(&chunk);
     }
-sqlFreeResult(&sr);
-hFreeConn(&conn);
-carefulClose(&f);
+
+lineFileClose(&lf);
 }
 
 
 int main(int argc, char *argv[])
-/* read chrN_snp126hg18ortho, look up sequence */
+/* read bed 6 file, look up sequence */
 {
-struct hashCookie cookie;
-char *chromName;
-char tableName[64];
-
 if (argc != 3)
     usage();
 
-snpDb = argv[1];
-hSetDb(snpDb);
+if (!fileExists(argv[1]))
+    errAbort("can't find %s\n", argv[1]);
+if (!fileExists(argv[2]))
+    errAbort("can't find %s\n", argv[2]);
 
-chromHash = loadChroms();
-if (chromHash == NULL) 
-    {
-    verbose(1, "couldn't get chrom info\n");
-    return 1;
-    }
+doFetch(argv[1], argv[2]);
 
-errorFileHandle = mustOpen("fetchSeq.errors", "w");
-
-cookie = hashFirst(chromHash);
-while ((chromName = hashNextName(&cookie)) != NULL)
-    {
-    safef(tableName, ArraySize(tableName), "%s_snp126hg18ortho", chromName);
-    if (!hTableExists(tableName)) continue;
-    verbose(1, "chrom = %s\n", chromName);
-    doFetch(argv[2], chromName);
-    }
-
-carefulClose(&errorFileHandle);
 return 0;
 }
