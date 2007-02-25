cb0a8dfb79bd9bf2d8f01f0b846775708cee91b8
kent <kent>
2007-02-25 23:29:55 +0000

    Adding information about alignment of source rna to genome.

--- src/hg/txCds/txInfoAssemble/txInfoAssemble.c
+++ src/hg/txCds/txInfoAssemble/txInfoAssemble.c
@@ -1,5 +1,6 @@
 /* txInfoAssemble - Assemble information from various sources into txInfo table.. */
 #include "common.h"
+#include "obscure.h"
 #include "linefile.h"
 #include "hash.h"
 #include "options.h"
@@ -7,7 +8,9 @@
 #include "borf.h"
 #include "binRange.h"
 #include "genbank.h"
+#include "psl.h"
 #include "txInfo.h"
+#include "rangeTree.h"
 
 void usage()
 /* Explain usage and exit. */
@@ -15,7 +18,7 @@ void usage()
 errAbort(
   "txInfoAssemble - Assemble information from various sources into txInfo table.\n"
   "usage:\n"
-  "   txInfoAssemble bestCds.bed best.tce all.borf altSplice.bed txWalk.exceptions out.info\n"
+  "   txInfoAssemble bestCds.bed best.tce all.borf altSplice.bed txWalk.exceptions sizePolyA.tab all.psl flippedRna out.info\n"
   "options:\n"
   "   -xxx=XXX\n"
   );
@@ -203,8 +206,41 @@ for (i=0; i<bed->blockCount; ++i)
 return gotOne;
 }
 
+int pslBedOverlap(struct psl *psl, struct bed *bed)
+/* Return number of bases psl and bed overlap at the block level */
+{
+/* No overlap if on wrong chromosome or wrong strand. */
+if (psl->strand[0] != bed->strand[0])
+    return 0;
+if (!sameString(psl->tName, bed->chrom)) 
+    return 0;
+
+/* Build up range tree covering bed */
+struct rbTree *rangeTree = rangeTreeNew();
+int i;
+for (i=0; i<bed->blockCount; ++i)
+    {
+    int start = bed->chromStart + bed->chromStarts[i];
+    int end = start + bed->blockSizes[i];
+    rangeTreeAdd(rangeTree, start, end);
+    }
+
+/* Loop through psl accumulating total overlap. */
+int totalOverlap = 0;
+for (i=0; i<psl->blockCount; ++i)
+    {
+    int start = psl->tStarts[i];
+    int end = start + psl->blockSizes[i];
+    totalOverlap += rangeTreeOverlapSize(rangeTree, start, end);
+    }
+
+/* Clean up and return result. */
+rangeTreeFree(&rangeTree);
+return totalOverlap;
+}
+
 void txInfoAssemble(char *txBedFile, char *cdsEvFile, char *borfFile, char *altSpliceFile,
-	char *exceptionFile, char *outFile)
+	char *exceptionFile, char *sizePolyAFile, char *pslFile, char *flipFile, char *outFile)
 /* txInfoAssemble - Assemble information from various sources into txInfo table.. */
 {
 /* Build up hash of evidence keyed by transcript name. */
@@ -230,6 +266,20 @@ struct hash *retainedIntronHash = bedsIntoHashOfKeepers(retainedIntronList);
 struct hash *selenocysteineHash, *altStartHash;
 genbankExceptionsHash(exceptionFile, &selenocysteineHash, &altStartHash);
 
+/* Read in polyA sizes */
+struct hash *sizePolyAHash = hashNameIntFile(sizePolyAFile);
+verbose(2, "Loaded %d from %s\n", sizePolyAHash->elCount, sizePolyAFile);
+
+/* Read in psls */
+struct hash *pslHash = hashNew(20);
+struct psl *psl, *pslList = pslLoadAll(pslFile);
+for (psl = pslList; psl != NULL; psl = psl->next)
+    hashAdd(pslHash, psl->qName, psl);
+verbose(2, "Loaded %d from %s\n", pslHash->elCount, pslFile);
+
+/* Read in accessions that we flipped for better splice sites. */
+struct hash *flipHash = hashWordsInFile(flipFile, 0);
+
 /* Open primary gene input and output. */
 struct lineFile *lf = lineFileOpen(txBedFile, TRUE);
 FILE *f = mustOpen(outFile, "w");
@@ -254,7 +304,52 @@ while (lineFileRow(lf, row))
         info.sourceAcc += 1;
     info.isRefSeq = startsWith("NM_", info.sourceAcc);
 
-    /* Get source size and start/end complete from cdsEv. */
+    /* Loop through all psl's associated with our RNA.  Figure out
+     * our overlap with each, and pick best one. */
+    struct hashEl *hel, *firstPslHel = hashLookup(pslHash, info.sourceAcc);
+    if (firstPslHel == NULL)
+        errAbort("%s is not in %s", info.sourceAcc, pslFile);
+    int mapCount = 0;
+    struct psl *psl, *bestPsl = NULL;
+    int coverage, bestCoverage = 0;
+    boolean isFlipped = (hashLookup(flipHash, info.sourceAcc) != NULL);
+    for (hel = firstPslHel; hel != NULL; hel = hashLookupNext(hel))
+        {
+	psl = hel->val;
+	mapCount += 1;
+	coverage = pslBedOverlap(psl, bed);
+	if (coverage > bestCoverage)
+	    {
+	    bestCoverage = coverage;
+	    bestPsl = psl;
+	    }
+	/* If we flipped it, try it on the opposite strand too. */
+	if (isFlipped)
+	    {
+	    psl->strand[0] = (psl->strand[0] == '+' ? '-' : '+');
+	    coverage = pslBedOverlap(psl, bed);
+	    if (coverage > bestCoverage)
+		{
+		bestCoverage = coverage;
+		bestPsl = psl;
+		}
+	    psl->strand[0] = (psl->strand[0] == '+' ? '-' : '+');
+	    }
+	}
+    if (bestPsl == NULL)
+        errAbort("%s has no overlapping alignments with %s in %s", 
+		bed->name, info.sourceAcc, pslFile);
+
+    /* Figure out and save alignment statistics. */
+    int polyA = hashIntValDefault(sizePolyAHash, bed->name, 0);
+    info.sourceSize = bestPsl->qSize - polyA;
+    info.aliCoverage = (double)bestCoverage / info.sourceSize;
+    info.aliIdRatio = (double)(bestPsl->match + bestPsl->repMatch)/
+    			(bestPsl->match + bestPsl->misMatch + bestPsl->repMatch);
+    info. genoMapCount = mapCount;
+
+
+    /* Get orf size and start/end complete from cdsEv. */
     if (bed->thickStart < bed->thickEnd)
 	{
 	cdsEv = hashFindVal(cdsEvHash, bed->name);
@@ -282,6 +377,7 @@ while (lineFileRow(lf, row))
 
     /* Loop through bed looking for small gaps indicative of frame shift/stop */
     int i, lastBlock = bed->blockCount-1;
+    int exonCount = 1;
     for (i=0; i < lastBlock; ++i)
         {
 	int gapStart = bed->chromStarts[i] + bed->blockSizes[i];
@@ -296,8 +392,12 @@ while (lineFileRow(lf, row))
 	    case 3:
 	        info.genomicStop = TRUE;
 		break;
+	    default:
+	        exonCount += 1;
+		break;
 	    }
 	}
+    info.exonCount = exonCount;
 
     /* Write info, free bed. */
     txInfoTabOut(&info, f);
@@ -312,8 +412,9 @@ int main(int argc, char *argv[])
 /* Process command line. */
 {
 optionInit(&argc, argv, options);
-if (argc != 7)
+if (argc != 10)
     usage();
-txInfoAssemble(argv[1], argv[2], argv[3], argv[4], argv[5], argv[6]);
+txInfoAssemble(argv[1], argv[2], argv[3], argv[4], argv[5], 
+	argv[6], argv[7], argv[8], argv[9]);
 return 0;
 }
