b6717d8eccf396c0972560eccf5a0144297da7b2
kent <kent>
2007-03-03 05:09:00 +0000

    Adding -list option to subColumn.

--- src/utils/subColumn/subColumn.c
+++ src/utils/subColumn/subColumn.c
@@ -3,9 +3,12 @@
 #include "linefile.h"
 #include "hash.h"
 #include "options.h"
+#include "dystring.h"
 #include "obscure.h"
 
-static char const rcsid[] = "$Id: subColumn.c,v 1.1 2007/03/02 00:44:26 kent Exp $";
+static char const rcsid[] = "$Id: subColumn.c,v 1.2 2007/03/03 05:09:00 kent Exp $";
+
+boolean isList = FALSE;
 
 void usage()
 /* Explain usage and exit. */
@@ -20,13 +23,35 @@ errAbort(
   "    sub.tab is a where first column is old values, second new\n"
   "    out.tab is the substituted output\n"
   "options:\n"
-  "   -xxx=XXX\n"
+  "   -list - Column is a comma-separated list.  Substitute all elements in list\n"
   );
 }
 
 static struct optionSpec options[] = {
+   {"list", OPTION_BOOLEAN},
    {NULL, 0},
 };
+
+char *subCommaList(struct hash *subHash, char *in)
+/* In is a comma-separated list.  Return the list with
+ * substitution done.  This routine not reentrant. */
+{
+static struct dyString *dy;
+if (dy == NULL)
+    dy = dyStringNew(0);
+else
+    dyStringClear(dy);
+while (in != NULL && in[0] != 0)
+    {
+    char *e = strchr(in, ',');
+    if (e != NULL)
+        *e++ = 0;
+    char *s = hashMustFindVal(subHash, in);
+    dyStringPrintf(dy, "%s,", s);
+    in = e;
+    }
+return dy->string;
+}
 
 void subColumn(char *asciiColumn, char *inFile, char *subFile, char *outFile)
 /* subColumn - Substitute one column in a tab-separated file.. */
@@ -53,10 +78,17 @@ while ((rowCount = lineFileChopNextTab(lf, row, ArraySize(row))) > 0)
 	char *s = row[i];
 	if (i == column)
 	    {
-	    char *sub = hashFindVal(subHash, s);
-	    if (sub == NULL)
-	        errAbort("%s not in %s line %d of %s", s, subFile, lf->lineIx, lf->fileName);
-	    s = sub;
+	    if (isList)
+	        {
+		s = subCommaList(subHash, s);
+		}
+	    else
+		{
+		char *sub = hashFindVal(subHash, s);
+		if (sub == NULL)
+		    errAbort("%s not in %s line %d of %s", s, subFile, lf->lineIx, lf->fileName);
+		s = sub;
+		}
 	    }
 	fputs(s, f);
 	if (i == rowCount-1)
@@ -74,6 +106,7 @@ int main(int argc, char *argv[])
 optionInit(&argc, argv, options);
 if (argc != 5)
     usage();
+isList = optionExists("list");
 subColumn(argv[1], argv[2], argv[3], argv[4]);
 return 0;
 }
