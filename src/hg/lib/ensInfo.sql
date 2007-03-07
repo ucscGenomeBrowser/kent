7ecf41dccca9a4db5b44d67894c3fb410cfd2630
baertsch <baertsch>
2007-03-07 20:02:22 +0000

    rename method to class

--- src/hg/lib/ensInfo.sql
+++ src/hg/lib/ensInfo.sql
@@ -3,7 +3,7 @@ CREATE TABLE ensInfo (
     transcriptId varchar(255) not null,	# Ensembl transcript ID
     otterId varchar(255) not null,	# otter (Ensembl db) transcript ID
     geneId varchar(255) not null,	# Ensembl gene ID
-    method varchar(255) not null,	# GTF method field
+    class varchar(255) not null,	# Ensembl gene class
     geneDesc varchar(255) not null,	# Ensembl gene description
     confidence varchar(255) not null,	# Ensembl gene confidence
               #Indices
