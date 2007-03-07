ed8317e89154726955446a2b6743a3a34f86fa30
baertsch <baertsch>
2007-03-07 20:49:10 +0000

    rename field confidence to class  and class to type for compatibility with kate's itemClass close

--- src/hg/lib/ensInfo.sql
+++ src/hg/lib/ensInfo.sql
@@ -3,9 +3,9 @@ CREATE TABLE ensInfo (
     name varchar(255) not null,	# Ensembl transcript ID
     otherId varchar(255) not null,	# other ID
     geneId varchar(255) not null,	# Ensembl gene ID
-    class varchar(255) not null,	# Ensembl gene class
+    type varchar(255) not null,	# Ensembl gene type
     geneDesc longblob not null,	# Ensembl gene description
-    confidence varchar(255) not null,	# Ensembl gene confidence
+    class varchar(255) not null,	# Ensembl gene confidence
               #Indices
     PRIMARY KEY(name)
 );
