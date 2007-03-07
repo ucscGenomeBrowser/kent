bd15e75557dc11b0dc0d6f3ba604cd6e444e4bf0
baertsch <baertsch>
2007-03-07 20:14:57 +0000

    longer description

--- src/hg/lib/ensInfo.sql
+++ src/hg/lib/ensInfo.sql
@@ -4,7 +4,7 @@ CREATE TABLE ensInfo (
     otherId varchar(255) not null,	# other ID
     geneId varchar(255) not null,	# Ensembl gene ID
     class varchar(255) not null,	# Ensembl gene class
-    geneDesc varchar(255) not null,	# Ensembl gene description
+    geneDesc varchar(512) not null,	# Ensembl gene description
     confidence varchar(255) not null,	# Ensembl gene confidence
               #Indices
     PRIMARY KEY(name)
