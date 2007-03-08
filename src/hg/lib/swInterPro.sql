889e63a048e4556a05044663407464f32807012f
fanhsu <fanhsu>
2007-03-08 20:52:49 +0000

    Get rid of the first comment line.

--- src/hg/lib/swInterPro.sql
+++ src/hg/lib/swInterPro.sql
@@ -1,4 +1,3 @@
-/* cross-reference table between SWISS-PROT accession number and InterPro ID */
 CREATE TABLE swInterPro (
   accession varchar(40) NOT NULL default '',
   interProId varchar(40) NOT NULL default '',
