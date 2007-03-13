e9669a40a704ab2d50474655402b8877551d78d1
kate <kate>
2007-03-13 04:37:11 +0000

    Wrong -- not a typo

--- src/hg/utils/automation/doRecipBest.pl
+++ src/hg/utils/automation/doRecipBest.pl
@@ -6,7 +6,7 @@
 # This script should probably be folded back into doBlastzChainNet.pl
 # eventually.
 
-# $Id: doRecipBest.pl,v 1.3 2007/03/13 04:35:50 kate Exp $
+# $Id: doRecipBest.pl,v 1.4 2007/03/13 04:37:11 kate Exp $
 
 use Getopt::Long;
 use warnings;
@@ -70,7 +70,7 @@ Assumptions:
 2. $HgAutomate::clusterData/\$db/chrom.sizes contains all sequence names and sizes from
    \$db.2bit (for both \$tDb and \$qDb).
 3. The buildDir contains axtChain/\$tDb.\$qDb.over.chain.gz and the download dir
-   goldenPath/\$tDb/vs\$qDb already exists.
+   goldenPath/\$tDb/vs\$QDb already exists.
 4. The blastz DEF file is located in $hgAutomate::clusterData/\$tDb/blastz.$qDb.
 " if ($detailed);
   print "\n";
@@ -292,7 +292,7 @@ sub loadDef {
 #########################################################################
 # main
 
-#$opt_debug = 1;
+$opt_debug = 1;
 
 # Prevent "Suspended (tty input)" hanging:
 &HgAutomate::closeStdin();
