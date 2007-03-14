40fc4a8f7b4975116be056ec8557ea537ca5418f
heather <heather>
2007-03-14 21:20:09 +0000

    functions moved to kent/src/hg/lib; name changes

--- src/hg/snp/snpLoad/hapmapSummary.c
+++ src/hg/snp/snpLoad/hapmapSummary.c
@@ -36,9 +36,9 @@
 #include "common.h"
 #include "hash.h"
 #include "hdb.h"
-#include "hapmapAllelesCombined.h"
+#include "hapmapSnpsCombined.h"
 
-static char const rcsid[] = "$Id: hapmapSummary.c,v 1.8 2007/03/13 20:56:36 heather Exp $";
+static char const rcsid[] = "$Id: hapmapSummary.c,v 1.9 2007/03/14 21:20:09 heather Exp $";
 
 struct orthoAllele
     {
@@ -57,7 +57,7 @@ void usage()
 /* Explain usage and exit. */
 {
 errAbort(
-    "hapmapSummary - get hapmapAllelesSummary table from overall hapmapAllelesCombined and ortho tables\n"
+    "hapmapSummary - create hapmapAllelesSummary table from overall hapmapSnpsCombined and ortho tables\n"
     "usage:\n"
     "    hapmapSummary db hapmapTable hapmapOrthoTable1 hapmapOrthoTable2\n");
 }
@@ -84,17 +84,6 @@ while ((row = sqlNextRow(sr)) != NULL)
     }
 sqlFreeResult(&sr);
 hFreeConn(&conn);
-return ret;
-}
-
-int getPopCount(struct hapmapAllelesCombined *hmac)
-/* move this to kent/src/hg/lib/hapmapAllelesCombined.c */
-{
-int ret = 0;
-if (hmac->allele1CountCEU > 0 || hmac->allele2CountCEU > 0 || hmac->heteroCountCEU > 0) ret++;
-if (hmac->allele1CountCHB > 0 || hmac->allele2CountCHB > 0 || hmac->heteroCountJPT > 0) ret++;
-if (hmac->allele1CountJPT > 0 || hmac->allele2CountJPT > 0 || hmac->heteroCountCHB > 0) ret++;
-if (hmac->allele1CountYRI > 0 || hmac->allele2CountYRI > 0 || hmac->heteroCountYRI > 0) ret++;
 return ret;
 }
 
@@ -127,97 +116,6 @@ return s;
 
 
 
-boolean isMixed(struct hapmapAllelesCombined *hmac)
-/* move this to kent/src/hg/lib/hapmapAllelesCombined.c */
-/* return TRUE if different populations have a different major allele */
-/* don't need to consider heteroCount */
-{
-int allele1Count = 0;
-int allele2Count = 0;
-
-if (hmac->allele1CountCEU >= hmac->allele2CountCEU && hmac->allele1CountCEU > 0) 
-    allele1Count++;
-else if (hmac->allele2CountCEU > 0)
-    allele2Count++;
-
-if (hmac->allele1CountCHB >= hmac->allele2CountCHB && hmac->allele1CountCHB > 0) 
-    allele1Count++;
-else if (hmac->allele2CountCHB > 0)
-    allele2Count++;
-
-if (hmac->allele1CountJPT >= hmac->allele2CountJPT && hmac->allele1CountJPT > 0) 
-    allele1Count++;
-else if (hmac->allele2CountJPT > 0)
-    allele2Count++;
-
-if (hmac->allele1CountYRI >= hmac->allele2CountYRI && hmac->allele1CountYRI > 0) 
-    allele1Count++;
-else if (hmac->allele2CountYRI > 0)
-    allele2Count++;
-
-if (allele1Count > 0 && allele2Count > 0) return TRUE;
-
-return FALSE;
-}
-
-void printOne(struct hapmapAllelesCombined *hmac)
-/* move this to kent/src/hg/lib/hapmapAllelesCombined.c */
-{
-verbose(1, "chrom = %s\n", hmac->chrom);
-verbose(1, "chromStart = %d\n", hmac->chromStart);
-verbose(1, "chromEnd = %d\n", hmac->chromEnd);
-verbose(1, "name = %s\n", hmac->name);
-verbose(1, "score = %d\n", hmac->score);
-verbose(1, "strand = %s\n", hmac->strand);
-verbose(1, "observed = %s\n", hmac->observed);
-verbose(1, "allele1 = %s\n", hmac->allele1);
-verbose(1, "allele1CountCEU = %d\n", hmac->allele1CountCEU);
-verbose(1, "allele1CountCHB = %d\n", hmac->allele1CountCHB);
-verbose(1, "allele1CountJPT = %d\n", hmac->allele1CountJPT);
-verbose(1, "allele1CountYRI = %d\n", hmac->allele1CountYRI);
-verbose(1, "allele2 = %s\n", hmac->allele2);
-verbose(1, "allele2CountCEU = %d\n", hmac->allele2CountCEU);
-verbose(1, "allele2CountCHB = %d\n", hmac->allele2CountCHB);
-verbose(1, "allele2CountJPT = %d\n", hmac->allele2CountJPT);
-verbose(1, "allele2CountYRI = %d\n", hmac->allele2CountYRI);
-verbose(1, "heteroCountCEU = %d\n", hmac->heteroCountCEU);
-verbose(1, "heteroCountCHB = %d\n", hmac->heteroCountCHB);
-verbose(1, "heteroCountJPT = %d\n", hmac->heteroCountJPT);
-verbose(1, "heteroCountYRI = %d\n", hmac->heteroCountYRI);
-}
-
-
-int calcHet(struct hapmapAllelesCombined *hmac)
-/* calculate heterozygosity (2pq) */
-/* convert from individuals to alleles */
-{
-int allele1Count = 0;
-int allele2Count = 0;
-int total = 0;
-float p = 0.0; // freq1
-float q = 0.0; // freq2
-
-allele1Count = allele1Count + (2 * hmac->allele1CountCEU) + hmac->heteroCountCEU;
-allele1Count = allele1Count + (2 * hmac->allele1CountCHB) + hmac->heteroCountCHB;
-allele1Count = allele1Count + (2 * hmac->allele1CountJPT) + hmac->heteroCountJPT;
-allele1Count = allele1Count + (2 * hmac->allele1CountYRI) + hmac->heteroCountYRI;
-
-allele2Count = allele2Count + (2 * hmac->allele2CountCEU) + hmac->heteroCountCEU;
-allele2Count = allele2Count + (2 * hmac->allele2CountCHB) + hmac->heteroCountCHB;
-allele2Count = allele2Count + (2 * hmac->allele2CountJPT) + hmac->heteroCountJPT;
-allele2Count = allele2Count + (2 * hmac->allele2CountYRI) + hmac->heteroCountYRI;
-
-total = allele1Count + allele2Count;
-
-if (total == 0) return 0;
-
-p = (float)allele1Count / (float)total;
-q = (float)allele2Count / (float)total;
-
-return 2000*p*q;
-}
-
-
 void processSnps(char *inputTableName, struct hash *orthoHash1, struct hash *orthoHash2)
 /* read combined table, write summary table */
 /* bin chrom chromStart chromEnd name score (heterozygosity) strand observed
@@ -236,9 +134,9 @@ struct sqlResult *sr;
 char **row;
 struct hashEl *hel = NULL;
 FILE *outputFileHandle = mustOpen("hapmapSummary.tab", "w");
-struct hapmapAllelesCombined *hmac = NULL;
+struct hapmapSnpsCombined *thisItem = NULL;
 struct orthoAllele *a = NULL;
-struct alleleSummary *i = NULL;
+struct alleleSummary *as = NULL;
 int popCount = 0;
 int score = 0;
 
@@ -246,40 +144,40 @@ safef(query, sizeof(query), "select * from %s", inputTableName);
 sr = sqlGetResult(conn, query);
 while ((row = sqlNextRow(sr)) != NULL)
     {
-    hmac = hapmapAllelesCombinedLoad(row + 1);
-    score = calcHet(hmac);
+    thisItem = hapmapSnpsCombinedLoad(row + 1);
+    score = calcHet(thisItem);
     /* update score (column 5); otherwise first 9 columns are the same */
     fprintf(outputFileHandle, "%s\t%d\t%d\t%s\t%d\t%s\t%s\t%s\t%s\t", 
-	    hmac->chrom, hmac->chromStart, hmac->chromEnd, hmac->name, 
-	    score, hmac->strand, hmac->observed, hmac->allele1, hmac->allele2);
-    popCount = getPopCount(hmac);
+	    thisItem->chrom, thisItem->chromStart, thisItem->chromEnd, thisItem->name, 
+	    score, thisItem->strand, thisItem->observed, thisItem->allele1, thisItem->allele2);
+    popCount = getPopCount(thisItem);
     fprintf(outputFileHandle, "%d\t", popCount);
-    if (isMixed(hmac))
+    if (isMixed(thisItem))
         fprintf(outputFileHandle, "YES\t");
     else
         fprintf(outputFileHandle, "NO\t");
 
-    i = getAlleleSummary(hmac->allele1, hmac->allele1CountCEU, 
-                         hmac->allele2, hmac->allele2CountCEU,
-			 hmac->heteroCountCEU);
-    fprintf(outputFileHandle, "%s\t%d\t%d\t", i->allele, i->count, i->total);
+    as = getAlleleSummary(thisItem->allele1, thisItem->homoCount1CEU, 
+                         thisItem->allele2, thisItem->homoCount2CEU,
+			 thisItem->heteroCountCEU);
+    fprintf(outputFileHandle, "%s\t%d\t%d\t", as->allele, as->count, as->total);
 
-    i = getAlleleSummary(hmac->allele1, hmac->allele1CountCHB, 
-                         hmac->allele2, hmac->allele2CountCHB,
-			 hmac->heteroCountCHB);
-    fprintf(outputFileHandle, "%s\t%d\t%d\t", i->allele, i->count, i->total);
+    as = getAlleleSummary(thisItem->allele1, thisItem->homoCount1CHB, 
+                         thisItem->allele2, thisItem->homoCount2CHB,
+			 thisItem->heteroCountCHB);
+    fprintf(outputFileHandle, "%s\t%d\t%d\t", as->allele, as->count, as->total);
 
-    i = getAlleleSummary(hmac->allele1, hmac->allele1CountJPT, 
-                         hmac->allele2, hmac->allele2CountJPT,
-			 hmac->heteroCountJPT);
-    fprintf(outputFileHandle, "%s\t%d\t%d\t", i->allele, i->count, i->total);
+    as = getAlleleSummary(thisItem->allele1, thisItem->homoCount1JPT, 
+                         thisItem->allele2, thisItem->homoCount2JPT,
+			 thisItem->heteroCountJPT);
+    fprintf(outputFileHandle, "%s\t%d\t%d\t", as->allele, as->count, as->total);
 
-    i = getAlleleSummary(hmac->allele1, hmac->allele1CountYRI, 
-                         hmac->allele2, hmac->allele2CountYRI,
-			 hmac->heteroCountYRI);
-    fprintf(outputFileHandle, "%s\t%d\t%d\t", i->allele, i->count, i->total);
+    as = getAlleleSummary(thisItem->allele1, thisItem->homoCount1YRI, 
+                         thisItem->allele2, thisItem->homoCount2YRI,
+			 thisItem->heteroCountYRI);
+    fprintf(outputFileHandle, "%s\t%d\t%d\t", as->allele, as->count, as->total);
 
-    hel = hashLookup(orthoHash1, hmac->name);
+    hel = hashLookup(orthoHash1, thisItem->name);
     if (hel) 
         {
 	a = (struct orthoAllele *)hel->val;
@@ -287,7 +185,7 @@ while ((row = sqlNextRow(sr)) != NULL)
 	}
     else
         fprintf(outputFileHandle, "none\t0\t");
-    hel = hashLookup(orthoHash2, hmac->name);
+    hel = hashLookup(orthoHash2, thisItem->name);
     if (hel) 
         {
 	a = (struct orthoAllele *)hel->val;
@@ -305,7 +203,7 @@ hFreeConn(&conn);
 
 
 int main(int argc, char *argv[])
-/* output hapmapAllelesSummary table from overall hapmapAllelesCombined and ortho tables */
+/* output hapmapAllelesSummary table from overall hapmapSnpsCombined and ortho tables */
 {
 char *database = NULL;
 char *inputTableName = NULL;
