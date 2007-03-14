226047da957b1cf980f4e016171a9d774e8f04fd
heather <heather>
2007-03-14 20:33:56 +0000

    name changes

--- src/hg/snp/snpLoad/hapmapMixed.c
+++ src/hg/snp/snpLoad/hapmapMixed.c
@@ -1,21 +1,21 @@
 /* hapmapMixed -- compare mixed */
-/* A little utility to read the hapmapAllelesCombined table */
+/* A little utility to read the hapmapSnpsCombined table */
 /* It looks only at rows where all 4 populations have data and the major allele is mixed */
 /* It reports counts where 3 populations have a certain allele and the 4th is different */
 
 #include "common.h"
 
-#include "hapmapAllelesCombined.h"
+#include "hapmapSnpsCombined.h"
 #include "hdb.h"
 #include "sqlNum.h"
 
-static char const rcsid[] = "$Id: hapmapMixed.c,v 1.1 2007/02/16 19:04:55 heather Exp $";
+static char const rcsid[] = "$Id: hapmapMixed.c,v 1.2 2007/03/14 20:33:56 heather Exp $";
 
 void usage()
 /* Explain usage and exit. */
 {
 errAbort(
-  "hapmapMixed - Read hapmapAllelesCombined, report counts of mixed.\n"
+  "hapmapMixed - Read hapmapSnpsCombined, report counts of mixed.\n"
   "usage:\n"
   "  hapmapMixed database\n");
 }
@@ -31,42 +31,44 @@ return allele2;
 }
 
 
-int getPopCount(struct hapmapAllelesCombined *hmac)
+int getPopCount(struct hapmapSnpsCombined *thisItem)
+/* move this to kent/src/hg/lib */
 {
 int ret = 0;
-if (hmac->allele1CountCEU > 0 || hmac->allele2CountCEU > 0 || hmac->heteroCountCEU > 0) ret++;
-if (hmac->allele1CountCHB > 0 || hmac->allele2CountCHB > 0 || hmac->heteroCountJPT > 0) ret++;
-if (hmac->allele1CountJPT > 0 || hmac->allele2CountJPT > 0 || hmac->heteroCountCHB > 0) ret++;
-if (hmac->allele1CountYRI > 0 || hmac->allele2CountYRI > 0 || hmac->heteroCountYRI > 0) ret++;
+if (thisItem->homoCount1CEU > 0 || thisItem->homoCount2CEU > 0 || thisItem->heteroCountCEU > 0) ret++;
+if (thisItem->homoCount1CHB > 0 || thisItem->homoCount2CHB > 0 || thisItem->heteroCountJPT > 0) ret++;
+if (thisItem->homoCount1JPT > 0 || thisItem->homoCount2JPT > 0 || thisItem->heteroCountCHB > 0) ret++;
+if (thisItem->homoCount1YRI > 0 || thisItem->homoCount2YRI > 0 || thisItem->heteroCountYRI > 0) ret++;
 return ret;
 }
 
 
-boolean isMixed(struct hapmapAllelesCombined *hmac)
+boolean isMixed(struct hapmapSnpsCombined *thisItem)
+/* move this to kent/src/hg/lib */
 /* return TRUE if different populations have a different major allele */
 /* don't need to consider heteroCount */
 {
 int allele1Count = 0;
 int allele2Count = 0;
 
-if (hmac->allele1CountCEU >= hmac->allele2CountCEU && hmac->allele1CountCEU > 0) 
+if (thisItem->homoCount1CEU >= thisItem->homoCount2CEU && thisItem->homoCount1CEU > 0) 
     allele1Count++;
-else if (hmac->allele2CountCEU > 0)
+else if (thisItem->homoCount2CEU > 0)
     allele2Count++;
 
-if (hmac->allele1CountCHB >= hmac->allele2CountCHB && hmac->allele1CountCHB > 0) 
+if (thisItem->homoCount1CHB >= thisItem->homoCount2CHB && thisItem->homoCount1CHB > 0) 
     allele1Count++;
-else if (hmac->allele2CountCHB > 0)
+else if (thisItem->homoCount2CHB > 0)
     allele2Count++;
 
-if (hmac->allele1CountJPT >= hmac->allele2CountJPT && hmac->allele1CountJPT > 0) 
+if (thisItem->homoCount1JPT >= thisItem->homoCount2JPT && thisItem->homoCount1JPT > 0) 
     allele1Count++;
-else if (hmac->allele2CountJPT > 0)
+else if (thisItem->homoCount2JPT > 0)
     allele2Count++;
 
-if (hmac->allele1CountYRI >= hmac->allele2CountYRI && hmac->allele1CountYRI > 0) 
+if (thisItem->homoCount1YRI >= thisItem->homoCount2YRI && thisItem->homoCount1YRI > 0) 
     allele1Count++;
-else if (hmac->allele2CountYRI > 0)
+else if (thisItem->homoCount2YRI > 0)
     allele2Count++;
 
 if (allele1Count > 0 && allele2Count > 0) return TRUE;
@@ -82,7 +84,7 @@ char query[64];
 struct sqlConnection *conn = hAllocConn();
 struct sqlResult *sr;
 char **row;
-struct hapmapAllelesCombined *hmac = NULL;
+struct hapmapSnpsCombined *thisItem = NULL;
 int popCount = 0;
 char *majorAlleleCEU;
 char *majorAlleleCHB;
@@ -93,24 +95,24 @@ int countCHB = 0;
 int countJPT = 0;
 int countYRI = 0;
 
-safef(query, sizeof(query), "select * from hapmapAllelesCombined");
+safef(query, sizeof(query), "select * from hapmapSnpsCombined");
 
 sr = sqlGetResult(conn, query);
 while ((row = sqlNextRow(sr)) != NULL)
     {
-    hmac = hapmapAllelesCombinedLoad(row + 1);
-    popCount = getPopCount(hmac);
+    thisItem = hapmapSnpsCombinedLoad(row + 1);
+    popCount = getPopCount(thisItem);
     if (popCount != 4) continue;
-    if (!isMixed(hmac)) continue;
+    if (!isMixed(thisItem)) continue;
 
-    majorAlleleCEU = getMajorAllele(hmac->allele1, hmac->allele1CountCEU, 
-                                    hmac->allele2, hmac->allele2CountCEU);
-    majorAlleleCHB = getMajorAllele(hmac->allele1, hmac->allele1CountCHB, 
-                                    hmac->allele2, hmac->allele2CountCHB);
-    majorAlleleJPT = getMajorAllele(hmac->allele1, hmac->allele1CountJPT, 
-                                    hmac->allele2, hmac->allele2CountJPT);
-    majorAlleleYRI = getMajorAllele(hmac->allele1, hmac->allele1CountYRI, 
-                                    hmac->allele2, hmac->allele2CountYRI);
+    majorAlleleCEU = getMajorAllele(thisItem->allele1, thisItem->homoCount1CEU, 
+                                    thisItem->allele2, thisItem->homoCount2CEU);
+    majorAlleleCHB = getMajorAllele(thisItem->allele1, thisItem->homoCount1CHB, 
+                                    thisItem->allele2, thisItem->homoCount2CHB);
+    majorAlleleJPT = getMajorAllele(thisItem->allele1, thisItem->homoCount1JPT, 
+                                    thisItem->allele2, thisItem->homoCount2JPT);
+    majorAlleleYRI = getMajorAllele(thisItem->allele1, thisItem->homoCount1YRI, 
+                                    thisItem->allele2, thisItem->homoCount2YRI);
     if (sameString(majorAlleleCEU, majorAlleleCHB) && sameString(majorAlleleCHB, majorAlleleJPT))
         countYRI++;
     else if (sameString(majorAlleleCEU, majorAlleleCHB) && sameString(majorAlleleCHB, majorAlleleYRI))
@@ -137,9 +139,9 @@ if (argc != 2)
 
 hSetDb(argv[1]);
 
-if (!hTableExists("hapmapAllelesCombined")) 
+if (!hTableExists("hapmapSnpsCombined")) 
     {
-    verbose(1, "can't find hapmapAllelesCombined table \n");
+    verbose(1, "can't find hapmapSnpsCombined table \n");
     return 1;
     }
 
