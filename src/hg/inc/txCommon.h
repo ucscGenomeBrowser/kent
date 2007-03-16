afbf801d1f8a5a04f03758738a3cb0e28406028d
kent <kent>
2007-03-16 18:53:29 +0000

    Adding txCommon module for stuff that many txXxx programs need to share.

---
+++ src/hg/inc/txCommon.h
@@ -0,0 +1,12 @@
+/* txCommon - contains routines shared by various programs in
+ * the txXxx family (which is used to make the UCSC gene set */
+
+#ifndef TXCOMMON_H
+#define TXCOMMON_H
+
+char *txAccFromTempName(char *tempName);
+/* Given name in this.that.acc.version format, return
+ * just acc.version. */
+
+#endif /* TXCOMMON_H */
+
---
+++ src/hg/lib/txCommon.c
@@ -0,0 +1,29 @@
+/* txCommon - contains routines shared by various programs in
+ * the txXxx family (which is used to make the UCSC gene set */
+
+#include "common.h"
+#include "linefile.h"
+#include "hash.h"
+#include "txCommon.h"
+
+char *txAccFromTempName(char *tempName)
+/* Given name in this.that.acc.version format, return
+ * just acc.version. */
+{
+char *s = tempName + strlen(tempName);
+int periodCount = 0;
+while (s > tempName && periodCount < 2)
+    {
+    char c = *(--s);
+    if (c == '.')
+        ++periodCount;
+    }
+if (periodCount < 2)
+    errAbort("Can't extract accession.ver from %s, expecting at least two periods",
+    	tempName); 
+s += 1;
+int len = strlen(s);
+if (len < 8 || len > 16)
+    errAbort("Accession seems a little too long or short in %s", tempName);
+return s;
+}
