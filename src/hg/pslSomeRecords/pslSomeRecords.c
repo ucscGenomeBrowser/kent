7911f2957d7e32175f52286da3d02985527726a9
kent <kent>
2007-03-23 16:00:56 +0000

    Adding -tToo option.

--- src/hg/pslSomeRecords/pslSomeRecords.c
+++ src/hg/pslSomeRecords/pslSomeRecords.c
@@ -3,7 +3,10 @@
 #include "linefile.h"
 #include "hash.h"
 #include "options.h"
+#include "obscure.h"
 #include "psl.h"
+
+boolean tToo = FALSE;
 
 void usage()
 /* Explain usage and exit. */
@@ -11,13 +14,21 @@ void usage()
 errAbort(
   "pslSomeRecords - Extract multiple psl records\n"
   "usage:\n"
-  "   pslSomeRecords pslIn listOfQNames pslOut\n"
+  "   pslSomeRecords pslIn listFile pslOut\n"
+  "where:\n"
+  "   pslIn is the input psl file\n"
+  "   listFile is a file with a qName (rna accession usually)\n"
+  "          on each line\n"
+  "   pslOut is the output psl file\n"
   "options:\n"
-  "   -xxx=XXX\n"
+  "   -tToo - if set, the list file is two column, qName and tName.\n"
+  "           In this case only records matching on both q and t are\n"
+  "           output\n"
   );
 }
 
 static struct optionSpec options[] = {
+   {"tToo", OPTION_BOOLEAN},
    {NULL, 0},
 };
 
@@ -36,15 +47,28 @@ return hash;
 void pslSomeRecords(char *pslIn, char *listName, char *pslOut)
 /* pslSomeRecords - Extract multiple psl records. */
 {
-struct hash *hash = hashLines(listName);
+struct hash *hash;
+if (tToo)
+    hash = hashTwoColumnFile(listName);
+else
+    hash = hashWordsInFile(listName, 16);
 struct lineFile *lf = pslFileOpen(pslIn);
 FILE *f = mustOpen(pslOut, "w");
 struct psl *psl;
 
 while ((psl = pslNext(lf) ) != NULL)
     {
-    if (hashLookup(hash, psl->qName))
-	pslTabOut(psl, f);
+    if (tToo)
+        {
+	char *tName = hashFindVal(hash, psl->qName);
+	if (tName != NULL && sameString(psl->tName, tName))
+	    pslTabOut(psl, f);
+	}
+    else
+	{
+	if (hashLookup(hash, psl->qName))
+	    pslTabOut(psl, f);
+	}
     pslFree(&psl);
     }
 }
@@ -52,9 +76,10 @@ while ((psl = pslNext(lf) ) != NULL)
 int main(int argc, char *argv[])
 /* Process command line. */
 {
-optionHash(&argc, argv);
+optionInit(&argc, argv, options);
 if (argc != 4)
     usage();
+tToo = optionExists("tToo");
 pslSomeRecords(argv[1], argv[2], argv[3]);
 return 0;
 }
