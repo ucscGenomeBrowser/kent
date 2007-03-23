36997ba8dc5acc70cef76d957c1681534f19c214
kent <kent>
2007-03-23 06:02:42 +0000

    Refactoring and adding a test case.

--- src/utils/spacedToTab/spacedToTab.c
+++ src/utils/spacedToTab/spacedToTab.c
@@ -4,7 +4,7 @@
 #include "hash.h"
 #include "options.h"
 
-static char const rcsid[] = "$Id: spacedToTab.c,v 1.1 2003/09/24 11:24:53 kent Exp $";
+static char const rcsid[] = "$Id: spacedToTab.c,v 1.2 2007/03/23 06:02:42 kent Exp $";
 
 void usage()
 /* Explain usage and exit. */
@@ -23,23 +23,42 @@ static struct optionSpec options[] = {
    {NULL, 0},
 };
 
-struct column
+struct spacedColumn
 /* Specs on a column. */
     {
-    struct column *next;
+    struct spacedColumn *next;
     int start;	/* Starting index. */
     int size;	/* Size of column. */
     };
 
-struct column *figureColumns(char *fileName)
-/* Read file and figure out where columns are. */
+struct spacedColumn *spacedColumnListFromSample(char *sample)
+/* Return spaced column list from a sampleline , which is assumed to
+ * have no spaces except between columns */
 {
-struct lineFile *lf = lineFileOpen(fileName, TRUE);
+struct spacedColumn *col, *colList = NULL;
+char *dupe = cloneString(sample);
+char *word, *line = dupe;
+while ((word = nextWord(&line)) != NULL)
+    {
+    AllocVar(col);
+    col->start = word - dupe;
+    col->size = strlen(word);
+    slAddHead(&colList, col);
+    }
+freeMem(dupe);
+slReverse(&colList);
+return colList;
+}
+
+struct spacedColumn *spacedColumnListFromLineFile(struct lineFile *lf)
+/* Scan through lineFile and figure out column spacing. Assumes
+ * file contains nothing but columns. */
+{
 int maxLine = 64*1024;
 int lineSize, widestLine = 0;
 char *projection = needMem(maxLine+1);
-char *line, *word;
-struct column *colList = NULL, *col;
+char *line;
+struct spacedColumn *colList;
 int i;
 
 /* Create projection of all lines. */
@@ -56,56 +75,73 @@ while (lineFileNext(lf, &line, &lineSize))
 	    projection[i] = line[i];
 	}
     }
-lineFileClose(&lf);
 projection[widestLine] = 0;
-
-/* Chop up projection by white space. */
-line = projection;
-while ((word = nextWord(&line)) != NULL)
-    {
-    AllocVar(col);
-    col->start = word - projection;
-    col->size = strlen(word);
-    slAddHead(&colList, col);
-    }
-slReverse(&colList);
+colList = spacedColumnListFromSample(projection);
+freeMem(projection);
 return colList;
 }
 
-int biggestColSize(struct column *colList)
+struct spacedColumn *spacedColumnListFromFile(char *fileName)
+/* Read file and figure out where columns are. */
+{
+struct lineFile *lf = lineFileOpen(fileName, TRUE);
+struct spacedColumn *colList = spacedColumnListFromLineFile(lf);
+lineFileClose(&lf);
+return colList;
+}
+
+int spacedColumnListBiggestSize(struct spacedColumn *colList)
 /* Return size of biggest column. */
 {
 int maxSize = 0;
-struct column *col;
+struct spacedColumn *col;
 for (col = colList; col != NULL; col = col->next)
     if (maxSize < col->size)
         maxSize = col->size;
 return maxSize;
 }
 
+boolean spacedColumnParseLine(struct spacedColumn *colList, 
+	char *line, char *row[])
+/* Parse line into row according to colList.  This will
+ * trim leading and trailing spaces. It will write 0's
+ * into line.  Returns FALSE if there's a problem (like
+ * line too short.) */
+{
+struct spacedColumn *col;
+int i, len = strlen(line);
+for (i=0, col = colList; col != NULL; col = col->next, ++i)
+    {
+    if (col->start > len)
+	return FALSE;
+    int end = col->start + col->size;
+    if (end > len) end = len;
+    line[end] = 0;
+    row[i] = trimSpaces(line + col->start);
+    }
+return TRUE;
+}
+
 void spacedToTab(char *inFile, char *outFile)
 /* spacedToTab - Convert fixed width space separated fields to tab separated. */
 {
-struct column *col, *colList = figureColumns(inFile);
-int maxSize = biggestColSize(colList);
-char *buf = needMem(maxSize+1);
+struct spacedColumn *colList = spacedColumnListFromFile(inFile);
+int colCount = slCount(colList);
 struct lineFile *lf = lineFileOpen(inFile, TRUE);
-char *line, *word;
+char *line;
 int lineSize;
 FILE *f = mustOpen(outFile, "w");
+char *row[colCount];
 
 while (lineFileNext(lf, &line, &lineSize))
     {
-    for (col = colList; col != NULL; col = col->next)
+    if (!spacedColumnParseLine(colList, line, row))
+         errAbort("Short line %d of %s\n", lf->lineIx, lf->fileName);
+    int i, lastCol = colCount-1;
+    for (i = 0; i<=lastCol; ++i)
         {
-	if (col->start < lineSize)
-	    {
-	    memcpy(buf, line + col->start, col->size);
-	    buf[col->size] = 0;
-	    word = trimSpaces(buf);
-	    fputs(word, f);
-	    }
-	if (col->next == NULL)
+	fputs(row[i], f);
+	if (i == lastCol)
 	    fputc('\n', f);
 	else
 	    fputc('\t', f);
