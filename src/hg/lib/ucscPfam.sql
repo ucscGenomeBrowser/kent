0fed735295782c6e81aa71b35134be868fd11656
fanhsu <fanhsu>
2007-03-25 17:08:53 +0000

    Fixed a typo.

--- src/hg/lib/ucscPfam.sql
+++ src/hg/lib/ucscPfam.sql
@@ -4,5 +4,5 @@ start 	 int(11) NOT NULL,
 end 	 int(11) NOT NULL,
 domainName varchar(255) NOT NULL,
 KEY 	 ucscId (ucscId(16)),
-KEY 	 domainName (domanName(16))
+KEY 	 domainName (domainName(16))
 ) TYPE=MyISAM;
