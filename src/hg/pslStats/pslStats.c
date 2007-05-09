787820ab1c00e4a22912487f17373a8b00de642d
markd <markd>
2007-05-09 06:16:41 +0000

    added options to include unaligned in stats

--- src/hg/pslStats/pslStats.c
+++ src/hg/pslStats/pslStats.c
@@ -4,14 +4,20 @@
 #include "obscure.h"
 #include "linefile.h"
 #include "hash.h"
+#include "localmem.h"
 #include "psl.h"
+#include "sqlNum.h"
 
-static char const rcsid[] = "$Id: pslStats.c,v 1.3 2007/04/08 00:35:23 markd Exp $";
+static char const rcsid[] = "$Id: pslStats.c,v 1.4 2007/05/09 06:16:41 markd Exp $";
+
+/* size for query name hashs */
+static int queryHashPowTwo = 22;
 
 /* command line option specifications */
 static struct optionSpec optionSpecs[] = {
     {"queryStats", OPTION_BOOLEAN},
     {"overallStats", OPTION_BOOLEAN},
+    {"queries", OPTION_STRING},
     {NULL, 0}
 };
 
@@ -27,14 +33,55 @@ errAbort(
   "Options:\n"
   "  -queryStats - output per-query statistics, the default is per-alignment stats\n"
   "  -overallStats - output overall statstics.\n"
-  );
+  "  -queries=querySizeFile - tab separated file with of expected qNames and sizes.\n"
+  "   If specified statistic will include queries that didn't align.\n");
 }
 
+struct querySizeCnt
+/* structure used to hold query size and a count */
+{
+    unsigned qSize;
+    unsigned alnCnt;
+};
+
+static struct querySizeCnt *querySizeCntGet(struct hash* querySizesTbl,
+                                            char *qName, unsigned qSize)
+/* get entry with size and alignment count, create if not present. */
+{
+struct hashEl *hel = hashStore(querySizesTbl, qName);
+struct querySizeCnt *qs = hel->val;
+if (qs != NULL)
+    {
+    if (qs->qSize != qSize)
+        errAbort("conflicting query for %s: %d and %d", qName, qs->qSize, qSize);
+    }
+else
+    {
+    lmAllocVar(querySizesTbl->lm, qs);
+    hel->val = qs;
+    qs->qSize = qSize;
+    }
+return qs;
+}
+
+/* read qNames and sizes into a hash of querySizeCnt objet */
+static struct hash* querySizeCntLoad(char *querySizeFile)
+{
+struct hash* querySizesTbl = hashNew(queryHashPowTwo);
+struct lineFile *lf = lineFileOpen(querySizeFile, TRUE);
+char *row[2];
+
+while (lineFileNextRowTab(lf, row, ArraySize(row)))
+    querySizeCntGet(querySizesTbl, row[0], sqlUnsigned(row[1]));
+
+lineFileClose(&lf);
+return querySizesTbl;
+}
 
 struct sumStats
 /* structure to hold summary statistics, either per query or overall */
 {
-    char *qName;        /* query name; string is in hash table */
+    char *qName;        /* query name; string is in hash table, NULL if overall */
     unsigned queryCnt;  /* total number of queries, if summary */
     unsigned minQSize;  /* min/max/total sizes of queries */
     unsigned maxQSize;
@@ -50,6 +97,38 @@ struct sumStats
     float minRepMatch; /* fraction that is repeat matches */
     float maxRepMatch;
 };
+
+struct sumStats *sumStatsGetForQuery(struct hash *queryStatsTbl,
+                                     char *qName, unsigned qSize)
+/* lookup a stats on query by name, creating if it doesn't exist */
+{
+struct hashEl *hel = hashStore(queryStatsTbl, qName);
+struct sumStats *qs = hel->val;
+if (qs == NULL)
+    {
+    AllocVar(qs);
+    qs->qName = hel->name;  /* use string in hash */
+    hel->val = qs;
+    qs->minQSize = qs->maxQSize = qSize;
+    }
+else if (qs->minQSize != qSize)
+    errAbort("conflicting query for %s: %d and %d", qName, qs->minQSize, qSize);
+return hel->val;
+}
+
+/* read qNames and sizes into a hash of sumStats objects */
+static struct hash* sumStatsLoad(char *querySizeFile)
+{
+struct hash* querySizesTbl = hashNew(queryHashPowTwo);
+struct lineFile *lf = lineFileOpen(querySizeFile, TRUE);
+char *row[2];
+
+while (lineFileNextRowTab(lf, row, ArraySize(row)))
+    sumStatsGetForQuery(querySizesTbl, row[0], sqlUnsigned(row[1]));
+
+lineFileClose(&lf);
+return querySizesTbl;
+}
 
 static unsigned calcAligned(struct psl *psl)
 /* compute the number of bases aligned */
@@ -163,17 +242,27 @@ ss->alnCnt++;
 static char *alnStatsHdr = "#qName\t" "qSize\t" "tName\t" "tStart\t" "tEnd\t"
 "ident\t" "qCover\t" "repMatch\n";
 
-static void alnStatsOutput(FILE *fh, struct psl *psl)
-/* output statistic on a psl */
+/* format for alignStats output */
+static char *alnStatsFmt = "%s\t%d\t%s\t%d\t%d\t%0.4f\t%0.4f\t%0.4f\n";
+
+static void alignStatsOutputUnaligned(FILE *fh, struct hash* querySizesTbl)
+/* output stats on unaligned */
 {
-fprintf(fh, "%s\t%d\t%s\t%d\t%d\t%0.4f\t%0.4f\t%0.4f\n",
-        psl->qName, psl->qSize, psl->tName, psl->tStart, psl->tEnd,
-        calcIdent(psl), calcQCover(psl), calcRepMatch(psl));
+struct hashCookie cookie = hashFirst(querySizesTbl);
+struct hashEl *hel;
+while ((hel = hashNext(&cookie)) != NULL)
+    {
+    struct querySizeCnt *qs = hel->val;
+    if (qs->alnCnt == 0)
+        fprintf(fh, alnStatsFmt, hel->name, qs->qSize, "", 0, 0, 0.0, 0.0, 0.0);
+    }
 }
 
-static void pslAlignStats(char *pslFile, char *statsFile)
+static void pslAlignStats(char *pslFile, char *statsFile, char *querySizeFile)
 /* collect and output per-alignment stats */
 {
+struct hash* querySizesTbl = (querySizeFile != NULL)
+    ? querySizeCntLoad(querySizeFile) : NULL;
 struct lineFile *pslLf = pslFileOpen(pslFile);
 FILE *fh = mustOpen(statsFile, "w");
 struct psl* psl;
@@ -181,27 +270,18 @@ struct psl* psl;
 fputs(alnStatsHdr, fh);
 while ((psl = pslNext(pslLf)) != NULL)
     {
-    alnStatsOutput(fh, psl);
+    fprintf(fh, alnStatsFmt, psl->qName, psl->qSize, psl->tName, psl->tStart, psl->tEnd,
+            calcIdent(psl), calcQCover(psl), calcRepMatch(psl));
+    if (querySizesTbl != NULL)
+        querySizeCntGet(querySizesTbl, psl->qName, psl->qSize)->alnCnt++;
     pslFree(&psl);
     }
 lineFileClose(&pslLf);
-carefulClose(&fh);
-}
 
-struct sumStats *queryStatsGet(struct hash *queryStatsTbl,
-                               struct psl *psl)
-/* lookup a stats on query by name, creating if it doesn't exist */
-{
-struct hashEl *hel = hashStore(queryStatsTbl, psl->qName);
-if (hel->val == NULL)
-    {
-    struct sumStats *qs;
-    AllocVar(qs);
-    qs->qName = hel->name;  /* use string in hash */
-    qs->queryCnt = 1;
-    hel->val = qs;
-    }
-return hel->val;
+if (querySizesTbl != NULL)
+    alignStatsOutputUnaligned(fh, querySizesTbl);
+
+carefulClose(&fh);
 }
 
 /* header for query statistics */
@@ -233,16 +313,20 @@ while ((hel = hashNext(&cookie)) != NULL)
 carefulClose(&fh);
 }
 
-static void pslQueryStats(char *pslFile, char *statsFile)
+static void pslQueryStats(char *pslFile, char *statsFile, char *querySizeFile)
 /* collect and output per-query stats */
 {
-struct hash *queryStatsTbl = hashNew(22);
+struct hash *queryStatsTbl = (querySizeFile != NULL)
+    ? sumStatsLoad(querySizeFile)
+    : hashNew(queryHashPowTwo);
+
 struct lineFile *pslLf = pslFileOpen(pslFile);
 struct psl* psl;
 
 while ((psl = pslNext(pslLf)) != NULL)
     {
-    sumStatsAccumulate(queryStatsGet(queryStatsTbl, psl), psl);
+    struct sumStats *ss = sumStatsGetForQuery(queryStatsTbl, psl->qName, psl->qSize);
+    sumStatsAccumulate(ss, psl);
     pslFree(&psl);
     }
 lineFileClose(&pslLf);
@@ -269,23 +353,26 @@ fprintf(fh, "%d\t%d\t%d\t%d\t%d\t" "%0.4f\t%0.4f\t%0.4f\t"
 carefulClose(&fh);
 }
 
-static void pslOverallStats(char *pslFile, char *statsFile)
+static void pslOverallStats(char *pslFile, char *statsFile,
+                            char *querySizeFile)
 /* collect and output overall stats */
 {
-struct hash *qNameTbl = hashNew(22); // each qName to int value of 1
 struct sumStats os;
 ZeroVar(&os);
 struct lineFile *pslLf = pslFileOpen(pslFile);
 struct psl* psl;
+struct hash* querySizesTbl = (querySizeFile != NULL)
+    ? querySizeCntLoad(querySizeFile)
+    : hashNew(queryHashPowTwo);
 
 while ((psl = pslNext(pslLf)) != NULL)
     {
-    hashStore(qNameTbl, psl->qName)->val = intToPt(1);
+    querySizeCntGet(querySizesTbl, psl->qName, psl->qSize);
     sumStatsAccumulate(&os, psl);
     pslFree(&psl);
     }
 lineFileClose(&pslLf);
-os.queryCnt = hashIntSum(qNameTbl);
+os.queryCnt = hashNumEntries(querySizesTbl);
 outputOverallStats(statsFile, &os);
 }
 
@@ -295,14 +382,15 @@ int main(int argc, char *argv[])
 optionInit(&argc, argv, optionSpecs);
 if (argc != 3)
     usage();
+char *querySizeFile = optionVal("queries", NULL);
 if (optionExists("queryStats") && optionExists("overallStats"))
     errAbort("can't specify both -queryStats and -overallStats");
 if (optionExists("queryStats"))
-    pslQueryStats(argv[1], argv[2]);
+    pslQueryStats(argv[1], argv[2], querySizeFile);
 else if (optionExists("overallStats"))
-    pslOverallStats(argv[1], argv[2]);
+    pslOverallStats(argv[1], argv[2], querySizeFile);
 else
-    pslAlignStats(argv[1], argv[2]);
+    pslAlignStats(argv[1], argv[2], querySizeFile);
 return 0;
 }
 /*
