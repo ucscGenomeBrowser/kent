f9fc5760bfb75b50809f18b9fc5bfe1513717a02
galt <galt>
2008-01-10 22:50:51 +0000

    added some basic .ra processing

--- src/hg/encode/encodeLoad/doEncodeLoad.rb
+++ src/hg/encode/encodeLoad/doEncodeLoad.rb
@@ -9,7 +9,7 @@
 
 # DO NOT EDIT the /cluster/bin/scripts copy of this file -- 
 # edit the CVS'ed source at:
-# $Header: /projects/compbio/cvsroot/kent/src/hg/encode/encodeLoad/doEncodeLoad.rb,v 1.1 2008/01/10 03:07:39 galt Exp $
+# $Header: /projects/compbio/cvsroot/kent/src/hg/encode/encodeLoad/doEncodeLoad.rb,v 1.2 2008/01/10 22:50:51 galt Exp $
 
 # Global constants
 
@@ -19,9 +19,16 @@ $loadRa = 'load.ra'
 $submitDir = ""
 $submitType = ""
 
+#probably get rid of this:
+$err = false
+
+def errAbort(msg)
+  STDERR.puts msg
+  exit 1
+end
+
 def usage 
-    puts "usage: encodeLoad.rb submission_type project_submission_dir\n"
-    exit 1
+    errAbort "usage: encodeLoad.rb submission_type project_submission_dir\n"
 end
 
 def verbose(level, string)
@@ -32,7 +39,7 @@ end
 
 
 
-def loadWig 
+def loadWig(tablename, files)
     #my ($file) = @_;
     #my $err = system (
     #    "head -10 $file | $loaderPath/wigEncode stdin /dev/null /dev/null >validateWig.out 2>&1");
@@ -46,7 +53,7 @@ def loadWig
     end
 end
 
-def loadBed 
+def loadBed3(tablename, files)
     #my ($file, $type) = @_;
     #my $err = system (
         #"head -10 $file | egrep -v '^track|browser' | $loaderPath/hgLoadBed -noLoad hg18 testTable stdin >validateBed.out 2>&1");
@@ -63,17 +70,44 @@ end
 ############################################################################
 # Misc subroutines
 
-def readRaFile 
-    # Read records from a .ra file into a hash of hashes and return it.
-    #my ($file, $type) = @_;
-    #open(RA, $file) || die "ERROR: Can't open RA file \'$fieldConfigFile\'\n";
-    #my @lines = <RA>;
-    #my %ra = ();
-    #my $raKey = undef;
-    #foreach my $line (@lines) {
-    #}
-    #close(RA);
-    #return %ra;
+def readRaFile(file)
+  inRecord = false
+  ra = []
+  hash = {}
+  keyword = nil
+  f = File.open(file)
+  f.readlines.each do |line|
+    line.chomp!
+    STDERR.puts "#{line}\n"  #debug
+    if line[0,1] == '#'
+      continue
+    end
+    if line == ""
+      if inRecord
+        ra << [keyword,hash]
+        hash = {}
+      end
+      inRecord = false
+    else
+      spc = line.index(' ')
+      if spc == nil
+        errAbort "syntax error: invalid line in .ra file: #{line}\n"
+      end
+      word = line[0,spc]
+      value = line[spc+1,line.length]
+      #STDERR.puts "debug: #{word}=#{value}\n"  #debug
+      unless inRecord
+        inRecord = true
+        keyword = value
+      end
+      hash[word] = value
+    end
+  end
+  if inRecord
+    ra << [keyword,hash]
+  end
+  f.close
+  return ra
 end
 
 
@@ -96,12 +130,29 @@ Dir.chdir $submitDir
 
 # Load files listed in load.ra
 
-f = File.open($loadRa)
-f.readlines.each do |line|
-line.chomp!
-STDERR.puts "#{line}\n"
+ra = readRaFile $loadRa
+
+#STDERR.puts "debug: ra.length=#{ra.length}\n"  #debug
+#STDERR.puts "debug: #{ra.inspect}\n"  #debug
+
+ra.each do |x|
+  h = x[1]
+  STDERR.puts "debug: #{x[0]}\n"  #debug
+  STDERR.puts "  tablename #{h["tablename"]}\n"  #debug
+  STDERR.puts "  type      #{h["type"]}\n"  #debug
+  STDERR.puts "  assembly  #{h["assembly"]}\n"  #debug
+  STDERR.puts "  files     #{h["files"]}\n"  #debug
+  case h["type"]
+  when "wig"
+    STDERR.puts "  it's a WIGGLE!\n"  #debug
+    loadWig h["tablename"], h["files"].split
+  when "bed3"
+    STDERR.puts "  it's a BED3!\n"  #debug
+    loadBed3 h["tablename"], h["files"].split
+  else
+    errAbort "unexpected error: unknown type #{h["type"]} in load.ra\n"
+  end 
 end
-f.close
 
 exit 0
 
