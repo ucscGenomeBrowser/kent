b15dd73cc63834a3c37a490b0204dc59ce0f4776
galt <galt>
2008-01-25 00:59:41 +0000

    very basic loader starting to work

--- src/hg/encode/encodeLoad/doEncodeLoad.rb
+++ src/hg/encode/encodeLoad/doEncodeLoad.rb
@@ -9,104 +9,118 @@
 
 # DO NOT EDIT the /cluster/bin/scripts copy of this file -- 
 # edit the CVS'ed source at:
-# $Header: /projects/compbio/cvsroot/kent/src/hg/encode/encodeLoad/doEncodeLoad.rb,v 1.5 2008/01/16 03:07:26 galt Exp $
+# $Header: /projects/compbio/cvsroot/kent/src/hg/encode/encodeLoad/doEncodeLoad.rb,v 1.6 2008/01/25 00:59:41 galt Exp $
+
+$scripts = "/cluster/bin/scripts"
+
+require "#{$scripts}/err.rb"
+require "#{$scripts}/verbose.rb"
+require "#{$scripts}/ra.rb"
+#require "FileUtils"
+require "tempfile"
+
 
 # Global constants
 
 # Global variables
-$opt_verbose = 1
-$loadRa = 'load.ra'
+$loadRa = 'out/load.ra'
+$unloadRa = 'out/unload.ra'
 $submitDir = ""
 $submitType = ""
 $tempDir = "/data/tmp"
 $encodeDb = "hg18"
-
-#probably get rid of this:
-$err = false
-
-def errAbort(msg)
-  STDERR.puts msg
-  exit 1
-end
+$encInstance = ""
+$encProject = ""
+$sqlCreate = "/cluster/bin/sqlCreate"
 
 def usage 
     errAbort "usage: doEncodeLoad.rb submission_type project_submission_dir\n"
 end
 
-def verbose(level, string)
-  if level <= $opt_verbose
-    STDERR.puts string
+def loadGene(tableName, fileList)
+
+  #TODO replace head -999 with cat
+
+  if system( "head -1000 -q #{fileList} | egrep -v '^track|browser' | ldHgGene -genePredExt #{$encodeDb} #{tableName} stdin > out/loadGene.out 2>&1")
+    print "#{fileList} Passed\n";
+    #debug restore: File.delete "genePred.tab"
+  else
+    STDERR.print "ERROR: File(s) '#{fileList}' failed gene load.\n"
+    errAbort File.read("out/loadGene.out")
   end
 end
+
 
 
 
 def loadWig(tableName, fileList)
-  if system( "head -1000 -q #{fileList} | wigEncode stdin stdout #{tableName}.wib | hgLoadWiggle -pathPrefix=/gbdb/#{$encodeDb}/wib -tmpDir=#{$tempDir} #{$encodeDb} #{tableName} stdin >loadWig.out 2>&1" )
+
+  #TODO replace head -999 with cat
+
+  if system( "head -1000 -q #{fileList} | wigEncode stdin stdout #{tableName}.wib | hgLoadWiggle -pathPrefix=/gbdb/#{$encodeDb}/wib -tmpDir=#{$tempDir} #{$encodeDb} #{tableName} stdin >out/loadWig.out 2>&1" )
       system( "rm -f /gbdb/#{$encodeDb}/wib/#{tableName}.wib" )
       system( "ln -s #{tableName}.wib /gbdb/#{$encodeDb}/wib" )
-      print "#{fileList} Passed\n";
+      print "#{fileList} Loaded\n";
   else 
-      STDERR.print "ERROR: File(s) '#{fileList}' failed wiggle validation.\n";
-      errAbort File.read("validateWig.out")
+      STDERR.print "ERROR: File(s) #{fileList} failed wiggle load.\n"
+      errAbort File.read("out/loadWig.out")
   end
 end
 
-def loadBed3(tableName, fileList)
 
-  system "echo $SHELL" #debug
-  puts  "head -1000 -q #{fileList} | egrep -v '^track|browser' | hgLoadBed #{$encodeDb} #{tableName} stdin >loadBed.out 2>&1"  #debug
+def loadBed(tableName, fileList)
 
-  if system( "head -1000 -q #{fileList} | egrep -v '^track|browser' | hgLoadBed #{$encodeDb} #{tableName} stdin >loadBed.out 2>&1")
-      print "#{fileList} Passed\n";
-      #debug restore: File.delete "bed.tab"
+  #TODO replace head -999 with cat
+
+  cmd = "head -1000 -q #{fileList} | egrep -v '^track|browser' | hgLoadBed #{$encodeDb} #{tableName} stdin -tmpDir=out >out/loadBed.out 2>&1"
+
+  #STDERR.puts "debug: cmd = [#{cmd}]" #debug
+
+  if system( cmd )
+      print "#{fileList} Loaded\n"
+      #debug restore: File.delete "out/bed.tab"
   else
-      STDERR.print "ERROR: File(s) '#{fileList}' failed bed load.\n";
-      errAbort File.read("loadBed.out")
+      STDERR.print "ERROR: File(s) #{fileList} failed bed load.\n";
+      errAbort File.read("out/loadBed.out")
   end
+  temp_file.close
+
 end
 
-############################################################################
-# Misc subroutines
 
-def readRaFile(file)
-  inRecord = false
-  ra = []
-  hash = {}
-  keyword = nil
-  f = File.open(file)
-  f.readlines.each do |line|
-    line.chomp!
-    STDERR.puts "#{line}\n"  #debug
-    if line[0,1] == '#'
-      continue
-    end
-    if line == ""
-      if inRecord
-        ra << [keyword,hash]
-        hash = {}
-      end
-      inRecord = false
-    else
-      spc = line.index(' ')
-      if spc == nil
-        errAbort "syntax error: invalid line in .ra file: #{line}\n"
-      end
-      word = line[0,spc]
-      value = line[spc+1,line.length]
-      #STDERR.puts "debug: #{word}=#{value}\n"  #debug
-      unless inRecord
-        inRecord = true
-        keyword = value
-      end
-      hash[word] = value
-    end
+def loadBed5Plus(tableName, fileList, sqlTable)
+
+  #TODO replace head -999 with cat
+
+  #system "echo $SHELL" #debug
+
+  unless File.exist? "#{$sqlCreate}/#{sqlTable}.sql"
+    errAbort "#{$sqlCreate}/#{sqlTable}.sql not found "
   end
-  if inRecord
-    ra << [keyword,hash]
+  sql = File.read "#{$sqlCreate}/#{sqlTable}.sql"
+  unless sql.gsub! sqlTable, tableName
+    errAbort "sql names do not match for substitution: #{$sqlTable} #{tableName}"
   end
-  f.close
-  return ra
+
+  #STDERR.puts "sql=[#{sql}]"  #debug
+
+  temp_file = Tempfile.new('sql')
+  temp_file.print sql
+  temp_file.flush
+
+  cmd = "head -1000 -q #{fileList} | egrep -v '^track|browser' | hgLoadBed #{$encodeDb} #{tableName} stdin -tmpDir=out -sqlTable=#{temp_file.path} >out/loadBed.out 2>&1"
+
+  #STDERR.puts "debug: cmd = [#{cmd}]" #debug
+
+  if system( cmd )
+      print "#{fileList} Loaded\n"
+      #debug restore: File.delete "out/bed.tab"
+  else
+      STDERR.print "ERROR: File(s) #{fileList} failed bed load.\n";
+      errAbort File.read("out/loadBed.out")
+  end
+  temp_file.close
+
 end
 
 
@@ -123,34 +137,81 @@ end
 $submitType = ARGV[0]	# currently not used
 $submitDir = ARGV[1]
 
-verbose 1, "Loading submission in directory #{$submitDir}\n"
+$encInstance = File.dirname($submitDir)
+$encProject = File.basename($submitDir)
+
+und = $encInstance.rindex('_')
+if und
+  $encInstance = $encInstance[und,$encInstance.length]
+else
+  $encInstance = ""
+end
+
 Dir.chdir $submitDir
+
+# clean out any stuff from previous load
+unless system("doEncodeUnload.rb #{$submitType} #{$submitDir}")
+  errAbort "expected error running doEncodeUnload.rb cleanup script"
+end
+
+unless File.exist? $loadRa
+  errAbort "unexpected error: load.ra not found\n"
+end
+
+#TODO change to : FileUtils.cp $loadRa, $unloadRa
+system "cp #{$loadRa} #{$unloadRa}"
+
+verbose 2, "$encInstance=[#{$encInstance}] und=#{und}\n"
+
+verbose 1, "Loading project in directory #{$submitDir}\n"
 
 
 # Load files listed in load.ra
 
+#debug
+savev = $opt_verbose
+if $opt_verbose < 2
+#    $opt_verbose = 2
+end
+
 ra = readRaFile $loadRa
+
+$opt_verbose = savev  #debug
 
 #STDERR.puts "debug: ra.length=#{ra.length}\n"  #debug
 #STDERR.puts "debug: #{ra.inspect}\n"  #debug
 
+STDERR.puts "\n"  #debug
+
 ra.each do |x|
   h = x[1]
-  STDERR.puts "debug: #{x[0]}\n"  #debug
-  STDERR.puts "  tablename #{h["tablename"]}\n"  #debug
-  STDERR.puts "  type      #{h["type"]}\n"  #debug
-  STDERR.puts "  assembly  #{h["assembly"]}\n"  #debug
-  STDERR.puts "  files     #{h["files"]}\n"  #debug
+  tablenameExt = "#{h["tablename"]}#{$encInstance}_#{$encProject}"
+  verbose 2, "debug: #{x[0]}\n"  
+  verbose 2, "  tablename #{h["tablename"]}\n" 
+  verbose 2, "  type      #{h["type"]}\n"  
+  verbose 2, "  tableType #{h["tableType"]}\n"  
+  verbose 2, "  assembly  #{h["assembly"]}\n" 
+  verbose 2, "  files     #{h["files"]}\n"  
+  verbose 2, "  tablenameExt #{tablenameExt}\n" 
   case h["type"]
+  when "genePred"
+    loadGene tablenameExt, h["files"]
   when "wig"
-    STDERR.puts "  it's a WIGGLE!\n"  #debug
-    loadWig h["tablename"], h["files"]
-  when "bed3"
-    STDERR.puts "  it's a BED3!\n"  #debug
-    loadBed3 h["tablename"], h["files"]
+    loadWig tablenameExt, h["files"]
+  when "bed 5 +"
+    loadBed5Plus tablenameExt, h["files"], h["tableType"]
+  when "bed 3"
+    loadBed tablenameExt, h["files"]
+  when "bed 4"
+    loadBed tablenameExt, h["files"]
+  when "bed 5"
+    loadBed tablenameExt, h["files"]
+  when "bed 6"
+    loadBed tablenameExt, h["files"]
   else
     errAbort "unexpected error: unknown type #{h["type"]} in load.ra\n"
   end 
+  STDERR.puts "\n"  #debug
 end
 
 exit 0
