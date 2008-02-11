743fdcc651e5239e3ecbf7e71a12e4de90025e0f
galt <galt>
2008-02-11 23:44:08 +0000

    give each instance a unique cookie

--- src/hg/encode/hgEncodeSubmit/app/controllers/application.rb
+++ src/hg/encode/hgEncodeSubmit/app/controllers/application.rb
@@ -6,6 +6,7 @@ class ApplicationController < ActionController::Base
   include AuthenticatedSystem
 
   # Pick a unique cookie name to distinguish our session data from others'
-  session :session_key => '_encPipeline_session_id'
+  #session :session_key => '_encPipeline_session_id'
+  session :session_key => "_#{ActiveRecord::Base.configurations[RAILS_ENV]['database']}_session_id"
 
 end
