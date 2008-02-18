2031898726ed0aa48b0925b7303bca8803506d9b
braney <braney>
2008-02-18 02:05:02 +0000

    working fairly well, needs to get gene models for exon output

--- src/hg/ratStuff/mafToProtein/mafToProtein.c
+++ src/hg/ratStuff/mafToProtein/mafToProtein.c
@@ -25,6 +25,8 @@ errAbort(
   "   -geneName=foobar   name of gene as it appears in frameTable\n"
   "   -geneName=foolst   name of file with list of genes\n"
   "   -chrom=chr1        name of chromosome from which to grab genes\n"
+  "   -exons             output exons\n"
+  "   -noTrans           don't translate output into amino acids\n"
   );
 }
 
@@ -32,18 +34,25 @@ static struct optionSpec options[] = {
    {"geneName", OPTION_STRING},
    {"geneList", OPTION_STRING},
    {"chrom", OPTION_STRING},
+   {"exons", OPTION_BOOLEAN},
+   {"noTrans", OPTION_BOOLEAN},
    {NULL, 0},
 };
 
 char *geneName = NULL;
 char *geneList = NULL;
 char *onlyChrom = NULL;
+boolean inExons = FALSE;
+boolean noTrans = TRUE;
 
 struct geneInfo
 {
     struct geneInfo *next;
     struct mafFrames *frame;
     struct mafAli *ali;
+    int exonStart;
+    int exonSize;
+    char *name;
 };
 
 struct speciesInfo
@@ -152,8 +161,8 @@ struct mafAli *ali;
 struct mafAli *list = NULL;
 struct mafAli *nextAli;
 
-printf("getting alis for %s:%d-%d\n",frame->chrom, 
-    frame->chromStart + 1, frame->chromEnd);
+//printf("getting alis for %s:%d-%d\n",frame->chrom, 
+    //frame->chromStart + 1, frame->chromEnd);
 for(ali = aliAll; ali; ali = nextAli)
     {
     nextAli = ali->next;
@@ -164,10 +173,15 @@ for(ali = aliAll; ali; ali = nextAli)
 
     if (mafNeedSubset(ali, masterSrc, frame->chromStart, frame->chromEnd))
 	{
+//	printf("got subset\n");
 	subAli = mafSubset( ali, masterSrc, 
 	    frame->chromStart, frame->chromEnd);
+	//mafWrite(stdout, subAli);
 	if (subAli == NULL)
+	    {
+	    printf("subAli is NULL\n");
 	    continue;
+	    }
 	}
 
     if (subAli)
@@ -178,6 +192,7 @@ for(ali = aliAll; ali; ali = nextAli)
     else
 	slAddHead(&list, ali);
     }
+slReverse(&list);
 
 hFreeConn(&conn);
 
@@ -214,12 +229,129 @@ slReverse(&siList);
 return siList;
 }
 
-void writeOutSpecies(FILE *f, struct speciesInfo *si)
+
+void outSpeciesExon(FILE *f, struct speciesInfo *si, struct geneInfo *gi)
 {
-for(; si ; si = si->next)
+errAbort("haven't implemented trans with exons");
+}
+
+void outSpeciesExonNoTrans(FILE *f, struct speciesInfo *si, struct geneInfo *gi)
+{
+int start = 0;
+int exonNum = 0;
+int end;
+
+for(; gi; gi = gi->next, exonNum++)
     {
-    fprintf(f, ">%s\n", si->name);
-    fprintf(f, "%s\n", si->nucSequence);
+    struct speciesInfo *siTemp = si;
+
+    end = start + gi->exonSize;
+
+    for(; siTemp ; siTemp = siTemp->next)
+	{
+	fprintf(f, ">%s.%s-%d\n",gi->name, siTemp->name, exonNum);
+	for (; start < end; start++)
+	    fprintf(f, "%c", siTemp->nucSequence[start]);
+	fprintf(f, "\n");
+	}
+    }
+}
+
+aaSeq *doTranslate(struct dnaSeq *inSeq, unsigned offset, 
+    unsigned inSize, boolean stop)
+{
+aaSeq *seq;
+DNA *dna = inSeq->dna;
+AA *pep, aa;
+int i, lastCodon;
+int actualSize = 0;
+
+assert(offset <= inSeq->size);
+if ((inSize == 0) || (inSize > (inSeq->size - offset)))
+    inSize = inSeq->size - offset;
+lastCodon = offset + inSize - 3;
+
+AllocVar(seq);
+seq->dna = pep = needLargeMem(inSize/3+1);
+for (i=offset; i <= lastCodon; i += 3)
+    {
+    aa = lookupCodon(dna+i);
+    if (aa == 'X')
+	{
+	if ((dna[i] == '-') ||
+	    (dna[i+1] == '-') ||
+	    (dna[i+2] == '-'))
+	    aa = '-';
+	}
+    if (aa == 0)
+	{
+        if (stop)
+	    break;
+	else
+	    aa = 'Z';
+	}
+    *pep++ = aa;
+    ++actualSize;
+    }
+*pep = 0;
+assert(actualSize <= inSize/3+1);
+seq->size = actualSize;
+seq->name = cloneString(inSeq->name);
+return seq;
+}
+
+void translateProtein(struct speciesInfo *si)
+{
+struct dnaSeq thisSeq;
+aaSeq *outSeq;
+
+thisSeq.dna = si->nucSequence;
+thisSeq.size = si->size;
+outSeq =  doTranslate(&thisSeq, 0,  0, FALSE);
+si->aaSequence  = outSeq->dna;
+}
+
+boolean allDashes(char *seq)
+{
+while (*seq)
+    if (*seq++ != '-')
+	return FALSE;
+
+return TRUE;
+}
+
+void writeOutSpecies(FILE *f, struct speciesInfo *si, struct geneInfo *gi)
+{
+if (noTrans)
+    {
+    if (inExons)
+	outSpeciesExonNoTrans(f, si, gi);
+    else
+	{
+	for(; si ; si = si->next)
+	    {
+	    fprintf(f, ">%s-%s\n", gi->name, si->name);
+	    fprintf(f, "%s\n", si->nucSequence);
+	    }
+	}
+    }
+else
+    {
+    if (inExons)
+	outSpeciesExon(f, si, gi);
+    else
+	{
+	for(; si ; si = si->next)
+	    {
+
+	    translateProtein(si);
+	    if (!allDashes(si->aaSequence))
+		{
+		fprintf(f, ">%s-%s\n", gi->name, si->name);
+		fprintf(f, "%s\n", si->aaSequence);
+		}
+	    }
+	}
     }
 }
 
@@ -227,6 +359,7 @@ for(; si ; si = si->next)
 char *compText[MAX_COMPS]; 
 char *siText[MAX_COMPS]; 
 
+int ii, jj;
 int copyAli(struct hash *siHash, struct mafAli *ali, int start)
 {
 struct mafComp *comp = ali->components;
@@ -243,14 +376,15 @@ for(; comp; comp = comp->next)
 
     struct speciesInfo *si = hashMustFindVal(siHash, comp->src);
     compText[num] = comp->text;
+    //printf("comp->text %p\n",comp->text);
     siText[num] = &si->nucSequence[start];
+    //printf("siText[%d] %p]\n",num, &si->nucSequence[start]);
     ++num;
     if (num == MAX_COMPS)
 	errAbort("can only deal with maf's with less than %d components",
 	    MAX_COMPS);
     }
 
-int ii, jj;
 int count = 0; 
 
 for(jj = 0 ; jj < ali->textSize; jj++)
@@ -259,13 +393,15 @@ for(jj = 0 ; jj < ali->textSize; jj++)
 	{
 	for(ii=0; ii < num; ii++)
 	    {
-	    *siText[ii] = *compText[ii];
+	    if (compText[ii] != NULL)
+		*siText[ii] = *compText[ii];
 	    siText[ii]++;
 	    }
 	count++;
 	}
     for(ii=0; ii < num; ii++)
-	compText[ii]++;
+	if (compText[ii] != NULL)
+	    compText[ii]++;
     }
 
 return start + count;
@@ -278,10 +414,18 @@ struct geneInfo *gi = giList;
 
 for(; gi; gi = gi->next)
     {
-    struct mafAli *ali = gi->ali;
 
+    struct mafAli *ali = gi->ali;
+//    for(; ali; ali = ali->next)
+//	mafWrite(stdout, ali);
+
+
+    gi->exonStart = start;
+ //   ali = gi->ali;
     for(; ali; ali = ali->next)
 	start = copyAli(siHash, ali, start);
+
+    gi->exonSize = start - gi->exonStart;
     }
 
 struct mafComp *comp = giList->ali->components;
@@ -289,30 +433,42 @@ boolean frameNeg = (giList->frame->strand[0] == '-');
 
 if (frameNeg)
     {
+    int size = 0;
+
     for(; comp; comp = comp->next)
 	{
 	struct speciesInfo *si = hashMustFindVal(siHash, comp->src);
 
+	size = si->size;
 	reverseComplement(si->nucSequence, si->size);
+	}
+
+    gi = giList;
+    for(; gi; gi = gi->next)
+	{
+//	printf("exonStart was %d, size %d, length %d\n",gi->exonStart, size, gi->exonSize);
+	gi->exonStart = size - (gi->exonStart + gi->exonSize);
+//	printf("exonStart now %d\n",gi->exonStart);
 	}
     }
 }
 
-void outGene(char *geneName, char *mafTable, char *frameTable, 
-    char *org, struct slName *speciesNameList, char *outName)
+void outGene(FILE *f, char *geneName, char *mafTable, char *frameTable, 
+    char *org, struct slName *speciesNameList)
 {
 struct mafFrames *frames = getFrames(geneName, frameTable, org);
 struct mafFrames *frame;
 //struct sqlConnection *conn = hAllocConn();
-FILE *f = mustOpen(outName, "w");
 struct geneInfo *giList = NULL;
 
+printf("outing gene %s\n",geneName);
 for(frame = frames; frame; frame = frame->next)
     {
     struct geneInfo *gi;
 
     AllocVar(gi);
     gi->frame = frame;
+    gi->name = frame->name;
     gi->ali = getAliForFrame(mafTable, frame);
     slAddHead(&giList, gi);
     }
@@ -323,7 +479,7 @@ struct speciesInfo *speciesList = getSpeciesInfo(giList, speciesNameList,
     speciesInfoHash);
 
 copyMafs(speciesInfoHash, giList);
-writeOutSpecies(f, speciesList);
+writeOutSpecies(f, speciesList, giList);
 
 //hFreeConn(&conn);
 }
@@ -334,6 +490,7 @@ void mafToProtein(char *dbName, char *mafTable, char *frameTable,
 {
 struct slName *geneNames = NULL;
 struct slName *speciesNames = readList(speciesList);
+FILE *f = mustOpen(outName, "w");
 
 hSetDb(dbName);
 
@@ -349,7 +506,7 @@ else
     geneNames = queryNames(dbName, frameTable, org);
 
 for(; geneNames; geneNames = geneNames->next)
-    outGene(geneNames->name, mafTable, frameTable, org, speciesNames, outName);
+    outGene(f, geneNames->name, mafTable, frameTable, org, speciesNames);
 }
 
 int main(int argc, char *argv[])
@@ -363,6 +520,8 @@ if (argc != 7)
 geneName = optionVal("geneName", geneName);
 geneList = optionVal("geneList", geneList);
 onlyChrom = optionVal("chrom", onlyChrom);
+inExons = optionExists("exons");
+noTrans = optionExists("noTrans");
 
 if ((geneName != NULL) && (geneList != NULL))
     errAbort("cannot specify both geneList and geneName");
