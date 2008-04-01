83763df0fd0ff42fec4108539afb1c44c3b2242c
galt <galt>
2008-04-01 19:23:22 +0000

    removing troubling boolean from migration code

--- src/hg/encode/hgEncodeSubmit/db/migrate/025_create_project_archive_active.rb
+++ src/hg/encode/hgEncodeSubmit/db/migrate/025_create_project_archive_active.rb
@@ -6,7 +6,12 @@ class CreateProjectArchiveActive < ActiveRecord::Migration
         t.column :active, :boolean
     end
 
-    execute "insert into project_archive_active (archive_id, archive_no, active) select a.id, b.archive_no, true from project_archives a, project_archives b where a.project_id = b.project_id and a.archive_no >= b.archive_no order by a.id, b.archive_no"
+    # We don't need this anymore anyway, because the whole table got dropped.
+    # But, just as a note, Postgres and Mysql are not compatible for 
+    # any boolean type columns, so DO NOT USE BOOLEAN columns.
+    # use a char(1) instead or something like that.
+    
+    #execute "insert into project_archive_active (archive_id, archive_no, active) select a.id, b.archive_no, #{true} from project_archives a, project_archives b where a.project_id = b.project_id and a.archive_no >= b.archive_no order by a.id, b.archive_no"
 
   end
 
