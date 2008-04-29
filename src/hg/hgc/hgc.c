2c4eaabc0a08e3fbbbd2804b5085a58cdbe9c06f
angie <angie>
2008-04-29 00:01:23 +0000

    Use constant for PCR Results track name instead of hardcoded name.

--- src/hg/hgc/hgc.c
+++ src/hg/hgc/hgc.c
@@ -213,7 +213,7 @@
 #include "itemConf.h"
 #include "chromInfo.h"
 
-static char const rcsid[] = "$Id: hgc.c,v 1.1413 2008/04/23 17:52:48 angie Exp $";
+static char const rcsid[] = "$Id: hgc.c,v 1.1414 2008/04/29 00:01:23 angie Exp $";
 static char *rootDir = "hgcData"; 
 
 #define LINESIZE 70  /* size of lines in comp seq feature */
@@ -20128,7 +20128,7 @@ else if (sameWord(track, USER_PSL_TRACK_NAME))
     {
     doUserPsl(track, item);
     }
-else if (sameWord(track, "hgPcrResult"))
+else if (sameWord(track, PCR_RESULT_TRACK_NAME))
     {
     doPcrResult(track, item);
     }
