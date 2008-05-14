00f35b42ea1476a18b27b7a3d84c22c69f933b5d
markd <markd>
2008-05-14 07:19:22 +0000

    add -uniq flag to drop dups

--- src/utils/faFilter/faFilter.c
+++ src/utils/faFilter/faFilter.c
@@ -3,8 +3,10 @@
 #include "linefile.h"
 #include "options.h"
 #include "fa.h"
+#include "hash.h"
+#include "dystring.h"
 
-static char const rcsid[] = "$Id: faFilter.c,v 1.4 2007/12/19 00:33:51 lowec Exp $";
+static char const rcsid[] = "$Id: faFilter.c,v 1.5 2008/05/14 07:19:22 markd Exp $";
 
 void usage()
 /* Explain usage and exit. */
@@ -24,6 +26,7 @@ errAbort("faFilter - Filter fa records, selecting ones that match the specified 
          "    -v - invert match, select non-matching records.\n"
          "    -minSize=N - Only pass sequences at least this big.\n"
          "    -maxSize=N - Only pass sequences this size or smaller.\n"
+         "    -uniq - Removes duplicate sequence ids, keeping the first.\n"
          "\n"
          "All specified conditions must pass to pass a sequence.  If no conditions are\n"
          "specified, all records will be passed.\n"
@@ -36,6 +39,7 @@ static struct optionSpec options[] = {
     {"v", OPTION_BOOLEAN},
     {"minSize", OPTION_INT},
     {"maxSize", OPTION_INT},
+    {"uniq", OPTION_BOOLEAN},
     {NULL, 0},
 };
 
@@ -45,30 +49,25 @@ char *namePat = NULL;
 boolean vOption = FALSE;
 int minSize = -1;
 int maxSize = -1;
+struct hash *uniqHash = NULL;
 
-boolean matchName(char *seqHeader, char *pattern)
-/* see if the sequence name matches */
+char *parseSeqName(char *seqHeader)
+/* parse sequence id from a header seqHeader. 
+ * WARNING: static return. */
 {
-/* find end of name */
-char *nameSep = skipToSpaces(seqHeader);
-char sepChr = '\0';
-boolean isMatch = FALSE;
-
-if (nameSep != NULL)
-    {
-    sepChr = *nameSep; /* terminate name */
-    *nameSep = '\0';
-    }
-isMatch = wildMatch(pattern, seqHeader);
-if (nameSep != NULL)
-    *nameSep = sepChr;
-return isMatch;
+static struct dyString *buf = NULL;
+if (buf == NULL)
+    buf = dyStringNew(32);
+dyStringClear(buf);
+dyStringAppend(buf, seqHeader);
+char *sep = skipToSpaces(buf->string);
+if (sep != NULL)
+    *sep = '\0';
+return buf->string;
 }
 
 struct slName *readInPatterns(char *filename)
 {
-if(filename == NULL){return(NULL);}
-
 struct lineFile *lf = lineFileOpen(filename, TRUE);
 char *line;
 struct slName *ret = NULL, *curr = NULL;
@@ -81,31 +80,45 @@ while (lineFileNext(lf, &line, NULL))
 return(ret);
 }
 
+boolean matchesAPattern(char *name, struct slName *patternsList)
+/* does the sequence match one of the patterns */
+{
+struct slName *pat;
+for (pat = patternsList; pat != NULL; pat = pat->next)
+    {
+    if (wildMatch(pat->name, name))
+        return TRUE;
+    }
+return FALSE;
+}
+
 boolean recMatches(DNA *seq, int seqSize, char *seqHeader, struct slName *patternsList)
 /* check if a fasta record matches the sequence constraints */
 {
-boolean matchList = FALSE;
-struct slName *currPattern = NULL;
-
+char *name = parseSeqName(seqHeader);
 if ((minSize >= 0) && (seqSize < minSize))
     return FALSE;
 if ((maxSize >= 0) && (seqSize > maxSize))
     return FALSE;
-if ((namePat != NULL) && !matchName(seqHeader, namePat))
+if ((patternsList != NULL) && !matchesAPattern(name, patternsList))
     return FALSE;
-for(currPattern = patternsList; currPattern != NULL; currPattern = currPattern->next)
+if (uniqHash != NULL) 
     {
-    if (matchName(seqHeader, currPattern->name)) {matchList = TRUE;}
+    if (hashLookup(uniqHash, name) != NULL)
+        return FALSE;  // already seen
+    hashAdd(uniqHash, name, NULL);
     }
-if ((patternsList != NULL) && (!matchList))
-    return FALSE;
 return TRUE;
 }
 
 void faFilter(char *inFile, char *outFile)
 /* faFilter - Filter out fa records that don't match expression. */
 {
-struct slName *patternsList = readInPatterns(optNamePatList);
+struct slName *patternsList = NULL;
+if (optNamePatList != NULL)
+    readInPatterns(optNamePatList);
+if (namePat != NULL)
+    slSafeAddHead(&patternsList, slNameNew(namePat));
 struct lineFile *inLf = lineFileOpen(inFile, TRUE);
 FILE *outFh = mustOpen(outFile, "w");
 DNA *seq;
@@ -132,6 +145,8 @@ namePat = optionVal("name", NULL);
 vOption = optionExists("v");
 minSize = optionInt("minSize", -1);
 maxSize = optionInt("maxSize", -1);
+if (optionExists("uniq"))
+    uniqHash = hashNew(24);
 faFilter(argv[1],argv[2]);
 return 0;
 }
