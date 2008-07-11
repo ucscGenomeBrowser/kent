a940083bfcc58f1f29cc22957e19355f0c94534a
kent <kent>
2008-07-11 14:17:29 +0000

    Removing some extranious stuff that is causing problems from table create statment.

--- src/hg/lib/pdbSP.sql
+++ src/hg/lib/pdbSP.sql
@@ -3,5 +3,5 @@ CREATE TABLE pdbSP (
   sp varchar(40) NOT NULL default '',
   KEY i1 (pdb),
   KEY i2 (sp)
-) TYPE=MyISAM DATA DIRECTORY='./proteins060115/' INDEX DIRECTORY='./proteins060115/';
+); 
 
