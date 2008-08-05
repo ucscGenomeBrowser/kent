e6ebe6da453dd65e5e27bb959015e1c7c5728dea
larrym <larrym>
2008-08-05 18:09:10 +0000

    allow user specified STDERR file

--- src/hg/utils/automation/SafePipe.pm
+++ src/hg/utils/automation/SafePipe.pm
@@ -7,7 +7,7 @@
 # DO NOT EDIT the /cluster/bin/scripts copy of this file --
 # edit ~/kent/src/hg/utils/automation/SafePipe.pm instead.
 
-# $Id: SafePipe.pm,v 1.2 2008/07/26 23:47:15 larrym Exp $
+# $Id: SafePipe.pm,v 1.3 2008/08/05 18:09:10 larrym Exp $
 
 package SafePipe;
 
@@ -17,8 +17,10 @@ use strict;
 sub new
 {
 # $args{CMDS} (required): list of commands
-# $args{STDOUT} (optional): file to use for stdout; if not provided, we copy text of stdout into
-# the STDOUT field. Use STDOUT => '/dev/null' if you want to discard stdout.
+# $args{STDOUT} (optional): file to use for stdout; if not provided, we copy text of stdout into the STDOUT field.
+# $args{STDERR} (optional): file to use for stderr; if not provided, we copy text of stderr into the STDERR field.
+# Files are useful if you want to leave the data on disk for later use.
+# Use STDOUT/STDERR  => '/dev/null' if you want to discard stdout/stderr.
     my ($class, %args) = (@_);
     die "$class: missing CMDS list" unless(defined($args{CMDS}) && ref($args{CMDS}) eq 'ARRAY');
     my $ref = {};
@@ -29,15 +31,16 @@ sub new
 
 sub stderr
 {
-# returns text of STDERR from executed commands
+# returns text of STDERR from executed commands.
+# Use only if you did NOT provide a STDOUT file to the new method.
     my ($obj) = @_;
     return $obj->{STDERR};
 }
 
 sub stdout
 {
-# returns text of STDOUT from executed commands; use only
-# if you did NOT provide a STDOUT file to the new method.
+# returns text of STDOUT from executed commands.
+# Use only if you did NOT provide a STDOUT file to the new method.
     my ($obj) = @_;
     return $obj->{STDOUT};
 }
@@ -62,8 +65,14 @@ sub exec
 # Exececute the pipe; returns sum of statuses, to facilitate quick test by caller
 # for success. Individual statuses are available via statuses method.
     my ($obj) = @_;
-    my $errFile = "/tmp/SafePipe$$.err";
-    unlink($errFile);
+
+    my $stderrCreated = 0;
+    my $stderrFile = $obj->{STDERR};
+    if(!(defined($stderrFile))) {
+        $stderrFile = "/tmp/SafePipe$$.err";
+        $stderrCreated = 1;
+        unlink($stderrFile);
+    }
     my $stdoutCreated = 0;
     my $stdoutFile = $obj->{STDOUT};
     if(!(defined($stdoutFile))) {
@@ -71,8 +80,9 @@ sub exec
         $stdoutCreated = 1;
         unlink($stdoutFile);
     }
-    my $cmd = join(" 2>> $errFile | ", @{$obj->{CMDS}});
-    $cmd .= " > $stdoutFile 2>> $errFile; echo \${PIPESTATUS[@]}";
+
+    my $cmd = join(" 2>> $stderrFile | ", @{$obj->{CMDS}});
+    $cmd .= " > $stdoutFile 2>> $stderrFile; echo \${PIPESTATUS[@]}";
     $obj->{CMD} = $cmd;
     my $output = `$cmd`;
 
@@ -82,16 +92,20 @@ sub exec
     $obj->{STATUSES} = \@list;
     $obj->{SUM} = 0;
     $obj->{SUM} += abs for (@list);
-    open(ERR, $errFile) || die "ERROR: Can't open error file \'$errFile\': $!\n";
-    $obj->{STDERR} = join("", <ERR>);
-    close(ERR);
-    unlink($errFile);
+
+    if($stderrCreated) {
+        open(ERR, $stderrFile) || die "ERROR: Can't open stderr file \'$stderrFile\': $!\n";
+        $obj->{STDERR} = join("", <ERR>);
+        close(ERR);
+        unlink($stderrFile);
+    }
     if($stdoutCreated) {
         open(OUT, $stdoutFile) || die "ERROR: Can't open stdout file \'$stdoutFile\': $!\n";
         $obj->{STDOUT} = join("", <OUT>);
         close(OUT);
         unlink($stdoutFile);
     }
+
     return $obj->{SUM};
 }
 
