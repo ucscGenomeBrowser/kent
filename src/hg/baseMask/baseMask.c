2ccad8920a59108801f3f797b4586759a755ae1d
mikep <mikep>
2008-08-25 23:23:11 +0000

    added ability to specify only one input file, and print out statistics, plus test

--- src/hg/baseMask/baseMask.c
+++ src/hg/baseMask/baseMask.c
@@ -30,6 +30,27 @@ static char *usageMsg =
 errAbort("%s:  %s", msg, usageMsg);
 }
 
+void genomeRangeTreeStats(char *fileName, int *numChroms, int *nodes, int *size)
+{
+struct genomeRangeTreeFile *tf = genomeRangeTreeFileReadHeader(fileName);
+struct hashEl *c;
+int n;
+*size = 0;
+*nodes = 0;
+*numChroms = tf->numChroms;
+for (c = tf->chromList ; c ; c = c->next)
+    {
+    struct rangeStartSize *r;
+    n = hashIntVal(tf->nodes, c->name);
+    *nodes += n;
+    AllocArray(r, n);
+    rangeReadArray(tf->file, r, n, tf->isSwapped);
+    *size += rangeArraySize(r, n);
+    freez(&r);
+    }
+genomeRangeTreeFileFree(&tf);
+}
+
 /* entry */
 int main(int argc, char** argv)
 {
@@ -44,35 +65,50 @@ boolean or = optionExists("or");
 boolean quiet = optionExists("quiet");
 boolean saveMem = optionExists("saveMem");
 boolean orDirectToFile = optionExists("orDirectToFile");
-if (argc < 3 || argc > 4)
-    usage("wrong # args");
-if ((and && or) || (!and && !or))
-    usage("specify one of the options: -and or -or\n");
+--argc;
+++argv;
+if (argc < 1 || argc > 3)
+    usage("wrong # args\n");
+if (argc == 1 && (and || or))
+    usage("specify second file for options: -and or -or\n");
+if (argc >= 2 && ((and && or) || (!and && !or)))
+    usage("specify only one of the options: -and or -or\n");
 
-baseMask1 = argv[1];
-baseMask2 = argv[2];
-obama = (argc == 3 ? NULL : argv[3]);
+baseMask1 = argv[0];
+baseMask2 = (argc > 1 ? argv[1] : NULL);
+obama = (argc > 2 ? argv[2] : NULL);
 
-tf1 = genomeRangeTreeFileReadHeader(baseMask1);
-tf2 = genomeRangeTreeFileReadHeader(baseMask2);
-if (and)
+if (argc == 1)
     {
-    genomeRangeTreeFileIntersectionDetailed(tf1, tf2, obama, &numChroms, &nodes, 
-	(quiet ? NULL : &size), saveMem);
     if (!quiet)
-	fprintf(stderr, "%d bases in %d ranges in %d chroms in intersection\n", size, nodes, numChroms);
+	{
+	genomeRangeTreeStats(baseMask1, &numChroms, &nodes, &size);
+	fprintf(stderr, "%d bases in %d ranges in %d chroms in baseMask\n", size, nodes, numChroms);
+	}
     }
-else if (or)
+else
     {
-    genomeRangeTreeFileUnionDetailed(tf1, tf2, obama, &numChroms, &nodes, 
-	(quiet ? NULL : &size), saveMem, orDirectToFile);
-    if (!quiet)
-	fprintf(stderr, "%d bases in %d ranges in %d chroms in union\n", size, nodes, numChroms);
+    tf1 = genomeRangeTreeFileReadHeader(baseMask1);
+    tf2 = genomeRangeTreeFileReadHeader(baseMask2);
+    if (and)
+	{
+	genomeRangeTreeFileIntersectionDetailed(tf1, tf2, obama, &numChroms, &nodes, 
+	    (quiet ? NULL : &size), saveMem);
+	if (!quiet)
+	    fprintf(stderr, "%d bases in %d ranges in %d chroms in intersection\n", size, nodes, numChroms);
+	}
+    else if (or)
+	{
+	genomeRangeTreeFileUnionDetailed(tf1, tf2, obama, &numChroms, &nodes, 
+	    (quiet ? NULL : &size), saveMem, orDirectToFile);
+	if (!quiet)
+	    fprintf(stderr, "%d bases in %d ranges in %d chroms in union\n", size, nodes, numChroms);
+	}
+    t1 = genomeRangeTreeFileFree(&tf1);
+    genomeRangeTreeFree(&t1);
+    t2 = genomeRangeTreeFileFree(&tf2);
+    genomeRangeTreeFree(&t2);
     }
-t1 = genomeRangeTreeFileFree(&tf1);
-genomeRangeTreeFree(&t1);
-t2 = genomeRangeTreeFileFree(&tf2);
-genomeRangeTreeFree(&t2);
 return 0;
 }
 
