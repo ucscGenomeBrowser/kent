c8ea9c10e7e9a4b97b897a41254f1c5fc54f674d
braney <braney>
2008-08-26 22:47:52 +0000

    add some option bitmasks and change routine to handle them

--- src/hg/inc/mafGene.h
+++ src/hg/inc/mafGene.h
@@ -1,7 +1,11 @@
 
+/* options */
+#define MAFGENE_EXONS		(1 << 0)
+#define MAFGENE_NOTRANS		(1 << 1)
+#define MAFGENE_OUTBLANK	(1 << 2)
+
 /* output a FASTA alignment from the given mafTable
  * for a given genePred
  */
 void mafGeneOutPred(FILE *f, struct genePred *pred, char *dbName, 
-    char *mafTable,  struct slName *speciesNameList, 
-    boolean inExons, boolean noTrans);
+    char *mafTable,  struct slName *speciesNameList, unsigned options);
