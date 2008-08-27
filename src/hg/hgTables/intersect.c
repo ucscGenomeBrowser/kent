f5f0835b1061372883643d409c1961f7bf713ce9
kent <kent>
2008-08-27 20:56:00 +0000

    Adding comments to point Mike to where to start.

--- src/hg/hgTables/intersect.c
+++ src/hg/hgTables/intersect.c
@@ -17,7 +17,7 @@
 #include "customTrack.h"
 #include "wikiTrack.h"
 
-static char const rcsid[] = "$Id: intersect.c,v 1.41 2008/05/30 22:25:55 hiram Exp $";
+static char const rcsid[] = "$Id: intersect.c,v 1.42 2008/08/27 20:56:00 kent Exp $";
 
 /* We keep two copies of variables, so that we can
  * cancel out of the page. */
@@ -462,6 +462,7 @@ boolean invTable = cartCgiUsualBoolean(cart, hgtaInvertTable, FALSE);
 boolean invTable2 = cartCgiUsualBoolean(cart, hgtaInvertTable2, FALSE);
 char *op = cartString(cart, hgtaIntersectOp);
 char *table2 = cartString(cart, hgtaIntersectTable);
+/* --- TODO MIKE - replace bedList2, bits2 with baseMask stuff. */
 /* Load up intersecting bedList2 (to intersect with) */
 struct hTableInfo *hti2 = getHti(database, table2);
 struct lm *lm2 = lmInit(64*1024);
@@ -491,6 +492,7 @@ bedOrBits(bits2, chromSize, bedList2, hti2->hasBlocks, 0);
 /* Produce intersectedBedList. */
 if (isBpWise)
     {
+/* --- TODO MIKE - replace, bits1 with baseMask stuff. */
     /* Base-pair-wise operation: get bitmap  for primary table too */
     Bits *bits1 = bitAlloc(chromSize+8);
     boolean hasBlocks = hti1->hasBlocks;
