53061ddff53c472937e58b024f963f02c6b51034
fanhsu <fanhsu>
2008-09-08 16:00:55 +0000

    Updated to be compatible with Mark's new remotDb change.

--- src/hg/gsid/gsidSubj/gsidSubj.c
+++ src/hg/gsid/gsidSubj/gsidSubj.c
@@ -18,7 +18,7 @@
 #include "gsid.h"
 #include "gsidSubj.h"
 
-static char const rcsid[] = "$Id: gsidSubj.c,v 1.13 2008/02/26 19:21:01 fanhsu Exp $";
+static char const rcsid[] = "$Id: gsidSubj.c,v 1.14 2008/09/08 16:00:55 fanhsu Exp $";
 
 /* ---- Global variables. ---- */
 struct cart *cart;	/* This holds cgi and other variables between clicks. */
@@ -287,8 +287,8 @@ getDbAndGenome(cart, &database, &genome, oldCart);
 /* !!! force database to hiv1 until move to server hiv1 is complete 
    and the default database of hgcentral on it point to hiv1. */
 
-hSetDb(database);
-conn = hAllocConn();
+//hSetDb(database);
+conn = hAllocConn(database);
 
 curSubjId = cgiOptionalString("hgs_subj");
 if (curSubjId == NULL) curSubjId = strdup("");
