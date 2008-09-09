87d8f20076b1c7af93c3bb71cc6c8820f4a184c5
fanhsu <fanhsu>
2008-09-09 18:30:53 +0000

    updated vax003.sql.

--- src/hg/lib/vax003.sql
+++ src/hg/lib/vax003.sql
@@ -1,27 +1,27 @@
-#VAX003 HIV-1 gp120 Sequences
 CREATE TABLE vax003 (
-    bin smallint unsigned not null,	# used for efficient position indexing
-    match int unsigned not null,	# Number of bases that match that aren't repeats
-    misMatch int unsigned not null,	# Number of bases that don't match
-    repMatch int unsigned not null,	# Number of bases that match but are part of repeats
-    nCount int unsigned not null,	# Number of 'N' bases
-    qNumInsert int unsigned not null,	# Number of inserts in query
-    qBaseInsert int not null,	# Number of bases inserted in query
-    tNumInsert int unsigned not null,	# Number of inserts in target
-    tBaseInsert int not null,	# Number of bases inserted in target
-    strand char(2) not null,	# + or - for query strand. For mouse second +/- for genomic strand
-    qName varchar(255) not null,	# Query sequence name
-    qSize int unsigned not null,	# Query sequence size
-    qStart int unsigned not null,	# Alignment start position in query
-    qEnd int unsigned not null,	# Alignment end position in query
-    tName varchar(255) not null,	# Target sequence name
-    tSize int unsigned not null,	# Target sequence size
-    tStart int unsigned not null,	# Alignment start position in target
-    tEnd int unsigned not null,	# Alignment end position in target
-    blockCount int unsigned not null,	# Number of blocks in alignment
-    blockSizes longblob not null,	# Size of each block
-    qStarts longblob not null,	# Start of each block in query.
-    tStarts longblob not null,	# Start of each block in target.
-              #Indices
-    PRIMARY KEY(qName)
-);
+  bin smallint(5) unsigned NOT NULL default '0',
+  matches int(10) unsigned NOT NULL default '0',
+  misMatches int(10) unsigned NOT NULL default '0',
+  repMatches int(10) unsigned NOT NULL default '0',
+  nCount int(10) unsigned NOT NULL default '0',
+  qNumInsert int(10) unsigned NOT NULL default '0',
+  qBaseInsert int(10) unsigned NOT NULL default '0',
+  tNumInsert int(10) unsigned NOT NULL default '0',
+  tBaseInsert int(10) unsigned NOT NULL default '0',
+  strand char(2) NOT NULL default '',
+  qName varchar(255) NOT NULL default '',
+  qSize int(10) unsigned NOT NULL default '0',
+  qStart int(10) unsigned NOT NULL default '0',
+  qEnd int(10) unsigned NOT NULL default '0',
+  tName varchar(255) NOT NULL default '',
+  tSize int(10) unsigned NOT NULL default '0',
+  tStart int(10) unsigned NOT NULL default '0',
+  tEnd int(10) unsigned NOT NULL default '0',
+  blockCount int(10) unsigned NOT NULL default '0',
+  blockSizes longblob NOT NULL,
+  qStarts longblob NOT NULL,
+  tStarts longblob NOT NULL,
+  KEY tName (tName(4),bin),
+  KEY qName (qName(12))
+) TYPE=MyISAM;
+
