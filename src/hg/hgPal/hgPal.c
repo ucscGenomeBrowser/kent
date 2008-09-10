72a2f0d8e2eb9eab764b308b2a03b11d1f32993d
braney <braney>
2008-09-10 18:41:25 +0000

    take out some old stuff, javascript submit on MAF file

--- src/hg/hgPal/hgPal.c
+++ src/hg/hgPal/hgPal.c
@@ -15,7 +15,7 @@
 #include "hCommon.h"
 #include "genePredReader.h"
 
-static char const rcsid[] = "$Id: hgPal.c,v 1.5 2008/09/09 21:22:03 braney Exp $";
+static char const rcsid[] = "$Id: hgPal.c,v 1.6 2008/09/10 18:41:25 braney Exp $";
 
 /* Global Variables */
 struct cart *cart;             /* CGI and other variables */
@@ -89,7 +89,6 @@ return list;
 
 void showMafTracks(struct cart *cart, char *mafTable, char *geneTable, char *gene)
 {
-//char option[MAX_SP_SIZE];
 char query[512];
 struct sqlConnection *conn = hAllocConn(database);
 struct sqlResult *sr;
@@ -101,7 +100,7 @@ int ii;
 
 slAddHead(&speciesList, slNameNew(database));
 
-printf("MAF table: \n");
+printf("<B>MAF table:</B> \n");
 sr = sqlGetResult(conn, query);
 int count = 0;
 while ((row = sqlNextRow(sr)) != NULL)
@@ -129,20 +128,11 @@ for(ii=0; ii < count; ii++)
 if ((mafTable == NULL) || (checked == NULL))
     checked = mafTable = mafTrackExist[0];
 
-cgiMakeDropList("mafTable", mafTrackExist, count2,  checked);
-printf("<BR>");
+char *onChangeDB = "onchange=\" document.mainForm.submit();\"";
+cgiMakeDropListFull("mafTable", mafTrackExist, mafTrackExist, count2,  checked,onChangeDB);
+printf("<BR><BR>");
 
 struct trackDb *tdb = hTrackDbForTrack(database, mafTable);
-#ifdef NOTNOW
-char *speciesOrder = trackDbSetting(tdb, SPECIES_ORDER_VAR);
-char *speciesGroup = trackDbSetting(tdb, SPECIES_GROUP_VAR);
-char *speciesUseFile = trackDbSetting(tdb, SPECIES_USE_FILE);
-char *groups[20];
-int speciesCt = 0, groupCt = 1;
-int group;
-char sGroup[24];
-char *species[MAX_SP_SIZE];
-#endif
 boolean inExons = cartUsualBoolean(cart, "mafGeneExons", TRUE); 
 boolean noTrans = cartUsualBoolean(cart, "mafGeneNoTrans", FALSE); 
 boolean outBlank = cartUsualBoolean(cart, "mafGeneOutBlank", FALSE); 
@@ -156,76 +146,6 @@ cgiMakeCheckBox("mafGeneOutBlank", outBlank);
 printf("Output lines with just dashes<BR>");
 
 
-#ifdef NOTNOW
-/* determine species and groups for pairwise -- create checkboxes */
-if (speciesOrder == NULL && speciesGroup == NULL && speciesUseFile == NULL)
-    {
-//    if (isCustomTrack(tdb->tableName))
-//	return;
-    errAbort("Track %s missing required trackDb setting: speciesOrder, speciesGroups, or speciesUseFile", tdb->tableName);
-    }
-
-if (speciesGroup)
-    groupCt = chopLine(speciesGroup, groups);
-
-if (speciesUseFile)
-    {
-    if ((speciesGroup != NULL) || (speciesOrder != NULL))
-	errAbort("Can't specify speciesUseFile and speciesGroup or speciesOrder");
-    speciesOrder = cartGetOrderFromFile(cart, speciesUseFile);
-    }
-
-for (group = 0; group < groupCt; group++)
-    {
-    if (groupCt != 1 || !speciesOrder)
-        {
-        safef(sGroup, sizeof sGroup, "%s%s", 
-                                SPECIES_GROUP_PREFIX, groups[group]);
-        speciesOrder = trackDbRequiredSetting(tdb, sGroup);
-        }
-    speciesCt = chopLine(speciesOrder, species);
-    for (ii = 0; ii < speciesCt; ii++)
-        {
-//	printf("looking at %s\n", species[ii]);
-        slAddTail(&speciesList, slNameNew(species[ii]));
-        }
-    }
-struct slName *sl;
-
-printf("<B>Species to include:</B><BR>\n");
-puts("\n<TABLE><TR>");
-ii=0;
-struct slName *includeList = NULL;
-struct slName *next;
-
-for(sl = speciesList; sl; sl = next)
-    {
-    next = sl->next;
-    sl->next = NULL;
-    safef(option, sizeof(option), "%s.%s", tdb->tableName, sl->name);
-    int on = cartUsualBoolean(cart, option, TRUE);
-    
-    if (on)
-	slAddHead(&includeList, sl);
-
-    puts("<TD>");
-    cgiMakeCheckBox(option, on);
-    char *label = hOrganism(sl->name);
-    if (label == NULL)
-	    label = sl->name;
-    printf ("%s<BR>", label);
-    puts("</TD>");
-    if (++ii ==  5)
-	{
-	puts("</TR><TR>");
-	ii=0;
-	}
-    }
-puts("</TR></TABLE><BR>");
-
-if(includeList)
-    slReverse(&includeList);
-#endif
 struct slName *includeList = newSlName(database);
 struct wigMafSpecies *wmSpeciesList = wigMafSpeciesTable(cart, tdb, tdb->tableName, database);
 struct wigMafSpecies *wmSpecies = wmSpeciesList;
@@ -241,10 +161,7 @@ for(; wmSpecies; wmSpecies = wmSpecies->next)
 slReverse(&includeList);
 
 
-//for(sl = includeList; sl; sl = sl->next)
-    //printf("include %s\n<BR>", sl->name);
 cgiMakeButton("Submit", "Submit");
-//slReverse(&wmSpeciesList);
 struct genePred *pred = getPreds(gene, geneTable, database);
 
 printf("<pre>");
