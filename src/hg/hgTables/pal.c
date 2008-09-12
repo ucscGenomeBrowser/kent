950fdc437ecc2430d4b103deef26cce0bef23d77
braney <braney>
2008-09-12 22:37:21 +0000

    deal with the filters and interesections like seqOut does (rather than how the wiki routines do)

--- src/hg/hgTables/pal.c
+++ src/hg/hgTables/pal.c
@@ -41,7 +41,6 @@ return FALSE;
 #define STATIC_ARRAY_SIZE   1024
 static char *mafTrackNames[STATIC_ARRAY_SIZE];
 static char *mafTrackExist[STATIC_ARRAY_SIZE];
-//static char *speciesNames[STATIC_ARRAY_SIZE];
 
 #define hgtaCGIGeneMafTable "hgta_mafGeneMafTable" 
 #define hgtaJSGeneMafTable  "mafGeneMafTable" 
@@ -57,144 +56,39 @@ static char *curVars[] = {
 	hgtaCGIGeneNoTrans, hgtaCGIGeneOutBlank
 	};
 
-struct genePredReader *regionGenePredQuery(struct sqlConnection *conn, 
-	char *table,  struct region *region, char *extraWhere)
-/* Construct and execute query for table on region. Returns NULL if 
- * table doesn't exist (e.g. missing split table for region->chrom). */
-{
-struct genePredReader *reader;
-
-/* Check for missing split tables before querying: */
-char *db = sqlGetDatabase(conn);
-struct hTableInfo *hti = hFindTableInfo(db, region->chrom, table);
-if (hti == NULL)
-    return NULL;
-else if (hti->isSplit)
-    {
-    char fullTableName[256];
-    safef(fullTableName, sizeof(fullTableName),
-	  "%s_%s", region->chrom, table);
-    if (!sqlTableExists(conn, fullTableName))
-	return NULL;
-    }
-
-if (region->fullChrom) /* Full chromosome. */
-    {
-    char extra[2048];
-
-    if (extraWhere)
-	safef(extra, sizeof extra, "chrom='%s' AND %s",region->chrom,extraWhere);
-    else
-	safef(extra, sizeof extra, "chrom='%s'",region->chrom);
-    reader = genePredReaderQuery( conn, table, extra);
-    }
-else
-    {
-    reader = genePredReaderRangeQuery( conn,  table, region->chrom, 
-	region->start, region->end,  extraWhere);
-    }
-
-return reader;
-}
-
 void doGenePredPal(struct sqlConnection *conn)
 /* Output genePred protein alignment. */
 {
 cartRemove(cart, hgtaDoPal);
 cartRemove(cart, hgtaDoPalOut);
 cartSaveSession(cart);
-struct genePredReader *reader = NULL;
-if (anySubtrackMerge(database, curTable))
-    errAbort("Can't do protein alignment output when subtrack merge is on. "
-    "Please go back and select another output type (BED or custom track is good), or clear the subtrack merge.");
-if (anyIntersection())
-    errAbort("Can't do protein alignment output when intersection is on. "
-    "Please go back and select another output type (BED or custom track is good), or clear the intersection.");
+
+struct lm *lm = lmInit(64*1024);
+int fieldCount;
+struct bed *bed, *bedList = cookedBedsOnRegions(conn, curTable, getRegions(),
+	lm, &fieldCount);
+
+/* Make hash of all id's passing filters. */
+struct hash *hash = newHash(18);
+for (bed = bedList; bed != NULL; bed = bed->next)
+    hashAdd(hash, bed->name, NULL);
+
+//lmCleanup(&lm);
+
+struct genePredReader *reader = genePredReaderQuery( conn, curTable, NULL);
+int outCount = 0;
+struct genePred *list = NULL, *pred;
+while ((pred  = genePredReaderNext(reader)) != NULL)
+    {
+    if (hashLookup(hash, pred->name))
+	{
+	slAddHead(&list, pred);
+	++outCount;
+	}
+    }
+genePredReaderFree(&reader);
 
 textOpen();
-
-char *mafTable = cartString(cart, hgtaCGIGeneMafTable); 
-boolean inExons = cartUsualBoolean(cart, hgtaCGIGeneExons , TRUE); 
-boolean noTrans = cartUsualBoolean(cart, hgtaCGIGeneNoTrans, FALSE); 
-boolean outBlank = cartUsualBoolean(cart, hgtaCGIGeneOutBlank, FALSE); 
-struct region *regionList = getRegions();
-struct region *region;
-struct hTableInfo *hti = NULL;
-//struct dyString *fieldSpec = newDyString(256);
-struct hash *idHash = NULL;
-int outCount = 0;
-boolean isPositional;
-//int fieldCount;
-char *idField;
-//boolean showItemRgb = FALSE;
-//int itemRgbCol = -1;	/*	-1 means not found	*/
-//boolean printedColumns = FALSE;
-
-hti = getHti(database, curTable);
-idField = getIdField(database, curTrack, curTable, hti);
-//showItemRgb=bedItemRgb(curTrack);	/* should we expect itemRgb */
-					/*	instead of "reserved" */
-
-
-#ifdef NOTNOW
-/* If they didn't pass in a field list assume they want all fields. */
-if (fields != NULL)
-    {
-    dyStringAppend(fieldSpec, fields);
-    fieldCount = countChars(fields, ',') + 1;
-    }
-else
-    {
-    dyStringAppend(fieldSpec, "*");
-    fieldCount = countTableColumns(conn, table);
-    }
-
-dyStringAppend(fieldSpec, fields);
-fieldCount = countChars(fields, ',') + 1;
-#endif
-
-/* If can find id field for table then get
- * uploaded list of identifiers, create identifier hash
- * and add identifier column to end of result set. */
-if (idField != NULL)
-    {
-    idHash = identifierHash(database, curTable);
-    /*
-    if (idHash != NULL)
-	{
-	dyStringAppendC(fieldSpec, ',');
-	dyStringAppend(fieldSpec, idField);
-	}
-	*/
-    }
-isPositional = htiIsPositional(hti);
-
-struct genePred *list = NULL;
-
-/* Loop through each region. */
-for (region = regionList; region != NULL; region = region->next)
-    {
-    struct genePred *pred;
-    char *filter = filterClause(database, curTable, region->chrom);
-
-    reader = regionGenePredQuery(conn, curTable, region, filter);
-    if (reader == NULL)
-	continue;
-
-    //while ((row = sqlNextRow(sr)) != NULL)
-    while ((pred  = genePredReaderNext(reader)) != NULL)
-	{
-	if (idHash == NULL || hashLookup(idHash, pred->name))
-	    {
-	    slAddHead(&list, pred);
-//	    hPrintf("%s\n", pred->name);
-	    ++outCount;
-	    }
-	}
-    freez(&filter);
-    }
-
-genePredReaderFree(&reader);
 
 if (list != NULL)
     {
@@ -202,13 +96,14 @@ if (list != NULL)
 
     struct genePred *pred;
     unsigned options = 0;
+    char *mafTable = cartString(cart, hgtaCGIGeneMafTable); 
+    boolean inExons = cartUsualBoolean(cart, hgtaCGIGeneExons , TRUE); 
+    boolean noTrans = cartUsualBoolean(cart, hgtaCGIGeneNoTrans, FALSE); 
+    boolean outBlank = cartUsualBoolean(cart, hgtaCGIGeneOutBlank, FALSE); 
 
-    if (inExons)
-	options |= MAFGENE_EXONS;
-    if (noTrans)
-	options |= MAFGENE_NOTRANS;
-    if (outBlank)
-	options |= MAFGENE_OUTBLANK;
+    if (inExons)  options |= MAFGENE_EXONS;
+    if (noTrans)  options |= MAFGENE_NOTRANS;
+    if (outBlank) options |= MAFGENE_OUTBLANK;
 
     struct trackDb *maftdb = hTrackDbForTrack(database, mafTable);
     struct wigMafSpecies *wmSpecies;
@@ -225,6 +120,7 @@ if (list != NULL)
 	    }
 	}
     slReverse(&includeList);
+
     for(pred = list; pred ; pred = pred->next)
 	mafGeneOutPred(stdout, pred, database, mafTable, includeList, options);
     }
@@ -232,10 +128,8 @@ if (list != NULL)
 /* Do some error diagnostics for user. */
 if (outCount == 0)
     explainWhyNoResults(NULL);
-hashFree(&idHash);
+hashFree(&hash);
 }
-
-
 
 static char *onChangeEnd(struct dyString **pDy)
 /* Finish up javascript onChange command. */
@@ -301,7 +195,6 @@ if ((mafTable == NULL) || (checked == NULL))
     checked = mafTable = mafTrackExist[0];
 
 printf("<B>MAF table: </B>\n");
-//cgiMakeDropList(hgtaCGIGeneMafTable, mafTrackExist, count2,  checked);
 cgiMakeDropListFull(hgtaCGIGeneMafTable, mafTrackExist, mafTrackExist, count2,  checked, onChangeGenome());
 
 return mafTable;
@@ -318,12 +211,6 @@ cartSaveSession(cart);
 
 char *mafTable = outMafTableDrop(cart, conn);
 
-/*
-boolean inExons = cartUsualBoolean(cart, "mafGeneExons", TRUE); 
-boolean noTrans = cartUsualBoolean(cart, "mafGeneNoTrans", FALSE); 
-boolean outBlank = cartUsualBoolean(cart, "mafGeneOutBlank", FALSE); 
-*/
-
 printf("<BR><BR><B>Formatting options:</B><BR>\n");
 jsMakeTrackingCheckBox(cart, hgtaCGIGeneExons, hgtaJSGeneExons, FALSE);
 printf("Separate into exons<BR>");
@@ -332,17 +219,16 @@ printf("Show nucelotides<BR>");
 jsMakeTrackingCheckBox(cart, hgtaCGIGeneOutBlank, hgtaJSGeneOutBlank, FALSE);
 printf("Output lines with just dashes<BR>");
 
-
 printf("<BR>");
 struct trackDb *maftdb = hTrackDbForTrack(database, mafTable);
 wigMafSpeciesTable(cart, maftdb, mafTable, database);
 
-//getSpecies(mafTable, TRUE);
 cgiMakeButton(hgtaDoPalOut, "submit");
 hPrintf(" ");
 cgiMakeButton(hgtaDoMainPage, "cancel");
 hPrintf("<input type=\"hidden\" name=\"%s\" value=\"\">\n", hgtaDoPal);
 hPrintf("</FORM>\n");
+
 /* Hidden form - for benefit of javascript. */
     {
     static char *saveVars[32];
