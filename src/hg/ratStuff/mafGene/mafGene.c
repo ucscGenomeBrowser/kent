94cf4dbc68a9b7ba0c8d48bbd2d75069c8520c56
braney <braney>
2008-10-01 22:14:01 +0000

    add method to get genes for a bed file (useful for canonical sets)

--- src/hg/ratStuff/mafGene/mafGene.c
+++ src/hg/ratStuff/mafGene/mafGene.c
@@ -26,6 +26,7 @@ errAbort(
   "options:\n"
   "   -geneName=foobar   name of gene as it appears in genePred\n"
   "   -geneList=foolst   name of file with list of genes\n"
+  "   -geneBeds=foo.bed  name of bed file with genes and positions\n"
   "   -chrom=chr1        name of chromosome from which to grab genes\n"
   "   -exons             output exons\n"
   "   -noTrans           don't translate output into amino acids\n"
@@ -37,6 +38,7 @@ errAbort(
 static struct optionSpec options[] = {
    {"geneName", OPTION_STRING},
    {"geneList", OPTION_STRING},
+   {"geneBeds", OPTION_STRING},
    {"chrom", OPTION_STRING},
    {"exons", OPTION_BOOLEAN},
    {"noTrans", OPTION_BOOLEAN},
@@ -47,6 +49,7 @@ static struct optionSpec options[] = {
 
 char *geneName = NULL;
 char *geneList = NULL;
+char *geneBeds = NULL;
 char *onlyChrom = NULL;
 boolean inExons = FALSE;
 boolean noTrans = TRUE;
@@ -55,7 +58,7 @@ boolean newTableType;
 boolean noDash = FALSE;
 
 /* load one or more genePreds from the database */
-struct genePred *getPreds(char *geneName, char *geneTable, char *db)
+struct genePred *getPredsForName(char *name, char *geneTable, char *db)
 {
 struct sqlConnection *conn = hAllocConn(db);
 struct genePred *list = NULL;
@@ -72,9 +75,9 @@ if (!found)
 
 char extra[2048];
 if (onlyChrom != NULL)
-    safef(extra, sizeof extra, "name='%s' and chrom='%s'", geneName, onlyChrom);
+    safef(extra, sizeof extra, "name='%s' and chrom='%s'", name, onlyChrom);
 else
-    safef(extra, sizeof extra, "name='%s'", geneName);
+    safef(extra, sizeof extra, "name='%s'", name);
 
 reader = genePredReaderQuery( conn, splitTable, extra);
 
@@ -85,7 +88,7 @@ while ((gene  = genePredReaderNext(reader)) != NULL)
     }
 
 if (list == NULL)
-    errAbort("no genePred for gene %s in %s\n",geneName, geneTable);
+    errAbort("no genePred for gene %s in %s\n",name, geneTable);
 
 slReverse(&list);
 
@@ -98,10 +101,9 @@ return list;
 /* output the sequence for one gene for every species to 
  * the file stream 
  */
-void outGene(FILE *f, char *geneName, char *dbName, char *mafTable, 
-    char *geneTable, struct slName *speciesNameList)
+void outGenePred(FILE *f, struct genePred *pred, char *dbName, 
+    char *mafTable, char *geneTable, struct slName *speciesNameList)
 {
-struct genePred *pred = getPreds(geneName, geneTable, dbName);
 unsigned options = 0;
 
 if (inExons)
@@ -111,8 +113,7 @@ if (noTrans)
 if (!noDash)
     options |= MAFGENE_OUTBLANK;
 
-for(; pred; pred = pred->next)
-    mafGeneOutPred(f, pred, dbName, mafTable, speciesNameList, options);
+mafGeneOutPred(f, pred, dbName, mafTable, speciesNameList, options);
 }
 
 /* read a list of single words from a file */
@@ -137,35 +138,61 @@ return list;
 }
 
 /* query the list of gene names from the frames table */
-static struct slName *queryNames(char *dbName, char *geneTable)
+static struct genePred *queryPreds(char *dbName, char *geneTable)
 {
 struct sqlConnection *conn = hAllocConn(dbName);
-struct slName *list = NULL;
-char query[1024];
-struct sqlResult *sr = NULL;
-char **row;
+struct genePred *list = NULL;
+char buf[2048];
+char *extra = NULL;
+struct genePredReader *reader;
 
 if (onlyChrom != NULL)
-    safef(query, sizeof query, 
-	"select distinct(name) from %s where chrom='%s' and cdsStart != cdsEnd\n",
-	geneTable, onlyChrom);
-else
-    safef(query, sizeof query, 
-	"select distinct(name) from %s where cdsStart != cdsEnd\n", geneTable);
-
-sr = sqlGetResult(conn, query);
-
-while ((row = sqlNextRow(sr)) != NULL)
     {
-    int len = strlen(row[0]);
-    struct slName *sn = needMem(sizeof(*sn)+len);
-    strcpy(sn->name, row[0]);
-    slAddHead(&list, sn);
+    safef(buf, sizeof buf, "chrom='%s'", onlyChrom);
+    extra = buf;
     }
-sr = sqlGetResult(conn, query);
 
-sqlFreeResult(&sr);
+reader = genePredReaderQuery( conn, geneTable, extra);
+
+list = genePredReaderAll(reader);
+
 hFreeConn(&conn);
+
+return list;
+}
+
+struct genePred *getPredsFromBeds(char *file, char *table, char *db)
+{
+struct sqlConnection *conn = hAllocConn(db);
+struct lineFile *lf = lineFileOpen(file, TRUE);
+char *words[5000];
+int wordsRead;
+struct genePred *list = NULL;
+
+while( (wordsRead = lineFileChopNext(lf, words, sizeof(words)/sizeof(char *)) ))
+    {
+    if (wordsRead != 4)
+	errAbort("file '%s' must be bed4. Line %d has %d fields", 
+	    file, lf->lineIx, wordsRead);
+
+    char where[10 * 1024];
+    safef(where, sizeof where, 
+	"name = '%s' and chrom='%s' and txStart = %d and txEnd = %d",
+	words[3], words[0], sqlUnsigned(words[1]), sqlUnsigned(words[2]));
+
+    //printf("table %s where %s\n",table,where);
+    struct genePredReader *reader = genePredReaderQuery( conn, table, where);
+    struct genePred *pred;
+    while ((pred = genePredReaderNext(reader)) != NULL)
+	slAddHead(&list, pred);
+
+    genePredReaderFree(&reader);
+    }
+
+hFreeConn(&conn);
+
+if (list != NULL)
+    slReverse(&list);
 
 return list;
 }
@@ -174,26 +201,32 @@ void mafGene(char *dbName, char *mafTable, char *geneTable,
    char *speciesList, char *outName)
 /* mafGene - output protein alignments using maf and genePred. */
 {
-struct slName *geneNames = NULL;
 struct slName *speciesNames = readList(speciesList);
 FILE *f = mustOpen(outName, "w");
+struct genePred *list = NULL;
 
 if (geneList != NULL)
-    geneNames = readList(geneList);
-else if (geneName != NULL)
     {
-    int len = strlen(geneName);
-    geneNames = needMem(sizeof(*geneNames)+len);
-    strcpy(geneNames->name, geneName);
+    struct slName *geneNames = readList(geneList);
+    for(; geneNames; geneNames = geneNames->next)
+	{
+	struct genePred *pred = 
+	    getPredsForName(geneNames->name, geneTable, dbName);
+	if (pred != NULL)
+	    slCat(&list, pred);
+	}
     }
+else if (geneName != NULL)
+    list = getPredsForName(geneName, geneTable, dbName);
+else if (geneBeds != NULL)
+    list = getPredsFromBeds(geneBeds, geneTable, dbName);
 else
-    geneNames = queryNames(dbName, geneTable);
+    list = queryPreds(dbName, geneTable);
 
-for(; geneNames; geneNames = geneNames->next)
+for(; list; list = list->next)
     {
-    verbose(2, "outting  gene %s \n",geneNames->name);
-    outGene(f, geneNames->name, dbName, mafTable, 
-	geneTable,  speciesNames);
+    verbose(2, "outting  gene %s \n",list->name);
+    outGenePred(f, list, dbName, mafTable, geneTable,  speciesNames);
     if (delay)
 	{
 	verbose(2, "delaying %d seconds\n",delay);
@@ -212,14 +245,20 @@ if (argc != 6)
 
 geneName = optionVal("geneName", geneName);
 geneList = optionVal("geneList", geneList);
+geneBeds = optionVal("geneBeds", geneBeds);
 onlyChrom = optionVal("chrom", onlyChrom);
 inExons = optionExists("exons");
 noDash = optionExists("noDash");
 noTrans = optionExists("noTrans");
 delay = optionInt("delay", delay);
 
-if ((geneName != NULL) && (geneList != NULL))
-    errAbort("cannot specify both geneList and geneName");
+if (((geneName != NULL) && ((geneList != NULL) || geneBeds != NULL)) ||
+    ((geneList != NULL) && ((geneName != NULL) || geneBeds != NULL)) ||
+    ((geneBeds != NULL) && ((geneList != NULL) || geneName != NULL)))
+    errAbort("must specify only one of geneList, geneName, or geneBeds");
+
+if ((geneBeds != NULL) && (onlyChrom != NULL))
+    errAbort("cannot specify beds and chrom");
 
 mafGene(argv[1],argv[2],argv[3],argv[4],argv[5]);
 return 0;
