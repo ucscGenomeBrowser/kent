7c359ee1edffab5e0766191b1c3b6c7213889f7d
braney <braney>
2008-10-06 21:12:21 +0000

    add option to print out alignments in table format

--- src/hg/inc/mafGene.h
+++ src/hg/inc/mafGene.h
@@ -3,9 +3,11 @@
 #define MAFGENE_EXONS		(1 << 0)
 #define MAFGENE_NOTRANS		(1 << 1)
 #define MAFGENE_OUTBLANK	(1 << 2)
+#define MAFGENE_OUTTABLE	(1 << 3)
 
 /* output a FASTA alignment from the given mafTable
  * for a given genePred
  */
 void mafGeneOutPred(FILE *f, struct genePred *pred, char *dbName, 
-    char *mafTable,  struct slName *speciesNameList, unsigned options);
+    char *mafTable,  struct slName *speciesNameList, unsigned options,
+    int numCols);
