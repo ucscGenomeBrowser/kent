e54fd448c09cbab2ba78782f669536b7887094e7
kent <kent>
2008-10-26 05:50:55 +0000

    Starting to work with 2 mismatches.  Not as fast as splat though.

--- src/shortReads/sufaFind/sufaFind.c
+++ src/shortReads/sufaFind/sufaFind.c
@@ -8,7 +8,7 @@
 #include "dnaLoad.h"
 #include "sufa.h"
 
-static char const rcsid[] = "$Id: sufaFind.c,v 1.1 2008/10/26 03:27:10 kent Exp $";
+static char const rcsid[] = "$Id: sufaFind.c,v 1.2 2008/10/26 05:50:55 kent Exp $";
 
 boolean mmap;
 
@@ -29,18 +29,26 @@ static struct optionSpec options[] = {
    {NULL, 0},
 };
 
-int sufaUpperBound(char *allDna, bits32 *array, int arraySize, char *queryDna, int querySize)
+int upperBoundCount = 0;
+long binaryBranches = 0;
+
+int sufaUpperBound(char *allDna, bits32 *array, int arraySize, char *queryDna, int querySize,
+	int prefixSize)
 /* Find something that matches query, otherwise the place it should be inserted in array. */
 {
+++upperBoundCount;
 int startIx=0, endIx=arraySize-1, midIx;
 int offset;
+queryDna += prefixSize;
+querySize -= prefixSize;
 for (;;)
     {
+    ++binaryBranches;
     if (startIx == endIx)
 	return startIx;
     midIx = ((startIx + endIx)>>1);
     offset = array[midIx];
-    if (memcmp(allDna+offset, queryDna, querySize) < 0)
+    if (memcmp(allDna+offset+prefixSize, queryDna, querySize) < 0)
         startIx = midIx+1;
     else
         endIx = midIx;
@@ -50,7 +58,7 @@ for (;;)
 int sufaBinarySearch(char *allDna, bits32 *array, int arraySize, char *queryDna, int querySize)
 /* Find first index in array that matches query, or -1 if none such. */
 {
-int ix = sufaUpperBound(allDna, array, arraySize, queryDna, querySize);
+int ix = sufaUpperBound(allDna, array, arraySize, queryDna, querySize, 0);
 int offset = array[ix];
 if (memcmp(allDna+offset, queryDna, querySize) == 0)
     return ix;
@@ -74,11 +82,82 @@ return i;
 }
 
 
-void sufaFindOneOff(char *allDna, bits32 *array, int arraySize, char *queryDna, int querySize,
-	struct slInt **pHitList)
-/* Find all array position that match off by one. */
+char *bases4 = "ACGT";
+
+void dumpDnaAtIx(char *allDna, bits32 *array, int ix, int size, FILE *f)
+/* Write out ix, and first size letters of dna at that ix. */
 {
+fprintf(f, "%d ", ix);
+mustWrite(f, allDna + array[ix], size);
+fprintf(f, "\n");
 }
+
+void sufaFindOneOff(char *allDna, bits32 *array, int arraySize,
+	int sliceOffset, int sliceSize, 
+	char *prefix, int prefixSize,
+	char *query, int querySize, int subCount, int maxSubs, 
+	struct slInt **pHitList)
+/* Find all array position that match off by one.  
+ *    This is a recursive function*/
+{
+/* Start recursion with end conditions.  First is that we've matched the whole thing. */
+if (prefixSize == querySize)
+    {
+    struct slInt *hit = slIntNew(sliceOffset);
+    slAddHead(pHitList, hit);
+    return;
+    }
+/* If we have used up all of our substitutions, then look for an exact match to
+ * what is left and add it if it exists, then end. */
+if (subCount == maxSubs)
+    {
+    int ix = sufaUpperBound(allDna, array+sliceOffset, sliceSize, 
+    	query, querySize, prefixSize) + sliceOffset;
+    int offset = array[ix];
+       {
+       char *t = allDna + offset;
+       char *q = query;
+       }
+    if (memcmp(allDna+offset+prefixSize, query+prefixSize, querySize-prefixSize) == 0)
+	{
+	struct slInt *hit = slIntNew(ix);
+	slAddHead(pHitList, hit);
+	}
+    return;
+    }
+
+/* If not at end, then break up suffix array into quadrants, each starting with the
+ * prefix with four varients for each of the bases. */
+int quadrantIx;
+int quadrants[5];
+for (quadrantIx=0; quadrantIx<4; ++quadrantIx)
+    {
+    prefix[prefixSize] = bases4[quadrantIx];
+    quadrants[quadrantIx] = sufaUpperBound(allDna, array+sliceOffset, sliceSize, 
+    	prefix, prefixSize+1, prefixSize) + sliceOffset;
+    }
+char queryBase = query[prefixSize]; 
+quadrants[4] = sliceOffset + sliceSize;
+int quadrantEnd = quadrants[0];
+/* Further explore within each of the non-empty quadrants */
+for (quadrantIx=0; quadrantIx < 4; ++quadrantIx)
+    {
+    int quadrantStart = quadrantEnd;
+    quadrantEnd = quadrants[quadrantIx+1];
+    int quadrantSize = quadrantEnd - quadrantStart;
+    if (quadrantSize != 0)
+        {
+	char quadBase = bases4[quadrantIx];
+	prefix[prefixSize] = quadBase;
+	int extraSub = (queryBase == quadBase ? 0 : 1);
+	sufaFindOneOff(allDna, array, arraySize, quadrantStart, quadrantSize, 
+		prefix, prefixSize+1, query, querySize, subCount+extraSub, maxSubs,
+		pHitList);
+	prefix[prefixSize+1] = 0;
+	}
+    }
+}
+
 
 
 void sufaFind(char *sufaFile, char *queryFile, char *outputFile)
@@ -86,27 +165,38 @@ void sufaFind(char *sufaFile, char *queryFile, char *outputFile)
 {
 struct sufa *sufa = sufaRead(sufaFile, mmap);
 struct dnaLoad *qLoad = dnaLoadOpen(queryFile);
+int arraySize = sufa->header->basesIndexed;
 FILE *f = mustOpen(outputFile, "w");
 struct dnaSeq *qSeq;
+int qSeqCount = 0;
 while ((qSeq = dnaLoadNext(qLoad)) != NULL)
     {
+    struct slInt *hit, *hitList = NULL;
     verbose(2, "Processing %s\n", qSeq->name);
     toUpperN(qSeq->dna, qSeq->size);
-    int exactIx = sufaBinarySearch(sufa->allDna, sufa->array, sufa->header->basesIndexed, 
-        qSeq->dna, qSeq->size);
-    if (exactIx < 0)
+    char *prefix = needMem(qSeq->size+1);
+    sufaFindOneOff(sufa->allDna, sufa->array, arraySize, 0, arraySize, prefix, 0,
+        qSeq->dna, qSeq->size, 0, 2, &hitList);
+    if (hitList == NULL)
         fprintf(f, "%s miss\n", qSeq->name);
     else
 	{
-	int count = sufaCountIdenticalPrefix(sufa->allDna, qSeq->size, 
-		sufa->array + exactIx, sufa->header->basesIndexed - exactIx);
-	int i;
-	for (i=0; i<count; ++i)
-	    fprintf(f, "%s hits offset %d\n", qSeq->name, sufa->array[exactIx+i]);
+	for (hit = hitList; hit != NULL; hit = hit->next)
+	    {
+	    int hitIx = hit->val;
+	    int count = sufaCountIdenticalPrefix(sufa->allDna, qSeq->size, 
+		    sufa->array + hitIx, arraySize - hitIx);
+	    int i;
+	    for (i=0; i<count; ++i)
+		fprintf(f, "%s hits offset %d\n", qSeq->name, sufa->array[hitIx+i]);
+	    }
 	}
+    ++qSeqCount;
     dnaSeqFree(&qSeq);
     }
 carefulClose(&f);
+verbose(1, "%d queries, %d binary searches, %ld binary branches\n", qSeqCount, upperBoundCount,
+	binaryBranches);
 }
 
 int main(int argc, char *argv[])
