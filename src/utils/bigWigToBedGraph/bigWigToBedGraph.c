1af15b535e6fea2fec4352a4748e3e23b3b0c149
kent <kent>
2009-01-28 23:15:27 +0000

    Adding start/end parameters.

--- src/utils/bigWigToBedGraph/bigWigToBedGraph.c
+++ src/utils/bigWigToBedGraph/bigWigToBedGraph.c
@@ -6,9 +6,11 @@
 #include "localmem.h"
 #include "bigWig.h"
 
-static char const rcsid[] = "$Id: bigWigToBedGraph.c,v 1.1 2009/01/28 14:42:08 kent Exp $";
+static char const rcsid[] = "$Id: bigWigToBedGraph.c,v 1.2 2009/01/28 23:15:27 kent Exp $";
 
 char *clChrom = NULL;
+int clStart = -1;
+int clEnd = -1;
 
 void usage()
 /* Explain usage and exit. */
@@ -19,11 +21,15 @@ errAbort(
   "   bigWigToBedGraph in.bigWig out.bedGraph\n"
   "options:\n"
   "   -chrom=chr1 - if set restrict output to given chromosome\n"
+  "   -start=N - if set, restrict output to only that over start\n"
+  "   -end=N - if set, restict output to only that under end\n"
   );
 }
 
 static struct optionSpec options[] = {
    {"chrom", OPTION_STRING},
+   {"start", OPTION_INT},
+   {"end", OPTION_INT},
    {NULL, 0},
 };
 
@@ -39,8 +45,13 @@ for (chrom = chromList; chrom != NULL; chrom = chrom->next)
         continue;
     char *chromName = chrom->name;
     struct lm *lm = lmInit(0);
+    int start = 0, end = chrom->size;
+    if (clStart > 0)
+        start = clStart;
+    if (clEnd > 0)
+        end = clEnd;
     struct bigWigInterval *interval, *intervalList = bigWigIntervalQuery(bwf, chromName, 
-    	0, chrom->size, lm);
+    	start, end, lm);
     for (interval = intervalList; interval != NULL; interval = interval->next)
         fprintf(f, "%s\t%u\t%u\t%g\n", chromName, interval->start, interval->end, interval->val);
     lmCleanup(&lm);
@@ -54,7 +65,9 @@ int main(int argc, char *argv[])
 /* Process command line. */
 {
 optionInit(&argc, argv, options);
-clChrom = optionVal("chrom", NULL);
+clChrom = optionVal("chrom", clChrom);
+clStart = optionInt("start", clStart);
+clEnd = optionInt("end", clEnd);
 if (argc != 3)
     usage();
 bigWigToBedGraph(argv[1], argv[2]);
