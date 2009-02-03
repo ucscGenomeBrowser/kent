43c247894de17c40a985e9266343cc32466b28cc
markd <markd>
2009-02-03 23:42:57 +0000

    fixed really bogus html

--- src/hg/hgGateway/hgGateway.c
+++ src/hg/hgGateway/hgGateway.c
@@ -15,7 +15,7 @@
 #include "customTrack.h"
 #include "hgConfig.h"
 
-static char const rcsid[] = "$Id: hgGateway.c,v 1.109 2008/09/03 19:18:49 markd Exp $";
+static char const rcsid[] = "$Id: hgGateway.c,v 1.110 2009/02/03 23:42:57 markd Exp $";
 
 boolean isPrivateHost;		/* True if we're on genome-test. */
 struct cart *cart = NULL;
@@ -50,24 +50,20 @@ if (sameString(position, "genome") || sameString(position, "hgBatch"))
     position = defaultPosition;
 
 puts(
-"<FORM ACTION=\"../cgi-bin/hgTracks\" NAME=\"mainForm\" METHOD=\"GET\">\n"
 "<CENTER>"
 "<TABLE BGCOLOR=\"FFFEF3\" BORDERCOLOR=\"cccc99\" BORDER=0 CELLPADDING=1>\n"
-"<TR><TD><FONT SIZE=\"2\">\n"
-"<CENTER>\n"
+"<TR><TD>\n"
+"<CENTER><FONT SIZE=\"2\">\n"
 "The UCSC Genome Browser was created by the \n"
 "<A HREF=\"../staff.html\">Genome Bioinformatics Group of UC Santa Cruz</A>.\n"
 "<BR>"
 "Software Copyright (c) The Regents of the University of California.\n"
 "All rights reserved.\n"
-"</CENTER>\n"
-"</FONT></TD></TR></TABLE></CENTER>\n"
+"</FONT></CENTER>\n"
+"</TD></TR></TABLE></CENTER>\n"
 );
 
-cartSaveSession(cart);	/* Put up hgsid= as hidden variable. */
-
 puts(
-"<input TYPE=\"IMAGE\" BORDER=\"0\" NAME=\"hgt.dummyEnterButton\" src=\"../images/DOT.gif\" WIDTH=1 HEIGHT=1 ALT=dot>\n"
 "<center>\n"
 "<table bgcolor=\"cccc99\" border=\"0\" CELLPADDING=1 CELLSPACING=0>\n"
 "<tr><td>\n"
@@ -75,8 +71,13 @@ puts(
 "<tr><td>\n"
 "<table bgcolor=\"fffef3\" border=0>\n"
 "<tr>\n"
-"<td>\n"
-"<table><tr>");
+"<td>\n");
+
+puts(
+"<FORM ACTION=\"../cgi-bin/hgTracks\" NAME=\"mainForm\" METHOD=\"GET\">\n"
+"<input TYPE=\"IMAGE\" BORDER=\"0\" NAME=\"hgt.dummyEnterButton\" src=\"../images/DOT.gif\" WIDTH=1 HEIGHT=1 ALT=dot>\n");
+cartSaveSession(cart);	/* Put up hgsid= as hidden variable. */
+puts("<table><tr>");
 if (gotClade)
     puts("<td align=center valign=baseline>clade</td>");
 puts(
@@ -103,14 +104,7 @@ else
 puts("</td>\n");
 
 puts("<td align=center>\n");
-/*   HACK ALERT - Zoo needs to have different pulldown behavior - Hiram */
-if ( startsWith( "Zoo", organism ) ) {
-puts("<select NAME=\"db\" onchange=\"document.orgForm.db.value = document.mainForm.db.options[document.mainForm.db.selectedIndex].value; document.orgForm.submit();\">"
-    "\t<option SELECTED VALUE=\"zooHuman3\">June 2002</option>"
-    "\t</select>");
-} else {
-    printAssemblyListHtml(db, onChangeDB);
-}
+printAssemblyListHtml(db, onChangeDB);
 puts("</td>\n");
 
 puts("<td align=center>\n");
@@ -128,14 +122,16 @@ position = NULL;
 
 puts("<td align=center>\n");
 cgiMakeIntVar("pix", cartUsualInt(cart, "pix", hgDefaultPixWidth), 4);
-printf("</td>\n");
-printf("<td align=center>");
+puts("</td>\n");
+puts("<td align=center>");
 cgiMakeButton("Submit", "submit");
-printf("</td>\n");
+puts(
+"</td>\n"
+"</tr></table>\n"
+"</FORM></td></tr>\n");
 
 puts(
-"</tr></table>\n"
-"</td></tr><tr><td><center>\n"
+"<tr><td><center>\n"
 "<a HREF=\"../cgi-bin/cartReset\">Click here to reset</a> the browser user interface settings to their defaults.");
 
 #define SURVEY 1
@@ -151,11 +147,11 @@ puts(
 );
 
 puts("<TABLE BORDER=\"0\">");
-puts("<TR><TD VALIGN=\"TOP\">");
-cartSaveSession(cart);	/* Put up hgsid= as hidden variable. */
-printf("</FORM>");
+puts("<TR>");
 
-/* disable hgCustom button on GSID server, until necessary additional work is authorized. */
+// custom track button. disable hgCustom button on GSID server, until
+// necessary additional work is authorized.
+puts("<TD VALIGN=\"TOP\">");
 if (!hIsGsidServer())
     {
     printf(
@@ -165,15 +161,25 @@ if (!hIsGsidServer())
     cartSaveSession(cart);	/* Put up hgsid= as hidden variable. */
     puts("</FORM>");
     }
-puts("</TD><TD VALIGN=\"TOP\">");
+puts("</TD>");
+
+// configure button
+puts("<TD VALIGN=\"TOP\">");
 puts("<FORM ACTION=\"../cgi-bin/hgTracks\" NAME=\"buttonForm\" METHOD=\"GET\">\n");
 cartSaveSession(cart);	/* Put up hgsid= as hidden variable. */
 cgiMakeButton("hgTracksConfigPage", "configure tracks and display");
-puts("</TD><TD VALIGN=\"TOP\">");
-cgiMakeOnClickButton("document.mainForm.position.value=''","clear position");
+puts("</FORM></TD>");
 
-puts("</TD></TR>");
+// clear possition button
+puts("<TD VALIGN=\"TOP\">");
+puts("<FORM ACTION=\"../cgi-bin/hgTracks\" NAME=\"buttonForm\" METHOD=\"GET\">\n");
+cartSaveSession(cart);	/* Put up hgsid= as hidden variable. */
+cgiMakeOnClickButton("document.mainForm.position.value=''","clear position");
+puts("</FORM></TD>");
+
+puts("</TR>");
 puts("</TABLE>");
+
 puts("</center>\n"
 "</td></tr></table>\n"
 "</td></tr></table>\n"
@@ -195,9 +201,6 @@ if (hIsGsidServer())
     }
 
 hgPositionsHelpHtml(organism, db);
-
-puts("</FORM>\n"
-);
 
 puts("<FORM ACTION=\"../cgi-bin/hgGateway\" METHOD=\"GET\" NAME=\"orgForm\">");
 cartSaveSession(cart);	/* Put up hgsid= as hidden variable. */
