b976625172e3580b40d70dc78aeb56089363ce16
kent <kent>
2009-02-07 17:23:22 +0000

    Moving udc stuff to library and adding slow: protocal for testing race conditions and udcCleanup.

---
+++ src/inc/udc.h
@@ -0,0 +1,47 @@
+/* udc - url data cache - a caching system that keeps blocks of data fetched from URLs in
+ * sparse local files for quick use the next time the data is needed. 
+ *
+ * This cache is enormously simplified by there being no local _write_ to the cache,
+ * just reads.  
+ *
+ * The overall strategy of the implementation is to have a root cache directory
+ * with a subdir for each file being cached.  The directory for a single cached file
+ * contains two files - "bitmap" and "sparseData" that contains information on which
+ * parts of the URL are cached and the actual cached data respectively. The subdirectory name
+ * associated with the file is constructed from the URL in a straightforward manner.
+ *     http://genome.ucsc.edu/cgi-bin/hgGateway
+ * gets mapped to:
+ *     rootCacheDir/http/genome.ucsc.edu/cgi-bin/hgGateway/
+ * The URL protocol is the first directory under the root, and the remainder of the
+ * URL, with some necessary escaping, is used to define the rest of the cache directory
+ * structure, with each '/' after the protocol line translating into another directory
+ * level.
+ *    
+ * The bitmap file contains time stamp and size data as well as an array with one bit
+ * for each block of the file that has been fetched.  Currently the block size is 8K. */
+
+#ifndef UDC_H
+#define UDC_H
+
+struct udcFile;
+/* Handle to a cached file.  Inside of structure mysterious unless you are udc.c. */
+
+struct udcFile *udcFileOpen(char *url, char *cacheDir);
+/* Open up a cached file. */
+
+void udcFileClose(struct udcFile **pFile);
+/* Close down cached file. */
+
+int udcRead(struct udcFile *file, void *buf, int size);
+/* Read a block from file.  Return amount actually read. */
+
+void udcSeek(struct udcFile *file, bits64 offset);
+/* Seek to a particular (absolute) position in file. */
+
+bits64 udcTell(struct udcFile *file);
+/* Return current file position. */
+
+bits64 udcCleanup(char *cacheDir, double maxDays);
+/* Remove cached files older than maxDays old. */
+
+#endif /* UDC_H */
