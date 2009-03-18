7e11550cf87113f71b5b6e9e9a33d584f0a7d4f3
hiram <hiram>
2009-03-18 21:13:28 +0000

    More constants brought in here from the code

--- src/hg/inc/chainCart.h
+++ src/hg/inc/chainCart.h
@@ -7,15 +7,14 @@
 #define MAX_OPT_STRLEN	128
 
 #define OPT_CHROM_COLORS "chainColor"
+#define OPT_CHROM_FILTER "chromFilter"
+#define CHAIN_SCORE_MAXIMUM	2000000000
 /*	TrackUi menu items, completing the sentence:
  *	Color chains by:
  */
 #define CHROM_COLORS "Chromosome"
 #define SCORE_COLORS "Normalized Score"
 #define NO_COLORS "Black"
-
-#define TDB_NORMALIZE_SCORE	"normalizedScore"
-/* trackDb option string value for SCORE_COLORS */
 
 extern enum chainColorEnum chainFetchColorOption(struct cart *cart,
     struct trackDb *tdb, boolean compositeLevel);
