b07162eda4e070c85fdb1a8030e0c09dd2548429
angie <angie>
2009-04-03 19:08:34 +0000

    curTrack is NULL when group is All Tables -- in that case, call hAllocConn instead of hAllocConnTrack in showSchemaDb.

--- src/hg/hgTables/schema.c
+++ src/hg/hgTables/schema.c
@@ -21,7 +21,7 @@
 #include "hgTables.h"
 #include "wikiTrack.h"
 
-static char const rcsid[] = "$Id: schema.c,v 1.57 2009/04/01 23:59:44 angie Exp $";
+static char const rcsid[] = "$Id: schema.c,v 1.58 2009/04/03 19:08:34 angie Exp $";
 
 static char *nbForNothing(char *val)
 /* substitute &nbsp; for empty strings to keep table formating sane */
@@ -335,7 +335,12 @@ if (tdb != NULL && isNotEmpty(tdb->html))
 static void showSchemaDb(char *db, struct trackDb *tdb, char *table)
 /* Show schema to open html page. */
 {
-struct sqlConnection *conn = hAllocConnTrack(db, tdb ? tdb : curTrack);
+struct trackDb *tdbForConn = tdb ? tdb : curTrack;
+struct sqlConnection *conn;
+if (tdbForConn == NULL)
+    conn = hAllocConn(db);
+else
+    conn = hAllocConnTrack(db, tdbForConn);
 struct joiner *joiner = allJoiner;
 struct joinerPair *jpList, *jp;
 struct asObject *asObj = asForTable(conn, table);
