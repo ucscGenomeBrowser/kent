998da5b9fb22c85cd8c65414569297f61b8781eb
hiram <hiram>
2009-04-06 17:54:40 +0000

    Make it work from parameters

--- src/utils/userApps/mkREADME.sh
+++ src/utils/userApps/mkREADME.sh
@@ -1,14 +1,42 @@
 #!/bin/sh
+# script to create a FOOTER (or README) file from all usage messages
+#	of binaries found in specified directory
+#
+#========================================================================
+#	$Id: mkREADME.sh,v 1.2 2009/04/06 17:54:40 hiram Exp $
+#========================================================================
+#
+usage() {
+    echo "usage: mkREADME.sh DESTDIR/BINDIR FOOTER"
+    echo "where DESTDIR/BINDIR is where all the binaries are"
+    echo "and FOOTER is the name of the result file"
+    exit 255
+}
+
+if [ "$#" -ne 2 ]; then
+    usage
+fi
+
+DESTDIR=$1
+RESULT=$2
+
+if [ ! -d "${DESTDIR}" ]; then
+    echo "ERROR: specified destination directory \"${DESTDIR}\""
+    echo "	does not exist."
+    usage
+fi
+
+cd "${DESTDIR}"
 
 echo "================================================================
 See also: the file in this directory: bigBedWigs.txt
 	for information on operating the bigWig and bigBed
 	utilities and loading custom tracks
 ================================================================
-" > README.txt
+" > "${RESULT}"
 
 echo "================================================================" \
-	>> README.txt
+	>> "${RESULT}"
 
 find . -mindepth 1 -type f | sed -e "s/^.\///; /mkREADME.sh/d" | sort | \
 while read F
@@ -19,8 +47,5 @@ do
 	./${F}
 	echo "================================================================"
 	fi
-done >> README.txt 2>&1
+done >> "${RESULT}" 2>&1
 
-#========================================================================
-#	$Id: mkREADME.sh,v 1.1 2009/03/30 17:55:21 hiram Exp $
-#========================================================================
