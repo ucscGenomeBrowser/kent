1aa93b30b703b8ff1d7768228fd68cc17bec4910
fanhsu <fanhsu>
2009-06-15 21:56:28 +0000

    Added doH1n1Gene().

--- src/hg/hgc/virusClick.h
+++ src/hg/hgc/virusClick.h
@@ -2,6 +2,8 @@
 #ifndef VIRUSCLICK_H
 #define VIRUSCLICK_H
 
+void doH1n1Gene(struct trackDb *tdb, char *item);
+
 void doH1n1Seq(struct trackDb *tdb, char *item);
 /* Show extra info for H1N1 Seq  Annotations track. */
 
