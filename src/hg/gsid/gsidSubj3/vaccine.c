36041350062ecc21bcb9234a118bd51ae84b7fb3
fanhsu <fanhsu>
2009-07-10 23:37:24 +0000

    Updated with actual fields of the latest data from GSID.

--- src/hg/gsid/gsidSubj3/vaccine.c
+++ src/hg/gsid/gsidSubj3/vaccine.c
@@ -10,98 +10,175 @@
 #include "hdb.h"
 #include "net.h"
 
-static char const rcsid[] = "$Id: vaccine.c,v 1.1 2009/04/16 21:37:57 fanhsu Exp $";
+static char const rcsid[] = "$Id: vaccine.c,v 1.2 2009/07/10 23:37:24 fanhsu Exp $";
 
 static boolean vaccineExists(struct section *section, 
 	struct sqlConnection *conn, char *subjId)
 /* Return TRUE if vaccineAll table exists and it has an entry with the gene symbol */
 {
-if (sqlTableExists(conn, "gsidSubjInfo3") == TRUE)
+if (sqlTableExists(conn, "gsidSubjInfo") == TRUE)
     {
     return(TRUE);
     }
 return(FALSE);
 }
 
+void vxprintf(char *templ, char *string)
+/* print "N/A" if the string to be printed is NULL */
+{
+if (string == NULL) 
+    printf(templ, "N/A");
+else
+    printf(templ, string);
+}
+
 static void vaccinePrint(struct section *section, 
 	struct sqlConnection *conn, char *subjId)
 /* Print out Vaccine section. */
 {
-char *immunStatus;
-char *daysInfectF, *daysInfectL;
-char *injections;
-char *newField1, *newField2;
+char *immunStatus = NULL;
+char *daysInfectF = NULL;
+char *daysInfectL = NULL;
+char *injections  = NULL;
 
+char *startDate       = NULL;
+char *lastSeroNegDay  = NULL;
+char *firstSeroPosDay = NULL;
+char *firstRNAPosDay  = NULL;
+char *ESDBasis	      = NULL;
+char *seqDay	      = NULL;
+
+char *art_daei        = NULL;
+char *art_sequencing  = NULL;
+ 
 char query[256];
 struct sqlResult *sr;
 char **row;
+
 printf("<TABLE>");
 
 safef(query, sizeof(query), 
-      //"select immunStatus, daysInfectF, daysInfectL, injections from gsidSubjInfo3 where subjId='%s'", subjId);
-      "select immunStatus, daysInfectF, daysInfectL, injections, newField1, newField2  from gsidSubjInfo3 where subjId='%s'", subjId);
+      "select art_daei, art_sequencing from hgFixed.artDaei where subjId='%s'", 
+      subjId);
 sr = sqlMustGetResult(conn, query);
 row = sqlNextRow(sr);
-    
 if (row != NULL) 
     {
-    immunStatus  = row[0];
-    daysInfectF	 = row[1];
-    daysInfectL  = row[2];
-    injections   = row[3];
-    newField1    = row[4];
-    newField2    = row[5];
-
-    printf("<TR>");
-    printf("<TD>");
-    printf("<B>Vaccine/Placebo:</B> %s%s", immunStatus, GSBLANKS);
-    printf("</TD>");
-    
-    printf("<TD>");
-    printf("<B>Estimated Study Day of Infection (ESDI)*:</B> %s\n", 
-	   daysInfectF);
-    printf("</TD>");
-
-    printf("</TR>");
-    
-    printf("<TR>");
-    printf("<TD>");
-
-    /* !!! currently all subjects in the database are infected. */
-    /* update this when non-infected subjects addded into DB */
-
-    printf("<B>HIV Status:</B> %s%s", "Infected", GSBLANKS);
-    printf("</TD>");
-
-    /* remove the following item per GSID user feedback */
-    /*printf("<TD>");
-    printf("<B>Days of infection relative to last negative date:</B> %s\n", 
-	   daysInfectL);
-    printf("</TD>");
-    */
-    printf("</TR>");
-    
-    printf("<TR>");
-    printf("<TD>");
-    printf("<B>Injections:</B> &nbsp;%s%s", injections, GSBLANKS);
-    printf("</TD>");
-    printf("</TR>");
-    
-    printf("<TR>");
-    printf("<TD>");
-    printf("<B>New Data Item1:</B> &nbsp;%s%s", newField1, GSBLANKS);
-    printf("</TD>");
-    printf("</TR>");
-    
-    printf("<TR>");
-    printf("<TD>");
-    printf("<B>New Data Item2:</B> &nbsp;%s%s", newField2, GSBLANKS);
-    printf("</TD>");
-    printf("</TR>");
+    art_daei       = cloneString(row[0]);
+    art_sequencing = cloneString(row[1]);
     }
-printf("</TABLE>");
 sqlFreeResult(&sr);
 
+safef(query, sizeof(query), 
+      "select immunStatus, daysInfectF, daysInfectL, injections from gsidSubjInfo where subjId='%s'", 
+      subjId);
+sr = sqlMustGetResult(conn, query);
+row = sqlNextRow(sr);
+if (row != NULL) 
+    {
+    immunStatus  = cloneString(row[0]);
+    daysInfectF	 = cloneString(row[1]);
+    daysInfectL  = cloneString(row[2]);
+    injections   = cloneString(row[3]);
+    }
+sqlFreeResult(&sr);
+
+safef(query, sizeof(query), 
+      "select startDate,lastSeroNegDay,firstSeroPosDay,firstRNAPosDay,ESDBasis,seqDay from hgFixed.testDates where subjId='%s'", subjId);
+sr = sqlMustGetResult(conn, query);
+row = sqlNextRow(sr);
+if (row != NULL) 
+    {
+    startDate 	    = cloneString(row[0]);
+    lastSeroNegDay  = cloneString(row[1]);
+    firstSeroPosDay = cloneString(row[2]);
+    firstRNAPosDay  = cloneString(row[3]);
+    ESDBasis        = cloneString(row[4]);
+    seqDay    	    = cloneString(row[5]);
+    }
+sqlFreeResult(&sr);
+
+printf("<TR>");
+printf("<TD>");
+printf("<B>Vaccine/Placebo:</B> %s%s", immunStatus, GSBLANKS);
+printf("</TD>");
+    
+printf("<TD>");
+vxprintf("<B>ART at Sequencing?</B> %s", art_sequencing);
+printf("</TD>");
+printf("</TR>");
+
+printf("<TR>");
+printf("<TD>");
+/* !!! currently all subjects in the database are infected. */
+/* update this when non-infected subjects addded into DB */
+
+printf("<B>HIV Status:</B> %s%s", "Infected", GSBLANKS);
+printf("</TD>");
+    
+printf("<TD>");
+vxprintf("<B>DAEI for ART Start:</B> %s", art_daei);
+/*if (sameWord(art_daei, "-1"))
+    printf("N/A");
+else
+    vxprintf("%s", art_daei);
+*/printf("</TD>");
+printf("</TR>");
+
+/* removed the following item per GSID user feedback */
+/*printf("<TD>");
+printf("<B>Days of infection relative to last negative date:</B> %s\n", 
+       daysInfectL);
+printf("</TD>");
+*/
+    
+printf("<TR>");
+printf("<TD>");
+printf("<B>Injections:</B> &nbsp;%s%s", injections, GSBLANKS);
+printf("</TD>");
+    
+printf("<TD>");
+vxprintf("<B>Study Day of Last Serology-Negative HIV Test: </B> %s\n", 
+	lastSeroNegDay);
+printf("</TD>");
+printf("</TR>");
+
+printf("<TR>");
+printf("<TD>");
+vxprintf("<B>Date of Study Start:</B> %s\n", 
+       startDate);
+printf("</TD>");
+
+printf("<TD>");
+vxprintf("<B>Study Day of First Serology-Positive HIV Test:</B> %s\n", 
+       firstSeroPosDay);
+printf("</TD>");
+printf("</TR>");
+
+printf("<TR>");
+printf("<TD>");
+vxprintf("<B>Estimated Study Day of Infection (ESDI)*:</B> %s\n", 
+	daysInfectF);
+printf("</TD>");
+
+printf("<TD>");
+vxprintf("<B>Study Day of First RNA-positive HIV Test (NAT):</B> %s\n", firstRNAPosDay);
+printf("</TD>");
+printf("</TR>");
+
+printf("<TR>");
+printf("<TD>");
+vxprintf("<B>Study Day of Sequencing:</B> %s\n", 
+	   seqDay);
+printf("</TD>");
+
+printf("<TD>");
+vxprintf("<B>Basis for ESDI:</B> %s\n", 
+        ESDBasis);
+printf("</TD>");
+printf("</TR>");
+
+printf("</TABLE>");
 return;
 }
 
