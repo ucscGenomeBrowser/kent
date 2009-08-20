bacdd4022d5babbb2a1f34ef0decaa81b31bc145
angie <angie>
2009-08-20 21:36:17 +0000

    Corrected terminology: PNG palette images do not have an alpha channel, just a parallel map of opacity values.  So useAlpha-->useTransparency.

--- src/lib/vPng.c
+++ src/lib/vPng.c
@@ -7,14 +7,14 @@
 #include "vGfx.h"
 #include "vGfxPrivate.h"
 
-static char const rcsid[] = "$Id: vPng.c,v 1.1 2009/08/05 23:34:31 angie Exp $";
+static char const rcsid[] = "$Id: vPng.c,v 1.2 2009/08/20 21:36:17 angie Exp $";
 
 struct memPng
 /* Something that handles a PNG. */
     {
     struct memGfx mg;	/* Memory form.  This needs to be first field. */
     char *fileName;	/* PNG file name. */
-    boolean useAlpha;   /* Do we use alpha channel for transparency of background color? */
+    boolean useTransparency;   /* Make background color transparent if TRUE. */
     };
 
 void memPngClose(struct memPng **pG)
@@ -24,16 +24,16 @@ struct memPng *g = *pG;
 if (g != NULL)
     {
     struct memGfx *mg = (struct memGfx *)g;
-    mgSavePng(mg, g->fileName, g->useAlpha);
+    mgSavePng(mg, g->fileName, g->useTransparency);
     freez(&g->fileName);
     mgFree(&mg);
     *pG = NULL;
     }
 }
 
-struct vGfx *vgOpenPng(int width, int height, char *fileName, boolean useAlpha)
+struct vGfx *vgOpenPng(int width, int height, char *fileName, boolean useTransparency)
 /* Open up something that will write out a PNG file upon vgClose.  
- * If useAlpha, then the first color in memgfx's colormap/palette is
+ * If useTransparency, then the first color in memgfx's colormap/palette is
  * assumed to be the image background color, and pixels of that color
  * are made transparent. */
 {
@@ -50,7 +50,7 @@ vg->close = (vg_close)memPngClose;
  * inheritence from mg essentially. */
 AllocVar(png);
 png->fileName = cloneString(fileName);
-png->useAlpha = useAlpha;
+png->useTransparency = useTransparency;
 
 /* Fill in the mg part of this structure with normal memGfx. */
 mg = mgNew(width, height);
