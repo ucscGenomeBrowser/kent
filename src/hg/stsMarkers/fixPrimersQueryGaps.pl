bcf159f467706a8a080cad20786da998b1a1a5ed
hiram <hiram>
2009-08-20 21:51:19 +0000

    Cleaned code and working better May 6th 2009

--- src/hg/stsMarkers/fixPrimersQueryGaps.pl
+++ src/hg/stsMarkers/fixPrimersQueryGaps.pl
@@ -1,4 +1,4 @@
-#!/usr/local/bin/perl
+#!/usr/bin/env perl
 # File: fixPrimersQueryGaps
 # Author: Terry Furey
 # Date: 7/5/2003
@@ -6,56 +6,64 @@
 # Description: Fix query gap sizes from output of isPcr to match 
 # true pcr product size
 
+use strict;
+use warnings;
+
 # Usage message
 if ($#ARGV < 0) {
-  print stderr "USAGE: fixPrimersQueryGaps <primer file> <psl file>\n";
+  print STDERR "USAGE: fixPrimersQueryGaps <primer file> <psl file>\n";
   exit(1);
 }
 
 # Read in and check parameters
-$file = shift(@ARGV);
-open(FILE, "<$file") || die("Could not open $file\n");
-$psl = shift(@ARGV);
-open(PSL, "$psl") || die("Could not open $psl\n");
+my $file = shift(@ARGV);
+open(FILE, "<$file") || die("Can not open primer file '$file'");
+my $psl = shift(@ARGV);
+open(PSL, "$psl") || die("Can not open psl file '$psl'");
 
+my %length;
+
+printf STDERR "reading pcr product lengths from $file\n";
 # Read in lengths of pcr products
-while($line = <FILE>) {
+while(my $line = <FILE>) {
   chomp($line);
-  ($id, $left, $right, $length, $otherid) = split(' ',$line);
-  if ($length == "-") {
+  my ($id, $left, $right, $length, $otherid) = split('\s+',$line);
+  if ($length eq "-") {
       $length{$id} = 500;
-  } elsif ($length =~ "-") {
-      ($start, $end) = split("-",$length);
+  } elsif ($length =~ m/-/) {
+      my ($start, $end) = split('-',$length);
       $length{$id} = int(($end - $start)/2) + $start;
   } else {
       $length{$id} = $length;
   }
-  @left = split(//,$left);
-  @right = split(//,$right);
-  $numN = $length - $#left - $#right - 2;
+  my @left = split(//,$left);
+  my @right = split(//,$right);
+  my $numN = $length{$id} - $#left - $#right - 2;
   if ($numN <=0) {
       $length{$id} = 50;
   }
 }
 
 # Correct query gap sizes in psl file
-while ($line = <PSL>) {
+printf STDERR "reading psl file $psl\n";
+while (my $line = <PSL>) {
     chomp($line);
-    @fields = split("\t",$line);
+    my @fields = split("\t",$line);
     if ($fields[4] != 1) {
-	print stderr "check $line\n";
+	print STDERR "check $line\n";
     }
-    ($dbsts, $id) = split("_", $fields[9]);
+    my ($dbsts, $id) = split("_", $fields[9]);
     # change query gap
+    die "can not find length for id $id" if (!exists($length{$id}));
     $fields[5] = $length{$id} - $fields[0] - $fields[1];
     # change query size
     $fields[10] = $length{$id};
     $fields[12] = $length{$id};
     # change query starts
-    @qstarts = split(/\,/, $fields[19]);    
-    @blocks = split(/\,/, $fields[18]);
-    $secondStart = $length{$id} - $blocks[1];
-    $fields[19] = "$qstarts[0],$secondStart";
+    my @qstarts = split(/\,/, $fields[19]);    
+    my @blocks = split(/\,/, $fields[18]);
+    my $secondStart = $length{$id} - $blocks[1];
+    $fields[19] = "$qstarts[0],$secondStart,";
     # put back together and print
     $line = join("\t", @fields);
     print "$line\n";
