de5743dc274e2ae97b0faa758d3aa414bf456b4b
hiram <hiram>
2009-11-25 18:01:46 +0000

    Add viewLimits recommended calculation

--- src/hg/makeDb/hgLoadWiggle/wigTableStats.sh
+++ src/hg/makeDb/hgLoadWiggle/wigTableStats.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-#	$Id: wigTableStats.sh,v 1.1 2009/11/22 22:50:02 hiram Exp $
+#	$Id: wigTableStats.sh,v 1.2 2009/11/25 18:01:46 hiram Exp $
 
 DB=$1
 T=$2
@@ -21,6 +21,21 @@ echo -e -n "${DB}.${T}\t"
 hgsql -N ${DB} \
 -e "select lowerLimit,dataRange,validCount,sumData,sumSquares from ${T}" \
     | awk '
+function abs(value) { if (value < 0) {return -value;} else {return value;} }
+function viewUpper(min, max, mean, stdDev,  fiveDev, range, upper) {
+fiveDev = 5 * stdDev;
+range = abs(max-min);
+upper = mean + fiveDev;
+if (upper > max) upper = max;
+return upper;
+}
+function viewLower(min, max, mean, stdDev,  fiveDev, range, lower) {
+fiveDev = 5 * stdDev;
+range = abs(max-min);
+lower = mean - fiveDev;
+if (lower < min) lower = min;
+return lower;
+}
 BEGIN { lower=3.0e+100; upper=-3.0e+100; count = 0; sumData = 0.0;
 	sumSquares = 0.0 }
 {
@@ -37,7 +52,10 @@ if (count > 0) {
     var = sumSquares - (sumData*sumData)/count;
     stdDev = var;
     if (count > 1) { stdDev = sqrt(var/(count-1)); }
-    printf "%g %g %g %d %g %g\n", lower, upper, mean, count, sumData, stdDev
+    vLower = viewLower(lower, upper, mean, stdDev);
+    vUpper = viewUpper(lower, upper, mean, stdDev);
+    printf "%g %g %g %d %g %g viewLimits=%g:%g\n",
+	lower, upper, mean, count, sumData, stdDev, vLower, vUpper
 } else {
 printf "empty data set\n"
 }
