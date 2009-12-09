85dade0d129c55a3298a66588e5b6abf736699a9
fanhsu <fanhsu>
2009-12-09 18:50:25 +0000

    Added table definition for 3 new Illumina SNP array chip raw data.

---
+++ src/hg/lib/snpArrayIlluminaHuman660W_QuadRaw.sql
@@ -0,0 +1,11 @@
+CREATE TABLE snpArrayIlluminaHuman660W_QuadRaw (
+  SNPname varchar(20),
+  dbSNPrefStrand char(1),
+  ILMNrefStrand char(1),
+  SNPalleles varchar(20),
+  GenomeBuild varchar(10),
+  Chromosome varchar(10),
+  Coordinate int,
+  KEY SNPname (SNPname)
+) TYPE=MyISAM;
+
