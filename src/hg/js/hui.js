f778cc09ffe74389d3edc85cc1739ae7bd582725
tdreszer <tdreszer>
2009-12-11 23:08:06 +0000

    Really fixed.

--- src/hg/js/hui.js
+++ src/hg/js/hui.js
@@ -1,5 +1,5 @@
 // JavaScript Especially for hui.c
-// $Header: /projects/compbio/cvsroot/kent/src/hg/js/hui.js,v 1.45 2009/12/11 22:55:27 tdreszer Exp $
+// $Header: /projects/compbio/cvsroot/kent/src/hg/js/hui.js,v 1.46 2009/12/11 23:08:06 tdreszer Exp $
 
 var compositeName = "";
 //var now = new Date();
@@ -459,8 +459,9 @@ function compositeCfgUpdateSubtrackCfgs(inp)
     }
     else if(inp.type.indexOf("radio") == 0) {
         var list = $("input:radio[name$='"+suffix+"']").not("[name='"+inp.name+"']");
+        list = $(list).filter("[value='"+inp.value+"']")
         if($(list).length>0)
-            $(list).attr("checked",$(inp).attr("checked"));
+            $(list).attr("checked",true);
     }
     else {  // Various types of inputs
         var list = $("input[name$='"+suffix+"']").not("[name='"+inp.name+"']");//.not("[name^='boolshad.']"); // Exclude self from list
