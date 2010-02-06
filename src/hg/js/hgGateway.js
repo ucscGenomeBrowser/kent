64dc7e6eed1f9b62e522e96fa96f79397b25931b
larrym <larrym>
2010-02-06 01:19:17 +0000

    javascript for hgGateway CGI

---
+++ src/hg/js/hgGateway.js
@@ -0,0 +1,12 @@
+
+$(document).ready(function()
+{
+    $('input#suggest').autocomplete({
+                                        delay: 500,
+                                        minchars: 1,
+                                        ajax_get: ajaxGet(function () {return document.orgForm.db.value;}, new Object),
+                                        callback: function (obj) {
+                                            document.mainForm.position.value = obj.id;
+                                        }
+                                       });
+});
