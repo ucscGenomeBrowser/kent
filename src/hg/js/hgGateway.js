6bc09e681dc9ce2f65416d510889e473c0cedafe
larrym <larrym>
2010-02-24 22:10:04 +0000

    turn on autowidth so strings don't get squashed in pulldown

--- src/hg/js/hgGateway.js
+++ src/hg/js/hgGateway.js
@@ -4,6 +4,7 @@ $(document).ready(function()
     $('input#suggest').autocomplete({
                                         delay: 500,
                                         minchars: 1,
+                                        autowidth: true,
                                         ajax_get: ajaxGet(function () {return document.orgForm.db.value;}, new Object),
                                         callback: function (obj) {
                                             document.mainForm.position.value = obj.id;
