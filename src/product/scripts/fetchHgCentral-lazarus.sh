5090306efae8b1475ec8747c7503e00c7523ba70
hiram <hiram>
2010-03-19 16:55:33 +0000

    simple script to fetch hgcentral.sql

---
+++ src/product/scripts/fetchHgCentral-lazarus.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+#
+#	$Id: fetchHgCentral-lazarus.sh,v 1.1.2.1 2010/03/19 16:55:31 hiram Exp $
+#
+
+if [ $# -ne 1 ]; then
+    echo "usage: fetchCentral.sh go"
+    echo "need to use the argument go in order to get this to run."
+    echo "  This script simply runs a wget command to fetch the hgcentral.sql"
+    echo "database definition from UCSC.  Output is to stdout, reroute the"
+    echo "output to a file to save, e.g.:"
+    echo "./fetchHgCentral.sh > ucsc.hgcentral.sql"
+    exit 255
+fi
+
+if [ $1 = "go" ]; then
+    wget --timestamping -O /dev/stdout \
+	http://hgdownload.cse.ucsc.edu/admin/hgcentral.sql
+else
+    echo "ERROR: need to use the argument: go"
+    echo "in order to get this to run"
+fi
