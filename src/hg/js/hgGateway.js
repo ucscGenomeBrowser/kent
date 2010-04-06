ea6b0c17fddc508c028744336d3229b3bd6a3b23
larrym <larrym>
2010-04-06 07:36:30 +0000

    change minchars to 2

--- src/hg/js/hgGateway.js
+++ src/hg/js/hgGateway.js
@@ -3,7 +3,7 @@ $(document).ready(function()
 {
     $('input#suggest').autocomplete({
                                         delay: 500,
-                                        minchars: 1,
+                                        minchars: 2,
                                         autowidth: true,
                                         ajax_get: ajaxGet(function () {return document.orgForm.db.value;}, new Object),
                                         callback: function (obj) {
