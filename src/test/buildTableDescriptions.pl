1e66d2c90fe48140fbb6602b881160427d14a373
angie <angie>
2010-04-13 17:16:50 +0000

    Remove check for canonical db name format -- vestige from when hgwdev had dozens of Lowe lab databases.  New db names e.g. felCatV17e don't fit the old regex, but having active=1 in dbDb is good enough.

--- src/test/buildTableDescriptions.pl
+++ src/test/buildTableDescriptions.pl
@@ -379,12 +379,6 @@ my @auxDbs = ('hgFixed', 'proteome');
 my @dbs = (defined $opt_db) ? split(',', $opt_db) :
                               (&getActiveDbs($hgConf), @auxDbs);
 foreach my $db (@dbs) {
-  if (($db !~ /^(\w\w\d+|\w\w\w\w\w\w\d+|caePb\d+|hiv.*)$/ &&
-       (scalar(grep {$_ eq $db} @auxDbs) == 0)) ||
-      $db =~ /^zoo/) {
-    print "Skipping database $db.\n";
-    next;
-  }
   my $sqlFile = "$db.tableDescriptions.sql";
   open(SQL, ">$sqlFile") || die "Can't open $sqlFile for writing";
   print SQL "use $db;\n";
