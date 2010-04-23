5613cb5d140dc24e34a2a94e5d63c25e70953b01
tdreszer <tdreszer>
2010-04-23 02:56:21 +0000

    Made RA style the default output

--- src/hg/makeDb/mdbUpdate/mdbPrint.c
+++ src/hg/makeDb/mdbUpdate/mdbPrint.c
@@ -4,7 +4,7 @@
 #include "options.h"
 #include "mdb.h"
 
-static char const rcsid[] = "$Id: mdbPrint.c,v 1.1 2010/04/13 19:59:16 tdreszer Exp $";
+static char const rcsid[] = "$Id: mdbPrint.c,v 1.2 2010/04/23 02:56:21 tdreszer Exp $";
 
 #define OBJTYPE_DEFAULT "table"
 
@@ -14,7 +14,7 @@ void usage()
 errAbort(
   "mdbPrint - Prints metadata objects and variables from the mdb metadata table.\n"
   "usage:\n"
-  "   mdbPrint {db} [-table=] [-byVar] [-ra/-countObjs/-countVars/-countVals]\n"
+  "   mdbPrint {db} [-table=] [-byVar] [-ra/-line/-countObjs/-countVars/-countVals]\n"
   "                 [-all]\n"
   "                 [-obj= [-var= [-val=]]]\n"
   "                 [-var= [-val=]]\n"
@@ -25,8 +25,9 @@ errAbort(
   "             '" MDB_DEFAULT_NAME "'.\n"
   "    -byVar   Print each var and val, then all objects that match,\n"
   "             as opposed to printing objects and all the var=val pairs that match.\n"
-  "    -ra      Print each obj with a set of indented var val pairs on separate lines.\n"
-  "             With -byVar prints pseudo-RA style with 2 levels of indentation.\n"
+  "    -ra      Default. Print each obj with a set of indented var val pairs on separate\n"
+  "             lines. With -byVar prints pseudo-RA style with multiple objects per stanza.\n"
+  "    -line    Print each obj and all var=val pairs on a single line.\n"
   "    -countObjs   Just print count of objects returned in the query.\n"
   "    -countVars   Just print count of variables returned in the query.\n"
   "    -countVals   Just print count of values returned in the query.\n"
@@ -37,19 +38,19 @@ errAbort(
   "    -vars={var=val...}  Request a combination of var=val pairs.\n\n"
   "                    Use of 'var!=val', 'var=v%%' and 'var=?' are supported.\n"
   "There are two basic views of the data: by objects and by variables.  The default view "
-  "is by object.  Each line of output will contain an object and all it's var=val pairs "
-  "as a 'formatted metadata' line.  In 'byVar' view, a single line per var=val and then "
-  "a space seperated list of each objects is printed.  RA style will place each object "
-  "and var val on a separate line with indentation.  Alternatively, request only counting "
-  "of objects, variables or values.\n"
+  "is by object.  Each object will print out in an RA style stanza (by default) or as "
+  "a single line of output containing all var=val pairs. In 'byVar' view, each RA style "
+  "stanza holds a var val pair and all objects belonging to that pair on separate lines.  "
+  "Linear 'byVar' view puts the entire var=val pair on one line.  Alternatively, request "
+  "only counting of objects, variables or values.\n"
   "HINT: Use '%%' in any obj, var or val as a wildcard for selection.\n\n"
   "Examples:\n"
   "  mdbPrint hg19 -vars=\"grant=Snyder cell=GM12878 antibody=CTCF\"\n"
   "           Return all objs that satify ALL of the constraints.\n"
-  "  mdbPrint mm9 -byVar -vars=\"grant=Snyder cell=GM12878 antibody=?\"\n"
-  "           Return each var=val pair and ANY objects that have the constraint.\n"
-  "  mdbPrint hg18 -obj=wgEncodeUncFAIREseqPeaksPanislets\n"
-  "           Return the formatted metadata line for the one object.\n"
+  "  mdbPrint mm9 -vars=\"grant=Snyder cell=GM12878 antibody=?\" -byVar\n"
+  "           Return each all vars for all objects with the constraint.\n"
+  "  mdbPrint hg18 -obj=wgEncodeUncFAIREseqPeaksPanislets -line\n"
+  "           Return a single formatted metadata line for one object.\n"
   "  mdbPrint hg18 -countObjs -var=cell -val=GM%%\n"
   "           Return the count of objects which have a declared cell begining with 'GM'.\n"
   );
@@ -58,6 +59,7 @@ errAbort(
 static struct optionSpec optionSpecs[] = {
     {"table",    OPTION_STRING}, // default "mdb"
     {"ra",       OPTION_BOOLEAN},// ra format
+    {"line",     OPTION_BOOLEAN},// linear format
     {"countObjs",OPTION_BOOLEAN},// returns only count of objects
     {"countVars",OPTION_BOOLEAN},// returns only count of variables
     {"countVals",OPTION_BOOLEAN},// returns only count of values
@@ -89,7 +91,9 @@ if(argc < 2)
 
 char *db        = argv[1];
 char *table     = optionVal("table",NULL);
-boolean raStyle = optionExists("ra");
+boolean raStyle = TRUE;
+if(optionExists("line") && !optionExists("ra"))
+    raStyle = FALSE;
 boolean cntObjs = optionExists("countObjs");
 boolean cntVars = optionExists("countVars");
 boolean cntVals = optionExists("countVals");
