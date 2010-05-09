a99714b0d4a2eaed42d0a1e9516b786bf2f225f3
kent <kent>
2010-05-09 17:23:57 +0000

    Put in commented out line that does not have force-warnings-to-errors compiler flag.

--- src/inc/common.mk
+++ src/inc/common.mk
@@ -55,6 +55,7 @@ ifeq (${HG_WARN},)
       HG_WARN_UNINIT=-Wuninitialized
     else
       HG_WARN = -Wall -Werror -Wformat -Wimplicit -Wreturn-type
+      # HG_WARN = -Wall -Wformat -Wimplicit -Wreturn-type
       HG_WARN_UNINIT=-Wuninitialized
     endif
   endif
