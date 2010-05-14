0b10fb5253e90e54edb91765c645cb6e0854c71b
tdreszer <tdreszer>
2010-05-14 17:29:34 +0000

    Added setCartVars to set an array of names and values.  Needed for inageV2 imgOrd

--- src/hg/js/ajax.js
+++ src/hg/js/ajax.js
@@ -50,9 +50,13 @@ function loadXMLDoc(url, callBack)
     }
 }
 
-function setCartVar(name, value)
+function setCartVars(names, values)
 {
-// Asynchronously set a cart variable.
+// Asynchronously sets the array of cart vars with values
+    if(names.length <= 0)
+        return;
+
+    // Set up constant portion of url
     var loc = window.location.href;
     if(loc.indexOf("?") > -1) {
         loc = loc.substring(0, loc.indexOf("?"));
@@ -62,8 +66,27 @@ function setCartVar(name, value)
     }
     loc = loc + "/cartDump";
     var hgsid = getHgsid();
-    loc = loc + "?submit=1&noDisplay=1&cartDump.varName=" + escape(name) + "&cartDump.newValue=" + escape(value) + "&hgsid=" + hgsid;
-    loadXMLDoc(loc);
+    loc = loc + "?submit=1&noDisplay=1&hgsid=" + hgsid;
+
+    // Set up dynamic portion of url
+    var ix=0;
+    while( ix < names.length ) { // Sends multiple messages if the URL gets too long
+        var pairs = "";
+        for(  ;ix<names.length && pairs.length < 5000;ix++) {  // FIXME: How big is too big?
+            //pairs = pairs + "&cartDump.varName=" + escape(names[ix]) + "&cartDump.newValue=" + escape(values[ix]);
+            pairs = pairs + "&" + escape(names[ix]) + "=" + escape(values[ix]);
+        }
+        if(pairs.length == 0)
+            return;
+        //warn(pairs);
+        loadXMLDoc(loc + pairs);
+    }
+}
+
+function setCartVar(name, value)
+{
+// Asynchronously set a cart variable.
+    setCartVars( [ name ], [ value ] );
 }
 
 function submitMain()
