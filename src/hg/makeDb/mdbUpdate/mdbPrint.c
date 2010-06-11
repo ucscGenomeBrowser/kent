920c230612bf3a9e6e174a5d315cd0ec470d582d
tdreszer <tdreszer>
2010-06-11 17:33:57 +0000

    Added special option to print mdbUpdate lines to assist in importing vars from one db.mdb to another.

--- src/hg/makeDb/mdbUpdate/mdbPrint.c
+++ src/hg/makeDb/mdbUpdate/mdbPrint.c
@@ -4,7 +4,7 @@
 #include "options.h"
 #include "mdb.h"
 
-static char const rcsid[] = "$Id: mdbPrint.c,v 1.5 2010/04/27 22:40:12 tdreszer Exp $";
+static char const rcsid[] = "$Id: mdbPrint.c,v 1.6 2010/06/11 17:33:57 tdreszer Exp $";
 
 #define OBJTYPE_DEFAULT "table"
 
@@ -18,7 +18,8 @@ errAbort(
   "                 [-all]\n"
   "                 [-obj= [-var= [-val=]]]\n"
   "                 [-var= [-val=]]\n"
-  "                 [-vars=\"var1=val1 var2=val2...]\n"
+  "                 [-vars=\"var1=val1 var2=val2...\"]\n"
+  "                 [-specialHelp]\n"
   "Options:\n"
   "    {db}     Database to query metadata from.  This argument is required.\n"
   "    -table   Table to query metadata from.  Default is the sandbox version of\n"
@@ -31,6 +32,7 @@ errAbort(
   "    -countObjs   Just print count of objects returned in the query.\n"
   "    -countVars   Just print count of variables returned in the query.\n"
   "    -countVals   Just print count of values returned in the query.\n"
+  "    -specialHelp Prints help for some special case features.\n"
   "  Four alternate ways to select metadata:\n"
   "    -all       Will print entire table (this could be huge).\n"
   "    -obj={objName}  Request a single object.  Can be narrowed by var and val.\n"
@@ -65,12 +67,59 @@ static struct optionSpec optionSpecs[] = {
     {"countVals",OPTION_BOOLEAN},// returns only count of values
     {"all",      OPTION_BOOLEAN},// query entire table
     {"byVar",    OPTION_BOOLEAN},// With -all prints from var perspective
+    {"specialHelp",OPTION_BOOLEAN},// Certain very specialized features are described
     {"obj",      OPTION_STRING}, // objName or objId
     {"var",      OPTION_STRING}, // variable
     {"val",      OPTION_STRING}, // value
     {"vars",     OPTION_STRING},// var1=val1 var2=val2...
+    {"updDb",    OPTION_STRING},// DB to update
+    {"updMdb",   OPTION_STRING},// MDB table to update
+    {"updSelect",OPTION_STRING},// Experiment defining vars: "var1,var2"
+    {"updVars",  OPTION_STRING},// Vars to update: "var1,var2"
     {NULL,       0}
 };
+
+void specialHelp()
+/* Explain usage and exit. */
+{
+errAbort(
+  "mdbPrint - Prints specially output from metadata objects and variables.\n"
+  "usage:\n"
+  "   mdbPrint {db} [-table=] -vars=\"var1=val1 var2=val2...\"\n"
+  "            -updDB={db} -updMdb={metaDb} -updSelect=var1,var2,... -updVars=varA,varB,...\n"
+  "Options:\n"
+  "    {db}     Database to query metadata from.  This argument is required.\n"
+  "    -table   Table to query metadata from.  Default is the sandbox version of\n"
+  "             '" MDB_DEFAULT_NAME "'.\n"
+  "    -vars={var=val...}  Request a combination of var=val pairs.\n"
+  "                    Use of 'var!=val', 'var=v%%' and 'var=?' are supported.\n"
+  "  Special functions:\n"
+  "    Print mdbUpdate lines to assist importing metadata from one db.table to another.\n"
+  "    -updDb      Database to aim mdbUpdate lines at.\n"
+  "    -updMdb     The metaDb table to aim mdbUpdate lines at.\n"
+  "    -updSelect  A comma separated list of variables that will be selected with\n"
+  "                the mdbUpdate (via '-vars').\n"
+  "    -updVars    A comma separated list of variables that will be set in the\n"
+  "                mdbUpdate lines (via '-setVars').\n"
+  "The purpose of this special option is to generate mdbUpdate commands from existing metadata.\n"
+  "Examples:\n"
+  "  mdbPrint hg18 -vars=\"composite=wgEncodeYaleChIPseq\" -upd=hg19 -updMdb=metaDb_braney\n"
+  "    (cont.)     -updSelect=grant,cell,antibody,treatment -updVars=dateSubmitted,dateUnrestricted\n"
+  "           This command assists importing dateSubmitted from hg18 to hg19 for all\n"
+  "           objects in hg19 that match the grant, cell, antibody, and treatment of\n"
+  "           objects in hg18.  It would result in output that looks something like:\n"
+  "     mdbUpdate hg19 -table=metaDb_cricket -vars=\"grant=Snyder cell=GM12878 antibody=c-Fos treatment=None\"\n"
+  "       (cont.) -setVars=\"dateSubmitted=2009-02-13 dateUnrestricted=2009-11-12\" -test\n"
+  "     mdbUpdate hg19 -table=metaDb_braney -vars=\"grant=Snyder cell=GM12878 antibody=c-Jun treatment=None\"\n"
+  "       (cont.) -setVars=\"dateSubmitted=2009-01-08 dateUnrestricted=2009-08-07\" -test\n"
+  "     mdbUpdate hg19 ...\n"
+  "           Note the -test flag in the output that will allow confirmation of effects before actual update.\n"
+  "  mdbPrint hg18 -vars=\"composite=wgEncodeYaleChIPseq view=RawSignal\" -upd=hg18 -updMdb=metaDb_vsmalladi\n"
+  "    (cont.)     -updSelect=obj -updVars=fileName\n"
+  "           You can select by object too (but not in combination with other vars).  This example shows\n"
+  "           how to assist updating vals to the same mdb, where an editor or awk is also needed.\n"
+  );
+}
 
 int main(int argc, char *argv[])
 // Process command line.
@@ -83,6 +132,10 @@ if(argc == 1)
     usage();
 
 optionInit(&argc, argv, optionSpecs);
+
+if(optionExists("specialHelp"))
+    specialHelp();
+
 if(argc < 2)
     {
     verbose(1, "REQUIRED 'DB' argument not found:\n");
@@ -173,7 +226,18 @@ else
         varsCnt=mdbObjCount(queryResults,FALSE);
         valsCnt=varsCnt;
         if(!cntObjs && !cntVars && !cntVals)
-            mdbObjPrint(queryResults,raStyle);
+            {
+            if(optionExists("updSelect")) // Special print of mdbUpdate lines
+                {
+                if(!optionExists("updDb") || !optionExists("updMdb") || !optionExists("updVars"))
+                    errAbort("To print mdbUpdate lines, all the following values are needed: '-updDb=' '-updMdb=' '-updSelect=' '-updVars='.\n");
+
+                mdbObjPrintUpdateLines(&queryResults,optionVal("updDb",NULL),optionVal("updMdb",NULL),
+                                                    optionVal("updSelect",NULL),optionVal("updVars",NULL));
+                }
+            else
+                mdbObjPrint(queryResults,raStyle);
+            }
         mdbObjsFree(&queryResults);
         }
     }
