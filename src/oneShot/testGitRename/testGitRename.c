18cad7657899363769f7dc179754ad45e99cb46b
Jim Kent <kent@soe.ucsc.edu>
2010-06-21 13:38:48 -0700

    Renaming function and putting prototype for it in a shared header file.  Again this is all part of testing renaming/splitting/merging functions.

--- src/oneShot/testGitRename/testGitRename.c
+++ src/oneShot/testGitRename/testGitRename.c
@@ -3,6 +3,7 @@
 #include "linefile.h"
 #include "hash.h"
 #include "options.h"
+#include "upperFile.h"
 
 static char const rcsid[] = "$Id: newProg.c,v 1.30 2010/03/24 21:18:33 hiram Exp $";
 
@@ -22,8 +23,6 @@ static struct optionSpec options[] = {
    {NULL, 0},
 };
 
-void testGitRename(char *input, char *output);
-/* testGitRename - Just a little something to test renaming, merging, etc.. */
 
 int main(int argc, char *argv[])
 /* Process command line. */
@@ -31,6 +30,6 @@ int main(int argc, char *argv[])
 optionInit(&argc, argv, options);
 if (argc != 3)
     usage();
-testGitRename(argv[1], argv[2]);
+upperFile(argv[1], argv[2]);
 return 0;
 }
--- src/oneShot/testGitRename/upperFile.c
+++ src/oneShot/testGitRename/upperFile.c
@@ -2,9 +2,10 @@
 
 #include "common.h"
 #include "linefile.h"
+#include "upperFile.h"
 
-void testGitRename(char *input, char *output)
-/* testGitRename - Just a little something to test renaming, merging, etc.. */
+void upperFile(char *input, char *output)
+/* upperFile - convert file to upper case. */
 {
 struct lineFile *lf = lineFileOpen(input, TRUE);
 FILE *f = mustOpen(output, "w");
@@ -14,7 +15,6 @@ while (lineFileNext(lf, &line, NULL))
     touppers(line);
     fprintf(f, "%s\n", line);
     }
-
 carefulClose(&f);
 }
 
