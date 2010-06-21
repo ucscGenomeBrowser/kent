7451d36950210fb0e91f5983eb98effbf63d519c
Jim Kent <kent@soe.ucsc.edu>
2010-06-21 13:47:22 -0700

    Merged two files.

--- src/oneShot/testGitRename/justAnotherTest.c
+++ src/oneShot/testGitRename/justAnotherTest.c
@@ -33,3 +33,23 @@ if (argc != 3)
 upperFile(argv[1], argv[2]);
 return 0;
 }
+/* upperFile - convert file to upper case. */
+
+#include "common.h"
+#include "linefile.h"
+#include "upperFile.h"
+
+void upperFile(char *input, char *output)
+/* upperFile - convert file to upper case. */
+{
+struct lineFile *lf = lineFileOpen(input, TRUE);
+FILE *f = mustOpen(output, "w");
+char *line;
+while (lineFileNext(lf, &line, NULL))
+    {
+    touppers(line);
+    fprintf(f, "%s\n", line);
+    }
+carefulClose(&f);
+}
+
