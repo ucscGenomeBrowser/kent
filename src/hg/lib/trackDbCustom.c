99f1edbe4c7aa76d67bbc5f8b1869347dbfaf255
Jim Kent <kent@soe.ucsc.edu>
2010-08-31 12:10:01 -0700

    Making trackDbFromRa() able to take a URL as well as a file name.

--- src/hg/lib/trackDbCustom.c
+++ src/hg/lib/trackDbCustom.c
@@ -250,7 +250,7 @@ else
 struct trackDb *trackDbFromRa(char *raFile)
 /* Load track info from ra file into list. */
 {
-struct lineFile *lf = lineFileOpen(raFile, TRUE);
+struct lineFile *lf = netLineFileOpen(raFile);
 char *line, *word;
 struct trackDb *btList = NULL, *bt;
 boolean done = FALSE;
