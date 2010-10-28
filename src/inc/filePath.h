cc79022c128caab81ba5686174b0085ce480259f
Jim Kent <kent@soe.ucsc.edu>
2010-10-27 17:53:19 -0700

    Adding new function pathRelativeToFile

--- src/inc/filePath.h
+++ src/inc/filePath.h
@@ -14,6 +14,16 @@ void splitPath(char *path, char dir[PATH_LEN], char name[FILENAME_LEN],
 char *expandRelativePath(char *baseDir, char *relPath);
 /* Expand relative path to more absolute one. */
 
+char *pathRelativeToFile(char *baseFile, char *relPath);
+/* Given a base file name and a path relative to that, return
+ * relative path interpreted as if it were seen from the
+ * same directory holding the baseFile.  
+ *   An example of using this would be in processing include
+ * files.  In this case the baseFile would be the current
+ * source file, and the relPath would be from the include
+ * statement.  The returned result could then be used to
+ * open the include file. */
+
 void undosPath(char *path);
 /* Convert '\' to '/' in path. (DOS/Windows is typically ok with
  * this actually.) */
