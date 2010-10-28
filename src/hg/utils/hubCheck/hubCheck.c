35672bc6efa848f01a78f01298e60350d50da6db
Jim Kent <kent@soe.ucsc.edu>
2010-10-28 12:15:45 -0700

    Checking trackDb.ra file to make sure items are containers or have bigDataUrl.  Some other checks.

--- src/hg/utils/hubCheck/hubCheck.c
+++ src/hg/utils/hubCheck/hubCheck.c
@@ -8,6 +8,10 @@
 #include "filePath.h"
 #include "htmlPage.h"
 #include "trackDb.h"
+#include "bigWig.h"
+#include "bigBed.h"
+#include "dnaseq.h"
+#include "bamFile.h"
 
 static char const rcsid[] = "$Id: newProg.c,v 1.30 2010/03/24 21:18:33 hiram Exp $";
 
@@ -49,7 +53,7 @@ struct trackHubGenome
 /* A genome serviced within a track hub. */
     {
     struct trackHubGenome *next;
-    char *genome;	/* Something like hg18 or mm9 - a UCSC assembly database name. */
+    char *name;	/* Something like hg18 or mm9 - a UCSC assembly database name. */
     char *trackDbFile;	/* The base trackDb.ra file. */
     };
 
@@ -106,9 +110,9 @@ while ((ra = raNextRecord(lf)) != NULL)
     if (trackDb == NULL)
         badGenomeStanza(lf);
     AllocVar(el);
-    el->genome = cloneString(genome);
+    el->name = cloneString(genome);
     el->trackDbFile = trackHubRelativeUrl(url, trackDb);
-    hashAdd(hash, el->genome, el);
+    hashAdd(hash, el->name, el);
     slAddHead(&list, el);
     hashFree(&ra);
     }
@@ -157,7 +161,6 @@ hub->genomesFile = trackHubRequiredSetting(hub, "genomesFile");
 
 lineFileClose(&lf);
 char *genomesUrl = trackHubRelativeUrl(hub->url, hub->genomesFile);
-uglyf("genomesUrl is %s\n", genomesUrl);
 
 hub->genomeHash = hashNew(8);
 hub->genomeList = trackHubGenomeReadRa(genomesUrl, hub->genomeHash);
@@ -180,29 +183,152 @@ if (hub != NULL)
     }
 }
 
+static char *requiredSetting(struct trackHub *hub, struct trackHubGenome *genome,
+	struct trackDb *tdb, char *setting)
+/* Fetch setting or give an error message, a little more specific than the
+ * error message from trackDbRequiredSetting(). */
+{
+char *val = trackDbSetting(tdb, setting);
+if (val == NULL)
+    errAbort("Missing required %s setting in hub %s genome %s track %s", setting,
+    	hub->url, genome->name, tdb->track);
+return val;
+}
+
+static void checkTagsLegal(struct trackHub *hub, struct trackHubGenome *genome,
+	struct trackDb *tdb)
+/* Make sure that tdb has all the required tags and is of a supported type. */
+{
+/* Check for existence of fields required in all tracks */
+requiredSetting(hub, genome, tdb, "shortLabel");
+requiredSetting(hub, genome, tdb, "longLabel");
+
+/* Further checks depend whether it is a container. */
+if (tdb->subtracks != NULL)
+    {
+    if (trackDbSetting(tdb, "compositeTrack"))
+        {
+	}
+    else if (trackDbSetting(tdb, "container"))
+        {
+	}
+    else
+        {
+	errAbort("Parent track %s is not compositeTrack or container in hub %s genome %s", 
+		tdb->track, hub->url, genome->name);
+	}
+    }
+else
+    {
+    /* Check type field. */
+    char *type = requiredSetting(hub, genome, tdb, "type");
+    if (startsWithWord("bigWig", type))
+	;
+    else if (startsWithWord("bigBed", type))
+	;
+    else if (startsWithWord("bam", type))
+	;
+    else
+	errAbort("Unsupported type %s in hub %s genome %s track %s", type,
+	    hub->url, genome->name, tdb->track);
+
+    requiredSetting(hub, genome, tdb, "bigDataUrl");
+    }
+
+}
+
 struct trackDb *trackHubTracksForGenome(struct trackHub *hub, struct trackHubGenome *genome)
-/* Get list of tracks associated with genome. */
+/* Get list of tracks associated with genome.  Check that it only is composed of legal
+ * types.  Do a few other quick checks to catch errors early. */
 {
 struct lineFile *lf = udcWrapShortLineFile(genome->trackDbFile, NULL, 16*1024*1024);
 struct trackDb *tdbList = trackDbFromOpenRa(lf);
 lineFileClose(&lf);
+
+/* Connect up subtracks and parents.  Note this loop does not actually move tracks
+ * from list to parent subtracks, it just uses the field as a marker. Just do this
+ * so when doing error checking can distinguish between container tracks and others.
+ * This does have the pleasant side effect of making good error messages for
+ * non-existant parents. */
+struct trackDb *tdb;
+struct hash *hash = hashNew(0);
+for (tdb = tdbList; tdb != NULL; tdb = tdb->next)
+    hashAdd(hash, tdb->track, tdb);
+for (tdb = tdbList; tdb != NULL; tdb = tdb->next)
+    {
+    char *parentName = trackDbLocalSetting(tdb, "parent");
+    if (parentName != NULL)
+         {
+	 struct trackDb *parent = hashFindVal(hash, parentName);
+	 if (parent == NULL)
+	    errAbort("Parent %s of track %s doesn't exist in hub %s genome %s", parentName,
+		tdb->track, hub->url, genome->name);
+	 tdb->parent = parent;
+	 parent->subtracks = tdb;
+	 }
+    }
+hashFree(&hash);
+
+/* Loop through list checking tags and removing ad-hoc use of parent and subtracks tags. */
+for (tdb = tdbList; tdb != NULL; tdb = tdb->next)
+    {
+    checkTagsLegal(hub, genome, tdb);
+    tdb->parent = tdb->subtracks = NULL;
+    }
 return tdbList;
 }
 
+
+void hubCheckTrack(struct trackHub *hub, struct trackHubGenome *genome, struct trackDb *tdb)
+/* Make sure that track is ok. */
+{
+char *relativeUrl = trackDbSetting(tdb, "bigDataUrl");
+if (relativeUrl != NULL)
+    {
+    char *bigDataUrl = trackHubRelativeUrl(genome->trackDbFile, relativeUrl);
+    char *type = trackDbRequiredSetting(tdb, "type");
+    verbose(1, "checking %s.%s type %s at %s\n", genome->name, tdb->track, type, bigDataUrl);
+
+    if (startsWithWord("bigWig", type))
+	{
+	/* Just open and close to verify file exists and is correct type. */
+	struct bbiFile *bbi = bigWigFileOpen(bigDataUrl);
+	bbiFileClose(&bbi);
+	}
+    else if (startsWithWord("bigBed", type))
+	{
+	/* Just open and close to verify file exists and is correct type. */
+	struct bbiFile *bbi = bigBedFileOpen(bigDataUrl);
+	bbiFileClose(&bbi);
+	}
+    else if (startsWithWord("bam", type))
+	{
+	/* For bam files, the following call checks both main file and index. */
+	bamFileExists(bigDataUrl);
+	}
+    else
+	errAbort("unrecognized type %s in genome %s track %s", type, genome->name, tdb->track);
+    freez(&bigDataUrl);
+    }
+
+}
 
 void hubCheckGenome(struct trackHub *hub, struct trackHubGenome *genome)
 /* Check out genome within hub. */
 {
 struct trackDb *tdbList = trackHubTracksForGenome(hub, genome);
-uglyf("%d tracks in %s\n", slCount(tdbList), genome->genome);
+struct trackDb *tdb;
+for (tdb = tdbList; tdb != NULL; tdb = tdb->next)
+    hubCheckTrack(hub, genome, tdb);
+verbose(1, "%d tracks in %s\n", slCount(tdbList), genome->name);
 }
 
 void hubCheck(char *hubUrl)
 /* hubCheck - Check a track data hub for integrity. */
 {
 struct trackHub *hub = trackHubOpen(hubUrl);
-uglyf("hub %s\nshortLabel %s\n", hubUrl, hub->shortLabel);
-uglyf("%s has %d elements\n", hub->genomesFile, slCount(hub->genomeList));
+verbose(1, "hub %s\nshortLabel %s\nlongLabel %s\n", hubUrl, hub->shortLabel, hub->longLabel);
+verbose(1, "%s has %d elements\n", hub->genomesFile, slCount(hub->genomeList));
 struct trackHubGenome *genome;
 for (genome = hub->genomeList; genome != NULL; genome = genome->next)
     {
