9a6742a6bb8914efe0fd1c050bd5a9493bb0eaaa
Krishna Roskin <krish@soe.ucsc.edu>
2010-12-18 00:28:19 -0800

    fixed size bugs and makefile bug

--- src/utils/fastaToPsl/fastaToPsl.c
+++ src/utils/fastaToPsl/fastaToPsl.c
@@ -10,17 +10,13 @@
 #include "sqlNum.h"
 #include "obscure.h"
 
-static char const rcsid[] = "$Id: spideyToPsl.c,v 1.4 2008/02/21 02:13:24 markd Exp $";
-
 void usage()
 /* Explain usage and exit. */
 {
 errAbort(
   "fastaToPsl - Convert FASTA pair alignments to PSL format\n"
   "usage:\n"
-  "   fastaToPsl [options] in.fa out.psl\n"
-  "\n"
-  "where in.fa is the FASTA pair alignment. \n"
+  "   fastaToPsl [options] in.fasta out.psl\n"
   );
 }
 
@@ -42,32 +38,45 @@ struct psl* pslAlign;
 
 DNA *qSeq;
 int qSize;
+int qSeqLen;
 char *qHeader;
 
 DNA *tSeq;
 int tSize;
+int tSeqLen;
 char *tHeader;
 
 inLF  = lineFileOpen(inName, TRUE);
 
 /* read the query sequence */
 read = faMixedSpeedReadNext(inLF, &tmpSeq, &qSize, &tmpHeader);
-if (!read) errAbort("Could not read query FASTA entry.");
+if (!read)
+    errAbort("Could not read query FASTA entry.");
 qSeq    = cloneString(tmpSeq);
+qSeqLen = countNonDash(qSeq, qSize);
 qHeader = cloneString(tmpHeader);
+verbose(2, "Query sequence header: %s\n", qHeader);
+verbose(3, "Query sequence alignment length: %d\n", qSize);
+verbose(3, "Query sequence length: %d\n", qSeqLen);
 
 /* read the target sequence */
 read = faMixedSpeedReadNext(inLF, &tmpSeq, &tSize, &tmpHeader);
-if (!read) errAbort("Could not read target FASTA entry.");
+if (!read)
+    errAbort("Could not read target FASTA entry.");
 tSeq    = cloneString(tmpSeq);
+tSeqLen = countNonDash(tSeq, tSize);
 tHeader = cloneString(tmpHeader);
+verbose(2, "Target sequence header: %s\n", tHeader);
+verbose(3, "Target sequence alignment length: %d\n", tSize);
+verbose(3, "Target sequence length: %d\n", tSeqLen);
 
 /* try to read next sequence to see if there is one */
 read = faMixedSpeedReadNext(inLF, &tmpSeq, &tmpSize, &tmpHeader);
-if (read) warn("WARN: Found more than two FASTA entries, only processing the first two.");
+if (read)
+    verbose(0, "Found more than two FASTA entries, only processing the first two.\n");
 
-pslAlign = pslFromAlign(qHeader, countNonDash(qSeq, qSize), 0, qSize, qSeq,
-                        tHeader, countNonDash(tSeq, tSize), 0, tSize, tSeq,
+pslAlign = pslFromAlign(qHeader, qSeqLen, 0, qSeqLen, qSeq,
+                        tHeader, tSeqLen, 0, tSeqLen, tSeq,
                         "+", 0);
 pslWriteAll(pslAlign, outName, TRUE);
 
