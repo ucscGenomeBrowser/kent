88874df06e5134a06ad82cd75a8e965d8914e33f
Morgan Maddren <mmaddren@soe.ucsc.edu>
2011-01-25 16:21:18 -0800

    updated python scripts

--- python/ucscgenomics/rafile/test.py
+++ python/ucscgenomics/rafile/test.py
@@ -1,6 +1,7 @@
 import sys
 import radict
 import read
+import write
 
 if len(sys.argv) != 3:
     print 'Usage Error. use python test.py file key'
@@ -9,4 +10,4 @@ if len(sys.argv) != 3:
 file = sys.argv[1]
 key = sys.argv[2]
 d = read.readRaFile(file, key)
-print d
+write.writeRaFile(d, 'valueC')
