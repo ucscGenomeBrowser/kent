a89c648b36bf7ae11587c2d83f375cb8431afb0a
Galt Barber <galt@soe.ucsc.edu>
2011-03-31 01:54:40 -0700

    adding progress bar showing filesize, progress, total time, and avg download rate in MB/sec.

--- src/utils/paraFetch/paraFetch.c
+++ src/utils/paraFetch/paraFetch.c
@@ -17,18 +17,20 @@ errAbort(
     "   outPath is optional. If not specified, it will attempt to parse URL to discover output filename.\n"
     "options:\n"
     "   -newer  only download a file if it is newer than the version we already have.\n"
+    "   -progress  Show progress of download.\n"
     );
 }
 
 static struct optionSpec options[] = {
    {"newer", OPTION_BOOLEAN},
+   {"progress", OPTION_BOOLEAN},
    {NULL, 0},
 };
 
-boolean paraFetch(int numConnections, int numRetries, char *url, char *outPath, boolean newer)
+boolean paraFetch(int numConnections, int numRetries, char *url, char *outPath, boolean newer, boolean progress)
 /* Fetch given URL, send to stdout. */
 {
-return parallelFetch(url, outPath, numConnections, numRetries, newer);
+return parallelFetch(url, outPath, numConnections, numRetries, newer, progress);
 }
 
 char *parseUrlForName(char *url)
@@ -56,7 +58,7 @@ if (argc == 4)
     outName = parseUrlForName(argv[3]);	
 else
     outName = argv[4];
-if (!paraFetch(atoi(argv[1]), atoi(argv[2]), argv[3], outName, optionExists("newer")))
+if (!paraFetch(atoi(argv[1]), atoi(argv[2]), argv[3], outName, optionExists("newer"), optionExists("progress")))
     exit(1);
 return 0;
 }
