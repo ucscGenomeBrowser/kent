27ec73c3b70ae870422736f244fd2eecb8691f08
Jim Kent <kent@soe.ucsc.edu>
2011-04-05 15:22:31 -0700

    Testing some correlation ideas with freen.

--- src/hg/oneShot/freen/freen.c
+++ src/hg/oneShot/freen/freen.c
@@ -7,8 +7,8 @@
 #include "obscure.h"
 #include "jksql.h"
 #include "trackDb.h"
-#include "hdb.h"
 #include "hui.h"
+#include "correlate.h"
 
 void usage()
 {
@@ -19,48 +19,12 @@ errAbort("freen - test some hairbrained thing.\n"
 void freen(char *track)
 /* Test some hair-brained thing. */
 {
-// struct trackDb *tdbList = hTrackDb("hg19");
-struct trackDb *tdbList = NULL;
-struct hash *hash = trackHashMakeWithComposites("hg19","chr1",&tdbList,FALSE);
-struct trackDb *tdb, *wgEncodeReg = NULL, *wgEncodeRegTxn = NULL;
-uglyf("Got %d items in tdbList\n", slCount(tdbList));
-uglyf("Got %d items in hash\n", hash->elCount);
-for (tdb = tdbList; tdb != NULL; tdb = tdb->next)
-    {
-    if (startsWith("wgEncodeReg", tdb->track))
-	{
-        printf("%s parent %p\n", tdb->track, tdb->parent);
-	if (sameString("wgEncodeRegTxn", tdb->track))
-	    {
-	    uglyf("Got you %s!  parent is %p with %d kids\n", tdb->track, tdb->parent,
-	    	slCount(tdb->parent->subtracks));
-	    wgEncodeRegTxn = tdb;
-	    wgEncodeReg = tdb->parent;
-	    }
-	}
-    }
-
-uglyf("wgEncodeReg has %d kids\n", slCount(wgEncodeReg->subtracks));
-struct trackDb *parentTdb = tdbForTrack("hg19", "wgEncodeReg", &tdbList);
-uglyf("parentTdb has %d kids\n", slCount(parentTdb->subtracks));
-uglyf("wgEncodeReg = %p, parentTdb=%p\n", wgEncodeReg, parentTdb);
-char *super = trackDbGetSupertrackName(wgEncodeRegTxn);
-uglyf("super = %s\n", super);
-tdbMarkAsSuperTrack(wgEncodeReg);
-trackDbSuperMemberSettings(wgEncodeRegTxn);
-uglyf("after more stuff wgEncodeReg has %d kids\n", slCount(wgEncodeReg->subtracks));
-hTrackDbLoadSuper("hg19", wgEncodeReg);
-uglyf("after hTrackDbLoadSuper, wgEncodeReg has %d kids\n", slCount(wgEncodeReg->subtracks));
-#ifdef SOON
-if (super != NULL)
-    {
-    uglyf("And for super %s\n", super->track);
-    for (tdb = super->subtracks; tdb != NULL; tdb = tdb->next)
-	 uglyf("child %s\n", tdb->track);
-    }
-else
-    uglyf("super = NULL\n");
-#endif /* SOON */
+struct correlate *c = correlateNew();
+int i;
+for (i=0; i<6; ++i)
+    correlateNext(c, i&1, 1-(i&1));
+correlateNext(c, 4, 4);
+printf("1 in 7 correlation is %g\n", correlateResult(c));
 }
 
 int main(int argc, char *argv[])
