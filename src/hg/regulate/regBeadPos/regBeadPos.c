7d7b9d3af21465829b4e243898548dbfc30d8c42
Jim Kent <kent@soe.ucsc.edu>
2011-04-08 13:45:57 -0700

    Starting to work on a HMM that positions DNAse relative to a histone

---
+++ src/hg/regulate/regBeadPos/regBeadPos.c
@@ -0,0 +1,257 @@
+/* regBeadPos - Position regulatory beads along a chromosome string.  The beads are 
+ * nucleosomes, open regions and closed regions.
+ */
+#include "common.h"
+#include "linefile.h"
+#include "hash.h"
+#include "options.h"
+#include "bigWig.h"
+
+static char const rcsid[] = "$Id: newProg.c,v 1.30 2010/03/24 21:18:33 hiram Exp $";
+
+void usage()
+/* Explain usage and exit. */
+{
+errAbort(
+  "regBeadPos - Position regulatory beads along a chromosome string.  The beads\n"
+  "are nucleosomes, open regions and closed regions.\n"
+  "usage:\n"
+  "   regBeadPos in.tab outFile\n"
+  "options:\n"
+  "   -xxx=XXX\n"
+  );
+}
+
+static struct optionSpec options[] = {
+   {NULL, 0},
+};
+
+struct inFile
+/* Info about input files. */
+    {
+    struct inFile *next;
+    char *name;		/* Symbolic name used inside program - DNASE or HISTONE */
+    char *fileName;	/* Full path name. */
+    struct bbiFile *bigWig;			/* Open big wig */
+    struct bigWigValsOnChrom *chromVals;	/* Fast bigWig access */
+    double cuts[5];	/* Where we make the cuts on different levels */
+    };
+
+struct hash *makeInFilesHash(char *fileName)
+/* Read input and make hash out of it. */
+{
+struct lineFile *lf = lineFileOpen(fileName, TRUE);
+char *row[7];
+struct hash *hash = hashNew(0);
+while (lineFileRow(lf, row))
+    {
+    struct inFile *in;
+    AllocVar(in);
+    in->name = cloneString(row[0]);
+    in->fileName = cloneString(row[1]);
+    in->bigWig = bigWigFileOpen(in->fileName);
+    in->chromVals = bigWigValsOnChromNew();
+    int i;
+    for (i=0; i<ArraySize(in->cuts); ++i)
+        in->cuts[i] = lineFileNeedDouble(lf, row, i+2);
+    hashAdd(hash, in->name, in);
+    }
+return hash;
+}
+
+void *findInHashFromFile(char *key, struct hash *hash, char *fileName)
+/* Like hashMustFindVal, but with a better error message. */
+{
+void *val = hashFindVal(hash, key);
+if (val == NULL)
+    errAbort("Can't find %s in %s\n", key, fileName);
+return val;
+}
+
+inline UBYTE quant(struct inFile *inFile, double inVal)
+/* Quantize inVal into something small using inFile->cuts */
+{
+double *cuts = inFile->cuts;
+
+/* COuld do this if stack with a loop and a separate case for the
+ * last value, but actually would take as about as many lines of code and be 
+ * slower.  The code is not too slow because the first test handles 90% of
+ * the cases in any case.... */
+if (inVal < cuts[0])
+    return 0;
+else if (inVal < cuts[1])
+    return 1;
+else if (inVal < cuts[2])
+    return 2;
+else if (inVal < cuts[3])
+    return 3;
+else
+    return 4;
+}
+
+void makeInLetters(int chromSize, struct inFile *a, struct inFile *b, int basesInWindow, 
+	int *retLetterCount, UBYTE **retLetters)
+/* This does two things - averages over a small window, and then quantizes the
+ * result into one of a small number of values. The quantization is done in
+ * a and b separately, and the letter produced in the end is aBin * binCount + bBin */
+{
+int letterCount = chromSize/basesInWindow;
+UBYTE *letters;
+AllocArray(letters, letterCount);
+int i, startWin = 0;
+double *aVals = a->chromVals->valBuf;
+double *bVals = a->chromVals->valBuf;
+double invWinSize = 1.0/basesInWindow;
+for (i=0; i<letterCount; ++i)
+    {
+    int endWin = startWin + basesInWindow;
+    int j;
+    double aSum = 0, bSum = 0;
+    for (j=startWin; j<endWin; ++j)
+        {
+	aSum += aVals[j];
+	bSum += bVals[j];
+	}
+    double aMean = aSum*invWinSize;
+    double bMean = bSum*invWinSize;
+    UBYTE aQuant = quant(a, aMean), bQuant = quant(b, bMean);
+    UBYTE letter = aQuant * ArraySize(a->cuts) + bQuant;
+    letters[i] = letter;
+    startWin = endWin;
+    }
+*retLetterCount = letterCount;
+*retLetters = letters;
+}
+
+void dumpLetters(FILE *f, UBYTE *in, int count)
+/* Put out given number of letters (which we obtain by adding 'a' to the input) */
+{
+int i;
+for (i=0; i<count; ++i)
+    {
+    char c = in[i] + 'a';
+    fputc(c, f);
+    }
+}
+
+#define DNASE_LEVELS 5
+#define HISTONE_LEVELS 5
+#define HMM_LETTERS (DNASE_LEVELS * HISTONE_LEVELS)
+
+double *probsFromDnaseHistones(double *dnase, double *histones)
+{
+double *p;
+AllocArray(p, HMM_LETTERS);
+int i,j, ix=0;
+for (i=0; i<DNASE_LEVELS; ++i)
+    for (j=0; j<HISTONE_LEVELS; ++j)
+        {
+	p[ix] = dnase[i]*histones[j];
+	++ix;
+	}
+return p;
+}
+
+double *makeEmissionProbsForLo()
+{
+static double dnase[5] = {0.9, 0.08, 0.019, 0.001, 0};
+static double histone[5] = {0.8, 0.15, 0.04, 0.0099, 0.0001};
+return probsFromDnaseHistones(dnase, histone);
+}
+
+double *makeEmissionProbsForMed()
+{
+static double dnase[5] = {0.1, 0.2, 0.3, 0.2, 0.1};
+static double histone[5] = {0.1, 0.2, 0.3, 0.2, 0.1};
+return probsFromDnaseHistones(dnase, histone);
+}
+
+double *makeEmissionProbsForDnase()
+{
+static double dnase[5] = {0.05, 0.05, 0.1, 0.4, 0.4};
+static double histone[5] = {0.22, 0.23, 0.23, 0.22, 0.1};
+return probsFromDnaseHistones(dnase, histone);
+}
+
+double *makeEmissionProbsForHistones()
+{
+static double dnase[5] = {0.4, 0.25, 0.2, 0.1, 0.05};
+static double histone[5] = {0.05, 0.05, 0.1, 0.4, 0.4};
+return probsFromDnaseHistones(dnase, histone);
+}
+
+void dumpProb(FILE *f, double *p, char *name)
+{
+fprintf(f, "%s", name);
+int i, j, ix=0;
+for (i=0; i<5; ++i)
+    {
+    fprintf(f, "  ");
+    for (j=0; j<5; ++j)
+	{
+	fprintf(f, " %03d", round(p[ix]*1000));
+	++ix;
+	}
+    }
+fprintf(f, "\n");
+}
+
+void runHmmOnChrom(struct bbiChromInfo *chrom, struct inFile *dnaseIn, struct inFile *histoneIn, FILE *f)
+/* Do the HMM run on the one chromosome. */
+{
+int inLetterCount;
+UBYTE *inLetters;
+uglyf("Quantizing %s of size %d\n", chrom->name, chrom->size);
+makeInLetters(chrom->size, dnaseIn, histoneIn, 5, &inLetterCount, &inLetters);
+uglyf("Reduced to %d entities of 25 possible values\n", inLetterCount);
+int zeroCount = countLeadingChars((char *)inLetters, 0);
+if (zeroCount >= inLetterCount)
+    errAbort("Whaaa?  %d out of %d are zero?\n",  zeroCount, inLetterCount);
+uglyf("1st nonZero at %d of %d.  Next line shows next 100 chars\n", zeroCount, inLetterCount);
+dumpLetters(uglyOut, inLetters+zeroCount, 80);
+uglyf("\n");
+
+double *loEmitProb = makeEmissionProbsForLo();
+double *medEmitProb = makeEmissionProbsForMed();
+double *dnaseEmitProb = makeEmissionProbsForDnase();
+double *histoneEmitProb = makeEmissionProbsForHistones();
+dumpProb(uglyOut, loEmitProb, "lo");
+dumpProb(uglyOut, medEmitProb, "med");
+dumpProb(uglyOut, dnaseEmitProb, "dnase");
+dumpProb(uglyOut, histoneEmitProb, "histone");
+}
+
+
+void regBeadPos(char *inTab, char *outFile)
+/* regBeadPos - Position regulatory beads along a chromosome string.  The beads are nucleosomes, 
+ * open regions and closed regions.. */
+{
+struct hash *inHash = makeInFilesHash(inTab);
+uglyf("Read %d from %s\n", inHash->elCount, inTab);
+struct inFile *dnaseIn = findInHashFromFile("DNASE", inHash, inTab);
+struct inFile *histoneIn = findInHashFromFile("HISTONE", inHash, inTab);
+uglyf("%s and %s found\n", dnaseIn->name, histoneIn->name);
+FILE *f = mustOpen(outFile, "w");
+struct bbiChromInfo *chrom, *chromList = bbiChromList(dnaseIn->bigWig);
+verbose(2, "Processing %d chromosomes\n", slCount(chromList));
+for (chrom = chromList; chrom != NULL; chrom = chrom->next)
+    {
+    if (bigWigValsOnChromFetchData(dnaseIn->chromVals, chrom->name, dnaseIn->bigWig)
+    &&  bigWigValsOnChromFetchData(histoneIn->chromVals, chrom->name, histoneIn->bigWig))
+        {
+	uglyf("Got data\n");
+	runHmmOnChrom(chrom, dnaseIn, histoneIn, f);
+	}
+    }
+carefulClose(&f);
+}
+
+int main(int argc, char *argv[])
+/* Process command line. */
+{
+optionInit(&argc, argv, options);
+if (argc != 3)
+    usage();
+regBeadPos(argv[1], argv[2]);
+return 0;
+}
