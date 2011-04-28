9800980414cee0aef48710ef5162e386743150c3
Jim Kent <kent@soe.ucsc.edu>
2011-04-28 16:49:57 -0700

    Fixing Katrina's 7) Rainbow tracks: when Display mode is set to 'dense' and the Overlay method is set to 'none,' in hgTracks the labels are shifted down, resulting in partially cutting-off the bottom-most label.

--- src/hg/hgTracks/multiWig.c
+++ src/hg/hgTracks/multiWig.c
@@ -206,9 +206,19 @@ else
 	if (isSubtrackVisible(subtrack))
 	    {
 	    int height = subtrack->totalHeight(subtrack, vis);
-	    wigLeftAxisLabels(subtrack, seqStart, seqEnd, hvg, xOff, y, width, height, withCenterLabels,
-	    	font, subtrack->ixColor, vis, subtrack->shortLabel, subtrack->graphUpperLimit,
-		subtrack->graphLowerLimit, TRUE);
+	    if (vis == tvDense)
+	        {
+		/* Avoid wigLeftAxisLabels here because it will repeatedly add center label 
+		 * offsets, and in dense mode we will only draw the one label. */
+		hvGfxTextRight(hvg, xOff, y, width - 1, height, subtrack->ixColor, font, 
+			subtrack->shortLabel);
+		}
+	    else
+		{
+		wigLeftAxisLabels(subtrack, seqStart, seqEnd, hvg, xOff, y, width, height, 
+			withCenterLabels, font, subtrack->ixColor, vis, subtrack->shortLabel, 
+			subtrack->graphUpperLimit, subtrack->graphLowerLimit, TRUE);
+		}
 	    y += height+1;
 	    }
 	}
