c78a732374992ad77a8508bd0bf1ab6316bf274c
Tim Dreszer <tdreszer@soe.ucsc.edu>
2011-05-06 17:31:18 -0700

    Another tweek to hgFileSearch for spacing issues due to paragraph tags.

--- src/hg/hgFileSearch/hgFileSearch.c
+++ src/hg/hgFileSearch/hgFileSearch.c
@@ -408,10 +408,10 @@ if(doSearch)
 hFreeConn(&conn);
 
 webNewSection("About " FILE_SEARCH_NAME);
-printf("<p>Search for downloadable ENCODE files by entering search terms in "
+printf("Search for downloadable ENCODE files by entering search terms in "
         "the Track name or Description fields and/or by making selections with "
         "the group, data format, and/or ENCODE metadata drop-downs.");
-printf("<BR><a target='_blank' href='../goldenPath/help/fileSearch.html'>more help</a></p>\n");
+printf("<BR><a target='_blank' href='../goldenPath/help/fileSearch.html'>more help</a>\n");
 webEndSectionTables();
 }
 
