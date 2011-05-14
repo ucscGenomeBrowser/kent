d27a1d82e0f1124717a7c948281dba8d4a43bc1c
Tim Dreszer <tdreszer@soe.ucsc.edu>
2011-05-13 17:35:21 -0700

    The image size/centering problem was solved on IE>=8 but there were sizing issues on Mac FF, so I am generalizing the code to all browsers other than IE<8.  There is still an issue with the purple box in the left frame showing the portion of the image currently seen in the right frame.  This is in FF and Chrome but not IE.  All this is for redmine 3933

--- src/hg/visiGene/hgVisiGene/hgVisiGene.c
+++ src/hg/visiGene/hgVisiGene/hgVisiGene.c
@@ -346,11 +346,15 @@ if (imageId != 0)
 #endif
     jsIncludeFile("jquery.js", NULL);
     char *browserVersion;
-    if (btIE == cgiClientBrowser(&browserVersion, NULL, NULL) && *browserVersion >= '8')
+    if (btIE == cgiClientBrowser(&browserVersion, NULL, NULL) && *browserVersion < '8') // IE<8 is DTD 3.2
+        printf("<IFRAME name='bigImg' width='100%%' height='90%%' SRC='%s'></IFRAME><BR>\n", buf);
+    else
         {
         // IE using DTD 4.01 Transitional (IE>=8) has problem with setting iFRAME height.
         // To solve it, height is set large (800px), then dynamically changed in javascript at load time.
-        printf("<IFRAME name='bigImg' width='100%%' height='800px' SRC='%s'></IFRAME><BR>\n", buf);
+        // A similar problem was seen on Mac FF, so I am using the same code for all browsers.
+        //printf("<IFRAME name='bigImg' width='100%%' height='800px' SRC='%s'></IFRAME><BR>\n", buf);
+        printf("<IFRAME name='bigImg' width='100%%' height='800px'></IFRAME><BR>\n");
         printf("<script type='text/javascript'>$(document).ready( function () {;\n");
         printf("   $('[name=\"bigImg\"]').css('height',($(window).height()*0.85) + 'px');\n");
         printf("   $('[name=\"bigImg\"]').attr('src','%s');\n",buf);
@@ -361,8 +365,6 @@ if (imageId != 0)
         printf("});\n");
         printf("</script>\n");
         }
-    else // Other browsers have no problem with IFRAME height
-        printf("<IFRAME name='bigImg' width='100%%' height='90%%' SRC='%s'></IFRAME><BR>\n", buf);
 
     fullCaption(conn, imageId);
 
