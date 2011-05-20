7b077f49971bda909d2a3d3758b30f54cfa2344f
Tim Dreszer <tdreszer@soe.ucsc.edu>
2011-05-20 11:40:43 -0700

    Made -redirect an option not the default.  Made -db=hg19 the default.  Reorganized usage page to bring most used options to the top.

--- src/hg/encode/encodeDownloadsPage/encodeDownloadsPage.pl
+++ src/hg/encode/encodeDownloadsPage/encodeDownloadsPage.pl
@@ -31,6 +31,7 @@ use vars qw/
     $opt_db
     $opt_verbose
     $opt_checksum
+    $opt_redirect
     $opt_noMdb
     $opt_mdb
     $opt_sortable
@@ -46,15 +47,17 @@ usage: encodeDownloadsPage.pl {index.html} [downloads-dir]
 Creates an HTML page and README text file listing the downloads in the current directory or optional directory.
 
 options:
-    -help               Displays this usage info
-    -checksum           Generate checksum file
-    -db=hg18            Use a database other than the default hg18 (For aquiring releaseDate and metadata from trackDb)
-    -fileType=mask          mask for file types included (default '*.gz')
-    -mdb=tableName      Use an explicit mdb table (e.g. 'mdb_braney') and don't fall back on trackDb/fileDb.ra methods
-    -noMdb              Don't use an 'mdb' metadata table.  Instead, use the old trackDb/fileDb.ra methods
-    -preamble=file      File containing introductory information (written in HTML) that will be included in this file (default preamble.html)
-    -sortable           The HTML output will include jquery and utils.js javascript to allow sorting of the table (TESTING FEATURE)
+    -db=hg19            Use a database other than the default hg19 (For aquiring releaseDate and metadata from trackDb).
+    -redirect           HTML page will redirect to hgFileUi but only if 'fileSortOrder' exists in trackDb.
+    -sortable           The HTML output will include jquery and utils.js javascript to allow sorting of the table (TESTING FEATURE).
+    -checksum           Generate checksum file.
+    -mdb=tableName      Use an explicit mdb table (e.g. 'mdb_braney') and don't fall back on trackDb/fileDb.ra methods.
+    -preamble=file      File containing introductory information (in HTML) that will be included in this file (default preamble.html).
+    -help               Displays this usage info.
     -verbose=num        Set verbose level to num (default 1).
+  Historical options:
+    -noMdb              Don't use an 'mdb' metadata table.  Instead, use the old trackDb/fileDb.ra methods.
+    -fileType=mask      mask for file types included (default '*.gz').
 
 END
 exit 1;
@@ -151,12 +154,12 @@ sub htmlRedirectPage {
                 print OUT_FILE "<META http-equiv='Content-Script-Type' content='text/javascript'>\n";
                 print OUT_FILE "<META HTTP-EQUIV='Pragma' CONTENT='no-cache'>\n";
                 print OUT_FILE "<META HTTP-EQUIV='Expires' CONTENT='-1'>\n";
-                print OUT_FILE "<META http-equiv='refresh' content='0; URL=/cgi-bin/hgFileUi?db=$database&g=$composite'>\n";
+                print OUT_FILE "<META http-equiv='refresh' content='0; URL=http:///<!--#echo var=\"BROWSER_HOST\" -->//cgi-bin/hgFileUi?db=$database&g=$composite'>\n";
                 print OUT_FILE "<TITLE>Redirecting...</TITLE>\n";
                 print OUT_FILE "<LINK rel='STYLESHEET' href='../style/HGStyle-1304985632.css' TYPE='text/css' />\n";
                 print OUT_FILE "</HEAD>\n\n";
-                print OUT_FILE "<body onload=\"window.location.href='/cgi-bin/hgFileUi?db=$database&g=$composite';\" BGCOLOR='#FFF9D2' LINK='#0000CC' VLINK='#330066' ALINK='#6600FF' text='#000000'>\n";
-                print OUT_FILE "<p>Loading <a href='/cgi-bin/hgFileUi?db=$database&g=$composite'>redirection target</a></p>\n";
+                print OUT_FILE "<body onload=\"window.location.href='http:///<!--#echo var=\"BROWSER_HOST\" -->//cgi-bin/hgFileUi?db=$database&g=$composite';\" BGCOLOR='#FFF9D2' LINK='#0000CC' VLINK='#330066' ALINK='#6600FF' text='#000000'>\n";
+                print OUT_FILE "<p>Loading <a href='http:///<!--#echo var=\"BROWSER_HOST\" -->//cgi-bin/hgFileUi?db=$database&g=$composite'>redirection target</a></p>\n";
                 print OUT_FILE "<p>In approx. 1 second the redirection target page should load.<br>\n";
                 print OUT_FILE "If it doesn't please select the link above.</p>\n";
                 print OUT_FILE "</BODY>\n";
@@ -203,12 +206,12 @@ sub sortableHtmlRow {
     $row .= "|";
 
     if($releaseDate && length($releaseDate) > 0) {
-        $row .= "<TR valign='top'><TD align='left'>&nbsp;&nbsp;$releaseDate";
+        $row .= "<TR valign='top'><TD align='left' nowrap>&nbsp;&nbsp;$releaseDate";
     } else {
         $row .= "<TR valign='top'><TD align='left'>&nbsp;";
     }
     $row .= "<TD><A type='application/zip' target='_blank' HREF=\"$file\">$file</A>";
-    $row .= "<TD align='right'>&nbsp;&nbsp;$size<TD>&nbsp;&nbsp;$date&nbsp;&nbsp;";
+    $row .= "<TD align='right' nowrap>&nbsp;&nbsp;$size<TD nowrap>&nbsp;&nbsp;$date&nbsp;&nbsp;";
     if($metaData && length($metaData) > 0) {
         $row .= "<TD nowrap>$metaData";
         # Find cell,dataType,antibody,lab,type,subId
@@ -227,12 +230,12 @@ sub sortableHtmlRowWithColumns {
     $row .= "|";
 
     if($releaseDate && length($releaseDate) > 0) {
-        $row .= "<TR valign='top'><TD align='left'>&nbsp;&nbsp;$releaseDate</TD>";
+        $row .= "<TR valign='top'><TD align='left' nowrap>&nbsp;&nbsp;$releaseDate</TD>";
     } else {
         $row .= "<TR valign='top'><TD align='left'>&nbsp;</TD>";
     }
     $row .= "<TD><A type='application/zip' target='_blank' HREF=\"$file\">$file</A></TD>";
-    $row .= "<TD align='right'>&nbsp;&nbsp;$size<TD>&nbsp;&nbsp;$date&nbsp;&nbsp;</TD>";
+    $row .= "<TD align='right' nowrap>&nbsp;&nbsp;$size<TD nowrap>&nbsp;&nbsp;$date&nbsp;&nbsp;</TD>";
     my $cix = 0;
     while($colListVals[$cix]) {
         if($colListVals[$cix] ne "") {
@@ -423,6 +426,7 @@ sub metadataArraysJoin {
 my $now = time();
 
 my $ok = GetOptions("checksum",
+                    "redirect",
                     "fileType=s",
                     "db=s",
                     "mdb=s",
@@ -459,7 +463,7 @@ if(length(@fileList) == 0) {
 my @files;      # Stripped down to filenames, for checksumming
 
 # TODO determine whether we should even look this up
-$opt_db = "hg18" if(!defined $opt_db);
+$opt_db = "hg19" if(!defined $opt_db);
 my $db = HgDb->new(DB => $opt_db);
 
 open( OUT_FILE, "> $downloadsDir/$indexHtml") || die "SYS ERROR: Can't write to \'$downloadsDir/$indexHtml\' file; error: $!\n";
@@ -493,7 +497,7 @@ for my $line (@fileList) {
     my $tableName = "";
     my $dataType  = "";
     ### Special case for Elnitski BiPs !!!
-    my $database = "hg18"; # default
+    my $database = "hg19"; # default
     if ( ($fileName =~ tr/\.//) == 3 ) {  # tableName.db.dataType.gz
         ($tableName,$database,$dataType) = split('\.', $fileName);   # tableName I hope
         if($database ne "panTro2" && $database ne "mm9" && $database ne "canFam2"
@@ -737,7 +741,7 @@ for my $line (@fileList) {
 }
 
 # Redirect if the CGI hgFileUi should be displayed
-if (htmlRedirectPage(*OUT_FILE,$db,$opt_db,$composite) == 0) {
+if (!defined $opt_redirect || htmlRedirectPage(*OUT_FILE,$db,$opt_db,$composite) == 0) {
     # Can't redirect to the hgFileUi CGI so make a page here
 
     # Start the page
