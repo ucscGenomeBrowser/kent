99b7e610ebc351cd20322de060df967e7d021e53
Brian Raney <braney@soe.ucsc.edu>
2011-07-06 14:44:17 -0700

    change some table names to correlate with reality #4502

--- src/hg/hgTracks/simpleTracks.c
+++ src/hg/hgTracks/simpleTracks.c
@@ -4304,7 +4304,6 @@ if (trackDbSetting(tg->tdb, "wgEncodeGencodeVersion") != NULL)
     {
     if (startsWith("wgEncodeGencodeBasic", tg->tdb->track)
         || startsWith("wgEncodeGencodeComp", tg->tdb->track)
-        || startsWith("wgEncodeGencodeFull", tg->tdb->track)
         || startsWith("wgEncodeGencode2wayConsPseudo", tg->tdb->track)
         || startsWith("wgEncodeGencodePseudoGene", tg->tdb->track))
         dyQuery = gencodeFilterBySetQuery(tg, filterBySet, lf);
@@ -12788,7 +12787,7 @@ registerTrackHandlerOnFamily("wgEncodeGencode", gencodeGeneMethods);
 registerTrackHandlerOnFamily("wgEncodeSangerGencode", gencodeGeneMethods);
 registerTrackHandler("wgEncodeGencodeV7", gencodeGeneMethods);
 registerTrackHandler("wgEncodeGencodeBasicV7", gencodeGeneMethods);
-registerTrackHandler("wgEncodeGencodeFullV7", gencodeGeneMethods);
+registerTrackHandler("wgEncodeGencodeCompV7", gencodeGeneMethods);
 registerTrackHandler("wgEncodeGencodePseudoGeneV7", gencodeGeneMethods);
 registerTrackHandler("wgEncodeGencode2wayConsPseudoV7", gencodeGeneMethods);
 registerTrackHandler("wgEncodeGencodePolyaV7", gencodeGeneMethods);
