72e1915f66f71c3a94a5eb1911521513915fadc9
Jim Kent <kent@soe.ucsc.edu>
2011-08-03 12:38:21 -0700

    Starting to implement cartSimulator()

--- src/hg/oneShot/cartSim/cartSim.c
+++ src/hg/oneShot/cartSim/cartSim.c
@@ -14,6 +14,8 @@ int userCount = 10;
 int randSeed = 0;
 int cgiDelay = 100;
 int hitDelay = 100;
+int iterations = 100;
+double newRatio = 0.1;
 
 void usage()
 /* Explain usage and exit. */
@@ -27,10 +29,12 @@ errAbort(
   "   -randSeed=N random number generator seed.  Defaults to pid\n"
   "   -cgiDelay=N number of milliseconds to pretend cgi is crunching. Default is %d\n"
   "   -hitDelay=N number of milliseconds to delay between hits. Default is %d\n"
+  "   -iterations=N number of iterations to hit cart.  Default is %d\n"
+  "   -newRatio=0.N proportion of hits that are from new users. Default is %g\n"
   "   -verbose=N level of diagnostic output verbosity.  Default is 1.\n"
   "   -create=N create database if it doesn't exist with given number of dummy records\n"
   "   -clone=source clone existing database\n"
-  , userCount, cgiDelay, hitDelay
+  , userCount, cgiDelay, hitDelay, iterations, newRatio
   );
 }
 
@@ -41,6 +45,7 @@ static struct optionSpec options[] = {
    {"hitDelay", OPTION_INT},
    {"create", OPTION_INT},
    {"clone", OPTION_STRING},
+   {"newRatio", OPTION_DOUBLE},
    {NULL, 0},
 };
 
@@ -274,9 +279,114 @@ fakeCloneOldTable(oldConn, newConn, userTable);
 fakeCloneOldTable(oldConn, newConn, sessionTable);
 }
 
+int *getSomeInts(struct sqlConnection *conn, char *table, char *field, int limit)
+/* Return an array of ints from field in table. */
+{
+int *result, i;
+char query[512];
+safef(query, sizeof(query), "select %s from %s limit %d", field, table, limit);
+struct sqlResult *sr = sqlGetResult(conn, query);
+AllocArray(result, limit);
+for (i=0; i<limit; ++i)
+    {
+    char **row = sqlNextRow(sr);
+    if (row == NULL)
+        errAbort("Less than %d rows in %s", limit, table);
+    result[i] = sqlSigned(row[0]);
+    }
+sqlFreeResult(&sr);
+return result;
+}
+
 void cartSimulate(char *host, char *user, char *password, char *database)
 /* Simulate action of various UCSC Genome Browser CGIs on cart. */
 {
+/* Figure out size of tables. */
+struct sqlConnection *conn = sqlConnectRemote(host, user, password, database);
+int userDbSize = sqlQuickNum(conn, "select count(*) from userDb");
+int sessionDbSize = sqlQuickNum(conn, "select count(*) from sessionDb");
+int sampleSize = min(userDbSize, sessionDbSize);
+int maxSampleSize = 1024*1024;
+sampleSize = min(sampleSize, maxSampleSize);
+verbose(2, "# userDb has %d rows,  sessionDb has %d rows, sampling %d\n"
+	, userDbSize, sessionDbSize, sampleSize);
+
+/* Get sample of user id's. */
+uglyTime(NULL);
+int *userIds = getSomeInts(conn, "userDb", "id", sampleSize);
+int *sessionIds = getSomeInts(conn, "sessionDb", "id", sampleSize);
+uglyTime("Got user and session IDs");
+
+/* Get userCount random indexes. */
+int *randomIxArray, ix;
+AllocArray(randomIxArray, userCount);
+verbose(2, "random user ix:\n");
+for (ix=0; ix<userCount; ++ix)
+    {
+    randomIxArray[ix] = rand() % sampleSize;
+    verbose(2, "%d ", randomIxArray[ix]);
+    }
+verbose(2, "\n");
+
+sqlDisconnect(&conn);
+
+int iteration = 0;
+int querySize = 1024*1024*16;
+char *query = needLargeMem(querySize);
+for (;;)
+    {
+    for (ix = 0; ix < userCount; ++ix)
+	{
+	// int randomIx = randomIxArray[ix];
+	int randomIx = rand()%sampleSize;
+	long startTime = clock1000();
+	struct sqlConnection *conn = sqlConnectRemote(host, user, password, database);
+	long connectTime = clock1000();
+	struct dyString *contents = fakeCart(randomFakeSize());
+
+
+	safef(query, querySize, "select contents from userDb where id=%d", 
+		userIds[randomIx]);
+	char *userContents = sqlQuickString(conn, query);
+	long userReadTime = clock1000();
+
+	safef(query, querySize, "select contents from sessionDb where id=%d", 
+		sessionIds[randomIx]);
+	char *sessionContents = sqlQuickString(conn, query);
+	long sessionReadTime = clock1000();
+
+	safef(query, querySize, "update userDb set contents='%s' where id=%d",
+		contents->string, userIds[randomIx]);
+	sqlUpdate(conn, query);
+	long userWriteTime = clock1000();
+
+	safef(query, querySize, "update sessionDb set contents='%s' where id=%d",
+		contents->string, sessionIds[randomIx]);
+	sqlUpdate(conn, query);
+	long sessionWriteTime = clock1000();
+
+	sqlDisconnect(&conn);
+	long disconnectTime = clock1000();
+
+	printf("%ld total, %ld size, %ld connect, %ld userRead, %ld sessionRead, %ld userWrite, %ld sessionWrite\n",
+		disconnectTime - startTime,
+		(long) strlen(userContents) + strlen(sessionContents),
+		connectTime - startTime,
+		userReadTime - connectTime,
+		sessionReadTime - userReadTime,
+		userWriteTime - sessionReadTime,
+		sessionWriteTime - userReadTime);
+
+	dyStringFree(&contents);
+	freez(&userContents);
+	freez(&sessionContents);
+
+	sleep1000(hitDelay);
+	if (++iteration >= iterations)
+	    return;
+	}
+    }
+
 errAbort("cartSimulate(%s %s %s %s) not implemented", host, user, password, database);
 }
 
@@ -317,6 +427,8 @@ randSeed = optionInt("randSeed", getpid());
 srand(randSeed);
 cgiDelay = optionInt("cgiDelay", cgiDelay);
 hitDelay = optionInt("hitDelay", hitDelay);
+iterations = optionInt("iterations", iterations);
+newRatio = optionDouble("newRatio", newRatio);
 cartSim(argv[1], argv[2], argv[3], argv[4]);
 return 0;
 }
