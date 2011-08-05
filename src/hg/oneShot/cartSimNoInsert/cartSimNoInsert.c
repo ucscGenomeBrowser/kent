f281b592b2d1eda4261f1ac33c6e98e9b1ed5635
Jim Kent <kent@soe.ucsc.edu>
2011-08-05 16:52:42 -0700

    Removing stray function prototype.

--- src/hg/oneShot/cartSimNoInsert/cartSimNoInsert.c
+++ src/hg/oneShot/cartSimNoInsert/cartSimNoInsert.c
@@ -13,11 +13,6 @@ static char const rcsid[] = "$Id: newProg.c,v 1.30 2010/03/24 21:18:33 hiram Exp
 int userCount = 10;
 int randSeed = 0;
 
-struct sqlConnection *sqlConnectRemote(char *host, char *user, char *password,
-                                       char *database);
-/* Connect to database somewhere as somebody. Database maybe NULL to
- * just connect to the server. Abort on error. */
-
 void usage()
 /* Explain usage and exit. */
 {
