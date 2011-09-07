f82b0215a6ef075cba243190795272bf06bad904
Kate Rosenbloom <kate@soe.ucsc.edu>
2011-09-07 13:46:04 -0700

    Prune composites

--- src/hg/encode/encodeExp/listAccessionsHg19.sh
+++ src/hg/encode/encodeExp/listAccessionsHg19.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-mdbQuery "select fileName, dccAccession from hg19" | sed -e 's/^.*fileName=//' -e 's/; dccAccession=/      /' -e 's/;//' | sort 
+mdbQuery "select fileName, dccAccession from hg19" | sed -e 's/^.*fileName=//' -e 's/; dccAccession=/      /' -e 's/;//' | grep '\.' | sort 
