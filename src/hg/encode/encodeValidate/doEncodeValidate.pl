fec0485380bac58141ec61cf7cbe437f94e1b190
Cricket Sloan <cricket@soe.ucsc.edu>
2011-10-28 08:08:40 -0700

    I do not think this line should have been commented out

--- src/hg/encode/encodeValidate/doEncodeValidate.pl
+++ src/hg/encode/encodeValidate/doEncodeValidate.pl
@@ -2133,7 +2133,7 @@ foreach my $ddfLine (@ddfLines) {
                 $cvTypeVar = "control";
             }
             #print "term = $terms\n"
-            #$subGroups .= " $groupVar=$terms{$cvTypeVar}->{$hash{$var}}->{'tag'}";
+            $subGroups .= " $groupVar=$terms{$cvTypeVar}->{$hash{$var}}->{'tag'}";
         }
         #Venkat: Commented out the below line such that if any lab has replicates the replicate number will be placed
         # in the table name. The below code was found to be to specific, however if there are any problems
