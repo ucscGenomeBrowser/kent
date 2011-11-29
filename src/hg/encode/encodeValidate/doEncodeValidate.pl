54ae3e9bdc0674fbe39f8cfaf90564543fdc6787
Matt Wong <wong@soe.ucsc.edu>
2011-11-29 14:35:24 -0800

    reworked the validateBed subroutine

--- src/hg/encode/encodeValidate/doEncodeValidate.pl
+++ src/hg/encode/encodeValidate/doEncodeValidate.pl
@@ -535,43 +535,77 @@ sub validateBed {
     my $lineNumber = 0;
     doTime("beginning validateBed") if $opt_timing;
     my $fh = Encode::openUtil($file, $path);
+    my @localerrors;
+    my $errorCount = 0;
+    my $errorLimit = 20;
+    my $oldError = 0;
     while(<$fh>) {
-        chomp;
+        my $line = $_;
+        chomp $_;
+        $line =~ s/\s+$//g;
         $lineNumber++;
+        if (scalar(@localerrors) != $oldError) {
+            $oldError = scalar(@localerrors);
+            $errorCount++;
+        }
+        if ($errorCount >= $errorLimit) {
+
+            last
+        }
         next if m/^#/; # allow comment lines, consistent with lineFile and hgLoadBed
-        my @fields = split /\s+/;
-        my $fieldCount = @fields;
+        my @fields = split /\t/, $line;
+        my $fieldCount = scalar(@fields);
         next if(!$fieldCount);
-        my $prefix = "Failed bed validation, file '$file'; line $lineNumber:";
-        if(/^(track|browser)/) {
-            ;
-        } elsif($fieldCount < 3) {
-            die "$prefix not enough fields; " . scalar(@fields) . " present; at least 3 are required\n";
-        } elsif (!$chromInfo{$fields[0]}) {
-            die "$prefix field 1 value ($fields[0]) is invalid; not a valid chrom name\n";
-        } elsif ($fields[1] !~ /^\d+$/) {
-            die "$prefix field 2 value ($fields[1]) is invalid; value must be a positive number\n";
-        } elsif ($fields[2] !~ /^\d+$/) {
-            die "$prefix field 3 value ($fields[2]) is invalid; value must be a positive number\n";
-        } elsif ($fields[2] < $fields[1]) {
-            die "$prefix field 3 value ($fields[2]) is less than field 2 value ($fields[1])\n";
-        } elsif ($fieldCount < 3 && $fields[4] !~ /^\d+$/ && $fields[4] !~ /^\d+\.\d+$/) {
-            die "$prefix field 5 value ($fields[4]) is invalid; value must be a positive number\n";
-        } elsif ($fieldCount < 3 && $fields[4] < 0 || $fields[4] > 1000) {
-            die "$prefix field 5 value ($fields[4]) is invalid; score must be 0-1000\n";
-        } elsif ($type eq 'bed5FloatScore' && $fieldCount < 6) {
-            die "$prefix field 6 invalid; bed5FloatScore requires 6 fields";
-        } elsif ($type eq 'bed5FloatScore' && $fields[5] !~ /^$floatRegEx$/) {
-            die "$prefix field 6 value '$fields[5]' is invalid; must be a float\n";
-        } else {
-            ;
+        my $prefix = "line $lineNumber:";
+        if($fieldCount < 3) {
+            push @localerrors, "$prefix not enough fields; " . scalar(@fields) . " present; at least 3 are required\n";
+            next
+        }
+        if ($type ne 'bed5FloatScore' and !$fields[3]) {
+            push @localerrors, "$prefix field 4 must contain a value\n";
+            next
+        }
+        if (!$chromInfo{$fields[0]}) {
+            push @localerrors, "$prefix field 1 value ($fields[0]) is invalid; not a valid chrom name\n";
+        } 
+        if ($fields[1] !~ /^\d+$/) {
+            push @localerrors, "$prefix field 2 value ($fields[1]) is invalid; value must be a positive integer\n";
+        }
+        if ($fields[2] !~ /^\d+$/) {
+            push @localerrors, "$prefix field 3 value ($fields[2]) is invalid; value must be a positive integer\n";
+        }
+        if ($fields[2] < $fields[1]) {
+            push @localerrors, "$prefix field 3 value ($fields[2]) is less than field 2 value ($fields[1])\n";
+        }
+        if ($fieldCount > 5 && $fields[4] !~ /^\d+$/ && $fields[4] =~ /^\d+\.\d+$/) {
+            push @localerrors, "$prefix field 5 value ($fields[4]) is invalid; value must be a positive integer\n";
+        }
+        if ($fieldCount > 5 && $fields[4] < 0 || $fields[4] > 1000) {
+            push @localerrors, "$prefix field 5 value ($fields[4]) is invalid; score must be 0-1000\n";
+        }
+        if ($type ne 'bed5FloatScore' && $fields[5] !~ m/^(\+|\-)$/) {
+            push @localerrors, "$prefix field 6 ($fields[5]) value must be either + or -\n";
+        }
+        if ($type eq 'bed5FloatScore' && $fieldCount < 6) {
+            push @localerrors, "$prefix field 6 invalid; bed5FloatScore requires 6 fields";
+            next
+        }
+        if ($type eq 'bed5FloatScore' && $fields[5] !~ /^$floatRegEx$/) {
+            push @localerrors, "$prefix field 6 value '$fields[5]' is invalid; must be a float\n";
+            next
         }
         last if($opt_quick && $lineNumber >= $quickCount);
     }
     $fh->close();
     HgAutomate::verbose(2, "File \'$file\' passed bed validation\n");
     doTime("done validateBed",$lineNumber) if $opt_timing;
-    return ();
+    if (scalar(@localerrors)) {
+        my $errstr = join "", @localerrors;
+        $errstr = "Failed BED validation in $file:\n" . $errstr;
+        return $errstr;
+    } else {
+        return ();
+    }
 }
 
 sub validateBedGraph {
