3bcfd933e0f4ffafebe2f1a539733d18dd8bd427
Brian Raney <braney@soe.ucsc.edu>
2011-12-01 09:15:00 -0800

    make it so it takes an argument that defines the table to use for hubPublic

--- src/hg/utils/hubPublicCheck/hubPublicCheck.c
+++ src/hg/utils/hubPublicCheck/hubPublicCheck.c
@@ -19,11 +19,10 @@ errAbort(
   "hubPublicCheck - checks that the labels in hubPublic match what is in the hub labels\n"
   "   outputs SQL statements to put the table into compliance\n"
   "usage:\n"
-  "   hubPublicCheck \n"
+  "   hubPublicCheck tableName \n"
   "options:\n"
   "   -udcDir=/dir/to/cache - place to put cache for remote bigBed/bigWigs\n"
-  "   -addHub=url           - output statments to add url to %s table\n"
-  , hubPublicTableName
+  "   -addHub=url           - output statments to add url to table\n"
   );
 }
 
@@ -33,13 +32,13 @@ static struct optionSpec options[] = {
    {NULL, 0},
 };
 
-int hubPublicCheck()
+int hubPublicCheck(char *table)
 /* hubPublicCheck - checks that the labels in hubPublic match what is in the hub labels. */
 {
 struct sqlConnection *conn = hConnectCentral();
 char query[512];
 safef(query, sizeof(query), "select hubUrl, shortLabel,longLabel from %s", 
-	hubPublicTableName); 
+	table); 
 struct sqlResult *sr = sqlGetResult(conn, query);
 char **row;
 int differences = 0;
@@ -70,19 +69,19 @@ while ((row = sqlNextRow(sr)) != NULL)
 	{
 	differences++;
 
-	printf("update %s set shortLabel=\"%s\" where hubUrl=\"%s\";\n",hubPublicTableName, tHub->shortLabel, url);
+	printf("update %s set shortLabel=\"%s\" where hubUrl=\"%s\";\n",table, tHub->shortLabel, url);
 	}
     if (!sameString(longLabel, tHub->longLabel))
 	{
 	differences++;
 
-	printf("update %s set longLabel=\"%s\" where hubUrl=\"%s\";\n",hubPublicTableName, tHub->longLabel, url);
+	printf("update %s set longLabel=\"%s\" where hubUrl=\"%s\";\n",table, tHub->longLabel, url);
 	}
     }
 return differences;
 }
 
-int hubPublicAdd(char *url)
+int hubPublicAdd(char *table, char *url)
 /* hubPublicAdd -- add url to hubPublic table */
 {
 struct errCatch *errCatch = errCatchNew();
@@ -114,7 +113,7 @@ while ((hel = hashNext(&cookie)) != NULL)
     }
 
 printf("insert into %s (hubUrl,shortLabel,longLabel,registrationTime,dbCount,dbList) values (\"%s\",\"%s\", \"%s\",now(),%d, \"%s\");\n",
-    hubPublicTableName, url, tHub->shortLabel, tHub->shortLabel, dbCount, dy->string); 
+    table, url, tHub->shortLabel, tHub->shortLabel, dbCount, dy->string); 
 
 return 0;
 }
@@ -123,14 +122,14 @@ int main(int argc, char *argv[])
 /* Process command line. */
 {
 optionInit(&argc, argv, options);
-if (argc != 1)
+if (argc != 2)
     usage();
 udcSetDefaultDir(optionVal("udcDir", udcDefaultDir()));
 
 char *hubUrl = optionVal("addHub", NULL);
 
 if (hubUrl != NULL)
-    return hubPublicAdd(hubUrl);
+    return hubPublicAdd(argv[1],hubUrl);
 else
-    return hubPublicCheck();
+    return hubPublicCheck(argv[1]);
 }
