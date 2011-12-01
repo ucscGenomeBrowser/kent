71a7f24285c283b54289eb7eeee00a3df82ff5c6
Morgan Maddren <mmaddren@soe.ucsc.edu>
2011-11-30 16:05:09 -0800

    track files will now generate their md5sum on the fly when it is retrieved when it was originally None. This should be a rare occurance since most files already have an md5sum which is attached through the md5sums.txt file

--- python/lib/ucscgenomics/track.py
+++ python/lib/ucscgenomics/track.py
@@ -1,4 +1,4 @@
-import os, re
+import os, re, hashlib
 from ucscgenomics import ra
 
 def readMd5sums(filename):
@@ -12,6 +12,14 @@ def readMd5sums(filename):
     else:
         return None
 
+        
+def hashfile(filename, hasher=hashlib.md5(), blocksize=65536):
+    afile = open(filename, 'rb')
+    buf = afile.read(blocksize)
+    while len(buf) > 0:
+        hasher.update(buf)
+        buf = afile.read(blocksize)
+    return hasher.hexdigest()
         
 class TrackFile(object):
     """
@@ -40,6 +48,8 @@ class TrackFile(object):
     @property 
     def md5sum(self):
         """The md5sum for this file, stored in the md5sum.txt file in the downloads directory"""
+        if self._md5sum == None:
+            self._md5sum = hashfile(self.fullname)
         return self._md5sum
         
     @property 
@@ -57,7 +67,8 @@ class TrackFile(object):
         """The size in bytes"""
         return self._metaObj
     
-    def __init__(self, fullname, md5, metaObj=None):
+    def __init__(self, fullname, md5=None, metaObj=None):
+        fullname = os.path.abspath(fullname)
         if not os.path.isfile(fullname):
             raise FileError('invalid file: %s' % fullname)
         self._path, self._name = fullname.rsplit('/', 1)
