3bfd4c523c42978c98b488f2aa0183f1edfb8fdd
Karen Hayden <kehayden@soe.ucsc.edu>
2012-02-01 10:07:45 -0800

    JK/KH update of learningLibs from Mtg2

--- src/kehayden/learningLibs/learningLibs.c
+++ src/kehayden/learningLibs/learningLibs.c
@@ -11,6 +11,8 @@
 #include "options.h"
 /* useful to parse commandline options */
 
+boolean allCaps = FALSE;
+
 void usage()
 /* Explain usage and exit. */
 {
@@ -20,16 +22,41 @@ errAbort(
   "    learningLibs textFile\n"
   "    example textFile path:  /cluster/home/kehayden/rpNH.txt\n"
   "options:\n"
-  "   -t=textFile [argv0] ** currently (-t) is not working\n"
+  "   -verbose=N print out extra diagnostic information. 0 for silent, 1 default, 2 extra\n"
+  "   -allCaps if set then will convert all words to upper case\n"
     );
 }
 
 static struct optionSpec options[] = {
   /*  Not sure what this is doing, but see something similar in lib/options.c */
+   {"allCaps", OPTION_BOOLEAN},
    {NULL, 0},
-   /* I think there needs to be some option for "-t" ?? */
+   
 };
 
+struct wordTracker
+/* Information to track a word. */
+    {
+    struct wordTracker *next;  /* next in list */
+    char *word;   /* The string value of the word - not allocated here. */
+int count;   /* Number of times a word is used. */
+};
+
+int wordTrackerCmpCount(const void *va, const void *vb)
+/* Compare two word trackers by count, useful with sorting. */
+{
+  const struct wordTracker *a = *((struct wordTracker **)va);
+  const struct wordTracker *b = *((struct wordTracker **)vb);
+  return a->count - b->count;
+}
+
+int wordTrackerCmpWord(const void *va, const void *vb)
+/* Compare two word trackers by word , useful with sorting. */
+{
+  const struct wordTracker *a = *((struct wordTracker **)va);
+  const struct wordTracker *b = *((struct wordTracker **)vb);
+  return strcmp(a->word, b->word);
+}
 
 void learningLibs(char *textFile)
 /* learningLibs - A program to help learn the kent libraries.*/
@@ -38,7 +65,7 @@ void learningLibs(char *textFile)
   struct lineFile *lf = lineFileOpen(textFile, TRUE); 
 
   /* initialize hash: similar to hash.h line 3 */
-  /* struct hash *hash = hashNew(0); */
+  struct hash *hash = hashNew(0); 
   #define COUNT 140
   #define INIT 1
   char *line;
@@ -46,10 +73,14 @@ void learningLibs(char *textFile)
   int i;
   int wordCount;
   int totalWords;
+  struct wordTracker *wordList = NULL;
   totalWords=0;
 
   while (lineFileNextReal(lf, &line)) 
-    {
+      {
+	if (allCaps)
+	  strUpper(line);
+
       char *words[COUNT];  /*character array named 'words' and an unspecified number of elements. */
       /* initialize inside of block so that automatic storage duration is set recreate each loop */
 
@@ -63,17 +94,32 @@ void learningLibs(char *textFile)
       /* printf("%6d\n",totalWords);*/
 
       /* process each element of array words[] */
-      for( i = 0; i < COUNT; i++ )
-	{
-	  printf("%4d\t%20s\n",i,words[i]);
+      for (i = 0; i < wordCount; i++)
+	  {
+	  verbose(2,"%4d\t%20s\n",i,words[i]);
+          char *word = words[i];
+          struct wordTracker *tracker = hashFindVal(hash, word);
+          if (!tracker)
+	    {
+	      AllocVar(tracker);
+	      hashAddSaveName(hash, word, tracker, &tracker->word);
+              slAddHead(&wordList, tracker);
+	    }
+          tracker->count += 1;
 	  /* store each word into a hash counter */
 	  /* if exists in hash:  increment counter */
 	  /* else inialize hash with count = 1 */
 	  /*hashAdd(hash, words[i], INIT) ; */
-	}
-
+	  }
       
-    }		
+    }
+
+  // Sort, loop through list and iterate.     
+  slSort(&wordList, wordTrackerCmpWord);
+  struct wordTracker *tracker;
+  for (tracker = wordList; tracker != NULL; tracker = tracker->next)
+    printf("%s %d\n", tracker->word, tracker->count);
+		
       lineFileClose(&lf);
 }
 
@@ -84,8 +130,9 @@ int main(int argc, char *argv[])
 {
 
 optionInit(&argc, argv, options);
+allCaps = optionExists("allCaps");
 if (argc != 2)
      usage();
-     learningLibs(argv[1]);
-     return 0;
+learningLibs(argv[1]);
+return 0;
 }
