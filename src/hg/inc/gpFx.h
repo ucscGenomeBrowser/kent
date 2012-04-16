feeb7ee7651d3a85ebc34c4fb32243a841eacdf2
Brian Raney <braney@soe.ucsc.edu>
2012-04-16 16:23:04 -0700

    on-going work on annoGrator (#6152).  Started using Sequence Ontology numbers from EBI, and added a generic variant structure as input to gpFx library

--- src/hg/inc/gpFx.h
+++ src/hg/inc/gpFx.h
@@ -2,28 +2,14 @@
 #define GPFX_H
 
 #include "variant.h"
+#include "soterm.h"
 
+// a single gpFx variant effect call
 struct gpFx
 {
 struct gpFx *next;
-enum gpFxType { 
-	gpFxNone, 
-	gpFxUtr5, 
-	gpFxUtr3, 
-	gpFxSynon, 
-	gpFxNonsynon, 
-	gpFxSplice5, 
-	gpFxUpstream, 
-	gpFxDownstream, 
-	gpFxIntron 
-	} gpFxType; ;
 
-int    gpFxNumber;        // exon or intron number
-int    gpFxTransOffset;    //offset in transcript
-char   *gpFxBaseChange;     //base change in transcript
-int    gpFxCodonChange;    //codon triplet change in transcript
-int    gpFxProteinOffset;  //offset in protein
-int    gpFxProteinChange;  //peptide change in protein
+struct soCall so;
 };
 
 struct gpFx *gpFxPredEffect(struct variant *variant, struct genePred *pred,
