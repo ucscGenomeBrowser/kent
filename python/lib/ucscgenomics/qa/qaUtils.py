e35a4d4088d13d77abab5a462136ea95ed6b120f
Brooke Rhead <rhead@soe.ucsc.edu>
2012-05-04 12:30:42 -0700

    Made runCommand() use subprocess communicate(), and changed it to return stdout and stderr.

--- python/lib/ucscgenomics/qa/qaUtils.py
+++ python/lib/ucscgenomics/qa/qaUtils.py
@@ -40,10 +40,13 @@ def checkTablesExist(db, tables):
         if not tableExists(db, name):
             raise Exception("Table " + name + " in " + db + " does not exist")
 
-def runCommand(command, stdout=None, stderr=None):
-    """Runs command in subprocess and raises exception if return code is not 0."""
-    p = subprocess.Popen(command, stdout=stdout, stderr=stderr)
-    p.wait()
+def runCommand(command):
+    """Runs command in subprocess and raises exception if return code is not 0.
+    Returns tuple (stdoutdata, stderrdata). """
+    p = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    cmdout, cmderr = p.communicate()
     if p.returncode != 0:
+        # keep command arguments nicely quoted
         cmdstr = " ".join([pipes.quote(arg) for arg in command])
         raise Exception("Error from: " + cmdstr)
+    return cmdout, cmderr
