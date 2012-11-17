cc724e307e7912fb03517c142e28d7bc2723f5e4
Angie Hinrichs <angie@soe.ucsc.edu>
2012-11-16 16:25:12 -0800

    Track #9401 (DGV update): loaded Nov. '12 data.

--- src/hg/lib/dgv.sql
+++ src/hg/lib/dgv.sql
@@ -15,13 +15,13 @@ CREATE TABLE dgv (
     thickStart int unsigned not null,	# Start of where display should be thick (start codon)
     thickEnd int unsigned not null,	# End of where display should be thick (stop codon)
     itemRgb int unsigned not null,	# Item R,G,B color.
-    landmark varchar(255) not null,	# Genomic marker near the variation locus
+    landmark longblob not null,	# Genomic marker near the variation locus
     varType varchar(255) not null,	# Type of variation
     reference varchar(255) not null,	# Literature reference for the study that included this variant
     pubMedId int unsigned not null,	# For linking to pubMed abstract of reference
-    method varchar(255) not null,	# Brief description of method/platform
+    method longblob not null,	# Brief description of method/platform
     sample longblob not null,	# Description of sample population for the study
               #Indices
     INDEX (chrom,bin),
-    PRIMARY KEY (name)
+    INDEX (name)
 );
