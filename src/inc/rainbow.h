80bdba8d3041e92d617eb20e981aca01fb086e85
Jim Kent <kent@soe.ucsc.edu>
2013-02-13 11:31:09 -0800

    Exposing veryLightRainbowAtPos to rest of code.

--- src/inc/rainbow.h
+++ src/inc/rainbow.h
@@ -15,4 +15,8 @@ struct rgbColor lightRainbowAtPos(double pos);
 /* Given pos, a number between 0 and 1, return a lightish rainbow rgbColor
  * where 0 maps to red,  0.1 is orange, and 0.9 is violet and 1.0 is back to red */
 
+struct rgbColor veryLightRainbowAtPos(double pos);
+/* Given pos, a number between 0 and 1, return a light rainbow rgbColor
+ * where 0 maps to red,  0.1 is orange, and 0.9 is violet and 1.0 is back to red */
+
 #endif /* RAINBOW_H */
