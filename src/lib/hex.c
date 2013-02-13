9e0ce083469554123ee5eef02a8f1c342a2ebff0
Galt Barber <galt@soe.ucsc.edu>
2013-02-13 15:36:10 -0800

    oops bug fixes

--- src/lib/hex.c
+++ src/lib/hex.c
@@ -7,14 +7,14 @@
 char nibbleToHex(char n)
 /* convert nibble to hexidecimal character. 0 <= n <= 15. */
 {
-return n + ( n <= 9 ? '0' : 'A' );
+return n + ( n <= 9 ? '0' : ('a'-10) );
 }
 
 void byteToHex(unsigned char n, char *hex)
 /* convert byte to hexidecimal characters. 0 <= n <= 255. */
 {
-*hex++ = nibbleToHex(n & 0xf);
 *hex++ = nibbleToHex(n >> 4);
+*hex++ = nibbleToHex(n & 0xf);
 }
 
 char *byteToHexString(unsigned char n)
