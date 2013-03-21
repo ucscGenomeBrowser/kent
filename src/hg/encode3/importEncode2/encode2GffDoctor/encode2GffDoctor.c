6580a174a548cbe3d0259bbf408a828b87bdb360
Jim Kent <kent@soe.ucsc.edu>
2013-03-21 11:22:48 -0700

    Abbreviating more long things that might end up in bed->name field.

--- src/hg/encode3/importEncode2/encode2GffDoctor/encode2GffDoctor.c
+++ src/hg/encode3/importEncode2/encode2GffDoctor/encode2GffDoctor.c
@@ -58,25 +58,29 @@ return string;
 void encode2GffDoctor(char *inFile, char *outFile)
 /* encode2GffDoctor - Fix up gff/gtf files from encode phase 2 a bit.. */
 {
-char *transcriptTag = "transcript_id ";
 struct lineFile *lf = lineFileOpen(inFile, TRUE);
 char *row[9];
 FILE *f = mustOpen(outFile, "w");
 while (lineFileRowTab(lf, row))
     {
+    /* Fix mitochondria sequence name. */
     if (sameString(row[0], "chrMT"))
         row[0] = "chrM";
-    if (sameString(row[1], "Cufflinks"))
-        {
-	/* Abbreviate really long transcript IDs. */
-	char *trans = stringBetween(transcriptTag, ";", row[8]);
-	if (trans != NULL)
+
+    /* Abbreviate really long transcript IDs and gene IDs. */
+    char *tagsToShorten[] = {"transcript_id ", "gene_id ", "gene_ids ", "transcript_ids "};
+    int i;
+    for (i=0; i<ArraySize(tagsToShorten); ++i)
+	{
+	char *tag = tagsToShorten[i];
+	char *val = stringBetween(tag, ";", row[8]);
+	if (val != NULL)
 	    {
-	    char *s = trans;
+	    char *s = val;
 	    char *id = nextWordRespectingQuotes(&s);
 	    int maxSize = 200;
 	    if (strlen(id) > maxSize)
-	        {
+		{
 		id[maxSize-4] = 0;
 		char *e = strrchr(id, ',');
 		if (e != NULL)
@@ -87,24 +91,25 @@ while (lineFileRowTab(lf, row))
 		    *e++ = '"';
 		    *e = 0;
 		    }
-		row[8] = replaceFieldInGffGroup(row[8], transcriptTag, id);
+		row[8] = replaceFieldInGffGroup(row[8], tag, id);
 		}
-	    freez(&trans);
-	    }
-	else
-	    {
-	    /* No transcript tag?  If it's one of the ones with gene_ids and transcript_ids
-	     * instead of "gene_id" and "transcript_id" try to fix it by turning gene_ids to
-	     * gene_id.  */
-	    char *geneId = stringBetween("gene_id ", ";", row[8]);
-	    if (geneId == NULL)
-		{
-		row[8] = subForSmaller(row[8], "gene_ids", "gene_id");
-		}
-	    freez(&geneId);
+	    freez(&val);
 	    }
 	}
-    int i;
+    if (!stringIn("transcript_id ", row[8]))
+	{
+	/* No transcript tag?  If it's one of the ones with gene_ids and transcript_ids
+	 * instead of "gene_id" and "transcript_id" try to fix it by turning gene_ids to
+	 * gene_id.  */
+	char *geneId = stringBetween("gene_id ", ";", row[8]);
+	if (geneId == NULL)
+	    {
+	    row[8] = subForSmaller(row[8], "gene_ids", "gene_id");
+	    }
+	freez(&geneId);
+	}
+
+    /* Output fixed result. */
     fprintf(f, "%s", row[0]);
     for (i=1; i<9; ++i)
         fprintf(f, "\t%s", row[i]);
