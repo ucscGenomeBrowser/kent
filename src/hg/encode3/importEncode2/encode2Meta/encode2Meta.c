6395d593c5b176d6f5d52e41e7fb9bffcfc1ff57
Jim Kent <kent@soe.ucsc.edu>
2013-03-23 01:14:12 -0700

    An interesting first cut.

---
+++ src/hg/encode3/importEncode2/encode2Meta/encode2Meta.c
@@ -0,0 +1,428 @@
+/* encode2Meta - Create meta files.. */
+#include "common.h"
+#include "linefile.h"
+#include "hash.h"
+#include "options.h"
+#include "encode/encodeExp.h"
+#include "encode3/encode2Manifest.h"
+#include "mdb.h"
+
+char *metaDbs[] = {"hg19", "mm9"};
+char *Organisms[] = {"Human", "Mouse"};
+char *organisms[] = {"human", "mouse"};
+char *metaTable = "metaDb";
+char *expDb = "hgFixed";
+char *expTable = "encodeExp";
+
+
+void usage()
+/* Explain usage and exit. */
+{
+errAbort(
+  "encode2Meta - Create meta files.\n"
+  "usage:\n"
+  "   encode2Meta metadata.tab outFile\n"
+  "options:\n"
+  "   -xxx=XXX\n"
+  );
+}
+
+/* Command line validation table. */
+static struct optionSpec options[] = {
+   {NULL, 0},
+};
+
+struct metaNode
+/* A node in a metadata tree */
+    {
+    struct metaNode *next;	/* Next sibling. */
+    struct metaNode *children;  /* Children if any */
+    struct metaNode *parent;	/* Parent if any */
+    char *name;	/* Node's unique symbolic name. */
+    struct mdbVar *vars;    /* Variables used if any */
+    };
+
+char *mdbVarLookup(struct mdbVar *list, char *var)
+/* Return value associated with given var if var is on list, else NULL */
+{
+struct mdbVar *v;
+for (v = list; v != NULL; v = v->next)
+    {
+    if (sameString(v->var, var))
+        return v->val;
+    }
+return NULL;
+}
+
+char *metaLocalVal(struct metaNode *node, char *var)
+/* Look up value, not going up to parents. */
+{
+return mdbVarLookup(node->vars, var);
+}
+
+char *metaVal(struct metaNode *node, char *var)
+/* Return value of given var, or none if variable isn't defined.
+ * Looks first in self, and then in parents. */
+{
+char *val;
+while (node != NULL)
+    {
+    if ((val = metaLocalVal(node, var)) != NULL)
+        return val;
+    node = node->parent;
+    }
+return NULL;
+}
+
+struct mdbVar *mdbVarNew(char *var, char *val)
+/* Return a new mdbVar. */
+{
+struct mdbVar *v;
+AllocVar(v);
+v->var = cloneString(var);
+v->val = cloneString(val);
+return v;
+}
+
+void metaNodeAddVar(struct metaNode *node, char *var, char *val)
+/* Add var to node - but only if it is not already present at same value at a higher level */
+{
+if (val == NULL)
+    return;
+if (node->parent != NULL && sameOk(metaVal(node->parent, var), val))
+    return;   /* Already in parent, we are fine. */
+if (metaLocalVal(node, var))
+    errAbort("Redefining %s.%s\n", node->name, var);
+struct mdbVar *v = mdbVarNew(var, val);
+slAddHead(&node->vars, v);
+}
+
+void metaNodeAddVarVals(struct metaNode *node, char *varVals)
+/* Add string of var=vals to node */
+{
+if (varVals == NULL)
+    return;
+struct slPair *pair, *pairList = slPairListFromString(varVals, FALSE);
+for (pair = pairList; pair != NULL; pair = pair->next)
+    metaNodeAddVar(node, pair->name, pair->val);
+}
+
+struct metaNode *metaNodeNew(char *name)
+/* Make new but empty and unconnected node. */
+{
+struct metaNode *meta;
+AllocVar(meta);
+meta->name = cloneString(name);
+return meta;
+}
+
+struct metaNode *metaTreeNew(char *name)
+/* Make largely empty root node. */
+{
+return metaNodeNew(name);
+}
+
+struct mdbObj *getMdbList(char *database)
+/* Get list of metaDb objects for a database. */
+{
+struct sqlConnection *conn = sqlConnect(database);
+struct mdbObj *list = mdbObjsQueryAll(conn, metaTable);
+sqlDisconnect(&conn);
+return list;
+}
+
+struct metaNode *addExpToNode(struct encodeExp *exp, struct metaNode *parent)
+/* Wrap a metaNode arount exp,  add it to parent, and return it. */
+{
+struct metaNode *node = metaNodeNew(exp->accession);
+metaNodeAddVar(node, "organism", exp->organism);
+metaNodeAddVar(node, "lab", exp->lab);
+metaNodeAddVar(node, "dataType", exp->dataType);
+metaNodeAddVar(node, "cellType", exp->cellType);
+metaNodeAddVar(node, "updateTime", exp->updateTime);
+metaNodeAddVarVals(node, exp->expVars);
+return node;
+}
+
+void metaTreeWrite(int level, struct metaNode *node, FILE *f)
+/* Write out self and children to file recursively. */
+{
+int indent = level*4;
+spaceOut(f, indent);
+fprintf(f, "meta %s\n", node->name);
+struct mdbVar *v;
+for (v = node->vars; v != NULL; v = v->next)
+    {
+    spaceOut(f, indent);
+    fprintf(f, "%s %s\n", v->var, v->val);
+    }
+fprintf(f, "\n");
+struct metaNode *child;
+for (child = node->children; child != NULL; child = child->next)
+    metaTreeWrite(level+1, child, f);
+}
+
+boolean mdbVarRemove(struct mdbVar **pList, char *var)
+/* Find given variable in list and remove it. Returns TRUE if it
+ * actually removed it,  FALSE if it never found it. */
+{
+struct mdbVar **ln = pList;
+struct mdbVar *v;
+for (v = *pList; v != NULL; v = v->next)
+    {
+    if (sameString(v->var, var))
+        {
+	*ln = v->next;
+	return TRUE;
+	}
+    ln = &v->next;
+    }
+return FALSE;
+}
+
+
+void hoistOne(struct metaNode *node, char *var, char *val)
+/* We've already determined that var exists and has same value in all children.
+ * What we do here is add it to ourselves and remove it from children. */
+{
+if (mdbVarLookup(node->vars, var))
+    mdbVarRemove(&node->vars, var);
+metaNodeAddVar(node, var, val);
+struct metaNode *child;
+for (child = node->children; child != NULL; child = child->next)
+    {
+    mdbVarRemove(&child->vars, var);
+    }
+}
+
+struct slName *varsInAnyNode(struct metaNode *nodeList)
+/* Return list of variables that are used in any node in list. */
+{
+struct hash *varHash = hashNew(6);
+struct slName *var, *varList = NULL;
+struct metaNode *node;
+for (node = nodeList; node != NULL; node = node->next)
+    {
+    struct mdbVar *v;
+    for (v = node->vars; v != NULL; v = v->next)
+        {
+	if (!hashLookup(varHash, v->var))
+	    {
+	    var = slNameAddHead(&varList, v->var);
+	    hashAdd(varHash, var->name, var);
+	    }
+	}
+    }
+hashFree(&varHash);
+return varList;
+}
+
+char *allSameVal(char *var, struct metaNode *nodeList)
+/* Return value of variable if it exists and is the same in each node on list */
+{
+char *val = NULL;
+struct metaNode *node;
+for (node = nodeList; node != NULL; node = node->next)
+    {
+    char *oneVal = mdbVarLookup(node->vars, var);
+    if (oneVal == NULL)
+        return NULL;
+    if (val == NULL)
+        val = oneVal;
+    else
+        {
+	if (!sameString(oneVal, val))
+	    return NULL;
+	}
+    }
+return val;
+}
+
+void metaTreeHoist(struct metaNode *node)
+/* Move variables that are the same in all children up to parent. */
+{
+/* Do depth first recursion, but get early return if we're a leaf. */
+struct metaNode *child;
+if (node->children == NULL)
+    return;
+for (child = node->children; child != NULL; child = child->next)
+    metaTreeHoist(child);
+
+/* Build up list of variables used in any child. */
+struct slName *var, *varList = varsInAnyNode(node->children);
+
+/* Go through list and figure out ones that are same in all children. */
+for (var = varList; var != NULL; var = var->next)
+    {
+    char *val = allSameVal(var->name, node->children);
+    if (val != NULL)
+        {
+	hoistOne(node, var->name, val);
+	}
+    }
+}
+
+
+void encode2Meta(char *manifestIn, char *outFile)
+/* encode2Meta - Create meta files.. */
+{
+/* Create a three level meta.ra format file based on hgFixed.encodeExp
+ * and database.metaDb tables. The levels are composite, experiment, file */
+struct metaNode *metaTree = metaTreeNew("encode2");
+
+/* Load up the manifest. */
+struct encode2Manifest *mi, *miList = encode2ManifestLoadAll(manifestIn);
+struct hash *miHash = hashNew(18);
+for (mi = miList; mi != NULL; mi = mi->next)
+    hashAdd(miHash, mi->fileName, mi);
+verbose(1, "%d files in %s\n", miHash->elCount, manifestIn);
+
+/* Load up encodeExp info. */
+struct sqlConnection *expConn = sqlConnect(expDb);
+struct encodeExp *exp, *expList = encodeExpLoadByQuery(expConn, "select * from encodeExp");
+sqlDisconnect(&expConn);
+verbose(1, "%d experiments in encodeExp\n", slCount(expList));
+
+struct hash *compositeHash = hashNew(0);
+
+/* Go through each  organism database in turn. */
+int i;
+for (i=0; i<ArraySize(metaDbs); ++i)
+    {
+    char *db = metaDbs[i];
+    struct mdbObj *mdb, *mdbList = getMdbList(db);
+    verbose(1, "%d meta objects in %s\n", slCount(mdbList), db);
+
+    /* Get info on all composites. */
+    for (mdb = mdbList; mdb != NULL; mdb = mdb->next)
+        {
+	char *objType = mdbVarLookup(mdb->vars, "objType");
+	if (objType != NULL && sameString(objType, "composite"))
+	    {
+	    char compositeName[256];
+	    safef(compositeName, sizeof(compositeName), "%s%s", mdb->obj, Organisms[i]);
+	    struct metaNode *compositeNode = metaNodeNew(compositeName);
+	    slAddHead(&metaTree->children, compositeNode);
+	    compositeNode->parent = metaTree;
+	    struct mdbVar *v;
+	    for (v=mdb->vars; v != NULL; v = v->next)
+	        {
+		metaNodeAddVar(compositeNode, v->var, v->val);
+		}
+	    metaNodeAddVar(compositeNode, "assembly", db);
+	    hashAdd(compositeHash, mdb->obj, compositeNode);
+	    }
+	}
+
+    /* Make up one more for experiments with no composite. */
+    char *noCompositeName = "noComposite";
+    struct metaNode *noCompositeNode = metaNodeNew(noCompositeName);
+    slAddHead(&metaTree->children, noCompositeNode);
+    noCompositeNode->parent = metaTree;
+    hashAdd(compositeHash, noCompositeName, noCompositeNode);
+
+
+    /* Now go through objects trying to tie experiments to composites. */ 
+    struct hash *expToComposite = hashNew(16);
+    for (mdb = mdbList; mdb != NULL; mdb = mdb->next)
+        {
+	char *composite = mdbVarLookup(mdb->vars, "composite");
+	if (composite != NULL && !sameString(composite, "wgEncodeAwgTfbsUniform") 
+	    && !sameString(composite, "wgEncodeAwgDnaseUniform") && !startsWith("wgEncodeReg", composite))
+	    {
+	    char *dccAccession = mdbVarLookup(mdb->vars, "dccAccession");
+	    if (dccAccession != NULL)
+	        {
+		char *oldComposite = hashFindVal(expToComposite, dccAccession);
+		if (oldComposite != NULL)
+		    {
+		    if (!sameString(oldComposite, composite))
+		        verbose(2, "%s maps to %s ignoring mapping to %s", dccAccession, oldComposite, composite);
+		    }
+		else
+		    {
+		    hashAdd(expToComposite, dccAccession, composite);
+		    }
+		}
+	    }
+	}
+
+    /* Now get info on all experiments in this organism. */
+    struct hash *expHash = hashNew(0);
+    for (exp = expList; exp != NULL; exp = exp->next)
+        {
+	if (sameString(exp->organism, organisms[i]))
+	    {
+	    if (exp->accession != NULL)
+		{
+		char *composite = hashFindVal(expToComposite,  exp->accession);
+		struct metaNode *compositeNode;
+		if (composite != NULL)
+		    {
+		    compositeNode = hashMustFindVal(compositeHash, composite);
+		    }
+		else
+		    {
+		    compositeNode = noCompositeNode;
+		    }
+		struct metaNode *expNode = addExpToNode(exp, compositeNode);
+		hashAdd(expHash, expNode->name, expNode);
+		slAddHead(&compositeNode->children, expNode);
+		expNode->parent = compositeNode;
+		}
+	    }
+	}
+
+    char *fileName = NULL, *dccAccession = NULL, *composite = NULL, *view = NULL;
+    for (mdb = mdbList; mdb != NULL; mdb = mdb->next)
+	{
+	struct mdbVar *v;
+	for (v = mdb->vars; v != NULL; v = v->next)
+	    {
+	    char *var = v->var, *val = v->val;
+	    if (sameString("fileName", var))
+		fileName = val;
+	    else if (sameString("dccAccession", var))
+		dccAccession = val;
+	    else if (sameString("composite", var))
+		composite = val;
+	    else if (sameString("view", var))
+		view = val;
+	    }
+	if (fileName != NULL && dccAccession != NULL)
+	    {
+	    struct metaNode *expNode = hashFindVal(expHash, dccAccession);
+	    if (expNode != NULL)
+	        {
+		struct metaNode *fileNode = metaNodeNew(mdb->obj);
+		slAddHead(&expNode->children, fileNode);
+		fileNode->parent = expNode;
+		struct mdbVar *v;
+		for (v=mdb->vars; v != NULL; v = v->next)
+		    {
+		    metaNodeAddVar(fileNode, v->var, v->val);
+		    }
+		}
+	    }
+	}
+    }
+
+FILE *ugly = mustOpen("ugly.tree", "w");
+metaTreeWrite(0, metaTree, ugly);
+carefulClose(&ugly);
+
+metaTreeHoist(metaTree);
+FILE *f = mustOpen(outFile, "w");
+metaTreeWrite(0, metaTree, f);
+carefulClose(&f);
+}
+
+int main(int argc, char *argv[])
+/* Process command line. */
+{
+optionInit(&argc, argv, options);
+if (argc != 3)
+    usage();
+encode2Meta(argv[1], argv[2]);
+return 0;
+}
