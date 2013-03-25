44ff40f5dd4f709858a01227df923c6c99f91d89
Angie Hinrichs <angie@soe.ucsc.edu>
2013-03-25 15:07:10 -0700

    GAD data update. Refactored gadPos program and extended it to also use Gencode tables if available.  Added gadAll.as for documentation. refs #10449

--- src/hg/protein/gadPos/gadPos.c
+++ src/hg/protein/gadPos/gadPos.c
@@ -2,172 +2,161 @@
 #include "common.h"
 #include "hCommon.h"
 #include "hdb.h"
+#include "options.h"
+
+// Using options just to get -verbose:
+static struct optionSpec optionSpecs[] = {
+    {NULL, 0}
+};
 
 void usage()
 /* Explain usage and exit. */
 {
 errAbort(
-  "gadPos -  generate genomic positions for GAD entries\n"
+  "gadPos -  find genomic positions for GAD entries based on gene symbol\n"
   "usage:\n"
-  "   gadPos gdb outFile\n"
-  "      gdb is the genome database\n"
-  "      gadPos is the output file name for GAD positions (bed 4 format)\n"
-  "example: gadPos hg17 gadPos.tab\n");
+  "   gadPos db outFile\n"
+  "      db is the genome database that must contain updated gadAll table,\n"
+  "      known{Gene,Canonical,Isoforms}, kg{Xref,Alias}, and ref{Gene,Link}.\n"
+  "      Each geneSymbol with association=Y in gadAll is looked up first in \n"
+  "      knownCanonical; if not found then refGene; if not found, then kgAlias.\n"
+  "      If db contains wgEncodeGencode{Pseudogene,Comp}, they will be searched\n"
+  "      if geneSymbol still has not been found.\n"
+  "      outFile is the output file name for GAD positions (bed 4 format)\n"
+  "example: gadPos hg17 stdout | uniq > gadPos.tab\n");
 }
+
+boolean printBed4FromQueryAndName(struct sqlConnection *conn, char *query, char *name,
+				  FILE *outF)
+/* If there are results from query, assume that they are {chrom, chromStart, chromEnd},
+ * print BED4 with those coords and name, and return TRUE; */
+{
+boolean found = FALSE;
+struct sqlResult *sr = sqlGetResult(conn, query);
+char **row;
+while ((row = sqlNextRow(sr)) != NULL)
+    {
+    found = TRUE;
+    fprintf(outF, "%s\t%s\t%s\t%s\n", row[0], row[1], row[2], name);
+    }
+sqlFreeResult(&sr);
+return found;
+}
+
+boolean findInKnownCanonical(struct sqlConnection *conn, char *geneSymbol, FILE *outF)
+/* For each knownGene cluster for this geneSymbol, print output using the
+ * cluster bounds as {chrom,chromStart,chromEnd} and return TRUE. */
+{
+boolean found = FALSE;
+char query[1024];
+safef(query, sizeof(query), "select distinct clusterId from kgXref x, knownIsoforms i "
+		    "where x.geneSymbol='%s' and i.transcript=x.kgId", geneSymbol);
+struct slName *id, *clusterIds = sqlQuickList(conn, query);
+for (id = clusterIds;  id != NULL;  id = id->next)
+    {
+    safef(query, sizeof(query), "select k.chrom, min(txStart), max(txEnd) "
+	  "from knownGene k, knownIsoforms i, knownCanonical c "
+	  "where i.clusterId=%s and i.transcript=k.name "
+	  "and c.clusterId=i.clusterId and k.chrom=c.chrom",
+	  id->name);
+    found |= printBed4FromQueryAndName(conn, query, geneSymbol, outF);
+    }
+slFreeList(&clusterIds);
+return found;
+}
+
+boolean findInRefGene(struct sqlConnection *conn, char *geneSymbol, FILE *outF)
+/* If there is a refGene for geneSymbol, print output using the
+ * refGene coords as {chrom,chromStart,chromEnd} and return TRUE. */
+{
+char query[1024];
+safef(query, sizeof(query), "select chrom, txStart, txEnd from refGene rg, refLink rl "
+      "where rl.name = '%s' and rl.mrnaAcc = rg.name", geneSymbol);
+return printBed4FromQueryAndName(conn, query, geneSymbol, outF);
+}
+
+boolean findInKgAlias(struct sqlConnection *conn, char *geneSymbol, FILE *outF)
+/* If geneSymbol can be found in kgAlias, print output using the
+ * knownGene coords as {chrom,chromStart,chromEnd} and return TRUE. */
+{
+char query[1024];
+safef(query, sizeof(query), "select distinct(chrom), min(txStart), max(txEnd) "
+      "from knownGene, kgAlias where alias='%s' and name=kgId "
+      "group by chrom", geneSymbol);
+return printBed4FromQueryAndName(conn, query, geneSymbol, outF);
+}
+
+boolean findInGencode(struct sqlConnection *conn, char *geneSymbol, FILE *outF)
+/* If Gencode tables are present and geneSymbol can be found in them,
+ * use those coords and return TRUE. */
+{
+#define PSEUDOGENE_TABLE "wgEncodeGencodePseudoGeneV14"
+#define COMP_TABLE "wgEncodeGencodeCompV14"
+boolean found = FALSE;
+char query[1024];
+if (sqlTableExists(conn, PSEUDOGENE_TABLE))
+    {
+    safef(query, sizeof(query), "select chrom, txStart, txEnd from %s where name2 = '%s'",
+	  PSEUDOGENE_TABLE, geneSymbol);
+    found = printBed4FromQueryAndName(conn, query, geneSymbol, outF);
+    }
+if (!found && sqlTableExists(conn, COMP_TABLE))
+    {
+    safef(query, sizeof(query), "select chrom, txStart, txEnd from %s where name2 = '%s'",
+	  COMP_TABLE, geneSymbol);
+    found = printBed4FromQueryAndName(conn, query, geneSymbol, outF);
+    }
+return found;
+}
+
+void gadPos(char *db, char *outFileName)
+/* Try to get genomic positions for GAD gene symbols from knownGene, refGene,
+ * kgAlias and Gencode V14 in that order. */
+{
+FILE *outF = mustOpen(outFileName, "w");
+struct sqlConnection *conn = hAllocConn(db);
+
+/* loop over all gene symbols in GAD */	
+struct slName *geneSymbols = sqlQuickList(conn,
+			       "select distinct geneSymbol from gadAll where association='Y'");
+struct slName *symbol;
+int kcCount = 0, rgCount = 0, kaCount = 0, gcCount = 0, missingCount = 0;
+for (symbol = geneSymbols;  symbol != NULL;  symbol = symbol->next)
+    {
+    if (findInKnownCanonical(conn, symbol->name, outF))
+	kcCount++;
+    else if (findInRefGene(conn, symbol->name, outF))
+	rgCount++;
+    else if (findInKgAlias(conn, symbol->name, outF))
+	kaCount++;
+    else if (findInGencode(conn, symbol->name, outF))
+	gcCount++;
+    else
+	{
+	verbose(2, "No result for gene symbol '%s'\n", symbol->name);
+	missingCount++;
+	}
+    }
+verbose(1, "Found in knownCanonical: %d\n", kcCount);
+verbose(1, "Found in refGene: %d\n", rgCount);
+verbose(1, "Found in kgAlias: %d\n", kaCount);
+verbose(1, "Found in Gencode: %d\n", gcCount);
+verbose(1, "Not found: %d\n", missingCount);
+
+hFreeConn(&conn);
+carefulClose(&outF);
+}
+
 
 int main(int argc, char *argv[])
 {
-struct sqlConnection *conn, *conn2, *conn3, *conn4, *conn5;
- 
-char query[256], query2[256], query3[256], query4[256], query5[256];
-struct sqlResult *sr, *sr2, *sr3, *sr4, *sr5;
-char **row, **row2, **row3, **row4, **row5;
-
-char *gdb;
-char *refSeq;
-
-FILE   *outputFile;
-char *geneSymbol, *clusterId;
-char *chrom, *chromStart, *chromEnd;
-boolean found;
+optionInit(&argc, argv, optionSpecs);
 
 if (argc != 3) usage();
-gdb   = argv[1];
-   
-outputFile = mustOpen(argv[2], "w");
+char *db   = argv[1];
+char *outFileName = argv[2];
 
-conn  = hAllocConn(gdb);
-conn2 = hAllocConn(gdb);
-conn3 = hAllocConn(gdb);
-conn4 = hAllocConn(gdb);
-conn5 = hAllocConn(gdb);
-	
-/* loop over all gene symbols in GAD */	
-sprintf(query2,"select distinct geneSymbol from %s.gadAll where association='Y'", gdb);
-sr2 = sqlMustGetResult(conn2, query2);
-row2 = sqlNextRow(sr2);
-while (row2 != NULL)
-    {
-    geneSymbol  = row2[0];
-    found = FALSE;    
-    /* first look for start/end positions in KG clusters */
-    sprintf(query3, 
-       "select distinct clusterId from %s.kgXref x, %s.knownIsoforms i where x.geneSymbol='%s' and i.transcript=x.kgId",
-       gdb, gdb, geneSymbol);
-
-    sr3 = sqlMustGetResult(conn3, query3);
-    row3 = sqlNextRow(sr3);
-
-    if (row3 != NULL)
-    	{
-	while (row3 != NULL)
-	    {
-   	    clusterId = row3[0];
-
-    	    sprintf(query4, 
-	    	    "select chrom from %s.knownCanonical where clusterId=%s", 
-		    gdb, clusterId);
-		
-    	    sr4 = sqlMustGetResult(conn4, query4);
-    	    row4 = sqlNextRow(sr4);
-	    chrom = strdup(row4[0]);
-            sqlFreeResult(&sr4);
-	    
-	    /* keep the minimum of start and maximum of end position 
-	       of all genes in a cluster as the start/end position */
-	       
-    	    sprintf(query4, 
-    		"select min(txStart), max(txEnd) from %s.knownGene k, %s.knownIsoforms i, %s.knownCanonical c where i.clusterId=%s and i.transcript=k.name and c.clusterId=i.clusterId and k.chrom=c.chrom",
-		gdb, gdb, gdb, clusterId);
-		
-    	    sr4 = sqlMustGetResult(conn4, query4);
-    	    row4 = sqlNextRow(sr4);
-
-    	    while (row4 != NULL)
-	    	{
-		chromStart = row4[0];
-		chromEnd   = row4[1];
-		found = TRUE;
-		fprintf(outputFile, "%s\t%s\t%s\t%s\n", chrom, chromStart, chromEnd, geneSymbol);
-	        row4 = sqlNextRow(sr4);
-		}
-            sqlFreeResult(&sr4);
-	    
-	    row3 = sqlNextRow(sr3);
-	    }
-        sqlFreeResult(&sr3);
-	}
-    
-    if (!found)
-        {
-	/* if no KG cluster can be found for the GAD gene symbol, 
-	   try it with RefSeqs */
-        sprintf(query, 
-        "select distinct r.mrnaAcc from %s.refLink r where r.name='%s'",
-        gdb, geneSymbol);
-
-        sr = sqlMustGetResult(conn, query);
-        row = sqlNextRow(sr);
-	
-	if (row !=NULL)
-	    {
-	    while (row != NULL)
-	    	{
-   	    	refSeq  = row[0];
-    	    	sprintf(query4, 
-    			"select chrom, txStart, txEnd from %s.refGene where name='%s'", 
-			gdb, refSeq);
-		
-    	    	sr4 = sqlMustGetResult(conn4, query4);
-    	    	row4 = sqlNextRow(sr4);
-
-    	    	while (row4 != NULL)
-	    	    {
-		    chrom      = row4[0];
-		    chromStart = row4[1];
-		    chromEnd   = row4[2];
-		    found = TRUE;
-		    fprintf(outputFile, "%s\t%s\t%s\t%s\n", chrom, chromStart, chromEnd, geneSymbol);
-	            row4 = sqlNextRow(sr4);
-		    }
-            	sqlFreeResult(&sr4);
-	        row = sqlNextRow(sr);
-		}
-            sqlFreeResult(&sr);
-	    }
-	}
-
-    if (!found)
-	    /* if not finding the gene symbol in RefSeq, try it with kgAlias table then */
-	    {
-	    sprintf(query5, 
-    		   "select chrom, txStart, txEnd from %s.knownGene, %s.kgAlias where alias='%s' and name=kgId",
-		   gdb, gdb, geneSymbol);
-		
-    	    sr5 = sqlMustGetResult(conn5, query5);
-    	    row5 = sqlNextRow(sr5);
-
-    	    if (row5 != NULL)
-		{
-		chrom 	   = row5[0];
-		chromStart = row5[1];
-		chromEnd   = row5[2];
-		
-		fprintf(outputFile, "%s\t%s\t%s\t%s\n", chrom, chromStart, chromEnd, geneSymbol);
-		row5 = sqlNextRow(sr5);
-		}
-            sqlFreeResult(&sr5);
-	    }
-    row2 = sqlNextRow(sr2);
-    }
-sqlFreeResult(&sr2);
-
-hFreeConn(&conn);
-hFreeConn(&conn2);
-hFreeConn(&conn3);
-hFreeConn(&conn4);
-hFreeConn(&conn5);
-fclose(outputFile);
+gadPos(db, outFileName);
 
 return(0);
 }
