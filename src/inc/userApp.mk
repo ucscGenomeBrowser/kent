20257ca76462642269e671000c2a4dad67f6b8ea
Hiram Clawson <hiram@soe.ucsc.edu>
2013-03-29 11:43:53 -0700

    testing new makefile organization refs #9104

---
+++ src/inc/userApp.mk
@@ -0,0 +1,35 @@
+########################################################################
+# user App rules, typical three-line makefile to use this rule set,
+#   the binary program file name is specified by the 'A' variable:
+#	kentSrc = ../..
+#	A = aveCols
+#	include $(kentSrc)/inc/userApp.mk
+#
+# for more than one object file for the resulting 'A' program, use
+#       extraObjects = second.o third.o fourth.o etc.o
+#
+# to use object files built elsewhere:
+#       externObjects = ../path/other.o
+#
+include $(kentSrc)/inc/common.mk
+
+MYLIBS = $(kentSrc)/lib/$(MACHTYPE)/jkweb.a
+
+O = ${A}.o
+objects = ${O} ${extraObjects} ${externObjects}
+
+all ${A}: ${O} ${extraObjects}
+	${CC} ${COPT} -o ${DESTDIR}${BINDIR}/${A} ${objects} ${MYLIBS} ${L}
+	${STRIP} ${DESTDIR}${BINDIR}/${A}${EXE}
+
+compile: ${O} ${extraObjects} ${MYLIBS}
+	${CC} ${COPT} ${CFLAGS} -o ${A}${EXE} ${objects} ${MYLIBS} ${L}
+
+install:: compile
+	rm -f ${DESTDIR}${BINDIR}/${A}${EXE}
+	cp -p ${A}${EXE} ${DESTDIR}${BINDIR}/${A}${EXE}
+	${STRIP} ${A}${EXE} ${DESTDIR}${BINDIR}/${A}${EXE}
+	rm -f ${O} ${A}${EXE}
+
+clean:
+	rm -f ${O} ${extraObjects} ${A}${EXE}
