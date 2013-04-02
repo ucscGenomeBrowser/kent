859241e7263c238cac2ae37fb9794cdc0b73f4a6
Jim Kent <kent@soe.ucsc.edu>
2013-04-02 12:11:41 -0700

    Making it generate lowercase a-f rather than uppercase.  Didn't realize I'd changed case on last commit, and lower case is more compatible with md5sum program.

--- src/lib/hex.c
+++ src/lib/hex.c
@@ -5,7 +5,7 @@
 #include "common.h"
 
 char hexTab[16] = {'0', '1', '2', '3', '4', '5', '6', '7', 
-	'8', '9', 'A', 'B', 'C', 'D', 'E', 'F', };
+	'8', '9', 'a', 'b', 'c', 'd', 'e', 'f', };
 /* Convert 0-15 to a hex char */
 
 
