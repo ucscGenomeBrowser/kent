31cdab553a42d1ddabc06f05e7d2b351c6581010
Jim Kent <kent@soe.ucsc.edu>
2013-04-04 19:35:30 -0700

    Added new assembly loader for encode data warehouse

---
+++ src/hg/encode3/encodeDataWarehouse/edwAddAssembly/edwAddAssembly.c
@@ -0,0 +1,65 @@
+/* edwAddAssembly - Add an assembly to database.. */
+#include "common.h"
+#include "linefile.h"
+#include "hash.h"
+#include "options.h"
+#include "jksql.h"
+#include "twoBit.h"
+#include "encodeDataWarehouse.h"
+#include "edwLib.h"
+
+boolean symLink;    /* If set then just symlink the twobit file rather than copy */
+void usage()
+/* Explain usage and exit. */
+{
+errAbort(
+  "edwAddAssembly - Add an assembly to database.\n"
+  "usage:\n"
+  "   edwAddAssembly taxon name ucscDb twoBitFile\n"
+  "options:\n"
+  "   -symLink - if set just make a symbolic link to twoBitFile in bigDataWarehouse file dir.\n"
+  );
+}
+
+/* Command line validation table. */
+static struct optionSpec options[] = {
+   {"symLink", OPTION_BOOLEAN},
+   {NULL, 0},
+};
+
+void edwAddAssembly(char *taxonString, char *name, char *ucscDb, char *twoBitFile)
+/* edwAddAssembly - Add an assembly to database.. */
+{
+int taxon = sqlUnsigned(taxonString);
+
+struct sqlConnection *conn = sqlConnect(edwDatabase);
+char query[256 + PATH_LEN];
+safef(query, sizeof(query), "select id from edwAssembly where name='%s'", name);
+int asmId = sqlQuickNum(conn, query);
+if (asmId != 0)
+   errAbort("Assembly %s already exists", name);
+
+struct twoBitFile *tbf = twoBitOpen(twoBitFile);
+long long baseCount = twoBitTotalSize(tbf);
+twoBitClose(&tbf);
+
+long long twoBitFileId = edwGetLocalFile(conn, twoBitFile, symLink);
+safef(query, sizeof(query), 
+   "insert edwAssembly (taxon,name,ucscDb,twoBitId,baseCount) "
+                "values(%d, '%s', '%s', %lld, %lld)"
+		, taxon, name, ucscDb, twoBitFileId, baseCount);
+sqlUpdate(conn, query);
+
+sqlDisconnect(&conn);
+}
+
+int main(int argc, char *argv[])
+/* Process command line. */
+{
+optionInit(&argc, argv, options);
+if (argc != 5)
+    usage();
+symLink = optionExists("symLink");
+edwAddAssembly(argv[1], argv[2], argv[3], argv[4]);
+return 0;
+}
