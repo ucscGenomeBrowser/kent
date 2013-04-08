6b9af32fd48caa0245f13de708315dee2c13d91e
Brian Raney <braney@soe.ucsc.edu>
2013-04-08 15:37:25 -0700

    first cut at scripts that otto load gwasCatalog #5172

---
+++ src/hg/utils/gwas/gwasWrapper.sh
@@ -0,0 +1,8 @@
+#!/bin/sh -e
+
+PATH=/cluster/bin/x86_64:$PATH
+EMAIL="braney@soe.ucsc.edu"
+WORKDIR="/hive/data/outside/otto/gwas"
+
+cd $WORKDIR
+./checkGwas.sh $WORKDIR 2>&1 |  mail -s "GWAS Build" $EMAIL
