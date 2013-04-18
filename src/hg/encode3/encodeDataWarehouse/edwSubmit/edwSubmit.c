46cb09914a756223761e1ff1015ae4ed03547f69
Jim Kent <kent@soe.ucsc.edu>
2013-04-18 11:20:06 -0700

    Reporting file level errors in the submission as well as file tables.

--- src/hg/encode3/encodeDataWarehouse/edwSubmit/edwSubmit.c
+++ src/hg/encode3/encodeDataWarehouse/edwSubmit/edwSubmit.c
@@ -209,23 +209,22 @@ freez(&escapedFileName);
 return sqlLastAutoId(conn);
 }
 
-void writeErrToTableAndDie(struct sqlConnection *conn, char *table, int id, char *err)
-/* Write out error to stderr and also save it in errorMessage field of submit table. */
-{
-edwWriteErrToStderrAndTable(conn, table, id, err);
-noWarnAbort();
-}
-
 void handleSubmitError(struct sqlConnection *conn, int submitId, char *err)
 /* Write out error to stderr and also save it in errorMessage field of submit table. */
 {
-writeErrToTableAndDie(conn, "edwSubmit", submitId, err);
+edwWriteErrToStderrAndTable(conn, "edwSubmit", submitId, err);
+noWarnAbort();
 }
 
-void handleFileError(struct sqlConnection *conn, int fileId, char *err)
-/* Write out error to stderr and also save it in errorMessage field of file table. */
+void handleFileError(struct sqlConnection *conn, int submitId, int fileId, char *err)
+/* Write out error to stderr and also save it in errorMessage field of file 
+ * and submit table. */
 {
-writeErrToTableAndDie(conn, "edwFile", fileId, err);
+/* Write out error message to errorMessage field of table. */
+warn("%s", trimSpaces(err));
+edwWriteErrToTable(conn, "edwFile", fileId, err);
+edwWriteErrToTable(conn, "edwSubmit", submitId, err);
+noWarnAbort(err);
 }
 
 int mustMkstemp(char tempFileName[PATH_LEN])
@@ -337,7 +336,7 @@ if (errCatchStart(errCatch))
 errCatchEnd(errCatch);
 if (errCatch->gotError)
     {
-    handleFileError(conn, ef->id, errCatch->message->string);
+    handleFileError(conn, submitId, ef->id, errCatch->message->string);
     }
 return ef->id;
 }
