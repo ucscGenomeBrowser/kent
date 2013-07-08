cb92e13b192838a68129c51f5166e624b376010a
Jim Kent <kent@soe.ucsc.edu>
2013-07-08 13:55:42 -0700

    Moving password checking for script to library.

--- src/hg/encode3/encodeDataWarehouse/edwScriptSubmit/edwScriptSubmit.c
+++ src/hg/encode3/encodeDataWarehouse/edwScriptSubmit/edwScriptSubmit.c
@@ -18,33 +18,16 @@ errAbort(
   "This program is meant to be called as a CGI from a web server using HTTPS.");
 }
 
-void accessDenied()
-/* Sleep a bit and then deny access. */
-{
-sleep(5);
-errAbort("Access denied!");
-}
-
 void doMiddle()
 /* edwScriptSubmit - Handle submission by a script via web services.. */
 {
-/* Security check - make sure we are on https, and check user/password. */
 if (!cgiServerHttpsIsOn())
      usage();
-char *user = sqlEscapeString(cgiString("user"));
-char *password = sqlEscapeString(cgiString("password"));
+struct edwScriptRegistry *reg = edwScriptRegistryFromCgi();
 struct sqlConnection *conn = edwConnectReadWrite();
-char query[256];
-safef(query, sizeof(query), "select * from edwScriptRegistry where name='%s'", user);
-struct edwScriptRegistry *reg = edwScriptRegistryLoadByQuery(conn, query);
-if (reg == NULL)
-    accessDenied();
-char key[EDW_SID_SIZE];
-edwMakeSid(password, key);
-if (!sameString(reg->secretHash, key))
-    accessDenied();
 
 /* Get email associated with script. */
+char query[256];
 safef(query, sizeof(query), "select email from edwUser where id=%d", reg->userId);
 char *email = sqlNeedQuickString(conn, query);
 
