3cd0bb706b3cba77ed64c815f5e9c7c98be89ec8
Max <max@soe.ucsc.edu>
2013-07-09 12:01:12 -0700

    forgot one line in merge conflict

--- src/hg/hgc/hgc.c
+++ src/hg/hgc/hgc.c
@@ -934,7 +934,7 @@ if (sql != NULL)
     char buf[256];
     sqlSafef(buf, sizeof(buf), sql, itemName);
     struct sqlConnection *conn = hAllocConn(database);
-    id = sqlQuickString(conn, query);
+    id = sqlQuickString(conn, bug);
     hFreeConn(&conn);
     }
 return id;
