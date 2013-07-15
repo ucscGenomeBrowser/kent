679b391c0b37d075d36342e0a37f81f710d525dc
Jim Kent <kent@soe.ucsc.edu>
2013-07-15 14:18:18 -0700

    Adding cursory support (just magic number checking really) for idat and rcc files.

--- src/hg/encode3/encodeDataWarehouse/edwMakeValidFile/edwMakeValidFile.c
+++ src/hg/encode3/encodeDataWarehouse/edwMakeValidFile/edwMakeValidFile.c
@@ -497,6 +497,75 @@ vf->depth = (double)totalSize/assembly->baseCount;
 gffFileFree(&gff);
 }
 
+static void requireStartEndLines(char *fileName, char *reportFileName, char *startLine, char *endLine)
+/* Make sure first real line in file is startLine, and last is endLine.  Tolerate
+ * empty lines and white space. */
+{
+struct lineFile *lf = lineFileOpen(fileName, TRUE);
+char *line;
+
+/* Get first real line and make sure it is not empty and matches start line. */
+if (!lineFileNextReal(lf, &line))
+    errAbort("%s is empty", reportFileName);
+line = trimSpaces(line);
+if (!sameString(line, startLine))
+    errAbort("%s doesn't start with %s as expected", reportFileName, startLine);
+
+boolean lastSame = FALSE;
+
+for (;;)
+    {
+    if (!lineFileNextReal(lf, &line))
+        break;
+    line = trimSpaces(line);
+    lastSame = sameString(line, endLine);
+    }
+if (!lastSame)
+    errAbort("%s doesn't end with %s as expected", reportFileName, endLine);
+
+lineFileClose(&lf);
+}
+
+void makeValidRcc(struct sqlConnection *conn, char *path, struct edwFile *ef, struct edwValidFile *vf)
+/* Fill in info about a nanostring RCC file. */
+{
+requireStartEndLines(path, ef->submitFileName, "<Header>", "</Messages>");
+}
+
+boolean fileStartsWithOneOfPair(char *fileName,  char *one, char *two)
+/* Return TRUE if file starts with either one of two strings. */
+{
+/* Figure out size of one and two strings. */
+int oneLen = strlen(one);
+int twoLen = strlen(two);
+int maxLen = max(oneLen, twoLen);
+assert(maxLen > 0);
+int bufLen = maxLen+1;
+char buf[bufLen];
+
+/* Open up file and try to read enough data */
+FILE *f = fopen(fileName, "r");
+if (f == NULL)
+   return FALSE;
+int sizeRead = fread(buf, 1, maxLen, f);
+carefulClose(&f);
+
+/* Return TRUE if we match one or two, otherwise FALSE. */
+if (oneLen >= sizeRead && memcmp(buf, one, oneLen) == 0)
+    return TRUE;
+else if (twoLen >= sizeRead && memcmp(buf, two, twoLen) == 0)
+    return TRUE;
+return FALSE;
+}
+
+void makeValidIdat(struct sqlConnection *conn, char *path, struct edwFile *ef, struct edwValidFile *vf)
+/* Fill in info about a illumina idac file. */
+{
+if (!fileStartsWithOneOfPair(path, "IDAT", "DITA"))
+    errAbort("%s is not a valid .idat file, it does not start with IDAT or DITA", ef->submitFileName);
+}
+
+
 static void needAssembly(struct edwFile *ef, char *format, struct edwAssembly *assembly)
 /* Require assembly tag be present. */
 {
@@ -617,8 +686,13 @@ if (vf->format && vf->validKey)	// We only can validate if we have something for
 	needAssembly(ef, format, assembly);
 	makeValidGtf(conn, path, ef, assembly, vf);
 	}
-    else if (sameString(format, "unknown"))
+    else if (sameString(format, "rcc"))
         {
+	makeValidRcc(conn, path, ef, vf);
+	}
+    else if (sameString(format, "idat"))
+        {
+	makeValidIdat(conn, path, ef, vf);
 	}
     else
         {
