7dd60e7719a47ac82edc5e338dc0972ab2045d43
Hiram Clawson <hiram@soe.ucsc.edu>
2013-11-25 10:59:06 -0800

    generic trash cleaning system now available here refs #11957

--- src/product/scripts/trashCleanMonitor.sh
+++ src/product/scripts/trashCleanMonitor.sh
@@ -1,11 +1,76 @@
-#!/bin/sh
+#!/bin/bash
 #
 #	trashCleanMonitor.sh - script to call trashCleaner.csh and verify
 #		that it actually worked OK
 #
-export trashDir="/data/apache/trash"
+# different echo commands for different systems, the -e means:
+#    -e     enable interpretation of backslash escapes
+# a typical feature found in the GNU echo command
+export ECHO="/bin/echo -e"
+
+# address list, comma separated addresses, to send notifications
+export failMail="alert@some.where,extraAlert@other.where"
+
+# a simple command line mail program to use the failMail address
+# list to email messages from this cron job
+export mailCmd="/bin/mail"
+# when there is no mail system
+export mailCmd="/bin/echo"
+
+usage() {
+    ${ECHO} "usage:  trashCleanMonitor.csh <browserEnvironment.txt> searchAndDestroy" 1>&2
+    ${ECHO} "  the <browserEnvironment.txt> file contains definitions of how" 1>&2
+    ${ECHO} "  these scripts behave in your local environment." 1>&2
+    ${ECHO} "This script will run when given the argument searchAndDestroy" 1>&2
+    ${ECHO} "which is exactly what it will do to the trash files for the" 1>&2
+    ${ECHO} "genome browser.  There is no turning back after it gets going." 1>&2
+    ${ECHO} "This script is actually a monitor on the actual trash cleaner:" 1>&2
+    ${ECHO} "\tKENTHOME/src/product/scripts/trashCleaner.csh" 1>&2
+    ${ECHO} "It will verify the trash cleaner is functioning properly and if" 1>&2
+    ${ECHO} "there are problems it will email a failure message to" 1>&2
+    ${ECHO} "\t${failMail}" 1>&2
+    ${ECHO} "Files that belong to sessions will be moved from the directory" 1>&2
+    ${ECHO} "TRASHDIR/ct/ to USERDATA/ct/" 1>&2
+    ${ECHO} "activity logs can be found in USERDATA/log/YYYY/MM" 1>&2
+    exit 255
+}
+
+export includeFile=$1
+if [ "X${includeFile}Y" = "XY" ]; then
+    usage
+fi
+
+if [ -f "${includeFile}" ]; then
+    . "${includeFile}"
+else
+    ${ECHO} "trashCleanMonitor.sh: ERROR: can not find ${includeFile}" 1>&2
+    ${ECHO} "trashCleanMonitor.sh: ERROR: can not find ${includeFile}" \
+      | ${mailCmd} -s "ALERT:" ${failMail} > /dev/null 2> /dev/null
+    usage
+fi
+
+if [ -z "${TRASHDIR+x}" ]; then
+  ${ECHO} "trashCleanMonitor.sh: ERROR: missing definition for TRASHDIR" 1>&2
+  ${ECHO} "trashCleanMonitor.sh: ERROR: missing definition for TRASHDIR" \
+      | ${mailCmd} -s "ALERT:" ${failMail} > /dev/null 2> /dev/null
+  exit 255
+fi
+if [ -z "${USERDATA+x}" ]; then
+  ${ECHO} "trashCleanMonitor.sh: ERROR: missing definition for USERDATA" 1>&2
+  ${ECHO} "trashCleanMonitor.sh: ERROR: missing definition for USERDATA" \
+      | ${mailCmd} -s "ALERT:" ${failMail} > /dev/null 2> /dev/null
+  exit 255
+fi
+if [ -z "${KENTHOME+x}" ]; then
+  ${ECHO} "trashCleanMonitor.sh: ERROR: missing definition for KENTHOME" 1>&2
+  ${ECHO} "trashCleanMonitor.sh: ERROR: missing definition for KENTHOME" \
+      | ${mailCmd} -s "ALERT:" ${failMail} > /dev/null 2> /dev/null
+  exit 255
+fi
+export trashDir="${TRASHDIR}"
 export trashCt="${trashDir}/ct"
-export userData="/data/apache/userdata"
+export userData="${USERDATA}"
+export lockFile="${userData}/cleaner.pid"
 export userCt="${userData}/ct"
 export userLog="${userData}/log"
 export dateStamp=`date "+%Y-%m-%dT%H"`
@@ -13,27 +78,28 @@ export YYYY=`date "+%Y"`
 export MM=`date "+%m"`
 export logDir="${userLog}/${YYYY}/${MM}"
 export cleanerLog="${logDir}/cleanerLog.${dateStamp}.txt"
-export trashCleaner="$HOME/kent/src/product/scripts/trashCleaner.csh"
-export failMail="alert@some.where"
-export failMessage="ALERT: from trashCleanMonitor.sh - the trash cleaner is failing, check ${cleanerLog}"
+export trashCleaner="$KENTHOME/src/product/scripts/trashCleaner.csh"
+export failMessage="ALERT: from trashCleanMonitor.sh - the trash cleaner is failing, check the most recent file(s) in /var/tmp/ for clues, or perhaps in the ${cleanerLog}"
 
-export ECHO="/bin/echo -e"
 
-if [ "$1" != "searchAndDestroy" ]; then
-    ${ECHO} "usage:  trashCleanMonitor.csh searchAndDestroy"
-    ${ECHO} "This script will run when given the argument searchAndDestroy"
-    ${ECHO} "which is exactly what it will do to the trash files for the"
-    ${ECHO} "genome browser.  There is no turning back after it gets going."
-    ${ECHO} "This script is actually a monitor on the actual trash cleaner:"
-    ${ECHO} "\t${trashCleaner}"
-    ${ECHO} "It will verify the trash cleaner is functioning properly and if"
-    ${ECHO} "there are problems it will email a failure message to"
-    ${ECHO} "\t${failMail}"
-    ${ECHO} "Files that belong to sessions will be moved from the directory"
-    ${ECHO} "${trashCt} to ${userCt}"
-    ${ECHO} "activity logs can be found in ${userLog}"
-    exit 255
+if [ "$2" != "searchAndDestroy" ]; then
+    ${ECHO} "trashCleanMonitor.sh: ERROR: being run without arguments ?"
+    ${ECHO} "trashCleanMonitor.sh: ERROR: being run without arguments ?" \
+      | ${mailCmd} -s "ALERT:" ${failMail} > /dev/null 2> /dev/null
+    usage
 fi
+
+if [ -f "${lockFile}" ]; then
+    ${ECHO} "lockFile ${lockFile} exists" \
+        | ${mailCmd} -s "ALERT:" ${failMail} > /dev/null 2> /dev/null
+    ps -ef | grep "trashCleaner.csh" | grep -v "grep" \
+        | ${mailCmd} -s "ALERT:" ${failMail} > /dev/null 2> /dev/null
+    exit 255
+else
+    ${ECHO} "monitor $$ "`date +%Y-%m-%dT%H:%M:%S` > "${lockFile}"
+    chmod 666 "${lockFile}"
+fi
+
 
 if [ ! -d "${logDir}" ]; then
     mkdir -p "${logDir}"
@@ -41,6 +107,9 @@ if [ ! -d "${logDir}" ]; then
     chmod 755 "${userLog}/${YYYY}"
     chmod 755 "${logDir}"
 fi
+touch "${cleanerLog}"
+chmod 666 "${cleanerLog}"
+
 $trashCleaner searchAndDestroy > "${cleanerLog}" 2>&1
 returnCode=$?
 if [ "${returnCode}" -eq "0" ]; then
@@ -55,5 +124,10 @@ else
 	| mail -s "ALERT: TRASH" ${failMail} > /dev/null 2> /dev/null
     exit 255
 fi
+
+# it is expected that trashCleaner.csh will remove the lock file
+# when it can exit successfully
+
+# rm -f "${lockFile}"
 
 exit 0
