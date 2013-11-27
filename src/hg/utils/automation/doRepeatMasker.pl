8f63ebe026e4099af589a83e8951088cb9b71050
Hiram Clawson <hiram@soe.ucsc.edu>
2013-11-27 10:26:20 -0800

    now allow bash scripts in auto jobs and construct bigBed files for repeat masker result refs #11681

--- src/hg/utils/automation/doRepeatMasker.pl
+++ src/hg/utils/automation/doRepeatMasker.pl
@@ -422,19 +422,69 @@ sub doInstall {
   my $whatItDoes =
 "It loads $db.sorted.fa.out into the$split rmsk table and $db.nestedRepeats.bed\n" .
 "into the nestedRepeats table.  It also installs the masked 2bit.";
-  my $bossScript = new HgRemoteScript("$runDir/doLoad.csh", $dbHost,
+  my $bossScript = newBash HgRemoteScript("$runDir/doLoad.bash", $dbHost,
 				      $runDir, $whatItDoes);
 
   $split = "-nosplit";
   $split = "-split" if ($opt_splitTables);
   my $installDir = "$HgAutomate::clusterData/$db";
   $bossScript->add(<<_EOF_
-hgLoadOut -table=rmsk $split $db $db.sorted.fa.out
-hgLoadBed $db nestedRepeats $db.nestedRepeats.bed \\
+export db=$db
+
+hgLoadOut -table=rmsk $split \$db \$db.sorted.fa.out
+hgLoadOut -tabFile=\$db.rmsk.tab -table=rmsk -nosplit \$db \$db.sorted.fa.out
+# construct bbi files for assembly hub
+rm -fr classBed classBbi rmskClass
+mkdir classBed classBbi rmskClass
+sort -k12,12 \$db.rmsk.tab \\
+  | splitFileByColumn -ending=tab  -col=12 -tab stdin rmskClass
+for T in SINE LINE LTR DNA Simple Low_complexity Satellite
+do
+    fileCount=`(ls rmskClass/\${T}*.tab 2> /dev/null || true) | wc -l`
+    if [ "\$fileCount" -gt 0 ]; then
+       echo "working: "`ls rmskClass/\${T}*.tab | xargs echo`
+       \$HOME/kent/src/hg/utils/automation/rmskBed6+10.pl rmskClass/\${T}*.tab \\
+        | sort -k1,1 -k2,2n > classBed/\$db.rmsk.\${T}.bed
+       bedToBigBed -tab -type=bed6+10 -as=\$HOME/kent/src/hg/lib/rmskBed6+10.as \\
+         classBed/\$db.rmsk.\${T}.bed ../../chrom.sizes \\
+           classBbi/\$db.rmsk.\${T}.bb
+    fi
+done
+fileCount=`(ls rmskClass/*RNA.tab 2> /dev/null || true) | wc -l`
+if [ "\$fileCount" -gt 0 ]; then
+  echo "working: "`ls rmskClass/*RNA.tab | xargs echo`
+  \$HOME/kent/src/hg/utils/automation/rmskBed6+10.pl rmskClass/*RNA.tab \\
+     | sort -k1,1 -k2,2n > classBed/\$db.rmsk.RNA.bed
+  bedToBigBed -tab -type=bed6+10 -as=\$HOME/kent/src/hg/lib/rmskBed6+10.as \\
+     classBed/\$db.rmsk.RNA.bed ../../chrom.sizes \\
+        classBbi/\$db.rmsk.RNA.bb
+fi
+echo "working: "`ls rmskClass/*.tab | egrep -v "/SIN|/LIN|/LT|/DN|/Simple|/Low_complexity|/Satellit|RNA.tab" | xargs echo`
+\$HOME/kent/src/hg/utils/automation/rmskBed6+10.pl `ls rmskClass/*.tab | egrep -v "/SIN|/LIN|/LT|/DN|/Simple|/Low_complexity|/Satellit|RNA.tab"` \\
+        | sort -k1,1 -k2,2n > classBed/\$db.rmsk.Other.bed
+bedToBigBed -tab -type=bed6+10 -as=\$HOME/kent/src/hg/lib/rmskBed6+10.as \\
+  classBed/\$db.rmsk.Other.bed ../../chrom.sizes \\
+    classBbi/\$db.rmsk.Other.bb
+
+export bbiCount=`for F in classBbi/*.bb; do bigBedInfo \$F | grep itemCount; done | awk '{print \$NF}' | sed -e 's/,//g' | ave stdin | grep total | awk '{print \$2}' | sed -e 's/.000000//'`
+
+export firstTabCount=`cat \$db.rmsk.tab | wc -l`
+export splitTabCount=`cat rmskClass/*.tab | wc -l`
+
+if [ "\$firstTabCount" -ne \$bbiCount ]; then
+   echo "\$db.rmsk.tab count: \$firstTabCount, split class tab file count: \$splitTabCount, bbi class item count:  \$bbiCount"
+   echo "ERROR: did not account for all items in rmsk class bbi construction" 1>&2
+   exit 255
+fi
+wc -l classBed/*.bed > \$db.class.profile.txt
+wc -l rmskClass/*.tab >> \$db.class.profile.txt
+rm -fr rmskClass classBed \$db.rmsk.tab
+
+hgLoadBed \$db nestedRepeats \$db.nestedRepeats.bed \\
   -sqlTable=\$HOME/kent/src/hg/lib/nestedRepeats.sql
 
-rm -f $installDir/$db.rmsk.2bit
-ln -s $buildDir/$db.rmsk.2bit $installDir/$db.rmsk.2bit
+rm -f $installDir/\$db.rmsk.2bit
+ln -s $buildDir/\$db.rmsk.2bit $installDir/\$db.rmsk.2bit
 _EOF_
   );
   $bossScript->execute();
