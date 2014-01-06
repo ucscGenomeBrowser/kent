bebe7d814f0c6cde919f3b7ed64edaa10df79dfd
Chin Li <chinhli@soe.ucsc.edu>
2014-01-06 06:18:07 -0800

    Header for edwWebXSendFile

---
+++ src/hg/encode3/encodeDataWarehouse/edwWebXSendFile/edwWebXSendFile.h
@@ -0,0 +1,11 @@
+/* edwWebXSendFile.h  */
+
+#ifndef edwWebXSendFile_H
+#define edwWebXSendFile_H
+
+/* ---- global variables ---- */
+
+#define CFG_ENCODEDATAWAREHOUSE_DATAROOT "encodeDataWarehouse.dataRoot"
+#define CFG_ENCODEDATAWAREHOUSE_KEY "encodeDataWarehouse.key"
+#endif /* edwWebXSendFile_H */
+
