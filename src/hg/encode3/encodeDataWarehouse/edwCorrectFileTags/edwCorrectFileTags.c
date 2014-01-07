1d42550f45020b234c6c815605afff2da36b3ffc
Jim Kent <kent@soe.ucsc.edu>
2014-01-07 15:45:08 -0800

    Changing program so that it can add new tags as well as change existing ones.

--- src/hg/encode3/encodeDataWarehouse/edwCorrectFileTags/edwCorrectFileTags.c
+++ src/hg/encode3/encodeDataWarehouse/edwCorrectFileTags/edwCorrectFileTags.c
@@ -63,6 +63,18 @@ if (result == 0)
 return result;
 }
 
+struct hash *hashOfFields(struct fieldedTable *table)
+/* Return table keyed by table fields with integer values (index of field). */
+{
+struct hash *hash = hashNew(4);
+int i;
+for (i=0; i<table->fieldCount;  ++i)
+    {
+    hashAddInt(hash, table->fields[i], i);
+    }
+return hash;
+}
+
 void edwCorrectFileTags(char *tabFileName)
 /* edwCorrectFileTags - Use this to correct tags in the edwFile table and corresponding fields 
  * in the edwValidFile table without forcing a validateManifest rerun or a reupload.. */
@@ -74,23 +86,43 @@ struct fieldedTable *table = fieldedTableFromTabFile(tabFileName, tabFileName,
 	requiredFields, ArraySize(requiredFields));
 checkForbiddenFields(table, forbiddenFields, ArraySize(forbiddenFields));
 int accessionIx = stringArrayIx("accession", table->fields, table->fieldCount);
+verbose(1, "Loaded %d tags for %d accessions\n", table->fieldCount-1, slCount(table->rowList));
 
+struct hash *tableFields = hashOfFields(table);
 struct fieldedRow *fr;
 for (fr = table->rowList; fr != NULL; fr = fr->next)
     {
     char *acc = fr->row[accessionIx];
     long long id = edwNeedFileIdForLicensePlate(conn, acc);
     struct edwFile *ef = edwFileFromId(conn, id);
+    struct cgiDictionary *dict = cgiDictionaryFromEncodedString(ef->tags);
+    struct dyString *tags = dyStringNew(0);
+
+    /* Loop through fields of table adding them to tags string */
     int i;
-    char *tags = ef->tags;
     for (i=0; i<table->fieldCount; ++i)
         {
 	if (i != accessionIx)
-	    tags = cgiStringNewValForVar(tags, table->fields[i], fr->row[i]);
+	    cgiEncodeIntoDy(table->fields[i], fr->row[i], tags);
 	}
-    edwFileResetTags(conn, ef, tags);
+
+    /* Loop through existing tags and add ones not in table back. */
+    struct cgiVar *cgiVar;
+    for (cgiVar = dict->list; cgiVar != NULL; cgiVar = cgiVar->next)
+        {
+	if (!hashLookup(tableFields, cgiVar->name))
+	    cgiEncodeIntoDy(cgiVar->name, cgiVar->val, tags);
+	}
+
+    /* Reset new tags and schedule revalidation */
+    edwFileResetTags(conn, ef, tags->string);
+
+    /* Clean up temps. */
+    dyStringFree(&tags);
+    cgiDictionaryFree(&dict);
     edwFileFree(&ef);
     }
+hashFree(&tableFields);
 }
 
 int main(int argc, char *argv[])
