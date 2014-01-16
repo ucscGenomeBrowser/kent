b76ce965ae7e6121f4d5e27def08e2615a7499ce
Jim Kent <kent@soe.ucsc.edu>
2014-01-16 11:46:42 -0800

    Added info command.  Polishing and extending status command.

--- src/hg/encode3/eap/eapMonitor/eapMonitor.c
+++ src/hg/encode3/eap/eapMonitor/eapMonitor.c
@@ -25,6 +25,7 @@ errAbort(
   "   eapMonitor command\n"
   "commands:\n"
   "   status - print overall pipeline activity\n"
+  "   info - information about configuration\n"
   "   chill - remove jobs that haven't started from job table, but keep running ones going\n"
   "   cull N - remove jobs that have been running more than N days\n"
   "   err [id] print info about either most recent error or error of given id#\n"
@@ -73,22 +74,21 @@ for (fi = fiList; fi != NULL; fi = fi->next)
     }
 }
 
-int countParaRunning(struct sqlConnection *conn)
+
+int countParaRunning(struct sqlConnection *conn, struct hash *parasolRunningHash)
 /* Count up number of jobs in are table that Parasol thinks are currently running */
 {
 int count = 0;
 char query[512];
 sqlSafef(query, sizeof(query), "select * from %s where startTime > 0 and endTime = 0", clTable);
-struct hash *hash = eapParasolRunningHash(clParaHost, NULL);
 struct edwAnalysisJob *job, *jobList = edwAnalysisJobLoadByQuery(conn, query);
 for (job = jobList; job != NULL; job = job->next)
     {
-    if (hashLookup(hash, job->parasolId))
+    if (hashLookup(parasolRunningHash, job->parasolId))
         {
 	++count;
 	}
     }
-hashFree(&hash);
 return count;
 }
 
@@ -98,9 +98,19 @@ int countOldJobs(struct sqlConnection *conn, double days)
 int seconds = days * 3600 * 24;
 long long limit = edwNow() - seconds;
 char query[512];
-safef(query, sizeof(query), "select count(*) from %s where startTime < %lld and endTime=0", 
+safef(query, sizeof(query), 
+    "select count(*) from %s where startTime != 0 and startTime < %lld and endTime=0", 
     clTable, limit);
 return sqlQuickNum(conn, query);
+}
+
+int printDaysOldJobs(struct sqlConnection *conn, int days)
+/* Print info about job more than %d days old if any.  Return that count.  */
+{
+int jobCount = countOldJobs(conn, days);
+if (jobCount > 0)
+    printf("Jobs %d day old:    %-4d\n", days, countOldJobs(conn, days));
+return jobCount;
 }
 
 void doStatus()
@@ -115,11 +125,8 @@ for (q = qList; q != NULL; q = q->next)
     ++qCount;
     finishedJobs += slCount(q->lineList);
     }
-printf("Queue dir: %s\n", eapParaQueues);
 printf("Jobs finished: %ld\n", finishedJobs);
 printf("Total batches: %d\n", qCount);
-
-printf("Jobs table: %s\n", clTable);
 
 struct sqlConnection *conn = edwConnect();
 char query[512];
@@ -131,29 +138,47 @@ sqlSafef(query, sizeof(query), "select count(*) from %s where startTime>0", clTa
 int dbJobsStarted = sqlQuickNum(conn, query);
 printf("DB Jobs started:   %d\n", dbJobsStarted);
 
-sqlSafef(query, sizeof(query), "select count(*) from %s where endTime > 0", clTable);
-int dbJobsFinished = sqlQuickNum(conn, query);
-printf("DB Jobs Finished:  %d\n", dbJobsFinished);
+printf("Jobs 12 hours old: %-4d\n", countOldJobs(conn, 0.5));
+printDaysOldJobs(conn, 1);
+printDaysOldJobs(conn, 2);
+printDaysOldJobs(conn, 3);
+printDaysOldJobs(conn, 5);
+printDaysOldJobs(conn, 10);
 
-sqlSafef(query, sizeof(query), "select count(*) from %s where returnCode != 0", clTable);
-int dbJobsCrashed = sqlQuickNum(conn, query);
-printf("DB Jobs crashed:   %d\n", dbJobsCrashed);
+struct hash *runningHash = eapParasolRunningHash(clParaHost, NULL);
+int runningCount = countParaRunning(conn, runningHash);
+if (runningCount > 0)
+    {
+    sqlSafef(query, sizeof(query), "select min(startTime) from %s where startTime>0 and endTime=0",
+	clTable);
+    double oldestSeconds = edwNow() - sqlQuickNum(conn, query);
+    printf("Oldest job:        %-5.2f hours\n", oldestSeconds/(3600));
+    sqlSafef(query, sizeof(query), "select max(startTime) from %s where startTime>0 and endTime=0",
+	clTable);
+    double youngestSeconds = edwNow() - sqlQuickNum(conn, query);
+    printf("Youngest job:      %-5.2f hours\n", youngestSeconds/(3600));
+    }
 
 sqlSafef(query, sizeof(query), "select count(*) from %s where startTime>0 and endTime=0", clTable);
 int dbJobsPending = sqlQuickNum(conn, query);
-printf("DB Jobs scheduled: %d\n", dbJobsPending);
+printf("Jobs scheduled: %7d\n", dbJobsPending);
 
 sqlSafef(query, sizeof(query), "select count(*) from %s where startTime=0", clTable);
 int dbJobsQueued = sqlQuickNum(conn, query);
-printf("DB Jobs queued:    %d\n", dbJobsQueued);
+printf("Jobs queued:    %7d\n", dbJobsQueued);
 
-printf("Jobs 1 days old:   %d\n", countOldJobs(conn, 1));
-printf("Jobs 2 days old:   %d\n", countOldJobs(conn, 2));
-printf("Jobs 3 days old:   %d\n", countOldJobs(conn, 3));
-printf("Jobs 5 days old:   %d\n", countOldJobs(conn, 5));
-printf("Jobs 10 days old:  %d\n", countOldJobs(conn, 10));
+sqlSafef(query, sizeof(query), "select count(*) from %s where returnCode != 0", clTable);
+int dbJobsCrashed = sqlQuickNum(conn, query);
+printf("Jobs crashed:   %7d\n", dbJobsCrashed);
 
-printf("parasol running:   %d\n", countParaRunning(conn));
+sqlSafef(query, sizeof(query), "select max(startTime) from %s where returnCode != 0", clTable);
+long long lastCrashTime = sqlQuickLongLong(conn, query);
+printf("Last crash:     %5.2g hours ago\n", (double)(edwNow() - lastCrashTime)/3600);
+
+sqlSafef(query, sizeof(query), "select count(*) from %s where endTime > 0", clTable);
+int dbJobsFinished = sqlQuickNum(conn, query);
+printf("Jobs finished:  %7d\n", dbJobsFinished);
+
 }
 
 void doChill()
@@ -189,6 +214,16 @@ for (id = idList; idList != NULL; id = id->next)
     }
 }
 
+void doInfo()
+/* Print out configuration info. */
+{
+printf("Queue dir:     %s\n", eapParaQueues);
+printf("Database:      %s\n", clDatabase);
+printf("Jobs table:    %s\n", clTable);
+printf("Parasol host:  %s\n", clParaHost);
+}
+
+
 void eapMonitor(char *command, int argc, char **argv)
 /* eapMonitor - Monitor jobs running through the pipeline.. */
 {
@@ -206,6 +241,10 @@ else if (sameString(command, "cull"))
          usage();
     double days = sqlDouble(argv[0]);
     doCull(days);
+    }
+else if (sameString(command, "info"))
+    {
+    doInfo();
     }
 else if (sameString(command, "err"))
     {
