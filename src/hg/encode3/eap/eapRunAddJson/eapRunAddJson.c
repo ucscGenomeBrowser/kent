3a33b7ae84b0357df69a7daf53cccb1e7d7df6c4
Jim Kent <kent@soe.ucsc.edu>
2014-01-26 10:05:52 -0800

    Reworking it so that inputs (or outputs) of same name get bundled together in JSON list

--- src/hg/encode3/eap/eapRunAddJson/eapRunAddJson.c
+++ src/hg/encode3/eap/eapRunAddJson/eapRunAddJson.c
@@ -84,9 +84,41 @@ dyStringAppendC(dy, '}');
 dyJsonEndLine(dy, isMiddle);
 }
 
+void printIoListJson(struct sqlConnection *conn,
+    struct dyString *dy, struct eapInput *list, char *listName)
+/* Print out input (or output) list to JSON in dy*/
+{
+dyJsonListStart(dy, listName);
+struct eapInput *el;
+char *lastName = "";
+for (el = list; el != NULL; el = el->next)
+    {
+    if (!sameString(lastName, el->name))
+        {
+	dyJsonObjectStart(dy);
+	dyJsonString(dy, "type", el->name, TRUE);
+	dyStringAppend(dy, "\"value\": [");
+	}
+    struct edwValidFile *vf = edwValidFileFromFileId(conn, el->fileId);
+    assert(vf != NULL);
+    dyStringPrintf(dy, "\"%s\"", vf->licensePlate);
+    edwValidFileFree(&vf);
+    struct eapInput *next = el->next;
+    if (next != NULL && sameString(next->name, el->name))
+        dyStringAppendC(dy, ',');
+    else
+	{
+	dyStringAppend(dy, "]\n");
+	dyJsonObjectEnd(dy, el->next != NULL);
+	}
+    lastName = el->name;
+    }
+dyJsonListEnd(dy, TRUE);
+}
+
 char *jsonForRun(struct sqlConnection *conn, struct eapRun *run,  struct eapStep *step,
-    struct edwFile *inputFileList, struct edwFile *outputFileList, struct eapJob *job)
-/* Generate json for run given input and output file lists. */
+    struct eapInput *inList, struct eapOutput *outList, struct eapJob *job)
+/* Generate json for run given input and output lists. */
 {
 struct dyString *dy = dyStringNew(0);
 
@@ -123,38 +155,11 @@ while ((row = sqlNextRow(sr)) != NULL)
 dyJsonListEnd(dy, TRUE);
 
 /* Print input list */
-dyJsonListStart(dy, "inputs");
-struct edwFile *ef;
-int i;
-for (i=0, ef = inputFileList; ef != NULL; ef = ef->next, ++i)
-    {
-    dyJsonObjectStart(dy);
-    dyJsonString(dy, "type", step->inputTypes[i], TRUE);
-    dyStringAppend(dy, "\"value\": [");
-    struct edwValidFile *vf = edwValidFileFromFileId(conn, ef->id);
-    assert(vf != NULL);
-    dyStringPrintf(dy, "\"%s\"", vf->licensePlate);
-    edwValidFileFree(&vf);
-    dyStringAppend(dy, "]\n");
-    dyJsonObjectEnd(dy, ef->next != NULL);
-    }
-dyJsonListEnd(dy, TRUE);
+printIoListJson(conn, dy, inList, "inputs");
 
-/* Print output list */
-dyJsonListStart(dy, "outputs");
-for (i=0, ef = outputFileList; ef != NULL; ef = ef->next, ++i)
-    {
-    dyJsonObjectStart(dy);
-    dyJsonString(dy, "type", step->outputTypes[i], TRUE);
-    dyStringAppend(dy, "\"value\": [");
-    struct edwValidFile *vf = edwValidFileFromFileId(conn, ef->id);
-    assert(vf != NULL);
-    dyStringPrintf(dy, "\"%s\"", vf->licensePlate);
-    edwValidFileFree(&vf);
-    dyStringAppend(dy, "]\n");
-    dyJsonObjectEnd(dy, ef->next != NULL);
-    }
-dyJsonListEnd(dy, TRUE);
+/* Do some checking eapInput and eapOutput really same before casting. */
+assert(EAPINPUT_NUM_COLS == EAPOUTPUT_NUM_COLS);
+printIoListJson(conn, dy, (struct eapInput *)outList, "outputs");
 
 /* Print last field and finish up */
 dyJsonString(dy, "command_line", job->commandLine, FALSE);
@@ -178,25 +183,19 @@ if (step == NULL)
 
 /* Get list of input files */
 safef(query, sizeof(query), 
-    "select edwFile.* from edwFile,eapInput where edwFile.id=eapInput.fileId "
-    " and eapInput.runId = %u "
-    " order by eapInput.id"
-    , analysisRunId);
-struct edwFile *inputFileList = edwFileLoadByQuery(conn, query);
+    "select * from eapInput where eapInput.runId = %u order by eapInput.id" , analysisRunId);
+struct eapInput *inputList = eapInputLoadByQuery(conn, query);
 
 /* Get list of output files */
 safef(query, sizeof(query), 
-    "select edwFile.* from edwFile,eapOutput where edwFile.id=eapOutput.fileId "
-    " and eapOutput.runId = %u "
-    " order by eapOutput.id"
-    , analysisRunId);
-struct edwFile *outFile, *outFileList = edwFileLoadByQuery(conn, query);
+    "select * from eapOutput where eapOutput.runId = %u order by eapOutput.id" , analysisRunId);
+struct eapOutput *out, *outList = eapOutputLoadByQuery(conn, query);
 
 /* Count how many outputs are validated */
 int validCount = 0;
-for (outFile = outFileList; outFile != NULL; outFile = outFile->next)
+for (out = outList; out != NULL; out = out->next)
     {
-    struct edwValidFile *vf = edwValidFileFromFileId(conn, outFile->id);
+    struct edwValidFile *vf = edwValidFileFromFileId(conn, out->fileId);
     if (vf != NULL)
 	++validCount;
     }
@@ -206,7 +205,7 @@ if (validCount == step->outCount)
     {
     sqlSafef(query, sizeof(query), "select * from eapJob where id=%u", run->jobId);
     struct eapJob *job = eapJobLoadByQuery(conn, query);
-    run->jsonResult = jsonForRun(conn, run, step, inputFileList, outFileList, job);
+    run->jsonResult = jsonForRun(conn, run, step, inputList, outList, job);
     verbose(1, "creating json of %d characters\n", (int)strlen(run->jsonResult));
     jsonParse(run->jsonResult);  // Just for validation
 
