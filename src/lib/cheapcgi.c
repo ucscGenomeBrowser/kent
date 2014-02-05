ce40e23f075832f24396e2c2638ee1dfad83acca
Galt Barber <galt@soe.ucsc.edu>
2014-02-04 17:26:46 -0800

    Fixes #12630. This addresses a bug in + handling in FTP URLs which only have a path part and no query part.  Added cgiDecodeFull function to parallel the existing cgiEncodeFull function. These functions ignore the + character when encoding.

--- src/lib/cheapcgi.c
+++ src/lib/cheapcgi.c
@@ -952,6 +952,31 @@ for (i=0; i<inLength;++i)
 *out++ = 0;
 }
 
+void cgiDecodeFull(char *in, char *out, int inLength)
+/* Out will be a cgi-decoded version of in (no space from plus!).
+ * Out will be a little shorter than in typically, and
+ * can be the same buffer. */
+{
+char c;
+int i;
+for (i=0; i<inLength;++i)
+    {
+    c = *in++;
+    if (c == '%')
+	{
+	int code;
+        if (sscanf(in, "%2x", &code) != 1)
+	    code = '?';
+	in += 2;
+	i += 2;
+	*out++ = code;
+	}
+    else
+	*out++ = c;
+    }
+*out++ = 0;
+}
+
 char *cgiEncode(char *inString)
 /* Return a cgi-encoded version of inString.
  * Alphanumerics kept as is, space translated to plus,
