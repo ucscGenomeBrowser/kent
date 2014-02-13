163de1280de46d8c3067ffd5996fa513af7485a6
Jim Kent <kent@soe.ucsc.edu>
2014-02-12 21:59:47 -0800

    Adding table and fastq-file-list output

--- src/hg/encode3/eap/eapToHub/eapToHub.c
+++ src/hg/encode3/eap/eapToHub/eapToHub.c
@@ -4,6 +4,8 @@
 #include "hash.h"
 #include "options.h"
 #include "portable.h"
+#include "obscure.h"
+#include "htmshell.h"
 #include "../../encodeDataWarehouse/inc/encodeDataWarehouse.h"
 #include "../../encodeDataWarehouse/inc/edwLib.h"
 #include "eapDb.h"
@@ -225,6 +227,27 @@ else
 return cloneString(buf);
 }
 
+char *shortOutputType(char *outputType)
+/* Return a shortened version of outputType if possible */
+{
+if (sameString(outputType, "pooled_signal"))
+    return "signal";
+else if (sameString(outputType, "replicated_narrowPeak"))
+    return "narrow";
+else if (sameString(outputType, "replicated_broadPeak"))
+    return "broad";
+else if (sameString(outputType, "alignments"))
+    return "bam";
+else if (sameString(outputType, "hotspot_signal"))
+    return "signal";
+else if (sameString(outputType, "hotspot_narrow_peaks"))
+    return "narrow";
+else if (sameString(outputType, "hotspot_broad_peaks"))
+    return "broad";
+else
+    return outputType;
+}
+
 void outputMatchingTrack(char *parentName, struct fullExperiment *exp, struct replicate *rep, 
     char *outputType, struct vFile *vf,  char *indent, FILE *f)
 /* If vf is of outputType then write a track based around it to f */
@@ -238,10 +261,11 @@ if (vf != NULL)
 	    repName = "pooled";
 	fprintf(f, "%strack %s\n", indent, vf->valid->licensePlate);
 	char *biosample = exp->exp->biosample;
-	fprintf(f, "%sshortLabel %s %s\n", indent, biosample, repName);
+	fprintf(f, "%sshortLabel %s %s %c\n", indent, biosample, 
+		shortOutputType(outputType), repName[0]);
 	fprintf(f, "%sparent %s\n", indent, parentName);
-	fprintf(f, "%slongLabel %s %s %s from %s\n", 
-	    indent, biosample, exp->exp->dataType, repName, exp->exp->lab);
+	fprintf(f, "%slongLabel %s %s replicate %s from %s %s\n", 
+	    indent, biosample, exp->exp->dataType, repName, exp->exp->lab, vf->valid->licensePlate);
 	fprintf(f, "%ssubGroups view=%s biosample=%s replicate=%s\n", 
 		indent, outputType, biosample, repName);
 	fprintf(f, "%sbigDataUrl /warehouse/%s\n", indent, vf->file->edwFileName);
@@ -312,11 +336,11 @@ for (exp = expList; exp != NULL; exp = exp->next)
     }
 }
 
-void writeTrackDb(struct sqlConnection *conn, struct fullExperiment *expList, 
-    struct slName *bioList, char *outFile)
+void writeTrackDb(struct sqlConnection *conn, struct eapGraph *eg, struct fullExperiment *expList, 
+    struct slName *bioList, char *outRa)
 /* Write out trackDb info */
 {
-FILE *f = mustOpen(outFile, "w");
+FILE *f = mustOpen(outRa, "w");
 struct hash *otHash = hashNew(0);
 struct outputType *ot, *otList = outputTypesForExpList(expList, otHash);
 verbose(1, "%d outputTypes\n", slCount(otList));
@@ -360,7 +384,7 @@ fprintf(f, "dimensions dimensionY=biosample dimensionX=replicate\n");
 fprintf(f, "dragAndDrop subTracks\n");
 fprintf(f, "noInherit on\n");
 fprintf(f, "configurable on\n");
-fprintf(f, "type bigWig\n");  // Need something, evenif ignored
+fprintf(f, "type bigBed\n");  // Need something, seems like even if ignored
 fprintf(f, "\n");
 
 /* Now move on to views */
@@ -441,7 +465,6 @@ boolean replicateIsComplete(struct sqlConnection *conn, struct replicate *r)
  * then the head of the bamList will point to a pipeline generated alignment,
  * and the bigWig/narrow/broad lists will start with files generated from it. */
 {
-uglyf("  replicateIsComplete: broad %d, narrow %d, bigWig %d, bam %d\n", slCount(r->broadList), slCount(r->narrowList), slCount(r->bigWigList), slCount(r->bamList));
 // Rule out obviously incomplete ones, missing an expected part.
 if (r->broadList == NULL || r->narrowList == NULL || r->bigWigList == NULL || r->bamList == NULL)
     return FALSE;
@@ -493,6 +516,13 @@ slAddHead(pList, rep);
 return rep;
 }
 
+void fixOutputType(struct edwValidFile *valid)
+/* Convert pooled_signal to hotspot_signal */
+{
+if (sameString("pooled_signal", valid->outputType))
+   valid->outputType = cloneString("hotspot_signal");
+}
+
 struct fullExperiment *getFullExperimentList(struct sqlConnection *conn,
     struct edwExperiment *eeList, char *assembly, struct hash **retHash)
 /* Given list of edwExperiments, return list of ones replicated with full file sets on
@@ -523,6 +553,7 @@ sqlDyStringPrintf(q, "select edwValidFile.*,edwFile.*,eapOutput.* "
 		  " from edwValidFile,edwFile,eapOutput "
                   " where edwValidFile.fileId = edwFile.id and edwFile.id = eapOutput.fileId "
 		  " and edwFile.deprecated='' and edwFile.errorMessage='' "
+		  " and edwValidFile.ucscDb != 'centro.hg19' "
 		  " and edwValidFile.ucscDb like '%%%s' and edwValidFile.experiment in ("
 		  , assembly);
 for (ee = eeList; ee != NULL; ee = ee->next)
@@ -541,6 +572,7 @@ while ((row = sqlNextRow(sr)) != NULL)
     {
     ++vCount;
     struct edwValidFile *valid = edwValidFileLoad(row);
+    fixOutputType(valid);
     struct edwFile *file = edwFileLoad(row + EDWVALIDFILE_NUM_COLS);
     struct eapOutput *eapOutput = eapOutputLoad(row + EDWVALIDFILE_NUM_COLS + EDWFILE_NUM_COLS);
     struct vFile *vf = vFileNew(file, valid, eapOutput);
@@ -551,9 +583,9 @@ while ((row = sqlNextRow(sr)) != NULL)
         slAddHead(&rep->bamList, vf);
     else if (sameString(format, "bigWig"))
         slAddHead(&rep->bigWigList, vf);
-    else if (sameString(format, "narrowPeak"))
+    else if (sameString(format, "narrowPeak") && !sameString(valid->outputType, "replicated_narrowPeak"))
         slAddHead(&rep->narrowList, vf);
-    else if (sameString(format, "broadPeak"))
+    else if (sameString(format, "broadPeak") && !sameString(valid->outputType, "replicated_broadPeak"))
         slAddHead(&rep->broadList, vf);
     }
 sqlFreeResult(&sr);
@@ -626,12 +658,11 @@ return goodList;
 }
 
 
-void writeTrackDbTxt(struct sqlConnection *conn, struct edwExperiment *eeList, 
-    char *assembly, char *outFile)
+void writeTrackDbTxt(struct sqlConnection *conn, struct eapGraph *eg, 
+    struct fullExperiment *expList, char *assembly, char *outRa)
 /* Write out a trackDb that represents the replicated hotspot experiments we have on the 
  * given assembly */
 {
-struct fullExperiment *expList = getGoodExperiments(conn, eeList, assembly);
 uglyf("%d in expList\n", slCount(expList));
 
 /* Make up a list and hash of unique biosamples. */
@@ -648,14 +679,351 @@ for (exp = expList; exp != NULL; exp = exp->next)
 	}
     }
 slSort(&bioList, slNameCmp);
-writeTrackDb(conn, expList, bioList, outFile);
+writeTrackDb(conn, eg, expList, bioList, outRa);
 }
 
+struct replicate *findReplicateInList(struct replicate *list, char *name)
+/* Find replicate of given name on list */
+{
+struct replicate *rep;
+for (rep = list; rep != NULL; rep = rep->next)
+    {
+    if (sameString(name, rep->name))
+        return rep;
+    }
+return NULL;
+}
+
+struct replicate *mustFindReplicateInList(struct replicate *list, char *name)
+/* Find next replicate of given name in list or die trying */
+{
+struct replicate *rep = findReplicateInList(list, name);
+if (rep == NULL)
+    errAbort("mustFindReplicateInList couldn't find %s in list of %d", name, slCount(list));
+return rep;
+}
+
+struct replicate *mustFindPooledReplicate(struct replicate *list)
+/* Find pooled replicate - not sure honestly if it is called '' or 'pooled' */
+{
+struct replicate *pooled = findReplicateInList(list, "");
+if (pooled == NULL)
+    pooled = mustFindReplicateInList(list, "pooled");
+return pooled;
+}
+
+struct edwBamFile *bamFilesAncestralToFile(struct sqlConnection *conn, struct eapGraph *eg, 
+    long long fileId)
+/* Given a fileId return list of all bamFiles that led to it */
+{
+struct slRef *bamRef, *bamRefList = NULL;
+eapGraphAncestorsOfFormat(eg, fileId, "bam", -1, &bamRefList);
+struct edwBamFile *bamFileList = NULL;
+for (bamRef = bamRefList; bamRef != NULL; bamRef = bamRef->next)
+    {
+    struct eapInput *bamInput = bamRef->val;
+    struct edwBamFile *bamFile = edwBamFileFromFileId(conn, bamInput->fileId);
+    slAddHead(&bamFileList, bamFile);
+    }
+slFreeList(&bamRefList);
+return bamFileList;
+}
+
+long long pooledBroadPeaksForExp(struct fullExperiment *exp)
+/* Attempt to find the pooled broad peak file associated with experiment. */
+{
+struct replicate *pooled = mustFindPooledReplicate(exp->repList);
+struct vFile *broad;
+int bestScore = 0;
+struct vFile *bestFile = NULL;
+for (broad = pooled->broadList; broad != NULL; broad = broad->next)
+    {
+    int oneScore = 1;
+    if (sameString(broad->valid->outputType, "hotspot_broad_peaks"))
+        oneScore += 100;
+    if (oneScore > bestScore)
+        {
+	bestScore = oneScore;
+	bestFile = broad;
+	}
+    }
+if (bestFile == NULL)
+    return 0;
+return bestFile->valid->fileId;
+}
+
+void bamListStats(struct edwBamFile *bamList, 
+    long long *retReadCount, long long *retMappedCount, long long *retUniqueMappedCount
+    ,double *retU4mRatio)
+/* Gather statistics on a list of bams. */
+{
+long long readCount = 0, mappedCount = 0, uniqueMappedCount = 0;
+long long u4mReadCountTotal = 0, u4mUniquePosTotal = 0, fileCount = 0;
+struct edwBamFile *bamFile;
+for (bamFile = bamList; bamFile != NULL; bamFile = bamFile->next)
+    {
+    readCount += bamFile->readCount;
+    mappedCount += bamFile->mappedCount;
+    uniqueMappedCount = bamFile->uniqueMappedCount;
+    u4mReadCountTotal += bamFile->u4mReadCount;
+    u4mUniquePosTotal += bamFile->u4mUniquePos;
+    ++fileCount;
+    }
+*retReadCount = readCount;
+*retMappedCount = mappedCount;
+*retUniqueMappedCount = uniqueMappedCount;
+*retU4mRatio = (double)u4mUniquePosTotal/u4mReadCountTotal;
+}
+
+int fullExperimentCmp(const void *va, const void *vb)
+/* Compare two fullExperiments. */
+{
+const struct fullExperiment *a = *((struct fullExperiment **)va);
+const struct fullExperiment *b = *((struct fullExperiment **)vb);
+return strcmp(a->exp->biosample, b->exp->biosample);
+}
+#ifdef SOON
+#endif /* SOON */
+
+struct edwQaEnrich *edwQaEnrichFind(struct sqlConnection *conn, long long fileId, char *target)
+/* Given a file and a target, return corresponding edwQaEnrich record if any */
+{
+char query[256];
+sqlSafef(query, sizeof(query),
+    "select edwQaEnrich.* from edwQaEnrich "
+    " join edwQaEnrichTarget on qaEnrichTargetId=edwQaEnrichTarget.id "
+    " where edwQaEnrich.fileId = %lld and edwQaEnrichTarget.name = '%s' "
+    " order by enrichment desc limit 1"
+    ,  fileId, target);
+// uglyf really?
+return edwQaEnrichLoadByQuery(conn, query);
+}
+
+void printEnrichment(struct sqlConnection *conn, long long fileId, char *target, FILE *f)
+/* Get enrichment for file and target and print it if possible. */
+{
+struct edwQaEnrich *enrich = edwQaEnrichFind(conn, fileId, target);
+if (enrich != NULL)
+    {
+    fprintf(f, "<TD>%4.2fx</TD>\n", enrich->enrichment);
+    fprintf(f, "<TD>%4.3f</TD>\n", enrich->coverage);
+    }
+else
+    {
+    fprintf(f, "<TD>n/a</TD>\n");
+    fprintf(f, "<TD>n/a</TD>\n");
+    }
+edwQaEnrichFree(&enrich);
+}
+
+boolean uglyOne;
+
+struct edwQaPairCorrelation *edwQaPairCorrelationFromFiles(struct sqlConnection *conn,
+	    long long aFileId, long long bFileId)
+/* Return edwQaPairSampleOverlap if any that exists in database betweeen two files */
+{
+char query[256];
+sqlSafef(query, sizeof(query), 
+	"select * from edwQaPairCorrelation where (elderFileId=%lld and youngerFileId=%lld) "
+	" or (elderFileId = %lld and youngerFileId=%lld) "
+	, aFileId, bFileId, bFileId, aFileId);
+return edwQaPairCorrelationLoadByQuery(conn, query);
+}
+
+struct edwQaPairSampleOverlap *edwQaPairSampleOverlapFromFiles(struct sqlConnection *conn,
+	    long long aFileId, long long bFileId)
+/* Return edwQaPairSampleOverlap if any that exists in database betweeen two files */
+{
+char query[256];
+sqlSafef(query, sizeof(query), 
+	"select * from edwQaPairSampleOverlap where (elderFileId=%lld and youngerFileId=%lld) "
+	" or (elderFileId = %lld and youngerFileId=%lld) "
+	, aFileId, bFileId, bFileId, aFileId);
+return edwQaPairSampleOverlapLoadByQuery(conn, query);
+}
+
+struct edwQaPairSampleOverlap *findSampleOverlap(struct sqlConnection *conn, 
+	struct eapGraph *eg, long long pooledFileId)
+/* Return sampleOverlap between two replicates given fileId of a pooling of 2 replicates */
+{
+struct edwQaPairSampleOverlap *result = NULL;
+struct slRef *refList = NULL;
+eapGraphAncestorsOfFormat(eg, pooledFileId, "bam", -1, &refList);
+if (refList != NULL && refList->next != NULL)
+    {
+    struct slRef *aRef = refList, *bRef = refList->next;
+    struct eapInput *aIn = aRef->val, *bIn = bRef->val;
+    result = edwQaPairSampleOverlapFromFiles(conn, aIn->fileId, bIn->fileId);
+    }
+slFreeList(&refList);
+return result;
+}
+
+struct edwQaPairCorrelation *findCorrelation(struct sqlConnection *conn, 
+	struct eapGraph *eg, long long pooledFileId)
+/* Return pairCorrelation between two replicates given fileId of a pooling of 2 replicates */
+{
+struct edwQaPairCorrelation *result = NULL;
+#ifdef SOON
+struct slRef *refList = NULL;
+eapGraphAncestorsOfFormat(eg, pooledFileId, "bam", -1, &refList);
+if (refList != NULL && refList->next != NULL)
+    {
+    struct slRef *aRef = refList, *bRef = refList->next;
+    struct eapInput *aIn = aRef->val, *bIn = bRef->val;
+    result = edwQaPairCorrelation(conn, aIn->fileId, bIn->fileId);
+    }
+slFreeList(&refList);
+#endif /* SOON */
+return result;
+}
+
+struct edwBamFile *makeBamFileList(struct sqlConnection *conn, struct vFile *vFileList)
+/* Given vFile list make bamFile list */
+{
+struct edwBamFile *bam, *bamList = NULL;
+struct vFile *vFile;
+for (vFile = vFileList; vFile != NULL; vFile = vFile->next)
+    {
+    bam = edwBamFileFromFileId(conn, vFile->file->id);
+    slAddHead(&bamList, bam);
+    }
+slReverse(&bamList);
+return bamList;
+}
+
+void printOneRep(struct sqlConnection *conn, struct eapGraph *eg,
+    struct fullExperiment *exp, struct replicate *rep, FILE *f)
+{
+long long readCount = 0, mappedCount = 0, uniqueMappedCount = 0;
+double u4mUniqueRatio = 0;
+struct edwBamFile *bamFileList = makeBamFileList(conn, rep->bamList);
+bamListStats(bamFileList, &readCount, &mappedCount, &uniqueMappedCount, &u4mUniqueRatio);
+fprintf(f, "<TD>%4.1f%%</TD>\n", 100.0 * mappedCount / readCount);
+fprintf(f, "<TD>%4.1f%%</TD>\n", 100.0 * uniqueMappedCount / readCount);
+fprintf(f, "<TD>%4.1f%%</TD>\n", 100.0 * u4mUniqueRatio);
+}
+
+void writeTableHtml(struct sqlConnection *conn, struct eapGraph *eg, 
+    struct fullExperiment *expList, char *assembly, char *outFile)
+/* Write out a table in simple html based on eeList*/
+{
+FILE *f = mustOpen(outFile, "w");
+htmStart(f, "DNase-seq experiment table");
+fprintf(f, "<TABLE>\n");
+fprintf(f, "<TR>\n");
+fprintf(f, "<TH></TH>");
+fprintf(f, "<TH></TH>");
+fprintf(f, "<TH></TH>");
+fprintf(f, "<TH>promoter</TH>");
+fprintf(f, "<TH>promoter</TH>");
+fprintf(f, "<TH>open</TH>");
+fprintf(f, "<TH>open</TH>");
+fprintf(f, "<TH>cross</TH>");
+fprintf(f, "<TH>&nbsp; </TH>");
+fprintf(f, "<TH></TH>");
+fprintf(f, "<TH></TH>");
+fprintf(f, "<TH></TH>");
+fprintf(f, "<TH></TH>");
+fprintf(f, "<TH></TH>");
+fprintf(f, "<TH></TH>");
+fprintf(f, "</TR>\n");
+fprintf(f, "<TR>\n");
+fprintf(f, "<TH>Biosample</TH>");
+fprintf(f, "<TH>Reps</TH>");
+fprintf(f, "<TH>%%map</TH>");
+fprintf(f, "<TH>enrich</TH>");
+fprintf(f, "<TH>cover</TH>");
+fprintf(f, "<TH>enrich</TH>");
+fprintf(f, "<TH>cover</TH>");
+fprintf(f, "<TH>enrich</TH>");
+fprintf(f, "<TH>&nbsp; </TH>");
+fprintf(f, "<TH>map1</TH>");
+fprintf(f, "<TH>umap1</TH>");
+fprintf(f, "<TH>U4M</TH>");
+fprintf(f, "<TH>&nbsp; </TH>");
+fprintf(f, "<TH>map2</TH>");
+fprintf(f, "<TH>umap2</TH>");
+fprintf(f, "<TH>U4M</TH>");
+fprintf(f, "</TR>\n");
+struct fullExperiment *exp;
+for (exp = expList; exp != NULL; exp = exp->next)
+    {
+    struct edwExperiment *ee = exp->exp;
+    fprintf(f, "<TR>\n");
+    fprintf(f, "<TD>%s</TD>\n", ee->biosample);
+    fprintf(f, "<TD>%d</TD>\n", slCount(exp->repList)-1);
+    long long pooledPeakId = pooledBroadPeaksForExp(exp);
+    if (pooledPeakId > 0)
+	{
+	struct edwBamFile *bamFileList = bamFilesAncestralToFile(conn, eg, pooledPeakId);
+	long long readCount = 0, mappedCount = 0, uniqueMappedCount = 0;
+	double u4mUniqueRatio = 0;
+	bamListStats(bamFileList, &readCount, &mappedCount, &uniqueMappedCount, &u4mUniqueRatio);
+	fprintf(f, "<TD>%4.1f%%\n", 100.0 * mappedCount / readCount);
+	printEnrichment(conn, pooledPeakId, "promoter", f);
+	printEnrichment(conn, pooledPeakId, "open", f);
+	}
+    else
+        {
+	fprintf(f, "<TD>n/a</TD>\n");
+	fprintf(f, "<TD>n/a</TD>\n");
+	fprintf(f, "<TD>n/a</TD>\n");
+	}
+    uglyOne = (pooledPeakId == 74961);
+    struct edwQaPairSampleOverlap *so = findSampleOverlap(conn, eg, pooledPeakId);
+    if (so != NULL)
+        {
+	fprintf(f, "<TD>%4.2fx</TD>\n", so->sampleSampleEnrichment);
+	}
+    else
+        {
+	uglyf("Trouble finding sample/overlap for file %lld exp %s\n", pooledPeakId, exp->name);
+	fprintf(f, "<TD>n/a</TD>\n");
+	}
+    fprintf(f, "<TD>&nbsp;</TD>\n");
+    printOneRep(conn, eg, exp, mustFindReplicateInList(exp->repList, "1"), f);
+    fprintf(f, "<TH>&nbsp; </TH>");
+    printOneRep(conn, eg, exp, mustFindReplicateInList(exp->repList, "2"), f);
+    fprintf(f, "</TR>\n");
+    }
+fprintf(f, "</TABLE>\n");
+htmEnd(f);
+carefulClose(&f);
+}
+
+void writeFastqTab(struct sqlConnection *conn, struct eapGraph *eg, 
+    struct fullExperiment *expList, char *assembly, char *outFile)
+/* Write out a table in simple html based on eeList*/
+{
+FILE *f = mustOpen(outFile, "w");
+struct fullExperiment *exp;
+for (exp = expList; exp != NULL; exp = exp->next)
+    {
+    long long pooledPeakId = pooledBroadPeaksForExp(exp);
+    if (pooledPeakId > 0)
+	{
+	struct slRef *ref, *refList = NULL;
+	eapGraphAncestorsOfFormat(eg, pooledPeakId, "fastq", -1, &refList);
+	for (ref = refList; ref != NULL; ref = ref->next)
+	    {
+	    struct eapInput *in = ref->val;
+	    struct edwFile *file = edwFileFromId(conn, in->fileId);
+	    fprintf(f, "%s\t%s\t%s\n", file->edwFileName, exp->name, exp->exp->biosample);
+	    edwFileFree(&file);
+	    }
+	slFreeList(&refList);
+	}
+    }
+
+carefulClose(&f);
+}
 
 void eapToHub(char *outDir)
 /* eapToHub - Convert some analysis results to a hub for easy viz.. */
 {
 struct sqlConnection *conn = eapConnect();
+struct eapGraph *eg = eapGraphNew(conn);
 makeDirsOnPath(outDir);
 char path[PATH_LEN];
 safef(path, sizeof(path), "%s/%s", outDir, "hub.txt");
@@ -670,7 +1038,13 @@ for (i=0; i<ArraySize(assemblyArray); ++i)
     safef(path, sizeof(path), "%s/%s", outDir, a);
     makeDir(path);
     safef(path, sizeof(path), "%s/%s/%s", outDir, a, "trackDb.txt");
-    writeTrackDbTxt(conn, eeList, a, path);
+    struct fullExperiment *expList = getGoodExperiments(conn, eeList, a);
+    slSort(&expList, fullExperimentCmp);
+    writeTrackDbTxt(conn, eg, expList, a, path);
+    safef(path, sizeof(path), "%s/%s/%s", outDir, a, "table.html");
+    writeTableHtml(conn, eg, expList, a, path);
+    safef(path, sizeof(path), "%s/%s/%s", outDir, a, "fastq.tab");
+    writeFastqTab(conn, eg, expList, a, path);
     }
 }
 
