9b49ce45ea68e2fb8a677f99963197328b2a77fa
Jim Kent <kent@soe.ucsc.edu>
2014-03-14 11:27:21 -0700

    eapFreen starts testing posts of JSON.  Passwords scribbled in source before commit.

--- src/hg/encode3/eap/eapFreen/eapFreen.c
+++ src/hg/encode3/eap/eapFreen/eapFreen.c
@@ -4,6 +4,8 @@
 #include "linefile.h"
 #include "hash.h"
 #include "options.h"
+#include "obscure.h"
+#include "net.h"
 #include "../../encodeDataWarehouse/inc/encodeDataWarehouse.h"
 #include "../../encodeDataWarehouse/inc/edwLib.h"
 #include "eapDb.h"
@@ -11,13 +13,17 @@
 #include "eapGraph.h"
 #include "longList.h"
 
+char *host = "ec2-54-193-134-19.us-west-1.compute.amazonaws.com";
+char *userId = "XXXXXXXX";
+char *password = "xxxxxxxxxxxxxxxx";
+
 void usage()
 /* Explain usage and exit. */
 {
 errAbort(
   "eapFreen - Little program to help test things and figure them out.  It has no permanent one fixed purpose.\n"
   "usage:\n"
-  "   eapFreen output\n"
+  "   eapFreen type input.json\n"
   "options:\n"
   "   -xxx=XXX\n"
   );
@@ -28,94 +34,43 @@ static struct optionSpec options[] = {
    {NULL, 0},
 };
 
-void deprecateRun(struct eapGraph *eg, struct eapRun *run, FILE *f, 
-    struct edwValidFile *v1, struct edwValidFile *v2)
-/* Write code to f to deprecate run. */
+void eapFreen(char *type, char *jsonFile)
+/* eapFreen - Little program to help test things and figure them out.  It has no permanent one 
+ * fixed purpose.. */
 {
-struct slRef *ref, *refList = eapGraphRunOutputs(eg, run->id);
-for (ref = refList; ref != NULL; ref = ref->next)
-    {
-    struct eapOutput *out = ref->val;
-    fprintf(f, "# Fail on rep '%s' vs '%s'\n", v1->replicate, v2->replicate);
-    fprintf(f, "update edwFile set deprecated='Analysis run on wrong inputs.' where id = %u;\n", 
-	out->fileId);
-    }
-}
+if (!sameString(type, "software") && !sameString(type, "software_version") 
+    && !sameString(type, "analysis_step") && !sameString(type, "analysis_run"))
+    errAbort("Unrecognized type %s", type);
+char *jsonText = NULL;
+size_t jsonSize = 0;
+readInGulp(jsonFile, &jsonText, &jsonSize);
 
-void embraceRun(struct eapGraph *eg, struct eapRun *run, FILE *f,
-    struct edwValidFile *v1, struct edwValidFile *v2)
-/* Write comment saying how good run was. */
-{
-struct slRef *ref, *refList = eapGraphRunOutputs(eg, run->id);
-for (ref = refList; ref != NULL; ref = ref->next)
-    {
-    struct eapOutput *out = ref->val;
-    struct edwValidFile *vf = eapGraphValidFromId(eg, out->fileId);
-    fprintf(f, "# %u %s seems to originate from two replicates '%s' and '%s'\n", 
-	vf->fileId, vf->licensePlate, v1->replicate, v2->replicate);
-    }
-}
+/* Construct dyString for URL */
+struct dyString *dyUrl = dyStringNew(0);
+dyStringPrintf(dyUrl, "http://%s:%s@%s/%s/", userId, password, host, type);
+uglyf("%s\n", dyUrl->string);
 
-void eapFreen(char *output)
-/* eapFreen - Little program to help test things and figure them out.  It has no permanent one fixed purpose.. */
-{
-/* Load up our nice random access graph structure */
-struct sqlConnection *conn = eapConnect();
-struct eapGraph *eg = eapGraphNew(conn);
-FILE *f = mustOpen(output, "w");
+/* Construct dyString for http header */
+struct dyString *dyHeader = dyStringNew(0);
+dyStringPrintf(dyHeader, "Content-length: %d\r\n", (int)jsonSize);
+dyStringPrintf(dyHeader, "Content-type: text/javascript\r\n");
 
-/* Get list of all likely runs that might have output we want */
-char query[512];
-sqlSafef(query, sizeof(query), 
-    "select * from eapRun where analysisStep='replicated_hotspot' "
-    " and assemblyId in (4,5) "
-    " and createStatus > 0 ");
-struct eapRun *run, *runList = eapRunLoadByQuery(conn, query);
+/* Send header and then JSON */
+int sd = netOpenHttpExt(dyUrl->string, "POST", dyHeader->string);
+mustWriteFd(sd, jsonText, jsonSize);
 
-/* Try and figure out if input to run is appropriate - from two different replicates of same
- * experiment. */
-uglyf("Total of %d runs\n", slCount(runList));
-for (run = runList; run != NULL; run = run->next)
-    {
-    struct slRef *ref2, *ref1 = eapGraphRunInputs(eg, run->id);
-    int refCount = slCount(ref1);
-    assert(refCount == 2);
-
-    ref2 = ref1->next;
-    struct eapInput *in1 = ref1->val, *in2 = ref2->val;
-    struct edwValidFile *v1 = eapGraphValidFromId(eg, in1->fileId);
-    struct edwValidFile *v2 = eapGraphValidFromId(eg, in2->fileId);
-    if (sameString(v1->replicate, v2->replicate))
-        {
-	// Replicates same, no point, we should deprecate
-	deprecateRun(eg, run, f, v1, v2);
-	}
-    else
-        {
-	if (isEmpty(v1->replicate) || isEmpty(v2->replicate))
-	    {
-	    // Both sides need to be replicates
-	    deprecateRun(eg, run, f, v1, v2);
-	    }
-	else
-	    {
-	    // Got good one, two different real replicates
-	    embraceRun(eg, run, f, v1, v2);
-	    }
-	}
-    }
-
-
-eapGraphFree(&eg);
-carefulClose(&f);
+/* Grab response */
+struct dyString *dyText = netSlurpFile(sd);
+close(sd);
+puts(dyText->string);
 }
 
 int main(int argc, char *argv[])
 /* Process command line. */
 {
 optionInit(&argc, argv, options);
-if (argc != 2)
+if (argc != 3)
     usage();
-eapFreen(argv[1]);
+eapFreen(argv[1], argv[2]);
 return 0;
 }
