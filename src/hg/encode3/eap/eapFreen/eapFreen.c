f56049f5852b69bf4734a7064921aed8cfeb2119
Jim Kent <kent@soe.ucsc.edu>
2014-03-19 19:42:18 -0700

    Freen turns it's attention from Stanford metadata submission to UCSC data submission via script.

--- src/hg/encode3/eap/eapFreen/eapFreen.c
+++ src/hg/encode3/eap/eapFreen/eapFreen.c
@@ -4,18 +4,21 @@
 #include "linefile.h"
 #include "hash.h"
 #include "options.h"
+#include "cheapcgi.h"
 #include "obscure.h"
 #include "net.h"
 #include "../../encodeDataWarehouse/inc/encodeDataWarehouse.h"
 #include "../../encodeDataWarehouse/inc/edwLib.h"
+#include "htmlPage.h"
 #include "eapDb.h"
 #include "eapLib.h"
 #include "eapGraph.h"
 #include "longList.h"
 
-char *host = "ec2-54-193-134-19.us-west-1.compute.amazonaws.com";
-char *userId = "XXXXXXXX";
-char *password = "xxxxxxxxxxxxxxxx";
+char *submitUrl = "hgwdev-kent.cse.ucsc.edu/cgi-bin/edwScriptSubmit";
+char *statusUrl = "hgwdev-kent.cse.ucsc.edu/cgi-bin/edwScriptSubmitStatus";
+char *userId = "jagegixelumaveteyigu";
+char *password = "cdefbdb6677d4c94a9f274c52e2c8dfa";
 
 void usage()
 /* Explain usage and exit. */
@@ -23,7 +26,7 @@ void usage()
 errAbort(
   "eapFreen - Little program to help test things and figure them out.  It has no permanent one fixed purpose.\n"
   "usage:\n"
-  "   eapFreen type input.json\n"
+  "   eapFreen manifestUrl\n"
   "options:\n"
   "   -xxx=XXX\n"
   );
@@ -34,43 +37,37 @@ static struct optionSpec options[] = {
    {NULL, 0},
 };
 
-void eapFreen(char *type, char *jsonFile)
+void eapFreen(char *manifestUrl)
 /* eapFreen - Little program to help test things and figure them out.  It has no permanent one 
  * fixed purpose.. */
 {
-if (!sameString(type, "software") && !sameString(type, "software_version") 
-    && !sameString(type, "analysis_step") && !sameString(type, "analysis_run"))
-    errAbort("Unrecognized type %s", type);
-char *jsonText = NULL;
-size_t jsonSize = 0;
-readInGulp(jsonFile, &jsonText, &jsonSize);
-
 /* Construct dyString for URL */
 struct dyString *dyUrl = dyStringNew(0);
-dyStringPrintf(dyUrl, "http://%s:%s@%s/%s/", userId, password, host, type);
+dyStringPrintf(dyUrl, "https://%s?user=%s&password=%s", submitUrl, userId, password);
+dyStringPrintf(dyUrl, "&url=%s", cgiEncode(manifestUrl));
 uglyf("%s\n", dyUrl->string);
 
-/* Construct dyString for http header */
-struct dyString *dyHeader = dyStringNew(0);
-dyStringPrintf(dyHeader, "Content-length: %d\r\n", (int)jsonSize);
-dyStringPrintf(dyHeader, "Content-type: text/javascript\r\n");
-
-/* Send header and then JSON */
-int sd = netOpenHttpExt(dyUrl->string, "POST", dyHeader->string);
-mustWriteFd(sd, jsonText, jsonSize);
 
 /* Grab response */
-struct dyString *dyText = netSlurpFile(sd);
-close(sd);
-puts(dyText->string);
+struct htmlPage *page = htmlPageGet(dyUrl->string);
+puts(page->fullText);
+
+dyStringClear(dyUrl);
+dyStringPrintf(dyUrl, "https://%s?user=%s&password=%s", statusUrl, userId, password);
+dyStringPrintf(dyUrl, "&url=%s", cgiEncode(manifestUrl));
+uglyf("%s\n", dyUrl->string);
+
+page = htmlPageGet(dyUrl->string);
+puts(page->fullText);
+
 }
 
 int main(int argc, char *argv[])
 /* Process command line. */
 {
 optionInit(&argc, argv, options);
-if (argc != 3)
+if (argc != 2)
     usage();
-eapFreen(argv[1], argv[2]);
+eapFreen(argv[1]);
 return 0;
 }
