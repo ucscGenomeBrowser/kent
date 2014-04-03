ea343ab465d4c85b2518439cd84b9b7fbe93ab55
Chin Li <chinhli@soe.ucsc.edu>
2014-04-03 11:12:05 -0700

    Add 3 sql file to create the 3 new tables used by Track #11226 Change the geneReviews tables of hg19 and hg18 to match NCBI. geneReviewsGrshortNBKid.sql geneReviewsGrshortTitleNBKid.sql geneReviewsGeneGRshortNBKidGRtitle.sql
    lease enter the commit message for your changes. Lines starting

---
+++ src/hg/lib/geneReviewsGrshortNBKid.sql
@@ -0,0 +1,6 @@
+CREATE TABLE geneReviewGrshortNBKid (
+    geneSymbol  varchar(255) not null,   # refSeq gene symbol
+    grShort     varchar(255) not null,   # short name for GeneReviews article
+    NBKid       varchar(255) not null,   # NCBI book ID of the review article
+    index (geneSymbol)
+);
