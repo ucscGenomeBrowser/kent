932af4b8fcc82fd3edf3688c5dfe8dbd801f62c2
Kate Rosenbloom <kate@soe.ucsc.edu>
2014-04-14 16:05:32 -0700

    Remove superluous signout button

--- src/hg/encode3/encodeDataWarehouse/edwWebCreateUser/edwWebCreateUser.c
+++ src/hg/encode3/encodeDataWarehouse/edwWebCreateUser/edwWebCreateUser.c
@@ -43,7 +43,6 @@ else
     {
     struct sqlConnection *conn = sqlConnect(edwDatabase);
     struct edwUser *user = edwUserFromEmail(conn, oldUserEmail);
-    edwPrintLogOutButton();
     if (user != NULL && edwUserIsAdmin(conn, oldUserEmail)) 
 	{
 	printf("%s is authorized to create a new user<BR>\n", oldUserEmail);
