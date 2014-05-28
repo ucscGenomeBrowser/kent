8a12eebc67dd726515ffff161ed1e4fba130b6c8
Jim Kent <kent@soe.ucsc.edu>
2014-05-27 20:04:30 -0700

    It's doing a passible imitation of a Tukey box plot.

--- src/hg/oneShot/tryGtexBoxPlot/tryGtexBoxPlot.c
+++ src/hg/oneShot/tryGtexBoxPlot/tryGtexBoxPlot.c
@@ -11,9 +11,9 @@
 #include "math.h"
 
 /* Dimensions of a image parts */
-int boxWidth = 11;
-int boxHeight = 200;
-int boxPad = 2;
+int tukWidth = 11;
+int tukHeight = 400;
+int tukPad = 2;
 int margin = 50;
 
 /* Dimensions of image overall and our portion within it. */
@@ -22,8 +22,8 @@ int imageWidth, imageHeight, innerWidth, innerHeight, innerXoff, innerYoff;
 void setImageDims(int expCount)
 /* Set up image according to count */
 {
-innerHeight = boxHeight + 2*boxPad;
-innerWidth = boxPad + expCount*(boxPad + boxWidth);
+innerHeight = tukHeight + 2*tukPad;
+innerWidth = tukPad + expCount*(tukPad + tukWidth);
 imageWidth = innerWidth + 2*margin;
 imageHeight = innerHeight + 2*margin;
 innerXoff = margin;
@@ -121,26 +121,117 @@ for (i=0; i<count; ++i)
    double r = (radius * rand()) / RAND_MAX;
    if (i&1)
        r = -r;
-   vals[i] = mean + r;
+   double x = mean + r;
+   if (x < 0) x = 0;
+   vals[i] = x;
    }
 return sv;
 }
 
+int valToY(double val, double maxVal, int height)
+/* Convert a value from 0 to maxVal to 0 to height-1 */
+{
+double scaled = val/maxVal;
+int y = scaled * (height-1);
+return height - 1 - y;
+}
+
 void drawPointSmear(struct hvGfx *hvg, int xOff, int yOff, int height, 
-    struct sampleVals *sv, double maxExp, int colorIx)
+    struct sampleVals *sv, double maxExp, int colorIx, double midClipMin, double midClipMax)
 /* Draw a point for each sample value. */
 {
 int i, size = sv->size;
 double *vals = sv->vals;
 for (i=0; i<size; ++i)
     {
-    double exp = vals[i]/maxExp;
-    int expHeight = exp * height;
-    if (expHeight < 1)
-        expHeight = 1;
-    hvGfxBox(hvg, xOff-1, yOff + height - expHeight, 3, 1, colorIx);
-    hvGfxDot(hvg, xOff, yOff + height - expHeight - 1, colorIx);
-    hvGfxDot(hvg, xOff, yOff + height - expHeight + 1, colorIx);
+    double exp = vals[i];
+    if (exp < midClipMin || exp > midClipMax)
+	{
+	int y = valToY(exp, maxExp, height) + yOff;
+	uglyf("%d ", round(exp));
+	hvGfxDot(hvg, xOff-1, y, colorIx);
+	hvGfxDot(hvg, xOff+1, y, colorIx);
+	hvGfxDot(hvg, xOff, y-1, colorIx);
+	hvGfxDot(hvg, xOff, y+1, colorIx);
+	}
+    }
+uglyf("\n");
+}
+
+double sampleValsMax(struct sampleVals *sv)
+/* Return max sample val */
+{
+double *array = sv->vals;
+int count = sv->size;
+double val = array[0];
+int i;
+for (i=1; i<count; ++i)
+    {
+    double x = array[i];
+    if (x > val)
+        val = x;
+    }
+return val;
+}
+
+double sampleValsListMax(struct sampleVals *svList)
+/* Return maximum value on list */
+{
+double val = 0;
+struct sampleVals *sv;
+for (sv = svList; sv != NULL; sv = sv->next)
+    {
+    double x = sampleValsMax(sv);
+    if (x > val)
+        val = x;
+    }
+return val;
+}
+
+void svTukay(struct hvGfx *hvg, int fillColorIx, int lineColorIx, int x, int y, 
+    struct sampleVals *sv, double maxExp)
+/* Draw a Tukey-type box plot */
+{
+int medianColorIx = lineColorIx;
+int whiskColorIx = lineColorIx;
+double q1 = 0,q3 = 0,median = 0, minVal, maxVal;
+doubleBoxWhiskerCalc(sv->size, sv->vals, &minVal, &q1, &median, &q3, &maxVal);
+double xCen = x + tukWidth/2;
+uglyf("N %d\tq1 %g\tmedian %g\tq3 %g\tsamples %d\n", sv->size, q1, median, q3, sv->size);
+if (sv->size > 1)
+    {
+    int yQ1 = valToY(q1, maxExp, tukHeight) + y;
+    int yQ3 = valToY(q3, maxExp, tukHeight) + y;
+    int yMedian = valToY(median, maxExp, tukHeight);
+    double iq3 = q3 - median;
+    double iq1 = median - q1;
+    int qHeight = yQ1 - yQ3 + 1;
+    drawOutlinedBox(hvg, x,  yQ3, tukWidth, qHeight, fillColorIx, lineColorIx);
+#ifdef SOON
+#endif /* SOON */
+    double whisk1 = q1 - 1.5 * iq1;
+    double whisk2 = q3 + 1.5 * iq3;
+    if (whisk1 < minVal)
+        whisk1 = minVal;
+    if (whisk2 > maxVal)
+        whisk2 = maxVal;
+    int yWhisk1 = valToY(whisk1, maxExp, tukHeight) + y;
+    int yWhisk2 = valToY(whisk2, maxExp, tukHeight) + y;
+    assert(yQ1 <= yWhisk1);
+    assert(yQ3 >= yWhisk2);
+    hvGfxBox(hvg, xCen, yWhisk2, 1, yQ3 - yWhisk2, whiskColorIx);
+    hvGfxBox(hvg, xCen, yQ1, 1, yWhisk1 - yQ1, whiskColorIx);
+#ifdef SOON
+#endif /* SOON */
+    hvGfxBox(hvg, x, yWhisk1, tukWidth, 1, whiskColorIx);
+    hvGfxBox(hvg, x, yWhisk2, tukWidth, 1, whiskColorIx);
+
+#ifdef OLD_BAR_GRAPH
+    drawOutlinedBox(hvg, x, y + tukHeight - expHeight, tukWidth, expHeight, 
+	    fillColorIx, lineColorIx);
+#endif /* OLD_BAR_GRAPH */
+    hvGfxBox(hvg, x, y + yMedian, tukWidth, 1, medianColorIx);
+    drawPointSmear(hvg, xCen, y, tukHeight, sv, maxExp, lineColorIx, q1, q3);
     }
 }
 
@@ -158,14 +249,9 @@ for (i=0; i<expCount; ++i, sv = sv->next)
     {
     double colPos = invExpCount * i;
     struct rgbColor fillColor = saturatedRainbowAtPos(colPos);
-    double exp = sampleValsMean(sv)/maxExp;
-    int expHeight = boxHeight * exp;
     int fillColorIx = hvGfxFindColorIx(hvg, fillColor.r, fillColor.g, fillColor.b);
-    if (sv->size > 1)
-	drawOutlinedBox(hvg, x, y + boxHeight - expHeight, boxWidth, expHeight, 
-		fillColorIx, lineColorIx);
-    drawPointSmear(hvg, x+boxWidth/2, y, boxHeight, sv, maxExp, lineColorIx);
-    x += boxWidth + boxPad;
+    svTukay(hvg, fillColorIx, lineColorIx, x, y, sv, maxExp);
+    x += tukWidth + tukPad;
     }
 }
 
@@ -178,11 +264,9 @@ int expCount = 0;
 float *expVals = NULL;
 if (!hgExpLoadVals(NULL, conn, NULL, "1007_s_at", "gnfHumanAtlas2Median", &expCount, &expVals))
     errAbort("Can't load data");
-float maxExp = maxFloat(expCount, expVals);
-uglyf("Got %d values, max is %g\n", expCount, maxExp);
 
-if (expCount > 50)
-    expCount = 50;
+if (expCount > 20)
+    expCount = 20;
 setImageDims(expCount);
 
 /* Open graphics and draw boxes for boundaries of total region and drawable. */
@@ -195,13 +279,16 @@ struct sampleVals *svList = NULL, *sv;
 int i;
 for (i=0; i<expCount; ++i)
     {
-    int rCount = rand() % 8 + 2;
-    int rRad = rand() % 1000 + 1;
+    int rCount = rand() % 7 + 2;
+    int rRad = rand() % 2000 + 100;
     sv = randomAround(expVals[i], rRad, rCount);
     slAddHead(&svList, sv);
     }
 slReverse(&svList);
-plotVals(hvg, innerXoff + boxPad, innerYoff + boxPad, svList, maxExp);
+
+double maxExp = sampleValsListMax(svList);
+uglyf("Got %d values, max is %g\n", expCount, maxExp);
+plotVals(hvg, innerXoff + tukPad, innerYoff + tukPad, svList, maxExp);
 
 hvGfxClose(&hvg);
 sqlDisconnect(&conn);
