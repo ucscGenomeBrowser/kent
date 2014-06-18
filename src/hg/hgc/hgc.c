48b2f22e167169b687932c7121ca445ea43ba166
Tim Dreszer <tdreszer@soe.ucsc.edu>
2014-06-18 14:05:08 -0700

    Angie caught my bug in code review.  Thanks! #13457.

--- src/hg/hgc/hgc.c
+++ src/hg/hgc/hgc.c
@@ -956,6 +956,7 @@ char* replaceInUrl(struct trackDb *tdb, char *url, char *idInUrl, boolean encode
 struct dyString *uUrl = NULL;
 struct dyString *eUrl = NULL;
 char startString[64], endString[64];
+char begItem[64], endItem[64];
 char *ins[11], *outs[11];
 char *eItem = (encode ? cgiEncode(idInUrl) : cloneString(idInUrl));
 
@@ -1001,7 +1002,6 @@ if (cartOptionalString(cart, "o") && cartOptionalString(cart, "t"))
     {
     int itemBeg = cartIntExp(cart, "o") + 1; // Should strip any unexpected commas
     int itemEnd = cartIntExp(cart, "t");
-    char begItem[64], endItem[64];
     safef(begItem, sizeof begItem, "%d", itemBeg);
     safef(endItem, sizeof endItem, "%d", itemEnd);
     outs[9] = begItem;
