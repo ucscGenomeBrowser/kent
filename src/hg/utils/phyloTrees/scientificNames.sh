7d6e8a43e0808e50ea43db4c43298ad0206859ff
Hiram Clawson <hiram@soe.ucsc.edu>
2014-06-20 15:53:13 -0700

    catching up common and scientific names refs #10272

--- src/hg/utils/phyloTrees/scientificNames.sh
+++ src/hg/utils/phyloTrees/scientificNames.sh
@@ -21,8 +21,13 @@ s/chlSab1/Chlorocebus_sabaeus/;
 s/chlSab2/Chlorocebus_sabaeus/;
 s/panHod1/Pantholops_hodgsonii/;
 s/capHir1/Capra_hircus/;
+s/equPrz1/Equus_przewalskii/;
 s/chrAsi1/Chrysochloris_asiatica/;
 s/colLiv1/Columba_livia/;
+s/phoRub1/Phoenicopterus_ruber_ruber/;
+s/opiHoa1/Opisthocomus_hoazin/;
+s/fulGla1/Fulmarus_glacialis/;
+s/calAnn1/Calypte_anna/;
 s/conCri1/Condylura_cristata/;
 s/criGri1/Cricetulus_griseus/;
 s/mesAur1/Mesocricetus_auratus/;
@@ -34,13 +39,35 @@ s/eptFus1/Eptesicus_fuscus/;
 s/eriEur2/Erinaceus_europaeus/;
 s/falChe1/Falco_cherrug/;
 s/falPer1/Falco_peregrinus/;
+s/aquChr1/Aquila_chrysaetos_canadensis/;
+s/halAlb1/Haliaeetus_albicilla/;
+s/manVit1/Manacus_vitellinus/;
+s/corBra1/Corvus_brachyrhynchos/;
+s/acaChl1/Acanthisitta_chloris/;
 s/cotJap1/Coturnix_japonica/;
+s/lepDis1/Leptosomus_discolor/;
+s/merNub1/Merops_nubicus/;
+s/pelCri1/Pelecanus_crispus/;
+s/phaLep1/Phaethon_lepturus/;
+s/pteGut1/Pterocles_gutturalis/;
+s/egrGar1/Egretta_garzetta/;
+s/pygAde1/Pygoscelis_adeliae/;
+s/aptFor1/Aptenodytes_forsteri/;
+s/carCri1/Cariama_cristata/;
+s/mesUni1/Mesitornis_unicolor/;
+s/eurHel1/Eurypyga_helias/;
+s/chlUnd1/Chlamydotis_undulata/;
 s/ficAlb1/Ficedula_albicollis/;
 s/ficAlb2/Ficedula_albicollis/;
 s/hapBur1/Haplochromis_burtoni/;
 s/jacJac1/Jaculus_jaculus/;
+s/nanGal1/Nannospalax_galili/;
 s/lepOcu1/Lepisosteus_oculatus/;
+s/stePar1/Stegastes_partitus/;
+s/dicLab1/Dicentrarchus_labrax/;
+s/picPub1/Picoides_pubescens/;
 s/strCam1/Struthio_camelus/;
+s/colStr1/Colius_striatus/;
 s/mayZeb1/Maylandia_zebra/;
 s/micOch1/Microtus_ochrogaster/;
 s/myoDav1/Myotis_davidii/;
@@ -61,10 +88,14 @@ s/pytBiv1/Python_bivittatus/;
 s/cheMyd1/Chelonia_mydas/;
 s/apaSpi1/Apalone_spinifera/;
 s/anaPla1/Anas_platyrhynchos/;
+s/tytAlb1/Tyto_alba/;
 s/pelSin1/Pelodiscus_sinensis/;
 s/pseHum1/Pseudopodoces_humilis/;
+s/gavSte1/Gavia_stellata/;
+s/capCar1/Caprimulgus_carolinensis/;
 s/zonAlb1/Zonotrichia_albicollis/;
 s/gavGan1/Gavialis_gangeticus/;
+s/eidHel1/Eidolon_helvum/;
 s/pteAle1/Pteropus_alecto/;
 s/punNye1/Pundamilia_nyererei/;
 s/takFla1/Takifugu_flavidus/;
@@ -79,6 +110,7 @@ s/calMil1/Callorhinchus_milii/;
 s/perManBai1/Peromyscus_maniculatus_bairdii/;
 s/panTig1/Panthera_tigris_altaica/;
 s/bubBub1/Bubalus_bubalis/;
+s/bosMut1/Bos_mutus/;
 s/lipVex1/Lipotes_vexillifer/;
 s/bosTau7/Bos_taurus/;
 s/serCan1/Serinus_canaria/;
@@ -102,6 +134,7 @@ do
        echo "$DB -> $sciName from sed statement" 1>&2
     fi
     treeDocString="${treeDocString} $DB -> $sciName ;"
+    echo "$treeDocString" 1>&2
     echo "$treeDocString"
 done | tail -1 | while read treeDocString
 do
@@ -110,3 +143,4 @@ done | sed -e "s/00*)/)/g; s/00*,/,/g"
 
 # petMar2 -> Lamprey ;" \
 #	${F} | sed -e "s/X_trop/X._trop/; s/Burton_s/Burton's/; s/David_s/David's/;"
+
