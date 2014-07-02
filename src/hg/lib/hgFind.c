ucscGenomeBrowser/kent

commit c73b604073cf90818fdfafd1c711b9b508835e52

hdb's hGetParent wasn't working because it used hMaybeTrackInfo which looks up only one track, not its parent or children.  It was used in only one place in the code, so I replaced it with a call to tdbForTrack. fixes #13477 (Search results don't open super tracks)

src/hg/lib/hgFind.c
@@ -2431,6 +2431,7 @@ char *ui = getUiUrl(cart);
 char *extraCgi = hgp->extraCgi;
 char hgAppCombiner = (strchr(hgAppName, '?')) ? '&' : '?';
 boolean containerDivPrinted = FALSE;
+struct trackDb *tdbList = NULL;
 
 if (useWeb)
     {
@@ -2442,14 +2443,13 @@ for (table = hgp->tableList; table != NULL; table = table->next)
     {
     if (table->posList != NULL)
 	{
-	char *parent = hGetParent(db, table->name);
-	char *trackName = table->name;
-	// TODO: should be able to get this from settings hash for
-	// both hub tracks and normal tracks
-	if (!isHubTrack(table->name)) 
-	    trackName = hGetTrackForTable(db, table->name);
-        if (trackName == NULL)
-            errAbort("no track for table \"%s\" found via a findSpec", table->name); // wish we had searchName
+	char *tableName = table->name;
+	if (startsWith("all_", tableName))
+	    tableName += strlen("all_");
+	struct trackDb *tdb = tdbForTrack(db, tableName, &tdbList);
+	if (!tdb)
+            errAbort("no track for table \"%s\" found via a findSpec", tableName);
+	char *trackName = tdb->track;
 	char *vis = hCarefulTrackOpenVis(db, trackName);
 	boolean excludeTable = FALSE;
         if(!containerDivPrinted)
@@ -2478,10 +2478,16 @@ for (table = hgp->tableList; table != NULL; table = table->next)
 		fprintf(f, "%s=%s&", trackName, vis);
 		// this is magic to tell the browser to make the 
 		// composite and this subTrack visible
-		if (parent)
+		if (tdb->parentName)
 		    {
-		    fprintf(f, "%s_sel=1&", trackName);
-		    fprintf(f, "%s_sel=1&", parent);
+		    if (tdbIsSuperTrackChild(tdb))
+			fprintf(f, "%s=show&", tdb->parentName);
+		    else
+			{
+			// tdb is a subtrack of a composite or a view
+			fprintf(f, "%s_sel=1&", trackName);
+			fprintf(f, "%s_sel=1&", tdb->parentName);
+			}
 		    }
 		fprintf(f, "hgFind.matches=%s,\">", encMatches);
 		htmTextOut(f, pos->name);

src/hg/inc/hdb.h
@@ -658,9 +658,6 @@ boolean hTrackCanPack(char *db, char *trackName);
 bool hTrackIsSubtrack(char *db, char *trackName);
 /* Return TRUE if this track is a subtrack. */
 
-char *hGetParent(char *db, char *subtrackName);
-/* Return parent of subtrack. */
-
 char *hGetTrackForTable(char *db, char *table);
 /* Given a table name, get first track associated with it. */
 

src/hg/lib/hdb.c
@@ -4200,28 +4200,6 @@ char *hTrackOpenVis(char *db, char *trackName)
 return hTrackCanPack(db, trackName) ? "pack" : "full";
 }
 
-char *hGetParent(char *db, char *subtrackName)
-/* Given a subtrack table, find its parent */
-{
-// TODO--- hub tracks can have parents.  This is only called from
-// item search, but it should work there too.   We should be able
-// to grab these out of the settings hash, but then why doesn't
-// this happen for mySQL tracks?  It would be a lot faster methinks
-if (isHubTrack(subtrackName))
-    return NULL;
-
-struct sqlConnection *conn = hAllocConn(db);
-struct trackDb *tdb = hMaybeTrackInfo(conn, subtrackName);
-char *ret = NULL;
-if (tdb != NULL)
-    {
-    ret = firstWordInLine( trackDbLocalSetting(tdb, "parent"));
-    trackDbFree(&tdb);
-    }
-hFreeConn(&conn);
-return ret;
-}
-
 static struct hash *makeTrackSettingsHash(char *db)
 /* Create  a hash of hashes with all track settings for database.
  * The returned hash is keyed by track.   The contained hashes

commit 5f27d8692b5b17919e6849f223e31da7bb10d915

Ugh, c73b60407 introduced a bug: I assumed that if tdb->parent is non-NULL, tdb->parentName would be too -- nope, tdb->parentName is apparently only for children of superTracks, not children of views/composites. refs #13477 (cherry picked from commit a540e869caa6e2ca8e7f4f6c460b9e62bdef1d5d)

src/hg/lib/hgFind.c
@@ -2482,15 +2482,15 @@ for (table = hgp->tableList; table != NULL; table = table->next)
 		fprintf(f, "%s=%s&", trackName, vis);
 		// this is magic to tell the browser to make the 
 		// composite and this subTrack visible
-		if (tdb->parentName)
+		if (tdb->parent)
 		    {
 		    if (tdbIsSuperTrackChild(tdb))
-			fprintf(f, "%s=show&", tdb->parentName);
+			fprintf(f, "%s=show&", tdb->parent->track);
 		    else
 			{
 			// tdb is a subtrack of a composite or a view
 			fprintf(f, "%s_sel=1&", trackName);
-			fprintf(f, "%s_sel=1&", tdb->parentName);
+			fprintf(f, "%s_sel=1&", tdb->parent->track);
 			}
 		    }
 		fprintf(f, "hgFind.matches=%s,\">", encMatches);