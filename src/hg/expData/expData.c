2103511ad2e8f27a63eed4de12544b5ec8e8f132
Chris Eisenhart <ceisenhart@soe.ucsc.edu>
2014-07-06 12:34:51 -0700

    The program now prints out files in Json format

--- src/hg/expData/expData.c
+++ src/hg/expData/expData.c
@@ -7,6 +7,7 @@
 #include "expData.h"
 #include "sqlList.h"
 #include "hacTree.h"
+#include "jsonWrite.h"
 
 void usage()
 /* Explain usage and exit. */
@@ -24,8 +25,15 @@ errAbort(
 static struct optionSpec options[] = {
    {NULL, 0},
 };
-
-
+int gc = 0;
+int gc2 = 0;
+struct link 
+/* Contains the info for a Json link */
+    {
+    struct link *next;
+    int source;		// the source node
+    int target;		// the target node
+    };
 struct bioExpVector
 /* Contains expression information for a biosample on many genes */
     {
@@ -85,8 +93,81 @@ while (lineFileNextReal(lf, &line))
 lineFileClose(&lf);
 }
 
+void rPrintNodes(FILE *f, struct hacTree *tree)
+{
+// Recursively prints out the nodes in a depth first order starting on the left
+char *tissue = ((struct bioExpVector *)(tree->itemOrCluster))->name;
+if (tree->childDistance != 0) 
+    {
+    fprintf(f,"    %s\"%s\"%s\"%s\"%s", "{","name", ":", " ", ",");
+    fprintf(f,"\"%s\"%s%0.31f%s\n", "y", ":" , tree->childDistance, "},");
+    }
+else {
+    fprintf(f,"    %s\"%s\"%s\"%s\"%s", "{","name", ":", tissue, ",");
+    fprintf(f,"\"%s\"%s%0.31f%s\n", "y", ":" , tree->childDistance, "},");
+    }
+if (tree->left == NULL && tree->right == NULL)
+    { 
+    return;
+    }
+else if (tree->left == NULL || tree->right == NULL)
+    errAbort("\nHow did we get a node with one NULL kid??");
+rPrintNodes(f, tree->left);
+rPrintNodes(f, tree->right);
+}
 
-static void rPrintSlBioExpVectorTree(FILE *f, struct hacTree *tree, int level)
+
+int testSource = 0, testTarget = 0;
+void rPrintLinks(FILE *f, struct hacTree *tree, int source)
+{
+// recursively prints the links
+
+if (gc == gc2 - 1)
+    {
+    return;
+    }
+/* if the current location is a leaf */
+if (tree->left == NULL && tree->right == NULL)
+    { 
+    return;
+    }
+else if (tree->left == NULL || tree->right == NULL)
+    errAbort("\nHow did we get a node with one NULL kid??");
+/* check for the end of the tree */
+
+/* left recursgion, the source and target are always ofset by 1 */
+++testTarget;
+fprintf(f,"    %s\"%s\"%s%d%s", "{","source", ":", testTarget - 1, ",");
+fprintf(f,"\"%s\"%s%d%s\n", "target", ":" , testTarget, "},");  
+/* preps the source for the right links */
+source = testTarget;
+rPrintLinks(f, tree->left, source);
+/* print the right link */
+++testTarget;
+fprintf(f,"    %s\"%s\"%s%d%s", "{","source", ":", source - 1 , ",");
+fprintf(f,"\"%s\"%s%d%s\n", "target", ":" , testTarget, "},");  
+rPrintLinks(f, tree->right, ++source);
+}
+
+void printJson(FILE *f, struct hacTree *tree)
+/* Prints the hacTree into a Json file format */
+{
+int source = 0;
+// Basic json template for d3 visualizations
+fprintf(f,"%s\n", "{");
+fprintf(f,"  \"%s\"%s\n", "nodes", ":[" );
+rPrintNodes(f, tree);
+fprintf(f,  "%s\n", "],");
+// Basic json template for d3 visualizations
+fprintf(f,  "\"%s\"%s\n", "links", ":[" );
+rPrintLinks(f,tree, source);
+fprintf(f,"  %s\n", "]");
+
+fprintf(f,"%s\n", "}");
+}
+
+
+static void rPrintSlBioExpVectorTree(FILE *f, struct hacTree *tree, int level,double distance)
 /* Recursively print out cluster as nested-parens with {}'s around leaf nodes. */
 {
 char *tissue = ((struct bioExpVector *)(tree->itemOrCluster))->name;
@@ -95,15 +176,16 @@ for (i = 0;  i < level;  i++)
     fputc(' ', f);
 if (tree->left == NULL && tree->right == NULL)
     {
-    fprintf(f, "{%s}", tissue);
+    fprintf(f, "{%s}%0.31f", tissue, distance);
     return;
     }
 else if (tree->left == NULL || tree->right == NULL)
     errAbort("\nHow did we get a node with one NULL kid??");
-fprintf(f, "(%s\n", tissue);
-rPrintSlBioExpVectorTree(f, tree->left, level+1);
+fprintf(f, "(%s%f\n", "node", tree->childDistance);
+distance += tree->childDistance;
+rPrintSlBioExpVectorTree(f, tree->left, level+1, distance);
 fputs(",\n", f);
-rPrintSlBioExpVectorTree(f, tree->right, level+1);
+rPrintSlBioExpVectorTree(f, tree->right, level+1, distance);
 fputc('\n', f);
 for (i=0;  i < level;  i++)
     fputc(' ', f);
@@ -118,7 +200,8 @@ if (tree == NULL)
     fputs("Empty tree.\n", f);
     return;
     }
-rPrintSlBioExpVectorTree(f, tree, 0);
+double distance = 0;
+rPrintSlBioExpVectorTree(f, tree, 0, distance);
 fputc('\n', f);
 }
 
@@ -177,7 +260,8 @@ struct lm *localMem = lmInit(0);
 fillInNames(list, nameFile);
 struct hacTree *clusters = hacTreeFromItems((struct slList *)list, localMem,
 					    slBioExpVectorDistance, slBioExpVectorMerge, NULL, NULL);
-printSlBioExpVectorTree(f,clusters);
+printJson(f,clusters);
+//printSlBioExpVectorTree(f,clusters);
 }
 
 int main(int argc, char *argv[])
