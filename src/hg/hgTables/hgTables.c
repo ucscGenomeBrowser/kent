0e6683b64347327fb1aad3263b24dfc55f4ed141
Brian Raney <braney@soe.ucsc.edu>
2014-08-06 15:23:19 -0700

    in hgTables, don't look for bigDataUrl in composite headers regardless of their type #13769

--- src/hg/hgTables/hgTables.c
+++ src/hg/hgTables/hgTables.c
@@ -646,13 +646,16 @@ struct hTableInfo *hubTrackTableInfo(struct trackDb *tdb)
 /* Given trackDb entry for a hub track, wrap table info around it. */
 {
 struct hTableInfo *hti;
-if (startsWithWord("bigBed", tdb->type))
-    hti = bigBedToHti(tdb->track, NULL);
-else if (startsWithWord("bam", tdb->type))
-    hti = bamToHti(tdb->table);
-else if (startsWithWord("vcfTabix", tdb->type))
-    hti = vcfToHti(tdb->table, TRUE);
-else
+if (tdb->subtracks == NULL)
+    {
+    if (startsWithWord("bigBed", tdb->type))
+	hti = bigBedToHti(tdb->track, NULL);
+    else if (startsWithWord("bam", tdb->type))
+	hti = bamToHti(tdb->table);
+    else if (startsWithWord("vcfTabix", tdb->type))
+	hti = vcfToHti(tdb->table, TRUE);
+    }
+if (hti == NULL)
     {
     AllocVar(hti);
     hti->rootName = cloneString(tdb->track);
