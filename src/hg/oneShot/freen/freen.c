e17f555375caa88ec04c1bd824cb776fddd1ea54
Jim Kent <kent@soe.ucsc.edu>
2014-08-24 20:40:53 -0700

    Moving an alternative hacTree implementation that minimizes the merge steps to the library.

--- src/hg/oneShot/freen/freen.c
+++ src/hg/oneShot/freen/freen.c
@@ -44,21 +44,6 @@ uglyf("dblDistance %g %g = %g\n", i1->val, i2->val, d);
 return d;
 }
 
-int dblCmp(const struct slList *item1, const struct slList *item2, void *extraData)
-{
-struct slDouble *i1 = (struct slDouble *)item1;
-struct slDouble *i2 = (struct slDouble *)item2;
-uglyf("dblDistance %g %g\n", i1->val, i2->val);
-double v1 = i1->val, v2 = i2->val;
-double diff = v1 - v2;
-if (diff < 0)
-    return -1;
-else if (diff > 0)
-    return 1;
-else 
-    return 0;
-}
-
 struct slList *dblMerge(const struct slList *item1, const struct slList *item2, 
     void *extraData)
 {
@@ -67,99 +52,6 @@ struct slDouble *i2 = (struct slDouble *)item2;
 double d = 0.5 * (i1->val + i2->val);
 uglyf("dblMerge %g %g = %g\n", i1->val, i2->val, d);
 return (struct slList *)slDoubleNew(d);
-}
-
-
-void findClosestPair(struct dlList *list, struct hash *distanceHash, 
-    hacDistanceFunction *distF, void *extraData, struct dlNode **retNodeA, struct dlNode **retNodeB)
-/* Loop through list returning closest two nodes */
-{
-struct dlNode *aNode;
-double closestDistance = BIGDOUBLE;
-struct dlNode *closestA = NULL, *closestB = NULL;
-for (aNode = list->head; !dlEnd(aNode); aNode = aNode->next)
-    {
-    struct hacTree *aHt = aNode->val;
-    struct slList *a = aHt->itemOrCluster;
-    struct dlNode *bNode;
-    for (bNode = aNode->next; !dlEnd(bNode); bNode = bNode->next)
-        {
-	struct hacTree *bHt = bNode->val;
-	char key[64];
-	safef(key, sizeof(key), "%p %p", aHt, bHt);
-	double *pd = hashFindVal(distanceHash, key);
-	if (pd == NULL)
-	     {
-	     lmAllocVar(distanceHash->lm, pd);
-	     *pd = distF(a, bHt->itemOrCluster, extraData);
-	     hashAdd(distanceHash, key, pd);
-	     }
-	double d = *pd;
-	if (d < closestDistance)
-	    {
-	    closestDistance = d;
-	    closestA = aNode;
-	    closestB = bNode;
-	    }
-	}
-    }
-*retNodeA = closestA;
-*retNodeB = closestB;
-}
-
-struct hacTree *hacTreeForCostlyMerges(struct slList *itemList, struct lm *localMem,
-				 hacDistanceFunction *distF, hacMergeFunction *mergeF,
-				 void *extraData)
-/* Construct hacTree using a method that will minimize the number of calls to
- * the distance and merge functions, assuming they are expensive */
-{
-/* Make up a doubly-linked list in 'remaining' with all items in it */
-struct dlList remaining;
-dlListInit(&remaining);
-struct slList *item;
-int count = 0;
-struct hash *distanceHash = hashNew(0);
-for (item = itemList; item != NULL; item = item->next)
-    {
-    struct hacTree *ht;
-    lmAllocVar(localMem, ht);
-    ht->itemOrCluster = item;
-    struct dlNode *node;
-    lmAllocVar(localMem, node);
-    node->val = ht;
-    dlAddTail(&remaining, node);
-    count += 1;
-    }
-
-/* Loop through finding closest and merging until only one node left on remaining. */
-int i;
-for (i=1; i<count; ++i)
-    {
-    /* Find closest pair and take them off of remaining list */
-    struct dlNode *aNode, *bNode;
-    findClosestPair(&remaining, distanceHash, distF, extraData, &aNode, &bNode);
-    dlRemove(aNode);
-    dlRemove(bNode);
-
-    /* Allocated new hacTree item for them and fill it in with a merged value. */
-    struct hacTree *ht;
-    lmAllocVar(localMem, ht);
-    struct hacTree *left = ht->left = aNode->val;
-    struct hacTree *right = ht->right = bNode->val;
-    left->parent = right->parent = ht;
-    ht->itemOrCluster = mergeF(left->itemOrCluster, right->itemOrCluster, extraData);
-
-    /* Put merged item onto remaining list. */
-    struct dlNode *node;
-    lmAllocVar(localMem, node);
-    node->val = ht;
-    dlAddTail(&remaining, node);
-    }
-
-/* Clean up and go home. */
-hashFree(&distanceHash);
-struct dlNode *lastNode = dlPopHead(&remaining);
-return lastNode->val;
 }
 
 void freen(char *output)
@@ -178,7 +70,9 @@ for (i=0; i<10; ++i)
 struct lm *lm = lmInit(0);
 struct hacTree *ht = hacTreeForCostlyMerges((struct slList *)list, lm, dblDistance, dblMerge, 
     NULL);
-    // dblCmp, NULL);
+#ifdef OLD
+struct hacTree *ht = hacTreeFromItems((struct slList *)list, lm, dblDistance, dblMerge, NULL, NULL);
+#endif /* OLD */
 rDump(ht, 0, f);
 carefulClose(&f);
 }
