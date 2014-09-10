f0539c79bbdb58f4e42937c3e76cfcc77cc12189
Brian Raney <braney@soe.ucsc.edu>
2014-09-10 12:40:57 -0700

    CCDS changed their table schemas again

--- src/hg/makeDb/genbank/src/ccds/ncbiSql/createTables.sql
+++ src/hg/makeDb/genbank/src/ccds/ncbiSql/createTables.sql
@@ -3,7 +3,7 @@ CREATE TABLE [dbo].[AccessionRejectionCriteria] (
 	[name] [varchar] (64) COLLATE Latin1_General_BIN NOT NULL ,
 	[description] [varchar] (1024) COLLATE Latin1_General_BIN NULL 
 ) ON [PRIMARY]
-GO
+go
 
 CREATE TABLE [dbo].[Accessions] (
 	[accession_uid] [int] IDENTITY (1, 1) NOT FOR REPLICATION  NOT NULL ,
@@ -16,7 +16,7 @@ CREATE TABLE [dbo].[Accessions] (
 	[organization_uid] [int] NOT NULL ,
 	[alive] [bit] NOT NULL 
 ) ON [PRIMARY]
-GO
+go
 
 CREATE TABLE [dbo].[Accessions_GroupVersions] (
 	[accession_uid] [int] NOT NULL ,
@@ -25,20 +25,55 @@ CREATE TABLE [dbo].[Accessions_GroupVersions] (
 	[original_member] [bit] NOT NULL ,
 	[was_public] [bit] NOT NULL 
 ) ON [PRIMARY]
-GO
+go
+
+CREATE TABLE [dbo].[BuildQualityTests] (
+        [build_uid] [int] NOT NULL ,
+        [qa_analysis_id] [int] NOT NULL ,
+        [is_required] [bit] NOT NULL ,
+        [acc_rejection_uid] [int] NOT NULL ,
+        [val_description] [varchar] (64) COLLATE Latin1_General_BIN NULL
+) ON [PRIMARY]
+go
+
+CREATE TABLE [dbo].[Builds] (
+        [build_uid] [int] IDENTITY (1, 1) NOT FOR REPLICATION NOT NULL ,
+        [tax_id] [int] NOT NULL ,
+        [ncbi_build_number] [int] NOT NULL ,
+        [ncbi_build_version] [int] NOT NULL,
+        [ensembl_build_number] [int] NOT NULL ,
+        [assembly_acc] [varchar] (16) COLLATE Latin1_General_BIN NOT NULL ,
+        [assembly_version] [int] NOT NULL ,
+        [assembly_name] [varchar] (16) COLLATE Latin1_General_BIN NOT NULL ,
+        [prev_build_uid] [int] NULL ,
+        [date_created] [datetime] NOT NULL ,
+        [date_made_public] [datetime] NULL ,
+	[ncbi_nadb_accession_uid] [int] NULL ,
+	[ensembl_nadb_accession_uid] [int] NULL 
+
+) ON [PRIMARY]
+go
+
+CREATE TABLE [dbo].[CcdsStatistics] (
+        [statistics_uid] [int] IDENTITY (1, 1) NOT FOR REPLICATION NOT NULL ,
+        [statistics_type_uid] [int] NOT NULL ,
+        [statistics_html] [text] COLLATE Latin1_General_BIN NULL ,
+        [build_uid] [int] NOT NULL 
+) ON [PRIMARY]
+go
 
 CREATE TABLE [dbo].[CcdsStatusVals] (
 	[ccds_status_val_uid] [int] IDENTITY (1, 1) NOT FOR REPLICATION  NOT NULL ,
 	[ccds_status] [varchar] (50) COLLATE Latin1_General_BIN NOT NULL 
 ) ON [PRIMARY]
-GO
+go
 
 CREATE TABLE [dbo].[CcdsUids] (
 	[ccds_uid] [int] NOT NULL ,
 	[group_uid] [int] NOT NULL ,
 	[latest_version] [int] NOT NULL 
 ) ON [PRIMARY]
-GO
+go
 
 CREATE TABLE [dbo].[ChromosomeAccessions] (
 	[chromosome_accession_uid] [int] IDENTITY (1, 1) NOT FOR REPLICATION  NOT NULL ,
@@ -47,7 +82,7 @@ CREATE TABLE [dbo].[ChromosomeAccessions] (
 	[version] [int] NULL ,
 	[chromosome] [varchar] (8) COLLATE Latin1_General_BIN NOT NULL 
 ) ON [PRIMARY]
-GO
+go
 
 CREATE TABLE [dbo].[GroupVersions] (
 	[group_version_uid] [int] IDENTITY (1, 1) NOT FOR REPLICATION  NOT NULL ,
@@ -63,13 +98,13 @@ CREATE TABLE [dbo].[GroupVersions] (
 	[was_public] [bit] NOT NULL ,
         [build_uid] [int] NOT NULL 
 ) ON [PRIMARY]
-GO
+go
 
 CREATE TABLE [dbo].[GroupVersions_ChromosomeAccessions] (
 	[group_version_uid] [int] NOT NULL ,
 	[chromosome_accession_uid] [int] NOT NULL 
 ) ON [PRIMARY]
-GO
+go
 
 CREATE TABLE [dbo].[Groups] (
 	[group_uid] [int] NOT NULL ,
@@ -78,7 +113,7 @@ CREATE TABLE [dbo].[Groups] (
 	[chromosome] [varchar] (8) COLLATE Latin1_General_BIN NOT NULL ,
 	[orientation] [char] (1) COLLATE Latin1_General_BIN NOT NULL 
 ) ON [PRIMARY]
-GO
+go
 
 CREATE TABLE [dbo].[InterpretationSubtypes] (
 	[interpretation_subtype_uid] [int] IDENTITY (1, 1) NOT FOR REPLICATION  NOT NULL ,
@@ -87,14 +122,14 @@ CREATE TABLE [dbo].[InterpretationSubtypes] (
         [for_public] [bit] NOT NULL ,
         [can_edit_comment] [bit] NOT NULL 
 ) ON [PRIMARY]
-GO
+go
 
 CREATE TABLE [dbo].[InterpretationTypes] (
 	[interpretation_type_uid] [int] IDENTITY (1, 1) NOT FOR REPLICATION  NOT NULL ,
 	[interpretation_type] [varchar] (128) COLLATE Latin1_General_BIN NOT NULL ,
         [for_public] [bit] NOT NULL
 ) ON [PRIMARY]
-GO
+go
 
 CREATE TABLE [dbo].[Interpretations] (
 	[interpretation_uid] [int] IDENTITY (1, 1) NOT FOR REPLICATION  NOT NULL ,
@@ -116,7 +151,11 @@ CREATE TABLE [dbo].[Interpretations] (
 	[program_uid] [int] NULL ,
 	[reftrack_uid] [int] NULL 
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
-GO
+go
+
+ALTER TABLE [dbo].[Interpretations] ADD 
+	CONSTRAINT [DF_Interpretations_reftrack_uid] DEFAULT (0) FOR [reftrack_uid]
+go
 
 CREATE TABLE [dbo].[Interpreters] (
 	[interpreter_uid] [int] IDENTITY (1, 1) NOT FOR REPLICATION  NOT NULL ,
@@ -124,35 +163,43 @@ CREATE TABLE [dbo].[Interpreters] (
 	[name] [varchar] (128) COLLATE Latin1_General_BIN NULL ,
 	[email] [varchar] (500) COLLATE Latin1_General_BIN NULL 
 ) ON [PRIMARY]
-GO
+go
 
 CREATE TABLE [dbo].[Locations] (
 	[location_uid] [int] IDENTITY (1, 1) NOT FOR REPLICATION  NOT NULL ,
 	[chr_start] [int] NOT NULL ,
 	[chr_stop] [int] NOT NULL 
 ) ON [PRIMARY]
-GO
+go
 
 CREATE TABLE [dbo].[Locations_GroupVersions] (
 	[location_uid] [int] NOT NULL ,
 	[group_version_uid] [int] NOT NULL ,
 	[chromosome] [varchar] (8) COLLATE Latin1_General_BIN NOT NULL 
 ) ON [PRIMARY]
-GO
+go
+
+CREATE TABLE [dbo].[NADB_Accessions] (
+	[nadb_accession_uid] [int] IDENTITY (1, 1) NOT FOR REPLICATION  NOT NULL ,
+	[organization_uid] [int] NOT NULL ,
+	[acc] [varchar] (64) COLLATE Latin1_General_BIN NOT NULL ,
+	[version] [int] NOT NULL
+) ON [PRIMARY]
+go
 
 CREATE TABLE [dbo].[NextIds] (
 	[table_name] [varchar] (128) COLLATE Latin1_General_BIN NOT NULL ,
 	[next_number] [int] NOT NULL 
 ) ON [PRIMARY]
-GO
+go
 
 CREATE TABLE [dbo].[Organizations] (
 	[organization_uid] [int] IDENTITY (1, 1) NOT FOR REPLICATION  NOT NULL ,
 	[name] [varchar] (128) COLLATE Latin1_General_BIN NOT NULL ,
 	[approval_authority] [bit] NOT NULL ,
-	[tax_id] [int] NOT NULL 
+	[tax_id] [int] NULL 
 ) ON [PRIMARY]
-GO
+go
 
 CREATE TABLE [dbo].[Programs] (
 	[program_uid] [int] IDENTITY (1, 1) NOT FOR REPLICATION  NOT NULL ,
@@ -160,48 +207,17 @@ CREATE TABLE [dbo].[Programs] (
 	[name] [varchar] (500) COLLATE Latin1_General_BIN NOT NULL ,
 	[version] [varchar] (500) COLLATE Latin1_General_BIN NULL 
 ) ON [PRIMARY]
-GO
-
-ALTER TABLE [dbo].[Interpretations] ADD 
-	CONSTRAINT [DF_Interpretations_reftrack_uid] DEFAULT (0) FOR [reftrack_uid]
-GO
-
-CREATE TABLE [dbo].[StatisticsTypes] (
-        [statistics_type_uid] [int] IDENTITY (1, 1) NOT FOR REPLICATION  NOT NULL ,
-        [statistics_type] [varchar] (20) COLLATE Latin1_General_BIN NOT NULL 
-) ON [PRIMARY]
 go
 
-CREATE TABLE [dbo].[CcdsStatistics] (
-        [statistics_uid] [int] IDENTITY (1, 1) NOT FOR REPLICATION NOT NULL ,
-        [statistics_type_uid] [int] NOT NULL ,
-        [statistics_html] [text] COLLATE Latin1_General_BIN NULL ,
-        [build_uid] [int] NOT NULL 
-) ON [PRIMARY]
-go
-
-CREATE TABLE [dbo].[Builds] (
-        [build_uid] [int] IDENTITY (1, 1) NOT FOR REPLICATION NOT NULL ,
-        [tax_id] [int] NOT NULL ,
-        [ncbi_build_number] [int] NOT NULL ,
-        [ncbi_build_version] [int] NOT NULL,
-        [ensembl_build_number] [int] NOT NULL ,
-        [assembly_acc] [varchar] (16) COLLATE Latin1_General_BIN NOT NULL ,
-        [assembly_version] [int] NOT NULL ,
-        [assembly_name] [varchar] (16) COLLATE Latin1_General_BIN NOT NULL ,
-        [prev_build_uid] [int] NULL ,
-        [date_created] [datetime] NOT NULL ,
-        [date_made_public] [datetime] NULL
-
-) ON [PRIMARY]
-go
-
-CREATE TABLE [dbo].[BuildQualityTests] (
-        [build_uid] [int] NOT NULL ,
-        [qa_analysis_id] [int] NOT NULL ,
-        [is_required] [bit] NOT NULL ,
-        [acc_rejection_uid] [int] NOT NULL ,
-        [val_description] [varchar] (64) COLLATE Latin1_General_BIN NULL
+CREATE TABLE [dbo].[ProspectiveAnnotCompare] (
+        [group_version_uid] [int] NOT NULL ,
+        [prot_length_diff] [int] NOT NULL ,
+        [coverage_pct] [float] NOT NULL ,
+        [matched_splice_count] [int] NOT NULL ,
+        [unmatched_splice_count] [int] NOT NULL ,
+        [total_splice_count] [int] NOT NULL ,
+        [matched_splice_pct] [float] NOT NULL ,
+        [identity_pct] [float] NULL
 ) ON [PRIMARY]
 go
 
@@ -226,21 +242,9 @@ CREATE TABLE [dbo].[ProspectiveGroups] (
 ) ON [PRIMARY]
 go
 
-CREATE TABLE [dbo].[ProspectiveAnnotCompare] (
-        [group_version_uid] [int] NOT NULL ,
-        [prot_length_diff] [int] NOT NULL ,
-        [coverage_pct] [float] NOT NULL ,
-        [matched_splice_count] [int] NOT NULL ,
-        [unmatched_splice_count] [int] NOT NULL ,
-        [total_splice_count] [int] NOT NULL ,
-        [matched_splice_pct] [float] NOT NULL ,
-        [identity_pct] [float] NULL
-) ON [PRIMARY]
-go
-
-CREATE TABLE [dbo].[ReportTypes] (
-        [report_type_uid] [int] IDENTITY (1, 1) NOT FOR REPLICATION  NOT NULL ,
-        [report_type] [varchar] (128) COLLATE Latin1_General_BIN NOT NULL
+CREATE TABLE [dbo].[ProspectiveStatusVals] (
+	[prospective_status_val_uid] [int] IDENTITY (1, 1) NOT FOR REPLICATION  NOT NULL ,
+	[prospective_status] [varchar] (50) COLLATE Latin1_General_BIN NOT NULL
 ) ON [PRIMARY]
 go
 
@@ -255,9 +259,15 @@ CREATE TABLE [dbo].[ReportQueries]
 ) ON [PRIMARY]
 go
 
-CREATE TABLE [dbo].[ProspectiveStatusVals] (
-	[prospective_status_val_uid] [int] IDENTITY (1, 1) NOT FOR REPLICATION  NOT NULL ,
-	[prospective_status] [varchar] (50) COLLATE Latin1_General_BIN NOT NULL
+CREATE TABLE [dbo].[ReportTypes] (
+        [report_type_uid] [int] IDENTITY (1, 1) NOT FOR REPLICATION  NOT NULL ,
+        [report_type] [varchar] (128) COLLATE Latin1_General_BIN NOT NULL
+) ON [PRIMARY]
+go
+
+CREATE TABLE [dbo].[StatisticsTypes] (
+        [statistics_type_uid] [int] IDENTITY (1, 1) NOT FOR REPLICATION  NOT NULL ,
+        [statistics_type] [varchar] (20) COLLATE Latin1_General_BIN NOT NULL 
 ) ON [PRIMARY]
 go
 
