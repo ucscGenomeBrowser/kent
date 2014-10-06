8bd463dba83ae0ca0dabab935a210b25fa8bcfd0
Max <max@soe.ucsc.edu>
2014-10-06 03:53:54 -0700

    this commit should not show up on hgwdev or in code review

--- src/hg/inc/versionInfo.h
+++ src/hg/inc/versionInfo.h
@@ -1,3 +1,6 @@
 /* Copyright (C) 2014 The Regents of the University of California 
  * See README in this or parent directory for licensing information. */
-#define CGI_VERSION "305"
+// XX !!
+// this change should not be visible in into code review, as it's in my separate branch in my homedir
+// XX !!
+#define CGI_VERSION "306"
