81e8ea3771a035161ebb08b19c35521d7dcb677d
Max <max@soe.ucsc.edu>
2014-11-03 08:52:46 -0800

    making clinvar ottomatic, refs #14045

---
+++ src/hg/utils/otto/clinvar/doUpdate.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -o errexit -o pipefail
+cd /hive/data/outside/otto/clinvar/
+clinVarToBed --auto
+cp clinvarMain.hg19.bb /hive/data/genomes/hg19/bed/clinvar/clinvarMain.bb
+cp clinvarCnv.hg19.bb /hive/data/genomes/hg19/bed/clinvar/clinvarCnv.bb
+cp clinvarMain.hg38.bb /hive/data/genomes/hg38/bed/clinvar/clinvarMain.bb
+cp clinvarCnv.hg38.bb /hive/data/genomes/hg38/bed/clinvar/clinvarCnv.bb
