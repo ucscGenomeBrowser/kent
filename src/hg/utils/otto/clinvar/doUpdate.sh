125ee66764355902e3c616cdced05eddedfaf960
Max <max@soe.ucsc.edu>
2014-11-03 19:41:48 -0800

    changes after code review, refs #14298

--- src/hg/utils/otto/clinvar/doUpdate.sh
+++ src/hg/utils/otto/clinvar/doUpdate.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -o errexit -o pipefail
 cd /hive/data/outside/otto/clinvar/
-clinVarToBed --auto
+clinVarToBed --auto --maxDiff 0.1
 cp clinvarMain.hg19.bb /hive/data/genomes/hg19/bed/clinvar/clinvarMain.bb
 cp clinvarCnv.hg19.bb /hive/data/genomes/hg19/bed/clinvar/clinvarCnv.bb
 cp clinvarMain.hg38.bb /hive/data/genomes/hg38/bed/clinvar/clinvarMain.bb
