10894e15fba98c6ff17842aaaaa85fe38321d2f7
Max <max@soe.ucsc.edu>
2014-11-05 09:13:33 -0800

    bugfix for clinvar otto

--- src/hg/utils/otto/clinvar/doUpdate.sh
+++ src/hg/utils/otto/clinvar/doUpdate.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -o errexit -o pipefail
+export PATH=$PATH:/cluster/bin/x86_64/ # for bedSort and bedToBigBed
 cd /hive/data/outside/otto/clinvar/
 ./clinVarToBed --auto --maxDiff 0.1
 cp clinvarMain.hg19.bb /hive/data/genomes/hg19/bed/clinvar/clinvarMain.bb
