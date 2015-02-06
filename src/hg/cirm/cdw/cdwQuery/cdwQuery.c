345ec160f71a74bc23b622cf06d5a88b2ac22301
Jim Kent <kent@soe.ucsc.edu>
2015-02-05 22:09:41 -0800

    Expanding wildcards in field list, so allowing select *.  Also implementing count(*) function.

--- src/hg/cirm/cdw/cdwQuery/cdwQuery.c
+++ src/hg/cirm/cdw/cdwQuery/cdwQuery.c
@@ -48,6 +48,9 @@ else
     }
 }
 
+int matchCount = 0;
+boolean doSelect = FALSE;
+
 void traverse(struct tagStorm *tags, struct tagStanza *list, 
     struct rqlStatement *rql, struct lm *lm)
 /* Recursively traverse stanzas on list. */
@@ -61,90 +64,109 @@ for (stanza = list; stanza != NULL; stanza = stanza->next)
 	{
 	if (statementMatch(rql, stanza, lm))
 	    {
-	    struct slName *field;
-	    for (field = rql->fieldList; field != NULL; field = field->next)
+	    ++matchCount;
+	    if (doSelect)
 		{
-		char *val = tagFindVal(stanza, field->name);
-		if (val != NULL)
-		    printf("%s\t%s\n", field->name, val);
+		struct slName *field;
+		for (field = rql->fieldList; field != NULL; field = field->next)
+		    {
+		    char *val = tagFindVal(stanza, field->name);
+		    if (val != NULL)
+			printf("%s\t%s\n", field->name, val);
+		    }
+		printf("\n");
 		}
-	    printf("\n");
 	    }
 	}
     }
 }
 
-void cdwQuery(char *query)
+void cdwQuery(char *rqlQuery)
 /* cdwQuery - Get list of tagged files.. */
 {
-struct lineFile *lf = lineFileOnString("query", TRUE, cloneString(query));
-struct rqlStatement *rql = rqlStatementParse(lf);
+struct lineFile *lf = lineFileOnString("query", TRUE, cloneString(rqlQuery));
 struct sqlConnection *conn = cdwConnect();
 struct lm *lm = lmInit(0);
-struct slName *file;
-for (file = rql->tableList; file != NULL; file = file->next)
+struct rqlStatement *rql = rqlStatementParse(lf);
+int stormCount = slCount(rql->tableList);
+if (stormCount != 1)
+    errAbort("Can only handle one tag storm file in query, got %d", stormCount);
+char *tagsFileName = rql->tableList->name;
+
+struct tagStorm *tags = tagStormFromFile(tagsFileName);
+struct hash *hash = tagStormUniqueIndex(tags, "meta");
+
+/* Get list of all files including a bunch of fields we'll add to tagStorm.
+ * The files will end up as new stanzas underneath the stanza that matches the
+ * meta column. */
+char query[2048];
+sqlSafef(query, sizeof(query), 
+	"select %s from cdwFile,cdwValidFile where cdwFile.id=cdwValidFile.fileId "
+	"order by cdwFile.id ",
+	    "experiment meta,tags,cdwFileName,licensePlate accession,size,md5,format,"
+	    "submitFileName submit_file_name,"
+	    "outputType output,itemCount item_count,"
+	    "mapRatio map_ratio,depth,part file_part,pairedEnd paired_end ");
+int metaIx = 0, tagsIx = 1, cdwFileNameIx = 2;
+struct sqlResult *sr = sqlGetResult(conn, query);
+char *fieldNames[200];
+int fieldIx;
+for (fieldIx=0; fieldIx < ArraySize(fieldNames); ++fieldIx)
     {
-    struct tagStorm *tags = tagStormFromFile(file->name);
-    struct hash *hash = tagStormUniqueIndex(tags, "meta");
-    char query[2048];
-    sqlSafef(query, sizeof(query), 
-	    "select %s from cdwFile,cdwValidFile where cdwFile.id=cdwValidFile.fileId "
-	    "order by cdwFile.id ",
-		"experiment meta,tags,cdwFileName,licensePlate accession,size,md5,format,"
-		"submitFileName submit_file_name,"
-		"outputType output,itemCount item_count,"
-		"mapRatio map_ratio,depth,part file_part,pairedEnd paired_end ");
-    int metaIx = 0, tagsIx = 1, cdwFileNameIx = 2;
-    struct sqlResult *sr = sqlGetResult(conn, query);
-    char *fieldNames[200];
-    int fieldIx;
-    for (fieldIx=0; fieldIx < ArraySize(fieldNames); ++fieldIx)
-        {
-	char *field = sqlFieldName(sr);
-	if (field == NULL)
-	    break;
-	fieldNames[fieldIx] = field;
-	}
-    int fieldCount = fieldIx;
-    char **row;
-    while ((row = sqlNextRow(sr)) != NULL)
-        {
-	char *joiner = row[metaIx];
-	char *cgiTags = row[tagsIx];
-	char *cdwFileName = row[cdwFileNameIx];
-	struct tagStanza *parent = hashFindVal(hash, joiner);
-	if (parent != NULL)
-	    {
-	    struct tagStanza *stanza = tagStanzaNewAtEnd(tags, parent);
-	    char fileName[PATH_LEN];
-	    safef(fileName, sizeof(fileName), "%s%s", cdwRootDir, cdwFileName);
-	    tagStanzaAdd(tags, stanza, "file", fileName);
-	    int rowIx=0;
-	    for (rowIx = 0; rowIx < fieldCount; ++rowIx)
-	        {
-		char *name = fieldNames[rowIx];
-		if (rowIx != 1)  // avoid tags column
-		    {
-		    char *val = row[rowIx];
-		    if (!isEmpty(val))
-			tagStanzaAdd(tags, stanza, name, val);
-		    }
-		}
-	    struct cgiParsedVars *cgis = cgiParsedVarsNew(cgiTags);
-	    struct cgiVar *var;
-	    for (var = cgis->list; var != NULL; var = var->next)
-		{
-		if (!isEmpty(var->val))
-		    tagStanzaAdd(tags, stanza, var->name, var->val);
-		}
-	    cgiParsedVarsFree(&cgis);
-	    slReverse(&stanza->tagList);
-	    }
-	}
-    sqlFreeResult(&sr);
-    traverse(tags, tags->forest, rql, lm);
-    tagStormFree(&tags);
+    char *field = sqlFieldName(sr);
+    if (field == NULL)
+	break;
+    fieldNames[fieldIx] = field;
     }
+int fieldCount = fieldIx;
+char **row;
+while ((row = sqlNextRow(sr)) != NULL)
+    {
+    char *joiner = row[metaIx];
+    char *cgiTags = row[tagsIx];
+    char *cdwFileName = row[cdwFileNameIx];
+    struct tagStanza *parent = hashFindVal(hash, joiner);
+    if (parent != NULL)
+	{
+	struct tagStanza *stanza = tagStanzaNewAtEnd(tags, parent);
+	char fileName[PATH_LEN];
+	safef(fileName, sizeof(fileName), "%s%s", cdwRootDir, cdwFileName);
+	tagStanzaAdd(tags, stanza, "file", fileName);
+	int rowIx=0;
+	for (rowIx = 0; rowIx < fieldCount; ++rowIx)
+	    {
+	    char *name = fieldNames[rowIx];
+	    if (rowIx != 1)  // avoid tags column
+		{
+		char *val = row[rowIx];
+		if (!isEmpty(val))
+		    tagStanzaAdd(tags, stanza, name, val);
+		}
+	    }
+	struct cgiParsedVars *cgis = cgiParsedVarsNew(cgiTags);
+	struct cgiVar *var;
+	for (var = cgis->list; var != NULL; var = var->next)
+	    {
+	    if (!isEmpty(var->val))
+		tagStanzaAdd(tags, stanza, var->name, var->val);
+	    }
+	cgiParsedVarsFree(&cgis);
+	slReverse(&stanza->tagList);
+	}
+    }
+sqlFreeResult(&sr);
+
+/* Get list of all tag types in tree and use it to expand wildcards in the query
+ * field list. */
+struct slName *allFieldList = tagTreeFieldList(tags);
+rql->fieldList = wildExpandList(allFieldList, rql->fieldList, TRUE);
+
+/* Traverse tag tree outputting when rql statement matches */
+doSelect = sameWord(rql->command, "select");
+traverse(tags, tags->forest, rql, lm);
+if (sameWord(rql->command, "count"))
+    printf("%d\n", matchCount);
+tagStormFree(&tags);
 }
 
 int main(int argc, char *argv[])
