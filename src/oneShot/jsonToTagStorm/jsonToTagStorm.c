c51fb191e12be5fdeb18dcc2de3593c6c3bb3f86
Chris Eisenhart <ceisenhart@soe.ucsc.edu>
2015-04-23 11:26:39 -0700

    Completely refactored yet again, this time the code uses a tokenizer. Seems to be working and quite robust

--- src/oneShot/jsonToTagStorm/jsonToTagStorm.c
+++ src/oneShot/jsonToTagStorm/jsonToTagStorm.c
@@ -6,6 +6,7 @@
 #include "options.h"
 #include "jsonWrite.h"
 #include "tagStorm.h"
+#include "tokenizer.h"
 
 void usage()
 /* Explain usage and exit. */
@@ -24,83 +25,88 @@ static struct optionSpec options[] = {
 };
 
 
-void removePunctuation(char *s)
-/* Remove the remaining unwanted punctuation in a string */
+void parseJson(struct lineFile *lf, FILE *f)
+/* This function uses a tokenizer to tokenize a .json file, 
+ * the tokens are iterated over once. Depth is updated as the depth character
+ * '[' is encountered.  Name value pairs are deciphered and printed to 
+ * the correct depth. */ 
 {
-stripChar(s,'\\');
-stripChar(s,']');
-stripChar(s,'}');
-}
-
-
-void prettyPrint(char *name, char *value, int depth, FILE *f)
-/* Print the name value tag to the corresponding depth */
-{
-int i;
-for (i = 0; i < depth; ++i)
-    {
-    fprintf(f,"    "); 
-    }
-fprintf(f,"%s %s\n", name, value); 
-}
-
-
-void parseJson(char *line, FILE *f)
- /* This function uses three embedded 'for' loops to split apart a
- * .json single line file into name/value pairs for a tagstorm file. */
-{
-int arraySize = chopString(line, "[", NULL, 0);
-// First break the string apart by arrays
-char *chopByArray[arraySize]; 
-chopString(line, "[", chopByArray, arraySize);
 int i;
 int depth = 0;
-for (i = 1 ; i < arraySize; ++i)
+struct tokenizer *tkz = tokenizerOnLineFile(lf);
+char *temp = tokenizerNext(tkz); 
+char *name = NULL, *val = NULL; 
+bool beforeVal = TRUE, expVal = FALSE, firstLine = TRUE; 
+int count = 0, preventDoubleLineBreak = 0; 
+for (; temp != NULL; temp = tokenizerNext(tkz))
+    // Iterate over all the tokens in the input file 
     {
-    char *arrayLine = chopByArray[i];
-    int childrenSize = chopString(arrayLine, "{", NULL, 0);
-    // Break the arrays apart into objects
-    char *chopByChildren[childrenSize];
-    chopString (arrayLine, "{", chopByChildren, childrenSize);
-    int j;
-    for (j = 0 ; j < childrenSize; ++j)
-	// Iterate over the objects, at this point each object 
-	// contains the information required for a single tagStorm stanza. 
+    ++count; 
+    if (!strcmp(temp, "["))
+    	// Update depth and handle .json arrays 
         {
-	char *childrenLine = chopByChildren[j];
-	int commaSize = countSeparatedItems(childrenLine, ',');
-	// Break the list of name values apart 
-	char *chopByComma[commaSize];
-	int updateDepth = countSeparatedItems(childrenLine, ']') - 1;
-	chopString (childrenLine, ",", chopByComma, commaSize);
-	int k;
-	bool newArray = FALSE; 
-	for (k = 0 ; k < commaSize; ++k)
-	    //Iterate over all the name value pairs in a given stanza 
+	if (expVal)
 	    {
-	    char *commaLine = chopByComma[k];
-	    int colonSize = countSeparatedItems(commaLine, ':');
-	    char *chopByColon[colonSize];
-	    chopString (commaLine, ":", chopByColon, colonSize);
-	    assert(colonSize > 0);
-	    if (colonSize ==1) continue;
-	    removePunctuation(chopByColon[0]);
-	    removePunctuation(chopByColon[1]);
-	    if (lastNonwhitespaceChar(chopByColon[1])==NULL)
-	        //Ignore array names, they are not represented in tagStorm format. However 
-		//opening an array signifies an increase in depth. 
-	        {
-		++depth; 
-		newArray = TRUE; 
-		continue;
-		}
-	    prettyPrint(stripEnclosingChar(trimSpaces(chopByColon[0]), '"'), stripEnclosingChar(trimSpaces(chopByColon[1]), '"'), depth, f);
+	    expVal = FALSE;
+	    beforeVal = TRUE; 
 	    }
-	depth -= updateDepth;
-	// Update the depth after each stanza is printed
-	if (!newArray) fprintf(f,"\n");
-	// Print a newline after each stanza
+	if (firstLine)
+	    {
+	    firstLine = FALSE; 
+	    continue; 
+	    }
+	++depth; 
+	fprintf(f,"\n"); 
+	continue; 
 	}
+    if (!strcmp(temp, "]"))
+    	// Update depth
+        {
+	--depth; 
+	continue; 
+	}
+    if (!strcmp(temp, "}"))
+        {
+	if (preventDoubleLineBreak == count - 2)
+	    {
+	    continue; 
+	    }
+	preventDoubleLineBreak = count;
+	fprintf(f,"\n"); 
+	continue; 
+	}
+    if (!strcmp(temp, "{")) continue; 
+    if (!strcmp(temp, ":"))
+    	// Get ready for a value
+        {
+	expVal = TRUE;
+	continue;
+	} 
+    if (!strcmp(temp, ","))
+    	// Reset name and value
+        {
+	name=NULL; 
+	val=NULL;
+	beforeVal = TRUE; 
+	continue; 
+	}
+    if (beforeVal) 
+    	// Get the name
+        {
+	name = cloneString(temp);
+	beforeVal = FALSE; 
+	}
+    else
+        // Get the value, print name and value to propper depth
+        {
+	for (i = 0; i < depth; ++i)
+	    {
+	    fprintf(f,"    "); 
+	    }
+	fprintf(f,"%s ", name); 
+	val = temp; 
+	fprintf(f,"%s\n", val); 
+	} 
     }
 }
 
@@ -109,18 +115,16 @@ void jsonToTagStorm(char *input, char *output)
 /* jsonToTagStorm - Converts .json files into .tagStorm files */
 {
 char cmd[1024];
+// perl command to remove newlines and do a bit of formatting 
 safef(cmd, 1024, "perl -wpe 's/\\n//' %s > true%s", input, input);
 mustSystem(cmd);
 char cleanup[512], trueFile[512];
 safef(cleanup, 512, "rm true%s", input);
 safef(trueFile, 512, "true%s", input);
 struct lineFile *lf = lineFileOpen(trueFile, TRUE);
-char *line;
-if (!lineFileNext(lf, &line, NULL))
-    errAbort("There doesn't seem to be any text in this .json file, %s", input);
 FILE *f = mustOpen(output, "w");
 mustSystem(cleanup);
-parseJson(line, f);
+parseJson(lf, f);
 }
 
 int main(int argc, char *argv[])
