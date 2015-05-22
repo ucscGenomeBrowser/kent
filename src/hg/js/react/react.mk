ebd69ed96b9efc5289fe2a2490ddc06f818a393b
Angie Hinrichs <angie@soe.ucsc.edu>
2015-05-22 12:45:28 -0700

    common.mk: auto-detect hgwdev's installation of node.js tools. If jshint, jsxhint, jsx and/or uglifyjs are installed, set the corresponding JSHINT etc. variables; if not found, set variable to 'true' so makefile can invoke those steps without any effect (in which case the precompiled files in hg/js/react/bundle/ will not reflect any changes made to *.jsx).

--- src/hg/js/react/react.mk
+++ src/hg/js/react/react.mk
@@ -7,21 +7,6 @@ COMPILED_JS_DESTDIR=compiled
 # This directory holds bundled files that are in git so that mirrors don't need to install node:
 BUNDLE_DESTDIR=../bundle
 
-# jsxhint: off unless JSXHINT is already in environment
-ifeq (${JSXHINT},)
-    JSXHINT=true
-endif
-
-# jsx: off unless JSX is already in environment (that means edits to .jsx files don't affect .js!)
-ifeq (${JSX},)
-    JSX=true
-endif
-
-# uglifyjs: off unless UGLIFYJS is already in env (that means that rebuilt .js doesn't get bundled!)
-ifeq (${UGLIFYJS},)
-    UGLIFYJS=true
-endif
-
 # Don't remove the bundle files when cleaning -- they're under git control!
 clean:
 	rm -f ${COMPILED_JS_DESTDIR}/*
