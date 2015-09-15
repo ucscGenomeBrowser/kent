2b17d5b37e7336d22c0bba8b20df3bf95afea1e4
Max <max@soe.ucsc.edu>
2015-09-15 08:28:36 -0700

    fixing one letter typo

--- src/browserbox/root/updateBrowser.sh
+++ src/browserbox/root/updateBrowser.sh
@@ -197,7 +197,7 @@ cp /usr/local/apache/htdocs/inc/globalNavBar.inc /tmp/navbar.inc
 sed -i '/<li class="menuparent" id="mirrors">/,/^<\/li>$/d' /tmp/navbar.inc 
 sed -i '/<li class="menuparent" id="downloads">/,/^<\/li>$/d' /tmp/navbar.inc 
 # add a link to the gbib shared data folder
-sed -i '/Track Hubs/a          <li><a href="http:\/\/127.0.0.1:1234\/folders\/">GBIB Shared Data Folder<\/a><\/li>' /tmp/navbar.inc
+sed -i '/Track Hubs/a          <li><a href="http:\/\/127.0.0.1:1234\/folders\/">GBiB Shared Data Folder<\/a><\/li>' /tmp/navbar.inc
 cat /tmp/navbar.inc | grep -v hgNear | grep -v hgVisiGene | sed -e '/hgLiftOver/a <li><a href="../cgi-bin/hgMirror">Mirror tracks</a></li>' | uniq > /usr/local/apache/htdocs/inc/globalNavBar.inc 
 rm /tmp/navbar.inc
 
