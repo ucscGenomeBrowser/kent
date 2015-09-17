96046cd01c7c3b82272f18eee810b428243d6593
Brian Raney <braney@soe.ucsc.edu>
2015-09-17 13:27:26 -0700

    updates to the GWAS otto download files # no redmine

--- src/hg/utils/otto/gwas/checkGwas.sh
+++ src/hg/utils/otto/gwas/checkGwas.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -ex
+#!/bin/sh -e
 
 #	Do not modify this script, modify the source tree copy:
 #	src/hg/utils/omim/checkGwas.sh
@@ -6,6 +6,8 @@
 # this script assumes that the snp*Coords files have already been built
 # cut -f 1-4 /cluster/data/hg18/bed/snp130/snp130.bed | sort -k4,4 > snp130Coords.bed
 # zcat /cluster/data/hg19/bed/snp138/snp138.bed.gz | cut -f 1-4,6,8,18,21-24   | sort -k4,4  > snp138Coords.bed
+# zcat /cluster/data/hg19/bed/snp141/snp141.bed.gz | cut -f 1-4,6,8,18,21-24   | sort -k4,4  > snp141Coords.bed
+# zcat /cluster/data/hg38/bed/snp141/snp141.bed.gz | cut -f 1-4,6,8,18,21-24   | sort -k4,4  > hg38.snp141Coords.bed
 
 #	cron jobs need to ensure this is true
 umask 002
@@ -22,7 +24,8 @@ fi
 
 cd "${WORKDIR}"
 
-wget --timestamping http://www.genome.gov/admin/gwascatalog.txt
+#wget -q --timestamping http://www.genome.gov/admin/gwascatalog.txt
+wget -q --timestamping  -O gwascatalog.txt http://www.ebi.ac.uk/gwas/api/search/downloads/full
 
 if [  ! gwascatalog.txt  -nt old.gwascatalog.txt ]; then
     echo "Not newer"
@@ -56,21 +59,26 @@ hgLoadBed hg18 gwasCatalogNew gwasCatalog.bed -tab -sqlTable=$HOME/kent/src/hg/l
 
 
 # Mapping to hg19 by joining hg19 SNP coords with catalog flatfile (see hg18.txt)
-join -t "	" -1 4 ../snp138Coords.bed noCoords.tab -o 1.1,1.2,1.3,1.4,2.2,2.3,2.4,2.5,2.6,2.7,2.8,2.9,2.10,2.11,2.12,2.13,2.14,2.15,2.16,2.17,2.18,2.19,1.5,1.6,1.7,1.8,1.9,1.10,1.11 | sort -k1,1 -k2n,2n > gwasCatalogPlus.bed
+join -t "	" -1 4 ../snp141Coords.bed noCoords.tab -o 1.1,1.2,1.3,1.4,2.2,2.3,2.4,2.5,2.6,2.7,2.8,2.9,2.10,2.11,2.12,2.13,2.14,2.15,2.16,2.17,2.18,2.19,1.5,1.6,1.7,1.8,1.9,1.10,1.11 | sort -k1,1 -k2n,2n > gwasCatalogPlus.bed
 
 
 cut -f 1-22 gwasCatalogPlus.bed | hgLoadBed hg19 gwasCatalogNew stdin -tab -sqlTable=$HOME/kent/src/hg/lib/gwasCatalog.sql -notItemRgb -allowStartEqualEnd -renameSqlTable
 
+join -t "	" -1 4 ../hg38.snp141Coords.bed noCoords.tab -o 1.1,1.2,1.3,1.4,2.2,2.3,2.4,2.5,2.6,2.7,2.8,2.9,2.10,2.11,2.12,2.13,2.14,2.15,2.16,2.17,2.18,2.19,1.5,1.6,1.7,1.8,1.9,1.10,1.11 | sort -k1,1 -k2n,2n > hg38.gwasCatalog.bed
+cut -f 1-22 hg38.gwasCatalog.bed | hgLoadBed hg38 gwasCatalogNew stdin -tab -sqlTable=$HOME/kent/src/hg/lib/gwasCatalog.sql -notItemRgb -allowStartEqualEnd -renameSqlTable
+
 ../validateGwas.sh hg18
 ../validateGwas.sh hg19
+../validateGwas.sh hg38
 
-# now install for both hg18 and hg19
+# now install for hg18 and hg19 and hg38
 for i in gwasCatalog
 do 
     n=$i"New"
     o=$i"Old"
     hgsqlSwapTables hg18 $n $i $o -dropTable3
     hgsqlSwapTables hg19 $n $i $o -dropTable3
+    hgsqlSwapTables hg38 $n $i $o -dropTable3
 done
 
 echo "Gwas Installed `date`" 
