deafbe03444c22b94478efdba966f57d36d34ceb
Hiram Clawson <hiram@soe.ucsc.edu>
2016-01-21 13:58:47 -0800

    same special handling for ce11 as was for ce6 refs #15209

--- src/hg/hgc/hgc.c
+++ src/hg/hgc/hgc.c
@@ -8601,7 +8601,7 @@ char *ensemblIdUrl = trackDbSettingOrDefault(tdb, "ensemblIdUrl", "http://www.en
 
 /* shortItemName is the name without the "." + version */
 shortItemName = cloneString(itemName);
-/* ensembl gene names are different from their usual naming scheme on ce6 */
+/* ensembl gene names are different from their usual naming scheme on ce6/ce11*/
 if (! (startsWith("ce6", database) || startsWith("ce11", database)))
     {
     chp = strstr(shortItemName, ".");
@@ -8817,8 +8817,8 @@ else if (startsWith("vega", tdb->table))
    }
 /* shortItemName is the name without the "." + version */
 shortItemName = cloneString(itemName);
-/* ensembl gene names are different from their usual naming scheme on ce6 */
-if (!startsWith("ce6", database))
+/* ensembl gene names are different from their usual naming scheme on ce6/ce11*/
+if (! (startsWith("ce6", database) || startsWith("ce11", database)) )
     {
     chp = strstr(shortItemName, ".");
     if (chp != NULL)
