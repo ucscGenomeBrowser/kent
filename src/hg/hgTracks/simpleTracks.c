243331abd5c962fcd4a938888e29bee67743f3a3
Kate Rosenbloom <kate@soe.ucsc.edu>
2016-03-31 22:08:08 -0700

    Finally found where labeling happens, updating comment

--- src/hg/hgTracks/simpleTracks.c
+++ src/hg/hgTracks/simpleTracks.c
@@ -4251,7 +4251,6 @@ if (withLabels)
         }
     else
         {
-
 	// shift clipping to allow drawing label to left of currentWindow
 	int pdfSlop=nameWidth/5;
         hvGfxUnclip(hvg);
@@ -4262,7 +4261,7 @@ if (withLabels)
             hvGfxTextRight(hvg, textX, y, nameWidth, tg->heightPer, MG_WHITE, font, name);
             }
         else
-            // usual case
+            // usual labeling
             hvGfxTextRight(hvg, textX, y, nameWidth, tg->heightPer, labelColor, font, name);
         hvGfxUnclip(hvg);
         hvGfxSetClip(hvg, insideX, yOff, insideWidth, tg->height);
