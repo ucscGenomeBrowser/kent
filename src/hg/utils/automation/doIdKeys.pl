5b5f04b6df27df6b5a12bf3dbd9d87b70f1c8d29
Hiram Clawson <hiram@soe.ucsc.edu>
2016-04-28 15:05:42 -0700

    completed through cleanup step refs #17168

--- src/hg/utils/automation/doIdKeys.pl
+++ src/hg/utils/automation/doIdKeys.pl
@@ -4,6 +4,8 @@
 # edit ~/kent/src/hg/utils/automation/doIdKeys.pl instead.
 
 # calculates md5sum strings for each sequence in a 2bit file
+# and constructs a single md5sum from all those individual md5sums for a
+# single 'keySignature' identifier for the entire sequence.
 
 use Getopt::Long;
 use warnings;
@@ -26,7 +28,7 @@ use vars qw/
 my $stepper = new HgStepManager(
     [ { name => 'setup',   func => \&doSetup },
       { name => 'clusterRun',   func => \&doClusterRun },
-      { name => 'finalResult',   func => \&dofinalResult },
+      { name => 'finalResult',   func => \&doFinalResult },
       { name => 'cleanup', func => \&doCleanup },
     ]
 				);
@@ -34,7 +36,6 @@ my $stepper = new HgStepManager(
 # Option defaults:
 my $dbHost = 'hgwdev';
 my $bigClusterHub = 'ku';
-my $smallClusterHub = 'ku';
 my $workhorse = 'hgwdev';
 my $defaultWorkhorse = 'hgwdev';
 my $twoBit = "$HgAutomate::clusterData/\$db/\$db.2bit";
@@ -60,8 +61,7 @@ _EOF_
   ;
   print STDERR &HgAutomate::getCommonOptionHelp('dbHost' => $dbHost,
 						'workhorse' => $defaultWorkhorse,
-						'bigClusterHub' => $bigClusterHub,
-						'smallClusterHub' => $smallClusterHub);
+						'bigClusterHub' => $bigClusterHub);
   print STDERR "
 Automates the construction of an 'idKeys' file for a 2bit sequence
     The 'idKeys' are the md5sum results of each sequence in the 2bit file.
@@ -69,15 +69,14 @@ Automates the construction of an 'idKeys' file for a 2bit sequence
     setup: establish work directores and scripts for processing
     clusterRun: perform the cluster run
                 cluster run is performed only when number of
-                sequences is less than 5,000, else twoBitDup is used once
+                sequences is >= 5,000, else twoBitDup is used once
     finalResult: gather the results of the clusterRun (or twoBitDup) into
-                a single results file: two colums:
+                a single results file '<db>.idKeys.txt': two colums:
                 1. md5sum string (sorted on this column)
                 2. sequence name (chromosome name, scaffold name, contig...)
     cleanup: Removes or compresses intermediate files.
 All operations are performed in the build directory which is
-$HgAutomate::clusterData/\$db/$HgAutomate::trackBuild/idKeys unless -buildDir is given.
-";
+$HgAutomate::clusterData/\$db/$HgAutomate::trackBuild/idKeys unless -buildDir is given.";
   # Detailed help (-help):
   print STDERR "
 Assumptions:
@@ -107,10 +106,8 @@ sub checkOptions {
   usage(1) if ($err);
   $workhorse = $opt_workhorse if ($opt_workhorse);
   $bigClusterHub = $opt_bigClusterHub if ($opt_bigClusterHub);
-  $smallClusterHub = $opt_smallClusterHub if ($opt_smallClusterHub);
   $dbHost = $opt_dbHost if ($opt_dbHost);
 }
-
 
 #########################################################################
 # * step: setup [workhorse]
@@ -134,7 +131,7 @@ sub doSetup {
 twoBitInfo $twoBit stdout | sort -k2nr | cut -f1 > part.list
 export partCount=`cat part.list | wc -l`
 if [ "\${partCount}" -lt 5000 ]; then
-  twoBitDup -keyList=stdout $twoBit | grep -v "are duplicates" | sort > $db.idKeys.txt
+  time ( /cluster/home/hiram/kent/src/utils/twoBitDup/twoBitDup -keyList=stdout $twoBit | grep -v "are duplicates" | sort > $db.idKeys.txt) > twoBitDup.log 2>&1
 else
   mkdir -p splitList
   split -a 3 -d -l 5000 part.list splitList/part
@@ -167,20 +164,107 @@ fi
 _EOF_
   );
   $bossScript->execute();
-} # doIdKeys
+} # doSetup
 
 #########################################################################
-# * step: cleanup [fileServer]
+# * step: clusterRun [bigClusterHub]
+sub doClusterRun {
+  my $runDir = "$buildDir";
+  my $paraHub = $bigClusterHub;
+
+  # First, make sure previous step has completed:
+  if ( ! $opt_debug && ( ! -s "$runDir/parts.list" ) ) {
+    die "doClusterRun: previous 'setup' step has not completed, no parts.list file present.\n";
+  }
+  # Then, make sure we're starting clean.
+  if ( ! $opt_debug && ( -s "$runDir/run.time" ) ) {
+    die "doClusterRun: looks like this was run successfully already " .
+      "(file <db>/bed/idKeys/run.time exists).  Either run with -continue finalResult or some later " .
+        "stage, or move aside/remove $runDir and run again.\n";
+  }
+
+  my $whatItDoes = "Perform cluster run if necessary.";
+  my $bossScript = newBash HgRemoteScript("$runDir/clusterRun.bash", $paraHub,
+				      $runDir, $whatItDoes);
+
+  $bossScript->add(<<_EOF_
+if [ -s runOne ]; then
+  chmod +x runOne
+  $HgAutomate::paraRun
+else
+  if [ ! -s $db.idKeys.txt ]; then
+     printf "ERROR: previous step doIdKeys failed twoBitDup procedure" 1>&2
+     exit 255
+  else
+     cp -p twoBitDup.log run.time
+  fi
+fi
+_EOF_
+  );
+  $bossScript->execute();
+} # doClusterRun
+
+#########################################################################
+# * step: finalResult [workhorse]
+sub doFinalResult {
+  my $runDir = "$buildDir";
+
+  # First, make sure previous step has completed:
+  if ( ! $opt_debug && ( ! -s "$runDir/run.time" ) ) {
+    die "doFinalResult: previous 'clusterRun' step has not completed, no run.time file present.\n";
+  }
+  # Then, make sure we're starting clean.
+  if ( ! $opt_debug && ( -s "$runDir/doFinalResult.bash" ) ) {
+    die "doFinalResult: looks like this was run successfully already " .
+      "(file db/bed/doFinalResult.bash exists).  Either run with -continue cleanup " .
+        ", or move aside/remove $runDir and run again.\n";
+  }
+
+  my $whatItDoes = "Collect cluster run results into one single result file, and construct the 'keySignature' for the entire sequence.";
+  my $bossScript = newBash HgRemoteScript("$runDir/doFinalResult.bash",
+                   $workhorse, $runDir, $whatItDoes);
+
+  $bossScript->add(<<_EOF_
+if [ -d result ]; then
+  catDir result/part* | sort -k1,1 > $db.idKeys.txt
+fi
+if [ -s $db.idKeys.txt ]; then
+  printf "# finalStep: $db.idKeys.txt file is present and done\\n" 1>&2
+  cut -f 1 $db.idKeys.txt | md5sum | awk '{print \$1}' > $db.keySignature.txt
+else
+  printf "ERROR: finalstep: $db.idKeys.txt file is missing\\n" 1>&2
+  exit 255
+fi
+_EOF_
+  );
+  $bossScript->execute();
+} # doFinalResult
+
+#########################################################################
+# * step: cleanup [workhorse]
 sub doCleanup {
   my $runDir = "$buildDir";
+
+  # Make sure we're starting clean.
+  if ( ! $opt_debug && ( -s "$runDir/doCleanup.bash" ) ) {
+    die "doCleanup: looks like this was run successfully already " .
+      "(file db/bed/doCleanup.bash exists).\n";
+  }
+  # Verify previous step has completed
+  if ( ! $opt_debug && ( ! -s "$runDir/$db.idKeys.txt" ) ) {
+    die "doCleanup: ERROR: previous steps have not completed, there is no " .
+      "file $runDir/$db.idKeys.txt present.\n";
+  }
+
   my $whatItDoes = "It cleans up or compresses intermediate files.";
-  my $fileServer = &HgAutomate::chooseFileServer($runDir);
-  my $bossScript = new HgRemoteScript("$runDir/doCleanup.csh", $fileServer,
+  my $bossScript = newBash HgRemoteScript("$runDir/doCleanup.bash", $workhorse,
 				      $runDir, $whatItDoes);
   $bossScript->add(<<_EOF_
-rm -rf run.template/raw/
-rm -rf templateOtherBigTempFilesOrDirectories
-gzip template
+rm -fr err splitList result para.results batch para.bookmark batch.bak
+gzip part.list
+if [ -s jobList ]; then
+  gzip jobList
+fi
 _EOF_
   );
   $bossScript->execute();
