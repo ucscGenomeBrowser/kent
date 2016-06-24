13ae98eb7b6d5f3f37288d877d53507ce8acf7fa
Jonathan Casper <jcasper@soe.ucsc.edu>
2016-06-24 10:31:05 -0700

    Fixes to GWAS Catalog otto pipeline, refs #14465

--- src/hg/utils/otto/gwas/perlParser.pl
+++ src/hg/utils/otto/gwas/perlParser.pl
@@ -1,43 +1,64 @@
-    my $snpIdx = 27;
+my @saveIdx = (
+    1, # PUBMEDID
+    2, # FIRST AUTHOR
+    3, # DATE
+    4, # JOURNAL
+    6, # STUDY
+    7, # DISEASE/TRAIT
+    8, # INITIAL SAMPLE SIZE
+    9, # REPLICATION SAMPLE SIZE
+    10, # REGION
+    13, # REPORTED GENE(S)
+    20, # STRONGEST SNP-RISK ALLELE
+    26, # RISK ALLELE FREQUENCY
+    27, # P-VALUE
+    29, # P-VALUE (TEXT)
+    30, # OR or BETA
+    31, # 95% CI (TEXT)
+    32, # PLATFORM [SNPS PASSING QC]
+    33, # CNV
+);
+    my $snpIdx = 21;
+    my $riskIdx = 20;
 
-    use Time::Piece;
+# Restore this if they change time formats again
+#    use Time::Piece;
     while (<>) { 
                 next if (/^\s*$/); 
                 s/\r$//; 
                 @w = split("\t"); 
                 # Skip if SNPs column is empty
                 next if ($w[$snpIdx] !~ /^rs\d+/); 
-		$w[3] =  Time::Piece->strptime($w[3], '%d-%b-%Y')->strftime('%Y-%m-%d');
+# Restore this if they change time formats again
+#		$w[3] =  Time::Piece->strptime($w[3], '%d-%b-%Y')->strftime('%Y-%m-%d');
 
-                #if ($w[3] =~ /^(\d+)\/(\d+)\/(\d+)$/) { # transform to mysql DATE 
-             #     ($month, $day, $year) = ($1, $2, $3); 
-             #     $w[3] = "$year-$month-$day"; 
-             #   } else { die "Cant parse date ($w[3])\t" } 
                 $w[$snpIdx] =~ s/ //g; 
                 my @snps = split(",", $w[$snpIdx]); 
-                # discard columns (use descending order): 
-                # goal is to keep the following:
-#                foreach $i (28, 25, 24, 23, 22, 21, 19, 18, 17, 16, 15, 14, 12, 11, 5, 0) { 
-                foreach $i (35, 32, 31, 30, 29, 28, 27, 25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 12, 11, 5, 0) { 
-                  splice(@w, $i, 1); 
+
+                $w[$riskIdx] =~ s/\s+//g;
+
+                # Keep only the columns we care about
+                my @savedCols = ();
+                foreach $i (@saveIdx) { 
+                  $savedCols[@savedCols] = $w[$i];
                 } 
                 # trim leading/trailing spaces if any; 
                 # convert the Unicode in titles to HTML because non-ASCII gives Galaxy trouble. 
                 # NB: this conversion now handled externally by iconv.  If Galaxy can handle
                 # HTML entities, though, that might be even better - we could replace use of
                 # iconv with calls to HTML::Entities
-                foreach $i (0 .. $#w) { 
-                  $w[$i] =~ s/^\s*//;  $w[$i] =~ s/\s*$//; 
-                  @chars = split(//, $w[$i]); 
-                  $w[$i] = ""; 
+                foreach $i (0 .. $#savedCols) { 
+                  $savedCols[$i] =~ s/^\s*//;  $savedCols[$i] =~ s/\s*$//; 
+                  @chars = split(//, $savedCols[$i]); 
+                  $savedCols[$i] = ""; 
                   foreach $c (@chars) { 
                     if (ord($c) > 127) { 
                       $c = sprintf "&#%d;", ord($c); 
                     } 
-                  $w[$i] .= $c; 
+                  $savedCols[$i] .= $c; 
                   } 
                 } 
                 foreach $s (@snps) { 
-                  print join("\t", $s, @w) . "\n"; 
+                  print join("\t", $s, @savedCols) . "\n"; 
                 } 
               }
