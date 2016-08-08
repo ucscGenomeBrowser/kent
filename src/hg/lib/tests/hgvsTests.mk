aaf72102b545c05c42f66b7a3fc22d65b1ecf4fe
Angie Hinrichs <angie@soe.ucsc.edu>
2016-08-08 14:12:39 -0700

    Added recognition of a small subset of HGVS terms: coding (c.) SNVs relative to RefSeq NM_ or LRG transcript IDs, and protein (p.) simple substitutions relative to NP_. Also accepted (not HGVS but similar and popular): geneSymbol and abbreviated protein subst like "ALK G1494E". hgFind will map terms to the current genome if possible, and will display warnings about unrecognized accessions, out-of-bounds coordinates and mismatching reference alleles. refs #15071, #15554

---
+++ src/hg/lib/tests/hgvsTests.mk
@@ -0,0 +1,18 @@
+# makefile with hgHgvs tests
+include ../../../inc/common.mk
+
+BIN_DIR = bin/$(MACHTYPE)
+TESTER = ${BIN_DIR}/hgvsTester
+IN_DIR = input/hgvs
+EXP_DIR = expected/hgvs
+OUT_DIR = output/hgvs
+DB=hg38
+
+test: validTerms
+
+validTerms: mkout
+	${TESTER} ${DB} ${IN_DIR}/$@.txt > ${OUT_DIR}/$@.txt
+	diff -u ${EXP_DIR}/$@.txt ${OUT_DIR}/$@.txt
+
+mkout:
+	@${MKDIR} ${OUT_DIR}
