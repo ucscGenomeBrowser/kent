b534e5e167df93880881df0478ecec0225fdf136
Angie Hinrichs <angie@soe.ucsc.edu>
2016-08-31 17:01:24 -0700

    This commit adds the capability to pick apart complex HGVS sequence change descriptions, and apply those changes to reference sequence, in order to translate HGVS nucleotide terms into a variant representation suitable for functional prediction in hgVai.  VCF was chosen since it is easy to integrate into hgVai.  refs #11460
    Changes to existing code:
    * hgvsMapToGenome maps to BED6 instead of BED3 because we need to know strand in order to convert transcript changes into VCF forward-strand genomic changes.
    * hgvsMapToGenome maps insertions to zero-length points instead of 2-base ranges as in HGVS.
    
    New file hgHgvsParse.c contains a tokenizer and parser for HGVS sequence change descriptions; top-level interface is hgvsParseNucleotideChange.
    
    hgHgvs.c has new code to translate parsed HGVS nucleotide change(s) into VCF, optionally left-shifting ambiguous alignments (VCF convention, at odds with HGVS right-shifting convention); top-level interface is hgvsToVcfRow.
    
    New hgvsToVcf utility enables testing of corner cases and may come in handy as a command-line util.
    
    HGVS terms for testing have been taken from ClinVar and do not reflect the diversity of terms in the wild, nor do they cover the full HGVS spec.
    
    For example, the HGVS repeat notation can be parsed but not mapped to the genome because all of the ClinVar repeat terms that I looked at looked wonky to me and I believe the HGVS repeat notation is inherently error-prone.  The repeat notation is supposed to use the position of the first repeat unit and to specify the number of repeated copies starting at that point (right-shifted if ambiguous).  However, in ClinVar, sometimes the given repeat unit sequence did not match the reference sequence at the given position; sometimes the number of sepeats made sense only if they were not perfect repeats (some differing bases); sometimes ranges of repeat numbers were given.  Also, the reference assembly's number of repeats can change from one assembly to the next.  So it is hard given an HGVS repeat term to determine 1) whether it makes sense in relation to the reference assembly with/without fuzzy matching and 2) what the exact change is relative to the reference assembly.
    
    Insertions of inverted sequence from elsewhere in the same reference have not yet been tested.  http://varnomen.hgvs.org/recommendations/DNA/variant/inversion/ gives some complicated examples like "g.122_123ins213_234invinsAins123_211inv" but I have not yet seen terms like that in the wild.

---
+++ src/hg/utils/hgvsToVcf/hgvsToVcf.c
@@ -0,0 +1,84 @@
+/* hgvsToVcf - Convert HGVS terms to VCF tab-separated output. */
+
+/* Copyright (C) 2017 The Regents of the University of California
+ * See README in this or parent directory for licensing information. */
+
+#include "common.h"
+#include "options.h"
+#include "hdb.h"
+#include "hgHgvs.h"
+#include "vcf.h"
+#include "linefile.h"
+
+void usage()
+/* Explain usage and exit. */
+{
+errAbort(
+  "hgvsToVcf - Convert HGVS terms to VCF tab-separated output\n"
+  "usage:\n"
+  "   hgvsToVcf db input.hgvs output.vcf\n"
+  "options:\n"
+  "   -noLeftShift        Don't do the VCF-conventional left shifting of ambiguous placements\n"
+  "db is a UCSC database such as hg19, hg38 etc.\n"
+  "Only nucleotide HGVS terms (g., c., n., m.) are supported, not protein (p.).\n"
+  );
+}
+
+/* Command line option global vars */
+static boolean noLeftShift = TRUE;
+
+/* Command line validation table. */
+static struct optionSpec options[] = {
+    {"noLeftShift", OPTION_BOOLEAN},
+    {NULL, 0},
+};
+
+static void writeVcfHeader(FILE *f, char *db, char *inFile)
+/* Write a simple VCF header to f. */
+{
+fprintf(f,
+        "##fileformat=VCFv4.2\n"
+        "##reference=%s\n"
+        "##source=hgvsToVcf %s %s\n"
+        HGVS_VCF_HEADER_DEFS
+        "#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\n", db, db, inFile);
+}
+
+void hgvsToVcf(char *db, char *inFile, char *outFile, boolean doLeftShift)
+/* hgvsToVcf - Convert HGVS terms to VCF tab-separated output. */
+{
+struct lineFile *lf = lineFileOpen(inFile, TRUE);
+FILE *f = mustOpen(outFile, "w");
+writeVcfHeader(f, db, inFile);
+struct vcfRow *rowList = NULL;
+struct dyString *dyError = dyStringNew(0);
+char *term = NULL;
+while (lineFileNextReal(lf, &term))
+    {
+    // In order to output position-sorted VCF, we need to accumulate VCF rows and sort before
+    // printing.  In the meantime, print out a comment line for each HGVS term that we are
+    // unable to parse/map/etc.
+    dyStringClear(dyError);
+    struct vcfRow *row = hgvsToVcfRow(db, term, doLeftShift, dyError);
+    if (row)
+        slAddHead(&rowList, row);
+    else
+        fprintf(f, "# %s\n", dyStringContents(dyError));
+    }
+// Reverse so that when multiple terms map to a position, they are more or less in the
+// same order that we saw in the input.
+slReverse(&rowList);
+slSort(&rowList, vcfRowCmp);
+vcfRowTabOutList(f, rowList);
+}
+
+int main(int argc, char *argv[])
+/* Process command line. */
+{
+optionInit(&argc, argv, options);
+noLeftShift = optionExists("noLeftShift");
+if (argc != 4)
+    usage();
+hgvsToVcf(argv[1], argv[2], argv[3], !noLeftShift);
+return 0;
+}
