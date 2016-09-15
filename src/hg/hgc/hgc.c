3fbd5278d976e75ab044723b2356bea81ab4aa26
Mark Diekhans <markd@soe.ucsc.edu>
2016-09-15 13:04:15 -0700

    Always encode item names when generating URLs in hgcAnchorSomewhere.  This prevents item names with CGI-unsafe characters from being incorrectly encode.  This was happening with PSLs, but could happen in other structures

--- src/hg/hgc/hgc.c
+++ src/hg/hgc/hgc.c
@@ -525,9 +525,11 @@ void hgcAnchorSomewhere(char *group, char *item, char *other, char *chrom)
  * and other parameters. */
 {
 char *tbl = cgiUsualString("table", cgiString("g"));
+char *itemSafe = cgiEncode(item);
 printf("<A HREF=\"%s&g=%s&i=%s&c=%s&l=%d&r=%d&o=%s&table=%s\">",
-       hgcPathAndSettings(), group, item, chrom, winStart, winEnd, other,
+       hgcPathAndSettings(), group, itemSafe, chrom, winStart, winEnd, other,
        tbl);
+freeMem(itemSafe);
 }
 
 void hgcAnchorPosition(char *group, char *item)
@@ -5892,8 +5894,7 @@ for (psl = pslList; psl != NULL; psl = psl->next)
         char otherString[512];
 	safef(otherString, sizeof(otherString), "%d&aliTable=%s",
 	      psl->tStart, tableName);
-	hgcAnchorSomewhere(hgcCommandInWindow, cgiEncode(itemIn),
-			   otherString, psl->tName);
+	hgcAnchorSomewhere(hgcCommandInWindow, itemIn, otherString, psl->tName);
 	printf("<BR>View details of parts of alignment within browser window</A>.<BR>\n");
 	}
     }
@@ -20846,7 +20847,7 @@ while ((row = sqlNextRow(sr)) != NULL)
 	    longXenoPsl1Given(tdb, thisItem, otherOrg, "chromInfo",
 			      otherDb, thisPsl, pslTableName );
 	    safef(otherString, sizeof otherString, "%d&win=T", thisPsl->tStart );
-	    hgcAnchorSomewhere( tdb->track, cgiEncode(item), otherString, thisPsl->tName );
+	    hgcAnchorSomewhere( tdb->track, item, otherString, thisPsl->tName );
 	    printf("View individual alignment windows\n</a>");
 	    printf("<br><br>");
 	    }
@@ -23005,7 +23006,7 @@ if(info->stop >0)
 
 
 /* show genome sequence */
-hgcAnchorSomewhere("htcGeneInGenome", cgiEncode(info->name), tdb->track, seqName);
+hgcAnchorSomewhere("htcGeneInGenome", info->name, tdb->track, seqName);
 printf("View DNA for this putative fragment</A><BR>\n");
 
 /* show the detail alignment */
@@ -23019,7 +23020,7 @@ if(row != NULL)
     {
     safef(otherString, sizeof otherString, "&db=%s&pslTable=%s&chrom=%s&cStart=%d&cEnd=%d&strand=%s&qStrand=%s",
 	    database, pslTable, info->chrom,info->chromStart, info->chromEnd, info->strand, parts[2]);
-    hgcAnchorSomewhere("potentPsl", cgiEncode(parts[0]), otherString, info->chrom);
+    hgcAnchorSomewhere("potentPsl", parts[0], otherString, info->chrom);
     printf("<BR>View details of parts of alignment </A>.</BR>\n");
     }
 sqlFreeResult(&sr);
