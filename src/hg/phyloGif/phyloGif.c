d00bfa06dc2a084d1915779c2175a4d8bff0c879
Galt Barber <galt@soe.ucsc.edu>
2016-09-19 16:56:07 -0700

    fixing XSS problem in phyloGif

--- src/hg/phyloGif/phyloGif.c
+++ src/hg/phyloGif/phyloGif.c
@@ -642,8 +642,8 @@ if (errMsg)
 	puts("<html><head><title>PhyloTree parse error</title></head><body><pre>");
 	/* we dont think the specific error message coming back are correct or useful
 	 * so supply a generic err msg */
-    	printf("Original input tree:\n[%s]\n\n",cgiString("phyloGif_tree"));
-    	printf("Input tree as passed to parser:\n[%s]\n\n",phyloData);
+    	htmlPrintf("Original input tree:\n[%s]\n\n",cgiString("phyloGif_tree"));
+    	htmlPrintf("Input tree as passed to parser:\n[%s]\n\n",phyloData);
     	printf("Parser syntax error:\n%s",errMsg);
     	puts("</pre></body></html>");
 	}
