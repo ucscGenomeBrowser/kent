a46dc06c474d2bddcfe5743138e256ae35387459
Max <max@soe.ucsc.edu>
2016-11-01 13:28:00 -0700

    tiny change to crispr bigBed creator, refs #17235

--- src/hg/makeDb/crisprTrack/createBigBed.py
+++ src/hg/makeDb/crisprTrack/createBigBed.py
@@ -83,7 +83,7 @@ def rewriteBed(inFname, specScores, effScores, otOffsetPath, dataDir):
                 scoreDesc = "This guide sequence is not unique in the genome. The specificity scores were not determined."
             elif specScore==-2:
                 #scoreDesc = "This guide includes N character(s). Specificity scores could not be determined."
-                scoreDesc = "This guide has so many potential off-targets that the specificity score could not be calculated."
+                scoreDesc = "This guide has too many potential off-targets. The specificity score could not be calculated."
 
         # get eff scores
         guideId = "%s:%s-%s:%s" % (chrom, start, end, strand)
@@ -163,7 +163,7 @@ def rewriteBed(inFname, specScores, effScores, otOffsetPath, dataDir):
 
         row.extend([fusi, moreno, doench, oof])
 
-        if specScore!=0:
+        if specScore>=0:
             mouseOver = "MIT Spec. Score: %d, Doench 2016: %s, Moreno-Mateos: %s" % \
                 (specScore, fusiHover, morenoHover)
         else:
