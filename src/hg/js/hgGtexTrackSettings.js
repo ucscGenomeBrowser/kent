f3e281ea29cd25805890d53e0cdba63e4ab55f3c
Kate Rosenbloom <kate@soe.ucsc.edu>
2016-11-08 16:29:58 -0800

    First round code cleanup. refs #17369

--- src/hg/js/hgGtexTrackSettings.js
+++ src/hg/js/hgGtexTrackSettings.js
@@ -1,15 +1,19 @@
-// Module: gtexTrackSettings
+// gtexTrackSettings - Interactive features for GTEX Body Map version of GTEx Gene track UI page
+
+// Copyright (C) 2016 The Regents of the University of California
+
+// This file contains a single module that encapsulates all data and functions used to manage
+// the hgGtexTrackSettings page.
 
 
 var gtexTrackSettings = (function() {
 
-    // Data
+    // Globals
     
-    // SVG has its own DOM
-    var _svgDoc;
-    var _svgRoot;
     var _htmlDoc;
     var _htmlRoot;
+    var _svgDoc;        // SVG has its own DOM
+    var _svgRoot;
     var _topTissue;
     var _topAura;
 
@@ -28,37 +32,28 @@ var gtexTrackSettings = (function() {
     ];
 
     // Convenience functions
+
     function tissueFromSvgId(svgId) {
-    // Get tissue name from an SVG id. Convention here is <tis>_*
-            return svgId.split('_')[0];
+        // Get tissue name from an SVG id. Convention here is <tis>_*
+        return svgId.split('_')[0];
     }
 
     function initTissue(tis) {
         // Set tissue to unhighlighted state
         $("#" + tis + "_Pic_Hi", _svgRoot).hide();
 
-        // $("#" + tis + "_Lead_Hi", _svgRoot).hide();
-
-        // mark tissue labels in svg
+        // Mark tissue labels in svg
         var el = _svgDoc.getElementById(tis + "_Text_Hi");
         if (el !== null) {
             el.classList.add('tissueLabel');
         }
         $("#" + tis + "_Aura_Hi", _svgRoot).hide();
-
-        // force tissue to selected on init (till we have cart hooked up)
-        //onClickSetTissue(tis);
-    }
-
-    function highlightTissue(tis) {
-        // 
     }
 
     function initBodyMap(svg, doc) {
         // Set organs to unhighlighted state
         tissues.forEach(initTissue);
     }
-
 
     function onClickToggleTissue(tis) {
         // mark selected in tissue table
@@ -142,7 +137,6 @@ var gtexTrackSettings = (function() {
         }
     }
 
-
     function toggleHighlightTissue(tis) {
         var i;
         var isHovered = false;
@@ -175,14 +169,9 @@ var gtexTrackSettings = (function() {
             for (i = 0; i < textLineCount; i++) {
                 textEl.children[i].style.fill = "blue";
             }
-            //$(line).show();
-            //lineEl.style.stroke = '#EC1C24';
-            //lineEl.style.stroke = 'red';
             if (lineEl !== null) {     // cell types lack leader lines
             lineEl.style.stroke = 'blue';
             }
-            //lineEl.setAttribute("style", "stroke-width: 3; stroke: red;");
-            //
             $(pic).show();
             $(aura).show();
 
@@ -201,13 +190,9 @@ var gtexTrackSettings = (function() {
             }
             $(aura).hide();
             $(pic).hide();
-            //$(line).hide();
-            //lineEl.style.stroke = '#EC1C24';      // red
             if (lineEl !== null) {     // cell types lack leader lines
                 lineEl.style.stroke = '#F69296';      // pink
             }
-            //lineEl.setAttribute("style", "stroke-width: 1.7; stroke: #F69296;");
-            //
             _svgRoot.removeChild(_topTissue);
             _svgRoot.removeChild(_topAura);
         }
@@ -224,16 +209,14 @@ var gtexTrackSettings = (function() {
         toggleHighlightTissue(tis);
     }
 
-    //function animateTissue(tis, i, ignore) {
     function animateTissue(tis) {
-        //console.log(tis);
-        // add handlers to tissue table
+        // Add handlers to tissue table
         var textEl;
         var picEl;
         $('#' + tis).click(tis, onClickToggleTissue);
         $('#' + tis).hover(onHoverTissue, onHoverTissue);
 
-        // add mouseover and click handlers to tissue label
+        // Add mouseover and click handlers to tissue label
         textEl = _svgDoc.getElementById(tis + "_Text_Hi");
         if (textEl !== null) {
             if ($('#' + tis).hasClass('tissueSelected')) {
@@ -265,7 +248,7 @@ var gtexTrackSettings = (function() {
     // UI event handlers
 
     function onClickSetAll() {
-        // set all on body map
+        // Set all on body map
         tissues.forEach(onClickSetTissue);
         // set all on tissue table
         // NOTE: this shouldn't be needed (needs debugging in onClickSet)
@@ -277,15 +260,10 @@ var gtexTrackSettings = (function() {
         $('.tissueLabel').removeClass("tissueSelected");
     }
 
-    function onClickMapTissue() {
-        this.style.fill = "black";
-        //document.getElementById("bodyMapSvg").contentDocument.getElementById('endocervix').style.fill = "black";
-    }
-
     function submitForm() {
-    // Submit form from go button (see hgGateway.js)
+    // Submit the form (from GO button -- as in hgGateway.js)
     // Show a spinner -- sometimes it takes a while for hgTracks to start displaying.
-    $('.jwGoIcon').removeClass('fa-play').addClass('fa-spinner fa-spin');
+    $('.gbGoIcon').removeClass('fa-play').addClass('fa-spinner fa-spin');
     $form = $('form');
     $form.submit();
     }
@@ -296,14 +274,15 @@ var gtexTrackSettings = (function() {
         // cart.setCgi('gtexTrackSettings');
 
         $(function() {
-            // after page load, tweak layout and initialize event handlers
+            // After page load, tweak layout and initialize event handlers
+            // TODO: need to wait onready ?
             var bodyMapSvg = document.getElementById("bodyMapSvg");
             globalSvg = bodyMapSvg;
 
             _htmlDoc = document;
             _htmlRoot = document.documentElement;
 
-            // wait for SVG to load
+            // Wait for SVG to load
             bodyMapSvg.addEventListener("load", function() {
                 var svg = bodyMapSvg.contentDocument.documentElement;
                 _svgRoot = svg;
@@ -312,7 +291,6 @@ var gtexTrackSettings = (function() {
                 initBodyMap(svg, bodyMapSvg.contentDocument);
                 animateTissues();
             }, false);
-
             $('.goButtonContainer').click(submitForm);
         });
     }
@@ -323,4 +301,8 @@ var gtexTrackSettings = (function() {
     
 }()); // gtexTrackSettings
 
-gtexTrackSettings.init();
+$(document).ready(function() {
+    gtexTrackSettings.init();
+});
+
+//gtexTrackSettings.init();
