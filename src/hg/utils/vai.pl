b0b401942ab87c2208fec5bfbeeeb4a1cb1bc326
Angie Hinrichs <angie@soe.ucsc.edu>
2016-12-14 16:48:38 -0800

    If HGDB_CONF is not set and hg.conf is not in the usual places, look for it in hgVai's parent dir and set HGDB_CONF if found.  refs #12216 note-32

--- src/hg/utils/vai.pl
+++ src/hg/utils/vai.pl
@@ -259,17 +259,26 @@ if ($hgVaiParams{hgva_variantTrack} eq 'hgva_useVariantFileOrUrl' &&
 }
 
 # If env var ALL_JOINER_FILE is not already set, try to find an all.joiner to set it to.
-if (! $ENV{ALL_JOINER_FILE})
-  {
-    if (-e "all.joiner") {
-      $ENV{ALL_JOINER_FILE} = "all.joiner";
-    } else {
-      my $hgVaiDir = dirname $hgVai;
-      my $joinerFile = $hgVaiDir . "/all.joiner";
-      if (-e $joinerFile) {
-        $ENV{ALL_JOINER_FILE} = $joinerFile};
+my $hgVaiDir = dirname $hgVai;
+if (! $ENV{ALL_JOINER_FILE}) {
+  if (-e "all.joiner") {
+    $ENV{ALL_JOINER_FILE} = "all.joiner";
+  } else {
+    my $joinerFile =  "$hgVaiDir/all.joiner";
+    if (-e $joinerFile) {
+      $ENV{ALL_JOINER_FILE} = $joinerFile};
+  }
+}
+
+# If env var HGDB_CONF is not already set, try to find an hg.conf to set it to.
+if (! $ENV{HGDB_CONF}) {
+  if (! -e "hg.conf" && ! -e $ENV{HOME}."/.hg.conf") {
+    my $hgConf = "$hgVaiDir/hg.conf";
+    if (-e $hgConf) {
+      $ENV{HGDB_CONF} = $hgConf;
     }
   }
+}
 
 my @params = map { "$_=" . $hgVaiParams{$_} } keys %hgVaiParams;
 my $command = "$hgVai '" . join('&', @params) . "'";
