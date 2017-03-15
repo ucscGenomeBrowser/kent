3202c22c6702bbbaf211bd054403e2fbf3c8c071
Brian Raney <braney@soe.ucsc.edu>
2017-03-15 11:54:20 -0700

    fix brain dead error I introduced in snake support.  Also, clean up error handling.

--- src/hg/hgTracks/snakeTrack.c
+++ src/hg/hgTracks/snakeTrack.c
@@ -1280,13 +1280,16 @@ if (errCatchStart(errCatch))
     {
     char *fileName = trackDbSetting(tg->tdb, "bigDataUrl");
     char *otherSpecies = trackDbSetting(tg->tdb, "otherSpecies");
-    char *errString;
+    char *errString = "empty";
     int handle = -1;
     if (!isPsl)
         {
-        int handle = halOpenLOD(fileName, &errString);
+        handle = halOpenLOD(fileName, &errString);
         if (handle < 0)
-            warn("HAL open error: %s\n", errString);
+            {
+            errAbort("HAL open error: %s\n", errString);
+            goto out;
+            }
         }
     boolean isPackOrFull = (tg->visibility == tvFull) || 
 	(tg->visibility == tvPack);
@@ -1311,9 +1314,8 @@ if (errCatchStart(errCatch))
     // did we get any blocks from HAL
     if (head == NULL)
 	{
-	warn("HAL get blocks error: %s\n", errString);
-	errCatchEnd(errCatch);
-	return;
+	errAbort("HAL get blocks error: %s\n", errString);
+	goto out;
 	}
     struct hal_block_t* cur = head->mappedBlocks;
     struct linkedFeatures *lf = NULL;
@@ -1426,6 +1428,8 @@ if (errCatchStart(errCatch))
 
     tg->items = lfList;
     }
+
+out:
 errCatchEnd(errCatch);
 if (errCatch->gotError)
     {
