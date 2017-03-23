ucscGenomeBrowser/kent

commit 6072cb04b34e57ebdef620512124c4d8722b79df

hAnnoGetAssembly assumed that db.2bit would be found in hDbDbNibPath(db) but for old nib/ assemblies it is one level up in /gbdb/db/.  Similar to hdb's hNibForChrom, test file existence and look in ../ as a backup.  fixes #18445

src/hg/lib/hAnno.c
@@ -49,6 +49,12 @@ if (aa == NULL)
         char twoBitPath[HDB_MAX_PATH_STRING];
         safef(twoBitPath, sizeof(twoBitPath), "%s/%s.2bit", nibOrTwoBitDir, db);
         char *path = hReplaceGbdb(twoBitPath);
+        if (!fileExists(path))
+            {
+            // if 2bit file isn't in nibOrTwoBitDir, try up one directory
+            safef(twoBitPath, sizeof(twoBitPath), "%s/../%s.2bit", nibOrTwoBitDir, db);
+            path = hReplaceGbdb(twoBitPath);
+            }
         aa = annoAssemblyNew(db, path);
         freeMem(path);
         }

commit e52b9728905070e9979662928bccd604db61d8f4

Commit 6072cb04b (#18445) introduced a bug: when hReplaceGbdb changes a path into a URL, fileExists() is no longer a sufficient test.  Using hDbDbNibPath was unnecessarily complicated -- we have standardized on /gbdb/$db/$db.2bit for 2bit files.  fixes #19113

src/hg/lib/hAnno.c
@@ -43,18 +43,9 @@ if (aa == NULL)
         }
     else
         {
-        char *nibOrTwoBitDir = hDbDbNibPath(db);
-        if (nibOrTwoBitDir == NULL)
-            errAbort("Can't find .2bit for db '%s'", db);
         char twoBitPath[HDB_MAX_PATH_STRING];
-        safef(twoBitPath, sizeof(twoBitPath), "%s/%s.2bit", nibOrTwoBitDir, db);
+        safef(twoBitPath, sizeof(twoBitPath), "/gbdb/%s/%s.2bit", db, db);
         char *path = hReplaceGbdb(twoBitPath);
-        if (!fileExists(path))
-            {
-            // if 2bit file isn't in nibOrTwoBitDir, try up one directory
-            safef(twoBitPath, sizeof(twoBitPath), "%s/../%s.2bit", nibOrTwoBitDir, db);
-            path = hReplaceGbdb(twoBitPath);
-            }
         aa = annoAssemblyNew(db, path);
         freeMem(path);
         }