760d941586a240c1b4c4e1143f1fd2a3f8ff96c1
Brian Raney <braney@soe.ucsc.edu>
2017-04-07 11:26:45 -0700

    add the ability to specify a fielded tab-sep file for metadata in a track hub

--- src/hg/lib/trackHub.c
+++ src/hg/lib/trackHub.c
@@ -783,10 +783,10 @@ else
           startsWithWord("bigGenePred", type) ||
           startsWithWord("bigChain", type) ||
           startsWithWord("bam", type)))
-	{
-	errAbort("Unsupported type '%s' in hub %s genome %s track %s", type,
-	    hub->url, genome->name, tdb->track);
-	}
+        {
+        errAbort("Unsupported type '%s' in hub %s genome %s track %s", type,
+            hub->url, genome->name, tdb->track);
+        }
 
     requiredSetting(hub, genome, tdb, "bigDataUrl");
     }
@@ -868,6 +868,11 @@ struct lineFile *lf = udcWrapShortLineFile(genome->trackDbFile, NULL, 64*1024*10
 struct trackDb *tdbList = trackDbFromOpenRa(lf, NULL);
 lineFileClose(&lf);
 
+char *tabMetaName = hashFindVal(genome->settingsHash, "metaTab");
+char *absTabName  = NULL;
+if (tabMetaName)
+    absTabName  = trackHubRelativeUrl(hub->url, tabMetaName);
+
 char *tagStormName = hashFindVal(genome->settingsHash, "metaDb");
 char *absStormName  = NULL;
 if (tagStormName)
@@ -880,6 +885,8 @@ for (tdb = tdbList; tdb != NULL; tdb = tdb->next)
     expandBigDataUrl(hub, genome, tdb);
     if  (absStormName)
         hashReplace(tdb->settingsHash, "metaDb", absStormName);
+    if  (absTabName)
+        hashReplace(tdb->settingsHash, "metaTab", absTabName);
     }
 
 validateTracks(hub, genome, tdbList);
