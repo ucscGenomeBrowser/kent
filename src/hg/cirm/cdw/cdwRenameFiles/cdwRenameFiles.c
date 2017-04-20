d768bddae49de22bc73eab55b3b41fc6a269923a
Jim Kent <kent@soe.ucsc.edu>
2017-04-20 11:18:18 -0700

    Added more tests and making command line simpler by directly doing moves and database updates.

--- src/hg/cirm/cdw/cdwRenameFiles/cdwRenameFiles.c
+++ src/hg/cirm/cdw/cdwRenameFiles/cdwRenameFiles.c
@@ -3,9 +3,12 @@
 #include "linefile.h"
 #include "hash.h"
 #include "options.h"
+#include "obscure.h"
 #include "fieldedTable.h"
 #include "portable.h"
 #include "cdwLib.h"
+
+boolean clDry = FALSE;
 
 void usage()
 /* Explain usage and exit. */
@@ -13,42 +16,30 @@ void usage()
 errAbort(
   "cdwRenameFiles - Rename files' submitted name both in database and manifest.\n"
   "usage:\n"
-  "   cdwRenameFiles rename.tab oldManifest.txt newManifest.txt rename.sql\n"
+  "   cdwRenameFiles rename.tab manifest.txt\n"
   "where:\n"
   "   rename.tab is a two column tab or space separated files with old file name in\n"
   "              one column and current file name in the other\n"
-  "   oldManifest.txt is the manifest file with the old name in it\n"
-  "   newManifest.txt is the manifest file with the new name\n"
-  "   rename.sql is the sql to execute to make the change in the database\n"
+  "   manifest.txt is the manifest file with the old name in it. It will be rewritten with new\n"
+  "opions:\n"
+  "   -dry - only do test run, don't alter database or file\n"
   );
 }
 
 /* Command line validation table. */
 static struct optionSpec options[] = {
+   {"dry", OPTION_BOOLEAN},
    {NULL, 0},
 };
 
-struct oldToNew
-/* Store old name and new name */
-    {
-    struct oldToNew *next;
-    char *oldName;
-    char *newName;
-    };
-
-struct oldToNew *oldToNewFromFile(char *fileName)
-/* Read in a two column file into an oldToNew list */
+struct slPair *slPairTwoColumnFile(char *fileName)
+/* Read in a two column file into an slPair list */
 {
 char *row[2];
-struct oldToNew *list = NULL, *el;
+struct slPair *list = NULL;
 struct lineFile *lf = lineFileOpen(fileName, TRUE);
 while (lineFileRow(lf, row))
-    {
-    AllocVar(el);
-    el->oldName = cloneString(row[0]);
-    el->newName = cloneString(row[1]);
-    slAddHead(&list, el);
-    }
+    slPairAdd(&list, row[0], cloneString(row[1]));
 lineFileClose(&lf);
 slReverse(&list);
 return list;
@@ -87,49 +78,76 @@ for (fr = table->rowList; fr != NULL; fr = fr->next)
 carefulClose(&f);
 }
 
-void cdwRenameFiles(char *renameFile, char *oldMani, char *newMani, char *renameSql)
-/* cdwRenameFiles - Rename files submitted name.. */
+void cdwRenameFiles(char *renameFile, char *maniFile)
+/* cdwRenameFiles - Rename files submitted name. */
 {
 /* Figure out submission directory */
-struct sqlConnection *conn = cdwConnect();
+struct sqlConnection *conn = cdwConnectReadWrite();
 char *submitDir = getCurrentDir();
-char query[512];
+char query[3*PATH_LEN];
 sqlSafef(query, sizeof(query), "select id from cdwSubmitDir where url='%s'", submitDir);
 int submitDirId = sqlQuickNum(conn, query);
 if (submitDirId == 0)
     errAbort("%s not in cdwSubmit, run program from submission directory please\n", submitDir);
 
 char *requiredFields[] = {"file",} ;
-struct fieldedTable *maniTable = fieldedTableFromTabFile(oldMani, oldMani, requiredFields, 
+struct fieldedTable *maniTable = fieldedTableFromTabFile(maniFile, maniFile, requiredFields, 
     ArraySize(requiredFields));
 int fileIx = stringArrayIx("file", maniTable->fields, maniTable->fieldCount);
 struct hash *maniHash = fieldedTableUniqueIndex(maniTable, "file");
 
-FILE *sqlF = mustOpen(renameSql, "w");
-
-struct oldToNew *mv, *mvList = oldToNewFromFile(renameFile);
+/* Load in rename file and do one pass to make sure everything exists and is kosher */
+struct slPair *mv, *mvList = slPairTwoColumnFile(renameFile);
 verbose(1, "Got %d files to rename in %s\n", slCount(mvList), renameFile);
 for (mv = mvList; mv != NULL; mv = mv->next)
     {
-    verbose(2, "%s->%s\n", mv->oldName, mv->newName);
-    struct fieldedRow *fr = hashFindVal(maniHash, mv->oldName);
+    char *oldName = mv->name;
+    char *newName = mv->val;
+    verbose(2, "checking %s->%s\n", oldName, newName);
+    if (sameString(oldName, newName))
+        errAbort("Can't rename %s to itself", oldName);
+    struct fieldedRow *fr = hashFindVal(maniHash, oldName);
+    sqlSafef(query, sizeof(query), 
+	"select count(*) from cdwFile where submitFileName='%s' and submitDirId=%d",
+	oldName, submitDirId);
+    int count = sqlQuickNum(conn, query);
+    if (count == 0)
+        errAbort("%s not found in database in %s", oldName, submitDir);
     if (fr == NULL)
-        errAbort("%s not found in %s\n", mv->oldName, oldMani);
-    fr->row[fileIx] = mv->newName;
-    fprintf(sqlF, 
-	"update cdwFile set submitFileName='%s' where submitFileName='%s' and submitDirId=%d;\n",
-	mv->newName, mv->oldName, submitDirId);
+        errAbort("%s not found in %s", oldName, maniFile);
+    if (!fileExists(oldName))
+        errAbort("%s file does not exist.", oldName);
     }
-carefulClose(&sqlF);
-fieldedTableToTabFile(maniTable, newMani);
+
+for (mv = mvList; mv != NULL; mv = mv->next)
+    {
+    char *oldName = mv->name;
+    char *newName = mv->val;
+    verbose(2, "doing %s->%s\n", oldName, newName);
+    struct fieldedRow *fr = hashFindVal(maniHash, oldName);
+    fr->row[fileIx] = newName;
+    sqlSafef(query, sizeof(query), 
+	"update cdwFile set submitFileName='%s' where submitFileName='%s' and submitDirId=%d",
+	newName, oldName, submitDirId);
+    if (!clDry)
+	{
+        sqlUpdate(conn, query);
+	mustRename(oldName, newName);
+	}
+    else
+        verbose(1, "%s\n", query);
+    }
+if (!clDry)
+    fieldedTableToTabFile(maniTable, maniFile);
 }
 
 int main(int argc, char *argv[])
 /* Process command line. */
 {
 optionInit(&argc, argv, options);
-if (argc != 5)
+if (argc != 3)
     usage();
-cdwRenameFiles(argv[1], argv[2], argv[3], argv[4]);
+clDry = optionExists("dry");
+cdwRenameFiles(argv[1], argv[2]);
 return 0;
 }
