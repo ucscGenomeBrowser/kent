c7a2bf8be3b9d034ea579c9860ac2a774f05ead8
Jim Kent <kent@soe.ucsc.edu>
2017-04-20 17:39:14 -0700

    Polishign up cdwRenameDataset.

--- src/hg/cirm/cdw/cdwRenameDataset/cdwRenameDataset.c
+++ src/hg/cirm/cdw/cdwRenameDataset/cdwRenameDataset.c
@@ -1,4 +1,6 @@
-/* cdwRenameDataset - Rename a dataset, updating cdwDataset, cdwSubmitDir.url, and cdwSubmit.url tables.. */
+/* cdwRenameDataset - Rename a dataset, updating cdwDataset, cdwSubmitDir.url, and cdwSubmit.url 
+ * tables.. */
+
 #include "common.h"
 #include "linefile.h"
 #include "hash.h"
@@ -11,12 +13,12 @@ void usage()
 /* Explain usage and exit. */
 {
 errAbort(
-  "cdwRenameDataset - Rename a dataset, updating cdwDataset, cdwSubmitDir.url, and cdwSubmit.url tables.\n"
-  "You'll still need to update meta.txt and resubmit it\n"
+  "cdwRenameDataset - Rename a dataset, updating cdwDataset, cdwSubmitDir.url, and cdwSubmit.url\n"
+  "tables.  You'll still need to update meta.txt and resubmit it, and rename the wrangle dir.\n"
   "usage:\n"
   "   cdwRenameDataset oldName newName\n"
   "options:\n"
-  "   -xxx=XXX\n"
+  "   -dry - don't actually modify database.\n"
   );
 }
 
@@ -26,7 +28,7 @@ static struct optionSpec options[] = {
    {NULL, 0},
 };
 
-void update(struct sqlConnection *conn, char *query)
+static void update(struct sqlConnection *conn, char *query)
 /* If clDry permits update database, else just print. */
 {
 if (clDry)
@@ -35,34 +37,43 @@ else
     sqlUpdate(conn, query);
 }
 
-void cdwRenameDataset(char *oldName, char *newName)
-/* cdwRenameDataset - Rename a dataset, updating cdwDataset, cdwSubmitDir.url, and cdwSubmit.url tables.. */
+static void mustStrSwapOne(char *string, int sz, char *oldStr, char *newStr)
+/* Swap in a single string or die trying */
 {
+int subCount = strSwapStrs(string, sz, oldStr, newStr);
+if (subCount != 1)
+    errAbort("%d %s found in %s in mustStrSwapOne, must occur once", subCount, oldStr, string);
+}
+
+void cdwRenameDataset(char *oldName, char *newName)
+/* cdwRenameDataset - Rename a dataset, updating cdwDataset, cdwSubmitDir.url, and cdwSubmit.url 
+ * tables. */
+{
+/* Update entry in cdwDataset */
 struct sqlConnection *conn = cdwConnectReadWrite();
 char query[3*PATH_LEN];
-sqlSafef(query, sizeof(query), "update cdwDataset set name='%s' where name ='%s'", newName, oldName);
+sqlSafef(query, sizeof(query), 
+    "update cdwDataset set name='%s' where name ='%s'", newName, oldName);
 update(conn, query);
+
+/* Figure out submitDir that ends with the dataset name */
 sqlSafef(query, sizeof(query), "select count(*) from cdwSubmitDir where url like '/%%%s'", oldName);
 int dirCount = sqlQuickNum(conn, query);
 if (dirCount != 1)
     errAbort("Got %d cdwSubmitDir URLs ending in /%s, expecting 1", dirCount, oldName);
 sqlSafef(query, sizeof(query), "select id from cdwSubmitDir where url like '/%%%s'", oldName);
 int submitDirId = sqlQuickNum(conn, query);
-uglyf("submitDirId = %d\n", submitDirId);
 
+/* Update cdwSubmitDir.url with substituted url. */
 sqlSafef(query, sizeof(query), "select url from cdwSubmitDir where id=%d", submitDirId);
 char *oldSubmitUrl = sqlQuickString(conn, query);
-uglyf("oldSubmitUrl = %s\n", oldSubmitUrl);
-
 char newUrl[2*PATH_LEN];
 safef(newUrl, sizeof(newUrl), "%s", oldSubmitUrl);
-int subCount = strSwapStrs(newUrl, sizeof(newUrl), oldName, newName);
-if (subCount != 1)
-    errAbort("Expecting to make 1 substitution, made %d", subCount);
-uglyf("newSubmitUrl = %s, subCount=%d\n", newUrl, subCount);
+mustStrSwapOne(newUrl, sizeof(newUrl), oldName, newName);
 sqlSafef(query, sizeof(query), "update cdwSubmitDir set url='%s' where id=%d", newUrl, submitDirId);
 update(conn, query);
 
+/* Update cdwSubmit.url with substituted url wherever it matches submitDir. */
 struct sqlConnection *conn2 = cdwConnectReadWrite();
 sqlSafef(query, sizeof(query), "select id,url from cdwSubmit where submitDirId=%d", submitDirId);
 struct sqlResult *sr = sqlGetResult(conn, query);
@@ -72,9 +83,7 @@ while ((row = sqlNextRow(sr)) != NULL)
     char *id = row[0];
     char *oldUrl = row[1];
     safef(newUrl, sizeof(newUrl), "%s", oldUrl);
-    int subCount = strSwapStrs(newUrl, sizeof(newUrl), oldName, newName);
-    if (subCount != 1)
-	errAbort("Expecting to make 1 substitution, made %d", subCount);
+    mustStrSwapOne(newUrl, sizeof(newUrl), oldName, newName);
     sqlSafef(query, sizeof(query), "update cdwSubmit set url='%s' where id=%s", newUrl, id);
     update(conn2, query);
     }
