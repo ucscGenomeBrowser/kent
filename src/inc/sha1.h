71b6403f90f9f221004a07cbd4e5aecedd9f6438
Galt Barber <galt@soe.ucsc.edu>
2017-04-24 12:41:27 -0700

    Refs #17538. Adds ability for user to paste a custom BED region into the multi-region dialog box instead of having to provide a URL to an on-line server.  It still supports the URL through UDC however, which is more efficient for large custom region lists and also provides backwards compatibility.  Uses sha1 instead of md5 for faster speed now.

--- src/inc/sha1.h
+++ src/inc/sha1.h
@@ -5,16 +5,16 @@
 #define SHA1_H
 
 typedef struct {
-    uint32_t state[5];
-    uint32_t count[2];
-    uint8_t  buffer[64];
+    bits32 state[5];
+    bits32 count[2];
+    bits8  buffer[64];
 } SHA1_CTX;
 
 #define SHA1_DIGEST_SIZE 20
 
 void SHA1_Init(SHA1_CTX* context);
-void SHA1_Update(SHA1_CTX* context, const uint8_t* data, const size_t len);
-void SHA1_Final(SHA1_CTX* context, uint8_t digest[SHA1_DIGEST_SIZE]);
+void SHA1_Update(SHA1_CTX* context, const bits8* data, const size_t len);
+void SHA1_Final(SHA1_CTX* context, bits8 digest[SHA1_DIGEST_SIZE]);
 
 /* ============== Added by UCSC Genome Browser ============= */
 
