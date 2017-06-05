89ba7232309a06a09398d9207f71da284258ba66
Jim Kent <kent@soe.ucsc.edu>
2017-06-04 23:00:02 -0700

    Dealing with idf.Person* tags.  Also turning some weeding of redundant terms to more conservative 'replaceWithFirstChoice.

--- src/hca/fromArrayExpress/arrayExpressStormToHcaStorm/arrayExpressStormToHcaStorm.c
+++ src/hca/fromArrayExpress/arrayExpressStormToHcaStorm/arrayExpressStormToHcaStorm.c
@@ -34,16 +34,28 @@ char *weeds[] =
     "idf.Comment_SequenceDataURI",	// Takes you to web page can get to other ways
     "idf.Experimental_Design_Term_Source_REF",  // Can be deduced from related field
     "idf.Experimental_Factor_Term_Source_REF",	// Can be deduced from related field
+    "idf.Experimental_Factor_Term_Accession_Number", // Sparsely used and uninformative
     "idf.Experimental_Factor_Type",  // Redundant with idf.Experimental_Factor_Name
     "sdrf.Characteristics_organism_Term_Source_REF",  // Can be deduced from related field
     "sdrf.Derived_Array_Data_File",  // Redundant with sdrf.Comment_Derived_ArrayExpress_FTP_file
     "sdrf.Scan_Name",		     // Redundant with sdrf.Comment_SUBMITTED_FILE_NAME
     "sdrf.Extract_Name",	     // Redundant with sdrf.Source_Name
-    "sdrf.Factor_Value_cell_type",   // Redundant with Characteristics_cell_type
-    "sdrf.Factor_Value_disease",     // Redundant with Characteristics_disease
     "sdrf.Technology_Type",          // Always "sequencing assay" which is captured elsewhere
     "sdrf.Characteristics_age_Unit_Term_Accession_Number",  // Overkill for day/week/month/year
     "sdrf.Characteristics_age_Unit_Term_Source_REF",  // Overkill for day/week/month/year
+    };
+
+
+char *cellTypeTags[] =
+    {
+    "sdrf.Characteristics_cell_type", 
+    "sdrf.Factor_Value_cell_type",   // Redundant with Characteristics_cell_type
+    };
+
+char *diseaseTags[] =
+    {
+    "sdrf.Characteristics_disease", 
+    "sdrf.Factor_Value_disease",     // Redundant with Characteristics_disease
     };
 
 char *machineTags[] = 
@@ -57,49 +69,40 @@ char *machineTags[] =
 char *substitutions[][2] = 
 /* Tags we'll rename */
 {
-{"idf.Comment_AEExperimentType", "project.array_express_experiment_type",}, 
+{"idf.Comment_AEExperimentType", "project.array_express.experiment_type",}, 
 {"idf.Comment_AdditionalFile_Data", "project.supplementary_files",}, 
 {"idf.Comment_AdditionalFile_txt", "reformat.Comment_AdditionalFile_txt",}, 
-{"idf.Comment_ArrayExpressAccession", "project.array_express_accession",}, 
+{"idf.Comment_ArrayExpressAccession", "project.array_express.accession",}, 
 {"idf.Comment_ArrayExpressSubmissionDate", "project.submission_date",}, 
 {"idf.Comment_GEOLastUpdateDate", "project.last_update_date",}, 
 {"idf.Comment_GEOReleaseDate", "project.release_status",}, 
-{"idf.Comment_RelatedExperiment", "project.array_express_related_experiment",}, 
+{"idf.Comment_RelatedExperiment", "project.array_express.related_experiment",}, 
 {"idf.Comment_SecondaryAccession", "reformat.Comment_SecondaryAccession",}, 
 {"idf.Experiment_Description", "project.summary",}, 
 {"idf.Experimental_Design", "project.overall_design",}, 
 {"idf.Experimental_Design_Term_Accession_Number", "project.overall_design_accession",}, 
 {"idf.Experimental_Factor_Name", "project.experimental_factor_name",}, 
-{"idf.Experimental_Factor_Term_Accession_Number", "project.experimental_factor_ontology",}, 
 {"idf.Investigation_Title", "project.title",}, 
 {"idf.MAGE_TAB_Version", "project.mage_tab_version",}, 
-{"idf.Person_Address", "reformat.Person_Address",}, 
-{"idf.Person_Affiliation", "project.contact_institution",}, 
-{"idf.Person_Email", "project.contact_email",}, 
-{"idf.Person_First_Name", "reformat.Person_First_Name",}, 
-{"idf.Person_Last_Name", "reformat.Person_Last_Name",}, 
-{"idf.Person_Mid_Initials", "reformat.Person_Mid_Initials"},
-{"idf.Person_Phone", "project.contact_phone",}, 
-{"idf.Person_Roles", "reformat.Person_Roles",}, 
 {"idf.Protocol_Description", "project.extract_protocol",}, 
 {"idf.Protocol_Name", "reformat.Protocol_Name",}, 
 {"idf.Protocol_Term_Accession_Number", "reformat.Protocol_Term_Accession_Number",}, 
 {"idf.Protocol_Term_Source_REF", "reformat.Protocol_Term_Source_REF",}, 
 {"idf.Protocol_Type", "reformat.Protocol_Type",}, 
 {"idf.PubMed_ID", "project.pmid",}, 
+{"idf.PubMedID", "project.pmid",}, 
+{"idf.Pubmed_ID", "project.pmid",}, 
 {"idf.Public_Release_Date", "project.release_status",}, 
 {"idf.Publication_Author_List", "project.publication_author_list",}, 
 {"idf.Publication_DOI", "project.publication_doi",}, 
 {"idf.Publication_Title", "project.publication_title",}, 
-{"idf.Term_Source_File", "project.ebi_term_source_file",}, 
-{"idf.Term_Source_Name", "project.ebi_term_source_name",}, 
+{"idf.Term_Source_File", "project.array_express.term_source_file",}, 
+{"idf.Term_Source_Name", "project.array_express.term_source_name",}, 
 {"sdrf.Assay_Name", "assay.array_express_assay_name",}, 
 {"sdrf.Characteristics_age", "sample.donor.age",}, 
 {"sdrf.Characteristics_age_Unit", "sample.donor.age_unit",}, 
 {"sdrf.Characteristics_body_mass_index", "sample.donor.body_mass_index",}, 
-{"sdrf.Characteristics_cell_type", "cell.type",}, 
 {"sdrf.Characteristics_developmental_stage", "sample.donor.developmental_stage",}, 
-{"sdrf.Characteristics_disease", "sample.donor.disease",}, 
 {"sdrf.Characteristics_genotype", "sample.donor.genotype",}, 
 {"sdrf.Characteristics_individual", "sample.donor.id",}, 
 {"sdrf.Characteristics_organism", "sample.donor.species",}, 
@@ -131,7 +134,7 @@ char *substitutions[][2] =
 {"sdrf.Source_Name", "sample.submitted_id",}, 
 };
 
-void replaceWithBestChoice(struct tagStorm *storm, struct tagStanzaRef *leafList, 
+void replaceWithFirstChoice(struct tagStorm *storm, struct tagStanzaRef *leafList, 
     char *choices[], int choiceCount, char *replacement)
 /* On each leaf stanza, look for a value in list, which is ordered with
  * what is likely to be best tag first.  Choose the first one that has
@@ -156,6 +159,89 @@ for (ref = leafList; ref != NULL; ref = ref->next)
 tagStormWeedArray(storm, choices, choiceCount);
 }
 
+void addFirstWithVal(struct tagStorm *storm, struct tagStanza *stanza, char *oldTag, char *newTag)
+/* Look up old tag in stanza and pick first real value out of it.  Store this in newTag.
+ * If there are no real values then don't make newTag. */
+{
+struct slName *valList = csvParse(tagFindVal(stanza, oldTag));
+struct slName *val = valList;
+while (val != NULL && isEmpty(val->name))	// Skip over any with no value
+    val = val->next;
+if (val != NULL)
+    tagStanzaAppend(storm, stanza, newTag, val->name);
+slFreeList(&valList);
+}
+
+char *personTags[] = 
+    {
+    "idf.Person_Address",
+    "idf.Person_Affiliation",
+    "idf.Person_Email",
+    "idf.Person_First_Name",
+    "idf.Person_Last_Name",
+    "idf.Person_Mid_Initials",
+    "idf.Person_Phone",
+    "idf.Person_Roles",	    // Gets weeded but not replaced. Difficult not super informative.
+    };
+
+void reformatPersonTags(struct tagStorm *storm)
+/* Convert idf.Person_* tags to project.contact_* and project.contributor tag */
+{
+struct tagStanza *stanza = storm->forest;  // Project tags/idf tags live on top
+
+/* Fetch the components of names.  We'll be converting them to first,mid,last */
+struct slName *firstList = csvParse(tagFindVal(stanza, "idf.Person_First_Name"));
+struct slName *midList = csvParse(tagFindVal(stanza, "idf.Person_Mid_Initials"));
+struct slName *lastList= csvParse(tagFindVal(stanza, "idf.Person_Last_Name"));
+char combinedName[256], lastCombinedName[256] = "";
+
+/* We'll build up contributor string.  First element of it will be contact_name as well. */
+struct dyString *contributorOut = dyStringNew(0);
+struct slName *firstEl = firstList, *midEl = midList, *lastEl = lastList;
+boolean firstTime = TRUE;
+for (;;)
+    {
+    char *first = NULL, *mid = NULL, *last = NULL;
+    if (firstEl != NULL)
+        {
+	first = firstEl->name;
+	firstEl = firstEl->next;
+	}
+    if (midEl != NULL)
+        {
+	mid = midEl->name;
+	midEl = midEl->next;
+	}
+    if (lastEl != NULL)
+        {
+	last = lastEl->name;
+	lastEl = lastEl->next;
+	}
+    if (first == NULL && mid == NULL && last == NULL)
+        break;
+    safef(combinedName, sizeof(combinedName), "%s,%s,%s", 
+	emptyForNull(first), emptyForNull(mid), emptyForNull(last));
+    if (differentString(combinedName, lastCombinedName))	  // Work around for AE dupe problem
+	csvEscapeAndAppend(contributorOut, combinedName);
+    if (firstTime)
+        {
+	tagStanzaAppend(storm, stanza, "project.contact_name", contributorOut->string);
+	firstTime = FALSE;
+	}
+    strcpy(lastCombinedName, combinedName);
+    }
+tagStanzaAppend(storm, stanza, "project.contributor", contributorOut->string);
+
+/* Ok, we are done with names, now for easy things: email and phone. We only store first one */
+addFirstWithVal(storm, stanza, "idf.Person_Address", "project.contact_address");
+addFirstWithVal(storm, stanza, "idf.Person_Email", "project.contact_email");
+addFirstWithVal(storm, stanza, "idf.Person_Phone", "project.contact_phone");
+addFirstWithVal(storm, stanza, "idf.Person_Affiliation", "project.contact_institute");
+
+tagStormWeedArray(storm, personTags, ArraySize(personTags));
+}
+
+
 void arrayExpressStormToHcaStorm(char *inTags, char *outTags)
 /* arrayExpressStormToHcaStorm - Convert output of arrayExpressToTagStorm to somethng closer to 
  * what the Human Cell Atlas wants.. */
@@ -164,9 +250,17 @@ struct tagStorm *storm = tagStormFromFile(inTags);
 tagStormWeedArray(storm, weeds, ArraySize(weeds));
 tagStormSubArray(storm, substitutions, ArraySize(substitutions));
 
+/* Deal with Person tags */
+reformatPersonTags(storm);
+
 /* Get list of leafs for further work. */
 struct tagStanzaRef *leafList = tagStormListLeaves(storm);
-replaceWithBestChoice(storm, leafList, machineTags, ArraySize(machineTags), "assay.seq.machine");
+
+/* Deal with some tags that we just select one from a redundant set. */
+replaceWithFirstChoice(storm, leafList, machineTags,ArraySize(machineTags), "assay.seq.machine");
+replaceWithFirstChoice(storm, leafList, cellTypeTags,ArraySize(cellTypeTags), "cell.type");
+replaceWithFirstChoice(storm, leafList, diseaseTags,ArraySize(diseaseTags), "sample.donor.disease");
+
 
 tagStormWrite(storm, outTags, 0);
 }
