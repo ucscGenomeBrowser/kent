a79a8c54828f0c8d1c9173089d1bde771f404dd1
Jim Kent <kent@soe.ucsc.edu>
2017-06-28 16:53:55 -0700

    Making it deal with subobjects.

--- src/tagStorm/tagSchemaToJson/tagSchemaToJson.c
+++ src/tagStorm/tagSchemaToJson/tagSchemaToJson.c
@@ -4,10 +4,13 @@
 #include "linefile.h"
 #include "hash.h"
 #include "options.h"
+#include "portable.h"
 #include "tagSchema.h"
 #include "jsonWrite.h"
+#include "tagToJson.h"
 
 struct hash *gDescriptions = NULL;
+char *gInName = NULL;
 
 void usage()
 /* Explain usage and exit. */
@@ -16,7 +19,7 @@ errAbort(
   "tagSchemaToJson - Convert a tagStormSchema (such as used by tagStormCheck) to a JSON\n"
   "schema such as decribed in http://json-schema.org/\n"
   "usage:\n"
-  "   tagSchemaToJsonSchema tagSchema.in jsonSchema.json\n"
+  "   tagSchemaToJsonSchema tagSchema.in outDir\n"
   "options:\n"
   "   -descriptions=twoCol.txt - first col is tag, second description\n"
   );
@@ -120,30 +123,17 @@ else
 jsonWriteObjectEnd(jw);
 }
 
-void tagSchemaToJson(char *inTagSchema, char *outJson)
-/* tagSchemaToJsonSchema - Convert a tagStormSchema (such as used by tagStormCheck) to a JSON 
- * schema such as decribed in http://json-schema.org/. */
+void writeHighFields(struct jsonWrite *jw, struct ttjSubObj *obj, struct hash *schemaHash)
+/* Write out type, properties, patternProperties, required, and so forth to jw  */
 {
-struct tagSchema *schemaList = tagSchemaFromFile(inTagSchema);
-struct jsonWrite *jw = jsonWriteNew();
-jw->sep = '\n';
-
-jsonWriteObjectStart(jw, NULL);
-
-/* Write out bits that don't actually depend on our schema */
-jsonWriteString(jw, "$schema", "http://json-schema.org/draft-04/schema#");
-jsonWriteString(jw, "title", inTagSchema);
-char text[1024];
-safef(text, sizeof(text), "schema generate by tagSchemaToJson from %s", inTagSchema);
-jsonWriteString(jw, "description", text);
 jsonWriteString(jw, "type", "object");
+struct ttjSubObj *sub;
 
 /* Figure out if we have to do fixed properties and or pattern properties */
 boolean needFixed = FALSE, needPattern = FALSE;
-struct tagSchema *schema;
-for (schema = schemaList; schema != NULL; schema = schema->next)
+for (sub = obj->children; sub != NULL; sub = sub->next)
     {
-    if (anyWild(schema->name))
+    if (anyWild(sub->name))
         needPattern = TRUE;
     else
         needFixed = TRUE;
@@ -152,28 +142,42 @@ for (schema = schemaList; schema != NULL; schema = schema->next)
 if (needFixed)
     {
     jsonWriteObjectStart(jw, "properties");
-    for (schema = schemaList; schema != NULL; schema = schema->next)
+    for (sub = obj->children; sub != NULL; sub = sub->next)
 	{
-	if (!anyWild(schema->name))
+	if (sub->children != NULL)
 	    {
-	    writeOneSchema(jw, schema->name, schema);
+	    jsonWriteObjectStart(jw, sub->name);
+	    char refText[512];
+	    safef(refText, sizeof(refText), "#/definitions/%s", sub->name);
+	    jsonWriteString(jw, "$ref", refText);
+	    jsonWriteObjectEnd(jw);
 	    }
-
+	else
+	    {
+	    if (!anyWild(sub->name))
+		{
+		struct tagSchema *schema = hashMustFindVal(schemaHash, sub->fullName);
+		writeOneSchema(jw, sub->name, schema);
+		}
+	    }
 	}
     jsonWriteObjectEnd(jw);
     }
 if (needPattern)
     {
-    struct tagSchema *schema;
     jsonWriteObjectStart(jw, "patternProperties");
-    for (schema = schemaList; schema != NULL; schema = schema->next)
+    for (sub = obj->children; sub != NULL; sub = sub->next)
 	{
-	if (anyWild(schema->name))
+	if (sub->children == NULL)
 	    {
-	    struct dyString *dy = dyStringNew(0);
-	    addRegExWildEquivalent(dy, schema->name);
-	    writeOneSchema(jw, dy->string, schema);
-	    dyStringFree(&dy);
+	    if (anyWild(sub->name))
+		{
+		struct tagSchema *schema = hashMustFindVal(schemaHash, sub->fullName);
+		struct dyString *dy = dyStringNew(0);
+		addRegExWildEquivalent(dy, sub->name);
+		writeOneSchema(jw, dy->string, schema);
+		dyStringFree(&dy);
+		}
 	    }
 	}
     jsonWriteObjectEnd(jw);
@@ -183,12 +187,16 @@ jsonWriteBoolean(jw, "additionalProperties", FALSE);
 
 /* Figure out if any required fields  */
 boolean needsRequired = FALSE;
-for (schema = schemaList; schema != NULL; schema = schema->next)
+for (sub = obj->children; sub != NULL; sub = sub->next)
     {
-    if (schema->required != 0)
-        {
-	needsRequired = TRUE;
-	break;
+    if (sub->children == NULL)
+	{
+	struct tagSchema *schema = hashMustFindVal(schemaHash, sub->fullName);
+	if (schema->required != 0)
+	    {
+	    needsRequired = TRUE;
+	    break;
+	    }
 	}
     }
 
@@ -196,19 +204,117 @@ for (schema = schemaList; schema != NULL; schema = schema->next)
 if (needsRequired)
     {
     jsonWriteListStart(jw, "required");
-    for (schema = schemaList; schema != NULL; schema = schema->next)
+    for (sub = obj->children; sub != NULL; sub = sub->next)
 	{
-	if (schema->required != 0)
-	    jsonWriteString(jw, NULL, schema->name);
+	if (sub->children == NULL)
+	    {
+	    struct tagSchema *schema = hashMustFindVal(schemaHash, sub->fullName);
+	    if (schema->required != 0)
+		jsonWriteString(jw, NULL, schema->name);
+	    }
 	}
     jsonWriteListEnd(jw);
     }
+}
 
+void rWriteDefinitions(struct jsonWrite *jw, struct ttjSubObj *obj, struct hash *schemaHash)
+/* Recursiveley (bottom first) write out definitions for things with subobjects */
+{
+struct ttjSubObj *sub;
+for (sub = obj->children; sub != NULL; sub = sub->next)
+    {
+    if (sub->children != NULL)
+        {
+	rWriteDefinitions(jw, sub, schemaHash);
+	jsonWriteObjectStart(jw, sub->name);
+	writeHighFields(jw, sub, schemaHash);
+	jsonWriteObjectEnd(jw);
+	}
+    }
+}
+
+void makeObjectSchema(char *fileName, struct ttjSubObj *obj, struct hash *schemaHash)
+/* Make schema for object and all of it's subObject in fileName */
+{
+verbose(1, "making %s\n", fileName);
+struct jsonWrite *jw = jsonWriteNew();
+jw->sep = '\n';
+
+/* Write out start bits */
+jsonWriteObjectStart(jw, NULL);
+/* Write out bits that don't actually depend on our schema */
+jsonWriteString(jw, "$schema", "http://json-schema.org/draft-04/schema#");
+jsonWriteString(jw, "title", obj->name);
+char text[1024];
+safef(text, sizeof(text), "%s schema generate by tagSchemaToJson from %s", obj->name, gInName);
+jsonWriteString(jw, "description", text);
+
+/* See if have any subobjects */
+boolean hasSubobj = FALSE;
+struct ttjSubObj *sub;
+for (sub = obj->children; sub != NULL; sub = sub->next)
+    if (sub->children != NULL)
+        hasSubobj = TRUE;
+
+if (hasSubobj)
+     {
+     jsonWriteObjectStart(jw, "definitions");
+     rWriteDefinitions(jw, obj, schemaHash);
+     jsonWriteObjectEnd(jw);
+     }
+
+/* Write out top level fields */
+writeHighFields(jw, obj, schemaHash);
+
+
+/* Close out schema */
 jsonWriteObjectEnd(jw);
 
-FILE *f = mustOpen(outJson, "w");
+FILE *f = mustOpen(fileName, "w");
 fprintf(f, "%s\n", jw->dy->string);
 carefulClose(&f);
+}
+
+void tagSchemaToJson(char *inTagSchema, char *outDir)
+/* tagSchemaToJson- Convert a tagStormSchema (such as used by tagStormCheck) to a JSON 
+ * schema such as decribed in http://json-schema.org/. */
+{
+gInName = inTagSchema;
+
+/* Load in schema, and to reuse library code make a simple list of all fields */
+struct tagSchema *schema, *schemaList = tagSchemaFromFile(inTagSchema);
+struct hash *schemaHash = hashNew(0);
+struct slName *allFields = NULL;
+for (schema = schemaList; schema != NULL; schema = schema->next)
+    {
+    slNameAddHead(&allFields, schema->name);
+    hashAdd(schemaHash, schema->name, schema);
+    }
+slReverse(&allFields);
+
+/* Do some figuring based on all fields available of what objects to make */
+verbose(1, "Got %d fields in %s\n", slCount(allFields), inTagSchema);
+struct slName *topLevelList = ttjUniqToDotList(allFields, NULL, 0);
+verbose(1, "Got %d top level objects\n", slCount(topLevelList));
+
+/* Make list of objects */
+struct slName *topEl;
+struct ttjSubObj *obj, *objList = NULL;
+for (topEl = topLevelList; topEl != NULL; topEl = topEl->next)
+    {
+    verbose(1, "  %s\n", topEl->name);
+    struct ttjSubObj *obj = ttjMakeSubObj(allFields, topEl->name, topEl->name);
+    slAddHead(&objList, obj);
+    }
+
+/* Write out a separate schema file in outDir for each high level object */
+makeDirsOnPath(outDir);
+for (obj = objList; obj != NULL; obj = obj->next)
+    {
+    char outPath[PATH_LEN];
+    safef(outPath, sizeof(outPath), "%s/%s_schema.json", outDir, obj->name);
+    makeObjectSchema(outPath, obj, schemaHash);
+    }
 }
 
 int main(int argc, char *argv[])
