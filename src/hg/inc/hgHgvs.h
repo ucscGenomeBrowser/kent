e6e8263c1a4a85000be7cc4df0797d75dceba589
Angie Hinrichs <angie@soe.ucsc.edu>
2017-06-30 09:51:18 -0700

    Noncoding introns were not being mapped to the genome correctly, hgvsTranscriptToZeroBasedHalfOpen had a fencepost error for upstream/downstream and mapToDeletion was using q coords instead of t coords for upstream/downstream, doh!  helpful testcase: NR_111987:n.-1 .  fixes #19702 note-1.

--- src/hg/inc/hgHgvs.h
+++ src/hg/inc/hgHgvs.h
@@ -11,9 +11,11 @@
 #include "bed.h"
 
 /* The full nomenclature is extremely complicated, able to encode things such as gene fusions and
- * advanced clinical info (e.g. "=/" for somatic mosaicism, "=//" for chimerism).  I am starting
- * simple with single-base substitutions, which should cover the majority of use cases, and will
- * work up from there.
+ * advanced clinical info (e.g. "=/" for somatic mosaicism, "=//" for chimerism).  UCSC supports
+ * substitutions, insertions, deletions, duplications and inversions.  Conversions are parsed out
+ * of HGVS terms but not detected in genomic variants when generating HGVS terms.
+ * UCSC does not fully support repeated sequences because in practice they seem to be frequently
+ * incorrect and inherently error-prone.
  *
  * At the same time, since the spec has repeatedly changed, we will need to be flexible in our
  * parsing in order to support previously published HGVS (or HGVS-like) terms. */
@@ -22,10 +24,10 @@ enum hgvsSeqType
     // HGVS describes changes relative to several types of sequence: genomic, coding, protein, etc
     {
     hgvstUndefined,  // the usual int default value means we haven't actually checked
-    hgvstCoding,     // "c.": Coding DNA sequence only. Beware: complex coords for intron & UTR.
+    hgvstCoding,     // "c.": Coding cDNA sequence only. Beware: complex coords for intron & UTR.
     hgvstGenomic,    // "g.": Genomic DNA
     hgvstMito,       // "m.": Mitochondrial DNA
-    hgvstNoncoding,  // "n.": non-coding RNA
+    hgvstNoncoding,  // "n.": non-coding cDNA w/complex coords for intron
     hgvstRna,        // "r.": RNA (like DNA but lowercase and 'u' instead of 'T')
     hgvstProtein,    // "p.": Protein
     };
@@ -40,15 +42,15 @@ struct hgvsVariant
                              // with embedded position in protein terms); can be parsed into
                              // syntax tree by hgvsParseNucleotideChange (below)
     int start1;              // 1-based start of the variant in reference seq: can be negative !
-                             // For coding terms with complex intron or UTR coords, this is the
-                             // anchor coord (nearest CDS exon boundary)
+                             // For [cnr]. terms with complex intron coords, this is the
+                             // anchor coord (nearest exon boundary)
     int end;                 // End of the variant in the reference seq; can be negative !
                              // For a range, this is the second anchor coord; else same as start
-    // These two fields apply only to hgvstCoding ("c.") terms and Rna ("r.") for coding RNAs:
     int startOffset;         // offset into intron; 1-based, 0 means N/A; can be negative!
     int endOffset;           // offset into intron; 0 means N/A; can be negative!
                              // For a range, this is the second offset; else same as startOffset
-    enum hgvsSeqType type;   // type of sequence: genomic, coding, protein, etc
+    enum hgvsSeqType type;   // type of sequence: genomic/mito, coding, noncoding, protein
+    // These two fields apply only to hgvstCoding ("c.") terms and Rna ("r.") for coding RNAs:
     boolean startIsUtr3;     // TRUE if start is relative to *end* of coding sequence
     boolean endIsUtr3;       // TRUE if end is relative to *end* of coding sequence
     };
@@ -164,7 +166,7 @@ struct hgvsVariant *hgvsParsePseudoHgvs(char *db, char *term);
 boolean hgvsValidate(char *db, struct hgvsVariant *hgvs, char **retFoundAcc, int *retFoundVersion,
                      char **retDiffRefAllele);
 /* Return TRUE if hgvs coords are within the bounds of the sequence for hgvs->seqAcc.
- * Note: Coding terms may contain coords outside the bounds (upstream, intron, downstream) so
+ * Note: Transcript terms may contain coords outside the bounds (upstream, intron, downstream) so
  * those can't be checked without mapping the term to the genome; this returns TRUE if seq is found.
  * If retFoundAcc is not NULL, set it to our local accession (which may be missing the .version
  * of hgvs->seqAcc) or NULL if we can't find any match.
