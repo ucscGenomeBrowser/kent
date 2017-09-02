7ce01a443f1fc128226f3d50455c55b5fb0b9bce
Jim Kent <kent@soe.ucsc.edu>
2017-09-01 19:57:00 -0700

    Unpacking more arrays.

--- src/hca/v2v3/hcav2v3ModifyContributors/hcav2v3ModifyContributors.c
+++ src/hca/v2v3/hcav2v3ModifyContributors/hcav2v3ModifyContributors.c
@@ -24,14 +24,11 @@ static struct optionSpec options[] = {
    {NULL, 0},
 };
 
-void modifyContributors(struct tagStorm *tags, struct tagStanza *stanza, void *context)
-/* Modify contributors tag, unwinding an array. */
+void unpackArray(struct tagStorm *tags, struct tagStanza *stanza, char *inTag, char *outPrefix, char *outSuffix)
 {
-char *targetTag = "project.contributor";
-char *val = tagFindLocalVal(stanza, targetTag);
+char *val = tagFindLocalVal(stanza, inTag);
 if (val != NULL)
     {
-    uglyf("Got you %s\n", val);
     struct dyString *scratchIn = dyStringNew(0);
     struct dyString *scratchOut = dyStringNew(0);
     char *oneVal;
@@ -40,21 +37,34 @@ if (val != NULL)
     while ((oneVal = csvParseNext(&pos, scratchIn)) != NULL)
         {
 	char name[256];
-	safef(name, sizeof(name), "project.contributors.%d.name", ++ix);
+	safef(name, sizeof(name), "%s.%d.%s", outPrefix, ++ix, outSuffix);
 	char *escapedVal = csvEscapeToDyString(scratchOut, oneVal);
 	tagStanzaAppend(tags, stanza, name, escapedVal);
 	}
     dyStringFree(&scratchIn);
     dyStringFree(&scratchOut);
-    tagStanzaDeleteTag(stanza, targetTag);
+    tagStanzaDeleteTag(stanza, inTag);
     }
 }
+
+
+void modifyContributors(struct tagStorm *tags, struct tagStanza *stanza, void *context)
+/* Modify contributors tag, unwinding an array. */
+{
+unpackArray(tags, stanza, "project.contributor", "project.contributors", "name");
+unpackArray(tags, stanza, "project.experimental_design.1.text", "project.experimental_design", "text");
+unpackArray(tags, stanza, "sample.supplementary_protocols.1.type", "sample.supplementary_protocols", "type");
+unpackArray(tags, stanza, "sample.supplementary_protocols.1.description", "sample.supplementary_protocols", "description");
+}
+
+char *tagOrder[] = { "project.title", "project.core.id",};
 
 void hcav2v3ModifyContributors(char *inName, char *outName)
 /* hcav2v3ModifyContributors - Change format of contributers array in curated.tags for HCA v2v3 transition.. */
 {
 struct tagStorm *tags = tagStormFromFile(inName);
 tagStormTraverse(tags, tags->forest, NULL, modifyContributors);
+tagStormOrderSort(tags, tagOrder, ArraySize(tagOrder) );
 tagStormWrite(tags, outName, 0);
 }
 
