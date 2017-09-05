4929a9a5c8d7170a976fb2189f6dbee664be7790
Jim Kent <kent@soe.ucsc.edu>
2017-09-05 16:04:39 -0700

    Testing some of the tag storm array processing stuff for tagStormCheck

--- src/hg/oneShot/freen/freen.c
+++ src/hg/oneShot/freen/freen.c
@@ -27,6 +27,52 @@ errAbort("freen - test some hairbrained thing.\n"
          "usage:  freen input\n");
 }
 
+static boolean matchAndExtractIndexes(char *dottedName, struct slName *objArrayPieces,
+    struct dyString *scratch, char **retIndexes, boolean *retMatchEnd)
+/* Dotted name is something like this.that.12.more.2.ok
+ * The objArrayPieces is something like "this.that." ".more." ".notOk"
+ * Crucially it holds at least two elements, some of which may be ""
+ * This function will return TRUE if all but maybe the last of the objArrayPieces is
+ * found in the dottedName.  If this is the case it will put something like .12.2 in
+ * scratch and *retIndexes.  If the last one matches it will set *retMatchEnd.
+ * Sort of a complex routine but it plays a key piece in checking required array
+ * elements */
+{
+dyStringClear(scratch);
+struct slName *piece = objArrayPieces;
+char *pos = dottedName;
+for (;;)
+    {
+    /* Check to see if we match next piece, and return FALSE if not */
+    char *pieceString = piece->name;
+    int pieceLen = strlen(pieceString);
+    if (pieceLen != 0 && memcmp(pieceString, pos, pieceLen) != 0)
+        return FALSE;
+    pos += pieceLen;
+
+    /* Put the number into scratch with a leading dot separator. */
+    int digits = tagSchemaDigitsUpToDot(pos);
+    assert(digits >= 0);
+    dyStringAppendC(scratch, '.');
+    dyStringAppendN(scratch, pos, digits);
+    pos += digits;
+
+    /* Go to next piece,saving last piece for outside of the loop. */
+    piece = piece->next;
+    if (piece->next == NULL)
+        break;
+    }
+
+/* One more special case, where last piece needs to agree on emptiness at least in
+ * terms of matching */
+if (isEmpty(piece->name) != isEmpty(pos))
+    return FALSE;
+
+/* Otherwise both have something.  We return true/false depending on whether it matches */
+*retMatchEnd = (strcmp(piece->name, pos) == 0);
+*retIndexes = scratch->string;
+return TRUE;
+}
 
 static struct slName *makeObjArrayPieces(char *name)
 /* Given something like this.[].that return a list of "this." ".that".  That is
@@ -67,13 +113,37 @@ slReverse(&list);
 return list;
 }
 
+void checkIt(char *bracketed, char *numbered)
+/* Test something */
+{
+struct dyString *scratch = dyStringNew(0);
+
+printf("%s    %s :\n", bracketed, numbered);
+struct slName *el, *list = makeObjArrayPieces(bracketed);
+for (el = list; el != NULL; el = el->next)
+    printf("\t'%s'\n", el->name);
+char *index;
+boolean fullMatch = FALSE;
+if (matchAndExtractIndexes(numbered, list, scratch, &index, &fullMatch))
+    {
+    printf("index %s, fullMatch %d\n", index, fullMatch);
+    }
+else
+    printf("No match\n");
+printf("--------------\n");
+}
+
 void freen(char *input)
 /* Test something */
 {
-struct slName *el, *list = makeObjArrayPieces(input);
-printf("%s\n", input);
-for (el = list; el != NULL; el = el->next)
-    printf("\t'%s'\n", el->name);
+checkIt("this.[].that.[].more.so", "this.1.that.2.more.so");
+checkIt("this.[].that.[].more.so", "this.3.that.1.more.notSo");
+checkIt("this.[].that.[].more.so", "other.3.that.1.more.notSo");
+checkIt("[].name", "3.name");
+checkIt("[].name", "3.fancy");
+checkIt("name.[]", "3");
+checkIt("[].name", "3");
+// checkIt("[]", "abba");
 }
 
 int main(int argc, char *argv[])
