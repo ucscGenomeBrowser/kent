433f3c60dc573cc99a22886b57a264e9ce46e7d0
Hiram Clawson <hiram@soe.ucsc.edu>
2017-09-22 15:11:06 -0700

    Added manLeu1/Drill; refs #20190

--- src/hg/makeDb/genbank/src/lib/gbGenome.c
+++ src/hg/makeDb/genbank/src/lib/gbGenome.c
@@ -156,6 +156,7 @@ static char *galVarNames[] = {"Galeopterus variegatus", NULL};
 static char *rouAegNames[] = {"Rousettus aegyptiacus", NULL};
 static char *orcOrcNames[] = {"Orcinus orca", NULL};
 static char *colAngNames[] = {"Colobus angolensis", "Colobus angolensis palliatus", "Colobus guereza", "Colobus guereza kikuyuensis", NULL};
+static char *manLeuNames[] = {"Mandrillus leucophaeus", NULL};
 static char *hbvNames[] = {"Hepatitis B virus", "Hepatitis B virus subtype adw", NULL};
 static char *hpvNames[] = {"Human papillomavirus", "Human papillomavirus type 16", "Human papillomavirus type 18", "Human papillomavirus type 197", "Human papillomavirus type 38", "Human papillomavirus type 45", "Human papillomavirus type 58", "Human papillomavirus type 68", "Human papillomavirus type 6b", NULL};
 static char *staAurNames[] = {"Staphylococcus", "Staphylococcus aureus", "Staphylococcus aureus subsp. aureus NCTC 8325", "Staphylococcus aureus subsp. aureus RN4220", NULL};
@@ -289,6 +290,7 @@ static struct dbToSpecies dbToSpeciesMap[] = {
     {"rouAeg", rouAegNames},
     {"orcOrc", orcOrcNames},
     {"colAng", colAngNames},
+    {"manLeu", manLeuNames},
     {"hbv", hbvNames},
     {"hpv", hpvNames},
     {"staAur", staAurNames},
