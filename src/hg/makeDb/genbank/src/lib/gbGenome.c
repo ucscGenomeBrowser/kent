15379a1b22b23bc5101be9e4f2c93ec24b149b5b
Hiram Clawson <hiram@soe.ucsc.edu>
2017-09-28 13:46:53 -0700

    Added proCoq1/Coquerel's sifaka; refs #20235

--- src/hg/makeDb/genbank/src/lib/gbGenome.c
+++ src/hg/makeDb/genbank/src/lib/gbGenome.c
@@ -160,6 +160,7 @@ static char *manLeuNames[] = {"Mandrillus leucophaeus", NULL};
 static char *cerAtyNames[] = {"Cercocebus atys", NULL};
 static char *neoSchNames[] = {"Neomonachus schauinslandi", NULL};
 static char *aotNanNames[] = {"Aotus nancymaae", NULL};
+static char *proCoqNames[] = {"Propithecus coquereli", NULL};
 static char *hbvNames[] = {"Hepatitis B virus", "Hepatitis B virus subtype adw", NULL};
 static char *hpvNames[] = {"Human papillomavirus", "Human papillomavirus type 16", "Human papillomavirus type 18", "Human papillomavirus type 197", "Human papillomavirus type 38", "Human papillomavirus type 45", "Human papillomavirus type 58", "Human papillomavirus type 68", "Human papillomavirus type 6b", NULL};
 static char *staAurNames[] = {"Staphylococcus", "Staphylococcus aureus", "Staphylococcus aureus subsp. aureus NCTC 8325", "Staphylococcus aureus subsp. aureus RN4220", NULL};
@@ -297,6 +298,7 @@ static struct dbToSpecies dbToSpeciesMap[] = {
     {"cerAty", cerAtyNames},
     {"neoSch", neoSchNames},
     {"aotNan", aotNanNames},
+    {"proCoq", proCoqNames},
     {"hbv", hbvNames},
     {"hpv", hpvNames},
     {"staAur", staAurNames},
