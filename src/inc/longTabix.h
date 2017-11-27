de8ca293b4eda9ba40ff55ffb3bf94827a65b561
Kate Rosenbloom <kate@soe.ucsc.edu>
2017-11-27 15:42:22 -0800

    Cleanup to ease some fixes to longTabix.

---
+++ src/inc/longTabix.h
@@ -0,0 +1,10 @@
+/* longTabix -- long range pairwise interaction format, from Wash U
+ *    Documented here:  http://wiki.wubrowse.org/Long-range
+ */
+
+/* Copyright (C) 2017 The Regents of the University of California 
+ * See README in this or parent directory for licensing information. */
+
+struct asObject *longTabixAsObj();
+// Return asObject describing fields of longTabix file
+
