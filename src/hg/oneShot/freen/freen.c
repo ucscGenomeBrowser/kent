c7686e10cf2c33c4008a2e1c1528f53fc9ff6c98
Jim Kent <kent@soe.ucsc.edu>
2017-11-29 15:35:12 -0800

    Freen tests facetField.c

--- src/hg/oneShot/freen/freen.c
+++ src/hg/oneShot/freen/freen.c
@@ -15,6 +15,7 @@
 #include "jsonWrite.h"
 #include "tagSchema.h"
 #include "csv.h"
+#include "facetField.h"
 
 /* Command line validation table. */
 static struct optionSpec options[] = {
@@ -27,123 +28,33 @@ errAbort("freen - test some hairbrained thing.\n"
          "usage:  freen input\n");
 }
 
-static boolean matchAndExtractIndexes(char *dottedName, struct slName *objArrayPieces,
-    struct dyString *scratch, char **retIndexes, boolean *retMatchEnd)
-/* Dotted name is something like this.that.12.more.2.ok
- * The objArrayPieces is something like "this.that." ".more." ".notOk"
- * Crucially it holds at least two elements, some of which may be ""
- * This function will return TRUE if all but maybe the last of the objArrayPieces is
- * found in the dottedName.  If this is the case it will put something like .12.2 in
- * scratch and *retIndexes.  If the last one matches it will set *retMatchEnd.
- * Sort of a complex routine but it plays a key piece in checking required array
- * elements */
-{
-dyStringClear(scratch);
-struct slName *piece = objArrayPieces;
-char *pos = dottedName;
-for (;;)
-    {
-    /* Check to see if we match next piece, and return FALSE if not */
-    char *pieceString = piece->name;
-    int pieceLen = strlen(pieceString);
-    if (pieceLen != 0 && memcmp(pieceString, pos, pieceLen) != 0)
-        return FALSE;
-    pos += pieceLen;
-
-    /* Put the number into scratch with a leading dot separator. */
-    int digits = tagSchemaDigitsUpToDot(pos);
-    assert(digits >= 0);
-    dyStringAppendC(scratch, '.');
-    dyStringAppendN(scratch, pos, digits);
-    pos += digits;
-
-    /* Go to next piece,saving last piece for outside of the loop. */
-    piece = piece->next;
-    if (piece->next == NULL)
-        break;
-    }
-
-/* One more special case, where last piece needs to agree on emptiness at least in
- * terms of matching */
-if (isEmpty(piece->name) != isEmpty(pos))
-    return FALSE;
-
-/* Otherwise both have something.  We return true/false depending on whether it matches */
-*retMatchEnd = (strcmp(piece->name, pos) == 0);
-*retIndexes = scratch->string;
-return TRUE;
-}
-
-static struct slName *makeObjArrayPieces(char *name)
-/* Given something like this.[].that return a list of "this." ".that".  That is
- * return a list of all strings before between and after the []'s Other
- * examples:
- *        [] returns "" ""
- *        this.[] return "this." ""
- *        [].that returns "" ".that"
- *        this.[].that.and.[].more returns "this." ".that.and." ".more" */
-{
-struct slName *list = NULL;	// Result list goes here
-char *pos = name;
-
-/* Handle special case of leading "[]" */
-if (startsWith("[]", name))
-     {
-     slNameAddHead(&list, "");
-     pos += 2;
-     }
-
-char *aStart;
-for (;;)
-    {
-    aStart = strchr(pos, '[');
-    if (aStart == NULL)
-        {
-	slNameAddHead(&list, pos);
-	break;
-	}
-    else
-        {
-	struct slName *el = slNameNewN(pos, aStart-pos);
-	slAddHead(&list, el);
-	pos = aStart + 2;
-	}
-    }
-slReverse(&list);
-return list;
-}
-
-void checkIt(char *bracketed, char *numbered)
-/* Test something */
-{
-struct dyString *scratch = dyStringNew(0);
-
-printf("%s    %s :\n", bracketed, numbered);
-struct slName *el, *list = makeObjArrayPieces(bracketed);
-for (el = list; el != NULL; el = el->next)
-    printf("\t'%s'\n", el->name);
-char *index;
-boolean fullMatch = FALSE;
-if (matchAndExtractIndexes(numbered, list, scratch, &index, &fullMatch))
-    {
-    printf("index %s, fullMatch %d\n", index, fullMatch);
-    }
-else
-    printf("No match\n");
-printf("--------------\n");
-}
 
 void freen(char *input)
 /* Test something */
 {
-checkIt("this.[].that.[].more.so", "this.1.that.2.more.so");
-checkIt("this.[].that.[].more.so", "this.3.that.1.more.notSo");
-checkIt("this.[].that.[].more.so", "other.3.that.1.more.notSo");
-checkIt("[].name", "3.name");
-checkIt("[].name", "3.fancy");
-checkIt("name.[]", "3");
-checkIt("[].name", "3");
-// checkIt("[]", "abba");
+char *db = "cdw";
+char *table = "slimFoo";
+char *fields[] = 
+    {"data_set_id", "lab", "assay", "format", "read_size",
+    "sample_label", "species"};
+int fieldCount = ArraySize(fields);
+/* Print out high level tags table */
+
+struct sqlConnection *conn = sqlConnect(db);
+struct facetField *ffList = facetFieldsFromSqlTable(conn, table, fields, fieldCount, "N/A", NULL);
+uglyf("Got %d tags\n", slCount(ffList) );
+sqlDisconnect(&conn);
+
+struct facetField *ff;
+for (ff = ffList; ff != NULL; ff = ff->next)
+    {
+    printf("%s:\n", ff->fieldName);
+    struct facetVal *facetVal;
+    for (facetVal = ff->valList; facetVal != NULL; facetVal = facetVal->next)
+	{
+	printf("\t%s (%d)\n", facetVal->val, facetVal->useCount);
+	}
+    }
 }
 
 int main(int argc, char *argv[])
