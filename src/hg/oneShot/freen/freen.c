cf0b1e4a2737a97cd5d7d7fcf0ca60a871f5b94b
Jim Kent <kent@soe.ucsc.edu>
2017-12-11 17:44:23 -0800

    Working out some functions needed for substituting arrays.

--- src/hg/oneShot/freen/freen.c
+++ src/hg/oneShot/freen/freen.c
@@ -14,8 +14,6 @@
 #include "obscure.h"
 #include "jsonWrite.h"
 #include "tagSchema.h"
-#include "csv.h"
-#include "facetField.h"
 
 /* Command line validation table. */
 static struct optionSpec options[] = {
@@ -28,33 +26,133 @@ errAbort("freen - test some hairbrained thing.\n"
          "usage:  freen input\n");
 }
 
+static int nonDotSize(char *s)
+/* Return number of chars up to next dot or end of string */
+{
+char c;
+int size = 0;
+for (;;)
+    {
+    c = *s++;
+    if (c == '.' || c == 0)
+       return size;
+    ++size;
+    }
+}
+
+int tagSchemaParseIndexes(char *input, int indexes[], int maxIndexCount)
+/* This will parse out something that looks like:
+ *     this.array.2.subfield.subarray.3.name
+ * into
+ *     2,3   
+ * Where input is what we parse,   indexes is an array maxIndexCount long
+ * where we put the numbers. */
+{
+char dot = '.';
+char *s = input;
+int indexCount = 0;
+int maxMinusOne = maxIndexCount - 1;
+for (;;)
+    {
+    /* Check for end of string */
+    char firstChar = *s;
+    if (firstChar == 0)
+        break;
+
+    /* If leading char is a dot and if so skip it. */
+    boolean startsWithDot = (firstChar == dot);
+    if (startsWithDot)
+       ++s;
+
+    int numSize = tagSchemaDigitsUpToDot(s);
+    if (numSize > 0)
+        {
+	if (indexCount > maxMinusOne)
+	    errAbort("Too many array indexes in %s, maxIndexCount is %d",
+		input, maxIndexCount);
+	indexes[indexCount] = atoi(s);
+	indexCount += 1;
+	s += numSize;
+	}
+    else
+        {
+	int partSize = nonDotSize(s);
+	s += partSize;
+	}
+    }
+return indexCount;
+}
+
+char *tagSchemaInsertIndexes(char *bracketed, int indexes[], int indexCount,
+    struct dyString *scratch)
+/* Convert something that looks like:
+ *     this.array.[].subfield.subarray.[].name and 2,3
+ * into
+ *     this.array.2.subfield.subarray.3.name
+ * The scratch string holds the return value. */
+{
+char *pos = bracketed;
+int indexPos = 0;
+dyStringClear(scratch);
+
+/* Handle special case of leading "[]" */
+if (startsWith("[]", pos))
+     {
+     dyStringPrintf(scratch, "%d", indexes[indexPos]);
+     ++indexPos;
+     pos += 2;
+     }
+
+char *aStart;
+for (;;)
+    {
+    aStart = strchr(pos, '[');
+    if (aStart == NULL)
+        {
+	dyStringAppend(scratch, pos);
+	break;
+	}
+    else
+        {
+	if (indexPos >= indexCount)
+	    errAbort("Expecting %d '[]' in %s, got more.", indexCount, bracketed);
+	dyStringAppendN(scratch, pos, aStart-pos);
+	dyStringPrintf(scratch, "%d", indexes[indexPos]);
+	++indexPos;
+	pos = aStart + 2;
+	}
+    }
+return scratch->string;
+}
 
 void freen(char *input)
 /* Test something */
 {
-char *db = "cdw";
-char *table = "slimFoo";
-char *fields[] = 
-    {"data_set_id", "lab", "assay", "format", "read_size",
-    "sample_label", "species"};
-int fieldCount = ArraySize(fields);
-/* Print out high level tags table */
+struct dyString *withBrackets = dyStringNew(0);
+tagSchemaFigureArrayName(input, withBrackets);
+int maxIndexCount = 10;
+int indexes[maxIndexCount];
+int indexCount = tagSchemaParseIndexes(input, indexes, maxIndexCount);
+{
+uglyf("Got index count of %d, values:", indexCount);
+int i;
+for (i=0; i<indexCount; ++i)
+    uglyf(" %d", indexes[i]);
+uglyf("\n");
+}
 
-struct sqlConnection *conn = sqlConnect(db);
-struct facetField *ffList = facetFieldsFromSqlTable(conn, table, fields, fieldCount, "N/A", NULL);
-uglyf("Got %d tags\n", slCount(ffList) );
-sqlDisconnect(&conn);
-
-struct facetField *ff;
-for (ff = ffList; ff != NULL; ff = ff->next)
+if (indexCount > 0)
     {
-    printf("%s:\n", ff->fieldName);
-    struct facetVal *facetVal;
-    for (facetVal = ff->valList; facetVal != NULL; facetVal = facetVal->next)
-	{
-	printf("\t%s (%d)\n", facetVal->val, facetVal->useCount);
-	}
+    int i;
+    for (i=0; i<indexCount; ++i)
+	indexes[i] += 1;
+    struct dyString *withIncs = dyStringNew(0);
+    char *incString = tagSchemaInsertIndexes(withBrackets->string, 
+	indexes, indexCount, withIncs);
+    printf("%s -> %s\n", input, incString);
     }
+else
+    printf("%s has no array index subfields\n", input);
 }
 
 int main(int argc, char *argv[])
