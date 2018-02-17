a1231722750f05b919cd3fcf244a5431e26b2b7c
Brian Raney <braney@soe.ucsc.edu>
2018-02-17 08:14:05 -0800

    add hgCollection menus to top bar and hgTracks right click

--- src/hg/js/hgTracks.js
+++ src/hg/js/hgTracks.js
@@ -2732,7 +2732,7 @@ var rightClick = {
 
             $.ajax({
                 type: "PUT",
-                async: false,
+                async: true,
                 url: "../cgi-bin/hgCollection",
                 data: "cmd=addTrack&track=" + id + "&collection=" + collectionName + "&hgsid=" + getHgsid(),
                 trueSuccess: mySuccess,
@@ -3273,12 +3273,32 @@ var rightClick = {
                 {
                 // add delete from composite
                 }
+            else if ((!rec.hasChildren && !rec.type.startsWith("wigMaf")) &&
+                (rec.type.startsWith("bigWig") || rec.type.startsWith("multiWig") || rec.type.startsWith("wig") || rec.type.startsWith("bedGraph"))) {
+                o = {};
+                o[" Make a New Collection with \"" + rec.shortLabel + "\""] = {
+                    onclick: rightClick.makeHitCallback("newCollection")
+                };  
+                menu.push(o);
+
+                if (hgTracks.collections) {
+                    var ii;
+                    for(ii=0; ii < hgTracks.collections.length; ii++) {
+                        o = {};
+                        o[" Add to \"" + hgTracks.collections[ii].shortLabel + "\""] = {
+                            onclick: rightClick.makeHitCallback("addCollection")
+                        };  
+                        menu.push(o);
+                    }
+                }
+                menu.push($.contextMenu.separator);
+            }
 
             // add sort options if this is a custom composite
             if (rec.isCustomComposite) {
 
                 o = {};
-                o[" Sort by Expression "] = {
+                o[" Sort by Magnitude "] = {
                     onclick: function(menuItemClicked, menuObject) {
                         rightClick.hit(menuItemClicked, menuObject, "sortExp");
                         return true; }
