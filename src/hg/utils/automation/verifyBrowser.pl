c810fea4c95e63f3e88c9d42c2c6336445108f42
Hiram Clawson <hiram@soe.ucsc.edu>
2018-03-16 10:56:25 -0700

    tool to verify tables present in a completed browser build no redmine

--- src/hg/utils/automation/verifyBrowser.pl
+++ src/hg/utils/automation/verifyBrowser.pl
@@ -12,9 +12,19 @@ if ($argc != 1) {
   exit 255;
 }
 
-my %tableCheckList = ( 'all_est' => 1,
-'all_mrna' => 1,
-'augustusGene' => 1,
+my %optionalCheckList = ( 'extNcbiRefSeq' => 1,
+'ncbiRefSeq' => 1,
+'ncbiRefSeqCds' => 1,
+'ncbiRefSeqCurated' => 1,
+'ncbiRefSeqLink' => 1,
+'ncbiRefSeqOther' => 1,
+'ncbiRefSeqPepTable' => 1,
+'ncbiRefSeqPredicted' => 1,
+'ncbiRefSeqPsl' => 1,
+'seqNcbiRefSeq' => 1
+);
+
+my %tableCheckList = ( 'augustusGene' => 1,
 'chainHg38' => 1,
 'chainHg38Link' => 1,
 'chainMm10' => 1,
@@ -32,10 +42,8 @@ my %tableCheckList = ( 'all_est' => 1,
 'cpgIslandExt' => 1,
 'cpgIslandExtUnmasked' => 1,
 'cytoBandIdeo' => 1,
-'estOrientInfo' => 1,
 'gap' => 1,
 'gapOverlap' => 1,
-'gbStatus' => 1,
 'gc5BaseBw' => 1,
 'genscan' => 1,
 'genscanSubopt' => 1,
@@ -43,9 +51,7 @@ my %tableCheckList = ( 'all_est' => 1,
 'grp' => 1,
 'hgFindSpec' => 1,
 'history' => 1,
-'intronEst' => 1,
 'microsat' => 1,
-'mrnaOrientInfo' => 1,
 'nestedRepeats' => 1,
 'netHg38' => 1,
 'netMm10' => 1,
@@ -60,16 +66,19 @@ my %tableCheckList = ( 'all_est' => 1,
 'trackDb' => 1,
 'ucscToINSDC' => 1,
 'ucscToRefSeq' => 1,
-'windowmaskerSdust' => 1,
-'xenoRefFlat' => 1,
-'xenoRefGene' => 1,
-'xenoRefSeqAli' => 1,
-'gbLoaded' => 1,         ## genbank tables following
-'all_mrna' => 1,   ## from /cluster/data/genbank/etc/gbPerAssemblyTables.txt
+'windowmaskerSdust' => 1
+);
+
+## from /cluster/data/genbank/etc/gbPerAssemblyTables.txt
+## genbank tables
+## some of these should be present, do not need to be all
+my %gbCheckList = ( 'gbLoaded' => 1,
+'all_mrna' => 1,
 'xenoMrna' => 1,
 'mrnaOrientInfo' => 1,
 'all_est' => 1,
 'intronEst' => 1,
+'gbStatus' => 1,
 'xenoEst' => 1,
 'estOrientInfo' => 1,
 'refGene' => 1,
@@ -94,36 +103,53 @@ my %tableCheckList = ( 'all_est' => 1,
 'ccdsKgMap' => 1
 );
 
+sub checkTableExists($$) {
+  my ($db, $table) = @_;
+  my $lineCount = `hgsql -N -e 'desc $table;' $db 2> /dev/null | wc -l`;
+  chomp $lineCount;
+  if ($lineCount > 3) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
+
 my $db = shift;
+
+my $dbDbNames = `hgsql -N -e 'select organism,scientificName from dbDb where name="$db";' hgcentraltest`;
+chomp $dbDbNames;
+$dbDbNames =~ s/\t/, /;
 
 my $tableCount = 0;
 my %tableList;	# key is table name, value is 1
 open (FH, "hgsql -N -e 'show tables;' $db|") or die "can not hgsql -N -e 'show tables $db'";
 while (my $table = <FH>) {
    chomp $table;
-   $tableList{$table} = 1;
-   ++$tableCount;
+   if ($table !~ m/trackDb_|hgFindSpec_/ ) {
+     $tableList{$table} = 1;
+     ++$tableCount;
+   }
 }
 close (FH);
 
-printf STDERR "# %d tables in database %s\n", $tableCount, $db;
+printf STDERR "# %d tables in database %s - %s\n", $tableCount, $db, $dbDbNames;
 
 my %extraTables;
 my $extraTableCount = 0;
 my $tablesFound = 0;
+my $optionalCount = 0;
 
 foreach my $table (sort keys %tableList) {
-  if (defined($tableCheckList{$table})) {
+  if (defined($tableCheckList{$table}) || defined($gbCheckList{$table}) || defined($optionalCheckList{$table}) ) {
     ++$tablesFound;
+    $optionalCount += 1 if (defined($optionalCheckList{$table}));
   } else {
-    if (! ($table =~ m/trackDb_/ || $table =~ m/hgFindSpec_/) ) {
-      $extraTables{$table} = 1;
-      ++$extraTableCount;
-    }
+    $extraTables{$table} = 1;
+    ++$extraTableCount;
   }
 }
 
-printf STDERR "# verified %d tables, %d extra tables\n", $tablesFound, $extraTableCount;
+printf STDERR "# verified %d tables, %d extra tables, %d optional tables\n", $tablesFound, $extraTableCount, $optionalCount;
 
 my $shownTables = 0;
 foreach my $table (sort keys %extraTables) {
@@ -137,6 +163,17 @@ foreach my $table (sort keys %extraTables) {
   } else {
     printf STDERR "# %d\t%s\n", $shownTables, $table;
   }
+}
+
+my $gbTableCount = 0;
+foreach my $table (sort keys %tableList) {
+  $gbTableCount += 1 if (defined($gbCheckList{$table}));
+}
+
+if ($gbTableCount < 1) {
+  printf STDERR "# ERROR: no genbank tables found\n";
+} else {
+  printf STDERR "# %d genbank tables found\n", $gbTableCount;
 }
 
 my %missingTables;
@@ -168,3 +205,35 @@ foreach my $table (sort keys %missingTables) {
   ++$missedOut;
   printf STDERR "# %d\t%s\n", $missedOut, $table;
 }
+
+my @chainTypes = ("", "RBest", "Syn");
+my @otherDbs = ("hg38", "mm10");
+my $Db = ucfirst($db);
+for (my $i = 0; $i < scalar(@chainTypes); ++$i) {
+   my $chainTable = "chain" . $chainTypes[$i] .  $Db;
+   my $chainLinkTable = "chain" . $chainTypes[$i] .  $Db . "Link";
+   my $netTable = "net" . $chainTypes[$i] . $Db;
+   for (my $j = 0; $j < scalar(@otherDbs); ++$j) {
+      # mm10 Syntenics do not exist (yet)
+      next if ($otherDbs[$j] eq "mm10" && $chainTypes[$i] eq "Syn");
+      printf STDERR "# missing $otherDbs[$j].$chainTable\n" if (! checkTableExists($otherDbs[$j], $chainTable));
+      printf STDERR "# missing $otherDbs[$j].$chainLinkTable\n" if (! checkTableExists($otherDbs[$j], $chainLinkTable));
+      printf STDERR "# missing $otherDbs[$j].$netTable\n" if (! checkTableExists($otherDbs[$j], $netTable));
+   }
+}
+
+__END__
+printf STDERR "# missing hg38.$chainTable\n" if (! checkTableExists("hg38", $chainTable));
+printf STDERR "# missing hg38.$chainLinkTable\n" if (! checkTableExists("hg38", $chainLinkTable));
+printf STDERR "# missing hg38.$netTable\n" if (! checkTableExists("hg38", $netTable));
+printf STDERR "# missing mm10.$chainTable\n" if (! checkTableExists("mm10", $chainTable));
+printf STDERR "# missing mm10.$chainLinkTable\n" if (! checkTableExists("mm10", $chainLinkTable));
+printf STDERR "# missing mm10.$netTable\n" if (! checkTableExists("mm10", $netTable));
+
+chainRBestThaSir1
+chainRBestThaSir1Link
+chainSynThaSir1
+chainSynThaSir1Link
+netRBestThaSir1
+netSynThaSir1
+
