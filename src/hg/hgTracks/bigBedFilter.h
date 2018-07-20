6791876ba30394c3aad18702708feb4bfb6ec761
Brian Raney <braney@soe.ucsc.edu>
2018-07-19 17:04:44 -0700

    add bigBed numeric and filterBy filters

---
+++ src/hg/hgTracks/bigBedFilter.h
@@ -0,0 +1,23 @@
+#ifndef BIGBEDFILTER_H
+#define BIGBEDFILTER_H
+
+struct bigBedFilter
+/* Filter on a field in a bigBed file. */
+{
+struct bigBedFilter *next;
+int fieldNum;   // the field number
+enum {COMPARE_LESS, COMPARE_MORE, COMPARE_BETWEEN, COMPARE_HASH} comparisonType;  // the type of the comparison
+double value1, value2;
+struct hash *valueHash;
+};
+
+struct bigBedFilter *bigBedMakeFilter(struct cart *cart, struct bbiFile *bbi, struct trackDb *tdb, char *filter, char *defaultLimits,  char *field);
+/* Add a bigBed filter using a trackDb filterBy statement. */
+
+boolean bigBedFilterInterval(char **bedRow, struct bigBedFilter *filters);
+/* Go through a row and filter based on filters.  Return TRUE if all filters are passed. */
+
+struct bigBedFilter *bigBedBuildFilters(struct cart *cart, struct bbiFile *bbi, struct trackDb *tdb) ;
+/* Build all the numeric and filterBy filters for a bigBed */
+
+#endif /* BIGBEDFILTER_H */
