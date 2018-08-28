8d89f83e0a13fbf7a021d5156df5218ec08356d4
Kate Rosenbloom <kate@soe.ucsc.edu>
2018-08-28 16:30:50 -0700

    Add UI option to suppress dashed lines in reversed direction.  JK request. refs #21917

--- src/hg/inc/interactUi.h
+++ src/hg/inc/interactUi.h
@@ -12,14 +12,19 @@
 #define INTERACT_DEFHEIGHT "200"
 #define INTERACT_MINSCORE "minScore"
 
+/* trackDb settings:
+      interactDirectional on          use dashes for reverse direction
+      interactDirectional offsetSource  use dashes for reverse direction, and offset source
+      interactDirectional offsetTarget  use dashes for reverse direction, and offset target
+*/
 #define INTERACT_DIRECTIONAL    "interactDirectional"
 #define INTERACT_OFFSET_SOURCE  "offsetSource"
 #define INTERACT_OFFSET_TARGET  "offsetTarget"
 
-// trackDb settings:
-//      interactDirectional on          use dashes for reverse direction
-//      interactDirectional offsetSource  use dashes for reverse direction, and offset source */
-//      interactDirectional offsetTarget  use dashes for reverse direction, and offset target */
+/* Cart variables */
+
+#define INTERACT_DIRECTION_DASHES "dashes"
+#define INTERACT_DIRECTION_DASHES_DEFAULT TRUE
 
 #define INTERACT_DRAW           "draw"
 #define INTERACT_DRAW_LINE      "line"
@@ -27,11 +32,12 @@
 #define INTERACT_DRAW_CURVE     "curve"
 #define INTERACT_DRAW_DEFAULT   INTERACT_DRAW_CURVE
 
-#define INTERACT_ENDS_VISIBLE "endsVisible"
+#define INTERACT_ENDS_VISIBLE     "endsVisible"
 #define INTERACT_ENDS_VISIBLE_TWO "two"
 #define INTERACT_ENDS_VISIBLE_ONE "one"
 #define INTERACT_ENDS_VISIBLE_ANY "any"
 #define INTERACT_ENDS_VISIBLE_DEFAULT INTERACT_ENDS_VISIBLE_ANY
+
 
 void interactCfgUi(char *database, struct cart *cart, struct trackDb *tdb, char *track,
                         char *title, boolean boxed);
