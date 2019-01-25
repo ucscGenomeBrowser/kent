73c3b813ef766a41c1e44e4f2e17c4757ac155eb
Hiram Clawson <hiram@soe.ucsc.edu>
2019-01-25 09:51:26 -0800

    public hubs on drop down menu and other hub entry box plus time out to exit on long running queries refs #18869

--- src/hg/hubApi/hubApi.c
+++ src/hg/hubApi/hubApi.c
@@ -19,6 +19,7 @@
 #include "bigWig.h"
 #include "hubConnect.h"
 #include "obscure.h"
+#include "errCatch.h"
 
 /* Global Variables */
 struct cart *cart;             /* CGI and other variables */
@@ -27,6 +28,23 @@ static struct hash *trackCounter = NULL;
 static long totalTracks = 0;
 static boolean measureTiming = FALSE;	/* set by CGI parameters */
 static boolean allTrackSettings = FALSE;	/* checkbox setting */
+static struct slName *publicHubShortLabels = NULL;
+static char **shortLabels = NULL;	/* public hub short labels in array */
+static int publicHubCount = 0;
+static char *defaultHub = "Plants";
+static long enteredMainTime = 0;	/* will become = clock1000() on entry */
+		/* to allow calculation of when to bail out, taking too long */
+static long timeOutSeconds = 100;
+static boolean timedOut = FALSE;
+
+static boolean timeOutReached()
+{
+long nowTime = clock1000();
+timedOut = FALSE;
+if ((nowTime - enteredMainTime) > (1000 * timeOutSeconds))
+    timedOut= TRUE;
+return timedOut;
+}
 
 static void trackSettings(struct trackDb *tdb)
 /* process the settingsHash for a track */
@@ -36,34 +54,59 @@ struct hashEl *hel;
 struct hashCookie hc = hashFirst(tdb->settingsHash);
 while ((hel = hashNext(&hc)) != NULL)
     {
-    hPrintf("    <li>%s : %s</li>\n", hel->name, (char *)hel->val);
+    if (isEmpty((char *)hel->val))
+	hPrintf("    <li>%s : &lt;empty&gt;</li>\n", hel->name);
+    else
+	hPrintf("    <li>%s : '%s'</li>\n", hel->name, (char *)hel->val);
     }
 hPrintf("    </ul>\n");
 }
 
-static void bbiBriefMeasure(char *type, char *bigDataUrl, long *chromCount, long *itemCount)
+static int bbiBriefMeasure(char *type, char *bigDataUrl, long *chromCount, long *itemCount, struct dyString *errors)
 /* check a bigDataUrl to find chrom count and item count */
 {
+int retVal = 0;
 *chromCount = 0;
 *itemCount = 0;
-if (startsWithWord("bigBed", type))
+struct errCatch *errCatch = errCatchNew();
+if (errCatchStart(errCatch))
     {
-    struct bbiFile *bbi = bigBedFileOpen(bigDataUrl);
-    struct bbiChromInfo *chromList = bbiChromList(bbi);
-    *chromCount = slCount(chromList);
-    *itemCount = bigBedItemCount(bbi);
-    bbiFileClose(&bbi);
+    if (startsWithWord("bigNarrowPeak", type)
+            || startsWithWord("bigBed", type)
+            || startsWithWord("bigGenePred", type)
+            || startsWithWord("bigPsl", type)
+            || startsWithWord("bigChain", type)
+            || startsWithWord("bigMaf", type)
+            || startsWithWord("bigBarChart", type)
+            || startsWithWord("bigInteract", type))
+        {
+        struct bbiFile *bbi = NULL;
+        bbi = bigBedFileOpen(bigDataUrl);
+        struct bbiChromInfo *chromList = bbiChromList(bbi);
+        *chromCount = slCount(chromList);
+        *itemCount = bigBedItemCount(bbi);
+        bbiFileClose(&bbi);
+        }
+    else if (startsWithWord("bigWig", type))
+        {
+        struct bbiFile *bwf = bigWigFileOpen(bigDataUrl);
+        struct bbiChromInfo *chromList = bbiChromList(bwf);
+        struct bbiSummaryElement sum = bbiTotalSummary(bwf);
+        *chromCount = slCount(chromList);
+        *itemCount = sum.validCount;
+        bbiFileClose(&bwf);
+        }
     }
-else if (startsWithWord("bigWig", type))
+errCatchEnd(errCatch);
+if (errCatch->gotError)
     {
-    struct bbiFile *bwf = bigWigFileOpen(bigDataUrl);
-    struct bbiChromInfo *chromList = bbiChromList(bwf);
-    struct bbiSummaryElement sum = bbiTotalSummary(bwf);
-    *chromCount = slCount(chromList);
-    *itemCount = sum.validCount;
-    bbiFileClose(&bwf);
+    retVal = 1;
+    dyStringPrintf(errors, "%s", errCatch->message->string);
     }
-}
+errCatchFree(&errCatch);
+
+return retVal;
+}	/* static int bbiBriefMeasure() */
 
 static void trackList(struct trackDb *tdb)
 /* process the track list to show all tracks */
@@ -77,28 +120,42 @@ if (tdb)
 	{
         char *bigDataUrl = hashFindVal(track->settingsHash, "bigDataUrl");
       char *compositeTrack = hashFindVal(track->settingsHash, "compositeTrack");
-        boolean compositeOn = sameOk("on", compositeTrack) ? TRUE : FALSE;
+	char *superTrack = hashFindVal(track->settingsHash, "superTrack");
         boolean depthSearch = cartUsualBoolean(cart, "depthSearch", FALSE);
-        if (compositeOn)
+        if (compositeTrack)
            hashIncInt(countTracks, "composite container");
-        else
+        else if (superTrack)
+           hashIncInt(countTracks, "superTrack container");
+	else if (isEmpty(track->type))
+           hashIncInt(countTracks, "no type specified");
+	else
            hashIncInt(countTracks, track->type);
         if (depthSearch && bigDataUrl)
 	    {
 	    long chromCount = 0;
 	    long itemCount = 0;
-	    bbiBriefMeasure(track->type, bigDataUrl, &chromCount, &itemCount);
-	    if (startsWithWord("bigBed", track->type))
-	    	hPrintf("    <li>%s : %s : %ld chroms : %ld item count</li>\n", track->track, track->type, chromCount, itemCount);
-	    else if (startsWithWord("bigWig", track->type))
-	    	hPrintf("    <li>%s : %s : %ld chroms : %ld bases covered</li>\n", track->track, track->type, chromCount, itemCount);
+	    struct dyString *errors = newDyString(1024);
+	    int retVal = bbiBriefMeasure(track->type, bigDataUrl, &chromCount, &itemCount, errors);
+            if (retVal)
+		{
+		    hPrintf("    <li>%s : %s : ERROR: %s</li>\n", track->track, track->type, errors->string);
+		}
 	    else
-	    	hPrintf("    <li>%s : %s : %ld chroms : %ld count</li>\n", track->track, track->type, chromCount, itemCount);
+		{
+		if (startsWithWord("bigBed", track->type))
+		    hPrintf("    <li>%s : %s : %ld chroms : %ld item count</li>\n", track->track, track->type, chromCount, itemCount);
+		else if (startsWithWord("bigWig", track->type))
+		    hPrintf("    <li>%s : %s : %ld chroms : %ld bases covered</li>\n", track->track, track->type, chromCount, itemCount);
+		else
+		    hPrintf("    <li>%s : %s : %ld chroms : %ld count</li>\n", track->track, track->type, chromCount, itemCount);
+		}
 	    }
         else
 	    {
-	    if (compositeOn)
+	    if (compositeTrack)
 		hPrintf("    <li>%s : %s : composite track container</li>\n", track->track, track->type);
+	    else if (superTrack)
+		hPrintf("    <li>%s : %s : superTrack container</li>\n", track->track, track->type);
 	    else if (! depthSearch)
 		hPrintf("    <li>%s : %s : %s</li>\n", track->track, track->type, bigDataUrl);
             else
@@ -106,7 +163,9 @@ if (tdb)
 	    }
         if (allTrackSettings)
             trackSettings(track); /* show all settings */
-	}
+	if (timeOutReached())
+	    break;
+	}	/*	for ( ; track; track = track->next )	*/
     hPrintf("    <li>%d different track types</li>\n", countTracks->elCount);
     if (countTracks->elCount)
 	{
@@ -124,7 +183,7 @@ if (tdb)
 	}
     hPrintf("    </ul>\n");
     }
-}
+}	/*	static void trackList(struct trackDb *tdb)	*/
 
 static void assemblySettings(struct trackHubGenome *thg)
 /* display all the assembly 'settingsHash' */
@@ -140,6 +199,8 @@ while ((hel = hashNext(&hc)) != NULL)
         struct trackDb *tdb = trackHubTracksForGenome(thg->trackHub, thg);
 	trackList(tdb);
         }
+    if (timeOutReached())
+	break;
     }
 hPrintf("    </ul>\n");
 }
@@ -171,6 +232,8 @@ for ( ; thg; thg = thg->next )
 	long thisTime = clock1000();
 	hPrintf("<em>processing time %s: %ld millis</em><br>\n", thg->name, thisTime - lastTime);
 	}
+    if (timeOutReached())
+	break;
     }
 if (trackCounter->elCount)
     {
@@ -187,34 +250,79 @@ if (trackCounter->elCount)
 hPrintf("</ul>\n");
 }
 
-static void displayPublicHubs()
-/* show URLs to all public hubs */
+static void getPublicHubList()
+/* obtain shortLabel from all public hubs */
 {
-hPrintf("<h3>public hub URLs from hgwdev-gi</h3>\n");
-hPrintf("<ul>\n");
 struct sqlConnection *conn = hConnectCentral();
 // Build a query to find all public hub URL's
-struct dyString *query = sqlDyStringCreate("select hubUrl from %s",
+struct dyString *query = sqlDyStringCreate("select shortLabel from %s",
                                            hubPublicTableName());
 struct sqlResult *sr = sqlGetResult(conn, query->string);
 char **row;
 while ((row = sqlNextRow(sr)) != NULL)
+    {
+    slNameStore(&publicHubShortLabels, row[0]);
+    }
+sqlFreeResult(&sr);
+slNameSort(&publicHubShortLabels);
+int listSize = slCount(publicHubShortLabels);
+AllocArray(shortLabels, listSize);
+struct slName *el = publicHubShortLabels;
+int i = 0;
+for ( ; el != NULL; el = el->next )
+    {
+    shortLabels[i++] = el->name;
+    ++publicHubCount;
+    }
+}	/*	static void getPublicHubList()	*/
+
+static void displayPublicHubs()
+/* show URLs to all public hubs */
 {
-    hPrintf("<li>%s</li>\n", row[0]);
-}
+return;
+hPrintf("<h3>public hub URLs from hgwdev-gi</h3>\n");
+hPrintf("<ul>\n");
+struct sqlConnection *conn = hConnectCentral();
+// Build a query to find all public hub URL's
+struct dyString *query = sqlDyStringCreate("select hubUrl,shortLabel from %s",
+                                           hubPublicTableName());
+struct sqlResult *sr = sqlGetResult(conn, query->string);
+char **row;
+while ((row = sqlNextRow(sr)) != NULL)
+    {
+    hPrintf("<li>%s - %s</li>\n", row[1], row[0]);
+    }
+sqlFreeResult(&sr);
+hDisconnectCentral(&conn);
 
 hPrintf("</ul>\n");
+}
+
+static char *urlFromShortLabel(char *shortLabel)
+{
+char hubUrl[1024];
+char query[1024];
+struct sqlConnection *conn = hConnectCentral();
+// Build a query to select the hubUrl for the given shortLabel
+sqlSafef(query, sizeof(query), "select hubUrl from %s where shortLabel='%s'",
+      hubPublicTableName(), shortLabel);
+if (! sqlQuickQuery(conn, query, hubUrl, sizeof(hubUrl)))
+    hubUrl[0] = 0;
+hDisconnectCentral(&conn);
+return cloneString(hubUrl);
 }
 
 void doMiddle(struct cart *theCart)
 /* Set up globals and make web page */
 {
+getPublicHubList();	/* populates slName publicHubShortLabels	*/
 cart = theCart;
 measureTiming = hPrintStatus() && isNotEmpty(cartOptionalString(cart, "measureTiming"));
 measureTiming = TRUE;
 char *database = NULL;
 char *genome = NULL;
-char *url = "https://genome-test.gi.ucsc.edu/~hiram/hubs/Plants/hub.txt";
+
+
 getDbAndGenome(cart, &database, &genome, oldVars);
 initGenbankTableNames(database);
 
@@ -233,7 +341,15 @@ if ((NULL == pathInfo) || strlen(pathInfo) < 1) {
 
 cartWebStart(cart, database, "access mechanism to hub data resources");
 
-char *urlInput = cartUsualString(cart, "urlHub", url);
+char *goOtherHub = cartUsualString(cart, "goOtherHub", defaultHub);
+char *otherHubUrl = cartUsualString(cart, "urlHub", defaultHub);
+char *goPublicHub = cartUsualString(cart, "goPublicHub", defaultHub);
+char *hubDropDown = cartUsualString(cart, "publicHubs", defaultHub);
+char *urlDropDown = urlFromShortLabel(hubDropDown);
+char *urlInput = urlDropDown;	/* assume public hub */
+if (sameWord("go", goOtherHub))	/* requested other hub URL */
+    urlInput = otherHubUrl;
+
 long lastTime = clock1000();
 struct trackHub *hub = trackHubOpen(urlInput, "");
 if (measureTiming)
@@ -242,24 +358,47 @@ if (measureTiming)
     hPrintf("<em>hub open time: %ld millis</em><br>\n", thisTime - lastTime);
     }
 
+hPrintf("<h2>cart dump</h2>");
+hPrintf("<pre>\n");
+cartDump(cart);
+hPrintf("</pre>\n");
 
-hPrintf("<form action='%s' name='hubApiUrl' id='hubApiUrl' method='GET'\n\n", "../cgi-bin/hubApi");
-hPrintf("<br>Enter hub URL:&nbsp;");
+hPrintf("<form action='%s' name='hubApiUrl' id='hubApiUrl' method='GET'>\n\n", "../cgi-bin/hubApi");
+
+hPrintf("<b>Select public hub:&nbsp;</b>");
+#define JBUFSIZE 2048
+char javascript[JBUFSIZE];
+struct slPair *events = NULL;
+safef(javascript, sizeof(javascript), "this.lastIndex=this.selectedIndex;");
+slPairAdd(&events, "focus", cloneString(javascript));
+#define SMALLBUF 256
+// char class[SMALLBUF];
+// safef(class, sizeof(class), "viewDD normalText %s", "class");
+
+cgiMakeDropListClassWithIdStyleAndJavascript("publicHubs", "publicHubs",
+    shortLabels, publicHubCount, hubDropDown, NULL, "width: 400px", events);
+
+hWrites("&nbsp;");
+hButton("goPublicHub", "go");
+
+hPrintf("<br>Or, enter a hub URL:&nbsp;");
 hPrintf("<input type='text' name='urlHub' id='urlHub' size='60' value='%s'>\n", urlInput);
 hWrites("&nbsp;");
-hButton("goApi", "go");
+hButton("goOtherHub", "go");
+
 boolean depthSearch = cartUsualBoolean(cart, "depthSearch", FALSE);
 hPrintf("<br>\n&nbsp;&nbsp;");
 hCheckBox("depthSearch", cartUsualBoolean(cart, "depthSearch", FALSE));
-hPrintf("&nbsp;perform full bbi file measurement : %s<br>\n", depthSearch ? "TRUE" : "FALSE");
+hPrintf("&nbsp;perform full bbi file measurement : %s (will time out if taking longer than %ld seconds)<br>\n", depthSearch ? "TRUE" : "FALSE", timeOutSeconds);
 hPrintf("\n&nbsp;&nbsp;");
 allTrackSettings = cartUsualBoolean(cart, "allTrackSettings", FALSE);
 hCheckBox("allTrackSettings", allTrackSettings);
 hPrintf("&nbsp;display all track settings for each track : %s<br>\n", allTrackSettings ? "TRUE" : "FALSE");
 
+
 hPrintf("<br>\n</form>\n");
 
-hPrintf("<p>URL: %s<br>\n", urlInput);
+hPrintf("<p>URL: %s - %s<br>\n", urlInput, sameWord("go",goPublicHub) ? "public hub" : "other hub");
 hPrintf("name: %s<br>\n", hub->shortLabel);
 hPrintf("description: %s<br>\n", hub->longLabel);
 hPrintf("default db: '%s'<br>\n", isEmpty(hub->defaultDb) ? "(none available)" : hub->defaultDb);
@@ -268,10 +407,6 @@ printf("pathInfo:'%s'<br>\ndocRoot:'%s'<br>\n", pathInfo, docRoot);
 if (hub->genomeList)
     genomeList(hub);
 
-hPrintf("<h2>cart dump</h2>");
-hPrintf("<pre>\n");
-cartDump(cart);
-hPrintf("</pre>\n");
 hPrintf("</p>\n");
 
 displayPublicHubs();
@@ -286,12 +421,14 @@ char *excludeVars[] = {"Submit", "submit", NULL,};
 int main(int argc, char *argv[])
 /* Process command line. */
 {
-long enteredMainTime = clock1000();
+enteredMainTime = clock1000();
 cgiSpoof(&argc, argv);
 measureTiming = TRUE;
 verboseTimeInit();
 trackCounter = hashNew(0);
 cartEmptyShell(doMiddle, hUserCookie(), excludeVars, oldVars);
+if (timedOut)
+    hPrintf("<h1>Reached time out %ld seconds</h1>", timeOutSeconds);
 if (measureTiming)
     hPrintf("<em>Overall total time: %ld millis</em><br>\n", clock1000() - enteredMainTime);
 return 0;
