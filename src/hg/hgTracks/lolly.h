ae8dc8ab5e2c121398f31531c701ea064fb19aeb
Brian Raney <braney@soe.ucsc.edu>
2019-04-04 07:05:40 -0700

    add the ability to search the MANE track

---
+++ src/hg/hgTracks/lolly.h
@@ -0,0 +1,14 @@
+
+/* lolly.h - stuff unique to lollipop charts */
+
+/* Copyright (C) 2019 The Regents of the University of California 
+ * See README in this or parent directory for licensing information. */
+#ifndef LOLLY_H
+#define LOLLY_H
+
+
+/*	lollyOptions structure */
+struct lollyOptions
+    {
+    };
+#endif 
