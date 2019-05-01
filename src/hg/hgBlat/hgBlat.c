88483f54e928d2341e942d5f617ea935b8e5dbf4
Galt Barber <galt@soe.ucsc.edu>
2019-05-01 13:06:59 -0700

    Small improvement in hgBlat All Genomes output explanation in the header.

--- src/hg/hgBlat/hgBlat.c
+++ src/hg/hgBlat/hgBlat.c
@@ -1933,11 +1933,12 @@ else
 	slSort(&pfdDone, genomeHitsCmp);
 
 	// Print instructions
-        printf("The approximate results below are sorted by number of matching 'tiles', "
+        printf("The single best alignment found for each assembly is shown below.<br>\n"
+		"The approximate results below are sorted by number of matching 'tiles', "
                 "perfectly matching sub-sequences of length 11 (DNA) "
                 "or 4 (protein). <br>");
 	printf("Click the 'assembly' link to trigger a full BLAT alignment for that genome. \n");
-	printf("If its alignment score is < 20 bp, no match will be found.<br>\n");
+	printf("If its alignment score is < 20, including gaps and mismatches, no match will be found.<br>\n");
 
 	// Print report  // TODO move to final report at the end of ALL Assemblies
 	int lastSeqNumber = -1;
