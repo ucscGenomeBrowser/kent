734dde02cf470fb185706ac6ed8954f9dc3f3465
Christopher Lee <chmalee@ucsc.edu>
2019-07-31 16:28:27 -0700

    Making track hub errors show up in red from hubCheck

--- src/hg/utils/hubCheck/hubCheck.c
+++ src/hg/utils/hubCheck/hubCheck.c
@@ -475,7 +475,7 @@ char *makeChildObjectString(char *id, char *title, char *shortLabel, char *longL
 /* Construct a single child item for one of the jstree arrays */
 {
 struct dyString *item = dyStringNew(0);
-dyStringPrintf(item, "{icon: 'fa fa-plus', id:'%s', li_attr:{title: '%s', "
+dyStringPrintf(item, "{icon: 'fa fa-plus', id:'%s', li_attr:{class: 'hubError', title: '%s', "
         "shortLabel: '%s', longLabel: '%s', color: '%s', name:'%s'}, "
         "text:\"%s\", parent: '%s', state: {opened: true}}",
         id, title, shortLabel, longLabel, color, name, text, parent);
@@ -540,7 +540,7 @@ if (!doHtml)
 else
     {
     char *strippedMessage = NULL;
-    char *parentOrTrackString = trackHubSkipHubName(tdb->track);
+    char parentOrTrackString[512];
     char id[512];
     static int count = 0; // forces unique ID's which the jstree object needs
 
@@ -549,6 +549,7 @@ else
 
     stripChar(strippedMessage, '\n');
     safef(id, sizeof(id), "%s%d", trackHubSkipHubName(tdb->track), count);
+    safef(parentOrTrackString, sizeof(parentOrTrackString), "%s_%s", trackHubSkipHubName(genome->name), trackHubSkipHubName(tdb->track));
     dyStringPrintf(errors, "%s,",
             makeChildObjectString(id, "TrackDb Error", tdb->shortLabel, tdb->longLabel,
             "#550073", trackHubSkipHubName(tdb->track), strippedMessage, parentOrTrackString));
@@ -934,7 +935,7 @@ for (genome = hub->genomeList; genome != NULL; genome = genome->next)
             "%s (%d configuration error%s)", genomeName, numGenomeErrors,
             numGenomeErrors == 1 ? "" : "s");
         dyStringPrintf(errors, "%s,", makeFolderObjectString(genomeName, genomeTitleString, "#",
-            "Click to open node", TRUE, TRUE));
+            "Click to open node", TRUE, numGenomeErrors > 0 ? TRUE : FALSE));
         }
     retVal |= numGenomeErrors;
     }
