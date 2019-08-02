fe122ae0b137be8ee4dbeb0e64d9185190bd7e51
Jim Kent <kent@soe.ucsc.edu>
2019-08-02 14:29:14 -0700

    Made it so that you list output column name first and can use constants as well as input column names to say where it comes from.

--- src/tabFile/tabToTabDir/tabToTabDir.c
+++ src/tabFile/tabToTabDir/tabToTabDir.c
@@ -4,6 +4,7 @@
 #include "linefile.h"
 #include "hash.h"
 #include "options.h"
+#include "obscure.h"
 #include "portable.h"
 #include "ra.h"
 #include "fieldedTable.h"
@@ -15,17 +16,21 @@ errAbort(
 "tabToTabDir - Convert a large tab-separated table to a directory full of such tables according\n"
 "to a specification.\n"
 "usage:\n"
-"   tabToTabDir in.tsv spec.ra outDir\n"
+"   tabToTabDir in.tsv spec.txt outDir\n"
 "where:\n"
 "   in.tsv is a tab-separated input file.  The first line is the label names and may start with #\n"
-"   spec is a file that says what columns to put into the output, described in more detail below\n"
+"   spec.txt is a file that says what columns to put into the output, described in more detail below\n"
 "   outDir is a directory that will be populated with tab-separated files\n"
 "spec.ra file format:\n"
 "   This is in a ra format with one stanza per output table.\n"
 "   Each stanza should look like:\n"
-"        tableName    key-field\n"
-"        oldColumn	[newColumn]\n"
-"   if the newColumn is missing it is assumed the same as the old\n"
+"        tableName    key-column\n"
+"        columnName1	sourceField1\n"
+"        columnName2	sourceField2\n"
+"              ...\n"
+"   if the sourceField is missing it is assumed it is named to be a column of the same name in in.tsv\n"
+"   The sourceField can either be a column name in the in.tsv, or a string enclosed literal or something\n"
+"   more complicated enclosed in an eval() call\n"
 "options:\n"
 "   -xxx=XXX\n"
 );
@@ -47,21 +52,92 @@ for (i=0; i<count; ++i)
 return TRUE;
 }
 
-void selectUniqueIntoTable(struct fieldedTable *inTable, 
-    int keyFieldIx, int oldFieldIx[], struct fieldedTable *outTable)
+enum valType
+/* A type */
+    {
+    vtVar, vtLink, vtConst, 
+    };
+
+struct fieldVal
+/* An expression that can define what fits in a field */
+    {
+    struct fieldVal *next;	/* Might want to hang these on a list. */
+    char *name;	
+    enum valType type;			/* Constant, link, or variable */
+    int oldIx;				/* For variable and link ones where field is in old table */
+    char *val;			/* For constant ones the string value */
+    };
+
+struct fieldVal *parseFieldVal(char *name, char *input)
+/* return a fieldVal based on the contents of input, which are not destroyed */
+{
+/* Make up return structure. */
+
+struct fieldVal *fv;
+AllocVar(fv);
+fv->name = cloneString(name);
+
+char *s = skipLeadingSpaces(input);
+char c = s[0];
+if (c == 0)
+    {
+    fv->type = vtVar;
+    fv->val = cloneString(name);
+    }
+else
+    {
+    if (c == '"' || c == '\'')
+	{
+	char *val = fv->val = cloneString(s);
+	if (!parseQuotedString(val, val, NULL))
+	    errAbort("in %s", input);
+	fv->type = vtConst;
+	}
+    else if (c == '=')
+	{
+	char *val = fv->val = cloneString(skipLeadingSpaces(s+1));
+	trimSpaces(val);
+	if (isEmpty(val))
+	    errAbort("Empty line following equals");
+	fv->type = vtLink;
+	}
+    else
+        {
+	char *val = fv->val = cloneString(s);
+	trimSpaces(val);
+	fv->type = vtVar;
+	}
+    }
+return fv;
+}
+
+void selectUniqueIntoTable(struct fieldedTable *inTable, struct fieldVal *fieldList,
+    int keyFieldIx, struct fieldedTable *outTable)
 /* Populate out table with selected rows from newTable */
 {
 struct hash *uniqHash = hashNew(0);
 struct fieldedRow *fr;
 int outFieldCount = outTable->fieldCount;
 char *outRow[outFieldCount];
+
+if (slCount(fieldList) != outFieldCount)	// A little cheap defensive programming on inputs
+    internalErr();
+
 for (fr = inTable->rowList; fr != NULL; fr = fr->next)
     {
+    /* Create new row from a scan through old table */
     char **inRow = fr->row;
     char *key = inRow[keyFieldIx];
     int i;
-    for (i=0; i<outFieldCount; ++i)
-        outRow[i] = inRow[oldFieldIx[i]];
+    struct fieldVal *fv;
+    for (i=0, fv=fieldList; i<outFieldCount && fv != NULL; ++i, fv = fv->next)
+	{
+	if (fv->type == vtConst)
+	    outRow[i] = fv->val;
+	else
+	    outRow[i] = inRow[fv->oldIx];
+	}
+
     struct fieldedRow *uniqFr = hashFindVal(uniqHash, key);
     if (uniqFr == NULL)
         {
@@ -108,22 +184,26 @@ while ((specStanza = raNextStanzAsPairs(lf)) != NULL)
     /* Create empty output table and track which fields of input go to output. */
     int fieldCount = slCount(fieldList);
     char *fieldNames[fieldCount];
-    int oldFieldIx[fieldCount];
     int i;
     struct slPair *field;
+    struct fieldVal *fvList = NULL;
     for (i=0, field=fieldList; i<fieldCount; ++i, field=field->next)
         {
-	char *newName = trimSpaces(field->val);
-	if (isEmpty(newName))
-	    newName = field->name;
+	char *newName = field->name;
+	struct fieldVal *fv = parseFieldVal(newName, field->val);
+	if (fv->type == vtVar)
+	    fv->oldIx = fieldedTableMustFindFieldIx(inTable, fv->val);
+	else if (fv->type == vtLink)
+	    errAbort("Can't handle links yet for %s", fv->val);
 	fieldNames[i] = newName;
-	oldFieldIx[i] = fieldedTableMustFindFieldIx(inTable,  field->name);
+	slAddHead(&fvList, fv);
 	}
+    slReverse(&fvList);
     struct fieldedTable *outTable = fieldedTableNew(tableName, fieldNames, fieldCount);
     outTable->startsSharp = inTable->startsSharp;
 
     /* Populate table */
-    selectUniqueIntoTable(inTable, keyFieldIx, oldFieldIx, outTable);
+    selectUniqueIntoTable(inTable, fvList, keyFieldIx, outTable);
 
     /* Create output file name and save file. */
     char outTabName[FILENAME_LEN];
