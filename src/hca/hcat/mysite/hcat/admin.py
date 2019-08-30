bd66fde8159a3069c11b602c6c62205db07ebf9b
Jim Kent <kent@soe.ucsc.edu>
2019-08-30 13:51:30 -0700

    Renaming less interesting tables to begin with 'vocab' so as to unbury project table.  Adding comments and project origins fields.

--- src/hca/hcat/mysite/hcat/admin.py
+++ src/hca/hcat/mysite/hcat/admin.py
@@ -4,10 +4,11 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-from .models import Grant,Project,Lab,Contributor,Funder,ProjectState,Consent,Species,Organ,OrganPart,Disease,SampleType,AssayType,AssayTech,Publication
+from .models import Grant,Project,Lab,Contributor,VocabFunder,VocabProjectState,VocabConsent,Species,VocabOrgan,VocabOrganPart,VocabDisease,VocabSampleType,VocabAssayTech,VocabAssayType,VocabPublication,VocabComment,VocabCommentType,VocabProjectOrigin
 
 class ContributorAdmin(admin.ModelAdmin):
     list_filter = ['project_role']
+    autocomplete_fields = ['comments', 'projects', 'labs', 'grants']
     search_fields = ['name', 'project_role', 'email',]
     list_display = ['name', 'project_role', 'email', ]
 
@@ -15,25 +16,32 @@ admin.site.register(Contributor, ContributorAdmin)
 
 
 class LabAdmin(admin.ModelAdmin):
-    autocomplete_fields = ['contributors', 'pi', 'contact']
+    autocomplete_fields = ['contributors', 'pi', 'contact', 'grants', 'projects', 'comments']
     search_fields = ['short_name', 'pi', 'contact']
     list_display = ['short_name', 'institution',]
 
 admin.site.register(Lab, LabAdmin)
 
-class ProjectStateAdmin(admin.ModelAdmin):
+class VocabProjectStateAdmin(admin.ModelAdmin):
     list_display = ['state', 'description']
+    autocomplete_fields = ['comments']
     
-admin.site.register(ProjectState, ProjectStateAdmin)
+admin.site.register(VocabProjectState, VocabProjectStateAdmin)
+
+class VocabProjectOriginAdmin(admin.ModelAdmin):
+    list_display = ['short_name', 'description']
+    autocomplete_fields = ['comments']
+    
+admin.site.register(VocabProjectOrigin, VocabProjectOriginAdmin)
 
 class ProjectAdmin(admin.ModelAdmin):
     search_fields = ['short_name', 'title', 'contributors', 'labs', 'organ_part', 'organ', 'disease', 'species']
-    autocomplete_fields = ["contributors", "labs", "organ", "organ_part", "disease", "species", "sample_type", "assay_type", "assay_tech", "publications"]
-    list_display = ['short_name', 'score', 'state', 'wrangler1', 'title',]
-    list_filter = ['species', 'organ', 'state', 'assay_tech']
+    autocomplete_fields = ["contributors", "labs", "organ", "organ_part", "disease", "species", "sample_type", "assay_type", "assay_tech", "publications", "comments", "grants"]
+    list_display = ['short_name', 'stars', 'state', 'wrangler1', 'title',]
+    list_filter = ['species', 'origin', 'state', 'assay_tech']
     fieldsets = (
-        ('overall', { 'fields': ('short_name', 'title', 'description','publications')}),
-        ('wrangling',  { 'fields': ('state', 'consent', 'score', 'wrangler1', 'wrangler2', 'contributors')}),
+        ('overall', { 'fields': ('short_name', 'title', 'description','publications', )}),
+        ('wrangling',  { 'fields': ('state', 'consent', 'stars', 'wrangler1', 'wrangler2', 'contributors', 'labs', 'grants', 'comments', 'origin')}),
         ('biosample',  { 'fields': ('species', 'sample_type', 'organ', 'organ_part', 'disease')}),
         ('assay', { 'fields': ('assay_type', 'assay_tech', 'cells_expected')}),
     )
@@ -45,17 +53,18 @@ class ProjectAdmin(admin.ModelAdmin):
 admin.site.register(Project, ProjectAdmin)
 
 class GrantAdmin(admin.ModelAdmin):
-    autocomplete_fields = ["funded_contributors", "funded_projects", "funded_labs"]
+    autocomplete_fields = ["funded_contributors", "funded_projects", "funded_labs", "comments"]
     search_fields = ["funded_contributors", "funded_projects", "funded_labs"]
     list_display = ['grant_id', 'funder', 'grant_title',]
     list_filter = ['funder']
 
 admin.site.register(Grant, GrantAdmin)
 
-class FunderAdmin(admin.ModelAdmin):
+class VocabFunderAdmin(admin.ModelAdmin):
     list_display = ['short_name', 'description',]
+    autocomplete_fields = ['comments']
 
-admin.site.register(Funder, FunderAdmin)
+admin.site.register(VocabFunder, VocabFunderAdmin)
 
 
 class SpeciesAdmin(admin.ModelAdmin):
@@ -64,48 +73,73 @@ class SpeciesAdmin(admin.ModelAdmin):
 
 admin.site.register(Species, SpeciesAdmin)
 
-class ConsentAdmin(admin.ModelAdmin):
-    search_fields = ["short_name"]
-
-admin.site.register(Consent, ConsentAdmin)
-
-class DiseaseAdmin(admin.ModelAdmin):
+class VocabConsentAdmin(admin.ModelAdmin):
     search_fields = ["short_name"]
     list_display = ["short_name", "description"]
+    autocomplete_fields = ['comments']
 
-admin.site.register(Disease, DiseaseAdmin)
+admin.site.register(VocabConsent, VocabConsentAdmin)
 
-class OrganAdmin(admin.ModelAdmin):
-     search_fields = ["short_name"]
-
-admin.site.register(Organ, OrganAdmin)
-
-class OrganPartAdmin(admin.ModelAdmin):
-     search_fields = ["short_name"]
-
-admin.site.register(OrganPart, OrganPartAdmin)
-
-class SampleTypeAdmin(admin.ModelAdmin):
+class VocabDiseaseAdmin(admin.ModelAdmin):
     search_fields = ["short_name"]
     list_display = ["short_name", "description"]
+    autocomplete_fields = ['comments']
 
-admin.site.register(SampleType, SampleTypeAdmin)
+admin.site.register(VocabDisease, VocabDiseaseAdmin)
 
-class AssayTypeAdmin(admin.ModelAdmin):
+class VocabOrganAdmin(admin.ModelAdmin):
     search_fields = ["short_name"]
     list_display = ["short_name", "description"]
+    autocomplete_fields = ['comments']
 
-admin.site.register(AssayType, AssayTypeAdmin)
+admin.site.register(VocabOrgan, VocabOrganAdmin)
 
-class AssayTechAdmin(admin.ModelAdmin):
+class VocabOrganPartAdmin(admin.ModelAdmin):
     search_fields = ["short_name"]
     list_display = ["short_name", "description"]
+    autocomplete_fields = ['comments']
 
-admin.site.register(AssayTech, AssayTechAdmin)
 
-class PublicationAdmin(admin.ModelAdmin):
+admin.site.register(VocabOrganPart, VocabOrganPartAdmin)
+
+class VocabSampleTypeAdmin(admin.ModelAdmin):
+    search_fields = ["short_name"]
+    list_display = ["short_name", "description"]
+    autocomplete_fields = ['comments']
+
+admin.site.register(VocabSampleType, VocabSampleTypeAdmin)
+
+class VocabAssayTechAdmin(admin.ModelAdmin):
+    search_fields = ["short_name"]
+    list_display = ["short_name", "description"]
+    autocomplete_fields = ['comments']
+
+admin.site.register(VocabAssayTech, VocabAssayTechAdmin)
+
+class VocabAssayTypeAdmin(admin.ModelAdmin):
+    search_fields = ["short_name"]
+    list_display = ["short_name", "description"]
+    autocomplete_fields = ['comments']
+
+admin.site.register(VocabAssayType, VocabAssayTypeAdmin)
+
+class VocabPublicationAdmin(admin.ModelAdmin):
     search_fields = ["short_name"]
     list_display = ["short_name", "title"]
+    autocomplete_fields = ['comments']
 
-admin.site.register(Publication, PublicationAdmin)
+admin.site.register(VocabPublication, VocabPublicationAdmin)
+
+class VocabCommentTypeAdmin(admin.ModelAdmin):
+    search_fields = ["short_name"]
+    list_display = ["short_name", "description"]
+    short_description = "emotion"
+
+admin.site.register(VocabCommentType, VocabCommentTypeAdmin)
+
+class VocabCommentAdmin(admin.ModelAdmin):
+    search_fields = ["type", "text"]
+    list_display = ["type", "text"]
+
+admin.site.register(VocabComment, VocabCommentAdmin)
 
--- src/hca/hcat/mysite/hcat/models.py
+++ src/hca/hcat/mysite/hcat/models.py
@@ -6,20 +6,41 @@ from django.db import models
 from django.utils import timezone
 from django.core.validators import MaxValueValidator, MinValueValidator
 
+class VocabCommentType(models.Model):
+    short_name = models.CharField(unique=True, max_length=10)
+    description = models.TextField()
+    def __str__(self):
+       return self.short_name
+
+class VocabComment(models.Model):
+    type = models.ForeignKey(VocabCommentType, on_delete=models.PROTECT)
+    text = models.CharField(unique=True, max_length=255)
+    def __str__(self):
+       #return self.type.short_name + ": " + self.text[:80]
+       # The above breaks the lookup, possible to do but not worth it yet.
+       return self.text
+    
 class Contributor(models.Model):
     name = models.CharField(max_length=250, unique=True)
     email = models.EmailField(max_length=254, blank=True)
     phone = models.CharField(max_length=25, blank=True)
     project_role = models.CharField(max_length=100, default="contributor")
+    projects = models.ManyToManyField("Project", blank=True, through="project_contributors")
+    labs = models.ManyToManyField("Lab", blank=True, through="lab_contributors")
+    grants = models.ManyToManyField("Grant", blank=True, through="grant_funded_contributors")
+    comments = models.ManyToManyField(VocabComment, blank=True);
     def __str__(self):
        return self.name
 
 class Lab(models.Model):
     short_name =  models.CharField(max_length=50, unique=True)
     institution = models.CharField(max_length=250, blank="")
-    contributors = models.ManyToManyField(Contributor)
     contact = models.ForeignKey(Contributor, null=True, default=None, on_delete=models.SET_NULL, related_name="contact")
     pi = models.ForeignKey(Contributor, null=True, default=None, on_delete=models.SET_NULL, related_name="pi")
+    contributors = models.ManyToManyField(Contributor)
+    projects = models.ManyToManyField("Project", blank=True, through="project_labs")
+    grants = models.ManyToManyField("Grant", blank=True, through="grant_funded_labs")
+    comments = models.ManyToManyField(VocabComment, blank=True);
     def __str__(self):
        return self.short_name
 
@@ -29,99 +50,123 @@ class Species(models.Model):
      ncbi_taxon = models.IntegerField(unique=True)
      def __str__(self):
        return self.common_name
+     class Meta:
+       verbose_name_plural = "species"
 
-class ProjectState(models.Model):
+class VocabProjectState(models.Model):
     state = models.CharField(max_length=30, unique=True)
     description = models.CharField(max_length=100)
+    comments = models.ManyToManyField(VocabComment, blank=True);
     def __str__(self):
        return self.state
 
-class OrganPart(models.Model):
+class VocabOrganPart(models.Model):
     short_name = models.CharField(max_length=50, unique=True)
     description = models.CharField(max_length=250)
+    comments = models.ManyToManyField(VocabComment, blank=True);
     def __str__(self):
         return self.short_name
 
-class Organ(models.Model):
+class VocabOrgan(models.Model):
     short_name = models.CharField(max_length=50, unique=True)
     description = models.CharField(max_length=250)
+    comments = models.ManyToManyField(VocabComment, blank=True);
     def __str__(self):
         return self.short_name
     
-class Disease(models.Model):
+class VocabDisease(models.Model):
     short_name = models.CharField(max_length=50, unique=True)
     description = models.CharField(max_length=250)
+    comments = models.ManyToManyField(VocabComment, blank=True);
     def __str__(self):
         return self.short_name
 
-class Consent(models.Model):
+class VocabConsent(models.Model):
     short_name = models.CharField(max_length=50, unique=True)
     description = models.CharField(max_length=250)
+    comments = models.ManyToManyField(VocabComment, blank=True);
     def __str__(self):
         return self.short_name
 
-class SampleType(models.Model):
+class VocabSampleType(models.Model):
     short_name = models.CharField(max_length=50, unique=True)
     description = models.CharField(max_length=250)
+    comments = models.ManyToManyField(VocabComment, blank=True);
     def __str__(self):
         return self.short_name
 
-class AssayType(models.Model):
+class VocabAssayTech(models.Model):
     short_name = models.CharField(max_length=50, unique=True)
     description = models.CharField(max_length=250)
+    comments = models.ManyToManyField(VocabComment, blank=True);
     def __str__(self):
         return self.short_name
 
-class AssayTech(models.Model):
+class VocabAssayType(models.Model):
     short_name = models.CharField(max_length=50, unique=True)
     description = models.CharField(max_length=250)
+    comments = models.ManyToManyField(VocabComment, blank=True);
     def __str__(self):
         return self.short_name
 
-class Publication(models.Model):
+class VocabPublication(models.Model):
     short_name = models.CharField(max_length=50, unique=True)
     title = models.CharField(max_length=250, blank=True)
     pmid = models.CharField(max_length=16)
     doi = models.CharField(max_length=32)
+    comments = models.ManyToManyField(VocabComment, blank=True);
+    def __str__(self):
+        return self.short_name
+
+class VocabProjectOrigin(models.Model):
+    short_name = models.CharField(max_length=50, unique=True)
+    description = models.CharField(max_length=255)
+    comments = models.ManyToManyField(VocabComment, blank=True);
     def __str__(self):
         return self.short_name
 
 class Project(models.Model):
     short_name = models.CharField(max_length=80)
-    state = models.ForeignKey(ProjectState, blank=True, null=True, default=None, on_delete=models.SET_NULL)
-    score = models.IntegerField(blank=True,validators=[MinValueValidator(1),MaxValueValidator(5)], default=3)
+    state = models.ForeignKey(VocabProjectState, blank=True, null=True, default=None, on_delete=models.SET_NULL)
+    stars = models.IntegerField(blank=True,validators=[MinValueValidator(1),MaxValueValidator(5)], default=3)
     wrangler1 = models.ForeignKey(Contributor, blank=True, null=True, default=None, on_delete=models.SET_NULL, related_name="wrangler1")
     wrangler2 = models.ForeignKey(Contributor, blank=True, null=True, default=None, on_delete=models.SET_NULL, related_name="wrangler2")
     title = models.CharField(max_length=120)
     description = models.TextField()
     labs = models.ManyToManyField(Lab, blank=True)
+    organ = models.ManyToManyField(VocabOrgan, blank=True)
+    organ_part = models.ManyToManyField(VocabOrganPart, blank=True)
+    disease = models.ManyToManyField(VocabDisease, blank=True)
+    sample_type = models.ManyToManyField(VocabSampleType, blank=True)
+    consent = models.ForeignKey(VocabConsent, blank=True, null=True, default=None, on_delete=models.SET_NULL)
+    origin = models.ForeignKey(VocabProjectOrigin, blank=True, null=True, default=None, on_delete=models.SET_NULL)
+    assay_type = models.ManyToManyField(VocabAssayType, blank=True)
+    assay_tech = models.ManyToManyField(VocabAssayTech, blank=True)
+    cells_expected = models.IntegerField(blank=True, default=0)
+    publications = models.ManyToManyField(VocabPublication, blank=True)
     contributors = models.ManyToManyField(Contributor)
     species = models.ManyToManyField(Species, blank=True)
-    organ = models.ManyToManyField(Organ, blank=True)
-    organ_part = models.ManyToManyField(OrganPart, blank=True)
-    disease = models.ManyToManyField(Disease, blank=True)
-    sample_type = models.ManyToManyField(SampleType, blank=True)
-    consent = models.ForeignKey(Consent, blank=True, null=True, default=None, on_delete=models.SET_NULL)
-    assay_type = models.ManyToManyField(AssayType, blank=True)
-    assay_tech = models.ManyToManyField(AssayTech, blank=True)
-    cells_expected = models.IntegerField(blank=True, default=0)
-    publications = models.ManyToManyField(Publication, blank=True)
+    grants = models.ManyToManyField("Grant", blank=True, through="grant_funded_projects")
+    comments = models.ManyToManyField(VocabComment, blank=True);
     def __str__(self):
        return self.short_name
 
-class Funder(models.Model):
+class VocabFunder(models.Model):
     short_name = models.CharField(max_length=50)
     description = models.TextField()
+    comments = models.ManyToManyField(VocabComment, blank=True);
     def __str__(self):
        return self.short_name
+
 
 class Grant(models.Model):
     grant_title = models.CharField(max_length=200)
     grant_id = models.CharField(max_length=200)
-    funder = models.ForeignKey(Funder, null=True, default=None, on_delete=models.SET_NULL)
+    funder = models.ForeignKey(VocabFunder, null=True, default=None, on_delete=models.SET_NULL)
     funded_projects = models.ManyToManyField(Project, blank=True)
     funded_labs = models.ManyToManyField(Lab, blank=True)
     funded_contributors = models.ManyToManyField(Contributor, blank=True)
+    comments = models.ManyToManyField(VocabComment, blank=True);
     def __str__(self):
        return self.grant_title
 
