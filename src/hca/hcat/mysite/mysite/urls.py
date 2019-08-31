f82195528b91713f5167587acc48a7602c82a39a
Jim Kent <kent@soe.ucsc.edu>
2019-08-31 10:15:21 -0700

    Adding a piece needed to serve uploaded files.

--- src/hca/hcat/mysite/mysite/urls.py
+++ src/hca/hcat/mysite/mysite/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include,path
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
-    #path("accounts/", account.site.urls),
     path('admin/', admin.site.urls),
     path('hcat/', include('hcat.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
