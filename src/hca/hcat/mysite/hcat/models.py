3a16297829778f22d084827214ed1b6b650e866c
Jim Kent <kent@soe.ucsc.edu>
2019-09-05 16:31:51 -0700

    Making comments just text fields.  Making wranglers, curators, and software developers as separate tables with a link to plain old contributors.  A bunch of more minor changes that were easy to implement described in the 0.5 section of version.txt.

--- src/hca/hcat/mysite/hcat/models.py
+++ src/hca/hcat/mysite/hcat/models.py
@@ -6,61 +6,42 @@ from django.db import models
 from django.utils import timezone
 from django.core.validators import MaxValueValidator, MinValueValidator
 
-class CommentType(models.Model):
-    short_name = models.CharField(unique=True, max_length=10)
-    description = models.TextField()
+class ContributorType(models.Model):
+    short_name = models.CharField(max_length=50, unique=True)
+    description = models.CharField(max_length=250)
+    comments = models.CharField(max_length=255, blank=True)
     def __str__(self):
-       return self.short_name
+        return self.short_name
     class Meta:
-       verbose_name = 'Wrangler comment type'
+       verbose_name = 'Wrangler contributor type'
 
-class Comment(models.Model):
-    type = models.ForeignKey(CommentType, on_delete=models.PROTECT)
-    text = models.CharField(unique=True, max_length=255)
-    projects = models.ManyToManyField("Project", blank=True, through="project_comments")
-    grants = models.ManyToManyField("Grant", blank=True, through="grant_comments")
-    consents = models.ManyToManyField("Consent", blank=True, through="consent_comments")
-    labs = models.ManyToManyField("Lab", blank=True, through="lab_comments")
-    contributors = models.ManyToManyField("Contributor", blank=True, through="contributor_comments")
-    organs = models.ManyToManyField("Organ", blank=True, through="organ_comments");
-    organ_parts = models.ManyToManyField("OrganPart", blank=True, through="organpart_comments");
-    urls = models.ManyToManyField("Url", blank=True, through="url_comments");
-    files = models.ManyToManyField("File", blank=True, through="file_comments");
-    class Meta:
-       verbose_name = 'Wrangler comment'
-
-    def __str__(self):
-       #return self.type.short_name + ": " + self.text[:80]
-       # The above breaks the lookup, possible to do but not worth it yet.
-       return self.text
-    
 class Contributor(models.Model):
-    name = models.CharField(max_length=250, unique=True)
-    email = models.EmailField(max_length=254, blank=True)
-    phone = models.CharField(max_length=25, blank=True)
-    address = models.CharField(max_length=254, blank=True)
-    department = models.CharField(max_length=150, blank=True)
-    institute = models.CharField(max_length=150, blank=True)
+    name = models.CharField(max_length=255, unique=True)
+    email = models.EmailField(max_length=128, blank=True)
+    phone = models.CharField(max_length=32, blank=True)
+    address = models.CharField(max_length=255, blank=True)
+    department = models.CharField(max_length=255, blank=True)
+    institute = models.CharField(max_length=255, blank=True)
     city = models.CharField(max_length=100, blank=True)
     zip_postal_code = models.CharField(max_length=20, blank=True)
     country = models.CharField(max_length = 100, blank=True)
-    project_role = models.CharField(max_length=100, default="contributor")
     projects = models.ManyToManyField("Project", blank=True, through="project_contributors")
     labs = models.ManyToManyField("Lab", blank=True, through="lab_contributors")
     grants = models.ManyToManyField("Grant", blank=True, through="grant_funded_contributors")
-    comments = models.ManyToManyField(Comment, blank=True);
+    type = models.ForeignKey(ContributorType, blank=True, null=True, default=None, on_delete=models.SET_NULL)
+    comments = models.CharField(max_length=255, blank=True)
     def __str__(self):
        return self.name
 
 class Lab(models.Model):
     short_name =  models.CharField(max_length=50, unique=True)
     institution = models.CharField(max_length=250, null=True, blank=True)
-    contact = models.ForeignKey(Contributor, null=True, default=None, on_delete=models.SET_NULL, related_name="contact")
+    #contacts = models.ManyToManyField(Contributor, related_name="contacts")
     pi = models.ForeignKey(Contributor, blank=True, null=True, default=None, on_delete=models.SET_NULL, related_name="pi")
-    contributors = models.ManyToManyField(Contributor, blank=True)
+    contributors = models.ManyToManyField(Contributor, blank=True, related_name="contributors")
     projects = models.ManyToManyField("Project", blank=True, through="project_labs")
     grants = models.ManyToManyField("Grant", blank=True, through="grant_funded_labs")
-    comments = models.ManyToManyField(Comment, blank=True);
+    comments = models.CharField(max_length=255, blank=True)
     def __str__(self):
        return self.short_name
 
@@ -77,7 +58,7 @@ class Url(models.Model):
     short_name = models.CharField(unique=True, max_length=50)
     path = models.URLField()
     description = models.CharField(max_length=255)
-    comments = models.ManyToManyField(Comment, blank=True);
+    comments = models.CharField(max_length=255, blank=True)
     def __str__(self):
        return self.short_name
     class Meta:
@@ -87,7 +68,7 @@ class File(models.Model):
     short_name = models.CharField(unique=True, max_length=80)
     file = models.FileField(upload_to="uploads")
     description = models.CharField(max_length=255)
-    comments = models.ManyToManyField(Comment, blank=True);
+    comments = models.CharField(max_length=255, blank=True)
     def __str__(self):
        return self.short_name
     class Meta:
@@ -96,7 +77,6 @@ class File(models.Model):
 class ProjectState(models.Model):
     state = models.CharField(max_length=30, unique=True)
     description = models.CharField(max_length=100)
-    comments = models.ManyToManyField(Comment, blank=True);
     def __str__(self):
        return self.state
     class Meta:
@@ -106,7 +86,7 @@ class OrganPart(models.Model):
     short_name = models.CharField(max_length=50, unique=True)
     description = models.CharField(max_length=250)
     projects = models.ManyToManyField("Project", blank=True, through="project_organ_part")
-    comments = models.ManyToManyField(Comment, blank=True);
+    comments = models.CharField(max_length=255, blank=True)
     def __str__(self):
         return self.short_name
     class Meta:
@@ -115,8 +95,7 @@ class OrganPart(models.Model):
 class Organ(models.Model):
     short_name = models.CharField(max_length=50, unique=True)
     description = models.CharField(max_length=250)
-    #projects = models.ManyToManyField("Project", blank=True, through="project_organ", related_name='projects_organ_relationship')
-    comments = models.ManyToManyField(Comment, blank=True);
+    comments = models.CharField(max_length=255, blank=True)
     def __str__(self):
         return self.short_name
     class Meta:
@@ -125,8 +104,8 @@ class Organ(models.Model):
 class Disease(models.Model):
     short_name = models.CharField(max_length=50, unique=True)
     description = models.CharField(max_length=250)
-    comments = models.ManyToManyField(Comment, blank=True);
     projects = models.ManyToManyField("Project", blank=True, through="project_disease", related_name='projects_diseases_relationship')
+    comments = models.CharField(max_length=255, blank=True)
     def __str__(self):
         return self.short_name
     class Meta:
@@ -135,7 +114,7 @@ class Disease(models.Model):
 class Consent(models.Model):
     short_name = models.CharField(max_length=50, unique=True)
     description = models.CharField(max_length=250)
-    comments = models.ManyToManyField(Comment, blank=True);
+    comments = models.CharField(max_length=255, blank=True)
     def __str__(self):
         return self.short_name
     class Meta:
@@ -144,7 +123,7 @@ class Consent(models.Model):
 class SampleType(models.Model):
     short_name = models.CharField(max_length=50, unique=True)
     description = models.CharField(max_length=250)
-    comments = models.ManyToManyField(Comment, blank=True);
+    comments = models.CharField(max_length=255, blank=True)
     def __str__(self):
         return self.short_name
     class Meta:
@@ -153,7 +132,7 @@ class SampleType(models.Model):
 class AssayTech(models.Model):
     short_name = models.CharField(max_length=50, unique=True)
     description = models.CharField(max_length=250)
-    comments = models.ManyToManyField(Comment, blank=True);
+    comments = models.CharField(max_length=255, blank=True)
     projects = models.ManyToManyField("Project", blank=True, through="project_assay_tech", related_name='projects_assay_tech_relationship')
     def __str__(self):
         return self.short_name
@@ -163,7 +142,7 @@ class AssayTech(models.Model):
 class AssayType(models.Model):
     short_name = models.CharField(max_length=50, unique=True)
     description = models.CharField(max_length=250)
-    comments = models.ManyToManyField(Comment, blank=True);
+    comments = models.CharField(max_length=255, blank=True)
     projects = models.ManyToManyField("Project", blank=True, through="project_assay_type", related_name='projects_assay_type_relationship')
     def __str__(self):
         return self.short_name
@@ -174,8 +153,8 @@ class Publication(models.Model):
     short_name = models.CharField(max_length=50, unique=True)
     title = models.CharField(max_length=250, blank=True)
     pmid = models.CharField(max_length=16)
+    comments = models.CharField(max_length=255, blank=True)
     doi = models.CharField(max_length=32)
-    comments = models.ManyToManyField(Comment, blank=True);
     def __str__(self):
         return self.short_name
     class Meta:
@@ -184,26 +163,52 @@ class Publication(models.Model):
 class EffortType(models.Model):
     short_name = models.CharField(max_length=50, unique=True)
     description = models.CharField(max_length=255)
-    comments = models.ManyToManyField(Comment, blank=True);
+    comments = models.CharField(max_length=255, blank=True)
     def __str__(self):
         return self.short_name
     class Meta:
        verbose_name = 'Wrangler effort type'
 
+class SoftwareDeveloper(models.Model):
+    who = models.ForeignKey(Contributor, on_delete=models.PROTECT)
+    favorite_languages = models.CharField(max_length=255)
+    comments = models.CharField(max_length=255, blank=True)
+    def __str__(self):
+        return self.who.__str__()
+    class Meta:
+       verbose_name = 'Wrangler software developer'
+
+class Curator(models.Model):
+    who = models.ForeignKey(Contributor, on_delete=models.PROTECT)
+    advisor = models.ForeignKey(Contributor, null=True, on_delete=models.SET_NULL, related_name='advisor')
+    interests = models.CharField(max_length=128, blank=True)
+    comments = models.CharField(max_length=255, blank=True)
+    def __str__(self):
+        return self.who.__str__()
+
+class Wrangler(models.Model):
+    who = models.ForeignKey(Contributor, on_delete=models.PROTECT)
+    favorite_site = models.URLField(null=True,blank=True)
+    comments = models.CharField(max_length=255, blank=True)
+    def __str__(self):
+        return self.who.__str__()
+
 class Project(models.Model):
     short_name = models.CharField(max_length=80)
     cur_state = models.ForeignKey(ProjectState, blank=True, null=True, default=None, on_delete=models.SET_NULL, related_name="cur_state")
+    comments = models.CharField(max_length=255, blank=True)
     state_reached = models.ForeignKey(ProjectState, blank=True, null=True, default=None, on_delete=models.SET_NULL, related_name="state_reached")
     stars = models.IntegerField(blank=True,validators=[MinValueValidator(1),MaxValueValidator(5)], default=3)
-    wrangler1 = models.ForeignKey(Contributor, blank=True, null=True, default=None, on_delete=models.SET_NULL, related_name="wrangler1")
-    wrangler2 = models.ForeignKey(Contributor, blank=True, null=True, default=None, on_delete=models.SET_NULL, related_name="wrangler2")
+    chat_url = models.URLField(blank=True, null=True)
+    wrangler1 = models.ForeignKey(Wrangler, blank=True, null=True, default=None, on_delete=models.SET_NULL, related_name="wrangler1");
+    wrangler2 = models.ForeignKey(Wrangler, blank=True, null=True, default=None, on_delete=models.SET_NULL, related_name="wrangler2");
     contacts = models.ManyToManyField(Contributor, blank=True, related_name="projcontacts")
     first_contact_date = models.DateField(blank=True, null=True, default=None)
     last_contact_date = models.DateField(blank=True, null=True, default=None)
     responders = models.ManyToManyField(Contributor, blank=True, related_name="projresponders")
     first_response_date = models.DateField(blank=True, null=True, default=None)
     last_response_date = models.DateField(blank=True, null=True, default=None)
-    questionnaire = models.FileField(upload_to="uploads/project", blank=True, null=True, default=None)
+    questionnaire_comments = models.CharField(max_length=255, blank=True)
     questionnaire_date = models.DateField(blank=True, null=True, default=None)
     tAndC = models.FileField(upload_to="uploads/project", blank=True, null=True, default=None)
     tAndC_date = models.DateField(blank=True, null=True, default=None)
@@ -211,14 +216,18 @@ class Project(models.Model):
     sheet_template_date = models.DateField(blank=True, null=True, default=None)
     sheet_from_lab = models.FileField(upload_to="uploads/project", blank=True, null=True, default=None)
     sheet_from_lab_date = models.DateField(blank=True, null=True, default=None)
+    curator_assigned = models.ForeignKey(Curator, blank=True, null=True, default=None, on_delete=models.SET_NULL)
+    curator_assigned_date = models.DateField(blank=True, null=True, default=None)
     sheet_curated = models.FileField(upload_to="uploads/project", blank=True, null=True, default=None)
     sheet_curated_date = models.DateField(blank=True, null=True, default=None)
+    review_comments = models.CharField(max_length=255, blank=True)
+    review_accepted_date = models.DateField(blank=True, null=True, default=None)
     sheet_validated = models.FileField(upload_to="uploads/project", blank=True, null=True, default=None)
     sheet_validated_date = models.DateField(blank=True, null=True, default=None)
     staging_area = models.URLField(blank=True, null=True)
     staging_area_date = models.DateField(blank=True, null=True, default=None)
     submit_date = models.DateField(blank=True, null=True, default=None)
-    submit_comments = models.ManyToManyField(Comment, blank=True, related_name="submit_comments");
+    submit_comments = models.CharField(max_length=255, blank=True)
     cloud_date = models.DateField(blank=True, null=True, default=None)
     pipeline_date = models.DateField(blank=True, null=True, default=None)
     orange_date = models.DateField(blank=True, null=True, default=None)
@@ -239,7 +248,6 @@ class Project(models.Model):
     contributors = models.ManyToManyField(Contributor)
     species = models.ManyToManyField(Species, blank=True)
     grants = models.ManyToManyField("Grant", blank=True, through="grant_funded_projects")
-    comments = models.ManyToManyField(Comment, blank=True)
     files = models.ManyToManyField(File, blank=True)
     urls = models.ManyToManyField(Url, blank=True)
     def __str__(self):
@@ -248,7 +256,7 @@ class Project(models.Model):
 class Funder(models.Model):
     short_name = models.CharField(max_length=50)
     description = models.TextField()
-    comments = models.ManyToManyField(Comment, blank=True);
+    comments = models.CharField(max_length=255, blank=True)
     def __str__(self):
        return self.short_name
     class Meta:
@@ -262,7 +270,7 @@ class Grant(models.Model):
     funded_projects = models.ManyToManyField(Project, blank=True)
     funded_labs = models.ManyToManyField(Lab, blank=True)
     funded_contributors = models.ManyToManyField(Contributor, blank=True)
-    comments = models.ManyToManyField(Comment, blank=True);
+    comments = models.CharField(max_length=255, blank=True)
     def __str__(self):
        return self.grant_title
 
