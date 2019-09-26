24780d64efc50758fa6360857db108ae821529bb
Jim Kent <kent@soe.ucsc.edu>
2019-09-25 17:12:26 -0700

    Added doc strings to all classes.  Rearranged things per wranglers linking a bit,  simplifying the wrangling aspects of a project.  Separating wrangling_state from the main project state.

--- src/hca/hcat/mysite/hcat/models.py
+++ src/hca/hcat/mysite/hcat/models.py
@@ -8,6 +8,9 @@ from django.utils import timezone
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 class ContributorType(models.Model):
+    """ Manage a controlled vocabulary that describes the type of 
+    contribution to the project.
+    """
     short_name = models.CharField(max_length=50, unique=True)
     description = models.CharField(max_length=255)
     comments = models.CharField(max_length=255, blank=True)
@@ -17,6 +20,12 @@ class ContributorType(models.Model):
        verbose_name = 'x contributor type'
 
 class Contributor(models.Model):
+    """ Someone who has contributed to a project.  They may or may not
+    be staff with accounts with us.  Many are taken from the GEO
+    or ENA submissions records.  Because addresses and other contact
+    information can subject people to harrassment,  we limit the
+    public access of contributor to nothing more than name.
+    """
     name = models.CharField(max_length=255, unique=True)
     email = models.EmailField(max_length=128, blank=True)
     phone = models.CharField(max_length=32, blank=True)
@@ -35,9 +44,15 @@ class Contributor(models.Model):
        return self.name
 
 class Lab(models.Model):
+    """ A lab is kind of a vague concept. Some labs are huge and have internal
+    sublab structures.  Some are tiny.  Some last a long time, others are
+    fleeting.  People go back and forth between labs and can belong to more
+    than one lab at a time.  The relationship between lab and project is also
+    many to many.  In spite of all of this vagueness, in scientific databases
+    it is a decent way to group together people.
+    """
     short_name =  models.CharField(max_length=50, unique=True)
     institution = models.CharField(max_length=255, null=True, blank=True)
-    #contacts = models.ManyToManyField(Contributor, related_name="contacts")
     pi = models.ForeignKey(Contributor, blank=True, null=True, default=None, on_delete=models.SET_NULL, related_name="pi")
     contributors = models.ManyToManyField(Contributor, blank=True, related_name="contributors")
     projects = models.ManyToManyField("Project", blank=True, through="project_labs")
@@ -47,15 +62,21 @@ class Lab(models.Model):
        return self.short_name
 
 class Species(models.Model):
-     common_name = models.CharField(max_length = 40)
-     scientific_name = models.CharField(max_length=150)
-     ncbi_taxon = models.IntegerField(unique=True)
-     def __str__(self):
+    """ This list is not meant to be inclusive.  In fact it takes something close
+    to superuser status to add a new species.  Most of our data is human and mouse.
+    """
+    common_name = models.CharField(max_length = 40)
+    scientific_name = models.CharField(max_length=150)
+    ncbi_taxon = models.IntegerField(unique=True)
+    def __str__(self):
        return self.common_name
-     class Meta:
+    class Meta:
        verbose_name_plural = 'x species'
 
 class Url(models.Model):
+    """ These are URLs that are significant enough we want to track and describe.
+    They can be associated with a project.
+    """
     short_name = models.CharField(unique=True, max_length=50)
     path = models.URLField()
     description = models.CharField(max_length=255)
@@ -66,6 +87,9 @@ class Url(models.Model):
        verbose_name = 'x url'
 
 class File(models.Model):
+    """ These are files that are significant enough we want to track and describe.
+    They can be associated with a project.
+    """
     short_name = models.CharField(unique=True, max_length=80)
     file = models.FileField(upload_to="uploads")
     description = models.CharField(max_length=255)
@@ -76,14 +100,29 @@ class File(models.Model):
        verbose_name = 'x file'
 
 class ProjectState(models.Model):
+    """ High level project state.  Covers wrangling through analysis and interpretation """
     state = models.CharField(max_length=30, unique=True)
-    description = models.CharField(max_length=100)
+    description = models.CharField(max_length=150)
     def __str__(self):
        return self.state
     class Meta:
        verbose_name = 'x project state'
 
+class WranglingState(models.Model):
+    """ State within wrangling.  Covers from primary wrangler's first sight to successful 
+    submission and perhaps through multiple updates
+    """
+    state = models.CharField(max_length=30, unique=True)
+    description = models.CharField(max_length=150)
+    def __str__(self):
+       return self.state
+    class Meta:
+       verbose_name = 'x wrangling state'
+
 class OrganPart(models.Model):
+    """ This is an informal notion of something smaller than an organ.  Typically this
+    will need some curating to find the right ontology and term.
+    """
     short_name = models.CharField(max_length=50, unique=True)
     ontology_id = models.CharField(max_length=32, blank=True, null=True)
     ontology_label = models.CharField(max_length=255, blank=True, null=True)
@@ -96,6 +135,10 @@ class OrganPart(models.Model):
        verbose_name = 'x organ part'
 
 class Organ(models.Model):
+    """ This is an organ or a an term of similar level from an anatomical ontology.
+    The number of organs is small enough that typically the ontology_id and _label
+    fields are filled in or will be shortly by the wranglers.
+    """
     short_name = models.CharField(max_length=50, unique=True)
     ontology_id = models.CharField(max_length=32, blank=True, null=True)
     ontology_label = models.CharField(max_length=255, blank=True, null=True)
@@ -109,9 +152,13 @@ class Organ(models.Model):
        verbose_name = 'x organ'
     
 class Disease(models.Model):
+    """ Something that describes the disease (or health) status of a sample or the 
+    organism that it came from.  
+    """
     short_name = models.CharField(max_length=50, unique=True)
     description = models.CharField(max_length=255)
-    projects = models.ManyToManyField("Project", blank=True, through="project_disease", related_name='projects_diseases_relationship')
+    projects = models.ManyToManyField("Project", blank=True, through="project_disease", 
+        related_name='projects_diseases_relationship')
     comments = models.CharField(max_length=255, blank=True)
     def __str__(self):
         return self.short_name
@@ -119,6 +166,10 @@ class Disease(models.Model):
        verbose_name = 'x disease'
 
 class Consent(models.Model):
+    """ This Concent class is a controlled vocabulary that describes broadly the justification
+    for thinking we have the right to share the data with anyone.  This does not
+    go into or include the specific consent agreement when human subjects are involved.
+    """
     short_name = models.CharField(max_length=50, unique=True)
     description = models.CharField(max_length=255)
     comments = models.CharField(max_length=255, blank=True)
@@ -128,6 +179,10 @@ class Consent(models.Model):
        verbose_name = 'x consent'
 
 class SampleType(models.Model):
+    """ The SampleType is a broad classification of how fresh the sample was when delivered to
+    the sequencing or imaging assay process.  This also distinguishes between cultured and
+    in vivo samples.
+    """
     short_name = models.CharField(max_length=50, unique=True)
     description = models.CharField(max_length=255)
     comments = models.CharField(max_length=255, blank=True)
@@ -137,37 +192,37 @@ class SampleType(models.Model):
        verbose_name = 'x sample type'
 
 class AssayTech(models.Model):
+    """ AssayTech describes the technology used to make the cell-by-cell (or bulk) measurements.
+    This typically includes a combination of device, chemistry, and software.  At the
+    biochemical level when doing RNA sequencing it corresponds to the cDNA library preparation.
+    """
     short_name = models.CharField(max_length=50, unique=True)
     description = models.CharField(max_length=255)
     comments = models.CharField(max_length=255, blank=True)
-    projects = models.ManyToManyField("Project", blank=True, through="project_assay_tech", related_name='projects_assay_tech_relationship')
+    projects = models.ManyToManyField("Project", blank=True, 
+        through="project_assay_tech", related_name='projects_assay_tech_relationship')
     def __str__(self):
         return self.short_name
     class Meta:
        verbose_name = 'x assay tech'
 
-class AssayType(models.Model):
-    short_name = models.CharField(max_length=50, unique=True)
-    description = models.CharField(max_length=255)
-    comments = models.CharField(max_length=255, blank=True)
-    projects = models.ManyToManyField("Project", blank=True, through="project_assay_type", related_name='projects_assay_type_relationship')
-    def __str__(self):
-        return self.short_name
-    class Meta:
-       verbose_name = 'x assay type'
-
 class Publication(models.Model):
+    """ Most often a publication is an article in a scientific journal. """
     short_name = models.CharField(max_length=50, unique=True)
     title = models.CharField(max_length=255, blank=True)
-    pmid = models.CharField(max_length=16)
+    pmid = models.CharField(max_length=16, blank=True)
     comments = models.CharField(max_length=255, blank=True)
-    doi = models.CharField(max_length=32)
+    doi = models.CharField(max_length=32, blank=True)
     def __str__(self):
         return self.short_name
     class Meta:
        verbose_name = 'x publication'
 
 class EffortType(models.Model):
+    """ Distinguishes high level classes of effort such as importing data from the
+    scientific literature vs. helping labs organize, understand, and share
+    prepublication effort.
+    """
     short_name = models.CharField(max_length=50, unique=True)
     description = models.CharField(max_length=255)
     comments = models.CharField(max_length=255, blank=True)
@@ -177,6 +232,7 @@ class EffortType(models.Model):
        verbose_name = 'x effort type'
 
 class SoftwareDeveloper(models.Model):
+    """ Software developers contributing to the project. """
     who = models.ForeignKey(Contributor, on_delete=models.PROTECT)
     favorite_languages = models.CharField(max_length=255)
     comments = models.CharField(max_length=255, blank=True)
@@ -186,6 +242,7 @@ class SoftwareDeveloper(models.Model):
        verbose_name = 'x software developer'
 
 class Intern(models.Model):
+    """ Interns learning from the project. """
     who = models.ForeignKey(Contributor, on_delete=models.PROTECT)
     advisor = models.ForeignKey(Contributor, null=True, on_delete=models.SET_NULL, related_name='advisor')
     interests = models.CharField(max_length=128, blank=True)
@@ -196,14 +253,24 @@ class Intern(models.Model):
        verbose_name = 'x intern'
 
 class Wrangler(models.Model):
+    """ A wrangler helps get projects from lab into database where they can be shared
+    with others. 
+    """
     who = models.ForeignKey(Contributor, on_delete=models.PROTECT)
     comments = models.CharField(max_length=255, blank=True)
     def __str__(self):
         return self.who.__str__()
 
 class Project(models.Model):
+    """ A project, like a lab, is a somewhat vague concept that is still useful to describe
+    scientific work.  Typically a project is the work of a single lab or a small group of
+    closely collaborating labs.  Some projects will be focused around a particular organ.
+    Others will be focused on technologies or on different states of the same cell type.
+    Very often a successful project will be associated with a publication, or a record in
+    one of the public gene expression or sequencing databases.
+    """
     short_name = models.CharField(max_length=80)
-    cur_state = models.ForeignKey(ProjectState, blank=True, null=True, default=None, on_delete=models.SET_NULL, related_name="cur_state")
+    wrangling_state = models.ForeignKey(WranglingState, blank=True, null=True, default=None, on_delete=models.SET_NULL, related_name="cur_state")
     comments = models.CharField(max_length=255, blank=True)
     state_reached = models.ForeignKey(ProjectState, blank=True, null=True, default=None, on_delete=models.SET_NULL, related_name="state_reached")
     stars = models.IntegerField(blank=True,validators=[MinValueValidator(1),MaxValueValidator(5)], default=3)
@@ -213,9 +280,6 @@ class Project(models.Model):
     contacts = models.ManyToManyField(Contributor, blank=True, related_name="projcontacts")
     first_contact_date = models.DateField(blank=True, null=True, default=None)
     last_contact_date = models.DateField(blank=True, null=True, default=None)
-    responders = models.ManyToManyField(Contributor, blank=True, related_name="projresponders")
-    first_response_date = models.DateField(blank=True, null=True, default=None)
-    last_response_date = models.DateField(blank=True, null=True, default=None)
     questionnaire_comments = models.CharField(max_length=255, blank=True)
     questionnaire_date = models.DateField(blank=True, null=True, default=None)
     tAndC_comments = models.CharField(max_length=255, blank=True)
@@ -245,7 +309,6 @@ class Project(models.Model):
     consent = models.ForeignKey(Consent, blank=True, null=True, default=None, on_delete=models.SET_NULL)
     effort = models.ForeignKey(EffortType, blank=True, null=True, default=None, on_delete=models.SET_NULL)
     origin_name = models.CharField(max_length=200, blank=True)
-    assay_type = models.ManyToManyField(AssayType, blank=True)
     assay_tech = models.ManyToManyField(AssayTech, blank=True)
     cells_expected = models.IntegerField(blank=True, default=0)
     publications = models.ManyToManyField(Publication, blank=True)
@@ -258,6 +321,10 @@ class Project(models.Model):
        return self.short_name
 
 class Tracker(models.Model):
+    """ In Tuatara most of the weight of a project is presubmission.  Happily things don't
+    stop there.  The tracker class just extracts some of the easier to understand fields
+    from Parth's tracker.
+    """
     project = models.OneToOneField(Project, on_delete=models.CASCADE, primary_key=True)
     uuid = models.CharField(max_length=37, unique=True)
     submission_id = models.CharField(max_length=33, unique=True)
@@ -276,6 +343,9 @@ class Tracker(models.Model):
        verbose_name_plural = 'Tracking after submission'
 
 class Funder(models.Model):
+    """ It's possible that CZI will ask us to track grants, and other funders might as
+    well.  This class is here as a placeholder for that need.
+    """
     short_name = models.CharField(max_length=50)
     description = models.TextField()
     comments = models.CharField(max_length=255, blank=True)
@@ -286,6 +356,9 @@ class Funder(models.Model):
 
 
 class Grant(models.Model):
+    """ It's possible that CZI will ask us to track grants, and other funders might as
+    well.  This class is here as a placeholder for that need.
+    """
     grant_title = models.CharField(max_length=200)
     grant_id = models.CharField(max_length=200)
     funder = models.ForeignKey(Funder, null=True, default=None, on_delete=models.SET_NULL)
