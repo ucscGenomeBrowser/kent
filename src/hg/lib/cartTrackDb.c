ucscGenomeBrowser/kent

commit f87fbb580a1a393a2b5c8688780b972e3bb672df

Instead of warn() about invalid group, which messes up output of hgTables & hgIntegrator, fix group and fprintf(stderr).  refs #25003, #25016

src/hg/lib/cartTrackDb.c
@@ -79,6 +79,30 @@ struct hash *groupsInTrackList = newHash(0);
 struct hash *groupsInDatabase = newHash(0);
 struct trackDb *track;
 
+/* Do some error checking for tracks with group names that are not in database.
+ * Warnings at this stage mess up CGIs that may produce text output like hgTables & hgIntegrator,
+ * so don't warn, just put CTs in group user and others in group x. */
+groupsAll = hLoadGrps(db);
+if (!trackHubDatabase(db))
+    {
+    struct hash *allGroups = hashNew(0);
+    for (group = groupsAll;  group != NULL; group = group->next)
+        hashAdd(allGroups, group->name, group);
+    for (track = trackList; track != NULL; track = track->next)
+        {
+        if (!hashLookup(allGroups, track->grp))
+            {
+            fprintf(stderr, "Track %s has group %s, which isn't in grp table\n",
+                    track->table, track->grp);
+            if (isCustomTrack(track->track))
+                track->grp = cloneString("user");
+            else
+                track->grp = cloneString("x");
+            }
+        }
+    hashFree(&allGroups);
+    }
+
 /* Stream through track list building up hash of active groups. */
 for (track = trackList; track != NULL; track = track->next)
     {
@@ -87,7 +111,6 @@ for (track = trackList; track != NULL; track = track->next)
     }
 
 /* Scan through group table, putting in ones where we have data. */
-groupsAll = hLoadGrps(db);
 for (group = slPopHead(&groupsAll); group != NULL; group = slPopHead(&groupsAll))
     {
     if (hashLookup(groupsInTrackList, group->name))
@@ -122,16 +145,6 @@ for (group = slPopHead(pHubGrpList); group != NULL; group = slPopHead(pHubGrpLis
     else
 	slAddHead(&groupList, newGrp);
     hashAdd(groupsInDatabase, newGrp->name, newGrp);
-    }
-
-/* Do some error checking for tracks with group names that are
- * not in database.  Just warn about them. */
-if (!trackHubDatabase(db))
-    for (track = trackList; track != NULL; track = track->next)
-    {
-    if (!hashLookup(groupsInDatabase, track->grp))
-         warn("Track %s has group %s, which isn't in grp table",
-	 	track->table, track->grp);
     }
 
 /* Create dummy group for all tracks. */

commit 37dde06377a1daa9d86150ed431b89c49c699e10

fix a problem introduced by f87fbb58.   Track hub tracks are in a group called after the hub name (eg hub_#).  Refs #25050

src/hg/lib/cartTrackDb.c
@@ -90,7 +90,8 @@ if (!trackHubDatabase(db))
         hashAdd(allGroups, group->name, group);
     for (track = trackList; track != NULL; track = track->next)
         {
-        if (!hashLookup(allGroups, track->grp))
+        /* If track isn't in a track up, and has a group we don't know about, change it to one we do. */
+        if (!startsWith("hub_", track->grp) && !hashLookup(allGroups, track->grp))
             {
             fprintf(stderr, "Track %s has group %s, which isn't in grp table\n",
                     track->table, track->grp);