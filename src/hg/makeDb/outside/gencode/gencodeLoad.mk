b985a10e714b3fad02117a0eeba5a6b36bd0ca59
Mark Diekhans <markd@ucsc.edu>
2020-04-29 12:53:43 -0700

    loaded gencodeV34 final release

--- src/hg/makeDb/outside/gencode/gencodeLoad.mk
+++ src/hg/makeDb/outside/gencode/gencodeLoad.mk
@@ -35,11 +35,11 @@ mach = $(shell uname -m)
 # - ensemblPrevVersion is use to get chrom name mappings for pre-release,
 #   as this doesn't change between release.
 ##
-#db = hg38
-db = hg19
+db = hg38
+#db = hg19
 #db = mm10
-#preRelease = no
-preRelease = yes
+preRelease = no
+#preRelease = yes
 ifeq (${db},mm10)
     grcRefAssembly = GRCm38
     ver = M25
