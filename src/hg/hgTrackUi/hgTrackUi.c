3bc237f2fea899a81e6f06bacd111b1e2c8a1a71
Kate Rosenbloom <kate@soe.ucsc.edu>
2021-04-14 08:34:37 -0700

    Adjust HTML to work with description summary on supertrack member track UI. refs #24948

--- src/hg/hgTrackUi/hgTrackUi.c
+++ src/hg/hgTrackUi/hgTrackUi.c
@@ -3112,6 +3112,7 @@ if (tdbParent->html)
     printf("<p><table>");  // required by jsCollapsible
     jsBeginCollapsibleSectionFontSize(cart, tdb->track, "superDescription", "Description", FALSE,
                                             "medium");
+    // TODO: better done with regex
     char *html = replaceChars(tdbParent->html, "<H", "<h");
     html = replaceChars(html, "</H", "</h");
 
