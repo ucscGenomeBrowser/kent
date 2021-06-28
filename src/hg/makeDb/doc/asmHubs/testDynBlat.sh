9e3ba7e02f241e806c6a656329ea8aee730d000b
Hiram Clawson <hiram@soe.ucsc.edu>
2021-06-28 15:09:07 -0700

    indicate error when dynamic blat does not respond refs #27556

--- src/hg/makeDb/doc/asmHubs/testDynBlat.sh
+++ src/hg/makeDb/doc/asmHubs/testDynBlat.sh
@@ -20,11 +20,16 @@ export srcDir="/hive/data/genomes/asmHubs/$dataDir"
 export faaFile="/cluster/home/hiram/kent/src/hg/makeDb/doc/asmHubs/rtp1.ace2.faa.gz"
 
 time (gfClient -genome=$asmId -genomeDataDir=$dataDir -t=dnax -q=prot \
-  $blatHost $blatPort $srcDir $faaFile stdout > $asmId.rtp1.ace2.psl) 2> $asmId.dynBlat.log
+  $blatHost $blatPort $srcDir $faaFile stdout > $asmId.rtp1.ace2.psl || true) 2> $asmId.dynBlat.log
 
+export errCount=`grep -w error $asmId.dynBlat.log | wc -l`
 export realTime=`grep -w real $asmId.dynBlat.log | awk '{print $NF}'`
 export resultLines=`pslScore $asmId.rtp1.ace2.psl | wc -l`
-printf "%s\t%d\t%s\n" "${asmId}" "${resultLines}" "${realTime}"
+if [ "${errCount}" -gt 0 ]; then
+  printf "%s\t%d\t%s\tERROR\n" "${asmId}" "${resultLines}" "${realTime}"
+else
+  printf "%s\t%d\t%s\n" "${asmId}" "${resultLines}" "${realTime}"
+fi
 rm -f $asmId.rtp1.ace2.psl $asmId.dynBlat.log
 
 exit $?
