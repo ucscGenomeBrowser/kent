46cacb52f48b67dbdbb131af561827f3f5357a20
Brian Raney <braney@soe.ucsc.edu>
2021-08-10 13:20:23 -0700

    add utility to make a cartEdit that supports movinb existing tracks to a superTrack .  Add a new cartEdit1 to move a bunch of tracks to three new supertracks

--- src/hg/lib/cartRewrite.c
+++ src/hg/lib/cartRewrite.c
@@ -9,157 +9,19 @@
 #include "cart.h"
 #include "hgConfig.h"
 
-static char *edit0Mm10tracks[] =
-{
-"snp142",
-"snp142Mult",
-"snp138",
-"snp138Common",
-"snp138Mult",
-"snp137Common",
-"snp137Mult",
-"snp137",
-};
+void cartEdit0(struct cart *cart);
+void cartEdit1(struct cart *cart);
 
-static char *edit0Hg19tracks[] =
-{
-"snp151Flagged",
-"snp151Mult",
-"snp150Mult",
-"snp150Flagged",
-"snp150",
-"snp150Common",
-"snp149Mult",
-"snp149Flagged",
-"snp149",
-"snp149Common",
-"snp147Flagged",
-"snp147Mult",
-"snp147Common",
-"snp147",
-"snp146Mult",
-"snp146Flagged",
-"snp146",
-"snp146Common",
-"snp144Mult",
-"snp144Flagged",
-"snp144Common",
-"snp144",
-"snp142Mult",
-"snp142Flagged",
-"snp142Common",
-"snp142",
-"snp141Flagged",
-"snp141Common",
-"snp141",
-"snp138Flagged",
-"snp138Mult",
-"snp138",
-"snp138Common",
-"snp137Common",
-"snp137Flagged",
-"snp137Mult",
-"snp137",
-"snp135Common",
-"snp135Flagged",
-"snp135Mult",
-"snp135",
-"snp132Common",
-"snp132Flagged",
-"snp132Mult",
-"snp132",
-"snp131",
-"snp130",
-};
-
-static char *edit0Hg38tracks[] =
-{
-"snp151",
-"snp151Common",
-"snp151Flagged",
-"snp151Mult",
-"snp150Mult",
-"snp150",
-"snp150Common",
-"snp150Flagged",
-"snp149Mult",
-"snp149",
-"snp149Common",
-"snp149Flagged",
-"snp141Mult",
-"snp141Flagged",
-"snp141Common",
-"snp141",
-"snp142Mult",
-"snp142Flagged",
-"snp142Common",
-"snp142",
-"snp144Mult",
-"snp144Flagged",
-"snp144Common",
-"snp144",
-"snp147Mult",
-"snp147Flagged",
-"snp147Common",
-"snp147",
-"snp146Mult",
-"snp146Flagged",
-"snp146Common",
-"snp146",
-};
-
-static void turnOnSuper(struct cart *cart, char **trackNames, unsigned numTracks, char *superTrackName)
-{
-boolean turnOnSuper = FALSE;
-int ii;
-
-// go through all the tracks moved into the supertrack
-for(ii = 0; ii < numTracks; ii++)
-    {
-    char *vis = cartOptionalString(cart, trackNames[ii]);
-
-    if (vis)
-        {
-        if (differentString(vis, "hide"))
-            {
-            // Turn on the super track since one of its subtracks was visible before
-            turnOnSuper = TRUE;
-            break;
-            }
-        }
-    }
-
-if (turnOnSuper)
-    cartSetString(cart, superTrackName, "show");
-}
-
-static void cartEdit0(struct cart *cart)
-/* Moving a bunch of SNP tracks to an archive super track.   We need
- * to turn on the super track if any of what are now subTracks are visible.
- */
-{
-// hg38 tracks
-int length = ArraySize(edit0Hg38tracks);
-turnOnSuper(cart, edit0Hg38tracks, length, "dbSnpArchive");
-
-// mm10 tracks
-length = ArraySize(edit0Mm10tracks);
-turnOnSuper(cart, edit0Mm10tracks, length, "dbSnpArchive");
-
-// hg19 tracks
-length = ArraySize(edit0Hg19tracks);
-turnOnSuper(cart, edit0Hg19tracks, length, "dbSnpArchive");
-}
-
-// Here's the list of cart rewrite functions
 struct cartRewrite
 {
 void (*func)(struct cart *cart);
 };
 
+// Here's the list of cart rewrite functions
 static struct cartRewrite cartRewrites[] =
 {
 { cartEdit0},
+{ cartEdit1},
 };
 
 void cartRewrite(struct cart *cart, unsigned trackDbCartVersion, unsigned cartVersion)
@@ -184,3 +46,30 @@ for(; cartVersion < trackDbCartVersion; cartVersion++)
 cartSetVersion(cart, cartVersion);
 }
 
+void cartTurnOnSuper(struct cart *cart, char **trackNames, unsigned numTracks, char *superTrackName)
+/* Turn on a supertrack if any of the subtracks are not hidden.  ASSUMES ALL TRACKS ARE HIDDEN
+ * by default.
+ */
+{
+boolean cartTurnOnSuper = FALSE;
+int ii;
+
+// go through all the tracks moved into the supertrack
+for(ii = 0; ii < numTracks; ii++)
+    {
+    char *vis = cartOptionalString(cart, trackNames[ii]);
+
+    if (vis)
+        {
+        if (differentString(vis, "hide"))
+            {
+            // Turn on the super track since one of its subtracks was visible before
+            cartTurnOnSuper = TRUE;
+            break;
+            }
+        }
+    }
+
+if (cartTurnOnSuper)
+    cartSetString(cart, superTrackName, "show");
+}
---
+++ src/hg/utils/cartEditSuperTrack/cartEditSuperTrack.c
@@ -0,0 +1,100 @@
+/* cartEditSuperTrack - make a cart edit to put tracks into a supertrack. */
+#include "common.h"
+#include "linefile.h"
+#include "hash.h"
+#include "options.h"
+
+void usage()
+/* Explain usage and exit. */
+{
+errAbort(
+  "cartEditSuperTrack - make a cart edit to put tracks into a supertrack\n"
+  "usage:\n"
+  "   cartEditSuperTrack tracks.txt cartVersion#\n"
+  "where tracks.txt is a tab-separated two column file with a list of tracks and the supertrack they're moving to.\n"
+  "options:\n"
+  "   -xxx=XXX\n"
+  );
+}
+
+/* Command line validation table. */
+static struct optionSpec options[] = {
+   {NULL, 0},
+};
+
+static void writeFile(FILE *f, struct hash *superHash, char *version)
+{
+struct dyString *varDeclare = newDyString(4096);
+struct dyString *editFunc = newDyString(4096);
+
+dyStringPrintf(varDeclare,
+"#include \"common.h\"\n"
+"#include \"cart.h\"\n"
+"\n");
+
+dyStringPrintf(editFunc,
+"void cartEdit%s(struct cart *cart)\n"
+"{\n", version);
+
+struct hashCookie cookie = hashFirst(superHash);
+struct hashEl *hel;
+while ((hel = hashNext(&cookie)) != NULL)
+    {
+    char name[4096];
+
+    safef(name, sizeof name, "edit%s%sTracks", version, (char *)hel->name);
+
+    dyStringPrintf(editFunc, "cartTurnOnSuper(cart, %s,  ArraySize(%s),  \"%s\");\n", name, name, hel->name);
+
+    dyStringPrintf(varDeclare, "static char *%s[] =\n{\n", name);
+
+    struct hashCookie cookie2 = hashFirst((struct hash *)hel->val);
+    struct hashEl *hel2;
+    while ((hel2 = hashNext(&cookie2)) != NULL)
+        {
+        dyStringPrintf(varDeclare, "\"%s\",\n", hel2->name);
+        }
+    dyStringPrintf(varDeclare, "};\n\n");
+    }
+dyStringPrintf(editFunc, "}\n");
+fputs(varDeclare->string, f);
+fputs(editFunc->string, f);
+fclose(f);
+}
+
+void cartEditSuperTrack(char *input, char *versionNumber, char *outFile)
+/* cartEditSuperTrack - make a cart edit to put tracks into a supertrack. */
+{
+FILE *f = mustOpen(outFile, "w");
+struct lineFile *lf = lineFileOpen(input, TRUE);
+char *row[2];
+struct hash *superHash = newHash(5);
+
+while (lineFileRow(lf, row))
+    {
+    struct hash *trackHash = newHash(5);
+    struct hashEl *hel;
+
+    if ((hel = hashLookup(superHash, row[1])) == NULL)
+        {
+        trackHash = newHash(5);
+        hashAdd(superHash, row[1], trackHash);
+        }
+    else
+        trackHash = hel->val;
+
+    hashStore(trackHash, row[0]);
+    }
+
+writeFile(f, superHash, versionNumber);
+}
+
+int main(int argc, char *argv[])
+/* Process command line. */
+{
+optionInit(&argc, argv, options);
+if (argc != 4)
+    usage();
+cartEditSuperTrack(argv[1], argv[2], argv[3]);
+return 0;
+}
