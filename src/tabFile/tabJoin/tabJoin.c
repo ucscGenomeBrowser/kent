4604bbe49ece6900be69ff88f34522738dafa36c
Jim Kent <kent@soe.ucsc.edu>
2021-11-29 12:24:50 -0800

    Improving error messages.

--- src/tabFile/tabJoin/tabJoin.c
+++ src/tabFile/tabJoin/tabJoin.c
@@ -32,7 +32,7 @@ void tabJoin(char *fileName1, char *fieldName1, char *fileName2, char *fieldName
 struct fieldedTable *table1 = fieldedTableFromTabFile(fileName1, fileName1, &fieldName1, 1);
 struct fieldedTable *table2 = fieldedTableFromTabFile(fileName2, fileName2, &fieldName2, 1);
 if (table1->rowCount != table2->rowCount)
-    errAbort("%s has %d rows but %s has %d.  They must have same number of rows.\n", 
+    errAbort("%s has %d rows but %s has %d.  They must have same number of rows.", 
 	fileName1, table1->rowCount, fileName2, table2->rowCount);
 struct hash *hash1 = fieldedTableUniqueIndex(table1, fieldName1);
 struct hash *hash2 = fieldedTableUniqueIndex(table2, fieldName2);
@@ -63,8 +63,8 @@ for (row1 = table1->rowList; row1 != NULL; row1 = row1->next)
     char *key = row1->row[fieldIx1];
     struct fieldedRow *row2 = hashFindVal(hash2, key);
     if (row2 == NULL)
-        errAbort("%s is found in %s.%s  but not %s.%s", key, fileName1, fieldName1,
-	    fileName2, fieldName2);
+        errAbort("%s is found in %s field of %s  but not %s field of %s", key, fieldName1, fileName1, 
+	    fieldName2, fileName2);
 
     /* Write out data from table1 */
     for (i=0; i<table1->fieldCount; ++i)
