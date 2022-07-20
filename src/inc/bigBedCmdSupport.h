04b879361227c44e9602945464b83e5346b5dd4f
Mark Diekhans <markd@ucsc.edu>
2022-07-19 22:38:07 -0700

    add option to output autoSql style header to bigBedNamedItems

---
+++ src/inc/bigBedCmdSupport.h
@@ -0,0 +1,13 @@
+/* bigBedCmdSupport - functions to support writing bigBed related commands. */
+
+/* Copyright (C) 2022 The Regents of the University of California 
+ * See kent/LICENSE or http://genome.ucsc.edu/license/ for licensing information. */
+
+#ifndef bigBedCmdSupport_h
+#define bigBedCmdSupport_h
+#include "bigBed.h"
+
+void bigBedCmdOutputHeader(struct bbiFile *bbi, FILE *f);
+/* output a header from the autoSql in the file */
+
+#endif
