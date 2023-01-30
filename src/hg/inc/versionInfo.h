2c16cc726ba2e25098de6f3e16bd88ead5a192f0
Build Meister <chmalee@ucsc.edu>
2023-01-30 10:14:32 -0800

    New version number v443

--- src/hg/inc/versionInfo.h
+++ src/hg/inc/versionInfo.h
@@ -1,3 +1,3 @@
 /* Copyright (C) 2014 The Regents of the University of California 
  * See README in this or parent directory for licensing information. */
-#define CGI_VERSION "442"
+#define CGI_VERSION "443"
