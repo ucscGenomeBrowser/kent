eea1ea7511ae244feb7c92616d0d463896dc851d
Hiram Clawson <hiram@soe.ucsc.edu>
2023-02-02 13:22:59 -0800

    send assemblyTable and requestTable listings out to hgdownload no redmine

--- src/hg/gar/rebuild.sh
+++ src/hg/gar/rebuild.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-
-
 set -beEu -o pipefail
 
 if [ $# -ne 1 ]; then
@@ -31,5 +29,12 @@ time $srcDir/garTable.sh > next.html 2> ${dateStamp}.log
 rm -f index.html
 mv next.html index.html
 chmod +x index.html
+rm -f assemblyTable.txt requestTable.txt
+for F in `ls -rt *.tableData.txt`
+do
+   awk -F$'\t' '$2 == "view"' "${F}" | cut -f1,3- >> assemblyTable.txt
+   awk -F$'\t' '$2 == "request"' "${F}" | cut -f1,3- >> requestTable.txt
+done
+scp -p assemblyTable.txt requestTable.txt qateam@hgdownload:/mirrordata/hubs/
 printf "### finished ###\n"
 printf "# %s/index.html\n" "${workDir}"
