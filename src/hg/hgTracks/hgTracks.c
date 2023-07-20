901f8a2f2f4ed337dc849212025c0b20a5370a9a
Max <max@soe.ucsc.edu>
2023-07-20 06:18:30 -0700

    adding timer to make sure that slow performance warning is shown even if page doesnt load, refs #31576

--- src/hg/hgTracks/hgTracks.c
+++ src/hg/hgTracks/hgTracks.c
@@ -9887,7 +9887,8 @@ if (!maxTimeStr)
 
 double maxTime = atof(maxTimeStr);
 struct dyString *dy = dyStringNew(150);
-dyStringPrintf(dy, "$(document).ready( function() { hgtWarnTiming(%f)});\n", maxTime);
+dyStringPrintf(dy, "var warnTimingTimer = setTimeout( function() { hgtWarnTiming(0)}, %f);\n", maxTime);
+dyStringPrintf(dy, "$(document).ready( function() { clearTimeout(warnTimingTimer); hgtWarnTiming(%f)});\n", maxTime);
 jsInline(dy->string);
 dyStringFree(&dy);
 }
