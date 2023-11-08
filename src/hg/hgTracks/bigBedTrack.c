14efd98a0de9c675d4af048e617767371a26131b
Brian Raney <braney@soe.ucsc.edu>
2023-11-08 13:53:28 -0800

    make sure bigBed has a non-null name before checking it's name with hgFind
    (cherry picked from commit a2b2a6404ccc3f54bd6f9e3728c30677c550bef2)

--- src/hg/hgTracks/bigBedTrack.c
+++ src/hg/hgTracks/bigBedTrack.c
@@ -293,7 +293,7 @@ boolean bigBedFilterInterval(char **bedRow, struct bigBedFilter *filters)
 /* Go through a row and filter based on filters.  Return TRUE if all filters are passed. */
 {
 // if this is a hgFind match, it always passed the filters
-if ((hgFindMatches != NULL) && hashLookup(hgFindMatches, bedRow[3]))
+if ((hgFindMatches != NULL) && (bedRow[3] != NULL)  && hashLookup(hgFindMatches, bedRow[3]))
     return TRUE;
 
 struct bigBedFilter *filter;
