36174525f6181ecb2412a076afdf755dc226fdcd
Christopher Lee <chmalee@ucsc.edu>
2024-02-28 14:48:09 -0800

    Do not try to figure out bigBed type number for super tracks, refs #19984

--- src/hg/hgTracks/bigBedTrack.c
+++ src/hg/hgTracks/bigBedTrack.c
@@ -836,7 +836,7 @@ for(ii=0; ii < wordCount; ii++)
     newWords[ii] = words[ii];
 
 // let's help the user out and get the definedFieldCount if they didn't specify it on the type line
-if ((track->tdb->subtracks == NULL) && (wordCount == 1) && sameString(words[0], "bigBed")) 
+if (!tdbIsSuper(track->tdb) && (track->tdb->subtracks == NULL) && (wordCount == 1) && sameString(words[0], "bigBed"))
     {
     int fieldCount = getFieldCount(track);
     if (fieldCount > 3) 
