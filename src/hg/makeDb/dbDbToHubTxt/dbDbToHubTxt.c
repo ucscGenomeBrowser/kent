0aeb65a35dee3f20e536516fa2a824a1f3d7314f
Brian Raney <braney@soe.ucsc.edu>
2024-06-06 09:00:00 -0700

    Revert "in the absence of a 2bit file assume it is a track hub attach to a GenArk hub"
    This reverts commit 8cf5d084511a9bbd9d28edf8f5508d9df1ff98eb.

--- src/hg/makeDb/dbDbToHubTxt/dbDbToHubTxt.c
+++ src/hg/makeDb/dbDbToHubTxt/dbDbToHubTxt.c
@@ -27,37 +27,13 @@ static struct optionSpec options[] = {
 void dbDbToHubTxt(char *db, char *email, char *groups, char *outFile)
 /* dbDbToHubTxt - Reformat dbDb line to hub and genome stanzas for assembly hubs. */
 {
-/* check to see if this is an assembly hub or a track hub */
-char twoBit[PATH_LEN];
-safef(twoBit, sizeof(twoBit), "/gbdb/%s/%s.2bit", db, db);
-boolean trackHub = TRUE;	/* assume trackHub */
-if (fileExists(twoBit))
-   trackHub = FALSE;		/* twoBit file exists, not a trackHub */
-
 struct dbDb *dbDb = hDbDb(db);
 if (dbDb == NULL)
     errAbort("No dbDb entry for %s\n", db);
 
-char genArkHub[PATH_LEN];	/* will need GC accession for trackHub */
-/* expecting sourceName to have the GC accession as last word in parens:
-       sourceName: GRC BN/NHsdMcwi (GCA_036323735.1)
- */
-if (trackHub)
-    {
-    char *longLabel = cloneString(dbDb->sourceName);
-    char *words[1024];
-    int wordCount = chopByWhite(longLabel, words, sizeof(words));
-    stripChar(words[wordCount-1], '(');
-    stripChar(words[wordCount-1], ')');
-    safef(genArkHub, sizeof(genArkHub), "%s", words[wordCount-1]);
-    }
-
 FILE *f = mustOpen(outFile, "w");
 
-if (trackHub)
-    fprintf(f, "hub %s track hub\n", genArkHub);
-else
-    fprintf(f, "hub %s genome assembly\n", db);
+fprintf(f, "hub %s genome assembly\n", db);
 fprintf(f, "shortLabel %s\n", dbDb->description);
 fprintf(f, "longLabel %s\n", dbDb->sourceName);
 fprintf(f, "email %s\n", email);
@@ -65,46 +41,41 @@ fprintf(f, "desriptionUrl %s\n", dbDb->htmlPath);
 fprintf(f, "useOneFile on\n");
 fprintf(f, "\n");
 
-if (trackHub)
-    fprintf(f, "genome %s\n", genArkHub);
-else
+fprintf(f, "genome %s\n", db);
+fprintf(f, "taxId %d\n", dbDb->taxId);
+fprintf(f, "groups %s\n", groups);
+fprintf(f, "description %s\n", dbDb->description);
+fprintf(f, "twoBitPath /gbdb/%s/%s.2bit\n", db, db);
+fprintf(f, "chromAliasBb /gbdb/%s/hubs/chromAlias.bb\n", db);
+fprintf(f, "chromSizes /gbdb/%s/hubs/chromSizes.txt\n", db);
+fprintf(f, "organism %s\n", dbDb->organism);
+fprintf(f, "defaultPos %s\n", dbDb->defaultPos);
+fprintf(f, "scientificName %s\n", dbDb->scientificName);
+fprintf(f, "htmlPath %s\n", dbDb->htmlPath);
+
+//struct blatServerTable *blat = hFindBlatServer(char *db, boolean isTrans)
+struct blatServerTable *blat = hFindBlatServer(db, FALSE);
+
+if (blat)
     {
-    fprintf(f, "genome %s\n", db);
-    fprintf(f, "taxId %d\n", dbDb->taxId);
-    fprintf(f, "groups %s\n", groups);
-    fprintf(f, "description %s\n", dbDb->description);
-    fprintf(f, "twoBitPath /gbdb/%s/%s.2bit\n", db, db);
-    fprintf(f, "chromAliasBb /gbdb/%s/hubs/chromAlias.bb\n", db);
-    fprintf(f, "chromSizes /gbdb/%s/hubs/chromSizes.txt\n", db);
-    fprintf(f, "organism %s\n", dbDb->organism);
-    fprintf(f, "defaultPos %s\n", dbDb->defaultPos);
-    fprintf(f, "scientificName %s\n", dbDb->scientificName);
-    fprintf(f, "htmlPath %s\n", dbDb->htmlPath);
+    if (blat->isDynamic)
+        fprintf(f, "blat %s %s dynamic %s\n", blat->host, blat->port, db);
+    else
+        fprintf(f, "blat %s %s\n", blat->host, blat->port);
+    if (hgPcrOk(db))
+        fprintf(f, "isPcr %s %s dynamic %s\n", blat->host, blat->port, db);
 
-    //struct blatServerTable *blat = hFindBlatServer(char *db, boolean isTrans)
-    struct blatServerTable *blat = hFindBlatServer(db, FALSE);
+    }
 
-    if (blat)
-        {
-        if (blat->isDynamic)
-            fprintf(f, "blat %s %s dynamic %s\n", blat->host, blat->port, db);
-        else
-            fprintf(f, "blat %s %s\n", blat->host, blat->port);
-        if (hgPcrOk(db))
-            fprintf(f, "isPcr %s %s dynamic %s\n", blat->host, blat->port, db);
+// now get transblat
+blat = hFindBlatServer(db, TRUE);
 
-        }
-
-    // now get transblat
-    blat = hFindBlatServer(db, TRUE);
-
-    if (blat)
-        {
-        if (blat->isDynamic)
-            fprintf(f, "transBlat %s %s dynamic %s\n", blat->host, blat->port, db);
-        else
-            fprintf(f, "transBlat %s %s\n", blat->host, blat->port);
-        }
+if (blat)
+    {
+    if (blat->isDynamic)
+        fprintf(f, "transBlat %s %s dynamic %s\n", blat->host, blat->port, db);
+    else
+        fprintf(f, "transBlat %s %s\n", blat->host, blat->port);
     }
 
 fprintf(f, "\n");
