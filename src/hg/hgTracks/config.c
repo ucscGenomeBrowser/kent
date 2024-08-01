c3a239aa11cde08e85f3235b3e47861a507215cc
Christopher Lee <chmalee@ucsc.edu>
2024-08-01 13:34:53 -0700

    Add a configure page option for controlling whether hgc clicks open in pop-ups, refs #33216

--- src/hg/hgTracks/config.c
+++ src/hg/hgTracks/config.c
@@ -743,6 +743,17 @@ hPrintf("</TD><TD>");
 hPrintf("Enable highlight with drag-and-select "
         "(if unchecked, drag-and-select always zooms to selection)");
 hPrintf("</TD></TR>\n");
+
+// check if we can do hgc pages in a pop up before putting up the user control
+if (cfgOptionBooleanDefault("canDoHgcInPopUp", FALSE))
+    {
+    // put a checkbox, on by default, to control whether item clicks stay on hgTracks or
+    // go to hgTracks
+    hPrintf("<tr><td>");
+    hCheckBox("doHgcInPopUp", cartUsualBoolean(cart, "doHgcInPopUp", TRUE));
+    hPrintf("<td>Item clicks open in pop-up</td></tr>\n");
+    }
+
 hTableEnd();
 
 
