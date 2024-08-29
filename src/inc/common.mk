e10c349f9e394f6dfa688a2c83111115428b9535
Mark Diekhans <markd@ucsc.edu>
2024-08-28 23:49:05 -0700

    sort unpredictable output results

--- src/inc/common.mk
+++ src/inc/common.mk
@@ -123,7 +123,7 @@ endif
 ifeq (${HALDIR},)
     # ONLY on hgwdev, not any other machine here (i.e. hgcompute-01)
     ifeq (${IS_HGWDEV},yes)
-      HALDIR = /hive/groups/browser/hal/build/hal.2020-12-18
+      HALDIR = /hive/groups/browser/hal/build/hal.2024-08-20
       ifneq ($(wildcard ${HALDIR}),)
         ifeq (${USE_HAL},)
           USE_HAL=1
