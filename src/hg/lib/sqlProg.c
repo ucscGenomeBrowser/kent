a07f02977c359fe3bda4dd0893e76d3cf3c498f2
Mark Diekhans <markd@ucsc.edu>
2024-11-23 18:45:00 -0800

    improved hgsql error message when hg.conf is missing.  Simplified error message to remove internal and inccorect details.  #20323

--- src/hg/lib/sqlProg.c
+++ src/hg/lib/sqlProg.c
@@ -135,9 +135,9 @@ if (write (fileNo, paddedGroup, strlen(paddedGroup)) == -1)
 
 char *settings = sqlProfileToMyCnf(profile);
 if (!settings)
-    errAbort("Could not connect to MySQL. Profile '%s.*' not found in sqlProfileToMyCnf() -- failed for file '%s' with errno %d. "
-            "Most likely the statements %s.host, %s.user and %s.password are missing in ~/.hg.conf or (...)/cgi-bin/hg.conf.", 
-            profile, defaultFileName, errno, profile, profile, profile);
+    errAbort("Could not connect to MySQL. Profile '%s.*' not found. "
+            "Most likely the statements %s.host, %s.user and %s.password are missing in ~/.hg.conf or (...)/cgi-bin/hg.conf or the conf file was not found",
+            profile, profile, profile, profile);
 if (sameString(prog, "mysqldump"))
     {  // need to suppress the database setting, it messes up mysqldump and is not needed. comment it out
     settings = replaceChars(settings, "\ndatabase=", "\n#database=");
