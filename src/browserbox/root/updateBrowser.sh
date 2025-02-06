1f1580e080a70d06a0330a5693d47e1c3bcac077
Jonathan Casper <jcasper@soe.ucsc.edu>
2025-02-06 12:23:16 -0800

    New plan, letting GBiB update as normal but from a frozen set of binaries for now.  refs #35183

--- src/browserbox/root/updateBrowser.sh
+++ src/browserbox/root/updateBrowser.sh
@@ -45,7 +45,7 @@
 RSYNCOPTS="-ltrvh"
 # rsync server for CGIs and html files
 RSYNCSRC="rsync://hgdownload.soe.ucsc.edu"
-RSYNCCGIBIN=cgi-bin
+RSYNCCGIBIN=cgi-bin.v476
 RSYNCHTDOCS=htdocs
 UPDATEFLAG=http://hgdownload.soe.ucsc.edu/gbib/lastUpdate
 LOGFILE=/var/log/gbibUpdates.log
@@ -196,10 +196,6 @@ if [ -f /tmp/lastJob.pid ] && [ "$(ps x -o pgid | grep $(cat /tmp/lastJob.pid) |
     echo a hgMirror job is running right now, not updating
     exit 4
 fi
-
-# Avoid updating GBiB binaries while we work through some OS upgrades and changes to our
-# development environment.
-exit 0
 
 # --- now do the update ---
 
