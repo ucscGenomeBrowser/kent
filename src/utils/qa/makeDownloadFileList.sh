9d44445a77c9481b087864904bfbe189955af554
Lou Nassar <lrnassar@ucsc.edu>
2025-02-21 11:25:58 -0800

    I forgot to remove a troubleshooting stanza. Oops. Refs #34321

--- src/utils/qa/makeDownloadFileList.sh
+++ src/utils/qa/makeDownloadFileList.sh
@@ -6,11 +6,6 @@ today=$(date +"%d-%m-%Y")
 # Get the current day of the month for cron report
 day=$(date +%d)
 
-# Echo only if it's the 1st
-if [[ "$day" == "01" ]]; then
-    echo "I ran!"
-fi
-
 ssh qateam@hgdownload2 "find /usr/local/apache/htdocs/ -type f -print0 2>/dev/null | xargs -0 stat" > /hive/users/qateam/hgDowloadFileLists/download2.htdocs.$today.txt
 ssh qateam@hgdownload2 "find /data/ -type f -print0 2>/dev/null | xargs -0 stat" > /hive/users/qateam/hgDowloadFileLists/download2.data.$today.txt
 ssh qateam@hgdownload1 "find /usr/local/apache/htdocs/ -type f -print0 2>/dev/null | xargs -0 stat" > /hive/users/qateam/hgDowloadFileLists/download1.htdocs.$today.txt
