92a60d6e8e432af6a135b7ed99743517f8312267
Max <max@soe.ucsc.edu>
2025-04-29 02:40:31 -0700

    doing what I wrote in the previous commit message and actually setting to 10 seconds, not 15, refs #35585
    (cherry picked from commit 2a79675fbba7f505b6236c0ec13eccfdb04a183e)

--- src/hg/lib/botDelay.c
+++ src/hg/lib/botDelay.c
@@ -261,6 +261,7 @@ else
            "you think this delay is being imposed unfairly, please contact genome-www@soe.ucsc.edu."
            ,hogHost, asctime(localtime(&now)), botDelayMillis);
     puts("</body></html>");
+    sleep(10);
     }
 cgiExitTime(cgiExitName, enteredMainTime);
 exit(0);
