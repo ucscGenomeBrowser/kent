ucscGenomeBrowser/kent

commit 13f391382e54e6589f383d87069040926c910196

fixing web.c to output cookie so hgGateway uses cookies, refs #35824

src/hg/lib/web.c
@@ -119,8 +119,6 @@ printf("\t\t<meta charset=\"windows-1252\">\n"   // Be nice to be utf-8, but tha
 void webStartText()
 /* output the head for a text page */
 {
-/*printf("Content-Type: text/plain\n\n");*/
-
 webHeadAlreadyOutputed = TRUE;
 webInTextMode = TRUE;
 webPushErrHandlers();
@@ -176,7 +174,10 @@ dnaUtilOpen();
 
 
 if (withHttpHeader)
-    puts("Content-type:text/html\n");
+    {
+    char *cookieName = hUserCookie();
+    cartWriteHeaderAndCont(theCart, cookieName);
+    }
 
 // If the database name is not already in the title string, add it now
 if (endsWith(scriptName, "hgc") && db != NULL && !stringIn(db, textOutBufDb))
@@ -491,7 +492,8 @@ static void webStartGbOptionalBanner(struct cart *cart, char *db, char *title, b
    Optionally display banner above menubar.  Use flag with hgGateway, till that is migrated.
  */
 {
-puts("Content-type:text/html\n");
+char *cookieName = hUserCookie();
+cartWriteHeaderAndCont(cart, cookieName);
 
 char *csp = getCspMetaHeader();
 if (hgGateway)

src/hg/lib/cart.c
@@ -1523,8 +1523,7 @@ void printCaptcha()
     if (!cfSiteKey)
         return;
 
-    puts("Content-Type:text/html");
-    puts("\n");
+    puts("Content-Type:text/html\n"); // puts outputs one newline. Header requires two newlines.
     puts("<html><head>");
     puts("<script>");
     printf("function showWidget() { \n"
@@ -1574,13 +1573,11 @@ if (okUserAgent && sameOk(cgiUserAgent(), okUserAgent))
 if (userId && userIdFound && !cgiOptionalString("captcha"))
     return;
 
-// Do not show a captcha if the request is an AJAX request coming from jQuery
-// This is a hack to work around an error in hgGateway and may soon disappear.
+// This is a hack to let all AJAX requests pass without cookies, no needed anymore.
 // It's a hack because the header can be set by any curl script to get around
-// the captcha. Right now, hgGateway is not setting any cookie at all, so the 
-// correct solution would be to change that.
-if (sameOk(getenv("HTTP_X_REQUESTED_WITH"), "XMLHttpRequest"))
-    return;
+// the captcha. 
+//if (sameOk(getenv("HTTP_X_REQUESTED_WITH"), "XMLHttpRequest"))
+    //return;
 
 char *token = cgiOptionalString("token");
 
@@ -2712,6 +2709,15 @@ for (h = httpHeaders; h != NULL; h = h->next)
     }
 }
 
+void cartWriteHeaderAndCont(struct cart* cart, char *cookieName)
+/* write http headers including cookie and content type line */
+{
+addHttpHeaders();
+cartWriteCookie(cart, cookieName);
+puts("Content-Type: text/html\n"); // puts outputs one newline. Headers requires two.
+cartDidContentType = TRUE;
+}
+
 struct cart *cartAndCookieWithHtml(char *cookieName, char **exclude,
                                    struct hash *oldVars, boolean doContentType)
 /* Load cart from cookie and session cgi variable.  Write cookie
@@ -2725,12 +2731,7 @@ popWarnHandler();
 popAbortHandler();
 
 if (doContentType && !cartDidContentType)
-    {
-    addHttpHeaders();
-    cartWriteCookie(cart, cookieName);
-    puts("Content-Type:text/html");
-    cartDidContentType = TRUE;
-    }
+    cartWriteHeaderAndCont(cart, cookieName);
 
 return cart;
 }
@@ -2776,7 +2777,7 @@ if (!initted && !cgiOptionalString("ajax"))
     {
     if (!cartDidContentType)
         {
-        puts("Content-Type:text/html\n");
+        puts("Content-Type: text/html\n");
         cartDidContentType = TRUE;
         }
     htmStart(stdout, "Early Error");

commit c62c78194cfad8453b2d7c74b304fde3ef10b2dc

Fix missing include bug introduced by 13f391382e54e658, refs browser-qa email

src/hg/inc/cart.h
@@ -451,6 +451,9 @@ struct cart *cartAndCookieNoContent(char *cookieName, char **exclude,
 /* Load cart from cookie and session cgi variable. Don't write out
  * content type or any HTML. */
 
+void cartWriteHeaderAndCont(struct cart* cart, char *cookieName);
+/* write http headers including cookie and content type line */
+
 struct cart *cartAndCookieWithHtml(char *cookieName, char **exclude,
 	struct hash *oldVars, boolean doContentType);
 /* Load cart from cookie and session cgi variable.  Write cookie

src/hg/lib/web.c
@@ -24,6 +24,7 @@
 #include "trackHub.h"
 #include "versionInfo.h"
 #include "asmAlias.h"
+#include "cart.h"
 
 #ifndef GBROWSE
 #include "axtInfo.h"