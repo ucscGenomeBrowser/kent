6faaa066eb3d0d5533adc3a116fe55ae3811c33e
Build Meister <jcasper@soe.ucsc.edu>
2025-06-30 15:32:07 -0700

    New version number v484

--- src/inc/srcVersion.h
+++ src/inc/srcVersion.h
@@ -1,3 +1,3 @@
 /* Copyright (C) 2014 The Regents of the University of California 
  * See README in this or parent directory for licensing information. */
-#define SRC_VERSION "483"
+#define SRC_VERSION "484"
