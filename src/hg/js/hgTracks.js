70571cdf8a42757ffc0ec79bf6bca9c1e064b1d3
max <max@soe.ucsc.edu>
2025-09-10 08:33:03 -0700

    first commit of igv js mostly just hacking around in JimRs code, refs #36337

--- src/hg/js/hgTracks.js
+++ src/hg/js/hgTracks.js
@@ -44,6 +44,8 @@ function initVars()
         genomePos.originalSize = $('#size').text().replace(/,/g, ""); // strip out any commas
         dragSelect.originalCursor = jQuery('body').css('cursor');
 
+        window.initIgvUcsc();
+
         imageV2.imgTbl = $('#imgTbl');
         // imageV2.enabled === true unless: advancedJavascript===false, or trackSearch, or config pg
         imageV2.enabled = (imageV2.imgTbl && imageV2.imgTbl.length > 0);
@@ -4290,6 +4292,7 @@ var imageV2 = {
         if (typeof showMouseovers !== 'undefined' && showMouseovers) {
             convertTitleTagsToMouseovers();
         }
+        window.igvBrowser.updateLoci([genomePos.get()]);
     },
 
     updateImgForId: function (html, id, fullImageReload, newJsonRec)
@@ -4490,6 +4493,7 @@ var imageV2 = {
             return false;
         }
         document.TrackHeaderForm.submit();
+        window.igv.initUcsc();
     },
 
     updateImgAndMap: function (response, status)
@@ -6034,3 +6038,310 @@ function hgtWarnTiming(maxSeconds) {
     notifBoxSetup("hgTracks", "hideSpeedNotification", msg);
     notifBoxShow("hgTracks", "hideSpeedNotification");
 }
+
+(function () {
+
+    let filePicker = null;
+    let igvBrowser = null;
+
+    // Channel for communicating with file picker window
+    const channel = new BroadcastChannel('igv_file_channel');
+
+    // Represents the current state of the UCSC browser page.  Its not clear yet how this obtained, this is just a placeholder.
+    const ucscState = {};
+
+    function openFilePicker() {
+        return window.open('../admin/filePicker.html', 'filePicker' + Date.now(), 'width=600,height=800');
+    }
+
+    // Simulate a page reload restoring the UCSC user session, including the embedded igv sessin if any.  In this mockup we are
+    // storing the session in local storage.
+    // window.addEventListener("DOMContentLoaded", async (event) => {
+    window.initIgvUcsc = async function() {
+
+        // We are simulating the UCSC user session (a cookie?) with local storage.
+        const sessionString = localStorage.getItem("ucscSession");
+        //const sessionString = setCartVar;
+        if (sessionString) {
+            const ucscSession = JSON.parse(sessionString);
+            if (ucscSession.locus) {
+                ucscState.locus = ucscSession.locus;
+            }
+            ucscSession.genomeID = getDb();
+            ucscSession.locus = genomePos.get();
+
+            //const posEl = document.getElementById("positionDisplay");
+            //if (posEl) posEl.innerText = ucscState.locus;
+
+            // Restore the previously saved igv session, if any.
+            if (ucscSession.igvSession) {
+                const igvSessionString = igv.uncompressSession(`blob:${ucscSession.igvSession}`);
+                const igvSession = JSON.parse(igvSessionString);
+
+                // Reconnect any file-based tracks to the actual File objects.
+                if (igvSession.tracks) {
+                    const failed = await igvHelper.restoreTrackConfigurations(igvSession.tracks);
+                    if (failed.length > 0) {
+                        if (filePicker && !filePicker.closed) {
+                            channel.postMessage({type: "restoreFiles", files: failed});
+                            filePicker.focus();
+                            return;
+                        }
+                        if (filePicker) {
+                            // File picker is already open, this is not expected.
+                            alert(
+                                `The following file connections could not be restored:\n<ul>${
+                                    failed.map(f => `<li>${f}</li>`).join('')
+                                }</ul>\nTo restore the connection open the file manager by selecting 'Add IGV Tracks' followed by 'Choose Files' and select the files.`
+                            );
+                        } else {
+                            filePicker = openFilePicker();
+                            filePicker.onload = () => {
+                                channel.postMessage({type: "restoreFiles", files: failed});
+                                //alert(
+                                //    `The following file connections could not be restored:\n<ul>${
+                                //        failed.map(f => `<li>${f}</li>`).join('')
+                                //    }</ul>\nTo restore the connection select 'Choose Files' and select the files.`
+                                //)
+                            };
+                        }
+                    }
+                }
+
+                // Override locus  in the IGV session with the UCSC locus
+                // igvSession.locus = ucscState.locus;
+                igvSession.locus = hgTracks.chromName+":"+hgTracks.winStart+"-"+hgTracks.winEnd;
+                await createIGVBrowser(igvSession);
+            }
+        }
+    };
+
+    // Detect a page refresh (visibility change to hidden) and save the session to local storage.  This is meant to
+    // simulate  UCSC browser session handling.
+    // XX TODO - not enough time for sending an HTTP request to update cart - need a better system!
+    document.onvisibilitychange = () => {
+        if (document.visibilityState === "hidden") {
+            const ucscSession = Object.assign({}, ucscState); // Obviously a gross simplification here.
+            //setCartVar("igvState", JSON.stringify(ucscSession), null, false);
+            if (igvBrowser) {
+                ucscSession.igvSession = igvBrowser.compressedSession();
+            }
+            localStorage.setItem("ucscSession", JSON.stringify(ucscSession));
+        }
+    };
+
+    // The "Add IGV track" button handler.  The button opens the file picker window, unless
+    // it is already open in which case it brings that window to the front.  Tracks are added
+    // from the filePicker page by selecting track files.
+    window.addEventListener("DOMContentLoaded", () => {
+        document.getElementById('hgtIgv').addEventListener('click', async function (e) {
+            e.preventDefault(); // our
+            if (filePicker && !filePicker.closed) {
+                filePicker.focus();
+                return;
+            } else {
+
+                // A filePicker might be open from a previous instance of this page.  We can detect this by sending
+                // a message on the channel and waiting briefly for a response, but we cannot get a reference to the window
+                // so we ask the user to bring it to the front.
+
+                const responded = await pingFilePicker(channel);
+                if (responded) {
+                    alert("File picker is already open. Please switch to that window.");
+                } else {
+                    // No filePicker found, open a new one.
+                    filePicker = openFilePicker();
+                }
+            }
+        });
+    });
+
+
+    /**
+     * Create an IGV browser instance and insert it into the image table as a new row. The IGV browser essentially becomes
+     * a track in the UCSC browser context.  This function is called when the user adds the first IGV track, or
+     * the igv session is restored on page reload.
+     *
+     * @param config -- The IGV browser configuration object.  Must include a reference genome, but might also include
+     *                  an initial locus or tracks.
+     * @returns {Promise<Browser>}
+     */
+    async function createIGVBrowser(config) {
+
+        // Insert the IGV row into the image table.
+        const imgTbl = document.getElementById('imgTbl');
+        const tbody = imgTbl.querySelector('tbody');
+        const igvRow = document.createElement('tr');
+        igvRow.id = 'tr_igv';
+        igvRow.innerHTML = `
+                        <td style="background: grey">
+                            <div style="width:13px"></div>
+                        </td>
+                        <td style="background: #24d6ff">
+                            <div style="width: 140px;">blank</div>
+                        </td>
+                        <td>
+                            <div id="igv_div" style="width: auto"></div>
+                        </td>
+                    `;
+        tbody.appendChild(igvRow);
+
+        // Ammend the config to remove most of the IGV widgets.  We only want the track display area.
+        Object.assign(config, {
+            showNavigation: false,
+            showIdeogram: false,
+            showRuler: false,
+            showSequence: false,
+            showAxis: false
+            //TODO discuss if we want IGV track drag handles.  It allows users to rearrange IGV tracks within the IGV area
+            //showTrackDragHandles: false
+        });
+
+        const div = document.getElementById("igv_div");
+        igvBrowser = await igv.createBrowser(div, config);
+
+        // Add event handler to remove IGV row from table if all IGV tracks are removed.
+        igvBrowser.on('trackremoved', function (track) {
+
+            channel.postMessage({type: "removedTrack", config: track.config});
+
+            const allTracks = igvBrowser.findTracks(t => true);
+            if (allTracks.length === 0) {
+                igvRow.remove();
+                igvBrowser = null;
+            }
+        });
+
+        // Add event handler to track igv.js track dragging (locus change).  On the UCSC side this should be treated
+        // as if the user had dragged a track image
+        igvBrowser.on('locuschange', referenceFrameList => {
+                // We are currently not supporting multi-locus view, so there is a single reference frame.
+                const locusString = referenceFrameList[0].getLocusString();
+                // XX TODO I cannot run navigateInPlace(), because locuschange is triggered by
+                // createIgvBrowser(), and there is no need for yet another navigateInPlace() in then
+                alert(getDb()+"-"+encodeURIComponent(locusString));
+                //imageV2.navigateInPlace("db=" + getDb() + "&position=" + encodeURIComponent(locusString), null, false);
+                ucscState.locus = locusString;
+                //document.getElementById("positionDisplay").innerText = locusString;
+            }
+        );
+
+        window.igvBrowser = igvBrowser;
+        return igvBrowser;
+    }
+
+
+    // Respond to messages from the filePicker window.
+    channel.onmessage = async function (event) {
+        const msg = event.data;
+
+        if (msg.type === 'selectedFiles') {
+
+            // Convert file descriptor objects to igv.js track configuration objects.
+            const configs = igvHelper.getTrackConfigurations(event.data.files);
+
+            if (configs.length > 0) {
+
+                // Create igvBrowser if needed -- i.e. this is the first track being added.  State needs to be obtained
+                // from the UCSC browser for genome and locus.
+                if (!igvBrowser) {
+                    const defaultConfig = {
+                        reference: getMinimalReference(getDb()),
+                        locus: genomePos.getOriginalPos()
+                    };
+                    await createIGVBrowser(defaultConfig);
+                }
+
+                // First search for existing tracks referencing the same files.  This is to handle the situation
+                // of a user closing the file picker window, thus loosing file references, then reopening the file picker
+                // to restore them.
+                const newConfigs = [];
+
+                for (let config of configs) {
+                    const id = config.url.id;
+                    const matchingTracks = igvBrowser.findTracks(t => t.url && id === t.url.id);
+                    if (matchingTracks.length > 0) {
+                        // Just select the first matching track, there should only be one.  Restore its file reference(s).
+                        matchingTracks[0].config.url.file = config.url.file;
+                        if (config.indexURL) {
+                            matchingTracks[0].config.indexURL.file = config.indexURL.file;
+                        }
+                    } else {
+                        // This is a new track
+                        newConfigs.push(config);
+                    }
+                }
+
+                igvBrowser.loadTrackList(newConfigs);
+            }
+        }
+        if (msg.type === 'loadURL') {
+            igvBrowser.loadTrack(config);
+        }
+    };
+
+    /**
+     * Send a "ping" message to the file picker window and wait up to 100 msec for a "pong" response.  Used to
+     * determine if a file picker window is already open.
+     * @param channel
+     * @returns {Promise<unknown>}
+     */
+    async function pingFilePicker(channel) {
+        const waitForResponse = new Promise((resolve) => {
+            const originalOnMessage = channel.onmessage;
+            channel.onmessage = (event) => {
+                if (event.data && event.data.type === "pong") {
+                    channel.onmessage = originalOnMessage;
+                    resolve(true);
+                }
+            };
+            setTimeout(() => {
+                channel.onmessage = originalOnMessage;
+                resolve(false);
+            }, 100);
+        });
+
+        channel.postMessage({type: "ping"});
+
+        const responded = await waitForResponse;
+        return responded;
+    }
+
+    /**
+     * Return a minimal reference object for the given genomeID. We don't need or want default IGV tracks, only the
+     * reference sequence.
+     *
+     * Eventually expand or reimplement this function to support all UCSC browser genomes.
+     *
+     * @param genomeID
+     * @returns {{id: string, twoBitURL: string}}
+     */
+    function getMinimalReference(genomeID) {
+
+        switch (genomeID) {
+            case "hg19":
+                return {
+                    "id": "hg19",
+                    "twoBitURL": "https://hgdownload.soe.ucsc.edu/goldenPath/hg19/bigZips/hg19.2bit",
+                };
+            case "hg38":
+                return {
+                    "id": "hg38",
+                    "twoBitURL": "https://hgdownload.soe.ucsc.edu/goldenPath/hg38/bigZips/hg38.2bit",
+                };
+            case "mm10":
+                return {
+                    "id": "mm10",
+                    "twoBitURL": "https://hgdownload.soe.ucsc.edu/goldenPath/mm10/bigZips/mm10.2bit",
+                };
+            case "mm39": // GRCm39
+                return {
+                    "id": "mm39",
+                    "twoBitURL": "https://hgdownload.soe.ucsc.edu/goldenPath/mm39/bigZips/mm39.2bit",
+                };
+            default:
+                throw new Error(`Unsupported genomeID: ${genomeID}`);
+        }
+    }
+
+}) ();
