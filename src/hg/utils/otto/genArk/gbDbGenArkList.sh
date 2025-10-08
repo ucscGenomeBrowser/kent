7176aa3eb099704a9b1372bdc27930f172567d13
Hiram Clawson <hiram@soe.ucsc.edu>
2025-10-08 16:22:01 -0700

    adding the listing of /gbdb/database/quickLift files refs #34917

--- src/hg/utils/otto/genArk/gbDbGenArkList.sh
+++ src/hg/utils/otto/genArk/gbDbGenArkList.sh
@@ -7,6 +7,8 @@ export machName=`uname -n | cut -d'.' -f1`
 export directoryList="gbdbGenArk.${machName}.${DS}"
 export statList="gbdbGenArkStat.${machName}.${DS}"
 export fileList="gbdbGenark.fl"
+export quickLiftFileList="gbdbQuickLift.fl"
+export quickLiftStat="gbdbQuickLiftStat.${machName}.${DS}"
 
 find -L /gbdb/genark/GCA /gbdb/genark/GCF -type d | sort \
   | gzip -c > /dev/shm/${directoryList}.gz
@@ -23,9 +25,24 @@ zcat /dev/shm/${fileList}.gz | xargs stat --printf="%Y\t%n\n" | gzip -c > /dev/s
 scp -p /dev/shm/${statList}.gz \
   otto@hgwdev:/hive/data/inside/GenArk/pushRR/logs/${Y}/${M}/ > /dev/null
 
+cd /gbdb
+ls -d */quickLift | while read Q
+do
+  find -L ./${Q} -type f
+done | sed -e 's#^./##;' | gzip -c > /dev/shm/${quickLiftFileList}.gz
+
+zcat /dev/shm/g${quickLiftFileList}.gz | xargs stat -L --printf="%Y\t%n\n" \
+  | gzip -c > /dev/shm/${quickLiftStat}.gz
+
+scp -p /dev/shm/${quickLiftStat}.gz \
+  otto@hgwdev:/hive/data/inside/GenArk/pushRR/logs/${Y}/${M}/ > /dev/null
+
 if [[ "${machName}" =~ ^(hgw1|hgwbeta)$ ]]; then
   scp -p /dev/shm/${statList}.gz \
     otto@hgwdev:/hive/data/inside/GenArk/pushRR/${machName}.todayList.gz > /dev/null
+  scp -p /dev/shm/${quickLiftStat}.gz \
+    otto@hgwdev:/hive/data/inside/GenArk/pushRR/${machName}.today.quickLiftList.gz > /dev/null
 fi
 
-rm -f /dev/shm/${fileList}.gz /dev/shm/${statList}.gz
+rm -f /dev/shm/${fileList}.gz /dev/shm/${statList}.gz \
+   /dev/shm/${quickLiftStat}.gz /dev/shm/${quickLiftFileList}.gz
