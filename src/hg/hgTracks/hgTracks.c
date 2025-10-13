71cbef499bd026b90eb7b45db9e0d16eca279e07
max <max@soe.ucsc.edu>
2025-10-13 08:49:15 -0700

    shortening track mouseover a little, refs #34922

--- src/hg/hgTracks/hgTracks.c
+++ src/hg/hgTracks/hgTracks.c
@@ -8558,11 +8558,9 @@ if (track->hasUi)
     struct trackDb *tdb = track->tdb;
 
     if (tdbIsSuper(tdb))
-        dyStringPrintf(dsMouseOver, " - this is a container track with %d subtracks of different types "
-                "(super track)", slCount(tdb->children));
+        dyStringPrintf(dsMouseOver, " - container supertrack (%d subtracks) ", slCount(tdb->children));
     else if (tdbIsComposite(tdb))
-        dyStringPrintf(dsMouseOver, " - this is a container track with %d subtracks of similar types "
-                "(composite track)", slCount(tdb->subtracks));
+        dyStringPrintf(dsMouseOver, " - container track (%d subtracks) ", slCount(tdb->subtracks));
 
     // Print icons before the title when any are defined
     hPrintIcons(track->tdb);
