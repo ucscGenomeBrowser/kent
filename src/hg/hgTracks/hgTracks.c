64f0134fda55184221ebae340f9cf9c4ecc7a5a1
max <max@soe.ucsc.edu>
2026-01-12 05:16:58 -0800

    small note to hub errors, refs #36916

--- src/hg/hgTracks/hgTracks.c
+++ src/hg/hgTracks/hgTracks.c
@@ -9906,7 +9906,7 @@ if (!hideControls)
             if (group->errMessage)
                 {
                 hPrintf("<td colspan=8><b>Track hub error</b> ");
-                printInfoIcon("Use our track hub debugging help under <i>My Data > Track Hubs > Hub Development</i>. Or click the disconnect button above to improve performance now temporarily. Contact us if you cannot resolve the issue.");
+                printInfoIcon("Use our track hub debugging help under <i>My Data > Track Hubs > Hub Development</i>. Note that you need to switch off caching there to see changes that you made to the track hub files without delay. Or click the disconnect button above to improve performance now temporarily. Contact us if you cannot resolve the issue.");
                 hPrintf(": ");
                 hPrintf("<i>%s</i>", group->errMessage);
                 hPrintf("</td>\n");
