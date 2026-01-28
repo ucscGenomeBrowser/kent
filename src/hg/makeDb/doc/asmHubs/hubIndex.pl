c7eebb45ae05223d9f771acbb6abcbe061c4d6b6
Hiram Clawson <hiram@soe.ucsc.edu>
2026-01-28 14:45:46 -0800

    changing the word Archive in reference to GenArk to become Repository refs #37027

--- src/hg/makeDb/doc/asmHubs/hubIndex.pl
+++ src/hg/makeDb/doc/asmHubs/hubIndex.pl
@@ -17,12 +17,12 @@ if ($thisMachine ne "hgdownload") {
 #############################################################################
 sub startHtml() {
 printf '<!DOCTYPE HTML 4.01 Transitional>
-<!--#set var="TITLE" value="GenArk: UCSC Genome Archive" -->
+<!--#set var="TITLE" value="GenArk: UCSC Genome Repository" -->
 <!--#set var="ROOT" value=".." -->
 
 <!--#include virtual="$ROOT/inc/gbPageStartHardcoded.html" -->
 
-<h1>GenArk: UCSC Genome Archive</h1>
+<h1>GenArk: UCSC Genome Repository</h1>
 
 '
 }
