06767bdb38bd195d69b84b79499431709e625382
max <max@soe.ucsc.edu>
2026-05-28 01:35:36 -0700

    small wording change to hgTrackUi parent link, no redmine yet

--- src/hg/hgTrackUi/hgTrackUi.c
+++ src/hg/hgTrackUi/hgTrackUi.c
@@ -3638,7 +3638,7 @@ if (!tdb->parent)
 // show super-track info
 struct trackDb *tdbParent = tdb->parent;
 
-printf("<b>Back to parent track: "
+printf("<b>Configure track container: "
            "<img height=12 src='../images/ab_up.gif'>"
             "<a href='%s?%s=%s&c=%s&g=%s'>%s </a></b>",
             hgTrackUiName(), cartSessionVarName(), cartSessionId(cart),
