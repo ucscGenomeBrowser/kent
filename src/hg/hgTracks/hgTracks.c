48d45fa3cbdddcb62775f1fe01eef865407e035e
Brian Raney <braney@soe.ucsc.edu>
2026-06-24 13:47:07 -0700

    Revert "Add a right click option to change color or background highlight color of individual track items. Only works for bed like items, refs #37778"
    This reverts commit abd7278ac7167ede325d8c144a35ea59a0798766.

--- src/hg/hgTracks/hgTracks.c
+++ src/hg/hgTracks/hgTracks.c
@@ -9111,7 +9111,6 @@ zoomedToCodonLevel = (ceil(virtWinBaseCount/3) * tl.mWidth) <= fullInsideWidth;
 zoomedToCodonNumberLevel = (ceil(virtWinBaseCount/3) * tl.mWidth * 5) <= fullInsideWidth;
 zoomedToCdsColorLevel = (virtWinBaseCount <= fullInsideWidth*3);
 
-createItemColorHash();
 
 if (psOutput != NULL)
    {
@@ -9124,10 +9123,8 @@ if (psOutput != NULL)
 
 /* Tell browser where to go when they click on image. */
 hPrintf("<FORM ACTION=\"%s\" NAME=\"TrackHeaderForm\" id=\"TrackHeaderForm\" METHOD=\"GET\">\n\n", hgTracksName());
-jsonObjectAdd(jsonForClient, "insideX", newJsonNumber(insideX));
+jsonObjectAdd(jsonForClient, "insideX", newJsonNumber(insideX)); 
 jsonObjectAdd(jsonForClient, "revCmplDisp", newJsonBoolean(revCmplDisp));
-jsonObjectAdd(jsonForClient, "itemColors",
-              newJsonString(cartUsualString(cart, "itemColors", "")));
 
 if (hPrintStatus()) cartSaveSession(cart);
 
