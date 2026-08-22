ucscGenomeBrowser/kent

commit af6898753ba6d5f3872957cd270872ce4c4e3dfc

tolerate if browser CSS theme files do not exist, cleaning up the resource link mess a little, refs #7867

src/hg/hgTracks/config.c
@@ -28,10 +28,10 @@ static void themeDropDown(struct cart* cart)
  * */
 {
 struct slName* themes = cfgNamesWithPrefix("browser.theme.");
-//struct slName* themes = cfgNames();
 if (themes==NULL)
     return;
 
+slNameSort(&themes);
 hPrintf("<TR><TD>website style:");
 hPrintf("<TD style=\"text-align: right\">");
 
@@ -43,8 +43,10 @@ el = themes;
 for (el = themes; el != NULL && i<50; el = el->next)
     {
     char* name = el->name;
-    name = chopPrefix(name); // chop off first two words
+    name = chopPrefix(name); // chop off first three words
     name = chopPrefix(name);
+    name = chopPrefix(name);
+    replaceChar(name, '_', ' ');
     labels[i] = name;
     i++;
     }

src/hg/lib/cart.c
@@ -2621,7 +2621,7 @@ char *cartTheme = cartOptionalString(cart, "theme");
 // XXXX which setting should take precedence? Currently browser.theme does.
 
 char *styleFile = cfgOption("browser.style");
-if(styleFile != NULL)
+if (styleFile != NULL)
     {
     char buf[512];
     safef(buf, sizeof(buf), "<link rel='stylesheet' href='%s' type='text/css'>", styleFile);
@@ -2630,7 +2630,7 @@ if(styleFile != NULL)
     webSetStyle(copy);       // for web.c, used by hgc
     }
 
-if(isNotEmpty(cartTheme))
+if (isNotEmpty(cartTheme))
     {
     char *themeKey = catTwoStrings("browser.theme.", cartTheme);
     styleFile = cfgOption(themeKey);
@@ -2638,8 +2638,8 @@ if(isNotEmpty(cartTheme))
     if (isEmpty(styleFile))
         return;
 
-    char * link = webTimeStampedLinkToResourceOnFirstCall(styleFile, TRUE); // resource file link wrapped in html
-    if (link != NULL)
+    char * link = webCssLink(styleFile, FALSE); // resource file link wrapped in html
+    if (link != NULL && !sameOk(link, "<>")) // "<>" means "default settings" = "no file"
         {
         htmlSetStyleTheme(link); // for htmshell.c, used by hgTracks
         webSetStyle(link);       // for web.c, used by hgc

src/hg/inc/web.h
@@ -340,8 +340,15 @@ void webFinishPartialLinkOutTable(int rowIx, int itemPos, int maxPerRow);
 boolean validateGisaidUser(struct cart *cart);
 /* validate if the web user is an authenticated GISAID user */
 
+char *webCssLink(char *fileName, boolean mustExist);
+/* alternative for webTimeStampedLinkToResource for CSS files: prints a time-stamped
+ * link to a CSS file to stdout */
+
 char *webTimeStampedLinkToResource(char *fileName, boolean wrapInHtml);
-// Returns full path of timestamped link to the requested resource file (js, or css).
+// Consider using webCssLink instead of this function. Make new functions webJsLink(), webPngLink()
+// if not using CSS files.
+
+// Deprecated. Returns full path of timestamped link to the requested resource file (js, or css).
 // If wrapInHtml, then returns link embedded in style or script html. Free after use.
 // NOTE: png, jpg and gif should also be supported but are untested.
 

src/hg/lib/web.c
@@ -1219,6 +1219,50 @@ void webFinishPartialLinkTable(int rowIx, int itemPos, int maxPerRow)
 finishPartialTable(rowIx, itemPos, maxPerRow, webPrintLinkCellStart);
 }
 
+static struct dyString* getHtdocsSubdir(char *dirName)
+/* return dystring with subdirectory under htDocs, tolerant of missing docRoot */
+{
+struct dyString *fullDirName = NULL;
+char *docRoot = hDocumentRoot();
+if (docRoot != NULL)
+    fullDirName = dyStringCreate("%s/%s", docRoot, dirName);
+else
+    // tolerate missing docRoot (i.e. when running from command line)
+    fullDirName = dyStringCreate("%s", dirName);
+return fullDirName;
+}
+
+char *webCssLink(char *fileName, boolean mustExist)
+/* alternative for webTimeStampedLinkToResource for CSS files: returns a string with a time-stamped
+ * link to a CSS file as a html fragment <link .... >. returns empty string if file does not exist.
+ * */
+{
+// construct the absolute path to the file on disk
+char *relDir = cfgOptionDefault("browser.styleDir","style");
+struct dyString *absDir = getHtdocsSubdir(relDir);
+struct dyString *absFileName = dyStringCreate("%s/%s", dyStringContents(absDir), fileName);
+
+struct dyString *htmlFrag = NULL;
+if (!fileExists(dyStringContents(absFileName)))
+    {
+    if (mustExist)
+        errAbort("webTimeStampedLinkToResource: file: %s doesn't exist.\n", 
+                dyStringContents(absFileName));
+    else
+        htmlFrag = dyStringNew(0);
+    }
+else
+    {
+    // construct a link with the relative path to the file on the web server
+    long mtime = fileModTime(dyStringContents(absFileName));
+    htmlFrag = dyStringCreate("<link rel='stylesheet' href='../%s/%s?v=%ld' type='text/css'>\n", relDir, fileName, mtime);
+    }
+
+dyStringFree(&absFileName);
+dyStringFree(&absDir);
+return dyStringContents(htmlFrag);
+}
+
 char *webTimeStampedLinkToResource(char *fileName, boolean wrapInHtml)
 // If wrapInHtml
 //   returns versioned link embedded in style or script html (free after use).
@@ -1260,6 +1304,7 @@ else if (style)
     dirName = cfgOptionDefault("browser.styleDir","style");
 else if (image)
     dirName = cfgOptionDefault("browser.styleImagesDir","style/images");
+
 struct dyString *fullDirName = NULL;
 char *docRoot = hDocumentRoot();
 if (docRoot != NULL)
@@ -1267,6 +1312,7 @@ if (docRoot != NULL)
 else
     // tolerate missing docRoot (i.e. when running from command line)
     fullDirName = dyStringCreate("%s", dirName);
+
 if (!fileExists(dyStringContents(fullDirName)))
     errAbort("webTimeStampedLinkToResource: dir: %s doesn't exist. (host: %s)\n",
              dyStringContents(fullDirName), httpHost);

commit a08ccd7dd4930bff5bcf0a32bd35ec22db71215f

Fix the browser.theme drop down, which has never applied a theme, and retire browser.background and browser.bgcolor from the example configs. refs #38158
themeDropDown built the menu label from the hg.conf key and posted that label
back, while setThemeFromCart looks a theme up by key, so no entry carrying a sort
prefix or an underscore could ever resolve.  Broken since af6898753ba (2023)
introduced that key format and changed the menu side without the lookup side.
themeDropDown now passes the key suffix as the option value and the prettified
label as the display text, using cgiMakeDropListWithVals.

setThemeFromCart compared the generated link against "<>" instead of the config
value, so the "no theme file" marker never fired.  It also let an empty link from
a missing CSS file overwrite browser.style with nothing.  Both now test the right
string and leave browser.style alone when there is no theme to apply.

browser.background has not been read by any CGI since hBackgroundImage() and its
callers were removed in 2012 (8e6fcda9f58), and browser.bgcolor has never been
read at all, yet ex.hg.conf presented browser.background as a working setting.
Commented both out with a note, in ex.hg.conf and the three browserbox configs.

ex.hg.conf also documented two conflicting browser.theme formats in two places.
The comma form stopped working in 2023.  Consolidated to a single block that
describes what the code actually does.

src/browserbox/home/browser/.hg.conf
@@ -232,14 +232,16 @@ central.domain=127.0.0.1
 #	databases for multiple hosts, set a distinct cookie name per
 #	central database.
 # central.cookie=hguid.mylab
-#	personalize the background of the browser with a specified jpg
-#	floret.jpg is the standard UCSC default
-browser.background=../images/floret.jpg
+#	browser.background is dead: hBackgroundImage() and its callers were removed in
+#	2012 (commit 8e6fcda9f58) and no CGI has read this key since.  Set a background
+#	image in CSS instead, via browser.style.  refs #38158
+#browser.background=../images/floret.jpg
 
 # Change this default documentRoot if different in your installation,
 #	to allow some of the browser cgi binaries to find help text files
 browser.documentRoot=/usr/local/apache/htdocs
-#	personalize the background of CGIs that don't use images
+#	browser.bgcolor is dead too, no CGI has ever read it.  Set colors in the CSS
+#	file named by browser.style.  refs #38158
 #browser.bgcolor=FFF9D2
 
 # optional location of grepIndex files

src/browserbox/root/hg.conf.offline
@@ -257,14 +257,16 @@ central.domain=HTTPHOST
 #	databases for multiple hosts, set a distinct cookie name per
 #	central database.
 # central.cookie=hguid.mylab
-#	personalize the background of the browser with a specified jpg
-#	floret.jpg is the standard UCSC default
-browser.background=../images/floret.jpg
+#	browser.background is dead: hBackgroundImage() and its callers were removed in
+#	2012 (commit 8e6fcda9f58) and no CGI has read this key since.  Set a background
+#	image in CSS instead, via browser.style.  refs #38158
+#browser.background=../images/floret.jpg
 
 # Change this default documentRoot if different in your installation,
 #	to allow some of the browser cgi binaries to find help text files
 browser.documentRoot=/usr/local/apache/htdocs
-#	personalize the background of CGIs that don't use images
+#	browser.bgcolor is dead too, no CGI has ever read it.  Set colors in the CSS
+#	file named by browser.style.  refs #38158
 #browser.bgcolor=FFF9D2
 
 # optional location of grepIndex files

src/browserbox/usr/local/apache/cgi-bin/hg.conf
@@ -274,14 +274,16 @@ central.domain=HTTPHOST
 #	databases for multiple hosts, set a distinct cookie name per
 #	central database.
 # central.cookie=hguid.mylab
-#	personalize the background of the browser with a specified jpg
-#	floret.jpg is the standard UCSC default
-browser.background=../images/floret.jpg
+#	browser.background is dead: hBackgroundImage() and its callers were removed in
+#	2012 (commit 8e6fcda9f58) and no CGI has read this key since.  Set a background
+#	image in CSS instead, via browser.style.  refs #38158
+#browser.background=../images/floret.jpg
 
 # Change this default documentRoot if different in your installation,
 #	to allow some of the browser cgi binaries to find help text files
 browser.documentRoot=/usr/local/apache/htdocs
-#	personalize the background of CGIs that don't use images
+#	browser.bgcolor is dead too, no CGI has ever read it.  Set colors in the CSS
+#	file named by browser.style.  refs #38158
 #browser.bgcolor=FFF9D2
 
 # optional location of grepIndex files

src/hg/hgTracks/config.c
@@ -23,10 +23,28 @@
 #include "trackHub.h"
 #include "versionInfo.h"
 
+static void themeMenuEntry(char *cfgName, char **retLabel, char **retValue)
+/* Split an hg.conf browser.theme.* key into the menu label and the option value.
+ * The value is everything after "browser.theme.", which is what setThemeFromCart
+ * looks the theme up by, so it must survive the round trip through the form
+ * untouched.  The label is only the part after the last '.', with underscores as
+ * spaces, so a sort prefix like browser.theme.2.Sans_Serif shows as "Sans Serif".
+ * The first letter is upper cased, which the plain hDropList used to do for us. */
+{
+char *value = cloneString(cfgName + strlen("browser.theme."));
+char *label = cloneString(findTail(value, '.'));
+replaceChar(label, '_', ' ');
+label[0] = toupper((unsigned char)label[0]);
+*retLabel = label;
+*retValue = value;
+}
+
 static void themeDropDown(struct cart* cart)
-/* Create drop down for UI themes. 
- * specfied in hg.conf like this
- * browser.theme.modern=background.png,HGStyle
+/* Create drop down for UI themes.
+ * specified in hg.conf like this
+ * browser.theme.modern=theme-modern.css
+ * optionally with a sort prefix
+ * browser.theme.3.Sans_Serif=theme-modern.css
  * */
 {
 struct slName* themes = cfgNamesWithPrefix("browser.theme.");
@@ -37,24 +55,18 @@ slNameSort(&themes);
 hPrintf("<TR><TD>website style:");
 hPrintf("<TD style=\"text-align: right\">");
 
-// create labels for drop down box by removing prefix from hg.conf keys
 char *labels[50];
+char *values[50];
 struct slName* el;
 int i = 0;
-el = themes;
 for (el = themes; el != NULL && i<50; el = el->next)
     {
-    char* name = el->name;
-    name = chopPrefix(name); // chop off first three words
-    name = chopPrefix(name);
-    name = chopPrefix(name);
-    replaceChar(name, '_', ' ');
-    labels[i] = name;
+    themeMenuEntry(el->name, &labels[i], &values[i]);
     i++;
     }
 
 char* currentTheme = cartOptionalString(cart, "theme"); 
-hDropList("theme", labels, i, currentTheme);
+cgiMakeDropListWithVals("theme", labels, values, i, currentTheme);
 slFreeList(themes);
 hPrintf("</TD>");
 }

src/hg/lib/cart.c
@@ -3157,8 +3157,10 @@ void setThemeFromCart(struct cart *cart)
  * defined for this theme Also set the "styleTheme", with additional styles
  * that can overwrite the main style settings */
 {
-// Get theme from cart and use it to get background file from config;
-// format is browser.theme.<name>=<stylesheet>[,<background>]
+// Get theme from cart and use it to get the stylesheet from config;
+// format is browser.theme.<name>=<cssFileInStyleDir>, where <name> may carry a
+// sort prefix, browser.theme.3.Sans_Serif=theme-modern.css.  A value of "<>"
+// means "no theme file", leaving whatever browser.style set.
 
 char *cartTheme = cartOptionalString(cart, "theme");
 
@@ -3179,11 +3181,15 @@ if (isNotEmpty(cartTheme))
     char *themeKey = catTwoStrings("browser.theme.", cartTheme);
     styleFile = cfgOption(themeKey);
     freeMem(themeKey);
-    if (isEmpty(styleFile))
+    // "<>" means "default settings" = "no file".  Test styleFile, not the link:
+    // webCssLink returns html or an empty string, never the "<>" marker itself.
+    if (isEmpty(styleFile) || sameString(styleFile, "<>"))
         return;
 
     char * link = webCssLink(styleFile, FALSE); // resource file link wrapped in html
-    if (link != NULL && !sameOk(link, "<>")) // "<>" means "default settings" = "no file"
+    // An empty link means the file was not found.  Leave browser.style alone rather
+    // than overwriting it with nothing.
+    if (isNotEmpty(link))
         {
         htmlSetStyleTheme(link); // for htmshell.c, used by hgTracks
         webSetStyle(link);       // for web.c, used by hgc

src/product/ex.hg.conf
@@ -367,9 +367,11 @@ central.domain=mylab.university.edu
 
 
 
-#	personalize the background of the browser with a specified jpg
-#	floret.jpg is the standard UCSC default
-browser.background=../images/floret.jpg
+#	browser.background is dead.  It set a BACKGROUND attribute on the BODY tag via
+#	hBackgroundImage(), which commit 8e6fcda9f58 removed in 2012 along with its four
+#	callers.  No CGI has read this key since.  Set a background image in CSS instead,
+#	with browser.style below.  refs #38158
+#browser.background=../images/floret.jpg
 
 
 # Change this default documentRoot if different in your installation,
@@ -377,13 +379,19 @@ browser.background=../images/floret.jpg
 browser.documentRoot=/usr/local/apache/htdocs
 
 # Options to personalize the visual style of the browser
-#   Personalize the background of CGIs that don't use images
+#   browser.bgcolor is dead, no CGI has ever read it.  Set colors in the CSS file
+#   named by browser.style.  refs #38158
 #browser.bgcolor=FFF9D2
-#   Add a custom CSS file
-#browser.style=gbib.css
-#   Give the user a choice between different background images and CSS files
-#browser.theme.modern=background.png,HGStyle
-#browser.theme.classic=floret.png,HGStyle-old
+#   Add a custom CSS file.  The value is emitted verbatim as a stylesheet href, so it
+#   can be any path the web server serves, inside the kent tree or not.  It is loaded
+#   after HGStyle.css.
+#   The file must qualify its body rules as body.cgi or body.hgTracks; HGStyle.css has
+#   class-qualified body rules that outrank a bare "body" selector.  Set a background
+#   with the "background" shorthand rather than "background-image": body.hgTracks sets
+#   repeat-x / left bottom / fixed, and those longhands survive a background-image
+#   rule, leaving the image as a band across the bottom instead of tiled.
+#browser.style=/style/gbib.css
+#   To offer the user a choice of styles, see browser.theme further down this file.
 
 # optional location of grepIndex files
 grepIndex.genbank=/data/tmp/grepIndex
@@ -755,14 +763,21 @@ hgc.ghostscriptPath=/usr/bin/ghostscript
 #resolvCmd=/var/www/tools/urlResolver /var/www/tools/config
 
 # user-specific CSS files that can be activated via the configuration menu.
-# Not active on our main site and still under development
-# The format is browser.theme.<numberForSorting>.<labelUnderscoresAreSpaces>=<filenameInStyleDirectory>
-# A filename of "<>" means "no file", just the default CSS settings.
+# Not active on our main site and still under development.
+# The format is browser.theme.<name>=<filenameInStyleDirectory>, one CSS file per entry.
+# The selected theme replaces browser.style, it does not add to it.
+#
+# <name> may carry a leading number to order the menu, <sortNumber>.<label>, and any
+# underscore in the label shows as a space.  Entries sort by <name>, so number them if
+# you care about the order.  A value of "<>" means "no theme file", which leaves
+# whatever browser.style set, so it makes a good first entry.
+#
+# The value is one filename, not a list.  The old comma form, background.png,HGStyle,
+# has not worked since 2023 and is silently ignored.  refs #38158
 #browser.theme.1.Default=<>
 #browser.theme.2.Classic=theme-classic.css
 #browser.theme.3.Sans_Serif=theme-modern.css
-#browser.theme.4.Light_Blue=theme-lightBlue.css
-#browser.theme.5.Dark=theme-dark.css
+#browser.theme.4.Dark=theme-dark.css
 
 # print the javascript includes to the following external javascript libraries that allow
 # the interactive tutorial to work: